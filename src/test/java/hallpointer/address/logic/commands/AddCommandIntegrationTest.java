7f3802c375db93a64c603e2911200ea07e6937cb
Daniel Lai <e0360577@u.nus.edu>
2024-09-15 17:24:16 +0800

    Change package names

---
+++ src/test/java/hallpointer/address/logic/commands/AddCommandIntegrationTest.java
@@ -0,0 +1,48 @@
+package hallpointer.address.logic.commands;
+
+import static hallpointer.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static hallpointer.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static hallpointer.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hallpointer.address.logic.Messages;
+import hallpointer.address.model.Model;
+import hallpointer.address.model.ModelManager;
+import hallpointer.address.model.UserPrefs;
+import hallpointer.address.model.person.Person;
+import hallpointer.address.testutil.PersonBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code AddCommand}.
+ */
+public class AddCommandIntegrationTest {
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newPerson_success() {
+        Person validPerson = new PersonBuilder().build();
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.addPerson(validPerson);
+
+        assertCommandSuccess(new AddCommand(validPerson), model,
+                String.format(AddCommand.MESSAGE_SUCCESS, Messages.format(validPerson)),
+                expectedModel);
+    }
+
+    @Test
+    public void execute_duplicatePerson_throwsCommandException() {
+        Person personInList = model.getAddressBook().getPersonList().get(0);
+        assertCommandFailure(new AddCommand(personInList), model,
+                AddCommand.MESSAGE_DUPLICATE_PERSON);
+    }
+
+}
---
+++ src/test/java/hallpointer/address/logic/commands/ListCommandTest.java
@@ -0,0 +1,39 @@
+package hallpointer.address.logic.commands;
+
+import static hallpointer.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static hallpointer.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static hallpointer.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static hallpointer.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hallpointer.address.model.Model;
+import hallpointer.address.model.ModelManager;
+import hallpointer.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ListCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
