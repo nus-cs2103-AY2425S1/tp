45b03a3e0d9e3b8dfb290b2d4ab6453f883c40c9
Michael Yang <yql.michael@gmail.com>
2024-09-22 02:14:35 +0800

    #3 fix checkstyle

--- src/main/java/seedu/address/ui/DetailView.java
+++ src/main/java/seedu/address/ui/DetailView.java
@@ -6,6 +6,9 @@ import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 
+/**
+ * Detail view of a Person.
+ */
 public class DetailView extends UiPart<Region> {
 
     private static final String FXML = "PersonDetailView.fxml";
@@ -14,6 +17,11 @@ public class DetailView extends UiPart<Region> {
 
     private final Person person;
 
+    /**
+     * Initializes a new instance of a DetailView for a person.
+     *
+     * @param p The Person to display.
+     */
     public DetailView(Person p) {
         super(FXML);
         this.person = p;
