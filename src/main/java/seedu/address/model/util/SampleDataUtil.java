44d3a9643e9f43bedf8c645c5da124fe9e3fc3d9
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-09-28 16:07:48 +0800

    Style fixing

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -25,7 +25,7 @@ public class SampleDataUtil {
      * Returns an array of sample persons.
      */
     public static Person[] getSamplePersons() {
-        return new Person[]{
+        return new Person[] {
                 new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                         new Address("Blk 30 Geylang Street 29, #06-40"), EMPTY_REMARK,
                         getTagSet("friends")),
