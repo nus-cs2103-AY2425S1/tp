b3c27a0a06118531083c95f555ea518e52ff216d
Feng1231 <chenfeng021231@gmail.com>
2024-09-30 10:32:28 +0800

    Add Subject class
    Person class requires a Subject attribute to be added to it.
    Subject needs to be a defined set of values.
    
    Lets:
    - Add a Subject class to represent the Subject attribute
    - Add a Subjects enum to define the set of values for Subject
    - Add a Subject test class to test the Subject class

---
+++ src/test/java/seedu/address/model/person/SubjectTest.java
@@ -0,0 +1,58 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SubjectTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Subject(null));
+    }
+
+    @Test
+    public void constructor_invalidSubject_throwsIllegalArgumentException() {
+        String invalidSubject = "";
+        assertThrows(IllegalArgumentException.class, () -> new Subject(invalidSubject));
+    }
+
+    @Test
+    public void isValidSubject() {
+        // null subject
+        assertThrows(NullPointerException.class, () -> Subject.isValidSubject(null));
+
+        // invalid subjects
+        assertFalse(Subject.isValidSubject("")); // empty string
+        assertFalse(Subject.isValidSubject(" ")); // spaces only
+        assertFalse(Subject.isValidSubject("invalid")); // invalid subject
+
+        // valid subjects
+        assertTrue(Subject.isValidSubject("economics"));
+        assertTrue(Subject.isValidSubject("Biology"));
+        assertTrue(Subject.isValidSubject("eNglish"));
+        assertTrue(Subject.isValidSubject("math"));
+    }
+
+    @Test
+    public void equals() {
+        Subject subject = new Subject("economics");
+
+        // same values -> returns true
+        assertTrue(subject.equals(new Subject("economics")));
+
+        // same object -> returns true
+        assertTrue(subject.equals(subject));
+
+        // null -> returns false
+        assertFalse(subject.equals(null));
+
+        // different types -> returns false
+        assertFalse(subject.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(subject.equals(new Subject("biology")));
+    }
+}
