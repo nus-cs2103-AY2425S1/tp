ff605ebfde01eb62f3cddbfbf3148e513d0ca5b5
Feng1231 <chenfeng021231@gmail.com>
2024-09-30 13:35:10 +0800

    Import assertThrows from own package instead of junit

--- src/test/java/seedu/address/model/person/ScheduleTest.java
+++ src/test/java/seedu/address/model/person/ScheduleTest.java
@@ -3,8 +3,8 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
