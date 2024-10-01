359d213ed1992ec842675a621cd32c7f6e8b1895
weibinlim <limweibin0816@gmail.com>
2024-10-01 17:42:07 +0800

    refactor: Add JavaDocs for Guest and Rsvp classes

--- src/main/java/seedu/address/model/person/Guest.java
+++ src/main/java/seedu/address/model/person/Guest.java
@@ -5,38 +5,69 @@ import seedu.address.model.tag.Tag;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * Represents a Guest in the address book.
+ * A Guest is a person with an RSVP status indicating their attendance.
+ */
 public class Guest extends Person {
 
     private final Rsvp rsvp;
 
     /**
+     * Creates a Guest with the specified details.
      * Every field must be present and not null.
      *
-     * @param name
-     * @param phone
-     * @param email
-     * @param address
-     * @param tags
+     * @param name The name of the guest.
+     * @param phone The phone number of the guest.
+     * @param email The email address of the guest.
+     * @param address The address of the guest.
+     * @param tags The tags associated with the guest.
+     * @param rsvp The RSVP status of the guest.
      */
     public Guest(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Rsvp rsvp) {
         super(name, phone, email, address, tags);
         this.rsvp = rsvp;
     }
 
+    /**
+     * Returns true if the guest is coming, i.e., if their RSVP status is "ACCEPTED".
+     *
+     * @return true if the guest's RSVP status is "ACCEPTED"; false otherwise.
+     */
     public boolean isComing() {
         return rsvp.toString().equalsIgnoreCase("ACCEPTED");
     }
 
+    /**
+     * Returns the RSVP status of the guest.
+     *
+     * @return The RSVP status of the guest.
+     */
     public Rsvp getRsvp() {
         return this.rsvp;
     }
 
+    /**
+     * Checks if this guest is the same person as another person.
+     * Two guests are considered the same if they have the same attributes
+     * and the other person is also a Guest.
+     *
+     * @param otherPerson The other person to compare against.
+     * @return true if the guests are the same; false otherwise.
+     */
     @Override
     public boolean isSamePerson(Person otherPerson) {
         return super.isSamePerson(otherPerson)
                 && (otherPerson instanceof Guest);
     }
 
+    /**
+     * Checks if this guest is equal to another object.
+     *
+     * @param other The object to compare with.
+     * @return true if the other object is an instance of Guest and has
+     *         the same attributes; false otherwise.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -53,12 +84,23 @@ public class Guest extends Person {
                 getRsvp().equals(otherGuest.getRsvp());
     }
 
+    /**
+     * Returns the hash code of this guest.
+     *
+     * @return The hash code of the guest based on its attributes.
+     */
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
         return Objects.hash(getName(), getPhone(), getEmail(), getAddress(), getTags(), getRsvp());
     }
 
+    /**
+     * Returns a string representation of the guest, including their details
+     * and RSVP status.
+     *
+     * @return A string representation of the guest.
+     */
     @Override
     public String toString() {
         return super.toStringBuilder()
--- src/main/java/seedu/address/model/person/Rsvp.java
+++ src/main/java/seedu/address/model/person/Rsvp.java
@@ -3,12 +3,20 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+/**
+ * Represents an RSVP status for a person.
+ * The RSVP status can be one of the following: "PENDING", "ACCEPTED", "DECLINED".
+ * This class enforces the following constraints:
+ * RSVP Status should be either "PENDING", "ACCEPTED", or "DECLINED".</li>
+ * The first character of the RSVP status must not be a whitespace.</li>
+ */
 public class Rsvp {
 
     public static final String MESSAGE_CONSTRAINTS =
             "RSVP Status should be 'ACCEPTED', 'DECLINED'";
     private static final String[] RSVP_STATUS = {"PENDING", "ACCEPTED", "DECLINED"};
-    /*
+
+    /**
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
@@ -16,12 +24,25 @@ public class Rsvp {
 
     private String rsvp;
 
+    /**
+     * Constructs an {@code Rsvp} object with the specified RSVP status.
+     *
+     * @param rsvp A valid RSVP status.
+     * @throws NullPointerException if the given rsvp is null.
+     * @throws IllegalArgumentException if the given rsvp is invalid.
+     */
     public Rsvp(String rsvp) {
         requireNonNull(rsvp);
         checkArgument(isValidRsvp(rsvp), MESSAGE_CONSTRAINTS);
         this.rsvp = rsvp;
     }
 
+    /**
+     * Returns true if a given string is a valid RSVP status.
+     *
+     * @param str The string to be validated.
+     * @return true if the string is a valid RSVP status; false otherwise.
+     */
     public static boolean isValidRsvp(String str) {
 
         if (!str.matches(VALIDATION_REGEX)) {
@@ -37,11 +58,22 @@ public class Rsvp {
         return false;
     }
 
+    /**
+     * Returns the string representation of the RSVP status.
+     *
+     * @return The RSVP status as a string.
+     */
     @Override
     public String toString() {
         return rsvp;
     }
 
+    /**
+     * Returns true if this Rsvp is equal to another object.
+     *
+     * @param other The object to compare with.
+     * @return true if the other object is an instance of Rsvp and has the same status; false otherwise.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -57,6 +89,11 @@ public class Rsvp {
         return rsvp.equals(otherName.rsvp);
     }
 
+    /**
+     * Returns the hash code of this Rsvp.
+     *
+     * @return The hash code of the RSVP status.
+     */
     @Override
     public int hashCode() {
         return rsvp.hashCode();
