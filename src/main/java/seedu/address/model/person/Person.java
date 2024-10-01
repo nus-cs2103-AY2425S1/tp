cc2f0c20fb15cceb96d7582123675c7a60438dc1
Otto Tan <OttoTan>
2024-10-01 21:10:16 +0800

    Revert "Implement remark feature"
    This reverts commit 43b70cfbd82815927ef5f1efd08aeba78c701e1a.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -24,19 +24,17 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
-    private final Remark remark;
 
     /**
-     * Every field must be present and not null except for remark.
+     * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Remark remark) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
-        this.remark = remark;
     }
 
     public Name getName() {
@@ -53,10 +51,6 @@ public class Person {
 
     public Address getAddress() {
         return address;
-    }
-
-    public Remark getRemark() {
-        return remark;
     }
 
     /**
@@ -117,7 +111,6 @@ public class Person {
                 .add("email", email)
                 .add("address", address)
                 .add("tags", tags)
-                .add("remark", remark)
                 .toString();
     }
 
