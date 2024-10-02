3c919b7aeac787ca76af338729183b7eabf8b448
LI HAIXIN <lihaixin000@gmail.com>
2024-10-02 18:45:49 +0800

    Edit comment

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -14,7 +14,7 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
 /**
- * Adds a person to address book.
+ * Adds a person to the address book.
  */
 public class AddCommand extends Command {
 
