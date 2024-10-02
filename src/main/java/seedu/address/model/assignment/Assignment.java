29ec16bbab418e00e93a51696b967b4ca1135652
lm-44 <yauchengyong.literallyme@gmail.com>
2024-10-02 21:30:27 +0800

    Add Assignment Class

---
+++ src/main/java/seedu/address/model/assignment/Assignment.java
@@ -0,0 +1,96 @@
+package seedu.address.model.assignment;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.util.Objects;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Represents an Assignment in the address book.
+ * Guarantees: details are present and not null, field values are validated.
+ */
+public class Assignment {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "The maximum score should be greater than or equal to the minimum score of 0";
+
+    // Identity fields
+    private final Name name;
+
+    // Data fields
+    private final static int minScore = 0;
+    private final int maxScore;
+    private int score = 0;
+    private boolean hasSubmitted = false;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Assignment(Name name, int maxScore) {
+        requireNonNull(name);
+        this.name = name;
+        checkArgument(isValidScore(maxScore), MESSAGE_CONSTRAINTS);
+        this.maxScore = maxScore;
+    }
+
+    /**
+     * Returns true if a given maxScore is a valid score.
+     */
+    public static boolean isValidScore(int test) {
+        return test >= minScore;
+    }
+
+    /**
+     * Returns true if both assignments have the same name.
+     * This defines a weaker notion of equality between two assignments.
+     */
+    public boolean isSameAssignment(Assignment otherAssignment) {
+        if (otherAssignment == this) {
+            return true;
+        }
+
+        return otherAssignment != null
+                && otherAssignment.name.equals(name);
+    }
+
+    /**
+     * Returns true if both assignments have the same identity and data fields.
+     * This defines a stronger notion of equality between two assignments.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Assignment)) {
+            return false;
+        }
+
+        Assignment otherAssignment = (Assignment) other;
+        return name.equals(otherAssignment.name)
+                && minScore == otherAssignment.minScore
+                && maxScore == otherAssignment.maxScore
+                && score == otherAssignment.score
+                && hasSubmitted == otherAssignment.hasSubmitted;
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, minScore, maxScore);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("minScore", minScore)
+                .add("maxScore", maxScore)
+                .toString();
+    }
+
+}
