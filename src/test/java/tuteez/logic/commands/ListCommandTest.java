49d71394dbc552bdb5e53702256434e3225d1aa8
luileng <chengluileng@gmail.com>
2024-10-02 13:13:16 +0800

    Change package name

---
+++ src/test/java/tuteez/logic/commands/ListCommandTest.java
@@ -0,0 +1,39 @@
+package tuteez.logic.commands;
+
+import static tuteez.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static tuteez.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static tuteez.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static tuteez.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import tuteez.model.Model;
+import tuteez.model.ModelManager;
+import tuteez.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ListCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
