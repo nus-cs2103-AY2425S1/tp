0be8e9c39f33c1fc81de3a4e2e3d162096a57f6a
jialegitgud <jiale.official@gmail.com>
2024-10-02 22:29:38 +0800

    Update Goods Class with abstraction

--- src/main/java/seedu/address/model/goods/Goods.java
+++ src/main/java/seedu/address/model/goods/Goods.java
@@ -6,25 +6,27 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.time.LocalDateTime;
 
+import seedu.address.model.person.Name;
+
 /**
  * Represents goods.
  * Garauntees: Immutable; name is valid as declared in {@link #isValidGoodsName(String)};
- *     procurementDate is valid as declared in {@link #isValidProcurementDate(LocalDateTime)};
- *     arrivalDate is valid as declared in {@link #isValidArrivalDate(LocalDateTime)}
+ *     procurementDate is valid as declared in {@link #isValidProcurementDate(Date)};
+ *     arrivalDate is valid as declared in {@link #isValidArrivalDate(Date)}
  */
 public class Goods {
 
     public static final String MESSAGE_CONSTRAINTS = "Product quantity cannot be negative or 0!";
     public static final String VALIDATION_REGEX = "/^[\\w\\-\\s]+$/";
 
-    private final String goodsName;
+    private final GoodsName goodsName;
     private final int quantity;
-    private final int price;
+    private final double price;
     private final GoodsCategories category;
-    private final LocalDateTime procurementDate;
-    private final LocalDateTime arrivalDate;
+    private final Date procurementDate;
+    private final Date arrivalDate;
     private final boolean isDelivered;
-    private final String supplierName;
+    private final Name supplierName;
 
     /**
      * Constructs a {@Code Goods}.
@@ -38,10 +40,9 @@ public class Goods {
      * @param isDelivered Current delivery status.
      * @param supplierName A valid supplier name.
      */
-    public Goods(String goodsName, int quantity, int price, GoodsCategories category,
-            LocalDateTime procurementDate, LocalDateTime arrivalDate, boolean isDelivered, String supplierName) {
+    public Goods(GoodsName goodsName, int quantity, double price, GoodsCategories category,
+            Date procurementDate, Date arrivalDate, boolean isDelivered, Name supplierName) {
         requireAllNonNull(goodsName, quantity, category, procurementDate, arrivalDate, isDelivered, supplierName);
-        checkArgument(isValidGoodsName(goodsName));
         checkArgument(isValidProcurementDate(procurementDate));
         checkArgument(isValidArrivalDate(arrivalDate));
         this.goodsName = goodsName;
@@ -55,31 +56,24 @@ public class Goods {
     }
 
     /**
-     * Returns True if the goods name is valid.
-     */
-    public static boolean isValidGoodsName(String goodsName) {
-        return goodsName.matches(VALIDATION_REGEX);
-    }
-
-    /**
      * Returns True if the procurement date is valid.
      */
-    public static boolean isValidProcurementDate(LocalDateTime date) {
-        return date.isBefore(LocalDateTime.now());
+    public static boolean isValidProcurementDate(Date date) {
+        return date.getDateTime().isBefore(LocalDateTime.now());
     }
 
     /**
      * Returns True if the arrival date is valid.
      */
-    public static boolean isValidArrivalDate(LocalDateTime date) {
-        return date.isAfter(LocalDateTime.now());
+    public static boolean isValidArrivalDate(Date date) {
+        return date.getDateTime().isAfter(LocalDateTime.now());
     }
 
     /**
      * Returns True if the goods is from the given supplier.
      * @param supplier Name of the supplier to be checked against.
      */
-    public boolean isFromSupplier(String supplier) {
+    public boolean isFromSupplier(Name supplier) {
         requireNonNull(supplier);
         return this.supplierName == supplier;
     }
@@ -91,7 +85,7 @@ public class Goods {
     /**
      * Returns total price of the goods.
      */
-    public int getPriceTotal() {
+    public double getPriceTotal() {
         return this.quantity * this.price;
     }
 
--- src/main/java/seedu/address/model/goods/GoodsCategories.java
+++ src/main/java/seedu/address/model/goods/GoodsCategories.java
@@ -7,4 +7,7 @@ public enum GoodsCategories {
     CONSUMABLES,
     LIFESTYLE,
     SPECIALTY;
+
+    public static final String MESSAGE_UNKNOWN_CATEGORY =
+            "Category should strictly be one of [CONSUMABLES, LIFESTYLE, SPECIALTY]";
 }
