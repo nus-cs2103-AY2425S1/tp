fd48e50e3fb4278d745fcbe436d2b8c795c860e9
brendanng7 <brendanngjj@gmail.com>
2024-10-03 15:25:40 +0800

    Update SampleDataUtil.java

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -8,12 +8,7 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.contactdate.ContactDate;
 import seedu.address.model.contactdate.ContactDateList;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Nric;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -24,22 +19,28 @@ public class SampleDataUtil {
         return new Person[] {
             new Person(new Nric("S7083906G"), new Name("Alex Yeoh"), new Phone("87438807"),
                     new Email("alexyeoh@example.com"), new Address("Blk 30 Geylang Street 29, #06-40"),
-                    getTagSet("friends"), new ContactDateList(new ContactDate("2024-09-28"))),
+                    getTagSet("friends"), new ContactDateList(new ContactDate("2024-09-26")),
+                    new CallFrequency("5")),
             new Person(new Nric("S0964515B"), new Name("Bernice Yu"), new Phone("99272758"),
                     new Email("berniceyu@example.com"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    getTagSet("colleagues", "friends"), new ContactDateList(new ContactDate("2024-09-28"))),
+                    getTagSet("colleagues", "friends"), new ContactDateList(new ContactDate("2024-09-28")),
+                    new CallFrequency("6")),
             new Person(new Nric("S7419603I"), new Name("Charlotte Oliveiro"), new Phone("93210283"),
                     new Email("charlotte@example.com"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    getTagSet("neighbours"), new ContactDateList(new ContactDate("2024-09-28"))),
+                    getTagSet("neighbours"), new ContactDateList(new ContactDate("2024-09-27")),
+                    new CallFrequency("7")),
             new Person(new Nric("S9496815A"), new Name("David Li"), new Phone("91031282"),
                     new Email("lidavid@example.com"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("family"), new ContactDateList(new ContactDate("2024-09-28"))),
+                    getTagSet("family"), new ContactDateList(new ContactDate("2024-09-27")),
+                    new CallFrequency("6")),
             new Person(new Nric("S8388963B"), new Name("Irfan Ibrahim"), new Phone("92492021"),
                     new Email("irfan@example.com"), new Address("Blk 47 Tampines Street 20, #17-35"),
-                    getTagSet("classmates"), new ContactDateList(new ContactDate("2024-09-28"))),
+                    getTagSet("classmates"), new ContactDateList(new ContactDate("2024-09-28")),
+                    new CallFrequency("5")),
             new Person(new Nric("S7495552E"), new Name("Roy Balakrishnan"), new Phone("92624417"),
                     new Email("royb@example.com"), new Address("Blk 45 Aljunied Street 85, #11-31"),
-                    getTagSet("colleagues"), new ContactDateList(new ContactDate("2024-09-28")))
+                    getTagSet("colleagues"), new ContactDateList(new ContactDate("2024-09-26")),
+                    new CallFrequency("3"))
         };
     }
 
