7cafa54c29903ac0555643b6ea14fd1137419813
hovanleong <hovanleong1@gmail.com>
2024-10-04 00:08:52 +0800

    Implement AddGuestCommandIntegrationTest.java

---
+++ src/test/java/seedu/address/logic/commands/AddVendorCommandIntegrationTest.java
@@ -0,0 +1,48 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalVendors.getTypicalAddressBookWithVendors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Vendor;
+import seedu.address.testutil.VendorBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code AddVendorCommand}.
+ */
+public class AddGuestCommandIntegrationTest {
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBookWithVendors(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newGuest_success() {
+        Vendor validVendor = new VendorBuilder().build();
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.addPerson(validVendor);
+
+        assertCommandSuccess(new AddVendorCommand(validVendor), model,
+                String.format(AddVendorCommand.MESSAGE_SUCCESS, Messages.format(validVendor)),
+                expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateGuest_throwsCommandException() {
+        Vendor vendorInList = (Vendor) model.getAddressBook().getPersonList().get(0);
+        assertCommandFailure(new AddVendorCommand(vendorInList), model,
+                AddVendorCommand.MESSAGE_DUPLICATE_VENDOR);
+    }
+
+}
