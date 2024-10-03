42dc3ddff8fd3dc099a951da0a2fdc4a0684a5c9
hovanleong <hovanleong1@gmail.com>
2024-10-04 01:49:13 +0800

    Edit all usage of ALICE to AMY in VendorTest

--- src/test/java/seedu/address/testutil/TypicalVendors.java
+++ src/test/java/seedu/address/testutil/TypicalVendors.java
@@ -9,13 +9,12 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_RSVP_ACCEPTED;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_RSVP_PENDING;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COMPANY_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_COMPANY_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
 import seedu.address.model.AddressBook;
-import seedu.address.model.person.Guest;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Vendor;
 
@@ -28,97 +27,90 @@ import java.util.List;
  */
 public class TypicalVendors {
 
-    public static final Vendor ALICE = new VendorBuilder().withName("Alice Pauline")
-            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("94351253")
-            .withTags("friends").withCompany("hello").build();
-    public static final Vendor BOB = new VendorBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
-            .withCompany(VALID_COMPANY_BOB).build();
-
-    public static final Guest AVA = new GuestBuilder().withName("Ava Johnson")
+    public static final Vendor AVA = new VendorBuilder().withName("Ava Johnson")
             .withAddress("123, Jurong West Ave 6, #08-111")
             .withEmail("ava.johnson@example.com")
             .withPhone("94351253")
-            .withRsvp("ACCEPTED")
+            .withCompany("companyName")
             .withTags("friends").build();
-    public static final Guest BRIAN = new GuestBuilder().withName("Brian Lee")
+    public static final Vendor BRIAN = new VendorBuilder().withName("Brian Lee")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("brian.lee@example.com")
             .withPhone("98765432")
-            .withRsvp("PENDING")
+            .withCompany("companyName")
             .withTags("owesMoney", "friends").build();
-    public static final Guest CAM = new GuestBuilder().withName("Cameron White")
+    public static final Vendor CAM = new VendorBuilder().withName("Cameron White")
             .withPhone("95352563")
             .withEmail("cameron.white@example.com")
-            .withRsvp("DECLINED")
+            .withCompany("companyName")
             .withAddress("wall street").build();
-    public static final Guest DAVID = new GuestBuilder().withName("David Brown")
+    public static final Vendor DAVID = new VendorBuilder().withName("David Brown")
             .withPhone("87652533")
             .withEmail("david.brown@example.com")
             .withAddress("10th street")
-            .withRsvp("ACCEPTED")
+            .withCompany("companyName")
             .withTags("friends").build();
-    public static final Guest ELLA = new GuestBuilder().withName("Ella Thompson")
+    public static final Vendor ELLA = new VendorBuilder().withName("Ella Thompson")
             .withPhone("9482224")
             .withEmail("ella.thompson@example.com")
-            .withRsvp("PENDING")
+            .withCompany("companyName")
             .withAddress("michegan ave").build();
-    public static final Guest FAITH = new GuestBuilder().withName("Faith Garcia")
+    public static final Vendor FAITH = new VendorBuilder().withName("Faith Garcia")
             .withPhone("9482427")
             .withEmail("faith.garcia@example.com")
-            .withRsvp("DECLINED")
+            .withCompany("companyName")
             .withAddress("little tokyo").build();
-    public static final Guest GAVIN = new GuestBuilder().withName("Gavin Kim")
+    public static final Vendor GAVIN = new VendorBuilder().withName("Gavin Kim")
             .withPhone("9482442")
             .withEmail("gavin.kim@example.com")
-            .withRsvp("ACCEPTED")
+            .withCompany("companyName")
             .withAddress("4th street").build();
 
 
     // Manually added
-    public static final Guest HOON = new GuestBuilder().withName("Hoon Meier")
+    public static final Vendor HOON = new VendorBuilder().withName("Hoon Meier")
             .withPhone("8482424")
             .withEmail("stefan@example.com")
             .withAddress("little india")
-            .withRsvp("ACCEPTED").build();
-    public static final Guest IDA = new GuestBuilder().withName("Ida Mueller")
+            .withCompany("companyName").build();
+    public static final Vendor IDA = new VendorBuilder().withName("Ida Mueller")
             .withPhone("8482131")
             .withEmail("hans@example.com")
             .withAddress("chicago ave")
-            .withRsvp("PENDING").build();
+            .withCompany("companyName").build();
 
     // Manually added - Guest's details found in {@code CommandTestUtil}
-    public static final Guest AMY = new GuestBuilder().withName(VALID_NAME_AMY)
+    public static final Vendor AMY = new VendorBuilder().withName(VALID_NAME_AMY)
             .withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY)
             .withAddress(VALID_ADDRESS_AMY)
-            .withRsvp(VALID_RSVP_ACCEPTED)
+            .withCompany(VALID_COMPANY_AMY)
             .withTags(VALID_TAG_FRIEND).build();
-    public static final Guest BOB = new GuestBuilder().withName(VALID_NAME_BOB)
+
+    public static final Vendor BOB = new VendorBuilder().withName(VALID_NAME_BOB)
             .withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB)
             .withAddress(VALID_ADDRESS_BOB)
-            .withRsvp(VALID_RSVP_PENDING)
+            .withCompany(VALID_COMPANY_BOB)
             .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
-    private TypicalGuests() {} // prevents instantiation
+    private TypicalVendors() {} // prevents instantiation
 
     /**
      * Returns an {@code AddressBook} with all the typical guests.
      */
     public static AddressBook getTypicalAddressBookWithGuests() {
         AddressBook ab = new AddressBook();
-        for (Person person : getTypicalGuests()) {
+        for (Person person : getTypicalVendors()) {
             ab.addPerson(person);
         }
         return ab;
     }
 
-    public static List<Guest> getTypicalGuests() {
+    public static List<Vendor> getTypicalVendors() {
         return new ArrayList<>(Arrays.asList(AVA, BRIAN, CAM, DAVID, ELLA, GAVIN));
     }
 }
