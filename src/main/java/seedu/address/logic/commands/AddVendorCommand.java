e307ac5cad695411430a5316970f78409dac4b47
hovanleong <hovanleong1@gmail.com>
2024-10-03 21:12:46 +0800

    Edit AddressBookParser to take in AddVendor command word

--- src/main/java/seedu/address/logic/commands/AddVendorCommand.java
+++ src/main/java/seedu/address/logic/commands/AddVendorCommand.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_RSVP;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.commons.util.ToStringBuilder;
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddGuestCommand;
+import seedu.address.logic.commands.AddVendorCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
@@ -53,8 +54,11 @@ public class AddressBookParser {
 
         switch (commandWord) {
 
-        case AddGuestCommand.COMMAND_WORD:
+        case AddVendorCommand.COMMAND_WORD:
             return new AddGuestCommandParser().parse(arguments);
+
+        case AddGuestCommand.COMMAND_WORD:
+            return new AddVendorCommandParser().parse(arguments);
 
         case EditCommand.COMMAND_WORD:
             return new EditCommandParser().parse(arguments);
