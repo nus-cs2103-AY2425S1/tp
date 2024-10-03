7ed850f2c32b37ae2225fca56c41e0cd1891bed6
yong-zhuo <yap.aloysious@gmail.com>
2024-10-03 22:04:03 +0800

    Add DateUtil and DateOfBirth classes
    Currently there is no class to handle dates, hence there should
    be a utility class to validate and manage date format.
    
    A DateUtil class is added to help check if given dates follow a
    specific yyyy-MM-dd format. A DateOfBirth class is also added,
    which utilises the DateUtil class, that will serve as a parameter
    for the Person class.

--- src/main/java/seedu/address/commons/util/DateUtil.java
+++ src/main/java/seedu/address/commons/util/DateUtil.java
@@ -1,2 +1,54 @@
-package seedu.address.commons.util;public class DateUtil {
+package seedu.address.commons.util;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * A class for representing dates.
+ */
+public class DateUtil {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static final String MESSAGE_CONSTRAINTS = "Date should be in the format of " + DATE_FORMAT;
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
+
+    /**
+     * Constructs a {@code DateUtil} Object.
+     */
+    public DateUtil(String date) {
+        checkArgument(isValidDate(date), MESSAGE_CONSTRAINTS);
+    }
+
+    /**
+     * Returns if a given string is a valid date.
+     *
+     * @param date The date to be checked.
+     */
+    public static boolean isValidDate(String date) {
+        requireNonNull(date);
+        try {
+            LocalDate parsedDate = LocalDate.parse(date, DATE_FORMATTER);
+            return true;
+        } catch (DateTimeException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Returns if a given string is a date that is after today.
+     *
+     * @param date The date to be checked.
+     * @return true if the date is after today, false otherwise.
+     */
+    public static boolean isAfterToday(String date) {
+        checkArgument(isValidDate(date), MESSAGE_CONSTRAINTS);
+        LocalDate parsedDate = LocalDate.parse(date, DATE_FORMATTER);
+        return parsedDate.isAfter(LocalDate.now());
+    }
 }
--- src/main/java/seedu/address/model/person/DateOfBirth.java
+++ src/main/java/seedu/address/model/person/DateOfBirth.java
@@ -1,2 +1,31 @@
-package seedu.address.model.person;public class DateOfBirth {
+package seedu.address.model.person;
+
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import seedu.address.commons.util.DateUtil;
+
+/**
+ * Represents a patient's date of birth in MediBase3.
+ */
+public class DateOfBirth extends DateUtil {
+    public static final String MESSAGE_CONSTRAINTS = "Date of birth should not be after today's date";
+
+    /**
+     * Constructs a {@code DateOfBirth}.
+     *
+     * @param dob A valid date of birth.
+     */
+    public DateOfBirth(String dob) {
+        super(dob);
+        checkArgument(isValidDate(dob), MESSAGE_CONSTRAINTS);
+    }
+
+    /**
+     * Returns if a given string is a valid date of birth.
+     *
+     * @param dob The date of birth to be checked.
+     */
+    public static boolean isValidDateOfBirth(String dob) {
+        return isAfterToday(dob);
+    }
 }
