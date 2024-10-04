674d05bceda57e4e0e87db18a9b7618a228275f1
DESKTOP-C1DA8I2\KUSHAL DAS <e1121075@u.nus.edu>
2024-10-04 13:08:02 +0800

    Added job parameter to success message

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -55,7 +55,7 @@ public class CommandTestUtil {
     public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
 
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
-    public static final String INVALID_JOB_DESC = " " + PREFIX_JOB + "Engineer$"; // '$' not allowed in names
+    public static final String INVALID_JOB_DESC = " " + PREFIX_JOB + "Engineer$"; // '$' not allowed in jobs
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
