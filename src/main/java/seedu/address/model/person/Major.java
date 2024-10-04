392b6773412803e211bad236788e94d1535eda0f
Sankar <sankarram2002ybis@gmail.com>
2024-10-04 11:22:59 +0800

    Add Major and Role classes

---
+++ src/main/java/seedu/address/model/person/Major.java
@@ -0,0 +1,69 @@
+package seedu.address.model.person;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's major in the address book.
+ */
+public class Major {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Major is invalid. Please choose one of the following: ‘cs’, ‘bza’, ‘ceg’, ‘isys’, ‘isec’.";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[a-zA-Z]+";
+
+    public static final List<String> valid_majors = Arrays.asList("cs", "bza", "ceg", "isys", "isec");
+    public String major;
+
+    /**
+     * Constructs a {@code Major}.
+     *
+     * @param major a valid major.
+     */
+    public Major(String major) {
+        requireNonNull(major);
+        checkArgument(isValidMajor(major), MESSAGE_CONSTRAINTS);
+        this.major = major;
+    }
+
+    /**
+     * Returns if a given major string is a valid role.
+     */
+    public static boolean isValidMajor(String test) {
+        return test.matches(VALIDATION_REGEX) && valid_majors.contains(test);
+    }
+
+    @Override
+    public String toString() {
+        return this.major;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        //instanceOf handles nulls
+        if (!(other instanceof Major)) {
+            return false;
+        }
+
+        Major otherMajor = (Major) other;
+        return this.major.equals(otherMajor.major);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.major.hashCode();
+    }
+}
+
--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -20,6 +20,8 @@ public class Person {
     private final Name name;
     private final Phone phone;
     private final Email email;
+    private final Role role;
+    private final Major major;
 
     // Data fields
     private final Address address;
@@ -28,11 +30,13 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Role role, Major major, Address address, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, role, major, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
+        this.role = role;
+        this.major = major;
         this.address = address;
         this.tags.addAll(tags);
     }
@@ -52,6 +56,10 @@ public class Person {
     public Address getAddress() {
         return address;
     }
+
+    public Role getRole() {return role;}
+
+    public Major getMajor() {return major;}
 
     /**
      * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
@@ -93,6 +101,8 @@ public class Person {
         return name.equals(otherPerson.name)
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
+                && role.equals(otherPerson.role)
+                && major.equals(otherPerson.major)
                 && address.equals(otherPerson.address)
                 && tags.equals(otherPerson.tags);
     }
@@ -100,7 +110,7 @@ public class Person {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, role, major, address, tags);
     }
 
     @Override
@@ -109,6 +119,8 @@ public class Person {
                 .add("name", name)
                 .add("phone", phone)
                 .add("email", email)
+                .add("role", role)
+                .add("major", major)
                 .add("address", address)
                 .add("tags", tags)
                 .toString();
