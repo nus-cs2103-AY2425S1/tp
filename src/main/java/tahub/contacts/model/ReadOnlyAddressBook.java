326b273b4a29829e0e3717b3b11582d1831e807a
Zeepheru <changzeehang@gmail.com>
2024-10-04 11:44:00 +0800

    Rename main package from seedu.address to 	ahub.contacts

---
+++ src/main/java/tahub/contacts/model/ReadOnlyAddressBook.java
@@ -0,0 +1,17 @@
+package tahub.contacts.model;
+
+import javafx.collections.ObservableList;
+import tahub.contacts.model.person.Person;
+
+/**
+ * Unmodifiable view of an address book
+ */
+public interface ReadOnlyAddressBook {
+
+    /**
+     * Returns an unmodifiable view of the persons list.
+     * This list will not contain any duplicate persons.
+     */
+    ObservableList<Person> getPersonList();
+
+}
---
+++ src/main/java/tahub/contacts/storage/UserPrefsStorage.java
@@ -0,0 +1,36 @@
+package tahub.contacts.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import tahub.contacts.commons.exceptions.DataLoadingException;
+import tahub.contacts.model.ReadOnlyUserPrefs;
+import tahub.contacts.model.UserPrefs;
+
+/**
+ * Represents a storage for {@link tahub.contacts.model.UserPrefs}.
+ */
+public interface UserPrefsStorage {
+
+    /**
+     * Returns the file path of the UserPrefs data file.
+     */
+    Path getUserPrefsFilePath();
+
+    /**
+     * Returns UserPrefs data from storage.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @throws DataLoadingException if the loading of data from preference file failed.
+     */
+    Optional<UserPrefs> readUserPrefs() throws DataLoadingException;
+
+    /**
+     * Saves the given {@link tahub.contacts.model.ReadOnlyUserPrefs} to the storage.
+     * @param userPrefs cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException;
+
+}
