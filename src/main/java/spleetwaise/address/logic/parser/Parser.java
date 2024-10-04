5f15ce0dfc1b98efa838d65ee6e7f9eadf2deca9
Sean Lim <seanlimdev856@gmail.com>
2024-10-04 17:28:23 +0800

    Rename `spleetwaise.core` to `spleetwaise.address`

---
+++ src/main/java/spleetwaise/address/logic/parser/Parser.java
@@ -0,0 +1,16 @@
+package spleetwaise.address.logic.parser;
+
+import spleetwaise.address.logic.commands.Command;
+import spleetwaise.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Represents a Parser that is able to parse user input into a {@code Command} of type {@code T}.
+ */
+public interface Parser<T extends Command> {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    T parse(String userInput) throws ParseException;
+}
---
+++ src/main/java/spleetwaise/address/logic/parser/exceptions/ParseException.java
@@ -0,0 +1,17 @@
+package spleetwaise.address.logic.parser.exceptions;
+
+import spleetwaise.address.commons.exceptions.IllegalValueException;
+
+/**
+ * Represents a parse error encountered by a parser.
+ */
+public class ParseException extends IllegalValueException {
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+    public ParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
