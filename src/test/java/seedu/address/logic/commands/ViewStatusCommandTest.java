ee366d7f66d158e8788fca102b2222216b558187
bigismols <bigismols239@gmail.com>
2024-10-05 16:32:33 +0800

    Add test for View when succesful

--- src/test/java/seedu/address/logic/commands/ViewStatusCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewStatusCommandTest.java
@@ -9,48 +9,55 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_JOB_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.ViewStatusCommand.MESSAGE_ARGUMENTS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Job;
 import seedu.address.model.person.Name;
-
+import seedu.address.model.person.Person;
 
 
 public class ViewStatusCommandTest {
-    public final Name amy = new Name(VALID_NAME_AMY);
-    public final Name bob = new Name(VALID_NAME_BOB);
+    public final Name amyName = new Name(VALID_NAME_AMY);
+    public final Name bobName = new Name(VALID_NAME_BOB);
     public final Job amyJob = new Job(VALID_JOB_AMY);
     public final Job bobJob = new Job(VALID_JOB_BOB);
-    public final Model model = new ModelManager();
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void constructor_validInput_success() {
-        ViewStatusCommand command = new ViewStatusCommand(amy, amyJob);
+        ViewStatusCommand command = new ViewStatusCommand(amyName, amyJob);
 
         // Verify that the name and job are correctly initialized
-        assertEquals(amy, command.name);
+        assertEquals(amyName, command.name);
         assertEquals(amyJob, command.job);
     }
 
     @Test
-    public void execute_commandNotImplementedYet_throwsCommandException() {
-        ViewStatusCommand command = new ViewStatusCommand(amy, amyJob);
+    public void execute_viewSuccess() {
+        Person person = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        ViewStatusCommand viewStatusCommand = new ViewStatusCommand(person.getName(), person.getJob());
+        String expectedMessage = String.format(ViewStatusCommand.MESSAGE_VIEW_SUCCESS, Messages.formatStatus(person));
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
 
-        assertCommandFailure(command, model,
-                String.format(MESSAGE_ARGUMENTS, amy, amyJob));
+        assertCommandSuccess(viewStatusCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void equals() {
-        final ViewStatusCommand standardCommand = new ViewStatusCommand(amy, amyJob);
+        final ViewStatusCommand standardCommand = new ViewStatusCommand(amyName, amyJob);
 
         // same values -> returns true
-        ViewStatusCommand commandWithSameValues = new ViewStatusCommand(amy, amyJob);
+        ViewStatusCommand commandWithSameValues = new ViewStatusCommand(amyName, amyJob);
         assertTrue(standardCommand.equals(commandWithSameValues));
 
         // same object -> returns true
@@ -63,9 +70,9 @@ public class ViewStatusCommandTest {
         assertFalse(standardCommand.equals(new ClearCommand()));
 
         // different index -> returns false
-        assertFalse(standardCommand.equals(new ViewStatusCommand(bob, amyJob)));
+        assertFalse(standardCommand.equals(new ViewStatusCommand(bobName, amyJob)));
 
         // different remark -> returns false
-        assertFalse(standardCommand.equals(new ViewStatusCommand(amy, bobJob)));
+        assertFalse(standardCommand.equals(new ViewStatusCommand(amyName, bobJob)));
     }
 }
