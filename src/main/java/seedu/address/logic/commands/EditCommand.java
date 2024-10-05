bc18edc848bfa7513332c1bc3bf8a654e17c945e
valhrd <valentinhn872@gmail.com>
2024-10-06 01:07:57 +0800

    Update to fit checkstyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -8,7 +8,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.CollectionUtil;
@@ -16,7 +21,11 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -102,7 +111,7 @@ public class EditCommand extends Command {
             Phone phone = personToEdit.getPhone();
             boolean isPhoneDuplicate = phoneCount.get(phone) > 1;
 
-            EditPersonDescriptor newDescriptor= indicateDuplicatePhone(personToEdit, isPhoneDuplicate);
+            EditPersonDescriptor newDescriptor = indicateDuplicatePhone(personToEdit, isPhoneDuplicate);
             Person taggedPerson = createEditedPerson(personToEdit, newDescriptor);
             model.setPerson(personToEdit, taggedPerson);
             model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
