bd27823202014e37323e9bbf5a6c500b594ddef4
bmanara <bmanaranus@gmail.com>
2024-10-06 18:53:25 +0800

    Change code to follow style guide

--- src/main/java/seedu/address/logic/commands/FindPatientCommand.java
+++ src/main/java/seedu/address/logic/commands/FindPatientCommand.java
@@ -3,11 +3,15 @@ package seedu.address.logic.commands;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-public class FindPatientCommand extends Command{
+/**
+ * Finds and lists all patients in address book whose name contains any of the argument keywords.
+ * Keyword matching is case-insensitive.
+ */
+public class FindPatientCommand extends Command {
     public static final String COMMAND_WORD = "find-patient";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException("Command not implemented yet");
+        throw new CommandException("find-patient command not implemented yet");
     }
 }
