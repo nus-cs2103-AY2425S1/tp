2586d4d5a5dd39c60ad7cce7cce709f169f83681
JellyPenguinnn <>
2024-10-06 16:56:24 +0800

    Add BackupCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/BackupCommandParserTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Contains unit tests for {@code BackupCommandParser}.
+ */
+public class BackupCommandParserTest {
+    private final BackupCommandParser parser = new BackupCommandParser();
+
+    @Test
+    public void parse_emptyArgs_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse(""));
+    }
+
+    @Test
+    public void parse_whitespaceArgs_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse("   "));
+    }
+
+    @Test
+    public void parse_nullArgs_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> parser.parse(null));
+    }
+}
