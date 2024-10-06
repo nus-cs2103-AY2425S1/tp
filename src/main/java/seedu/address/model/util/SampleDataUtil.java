aa7c3c9bc2ff0354c7610354cb6bcbb2ff475224
KengHian <kenghiantoh@gmail.com>
2024-10-07 01:54:56 +0800

    chore(job): Add sample job data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.job.Job;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -18,32 +19,64 @@ import seedu.address.model.tag.Tag;
  */
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
-        return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+        return new Person[]{
+                new Person(new Name("Alex Yeoh"),
+                           new Phone("87438807"),
+                           new Email("alexyeoh@example.com"),
+                           new Address("Blk 30 Geylang Street 29, #06-40"),
+                           getTagSet("friends")), new Person(new Name("Bernice Yu"),
+                                                             new Phone("99272758"),
+                                                             new Email("berniceyu@example.com"),
+                                                             new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                                                             getTagSet("colleagues", "friends")),
+                new Person(new Name("Charlotte Oliveiro"),
+                           new Phone("93210283"),
+                           new Email("charlotte@example.com"),
+                           new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                           getTagSet("neighbours")), new Person(new Name("David Li"),
+                                                                new Phone("91031282"),
+                                                                new Email("lidavid@example.com"),
+                                                                new Address(
+                                                                        "Blk 436 Serangoon Gardens Street 26, #16-43"),
+                                                                getTagSet("family")),
+                new Person(new Name("Irfan Ibrahim"),
+                           new Phone("92492021"),
+                           new Email("irfan@example.com"),
+                           new Address("Blk 47 Tampines Street 20, #17-35"),
+                           getTagSet("classmates")), new Person(new Name("Roy Balakrishnan"),
+                                                                new Phone("92624417"),
+                                                                new Email("royb@example.com"),
+                                                                new Address("Blk 45 Aljunied Street 85, #11-31"),
+                                                                getTagSet("colleagues"))
         };
     }
+
+    public static Job[] getSampleJobs() {
+        return new Job[]{
+                new Job("Software Engineer, Google Pay, Core",
+                        "Google",
+                        "$100/m",
+                        "Experience with mobile development, 2 years of experience with software development",
+                        "As a software engineer, you will work on a specific project critical to Google’s needs with "
+                        + "opportunities to switch teams and projects as you and our fast-paced business grow and " + "evolve. "),
+                new Job("Software Engineering Manager II, YouTube",
+                        "YouTube",
+                        "$200/m",
+                        "8 years of experience with software development in one or more programming languages (e.g., "
+                        + "Python, C, C++, Java, JavaScript)",
+                        "As a Software Engineering Manager you manage your project goals, contribute to product " + 
+                        "strategy and help develop your team. ")
+        };
+    }
+
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
         AddressBook sampleAb = new AddressBook();
         for (Person samplePerson : getSamplePersons()) {
             sampleAb.addPerson(samplePerson);
+        }
+        for (Job sampleJob : getSampleJobs()) {
+            sampleAb.addJob(sampleJob);
         }
         return sampleAb;
     }
@@ -53,8 +86,8 @@ public class SampleDataUtil {
      */
     public static Set<Tag> getTagSet(String... strings) {
         return Arrays.stream(strings)
-                .map(Tag::new)
-                .collect(Collectors.toSet());
+                     .map(Tag::new)
+                     .collect(Collectors.toSet());
     }
 
 }
