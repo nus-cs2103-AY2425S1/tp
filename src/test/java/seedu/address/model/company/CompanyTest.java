afd5acd3a86db3241623dd5edfe83b52a36bb2b0
Green-Tea-123 <junboonsoo@gmail.com>
2024-10-06 17:10:07 +0800

    Fix remaining check-style error in CompanyTest class

--- src/test/java/seedu/address/model/company/CompanyTest.java
+++ src/test/java/seedu/address/model/company/CompanyTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalCompanies.NUS;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.testutil.CompanyBuilder;
 
 public class CompanyTest {
