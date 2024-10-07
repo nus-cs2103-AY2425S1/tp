7a053bfc40179ad7f0f76903bb615904888727ce
yanqiyqh <yanqi.yqh@gmail.com>
2024-10-07 16:19:50 +0800

    Add javadoc comments

--- src/main/java/seedu/address/model/person/InsurancePlan.java
+++ src/main/java/seedu/address/model/person/InsurancePlan.java
@@ -1,14 +1,42 @@
 package seedu.address.model.person;
 
+/**
+ * The {@code InsurancePlan} abstract class represents a general blueprint for an insurance plan.
+ * It defines common behaviors that can be shared across all types of insurance plans.
+ * This class is meant to be extended by specific insurance plan implementations.
+ */
 public abstract class InsurancePlan {
+
+    /** Unique identifier for the insurance plan. */
     private int insurancePlanId;
 
-    public int getInsurancePlanId() { return insurancePlanId; }
+    /**
+     * Retrieves the insurance plan's unique identifier.
+     *
+     * @return the insurance plan's ID
+     */
+    public int getInsurancePlanId() {
+        return insurancePlanId;
+    }
 
+    /**
+     * Returns a string representation of the insurance plan.
+     *
+     * @return a string that represents the insurance plan
+     */
+    @Override
     public String toString() {
         return "Insurance Plan";
     }
 
+    /**
+     * Compares this insurance plan to another object for equality.
+     * Two insurance plans are considered equal if they are the same object
+     * or if they are both instances of {@code InsurancePlan} and share the same insurance plan ID.
+     *
+     * @param other the object to be compared for equality
+     * @return {@code true} if the specified object is equal to this insurance plan, {@code false} otherwise
+     */
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
@@ -16,3 +44,4 @@ public abstract class InsurancePlan {
                 && insurancePlanId == ((InsurancePlan) other).getInsurancePlanId(); // check plan id
     }
 }
+
--- src/main/java/seedu/address/model/person/TravelPlan.java
+++ src/main/java/seedu/address/model/person/TravelPlan.java
@@ -1,13 +1,29 @@
 package seedu.address.model.person;
 
-public class TravelPlan extends InsurancePlan{
+/**
+ * The {@code TravelPlan} class represents a specific type of insurance plan tailored for travel purposes.
+ * It extends the {@code InsurancePlan} abstract class and provides custom behavior for travel-related insurance.
+ */
+public class TravelPlan extends InsurancePlan {
+
+    /** Unique identifier for the travel insurance plan. Initialized to 1 by default. */
     private int insurancePlanId = 1;
 
+    /**
+     * Default constructor for the {@code TravelPlan} class.
+     * Initializes a new instance of the travel insurance plan with default values.
+     */
     public TravelPlan() {
     }
 
+    /**
+     * Returns a string representation of the travel insurance plan.
+     *
+     * @return a string that represents the travel insurance plan
+     */
     @Override
     public String toString() {
         return "Travel Insurance Plan";
     }
 }
+
