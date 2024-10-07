c3f503b292d72221639282f3642b028163ccb2bc
weiseng18 <tws.hci@gmail.com>
2024-10-07 14:48:16 +0800

    Add test for ModuleCode

---
+++ src/test/java/seedu/address/model/person/ModuleCodeTest.java
@@ -0,0 +1,35 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class ModuleCodeTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new ModuleCode(null));
+    }
+
+    @Test
+    public void equals() {
+        ModuleCode name = new ModuleCode("CS1101S");
+
+        // same values -> returns true
+        assertTrue(name.equals(new ModuleCode("CS1101S")));
+
+        // same object -> returns true
+        assertTrue(name.equals(name));
+
+        // null -> returns false
+        assertFalse(name.equals(null));
+
+        // different types -> returns false
+        assertFalse(name.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(name.equals(new ModuleCode("MA1521")));
+    }
+}
