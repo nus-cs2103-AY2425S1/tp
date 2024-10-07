c3ed9eeb8d6c8deef25b96f94eb4cdc9dcb6f796
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-07 15:17:01 +0800

    Add relevant files for TagCommand

---
+++ src/main/java/seedu/address/logic/commands/TagCommand.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class TagCommand extends Command {
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException("Tag Command not implemented yet");
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.TagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class TagCommandParser implements Parser<TagCommand> {
+    @Override
+    public TagCommand parse(String args) throws ParseException {
+        throw new ParseException("Tag Command Parser not implemented yet.");
+    }
+}
