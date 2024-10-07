fa1afc18e7f76674048621f28039f85f148a0383
obrona <theodorelim2002@gmail.com>
2024-10-07 18:14:00 +0800

    Add Calendar.java and Lesson.java

---
+++ src/main/java/seedu/address/model/person/Calendar.java
@@ -0,0 +1,25 @@
+package seedu.address.model.person;
+
+import java.util.ArrayList;
+
+public class Calendar {
+    private ArrayList<Lesson> lessons;
+
+    public Calendar() {
+        lessons = new ArrayList<>();
+    }
+
+    /**
+     * Add a new lesson to Calendar
+     */
+    public void addLesson(Lesson lesson) {
+        lessons.add(lesson);
+    }
+
+    /**
+     * Remove the 1st index in lessons that returns true on Object.equals(lesson, lessons.get(i))
+     */
+    public void removeLesson(Lesson lesson) {
+        lessons.remove(lesson);
+    }
+}
