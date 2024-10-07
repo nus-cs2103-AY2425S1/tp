8d7c74640970886141f851c1cf3edaae078aeb40
bmanara <bmanaranus@gmail.com>
2024-10-07 14:39:04 +0800

    Adhere to character limit

--- src/test/java/seedu/address/logic/parser/FindPatientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindPatientCommandParserTest.java
@@ -17,7 +17,8 @@ public class FindPatientCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPatientCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPatientCommand.MESSAGE_USAGE));
     }
 
     @Test
