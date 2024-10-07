416b6d174a006687bd0057484b509daa99d35c5e
tzuennn <tzuen2406@gmail.com>
2024-10-07 22:32:06 +0800

    Add volunteer class and all required fields

---
+++ src/main/java/seedu/address/model/volunteer/exceptions/VolunteerNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.address.model.volunteer.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified volunteer.
+ */
+public class VolunteerNotFoundException extends RuntimeException {}
