678897149398b11ba268b73d5eca40927cfe0f1d
weiseng18 <tws.hci@gmail.com>
2024-10-07 14:46:06 +0800

    Add ModuleCode class

---
+++ src/main/java/seedu/address/model/person/ModuleCode.java
@@ -0,0 +1,49 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a module code in the address book.
+ * Guarantees: immutable
+ */
+public class ModuleCode {
+
+    public final String moduleCode;
+
+    /**
+     * Constructs a {@code ModuleCode}.
+     *
+     * @param String A module code.
+     */
+    public ModuleCode(String moduleCode) {
+        requireNonNull(moduleCode);
+        this.moduleCode = moduleCode;
+    }
+
+    @Override
+    public String toString() {
+        return moduleCode;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ModuleCode)) {
+            return false;
+        }
+
+        ModuleCode otherModuleCode = (ModuleCode) other;
+        return moduleCode.equals(otherModuleCode.moduleCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return moduleCode.hashCode();
+    }
+
+}
+
