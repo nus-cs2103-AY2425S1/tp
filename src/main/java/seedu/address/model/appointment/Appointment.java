e000453f53ed9c17a816baba1ea29bc813794597
Yadobler <49450747+yadobler@users.noreply.github.com>
2024-10-08 12:42:33 +0000

    Add start-time comparisons and time equality (clashing)

--- src/main/java/seedu/address/model/appointment/Appointment.java
+++ src/main/java/seedu/address/model/appointment/Appointment.java
@@ -4,20 +4,126 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 import static seedu.address.commons.util.DateUtil.isValidDate;
 
-import java.util.Objects;
-
 /**
  * Represents an Appointment in the MediBase.
  * Guarantees: immutable; name is valid as declared in {@link #isValidAppointmentName(String)}; date is valid as
  * declared in {@link seedu.address.commons.util.DateUtil#isValidDate(String)}; time period is valid as declared in
  * {@link #checkIsTimePeriodValid(String)} ;
  */
-public class Appointment {
+public class Appointment implements Comparable<Appointment> {
+
+    public static final String MESSAGE_CONSTRAINTS_APPT_DATE_WRONG_FORMAT = "Appointment date should be in the format"
+                                                                            + " of yyyy-MM-dd";
+    public static final String MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_WRONG_FORMAT = "Appointment time period should be"
+                                                                                   + " in 24-Hour Format with start "
+                                                                                   + "and end times: HHMM-HHMM";
+    public static final String MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_INVALID_ORDER = "Starting time of appointment "
+                                                                                    + "time should be before ending "
+                                                                                    + "time of appointment";
+    public static final String MESSAGE_CONSTRAINTS = "Appointment names should be alphanumeric";
+    public static final String NAME_VALIDATION_REGEX = "\\p{Alnum}[\\p{Alnum} ]*";
+
+    public final String appointmentName;
+    private final String appointmentDate;
+    private final TimePeriod appointmentTimePeriod;
+
+    /**
+     * Constructs an {@code Appointment}.
+     *
+     * @param appointmentName A valid appointment name.
+     */
+    public Appointment(String appointmentName, String appointmentDate, String appointmentTimePeriod) {
+        requireNonNull(appointmentName);
+        checkArgument(isValidAppointmentName(appointmentName), MESSAGE_CONSTRAINTS);
+        this.appointmentName = appointmentName;
+
+        requireNonNull(appointmentDate);
+        checkArgument(isValidDate(appointmentDate), MESSAGE_CONSTRAINTS_APPT_DATE_WRONG_FORMAT);
+        this.appointmentDate = appointmentDate;
+
+        requireNonNull(appointmentTimePeriod);
+        checkIsTimePeriodValid(appointmentTimePeriod);
+        this.appointmentTimePeriod = new TimePeriod(appointmentTimePeriod);
+    }
+
+
+    /**
+     * Checks if the given date and time period String clashes with this appointment.
+     *
+     * @param date String of the date to check.
+     * @param timePeriod String of the time Period to check.
+     * @return true if there is a clash.
+     */
+    public boolean isClashing(String date, String timePeriod) {
+        requireNonNull(date);
+        requireNonNull(timePeriod);
+        checkIsTimePeriodValid(timePeriod);
+        return date.equals(appointmentDate) && appointmentTimePeriod.isIntersecting(new TimePeriod(timePeriod));
+    }
+
+    /**
+     * Returns true if a given string is a valid appointment name.
+     */
+    public static boolean isValidAppointmentName(String test) {
+        return test.matches(NAME_VALIDATION_REGEX);
+    }
+
+    /**
+     *  checks if a given string is a valid appointment time period.
+     */
+    public static void checkIsTimePeriodValid(String test) {
+        checkArgument(TimePeriod.isValidAppointmentTimePeriodFormat(test),
+                      MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_WRONG_FORMAT);
+        checkArgument(TimePeriod.isValidAppointmentTimePeriodOrder(test),
+                      MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_INVALID_ORDER);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Appointment otherAppointment)) {
+            return false;
+        }
+
+        return appointmentName.equals(otherAppointment.appointmentName)
+               && appointmentDate.equals(otherAppointment.appointmentDate)
+               && appointmentTimePeriod.equals(otherAppointment.appointmentTimePeriod);
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    @Override
+    public String toString() {
+        return appointmentName + '[' + appointmentDate + ' ' + appointmentTimePeriod + ']';
+    }
+
+    /**
+     * Compares the appointment's <b>starting time</b>.
+     *
+     * @param other the other appointment to be compared.
+     * @return <code>0</code> if they start at the same time, <code><0</code> if <code>other</code> starts later,
+     * and <code>>0</code> if <code>other</code> starts earlier.
+     */
+    @Override
+    public int compareTo(Appointment other) {
+        return appointmentTimePeriod.compareTo(other.appointmentTimePeriod);
+    }
 
     /**
      * Convenience class for ease of checking and comparing.
      */
-    class TimePeriod {
+    static class TimePeriod implements Comparable<TimePeriod> {
+
         static final String TIME_VALIDATION_REGEX = "^([01]?[0-9]|2[0-3])[0-5][0-9]-([01]?[0-9]|2[0-3]):[0-5][0-9]$";
 
         private String timePeriod;
@@ -39,8 +145,8 @@ public class Appointment {
          * @return true if the TimePeriods intersect, else false.
          */
         public boolean isIntersecting(TimePeriod o) {
-           return (o.startTime <= startTime) && (startTime < o.endTime) // starts during other TimePeriod; or
-                  || (o.startTime < endTime) && (endTime <= o.endTime); // ends during other TimePeriod.
+            return (o.startTime <= startTime) && (startTime < o.endTime) // starts during other TimePeriod; or
+                   || (o.startTime < endTime) && (endTime <= o.endTime); // ends during other TimePeriod.
         }
 
         @Override
@@ -85,85 +191,17 @@ public class Appointment {
         public static boolean isValidAppointmentTimePeriodOrder(String test) {
             return Integer.parseInt(test.substring(0, 4)) < Integer.parseInt(test.substring(5,9));
         }
-    }
 
-    public static final String MESSAGE_CONSTRAINTS_APPT_DATE_WRONG_FORMAT = "Appointment date should be in the format"
-                                                                            + " of yyyy-MM-dd";
-    public static final String MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_WRONG_FORMAT = "Appointment time period should be"
-                                                                                   + " in 24-Hour Format with start "
-                                                                                   + "and end times: HHMM-HHMM";
-    public static final String MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_INVALID_ORDER = "Starting time of appointment "
-                                                                                    + "time should be before ending "
-                                                                                    + "time of appointment";
-    public static final String MESSAGE_CONSTRAINTS = "Appointment names should be alphanumeric";
-    public static final String NAME_VALIDATION_REGEX = "\\p{Alnum}[\\p{Alnum} ]*";
-
-    public final String apptName;
-    private final String appointmentDate;
-    private final TimePeriod appointmentTimePeriod;
-
-    /**
-     * Constructs an {@code Appointment}.
-     *
-     * @param appointmentName A valid appointment name.
-     */
-    public Appointment(String appointmentName, String appointmentDate, String appointmentTimePeriod) {
-        requireNonNull(appointmentName);
-        checkArgument(isValidAppointmentName(appointmentName), MESSAGE_CONSTRAINTS);
-        this.apptName = appointmentName;
-
-        requireNonNull(appointmentDate);
-        checkArgument(isValidDate(appointmentDate), MESSAGE_CONSTRAINTS_APPT_DATE_WRONG_FORMAT);
-        this.appointmentDate = appointmentDate;
-
-        requireNonNull(appointmentTimePeriod);
-        checkIsTimePeriodValid(appointmentTimePeriod);
-        this.appointmentTimePeriod = new TimePeriod(appointmentTimePeriod);
-    }
-
-    /**
-     * Returns true if a given string is a valid appointment name.
-     */
-    public static boolean isValidAppointmentName(String test) {
-        return test.matches(NAME_VALIDATION_REGEX);
-    }
-
-    /**
-     *  checks if a given string is a valid appointment time period.
-     */
-    public static void checkIsTimePeriodValid(String test) {
-        checkArgument(TimePeriod.isValidAppointmentTimePeriodFormat(test),
-                      MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_WRONG_FORMAT);
-        checkArgument(TimePeriod.isValidAppointmentTimePeriodOrder(test),
-                      MESSAGE_CONSTRAINTS_APPT_TIME_PERIOD_INVALID_ORDER);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
+        /**
+         * Compares the time period's <b>starting time</b>.
+         *
+         * @param other the object to be compared.
+         * @return <code>0</code> if they start at the same time, <code><0</code> if <code>other</code> starts later,
+         * and <code>>0</code> if <code>other</code> starts earlier.
+         */
+        @Override
+        public int compareTo(TimePeriod other) {
+            return startTime - other.startTime;
         }
-
-        // instanceof handles nulls
-        if (!(other instanceof Appointment otherAppointment)) {
-            return false;
-        }
-
-        return apptName.equals(otherAppointment.apptName)
-               && appointmentDate.equals(otherAppointment.appointmentDate)
-               && appointmentTimePeriod.equals(otherAppointment.appointmentTimePeriod);
-    }
-
-    @Override
-    public int hashCode() {
-        return toString().hashCode();
-    }
-
-    /**
-     * Format state as text for viewing.
-     */
-    @Override
-    public String toString() {
-        return apptName + '[' + appointmentDate + ' ' + appointmentTimePeriod + ']';
     }
 }
