d5d50f38fecbdaacf1c672b81ad52c87d6699d50
thisisaditya17 <joshi.adi1734@gmail.com>
2024-10-09 03:02:58 +0800

    Update Person.java

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -23,17 +23,20 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Skills skills;
+
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Address address, Skills skills, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, skills, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.skills = skills;
         this.tags.addAll(tags);
     }
 
@@ -51,6 +54,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Skills getSkills() {
+        return skills;
     }
 
     /**
@@ -94,13 +101,14 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
+                && skills.equals(otherPerson.skills)
                 && tags.equals(otherPerson.tags);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, skills, tags);
     }
 
     @Override
@@ -110,6 +118,7 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
+                .add("skills", skills)
                 .add("tags", tags)
                 .toString();
     }
