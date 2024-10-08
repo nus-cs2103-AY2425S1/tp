b6022542df2b533adc58ebfcbc5d2990f12cae6c
LAPTOP-1NO5TOS6\teren <e1122500@u.nus.edu>
2024-10-08 22:24:20 +0800

    Add manual command to parser

---
+++ src/main/java/seedu/address/logic/commands/ManualCommand.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+import seedu.address.model.Model;
+
+/**
+ * Format full instructions for every command for display.
+ */
+public class ManualCommand extends Command {
+    public static final String COMMAND_WORD = "manual";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows the user manual.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String SHOWING_MANUAL_MESSAGE = "Opened user manual.";
+
+    public ManualCommand() {
+    }
+
+    public static final String MESSAGE_NOT_IMPLEMENTED = "Manual command not implemented";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(MESSAGE_NOT_IMPLEMENTED);
+    }
+}
