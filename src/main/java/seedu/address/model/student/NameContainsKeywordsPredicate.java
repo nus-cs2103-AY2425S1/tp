f2352af5f50d54c18c8683cf4c1331a9725840d1
Ansel-Ch <achong2014@gmail.com>
2024-10-08 12:25:07 +0800

    Fix import order

--- src/main/java/seedu/address/model/student/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/student/NameContainsKeywordsPredicate.java
@@ -1,7 +1,7 @@
 package seedu.address.model.student;
 
-import java.util.function.Predicate;
 import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
