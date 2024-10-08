c3c9f65f2a02932222df64900f9c39b8d27ff27c
thisisaditya17 <joshi.adi1734@gmail.com>
2024-10-09 03:04:13 +0800

    Update PersonBuilder.java

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,11 +3,7 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -20,11 +16,13 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_SKILLS = "Java, Python";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private Skills skills;
     private Set<Tag> tags;
 
     /**
@@ -35,6 +33,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        skills = new Skills(DEFAULT_SKILLS);
         tags = new HashSet<>();
     }
 
@@ -46,6 +45,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        skills = personToCopy.getSkills();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -89,8 +89,16 @@ public class PersonBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code Skills} of the {@code Person} that we are building.
+     */
+    public PersonBuilder withSkills(String skills) {
+        this.skills = new Skills(skills);
+        return this;
+    }
+
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, skills, tags);
     }
 
 }
