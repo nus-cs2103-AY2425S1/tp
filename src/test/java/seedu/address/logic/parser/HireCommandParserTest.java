843b7e727ed7a03e9bdb8e45ef2b440bbb522efb
Ewen <e1113211@u.nus.edu>
2024-10-08 14:33:35 +0800

    Add additional tests HireCommandParserTest RejectCommandParserTest

--- src/test/java/seedu/address/logic/parser/HireCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/HireCommandParserTest.java
@@ -1,16 +1,25 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.HireCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
+import seedu.address.model.person.Job;
+import seedu.address.model.person.Name;
 
 public class HireCommandParserTest {
 
     private HireCommandParser parser = new HireCommandParser();
 
+    @Test
+    public void parse_validArgs_returnsHireCommand() throws Exception {
+        HireCommand expectedCommand = new HireCommand(new Name("Amy Bee"), new Job("Software Engineer"));
+        HireCommand command = parser.parse(" n/Amy Bee j/Software Engineer");
+        assertEquals(expectedCommand, command);
+    }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
@@ -19,11 +28,21 @@ public class HireCommandParserTest {
 
     @Test
     public void parse_missingName_throwsParseException() {
-        assertThrows(ParseException.class, () -> parser.parse(" Software Engineer"));
+        assertThrows(ParseException.class, () -> parser.parse(" j/Software Engineer"));
     }
 
     @Test
     public void parse_missingJob_throwsParseException() {
-        assertThrows(ParseException.class, () -> parser.parse("Amy Bee "));
+        assertThrows(ParseException.class, () -> parser.parse(" n/Amy Bee "));
+    }
+
+    @Test
+    public void parse_invalidPreamble_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse("invalidPreamble n/Amy Bee j/Software Engineer"));
+    }
+
+    @Test
+    public void parse_missingPrefixes_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse("Amy Bee Software Engineer"));
     }
 }
--- src/test/java/seedu/address/logic/parser/RejectCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RejectCommandParserTest.java
@@ -1,16 +1,25 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.RejectCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-
+import seedu.address.model.person.Job;
+import seedu.address.model.person.Name;
 
 public class RejectCommandParserTest {
 
     private RejectCommandParser parser = new RejectCommandParser();
 
+    @Test
+    public void parse_validArgs_returnsRejectCommand() throws Exception {
+        RejectCommand expectedCommand = new RejectCommand(new Name("Amy Bee"), new Job("Software Engineer"));
+        RejectCommand command = parser.parse(" n/Amy Bee j/Software Engineer");
+        assertEquals(expectedCommand, command);
+    }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
@@ -19,11 +28,21 @@ public class RejectCommandParserTest {
 
     @Test
     public void parse_missingName_throwsParseException() {
-        assertThrows(ParseException.class, () -> parser.parse(" Software Engineer"));
+        assertThrows(ParseException.class, () -> parser.parse(" j/Software Engineer"));
     }
 
     @Test
     public void parse_missingJob_throwsParseException() {
-        assertThrows(ParseException.class, () -> parser.parse("Amy Bee "));
+        assertThrows(ParseException.class, () -> parser.parse(" n/Amy Bee "));
+    }
+
+    @Test
+    public void parse_invalidPreamble_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse("invalidPreamble n/Amy Bee j/Software Engineer"));
+    }
+
+    @Test
+    public void parse_missingPrefixes_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse("Amy Bee Software Engineer"));
     }
 }
