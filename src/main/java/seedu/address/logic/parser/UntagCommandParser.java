a3051e87303f6efca47e886ab452082cdbe93121
DanzaSeah <e0959436@u.nus.edu>
2024-10-08 21:26:07 +0800

    Modify javadocs for UntagCommandParser class

--- src/main/java/seedu/address/logic/parser/UntagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UntagCommandParser.java
@@ -14,16 +14,16 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
 
 /**
- * Parses input arguments and creates a new {@code UntagCommand} object.
+ * Parses input arguments and creates a new UntagCommand object.
  */
 public class UntagCommandParser implements Parser<UntagCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the {@code UntagCommand}
-     * and returns a {@code UntagCommand} object for execution.
+     * Parses the given String of arguments in the context of the UntagCommand
+     * and returns a UntagCommand object for execution.
      *
      * @param args the user input string containing the index and tags to be removed
-     * @return a new {@code UntagCommand} object that contains the parsed index and list of tags
+     * @return a new UntagCommand object that contains the parsed index and list of tags
      * @throws ParseException if the input does not conform to the expected format (i.e., invalid index or missing tags)
      */
     public UntagCommand parse(String args) throws ParseException {
