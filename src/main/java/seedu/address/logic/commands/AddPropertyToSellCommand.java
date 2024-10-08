5f3f8e8819145eba044c911fb3b0e4b137504713
OrangeCatLoves <e1121707@u.nus.edu>
2024-10-09 01:37:48 +0800

    Made some changes to pass checkstyle

--- src/main/java/seedu/address/logic/commands/AddPropertyToSellCommand.java
+++ src/main/java/seedu/address/logic/commands/AddPropertyToSellCommand.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_HOUSING_TYPE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POSTAL_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SELLING_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_UNIT_NUMBER;
-import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
