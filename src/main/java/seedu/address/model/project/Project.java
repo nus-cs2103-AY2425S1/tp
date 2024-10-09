01a5c3081b334a48a153ce685d68e5c1381907e8
LeeZeHao <zehaolee2004@gmail.com>
2024-10-09 23:09:50 +0800

    Create Project.java based on Person.java

---
+++ src/main/java/seedu/address/model/project/Project.java
@@ -0,0 +1,88 @@
+package seedu.address.model.project;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.project.Name;
+import seedu.address.model.project.Id;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Represents a Project in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Project {
+
+    // Identity fields
+    private final Name name;
+    private final Id id;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Project(Name name, Id id) {
+        requireAllNonNull(name, phone, email, address, tags);
+        this.name = name;
+        this.id = id;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
+    /**
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSameProject(Project otherProject) {
+        if (otherProject == this) {
+            return true;
+        }
+
+        return otherProject != null
+                && otherProject.getId().equals(getId());
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Project)) {
+            return false;
+        }
+
+        Project otherProject = (Project) other;
+        return name.equals(otherProject.name)
+                && id.equals(otherProject.id);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", name)
+                .add("id", id)
+                .toString();
+    }
+
+}
