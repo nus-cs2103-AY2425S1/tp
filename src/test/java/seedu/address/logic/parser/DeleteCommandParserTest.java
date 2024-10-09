1fd39da5c7bb6e91e1e697f70ee654158b6856f2
Vicelya Visakha <>
2024-10-09 21:35:47 +0800

    Remove extra line before api.Test import in DeleteCommandParserTest

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.DeleteCommand;
