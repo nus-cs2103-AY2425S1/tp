a4e404c6374aa69b1e8b9268b7aa407d3a537b35
trpmy <mytran971@gmail.com>
2024-10-09 16:59:52 +0800

    Update to adhere to style guide

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Company;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Company; // Add this import
 import seedu.address.model.tag.Tag;
 
 /**
@@ -20,37 +20,37 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[]{
-                new Person(new Name("Alex Yeoh"),
+            new Person(new Name("Alex Yeoh"),
                         new Company("Tech Innovations Pte Ltd"), // Add company name
                         new Phone("87438807"),
                         new Email("alexyeoh@example.com"),
                         new Address("Blk 30 Geylang Street 29, #06-40"),
                         getTagSet("friends")),
-                new Person(new Name("Bernice Yu"),
+            new Person(new Name("Bernice Yu"),
                         new Company("Global Solutions Inc."), // Add company name
                         new Phone("99272758"),
                         new Email("berniceyu@example.com"),
                         new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                         getTagSet("colleagues", "friends")),
-                new Person(new Name("Charlotte Oliveiro"),
+            new Person(new Name("Charlotte Oliveiro"),
                         new Company("Creative Designs Co."), // Add company name
                         new Phone("93210283"),
                         new Email("charlotte@example.com"),
                         new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                         getTagSet("neighbours")),
-                new Person(new Name("David Li"),
+            new Person(new Name("David Li"),
                         new Company("Finance Experts Ltd."), // Add company name
                         new Phone("91031282"),
                         new Email("lidavid@example.com"),
                         new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                         getTagSet("family")),
-                new Person(new Name("Irfan Ibrahim"),
+            new Person(new Name("Irfan Ibrahim"),
                         new Company("Educational Systems Pte Ltd"), // Add company name
                         new Phone("92492021"),
                         new Email("irfan@example.com"),
                         new Address("Blk 47 Tampines Street 20, #17-35"),
                         getTagSet("classmates")),
-                new Person(new Name("Roy Balakrishnan"),
+            new Person(new Name("Roy Balakrishnan"),
                         new Company("Innovative Solutions Group"), // Add company name
                         new Phone("92624417"),
                         new Email("royb@example.com"),
