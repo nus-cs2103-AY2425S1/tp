432a2803c31303b88cd5fa9a8ac1a9171b186fb9
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-10-09 13:15:45 +0800

    Add ClearCommandParser and update ClearCommand
    ClearCommand currently clears the entire address book without
    considering any specifications by the user.
    
    This will not support our MVP specifications for the clear command to
    take in tags or filters to clear by.
    
    Let's add a ClearCommandParser to provide future parsing support when
    we add the functionality for ClearCommand in the future.
    
    The ClearCommandParser uses similar logic to the FindCommandParser, except
    throwing an error when the arguments is empty. This is to allow the original
    command to still take place.

---
+++ src/main/java/seedu/address/logic/parser/ClearCommandParser.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new ClearCommand object
+ */
+public class ClearCommandParser implements Parser<ClearCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ClearCommand
+     * and returns a ClearCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ClearCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new ClearCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+}
