a0d9796bc9224d02309f0fe4f5a72935fc8b8993
Yim Jian Bing <yeahitsyjb@gmail.com>
2024-10-10 04:55:19 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -31,7 +31,7 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME,PREFIX_CLASS, PREFIX_PHONE, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_CLASS, PREFIX_PHONE, PREFIX_TAG);
 
         Index index;
 
