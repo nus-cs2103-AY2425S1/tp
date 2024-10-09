12bf65799d54f7064fe1c7fcd556084d8d4457a2
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-10-09 19:46:58 +0800

    Fix DeleteCommandParser

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -12,13 +12,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /**
  * Parses input arguments and creates a new DeleteContactCommand object
  */
-public class DeleteCommandParser implements Parser<DeleteContactCommand> {
+public class DeleteCommandParser implements Parser<DeleteCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteContactCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteContactCommand parse(String args) throws ParseException {
+    public DeleteCommand parse(String args) throws ParseException {
 
 
         String[] splitArgs = args.trim().split("\\s+");
