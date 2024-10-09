3d378306e2bf0de3a2c7f112aecfb9a8f737fabc
Feng Yilong <fylnushs@gmail.com>
2024-10-09 19:14:40 +0800

    Refactor usage of Person in Logic
    Add final string field for client and vendor and add
    format method for client and vendor in Messages.java
    file.

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -5,7 +5,9 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import seedu.address.logic.parser.Prefix;
+import seedu.address.model.person.Client;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Vendor;
 
 /**
  * Container for user visible messages.
@@ -15,7 +17,12 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    // TODO: determine if "contact" should also be added here
+    public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "The client index provided is invalid";
+    public static final String MESSAGE_INVALID_VENDOR_DISPLAYED_INDEX = "The vendor index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d clients listed!";
+    public static final String MESSAGE_VENDORS_LISTED_OVERVIEW = "%1$d vendors listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
 
@@ -48,4 +55,41 @@ public class Messages {
         return builder.toString();
     }
 
+    /**
+     * Formats the {@code client} for display to the user.
+     */
+    public static String format(Client client) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(client.getName())
+                .append("; Phone: ")
+                .append(client.getPhone())
+                .append("; Email: ")
+                .append(client.getEmail())
+                .append("; Address: ")
+                .append(client.getAddress())
+                .append("; Date: ")
+                .append(client.getDate())
+                .append("; Tags: ");
+        client.getTags().forEach(builder::append);
+        return builder.toString();
+    }
+
+    /**
+     * Formats the {@code vendor} for display to the user.
+     */
+    public static String format(Vendor vendor) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(vendor.getName())
+                .append("; Phone: ")
+                .append(vendor.getPhone())
+                .append("; Email: ")
+                .append(vendor.getEmail())
+                .append("; Address: ")
+                .append(vendor.getAddress())
+                .append("; Service: ")
+                .append(vendor.getService())
+                .append("; Tags: ");
+        vendor.getTags().forEach(builder::append);
+        return builder.toString();
+    }
 }
