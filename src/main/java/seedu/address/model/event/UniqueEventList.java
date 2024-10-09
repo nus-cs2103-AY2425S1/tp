2b0b7f23e60091f58a1cf321bfc03ca59f4c9875
Haris-Irfan <harisirfan@gmail.com>
2024-10-09 22:57:20 +0800

    Clean up JavaDoc and include basic functionality of add and delete command

--- src/main/java/seedu/address/model/event/UniqueEventList.java
+++ src/main/java/seedu/address/model/event/UniqueEventList.java
@@ -12,9 +12,9 @@ import javafx.collections.ObservableList;
 /**
  * A list of events that enforces uniqueness between its elements and does not allow nulls.
  * A event is considered unique by comparing using {@code Event#isSameEvent(Event)}. As such, adding and updating of
- * events uses Event#isSameEvent(Event) for equality so as to ensure that the event being added or updated is
- * unique in terms of identity in the UniqueEventList. However, the removal of a event uses Event#equals(Object) so
- * as to ensure that the event with exactly the same fields will be removed.
+ * events uses Event#isSameEvent(Event) for equality to ensure that the event being added or updated is
+ * unique in terms of identity in the UniqueEventList. However, the removal of a event uses Event#equals(Object)
+ * to ensure that the event with exactly the same fields will be removed.
  * Supports a minimal set of list operations.
  *
  */
@@ -36,7 +36,7 @@ public class UniqueEventList implements Iterable<Event> {
      * The event must not already exist in the list.
      */
     public void add(Event toAdd) {
-    // To implement
+        internalList.add(toAdd);
     }
 
     /**
@@ -64,7 +64,7 @@ public class UniqueEventList implements Iterable<Event> {
      * The event must exist in the list.
      */
     public void remove(Event toRemove) {
-        //To implement
+        internalList.add(toRemove);
     }
 
     public void setEvents(UniqueEventList replacement) {
