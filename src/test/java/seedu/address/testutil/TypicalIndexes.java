2a07c175646c17ad809ea9ed9b614dc6286a44ec
xGladiate <sharonsohxuanhui@hotmail.com>
2024-10-10 01:22:32 +0800

    Add indexes to be tested

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -2,6 +2,10 @@ package seedu.address.testutil;
 
 import seedu.address.commons.core.index.Index;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A utility class containing a list of {@code Index} objects to be used in tests.
  */
@@ -9,4 +13,8 @@ public class TypicalIndexes {
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
     public static final Index INDEX_SECOND_PERSON = Index.fromOneBased(2);
     public static final Index INDEX_THIRD_PERSON = Index.fromOneBased(3);
+    public static final List<Index> INDEX_FIRST_PERSON_LIST = new ArrayList<>(Arrays.asList(INDEX_FIRST_PERSON));
+    public static final List<Index> INDEX_SECOND_PERSON_LIST = new ArrayList<>(Arrays.asList(INDEX_SECOND_PERSON));
+    public static final List<Index> INDEX_FIRST_SECOND_PERSON_LIST = new ArrayList<>(Arrays.asList(INDEX_FIRST_PERSON, INDEX_SECOND_PERSON));
+    public static final List<Index> INDEX_SECOND_THIRD_PERSON_LIST = new ArrayList<>(Arrays.asList(INDEX_SECOND_PERSON, INDEX_THIRD_PERSON));
 }
