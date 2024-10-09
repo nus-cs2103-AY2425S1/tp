931ae6bc9fe559847e3a5299c27c61ac037e6197
dovlin <dovlincs01@gmail.com>
2024-10-09 20:20:22 +0800

    Modify clear command to show confirmation message for clear instead

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,22 +2,22 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 
 /**
- * Clears the address book.
+ * Confirms whether user wants to clear the address book.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
-
+    public static final String MESSAGE_FAILURE = "Address book has not been cleared!";
+    public static final String MESSAGE_CONFIRMATION = "Are you sure you want to clear all your contacts? This is an "
+            + "irreversible action. (Y/Yes or N/No)";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(MESSAGE_CONFIRMATION, false, false, true);
     }
 }
