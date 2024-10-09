50521676f1026bf0d29b06fcfce6f975718e7336
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-10-09 19:48:14 +0800

    refactor: list command
    also update test cases

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -85,8 +85,9 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_list() throws Exception {
-        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
-        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " "
+                + ParserUtil.PERSON_ENTITY_STRING) instanceof ListCommand);
+        // TODO: assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
     }
 
     @Test
