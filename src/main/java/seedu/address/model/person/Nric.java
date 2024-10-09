d7168039c67ce4512b88cd7eb306ab3125df812a
suhayl13 <suhayl.pervez@gmail.com>
2024-10-09 10:10:55 +0800

    Add message constraints and validation regex

--- src/main/java/seedu/address/model/person/Nric.java
+++ src/main/java/seedu/address/model/person/Nric.java
@@ -1,6 +1,27 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Nric {
+
+    public static final String MESSAGE_CONSTRAINTS = "NRIC should adhere to the following format and constraints:\n"
+            + "1. The NRIC must be 9 characters long.\n"
+            + "2. The first character must be one of the following letters: 'S', 'T', 'F', or 'G'.\n"
+            + "   - 'S' and 'T' are for Singapore Citizens and Permanent Residents.\n"
+            + "   - 'F' and 'G' are for Foreigners.\n"
+            + "3. The next 7 characters must be digits.\n"
+            + "4. The last character must be an uppercase letter, which serves as a checksum to validate the NRIC.\n"
+            + "5. The checksum is computed based on a specific algorithm using the 7 digits of the NRIC.";
+
+    public static final String VALIDATION_REGEX = "\\d{7}";
+
+    public String nric;
+
+    public Nric(String nric) {
+        requireNonNull(nric);
+        checkArgument(isValidNric(nric), MESSAGE_CONSTRAINTS);
+    }
 
     public static boolean isValidNric(String nric) {
         // Check if the input is 9 characters long
@@ -16,7 +37,7 @@ public class Nric {
 
         // Check if the next 7 characters are digits
         String digits = nric.substring(1, 8);
-        if (!digits.matches("\\d{7}")) {
+        if (!digits.matches(VALIDATION_REGEX)) {
             return false;
         }
 
