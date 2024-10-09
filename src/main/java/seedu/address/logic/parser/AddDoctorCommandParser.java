ccebcfd2acd2798857f3521491162f432818f70d
sam-theman88 <samuel.lee9525@gmail.com>
2024-10-10 02:21:12 +0800

    Add parser support for AddDoctor command

--- src/main/java/seedu/address/logic/parser/AddDoctorCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddDoctorCommandParser.java
@@ -1,2 +1,66 @@
-package seedu.address.logic.parser;public class AddDoctorCommandParser {
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SPECIALTY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddDoctorCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.doctor.Doctor;
+import seedu.address.model.doctor.Speciality;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Parses input arguments and creates a new AddDoctorCommand object
+ */
+public class AddDoctorCommandParser implements Parser<AddDoctorCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddDoctorCommand
+     * and returns an AddDoctorCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddDoctorCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                        PREFIX_SPECIALTY, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL,
+                PREFIX_SPECIALTY) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDoctorCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                PREFIX_SPECIALTY);
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+        Speciality speciality = ParserUtil.parseSpecialty(argMultimap.getValue(PREFIX_SPECIALTY).get());
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        Doctor doctor = new Doctor(name, phone, email, address, speciality, tagList);
+
+        return new AddDoctorCommand(doctor);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
 }
