3857e0df83268a63b54fc15bbb288750f501a05d
dez2003 <desiree2021325@gmail.com>
2024-10-09 16:11:26 +0800

    Modify GUI to show student number

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -13,6 +13,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
 import seedu.address.model.student.Student;
+import seedu.address.model.student.StudentNumber;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -25,6 +26,7 @@ class JsonAdaptedPerson {
     private final String name;
     private final String email;
     private final List<JsonAdaptedTag> tags = new ArrayList<>();
+    private final String studentNumber;
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given student details.
@@ -32,12 +34,14 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name,
                              @JsonProperty("email") String email,
-                             @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+                             @JsonProperty("tags") List<JsonAdaptedTag> tags,
+                             @JsonProperty("student number") String studentNumber) {
         this.name = name;
         this.email = email;
         if (tags != null) {
             this.tags.addAll(tags);
         }
+        this.studentNumber = studentNumber;
     }
 
     /**
@@ -49,6 +53,7 @@ class JsonAdaptedPerson {
         tags.addAll(source.getTags().stream()
             .map(JsonAdaptedTag::new)
             .collect(Collectors.toList()));
+        studentNumber = source.getStudentNumber().value;
     }
 
     /**
@@ -78,8 +83,16 @@ class JsonAdaptedPerson {
         }
         final Email modelEmail = new Email(email);
 
+        if (studentNumber == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, StudentNumber.class.getSimpleName()));
+        }
+        if (!StudentNumber.isValidStudentNumber(studentNumber)) {
+            throw new IllegalValueException(StudentNumber.MESSAGE_CONSTRAINTS);
+        }
+        final StudentNumber modelStudentNumber = new StudentNumber(studentNumber);
+
         final Set<Tag> modelTags = new HashSet<>(personTags);
-        return new Student(modelName, modelEmail, modelTags);
+        return new Student(modelName, modelEmail, modelTags, modelStudentNumber);
     }
 
 }
