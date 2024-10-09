f771ac6b39e38794dbc60a563f29937d8beda17d
naythee169 <jordan.mitchell.chan@gmail.com>
2024-10-09 16:40:33 +0800

    Fix minor naming error

--- src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
+++ src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
@@ -68,20 +68,20 @@ public class JsonAddressBookStorageTest {
 
         // Save in new file and read back
         jsonAddressBookStorage.saveAddressBook(original, filePath);
-        ReadOnlyAddressBook readBack = jsonAddressBookStorage.readAddressBook(filePath).get();
+        ReadOnlyAddressBook readBack = jsonAddressBookStorage.readPawPatrol(filePath).get();
         assertEquals(original, new AddressBook(readBack));
 
         // Modify data, overwrite exiting file, and read back
         original.addPerson(HOON);
         original.removePerson(ALICE);
         jsonAddressBookStorage.saveAddressBook(original, filePath);
-        readBack = jsonAddressBookStorage.readAddressBook(filePath).get();
+        readBack = jsonAddressBookStorage.readPawPatrol(filePath).get();
         assertEquals(original, new AddressBook(readBack));
 
         // Save and read without specifying file path
         original.addPerson(IDA);
         jsonAddressBookStorage.saveAddressBook(original); // file path not specified
-        readBack = jsonAddressBookStorage.readAddressBook().get(); // file path not specified
+        readBack = jsonAddressBookStorage.readPawPatrol().get(); // file path not specified
         assertEquals(original, new AddressBook(readBack));
 
     }
