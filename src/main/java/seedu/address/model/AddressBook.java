2ca99078be85d236be4e9375b17462d5baa28370
Anvita2110 <e1070535@u.nus.edu>
2024-10-09 19:00:08 +0800

    Update AddressBook file due to build fail

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -103,7 +103,7 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("persons", persons)
+                .add("suppliers", persons)
                 .toString();
     }
 
