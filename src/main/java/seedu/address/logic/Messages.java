847dc531322d68bd81779cc783b03fdb0ccd9b74
ywllowsensor <isyazani@gmail.com>
2024-10-09 23:32:15 +0800

    Add a string variable representing a message that is not yet implemented

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -18,6 +18,7 @@ public class Messages {
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Message not implemented yet!";
 
     /**
      * Returns an error message indicating the duplicate prefixes.
