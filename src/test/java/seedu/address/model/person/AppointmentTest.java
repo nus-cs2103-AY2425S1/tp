e427b94e2606e1c3b1f7453f8fe3e7a5b463b1b7
M-kew <e0958776@u.nus.edu>
2024-10-09 15:19:11 +0800

    Add test cases for Policy,Appointment,Birthday,Payment

---
+++ src/test/java/seedu/address/model/person/AppointmentTest.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class AppointmentTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Appointment(null));
+    }
+
+    @Test
+    public void constructor_invalidAppointment_throwsIllegalArgumentException() {
+        String invalidAppointment = "";
+        assertThrows(IllegalArgumentException.class, () -> new Appointment(invalidAppointment));
+    }
+
+    @Test
+    public void isValidAppointment() {
+        // null appointment
+        assertThrows(NullPointerException.class, () -> Appointment.isValidAppointment(null));
+
+        // invalid appointments
+        assertFalse(Appointment.isValidAppointment("")); // empty string
+        assertFalse(Appointment.isValidAppointment(" ")); // spaces only
+        assertFalse(Appointment.isValidAppointment("2023-12-31")); // missing time
+        assertFalse(Appointment.isValidAppointment("appointment")); // non-datetime string
+
+        // valid appointments
+        assertTrue(Appointment.isValidAppointment("2023-12-31 23:59"));
+    }
+
+    @Test
+    public void equals() {
+        Appointment appointment = new Appointment("2023-12-31 14:30");
+
+        // same values -> returns true
+        assertTrue(appointment.equals(new Appointment("2023-12-31 14:30")));
+
+        // same object -> returns true
+        assertTrue(appointment.equals(appointment));
+
+        // null -> returns false
+        assertFalse(appointment.equals(null));
+
+        // different types -> returns false
+        assertFalse(appointment.equals(5));
+
+        // different values -> returns false
+        assertFalse(appointment.equals(new Appointment("2023-12-31 15:30")));
+    }
+}
