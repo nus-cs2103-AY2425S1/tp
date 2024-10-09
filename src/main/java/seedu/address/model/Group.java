129a4febb49437427bfad06a8ae9c4980b46b21c
Xan <iamxanng@gmail.com>
2024-10-10 01:07:04 +0800

    Add Javadoc comments

--- src/main/java/seedu/address/model/Group.java
+++ src/main/java/seedu/address/model/Group.java
@@ -2,6 +2,12 @@ package seedu.address.model;
 
 import seedu.address.model.person.UniquePersonList;
 
+/**
+ * A group of persons which has an identifying name, and can be made and edited by the user.
+ * Uses the implementation of UniquePersonList
+ *
+ * @see UniquePersonList
+ */
 public class Group extends UniquePersonList {
     private String name;
 
@@ -10,6 +16,9 @@ public class Group extends UniquePersonList {
         this.name = name;
     }
 
+    /**
+     * Renames the group to the specified {@code newName}.
+     */
     public void rename(String newName) {
         name = newName;
     }
