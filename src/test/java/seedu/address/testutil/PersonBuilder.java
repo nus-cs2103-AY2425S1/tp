88c3bee037449cd59da988071e0a3edb1ec8bd23
zyng233 <ZziyingTan@gmail.com>
2024-10-09 16:39:46 +0800

    removing unused import

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,7 +3,6 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.contactdate.ContactDateList;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.CallFrequency;
 import seedu.address.model.person.Email;
