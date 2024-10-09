f12849d6fd084b4d9d8379e550e03bab7e84dc33
Trisha Patodia <trishapatodia@Trishas-MacBook-Air.local>
2024-10-09 10:45:50 +0800

    Change indentation: No longer using Continuation

--- src/main/java/seedu/address/logic/parser/FindNameCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindNameCommandParser.java
@@ -16,8 +16,7 @@ public class FindNameCommandParser implements Parser<FindNameCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FindNameCommand
      * and returns a FindNameCommand object for execution.
-     * @throws ParseException if the user input does not
-    * conform the expected format
+     * @throws ParseException if the user input does not conform the expected format
      */
     public FindNameCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
