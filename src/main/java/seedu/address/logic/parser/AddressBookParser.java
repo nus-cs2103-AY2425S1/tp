a3332ffd2043b2fcb336cb61c3f14453311db46c
ywllowsensor <isyazani@gmail.com>
2024-10-09 23:35:53 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -77,7 +77,7 @@ public class AddressBookParser {
 
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-        
+
         case TagCommand.COMMAND_WORD:
             return new TagCommand();
 
