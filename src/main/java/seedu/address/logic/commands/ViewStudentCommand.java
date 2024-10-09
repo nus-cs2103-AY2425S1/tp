8c323a72b71c79fc49cce8488cb1941cb4405616
althea <altheatan328@gmail.com>
2024-10-10 00:28:15 +0800

    Reorder imports in ViewStudentCommand.java

--- src/main/java/seedu/address/logic/commands/ViewStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewStudentCommand.java
@@ -1,14 +1,13 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+import seedu.address.logic.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 
 import java.util.List;
 
