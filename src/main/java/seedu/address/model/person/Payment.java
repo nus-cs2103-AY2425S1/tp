ee9f98a0ea8c8ea1df25f46f6d37f562cbc2167f
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-10-09 13:12:39 +0800

    Create payment class under person

---
+++ src/main/java/seedu/address/model/person/Payment.java
@@ -0,0 +1,56 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Student's payment status in the address book.
+ */
+public class Payment {
+
+    public final Boolean hasPaid;
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param hasPaid Boolean for payment status
+     */
+    public Payment(Boolean hasPaid) {
+        requireNonNull(hasPaid);
+        this.hasPaid = hasPaid;
+    }
+
+    /**
+     * Constructs an {@code Address}.
+     * Set to false as default
+     */
+    public Payment() {
+        this.hasPaid = false;
+    }
+
+    @Override
+    public String toString() {
+        return hasPaid ? "Paid" : "Not Paid";
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Payment)) {
+            return false;
+        }
+
+        Payment otherPayment = (Payment) other;
+        return hasPaid.equals(otherPayment.hasPaid);
+    }
+
+    @Override
+    public int hashCode() {
+        return hasPaid.hashCode();
+    }
+
+}
+
