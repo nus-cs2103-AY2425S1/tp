799f2a85429000d19781726ba122dc550d64fd56
Otto Tan <OttoTan>
2024-10-09 22:05:45 +0800

    Update relevant tests regarding Edit commands

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -15,7 +15,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
---
+++ src/test/java/seedu/address/testutil/TypicalNames.java
@@ -0,0 +1,12 @@
+package seedu.address.testutil;
+
+import seedu.address.model.person.Name;
+
+/**
+ * A utility class containing a list of {@code NAME} objects to be used in tests.
+ */
+public class TypicalNames {
+    public static final Name NAME_FIRST_PERSON = new Name("Bob");
+    public static final Name NAME_SECOND_PERSON = new Name("Amy");
+    public static final Name NAME_THIRD_PERSON = new Name("Charlie");
+}
