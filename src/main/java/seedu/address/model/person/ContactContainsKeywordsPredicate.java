adca7b53baeea439332f33a93bd1ebcc5f71f68a
KrashKart <e1113755@u.nus.edu>
2024-10-09 23:54:52 +0800

    Add hierachy to ContainsKeywordsPredicate
    Let's
    * create an abstract ContainsKeywordsPredicate class
    * let Name-, Contact- and EmailContainsKeywordsPredicate inherit
    from the abstract class
    
    This is done to create a hierachy of Predicates used for the
    various find commands: find by name, contact and email

--- src/main/java/seedu/address/model/person/ContactContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/ContactContainsKeywordsPredicate.java
@@ -9,16 +9,16 @@ import seedu.address.commons.util.ToStringBuilder;
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
  */
-public class NameContainsKeywordsPredicate extends ContainsKeywordsPredicate {
+public class ContactContainsKeywordsPredicate extends ContainsKeywordsPredicate {
 
-    public NameContainsKeywordsPredicate(List<String> keywords) {
-         super(keywords);
+    public ContactContainsKeywordsPredicate(List<String> keywords) {
+        super(keywords);
     }
 
     @Override
     public boolean test(Person person) {
         return this.getKeywords().stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword));
     }
 
     @Override
@@ -28,11 +28,11 @@ public class NameContainsKeywordsPredicate extends ContainsKeywordsPredicate {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof NameContainsKeywordsPredicate)) {
+        if (!(other instanceof ContactContainsKeywordsPredicate)) {
             return false;
         }
 
-        NameContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (NameContainsKeywordsPredicate) other;
+        ContactContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (ContactContainsKeywordsPredicate) other;
         return this.getKeywords().equals(otherNameContainsKeywordsPredicate.getKeywords());
     }
 
---
+++ src/main/java/seedu/address/model/person/ContainsKeywordsPredicate.java
@@ -0,0 +1,42 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+abstract public class ContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public ContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    protected List<String> getKeywords() {
+        return this.keywords;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        ContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (ContainsKeywordsPredicate) other;
+        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
