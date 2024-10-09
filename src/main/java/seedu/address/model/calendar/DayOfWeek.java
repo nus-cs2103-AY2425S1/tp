2dd27e398e2539b467a4e30ec9e4066e567ad1f2
josh1248 <joshthoo@gmail.com>
2024-10-09 13:16:50 +0800

    Define attributes of Lesson, including enums

---
+++ src/main/java/seedu/address/model/calendar/DayOfWeek.java
@@ -0,0 +1,14 @@
+package seedu.address.model.calendar;
+
+/**
+ * Represents the 7 possible days "Monday, Tuesday, ..." in a calendar week.
+ */
+public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
--- src/main/java/seedu/address/model/calendar/Lesson.java
+++ src/main/java/seedu/address/model/calendar/Lesson.java
@@ -1,8 +1,14 @@
 package seedu.address.model.calendar;
 
+import java.time.LocalTime;
+
 /**
  * Lesson class
  */
 public class Lesson {
+    String name;
+    DayOfWeek startDay;
+    LocalTime startTime;
+    LocalTime endTime;
 
 }
