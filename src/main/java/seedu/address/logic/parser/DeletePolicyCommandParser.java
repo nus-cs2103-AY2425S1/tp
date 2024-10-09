c9bc449a08c9ea4185983af5cee86880077886f0
Justincjr <jchoojr@hotmail.com>
2024-10-09 22:26:45 +0800

    Add tests for DeletePolicyCommandParser and DeletePolicyCommand

--- src/main/java/seedu/address/logic/parser/DeletePolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePolicyCommandParser.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_TYPE;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.DeletePolicyCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.policy.PolicyMap;
@@ -22,22 +23,19 @@ public class DeletePolicyCommandParser implements Parser<DeletePolicyCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_POLICY_TYPE);
 
-        try {
-            Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
-
-            // Check if the policy type is present
-            if (!argMultimap.getValue(PREFIX_POLICY_TYPE).isPresent()) {
-                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                        DeletePolicyCommand.MESSAGE_USAGE));
-            }
-
-            PolicyMap policyDel = ParserUtil.parsePolicies(argMultimap.getAllValues(PREFIX_POLICY_TYPE));
-
-            return new DeletePolicyCommand(index, policyDel);
-
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePolicyCommand.MESSAGE_USAGE), pe);
+        if (!(argMultimap.getValue(PREFIX_POLICY_TYPE).isPresent())
+                || argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeletePolicyCommand.MESSAGE_USAGE));
         }
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble()); //index is the preamble
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePolicyCommand.MESSAGE_USAGE), ive);
+        }
+        PolicyMap policyDel = ParserUtil.parsePolicies(argMultimap.getAllValues(PREFIX_POLICY_TYPE));
+        return new DeletePolicyCommand(index, policyDel);
     }
 }
