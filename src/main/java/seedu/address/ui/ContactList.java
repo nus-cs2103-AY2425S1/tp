8983e16a487cc2c912d9ab1276cf1dcb474c687e
Nicholas-Cheng-De-Fei <nicholas_cheng@u.nus.edu>
2024-10-09 18:58:16 +0800

    Remove logger from ContactList.java

--- src/main/java/seedu/address/ui/ContactList.java
+++ src/main/java/seedu/address/ui/ContactList.java
@@ -1,11 +1,8 @@
 package seedu.address.ui;
 
-import java.util.logging.Logger;
-
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
 
 /**
@@ -14,8 +11,6 @@ import seedu.address.logic.Logic;
 public class ContactList extends UiPart<Region> {
 
     private static final String FXML = "ContactList.fxml";
-
-    private final Logger logger = LogsCenter.getLogger(getClass());
 
     private Logic logic;
 
