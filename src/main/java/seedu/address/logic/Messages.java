7c9a483467a263d885fa77d57a082a6f0a10fd3c
jiayr99 <e1249294@u.nus.edu>
2024-10-09 20:04:35 +0800

    Add support for reminder command

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -21,6 +21,9 @@ public class Messages {
     public static final String MESSAGE_INVALID_DATE_FORMAT = "Invalid date format! "
             + "Please use the format: yyyy-MM-dd HHmm";
 
+    public static final String MESSAGE_INVALID_REMINDER_FORMAT = "Invalid reminder time!"
+            + "Please enter either: n day/hour(s)";
+
     /**
      * Returns an error message indicating the duplicate prefixes.
      */
--- src/main/java/seedu/address/logic/parser/ReminderCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ReminderCommandParser.java
@@ -2,9 +2,12 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_DATE_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_REMINDER_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMINDER;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 import seedu.address.logic.commands.ReminderCommand;
@@ -26,13 +29,23 @@ public class ReminderCommandParser implements Parser<ReminderCommand> {
     public ReminderCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_DATE, PREFIX_REMINDER);
+        String name = argMultimap.getPreamble();
+        String appointmentDate = argMultimap.getValue(PREFIX_DATE).orElse("");
+        String reminderTime = argMultimap.getValue(PREFIX_REMINDER).orElse("");
+
+        // Check for missing name, date, or reminder
+        if (name.isEmpty() || appointmentDate.isEmpty()) {
+            throw new ParseException(MESSAGE_INVALID_DATE_FORMAT);
+        }
+        if (reminderTime.isEmpty()) {
+            throw new ParseException(MESSAGE_INVALID_REMINDER_FORMAT);
+        }
+
         try {
-            String name = argMultimap.getPreamble();
-            String appointmentDate = argMultimap.getValue(PREFIX_DATE).orElse("");
-            String reminderTime = argMultimap.getValue(PREFIX_REMINDER).orElse("");
-            return new ReminderCommand(name, appointmentDate, reminderTime);
+            LocalDateTime.parse(appointmentDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
         } catch (DateTimeParseException e) {
             throw new ParseException(MESSAGE_INVALID_DATE_FORMAT);
         }
+        return new ReminderCommand(name, appointmentDate, reminderTime);
     }
 }
