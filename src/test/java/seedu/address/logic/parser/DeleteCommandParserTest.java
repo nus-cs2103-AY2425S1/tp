b6174db4022dc85bd9695bd605067a8cab949c1a
xGladiate <sharonsohxuanhui@hotmail.com>
2024-10-10 01:19:39 +0800

    Modify testcases for DeleteCommandParser

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON_LIST;
 
 import org.junit.jupiter.api.Test;
 
@@ -22,11 +23,21 @@ public class DeleteCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteCommand(INDEX_FIRST_PERSON));
+        assertParseSuccess(parser, "1", new DeleteCommand(INDEX_FIRST_PERSON_LIST));
+    }
+
+    @Test
+    public void parse_validArgs_multipleArgs_returnsDeleteCommand() {
+        assertParseSuccess(parser, "1 2", new DeleteCommand(INDEX_FIRST_PERSON_LIST));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidArgs_multipleArgs_throwsParseException() {
+        assertParseFailure(parser, "1 a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
     }
 }
