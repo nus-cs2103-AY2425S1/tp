9bfa856c5137e1fb98c746a694538b1cdb494f88
chashaobao123 <chooi.shaoxian@gmail.com>
2024-10-09 13:22:17 +0800

    fixed minor typo in PersonBuilder.java file

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -98,7 +98,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, roomNumber, address, tags);
     }
 
 }
