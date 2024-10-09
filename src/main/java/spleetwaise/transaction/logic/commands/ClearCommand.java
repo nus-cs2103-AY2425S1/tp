07d65469c8e5e7a46cdf683a6b3e994cdb82a752
Yang Zhi <yangzhi.see@gmail.com>
2024-10-10 01:51:52 +0800

    Add basic clearTxn command
    - Added dedicated CommandException for transaction
    - Refactored addCommand to utilise dedicated transaction command exception class

---
+++ src/main/java/spleetwaise/transaction/logic/commands/ClearCommand.java
@@ -0,0 +1,21 @@
+package spleetwaise.transaction.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import spleetwaise.address.logic.commands.CommandResult;
+import spleetwaise.transaction.model.Model;
+import spleetwaise.transaction.model.TransactionBook;
+
+public class ClearCommand extends Command {
+
+    public static final String COMMAND_WORD = "clearTxn";
+    public static final String MESSAGE_SUCCESS = "Transaction book has been cleared!";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.setTransactionBook(new TransactionBook());
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
