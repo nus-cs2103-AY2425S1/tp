4442beb2d74b5966b07b1446830ef8973c2127cc
Edsel-Tan <riwk2015@gmail.com>
2024-10-09 21:18:38 +0800

    Add javadoc

--- src/main/java/seedu/address/logic/commands/DeleteAssignmentCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteAssignmentCommand.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGNMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ASSIGNMENT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_GRADE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 
+/**
+ * Deletes an assignment.
+ */
 public class DeleteAssignmentCommand extends Command {
     public static final String COMMAND_WORD = "adda";
 
--- src/main/java/seedu/address/logic/parser/DeleteAssignmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteAssignmentCommandParser.java
@@ -3,6 +3,9 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.DeleteAssignmentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new DeleteAssignmentCommand object
+ */
 public class DeleteAssignmentCommandParser implements Parser<DeleteAssignmentCommand> {
     @Override
     public DeleteAssignmentCommand parse(String userInput) throws ParseException {
