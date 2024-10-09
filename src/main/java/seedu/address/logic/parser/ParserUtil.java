1cfa52efd9c820dae53177f81921898dd227d97c
Ethan Wang <ethanwangkangen@gmail.com>
2024-10-09 19:34:49 +0800

    Add server command

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -13,6 +13,7 @@ import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
+import seedu.address.model.sort.SortParam;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -120,5 +121,15 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+
+    public static SortParam parseSortCommandParam(String sortParam) throws ParseException {
+        requireNonNull(sortParam);
+        String trimmedSortParam = sortParam.trim();
+        if (!SortParam.isValidSortParam(trimmedSortParam)) {
+            throw new ParseException(SortParam.MESSAGE_CONSTRAINTS);
+        }
+
+        return new SortParam(trimmedSortParam);
     }
 }
--- src/main/java/seedu/address/model/sort/SortParam.java
+++ src/main/java/seedu/address/model/sort/SortParam.java
@@ -1,4 +1,41 @@
 package seedu.address.model.sort;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.model.tag.Tag.isValidTagName;
+
 public class SortParam {
+
+    public static final String MESSAGE_CONSTRAINTS = "Sort parameters can only be 'name' or 'class'";
+
+    private SortField field;
+
+    private enum SortField{
+        NAME,
+        CLASS
+    };
+
+    /**
+     * Constructs a SortParam
+     *
+     * @param sortParamString either "name" or "class"
+     */
+    public SortParam(String sortParamString) {
+        requireNonNull(sortParamString);
+        checkArgument(isValidSortParam(sortParamString), MESSAGE_CONSTRAINTS);
+        if (sortParamString == "name") {
+            System.out.println("name");
+            this.field = SortField.NAME;
+        } else if (sortParamString == "class") {
+            System.out.println("class");
+            this.field = SortField.CLASS;
+        }
+    }
+
+    /**
+     * Returns true if a given string is a valid sort parameter.
+     */
+    public static boolean isValidSortParam(String test) {
+        return (test.equals("name") || test.equals("class"));
+    }
 }
