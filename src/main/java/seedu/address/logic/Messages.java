9b0b383369a77f8d57180e1bebdb03e5e3e65104
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-10-09 19:40:37 +0800

    Update Messages Class

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,8 +14,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The contact index provided is invalid";
-    public static final String MESSAGE_MISSING_INDEX = "The contact index is missing";
+    public static final String MESSAGE_INVALID_DISPLAYED_INDEX = "The index provided is invalid";
+    public static final String MESSAGE_MISSING_INDEX = "The index is missing";
     public static final String DELETE_COMMAND_USAGE = "Invalid delete command. Specify 'company', 'contact' or 'job'.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
