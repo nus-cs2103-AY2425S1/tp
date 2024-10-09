5a77e6175e582fdc570bbdc6a36b2edf5f142959
Casper Ng <casperngeen@gmail.com>
2024-10-09 10:48:02 +0800

    Rename package from address to ddd

---
+++ src/main/java/seedu/ddd/model/util/SampleDataUtil.java
@@ -0,0 +1,79 @@
+package seedu.ddd.model.util;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import seedu.ddd.model.AddressBook;
+import seedu.ddd.model.ReadOnlyAddressBook;
+import seedu.ddd.model.person.Address;
+import seedu.ddd.model.person.Client;
+import seedu.ddd.model.person.Contact;
+import seedu.ddd.model.person.Date;
+import seedu.ddd.model.person.Email;
+import seedu.ddd.model.person.Name;
+import seedu.ddd.model.person.Person;
+import seedu.ddd.model.person.Phone;
+import seedu.ddd.model.person.Service;
+import seedu.ddd.model.person.Vendor;
+import seedu.ddd.model.tag.Tag;
+
+/**
+ * Contains utility methods for populating {@code AddressBook} with sample data.
+ */
+public class SampleDataUtil {
+    public static Person[] getSamplePersons() {
+        return new Person[] {
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                new Address("Blk 30 Geylang Street 29, #06-40"),
+                getTagSet("friends")),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                getTagSet("colleagues", "friends")),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                getTagSet("neighbours")),
+            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                getTagSet("family")),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                new Address("Blk 47 Tampines Street 20, #17-35"),
+                getTagSet("classmates")),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                new Address("Blk 45 Aljunied Street 85, #11-31"),
+                getTagSet("colleagues"))
+        };
+    }
+
+    public static Contact[] getSampleContacts() {
+        return new Contact[] {
+            new Client(new Name("A"), new Phone("12345678"), new Email("a@a.com"), new Address("A"),
+                new Date("01 Jan 2000"),
+                getTagSet("test")),
+            new Vendor(new Name("B"), new Phone("12345678"), new Email("b@b.com"), new Address("B"),
+                new Service("Catering"),
+                getTagSet("test")),
+        };
+    }
+
+    public static ReadOnlyAddressBook getSampleAddressBook() {
+        AddressBook sampleAb = new AddressBook();
+        for (Person samplePerson : getSamplePersons()) {
+            sampleAb.addPerson(samplePerson);
+        }
+        for (Contact sampleContact : getSampleContacts()) {
+            sampleAb.addContact(sampleContact);
+        }
+        return sampleAb;
+    }
+
+    /**
+     * Returns a tag set containing the list of strings given.
+     */
+    public static Set<Tag> getTagSet(String... strings) {
+        return Arrays.stream(strings)
+                .map(Tag::new)
+                .collect(Collectors.toSet());
+    }
+
+}
---
+++ src/test/java/seedu/ddd/logic/commands/ListCommandTest.java
@@ -0,0 +1,39 @@
+package seedu.ddd.logic.commands;
+
+import static seedu.ddd.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.ddd.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.ddd.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.ddd.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.ddd.model.Model;
+import seedu.ddd.model.ModelManager;
+import seedu.ddd.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ListCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
