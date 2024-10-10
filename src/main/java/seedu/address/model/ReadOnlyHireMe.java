ff31118c1646a1e88df97f73a2c39fe620747504
choaticman <dylanchan66@yahoo.com.sg>
2024-10-10 21:07:45 +0800

    Add interfaces for HireMe load feature

---
+++ src/main/java/seedu/address/model/ReadOnlyHireMe.java
@@ -0,0 +1,16 @@
+package seedu.address.model;
+
+import javafx.collections.ObservableList;
+
+/**
+ * Unmodifiable view of an address book
+ */
+public interface ReadOnlyHireMe {
+
+    /**
+     * Returns an unmodifiable view of the intern applications list.
+     * This list will not contain any duplicate intern applications.
+     */
+    //ObservableList<InternshipApplication> getInternshipList();
+
+}
