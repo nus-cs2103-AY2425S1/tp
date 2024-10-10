0b4eedfe06939fed19d082b62e400a4c46d84ccf
ItsMeNengYi <20040408gry@gmail.com>
2024-10-10 23:19:11 +0800

    AddressBook: Add ProjectListPanel and ProjectListCard
    Add ProjectListCard.fxml and ProjectListPanel.fxml to resources/view.
    Add ProjectCard class and ProjectListPanel class to seedu.address.ui.
    
    Fix bug in logic.getFilteredProjectList() which returns an empty ObservableArray by temporarily replacing it with a static list containing a single project.
    
    The ProjectListPanel displays projects by retrieving the data from an ObservableArray. The bug prevented the panel from populating, so a static project is used as a placeholder for now.

---
+++ src/main/java/seedu/address/ui/ProjectCard.java
@@ -0,0 +1,45 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.project.Project;
+
+import java.util.Comparator;
+
+/**
+ * An UI component that displays information of a {@code Project}.
+ */
+public class ProjectCard extends UiPart<Region> {
+
+    private static final String FXML = "ProjectListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Project project;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+
+    /**
+     * Creates a {@code ProjectCode} with the given {@code Project} and index to display.
+     */
+    public ProjectCard(Project project, int displayedIndex) {
+        super(FXML);
+        this.project = project;
+        id.setText(displayedIndex + ". ");
+        name.setText(project.getName().fullName);
+    }
+}
