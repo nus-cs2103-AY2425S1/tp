97cea107985f6db97c115c603640e6e26cb0e02d
zwe <zwezeya02@gmail.com>
2024-10-10 13:03:39 +0800

    Style code

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -32,7 +32,8 @@ public class AddCommand extends Command {
             + PREFIX_DATE + "16/09/24";
 
     public static final String MESSAGE_SUCCESS = "New internship application added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This internship application already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_PERSON =
+            "This internship application already exists in the address book";
 
     private final Person toAdd;
 
