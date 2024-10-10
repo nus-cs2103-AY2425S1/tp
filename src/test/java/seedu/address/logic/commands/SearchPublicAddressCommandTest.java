3a8bc67d32bc502c309c00c4281ceb43b56d148a
travisim <64481469+travisim@users.noreply.github.com>
2024-10-10 22:29:27 +0800

    Add Hook SearchPublicAddressCommand and associated tests

---
+++ src/test/java/seedu/address/logic/commands/SearchPublicAddressCommandTest.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.SearchPublicAddressCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for SearchPublicAddressCommand.
+ */
+public class SearchPublicAddressCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    @Test
+    public void execute() {
+        assertCommandFailure(new SearchPublicAddressCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+}
