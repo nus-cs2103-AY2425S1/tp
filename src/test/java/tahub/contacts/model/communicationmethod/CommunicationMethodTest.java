2b751cc77fbfcfe73d176863bcb741546448a782
accountexeregister <yeohchongyics@gmail.com>
2024-10-10 14:50:53 +0800

    Fix import order in CommunicationMethodTest

--- src/test/java/tahub/contacts/model/communicationmethod/CommunicationMethodTest.java
+++ src/test/java/tahub/contacts/model/communicationmethod/CommunicationMethodTest.java
@@ -1,7 +1,8 @@
 package tahub.contacts.model.communicationmethod;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class CommunicationMethodTest {
 
