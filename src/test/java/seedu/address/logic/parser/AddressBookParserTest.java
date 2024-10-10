f20e32346b19ab4a3207cce9bbac65d0d54b8ad3
IanCheah <iancheah24@gmail.com>
2024-10-10 22:29:19 +0800

    Fix trailing white space from line 61

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -58,7 +58,7 @@ public class AddressBookParserTest {
                 DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
     }
-    
+
     @Test
     public void parseCommand_edit() throws Exception {
         Person person = new PersonBuilder().build();
