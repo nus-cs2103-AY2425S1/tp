c1e3721e0dcf79c82e7f52ccfa859a78aa2b6227
Zac <zacheryngky@gmail.com>
2024-10-11 00:35:05 +0800

    Create utility class to contain typical concerts

---
+++ src/test/java/seedu/address/testutil/TypicalConcerts.java
@@ -0,0 +1,36 @@
+package seedu.address.testutil;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_COACHELLA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_GLASTONBURY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_COACHELLA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_GLASTONBURY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_COACHELLA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_GLASTONBURY;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.concert.Concert;
+
+/**
+ * A utility class containing a list of {@code Concert} objects to be used in tests.
+ */
+public class TypicalConcerts {
+    public static final Concert COACHELLA = new ConcertBuilder()
+            .withName(VALID_NAME_COACHELLA)
+            .withAddress(VALID_ADDRESS_COACHELLA)
+            .withDate(VALID_DATE_COACHELLA)
+            .build();
+    public static final Concert GLASTONBURY = new ConcertBuilder()
+            .withName(VALID_NAME_GLASTONBURY)
+            .withAddress(VALID_ADDRESS_GLASTONBURY)
+            .withDate(VALID_DATE_GLASTONBURY)
+            .build();
+
+    private TypicalConcerts() {} // prevents instantiation
+
+    public static List<Concert> getTypicalConcerts() {
+        return new ArrayList<>(Arrays.asList(COACHELLA, GLASTONBURY));
+    }
+}
