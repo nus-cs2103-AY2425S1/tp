d10940986abd05d8981bd9fd475616576413b57b
ckaayy <e1122115@u.nus.edu>
2024-10-10 20:20:11 +0800

    Add general structure and methods to ListExpiringPoliciesCommand class, execute method logic yet to be implemented

--- src/main/java/seedu/address/logic/commands/ListExpiringPoliciesCommand.java
+++ src/main/java/seedu/address/logic/commands/ListExpiringPoliciesCommand.java
@@ -13,15 +13,42 @@ public class ListExpiringPoliciesCommand extends Command {
      */
     public static final String COMMAND_WORD = "listExpiringPolicies";
 
-    /**
-     * Describes how to use the command and gives an example for users.
-     */
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists all policies nearing expiry within the next 30 days.\n"
+            + "Example: " + COMMAND_WORD;
+
     private static final String MESSAGE_NOT_IMPLEMENTED_YET = "ListExpiringPolicies command has not "
             + "been implemented yet";
 
+    private static final String MESSAGE_SUCCESS = "The following policies are near expiry:";
+    private static final String MESSAGE_NO_EXPIRING_POLICY = "No policies expiring within the next 30 days!";
+    private static final String MESSAGE_FAILURE = "Failed to retrieve expiring policies. Please try again.";
+    private static final String MESSAGE_POLICY_LISTED_DETAILS = "Phone: %1$s, Policy Type: %2$s, Premium Amount: %3$s, "
+            + "Coverage Amount: %4$s, Expiry Date: %5$s";
+
+
     @Override
-    public CommandResult execute(Model mode) throws CommandException {
+    public CommandResult execute(Model model) throws CommandException {
         throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ListExpiringPoliciesCommand)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return COMMAND_WORD;
     }
 }
 
