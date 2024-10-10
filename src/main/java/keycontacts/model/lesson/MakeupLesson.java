6858e6fc7cd641d82ec2614afedb1629fdc0b683
Bryan <bryanchenyu616@gmail.com>
2024-10-10 23:46:04 +0800

    Add JavaDoc comment

--- src/main/java/keycontacts/model/lesson/MakeupLesson.java
+++ src/main/java/keycontacts/model/lesson/MakeupLesson.java
@@ -1,5 +1,8 @@
 package keycontacts.model.lesson;
 
+/**
+ * Represents a makeup lesson which happens if a student misses a lesson.
+*/
 public class MakeupLesson extends RegularLesson {
 
     public MakeupLesson(Day lessonDay, Time startTime, Time endTime) {
