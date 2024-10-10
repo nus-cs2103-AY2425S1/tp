0437eed89c9b407b53f49e62e2510a81d92332b2
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-10-10 22:56:54 +0800

    Add unpaid prefix

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,5 +12,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_HASPAID = new Prefix("paid/");
-
+    public static final Prefix PREFIX_NOTPAID = new Prefix("unpaid/");
 }
