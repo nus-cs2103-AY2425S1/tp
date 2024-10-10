8ae87a1a1e4558f2335a3b1bc532d183a5ec92bc
t-leongchuan <t.leongchuan@gmail.com>
2024-10-10 23:02:12 +0800

    Add additional test for IdentityNumber for better coverage

--- src/test/java/seedu/address/model/person/IdentityNumberTest.java
+++ src/test/java/seedu/address/model/person/IdentityNumberTest.java
@@ -42,7 +42,7 @@ public class IdentityNumberTest {
         assertFalse(IdentityNumber.isValidIdentityNumber("S1234567C"));
         assertFalse(IdentityNumber.isValidIdentityNumber("S7654321D"));
 
-        // valid identity numbers
+        // valid identity numbers (S or T [Type NRIC])
         assertTrue(IdentityNumber.isValidIdentityNumber("S6116686F"));
         assertTrue(IdentityNumber.isValidIdentityNumber("S1005756F"));
         assertTrue(IdentityNumber.isValidIdentityNumber("S1210769B"));
@@ -50,5 +50,50 @@ public class IdentityNumberTest {
         assertTrue(IdentityNumber.isValidIdentityNumber("S1383485G"));
         assertTrue(IdentityNumber.isValidIdentityNumber("S7147129B"));
         assertTrue(IdentityNumber.isValidIdentityNumber("S5940368J"));
+
+        assertTrue(IdentityNumber.isValidIdentityNumber("T2938587J"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("T2611250D"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("T9733087A"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("T6169846E"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("T9704009A"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("T5362621H"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("T6659225H"));
+
+        //valid identity numbers (F or G [Type FIN])
+        assertTrue(IdentityNumber.isValidIdentityNumber("F5999705W"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("F5586227M"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("F7378724Q"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("F2135323W"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("F5934095Q"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("F8194626N"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("F5944510M"));
+
+        assertTrue(IdentityNumber.isValidIdentityNumber("G5384954M"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("G0313597M"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("G6723425T"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("G5382668M"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("G8103638T"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("G2867837T"));
+        assertTrue(IdentityNumber.isValidIdentityNumber("G9276190N"));
+    }
+
+    @Test
+    public void equals() {
+        IdentityNumber identityNumber = new IdentityNumber("F5999705W");
+
+        // same values -> returns true
+        assertTrue(identityNumber.equals(new IdentityNumber("F5999705W")));
+
+        // same object -> returns true
+        assertTrue(identityNumber.equals(identityNumber));
+
+        // null -> returns false
+        assertFalse(identityNumber.equals(null));
+
+        // different types -> returns false
+        assertFalse(identityNumber.equals(0x534));
+
+        // different values -> returns false
+        assertFalse(identityNumber.equals(new IdentityNumber("S2065586J")));
     }
 }
