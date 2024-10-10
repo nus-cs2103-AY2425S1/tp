782673ac5340317b8f35096ae80b060d8c0ae17d
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-10-10 23:10:42 +0800

    Add Unpaid command case

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,6 +18,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.PaidCommand;
+import seedu.address.logic.commands.UnpaidCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -80,6 +81,8 @@ public class AddressBookParser {
 
             case PaidCommand.COMMAND_WORD:
             return new PaidCommandParser().parse(arguments);
+
+            case UnpaidCommand.COMMAND_WORD:
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
