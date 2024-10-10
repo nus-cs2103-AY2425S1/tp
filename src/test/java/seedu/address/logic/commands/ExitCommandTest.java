ed36b3c406c235352455df554a807daa5383bc71
awdse22 <ngkhengyang@gmail.com>
2024-10-10 21:03:16 +0800

    Re-package ExitCommand and HelpCommand into generalcommands

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+import static seedu.address.logic.commands.generalcommands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
 import static seedu.address.logic.commands.PersonCommandTestUtil.assertCommandSuccess;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.generalcommands.ExitCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
