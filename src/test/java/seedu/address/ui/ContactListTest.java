0c8d0605bcc555116a528747f5ed4cd837bb0ebe
Nicholas-Cheng-De-Fei <nicholas_cheng@u.nus.edu>
2024-10-10 20:37:13 +0800

    Use JFXPanel to test JavaFx

--- src/test/java/seedu/address/ui/ContactListTest.java
+++ src/test/java/seedu/address/ui/ContactListTest.java
@@ -2,53 +2,33 @@ package seedu.address.ui;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeoutException;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.TypicalPersons;
 
 public class ContactListTest {
 
-    private static CountDownLatch latch = new CountDownLatch(1);
     private ObservableList<Person> personList;
 
-    // Dummy JavaFX application
-    public static class TestApp extends Application {
-        @Override
-        public void start(javafx.stage.Stage primaryStage) {
-            latch.countDown(); // Release the latch when the application starts
-        }
-    }
-
-    @BeforeAll
-    static void setUpOnce() throws Exception {
-        // Start the JavaFX application
-        new Thread(() -> Application.launch(TestApp.class)).start();
-        // Wait for the application to start
-        latch.await();
+    @BeforeEach
+    public void setup() {
+        new JFXPanel(); // Initializes the JavaFX runtime
     }
 
     @Test
-    void testComponentNotNull() throws TimeoutException, InterruptedException {
-
+    void testComponentNotNull() {
         this.personList = FXCollections.observableArrayList(TypicalPersons.getTypicalPersons());
 
+        Scene scene = new Scene(new StackPane());
         assertDoesNotThrow(() -> new ContactList(this.personList));
-    }
 
-    @AfterAll
-    static void tearDownOnce() {
-        // Shut down the JavaFX application
-        Platform.exit();
     }
 
 }
