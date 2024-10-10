cf0bbefb469849383fd9529070122504c03be252
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-10-10 23:12:35 +0800

    Implement ExportCommand.equals defintion and tests

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.ExportCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
@@ -21,6 +23,23 @@ public class ExportCommandTest {
 
     @Test
     public void equals() {
-        // TODO: Implement test for ExportCommand.equals(otherCommand)
+        final ExportCommand standardCommand = new ExportCommand("csv");
+        final String differentFormat = "pdf";
+
+        // same values -> returns true
+        ExportCommand commandWithSameValues = new ExportCommand("csv");
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different export format -> returns false
+        assertFalse(standardCommand.equals(new ExportCommand(differentFormat)));
     }
 }
