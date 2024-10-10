284a5969e182d0c968e4c6827bf089b55c939d45
LAPTOP-1NO5TOS6\teren <e1122500@u.nus.edu>
2024-10-10 22:31:24 +0800

    Change reference to AB3 in website to SocialBook
    Add placeholder commands to help window

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.lang.StringBuilder;
+
 import java.util.logging.Logger;
 
 import javafx.fxml.FXML;
@@ -15,11 +17,29 @@ import seedu.address.commons.core.LogsCenter;
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String USERGUIDE_URL = "https://ay2425s1-cs2103t-f14b-1.github.io/tp/UserGuide.html";
+    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL + "\n";
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
+
+    private static final String TITLE = "Below are the commands you can use in the application:\n";
+
+    private static final String ADD_COMMAND = "add\n";
+
+    private static final String CLEAR_COMMAND = "clear\n";
+
+    private static final String DELETE_COMMAND = "delete\n";
+
+    private static final String EDIT_COMMAND = "edit\n";
+
+    private static final String FIND_COMMAND = "find\n";
+
+    private static final String LIST_COMMAND = "list\n";
+
+    private static final String HELP_COMMAND = "help\n";
+
+    private static final String[] COMMANDS = {ADD_COMMAND, CLEAR_COMMAND, DELETE_COMMAND, EDIT_COMMAND, FIND_COMMAND, LIST_COMMAND, HELP_COMMAND};
 
     @FXML
     private Button copyButton;
@@ -34,7 +54,12 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
-        helpMessage.setText(HELP_MESSAGE);
+        StringBuilder helpMessageBuilder = new StringBuilder(TITLE);
+        for (String command : COMMANDS) {
+            helpMessageBuilder.append(command);
+        }
+        helpMessageBuilder.append(HELP_MESSAGE);
+        helpMessage.setText(helpMessageBuilder.toString());
     }
 
     /**
