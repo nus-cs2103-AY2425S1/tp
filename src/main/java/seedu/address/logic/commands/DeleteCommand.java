f62923be82f3a8b4ed45bda74babc4388b8c1e81
Sparsh Kumar <e1121161@u.nus.edu>
2024-10-10 19:18:03 +0800

    Add alternative for delete command

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -18,6 +18,8 @@ public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD = "delete";
 
+    public static final String ALT_COMMAND_WORD = "del";
+
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the person identified by the index number used in the displayed person list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
