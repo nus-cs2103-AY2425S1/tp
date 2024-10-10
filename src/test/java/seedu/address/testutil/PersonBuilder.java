02ea386dddc40c4068b6a85abc34493f48941156
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-10-10 23:33:23 +0800

    fix: Update some broken tests

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -10,6 +10,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.PostalCode;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -22,11 +23,13 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_POSTAL_CODE = "123456";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private PostalCode postalCode;
     private Set<Tag> tags;
     private HashMap<Order, Integer> orderFrequency;
 
@@ -38,6 +41,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        postalCode = new PostalCode(DEFAULT_POSTAL_CODE);
         tags = new HashSet<>();
         orderFrequency = new HashMap<>();
     }
@@ -50,6 +54,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        postalCode = personToCopy.getPostalCode();
         tags = new HashSet<>(personToCopy.getTags());
         orderFrequency = new HashMap<>(personToCopy.getOrderFrequency());
     }
@@ -94,6 +99,11 @@ public class PersonBuilder {
         return this;
     }
 
+    public PersonBuilder withPostalCode(String postalCode) {
+        this.postalCode = new PostalCode(postalCode);
+        return this;
+    }
+
     /**
      * Sets the {@code orderFrequency} of the {@code Person} that we are building.
      */
@@ -103,7 +113,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags, orderFrequency);
+        return new Person(name, phone, email, address, postalCode, tags, orderFrequency);
     }
 
 }
