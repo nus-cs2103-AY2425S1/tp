0154b58bc198f5482f8961b7956e2928fec6c037
ItsMeNengYi <20040408gry@gmail.com>
2024-10-10 23:41:04 +0800

    Fix Checkstyle error

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
