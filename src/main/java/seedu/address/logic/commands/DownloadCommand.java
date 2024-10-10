14926f6903f6d0de0865b6fb15e5788a5d32fe50
arnav-goel10 <e1130141@u.nus.edu>
2024-10-10 20:56:11 +0800

    Add download command

--- src/main/java/seedu/address/logic/commands/DownloadCommand.java
+++ src/main/java/seedu/address/logic/commands/DownloadCommand.java
@@ -2,7 +2,12 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.ObservableList;
+import seedu.address.commons.util.CsvUtil;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.storage.StorageManager;
+
 
 /**
  * Downloads the csv file.
@@ -15,6 +20,9 @@ public class DownloadCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+        ObservableList<Person> addressBookJson = model.getAddressBook().getPersonList();
+        String addressBookCsv = CsvUtil.convertObservableListToCsv(addressBookJson);
+        StorageManager.saveCsvToFile(addressBookCsv);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
