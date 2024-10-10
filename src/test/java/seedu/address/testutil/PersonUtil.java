d7f9e8ce7c1aff7f5af746cf0a7d3b9c724eea47
FooChao <e1121427@u.nus.edu>
2024-10-10 22:10:07 +0800

    Fix Stylechecks Errors

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -8,8 +8,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Set;
 
-import seedu.address.logic.commands.personcommands.AddPersonCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.commands.personcommands.AddPersonCommand;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 
@@ -21,7 +21,7 @@ public class PersonUtil {
     /**
      * Returns an add command string for adding the {@code person}.
      */
-    public static String getAddCommand(Person person) {
+    public static String getAddPersonCommand(Person person) {
         return AddPersonCommand.COMMAND_WORD + " " + getPersonDetails(person);
     }
 
