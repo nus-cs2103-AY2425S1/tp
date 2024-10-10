6b6b9a0f02db6b3a54bc8d7ab4c6f04ca1f25beb
KrashKart <e1113755@u.nus.edu>
2024-10-10 11:12:48 +0800

    Add logic to check find type
    Use Regex to check which tag is input to find

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -3,7 +3,11 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import seedu.address.logic.commands.AbstractFindCommand;
+import seedu.address.logic.commands.FindByNameCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
@@ -11,23 +15,32 @@ import seedu.address.model.person.NameContainsKeywordsPredicate;
 /**
  * Parses input arguments and creates a new FindCommand object
  */
-public class FindCommandParser implements Parser<FindCommand> {
+public class FindCommandParser implements Parser<AbstractFindCommand> {
+
+    public static final Pattern KEYWORD_EXTRACTOR =
+            Pattern.compile("^^(?<tag>/[enc]) (?<argument>.+)$");
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public FindCommand parse(String args) throws ParseException {
+    public AbstractFindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
+        Matcher m = KEYWORD_EXTRACTOR.matcher(args);
+        if (trimmedArgs.isEmpty() || !m.matches()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        String tag = m.group("tag");
+        String searchTerm = m.group("argument");
+        switch (tag) {
+        case "/n":
+            return new FindByNameCommand(
+                    new NameContainsKeywordsPredicate(Arrays.asList(searchTerm)));
+        default:
+            return null; // temporary value, this should not occur due to regex
+        }
     }
-
 }
