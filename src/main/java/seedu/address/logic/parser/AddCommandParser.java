48c956b0c124af2e61059ab800feed82f77d25fd
CliffKoes <cliffkoes@gmail.com>
2024-10-10 13:09:13 +0800

    Update EditPersonDescriptorTest and PersonTest for tutorials

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
 
 import java.util.HashSet;
 import java.util.Set;
--- src/main/java/seedu/address/logic/parser/MarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
 
