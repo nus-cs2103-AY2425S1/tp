d4c5b7ffdcb4f2422466ffbc8fb60b83e0e327ce
Jaehyeok Choi <77jack0105@gmail.com>
2024-10-11 02:13:41 +0800

    Refractor EditCommand

--- src/main/java/seedu/address/logic/commands/EditPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/EditPersonCommand.java
@@ -1,26 +1,26 @@
 package seedu.address.logic.commands;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import seedu.address.commons.util.CollectionUtil;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.Model;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
-import seedu.address.logic.Messages;
-
-
 
 /**
  * Edits the details of an existing person in the address book.
