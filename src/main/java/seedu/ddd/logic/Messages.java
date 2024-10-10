1ff7fe5c94c6d51389f8838a06624f859b56233f
yizhong187 <yizhong187@gmail.com>
2024-10-10 17:00:51 +0800

    Add invalid contact type message

--- src/main/java/seedu/ddd/logic/Messages.java
+++ src/main/java/seedu/ddd/logic/Messages.java
@@ -16,6 +16,7 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_CONTACT_TYPE = "Invalid contact type! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     // TODO: determine if "contact" should also be added here
     public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "The client index provided is invalid";
