4a26181e079bcc02ff0ae7f7267cb0202477f23d
xqtann <xqxinquan@gmail.com>
2024-10-10 18:40:51 +0800

    Create AddBuyCommandParser with dummy return values

---
+++ src/main/java/seedu/address/logic/parser/AddBuyCommandParser.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class AddBuyCommandParser implements Parser<Command> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddBuyCommand
+     * and returns an AddBuyCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public Command parse(String args) throws ParseException {
+        return null;
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return false;
+    }
+}
