874fbadc6ebccf8204ef1c41e3e03b37daaf99a1
SeanFoongjt <seanfoongjt@gmail.com>
2024-10-10 23:13:06 +0800

    Improve Name.java to meet style standards

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,8 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String CHAR_MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters, spaces, hyphens commas +" +
-                    "or apostrophes, and it should not be blank";
+            "Names should only contain alphanumeric characters, spaces, hyphens commas +"
+                    + "or apostrophes, and it should not be blank";
 
     public static final String LENGTH_MESSAGE_CONSTRAINTS =
             "Names should only contain up to 300 characters for ease of storage.";
@@ -54,8 +54,9 @@ public class Name {
     /**
      * Returns true if a given string is valid.
      */
-    public static boolean isValidName(String test) { return isValidLength(test) && hasValidChars(test);}
-
+    public static boolean isValidName(String test) {
+        return isValidLength(test) && hasValidChars(test);
+    }
 
     @Override
     public String toString() {
