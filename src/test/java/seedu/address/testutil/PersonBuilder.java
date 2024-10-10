15cacf2f4208bd15743e4e7e575b74e8c7c0e4b6
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-10-11 02:18:22 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -7,6 +7,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.TelegramHandle;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -77,7 +78,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, tags);
+        return new Person(name, phone, email, new TelegramHandle(""), tags);
     }
 
 }
