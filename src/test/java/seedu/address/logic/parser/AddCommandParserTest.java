cf2cbd28f9bb448600b91eb965d6323c7d8d194d
stanleytangzh <111631172+stanleytangzh@users.noreply.github.com>
2024-10-10 23:55:40 +0800

    Fix test cases

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -51,6 +51,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Status;
@@ -176,7 +177,7 @@ public class AddCommandParserTest {
 
         // invalid note
         assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
-                + STATUS_DESC_BOB + INVALID_NOTE_DESC + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Address.MESSAGE_CONSTRAINTS);
+                + STATUS_DESC_BOB + INVALID_NOTE_DESC + TAG_DESC_HUSBAND + TAG_DESC_FRIEND, Note.MESSAGE_CONSTRAINTS);
 
         // invalid tag
         assertParseFailure(parser, NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB + ADDRESS_DESC_BOB
