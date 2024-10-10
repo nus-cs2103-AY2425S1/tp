5f95a3c6fff7c51ab22d43e71240f2b26317e744
gohqingkhang <143182152+gohqingkhang@users.noreply.github.com>
2024-10-10 23:38:08 +0800

    Update TypicalPersons.java

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -68,7 +68,7 @@ public class TypicalPersons {
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withRegisterNumber(VALID_REGISTER_NUMBER_BOB)
             .withSex(VALID_SEX_BOB).withStudentClass(VALID_STUDENT_CLASS_BOB)
-	    .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+            .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
