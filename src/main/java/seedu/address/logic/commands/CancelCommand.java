9ede58d00e04c86dfced066b5a47b9c022aac92e
marchjlim <marchjlim@gmail.com>
2024-10-10 11:58:18 +0800

    Add Cancel feature to cancel events

---
+++ src/main/java/seedu/address/logic/commands/CancelCommand.java
@@ -0,0 +1,72 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+/**
+ * Cancels an event identified using it's displayed index from the address book.
+ */
+public class CancelCommand extends Command {
+
+    public static final String COMMAND_WORD = "cancel";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Cancels an event identified by the index number used in the displayed event list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Cancelled event: %1$s";
+
+    private final Index targetIndex;
+
+    public CancelCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        //TODO: CHANGE THIS LATER FOR EVENT
+        // MODEL NEEDS TO HAVE GETFILTEREDEVENTLIST()
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+        }
+
+        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        //TODO: MODEL NEEDS TO HAVE DELETE PERSON
+        model.deletePerson(personToDelete);
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteCommand)) {
+            return false;
+        }
+
+        CancelCommand otherCancelCommand = (CancelCommand) other;
+        return targetIndex.equals(otherCancelCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetIndex", targetIndex)
+                .toString();
+    }
+}
