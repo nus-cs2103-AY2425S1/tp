e3fea30df1b57b3e965362790193853651a9c9e0
Sean Lim <seanlimdev856@gmail.com>
2024-10-10 22:33:55 +0800

    Integrate transaction storage

--- src/main/java/spleetwaise/address/logic/LogicManager.java
+++ src/main/java/spleetwaise/address/logic/LogicManager.java
@@ -43,7 +43,7 @@ public class LogicManager implements Logic {
      * Constructs a {@code LogicManager} with the given {@code Model} and {@code Storage}.
      */
     public LogicManager(spleetwaise.address.model.Model addressBookModel,
-                        spleetwaise.transaction.model.Model transactionModel, Storage storage) {
+        spleetwaise.transaction.model.Model transactionModel, Storage storage) {
         this.addressBookModel = addressBookModel;
         this.transactionModel = transactionModel;
         this.storage = storage;
@@ -96,7 +96,7 @@ public class LogicManager implements Logic {
     }
 
     private CommandResult executeAddressBookCommand(spleetwaise.address.logic.commands.Command addressBookCommand)
-            throws SpleetWaiseCommandException {
+        throws SpleetWaiseCommandException {
         CommandResult commandResult = addressBookCommand.execute(addressBookModel);
 
         // Save AddressBook data
@@ -112,10 +112,17 @@ public class LogicManager implements Logic {
     }
 
     private CommandResult executeTransactionCommand(spleetwaise.transaction.logic.commands.Command transactionCommand)
-            throws SpleetWaiseCommandException {
+        throws SpleetWaiseCommandException {
         CommandResult commandResult = transactionCommand.execute(transactionModel);
 
-        // TODO: Save Transactions data
+        // Save TransactionBook data
+        try {
+            storage.saveTransactionBook(transactionModel.getTransactionBook());
+        } catch (AccessDeniedException e) {
+            throw new CommandException(String.format(FILE_OPS_PERMISSION_ERROR_FORMAT, e.getMessage()), e);
+        } catch (IOException ioe) {
+            throw new CommandException(String.format(FILE_OPS_ERROR_FORMAT, ioe.getMessage()), ioe);
+        }
 
         return commandResult;
     }
