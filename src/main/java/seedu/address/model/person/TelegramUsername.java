44396ca5a8359a562fe56b4174fa1df441d9e4b5
chuajunyu <chuajunyu1@gmail.com>
2024-10-10 16:47:46 +0800

    Add TelegramUsername class and test file

---
+++ src/main/java/seedu/address/model/person/TelegramUsername.java
@@ -0,0 +1,66 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's Telegram Username in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidUsername(String)}
+ */
+public class TelegramUsername {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Telegram Usernames should only contain alphanumeric characters and underscores, between 5 to 32 characters"
+                    + "and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "^[a-zA-Z][a-zA-Z0-9_]{4,31}$";
+
+    public final String telegramUsername;
+
+    /**
+     * Constructs a {@code TelegramUsername}.
+     *
+     * @param username A valid telegram username.
+     */
+    public TelegramUsername(String username) {
+        requireNonNull(username);
+        checkArgument(isValidUsername(username), MESSAGE_CONSTRAINTS);
+        telegramUsername = username;
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidUsername(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return telegramUsername;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof seedu.address.model.person.TelegramUsername)) {
+            return false;
+        }
+
+        seedu.address.model.person.TelegramUsername otherUsername = (seedu.address.model.person.TelegramUsername) other;
+        return telegramUsername.equalsIgnoreCase(otherUsername.telegramUsername);
+    }
+
+    @Override
+    public int hashCode() {
+        return telegramUsername.hashCode();
+    }
+}
---
+++ src/test/java/seedu/address/model/person/TelegramUsernameTest.java
@@ -0,0 +1,68 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class TelegramUsernameTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new TelegramUsername(null));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidName = "";
+        assertThrows(IllegalArgumentException.class, () -> new TelegramUsername(invalidName));
+    }
+
+    @Test
+    public void isValidName() {
+        // null name
+        assertThrows(NullPointerException.class, () -> new TelegramUsername(null));
+
+        // invalid name
+        assertFalse(TelegramUsername.isValidUsername("")); // empty string
+        assertFalse(TelegramUsername.isValidUsername("1234")); // < 5 characters
+        assertFalse(TelegramUsername.isValidUsername("123456789012345678901234567890123")); // > 32 characters
+        assertFalse(TelegramUsername.isValidUsername("_1234")); // cannot start with underscore
+        assertFalse(TelegramUsername.isValidUsername("9hello")); // cannot start with a number
+        assertFalse(TelegramUsername.isValidUsername("^^^^^^^")); // only non-alphanumeric characters
+        assertFalse(TelegramUsername.isValidUsername("peter*")); // contains non-alphanumeric characters
+
+        // valid name
+        assertTrue(TelegramUsername.isValidUsername("peterjack")); // alphabets only
+        assertTrue(TelegramUsername.isValidUsername("p12345")); // alphabets and numbers only
+        assertTrue(TelegramUsername.isValidUsername("peter_the_2nd")); // alphanumeric characters with underscores
+        assertTrue(TelegramUsername.isValidUsername("CapitalTan")); // with capital letters
+        assertTrue(TelegramUsername.isValidUsername("David_Roger_Jackson_Ray_Jr_2nd")); // long names
+    }
+
+    @Test
+    public void equals() {
+        TelegramUsername username = new TelegramUsername("Valid_Name");
+
+        // same values -> returns true
+        assertEquals(username, new TelegramUsername("Valid_Name"));
+
+        // same values but different casing -> returns true
+        assertEquals(username, new TelegramUsername("valid_NAME"));
+
+        // same object -> returns true
+        assertEquals(username, username);
+
+        // null -> returns false
+        assertNotEquals(null, username);
+
+        // different types -> returns false
+        assertFalse(username.equals(5.0f));
+
+        // different values -> returns false
+        assertNotEquals(username, new TelegramUsername("Other_Valid_Name"));
+    }
+}
