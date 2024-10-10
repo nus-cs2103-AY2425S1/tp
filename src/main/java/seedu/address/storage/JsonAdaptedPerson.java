5c956d822ce6c42dd04a59156cc6f93beb85e5d5
gohqingkhang <143182152+gohqingkhang@users.noreply.github.com>
2024-10-10 22:53:16 +0800

    Update JsonAdaptedPerson.java

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -143,7 +143,7 @@ class JsonAdaptedPerson {
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
         return new Person(modelName, modelPhone, modelEmail, modelAddress, modelRegisterNumber, modelSex,
-				modelStudentClass, modelTags);
+	        modelStudentClass, modelTags);
     }
 
 }
