1a86e793e8b1bf26fd75923ebeb39e7bf1f7d745
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-10-10 11:17:04 +0800

    Touch up on the tests

--- src/test/java/tutorease/address/logic/parser/ContactCommandParserTest.java
+++ src/test/java/tutorease/address/logic/parser/ContactCommandParserTest.java
@@ -3,13 +3,11 @@ package tutorease.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static tutorease.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.jupiter.api.Test;
 
 import tutorease.address.logic.commands.Command;
 import tutorease.address.logic.commands.DeleteContactCommand;
-import tutorease.address.logic.commands.ListContactCommand;
 import tutorease.address.logic.parser.exceptions.ParseException;
 
 public class ContactCommandParserTest {
@@ -25,13 +23,6 @@ public class ContactCommandParserTest {
     }
 
     @Test
-    public void parse_validListCommand_success() throws Exception {
-        String validListCommand = "list";
-        Command result = parser.parse(validListCommand);
-        assertTrue(result instanceof ListContactCommand);
-    }
-
-    @Test
     public void parse_invalidCommandFormat_throwsParseException() {
         String invalidCommand = "unknownCommand";
         assertThrows(ParseException.class, () -> parser.parse(invalidCommand));
@@ -42,13 +33,6 @@ public class ContactCommandParserTest {
         String unknownSubCommand = "eat John Doe";
         ParseException exception = assertThrows(ParseException.class, () -> parser.parse(unknownSubCommand));
         assertEquals("Unknown contact sub-command: eat", exception.getMessage());
-    }
-
-    @Test
-    public void parse_emptyArgs_throwsParseException() {
-        String emptyArgs = " ";
-        ParseException exception = assertThrows(ParseException.class, () -> parser.parse(emptyArgs));
-        assertEquals(MESSAGE_INVALID_COMMAND_FORMAT, exception.getMessage());
     }
 }
 
