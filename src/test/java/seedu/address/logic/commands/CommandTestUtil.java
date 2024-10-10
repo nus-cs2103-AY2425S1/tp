dda23145573c5a5475bd0cba918d4fa6759f6f73
gohqingkhang <143182152+gohqingkhang@users.noreply.github.com>
2024-10-10 23:21:08 +0800

    Update CommandTestUtil.java

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -70,9 +70,9 @@ public class CommandTestUtil {
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_REGISTER_NUMBER_DESC = " " + PREFIX_REGISTER_NUMBER
-    		+ "41"; // Only numbers between 1 and 40 allowed
+            + "41"; // Only numbers between 1 and 40 allowed
     public static final String INVALID_SEX_DESC = " " + PREFIX_SEX + "H"; // Only 'M' and 'F' are allowed
-	public static final String INVALID_STUDENT_CLASS_DESC = " " + PREFIX_STUDENT_CLASS
+    public static final String INVALID_STUDENT_CLASS_DESC = " " + PREFIX_STUDENT_CLASS
             + "A1"; // wrong format used for student class
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
