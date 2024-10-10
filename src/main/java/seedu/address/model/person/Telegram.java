ed20551cb65a383a72f9a15e2943b1a01ee59523
zyshi's mac <ziyuan.shi@yahoo.com>
2024-10-10 19:38:53 +0800

    Change address to telegram
    Made many replacements of address to telegram, so as to update the add functionality to MVP specifications.

--- src/main/java/seedu/address/model/person/Telegram.java
+++ src/main/java/seedu/address/model/person/Telegram.java
@@ -4,18 +4,17 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Represents a Person's telegram username in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidTelegrram(String)}
  */
-public class Address {
+public class Telegram {
 
     public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
 
     /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
+     * The username cannot have any spaces
      */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = "[\\s]";
 
     public final String value;
 
@@ -33,7 +32,7 @@ public class Address {
     /**
      * Returns true if a given string is a valid email.
      */
-    public static boolean isValidAddress(String test) {
+    public static boolean isValidTelegram(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
