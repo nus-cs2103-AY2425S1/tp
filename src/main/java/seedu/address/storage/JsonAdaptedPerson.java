ccf2c7362ba25a21754c31df74b5baefc63cc04f
hj235 <hongjing235@gmail.com>
2024-10-10 21:30:52 +0800

    Fix import orders

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -11,8 +11,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.commons.Address;
-import seedu.address.model.person.Email;
 import seedu.address.model.commons.Name;
+import seedu.address.model.person.Email;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
