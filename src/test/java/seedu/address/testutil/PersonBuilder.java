2e386d21f2ba07b8cbfd20251202ce50fe481b81
DESKTOP-E0FPQPJ\ServiceUnit1 <e1186312@u.nus.edu>
2024-10-10 09:18:19 +0800

    Slight test fixes

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -90,7 +90,7 @@ public class PersonBuilder {
     }
 
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, tags,"");
     }
 
 }
