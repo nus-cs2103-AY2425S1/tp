a3d8e2bebd5d0a8b8d0f28a38539a13322104733
C5hives <e1122945@u.nus.edu>
2024-10-10 23:04:28 +0800

    Add new prefix for date

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_DESCRIPTION = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_DATE = new Prefix("d/");
 
 }
