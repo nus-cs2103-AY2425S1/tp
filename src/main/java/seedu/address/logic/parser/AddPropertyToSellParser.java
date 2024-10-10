b8374125b94b1e1176736d21ae8f26d167735f00
OrangeCatLoves <e1121707@u.nus.edu>
2024-10-10 22:52:17 +0800

    Updated some files to pass checkstyle

--- src/main/java/seedu/address/logic/parser/AddPropertyToSellParser.java
+++ src/main/java/seedu/address/logic/parser/AddPropertyToSellParser.java
@@ -6,6 +6,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_POSTAL_CODE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_SELLING_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_UNIT_NUMBER;
+
 import java.util.Set;
 import java.util.stream.Stream;
 
