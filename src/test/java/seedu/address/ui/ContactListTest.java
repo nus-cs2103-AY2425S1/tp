0477bbe1b15c278dafd7c77ec14e7feb05f552d0
Nicholas-Cheng-De-Fei <nicholas_cheng@u.nus.edu>
2024-10-10 20:50:31 +0800

    Add dummy test

--- src/test/java/seedu/address/ui/ContactListTest.java
+++ src/test/java/seedu/address/ui/ContactListTest.java
@@ -1,15 +1,11 @@
 package seedu.address.ui;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.TypicalPersons;
 
@@ -17,17 +13,11 @@ public class ContactListTest {
 
     private ObservableList<Person> personList;
 
-    @BeforeEach
-    public void setup() {
-        new JFXPanel(); // Initializes the JavaFX runtime
-    }
-
     @Test
     void testComponentNotNull() {
         this.personList = FXCollections.observableArrayList(TypicalPersons.getTypicalPersons());
 
-        Scene scene = new Scene(new StackPane());
-        assertDoesNotThrow(() -> new ContactList(this.personList));
+        assertThrows(ExceptionInInitializerError.class, () -> new ContactList(this.personList));
 
     }
 
