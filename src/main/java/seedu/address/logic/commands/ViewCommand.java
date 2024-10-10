fb65431d1c46d29cedccb16a6d0c999f2ce66d70
superb-sushi <caleb.ang.zx@gmail.com>
2024-10-10 09:40:51 +0800

    Add ViewCommand Class

---
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.model.Model;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
+
+
+public class ViewCommand extends Command {
+    public static final String COMMAND_WORD = "view";
+
+    public static final String MESSAGE_SUCCESS = "Contact Opened.";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds one contact whose telegram handle "
+            + "matches the specified telegram handle (case-insensitive) and displays a new page which "
+            + "contains all the contact's information.\n"
+            + "Parameters: TELEGRAM_HANDLE \n"
+            + "Example: " + COMMAND_WORD + "t/TELEGRAM_HANDLE";
+
+    private final NameContainsKeywordsPredicate predicate;
+
+    public ViewCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        // TO ADD FUNCTIONALITY HERE
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
