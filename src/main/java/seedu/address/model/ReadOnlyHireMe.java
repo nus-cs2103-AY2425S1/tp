4499c6bf5b3dae00bfa8b56aae531884f9756708
Xueting <xueting1966@gmail.com>
2024-10-10 22:46:36 +0800

    Update Model with hireMe
    - Update all file path methods and variables  to hireMe in Model
    - add javadoc headings

--- src/main/java/seedu/address/model/ReadOnlyHireMe.java
+++ src/main/java/seedu/address/model/ReadOnlyHireMe.java
@@ -2,6 +2,14 @@ package seedu.address.model;
 
 import javafx.beans.Observable;
 
+/**
+ * Unmodifiable view of hire me app
+ */
 public interface ReadOnlyHireMe {
+
+    /**
+     * Returns an unmodifiable view of the company list.
+     * This list will not contain any duplicate companies.
+     */
     //ObservableList<InternshipApplication> getInternshipList();
 }
--- src/main/java/seedu/address/storage/HireMeStorage.java
+++ src/main/java/seedu/address/storage/HireMeStorage.java
@@ -8,18 +8,39 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 
+/**
+ * Represents a storage for HireMe
+ */
 public interface HireMeStorage {
     /**
      * Returns the file path of the data file.
      */
     Path getHireMeFilePath();
 
+
+    /**
+     * Returns HireMe data as a {@link ReadOnlyHireMe}.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @throws DataLoadingException if loading the data from storage failed.
+     */
     Optional<ReadOnlyHireMe> readHireMe() throws DataLoadingException;
 
+    /**
+     * @see #getHireMeFilePath()
+     */
     Optional<ReadOnlyHireMe> readHireMe(Path filePath) throws DataLoadingException;
 
+    /**
+     * Saves the given {@link ReadOnlyHireMe} to the storage.
+     * @param hireMe cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
     void saveHireMe(ReadOnlyHireMe hireMe) throws IOException;
 
+    /**
+     * @see #saveHireMe(ReadOnlyHireMe)
+     */
     void saveHireMe(ReadOnlyHireMe hireMe, Path filePath) throws IOException;
 
 }
