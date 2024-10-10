a2258d4694bbf6583d035e874b3a56afe664dbe4
yizhong187 <yizhong187@gmail.com>
2024-10-10 14:36:02 +0800

    Add missing file

--- src/main/java/seedu/ddd/model/person/exceptions/DuplicateContactException.java
+++ src/main/java/seedu/ddd/model/person/exceptions/DuplicateContactException.java
@@ -1,2 +1,11 @@
-package seedu.ddd.model.person.exceptions;public class DuplicateContactException {
+package seedu.ddd.model.person.exceptions;
+
+/**
+ * Signals that the operation will result in duplicate Contacts
+ * (Contacts are considered duplicates if they have the same identity).
+ */
+public class DuplicateContactException extends RuntimeException {
+    public DuplicateContactException() {
+        super("Operation would result in duplicate contacts");
+    }
 }
