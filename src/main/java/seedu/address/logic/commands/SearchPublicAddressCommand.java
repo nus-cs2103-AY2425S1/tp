a35a95e5710a2003f2c5b81543087d7b89be2b0c
travisim <64481469+travisim@users.noreply.github.com>
2024-10-10 23:47:11 +0800

    Fix style issues

--- src/main/java/seedu/address/logic/commands/SearchPublicAddressCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchPublicAddressCommand.java
@@ -12,11 +12,12 @@ public class SearchPublicAddressCommand extends Command {
 
     public static final String COMMAND_WORD = "searchPublicAddress";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Searches for a public address and returns the user network and tag associated with it"
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Searches for a public address and returns the user, network and tag"
             + "throughout all the public addresses of all networks in the address book.\n"
             + "Parameters: PUBLIC_ADDRESS (must be a string) "
             + PREFIX_PUBLIC_ADDRESS + "[PUBLIC_ADDRESS]\n"
-            + "Example: " + COMMAND_WORD +" "+ PREFIX_PUBLIC_ADDRESS
+            + "Example: " + COMMAND_WORD + " " + PREFIX_PUBLIC_ADDRESS
             + "0x28f91d6e72eaf4372892e6c6e45dc41b574163e9fcdf94f4997958b46d772fa2";
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "SearchPublicAddress command not implemented yet";
