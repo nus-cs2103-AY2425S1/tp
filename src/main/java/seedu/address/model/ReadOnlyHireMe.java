fb344c7a3113cba42b8535ffa6d9282fee00e742
Xueting <xueting1966@gmail.com>
2024-10-10 22:53:37 +0800

    Fix stylecheck error
    - Remove unecessary imports in ReadOnlyHireMe
    - Correct order of imports in HireMeStorage

--- src/main/java/seedu/address/model/ReadOnlyHireMe.java
+++ src/main/java/seedu/address/model/ReadOnlyHireMe.java
@@ -1,15 +1,9 @@
 package seedu.address.model;
 
-import javafx.beans.Observable;
-
 /**
  * Unmodifiable view of hire me app
  */
 public interface ReadOnlyHireMe {
 
-    /**
-     * Returns an unmodifiable view of the company list.
-     * This list will not contain any duplicate companies.
-     */
     //ObservableList<InternshipApplication> getInternshipList();
 }
--- src/main/java/seedu/address/storage/HireMeStorage.java
+++ src/main/java/seedu/address/storage/HireMeStorage.java
@@ -1,12 +1,11 @@
 package seedu.address.storage;
 
-
-import seedu.address.commons.exceptions.DataLoadingException;
-import seedu.address.model.ReadOnlyHireMe;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.model.ReadOnlyHireMe;
 
 /**
  * Represents a storage for HireMe
