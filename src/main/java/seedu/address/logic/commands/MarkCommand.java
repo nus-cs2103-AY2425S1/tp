98b96201937f741239b79c02774209a1fad06e2e
CliffKoes <cliffkoes@gmail.com>
2024-10-10 12:59:04 +0800

    Add dummy UI to display tutorials attended

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -1,8 +1,14 @@
 package seedu.address.logic.commands;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Tutorial;
 
 /**
@@ -23,6 +29,9 @@ public class MarkCommand extends Command {
 
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Tutorial: %2$s";
 
+    public static final String MESSAGE_ADD_MARK_SUCCESS = "Marked tutorial for Person: %1$s";
+    public static final String MESSAGE_DELETE_MARK_SUCCESS = "Removed tutorial from Person: %1$s";
+
     private final Index index;
     private final Tutorial tutorial;
 
@@ -34,9 +43,41 @@ public class MarkCommand extends Command {
         this.index = index;
         this.tutorial = tutorial;
     }
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), tutorial));
+        List<Person> currDisplayedList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= currDisplayedList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = currDisplayedList.get(index.getZeroBased());
+        Set<Tutorial> newTutorials = new HashSet<>(personToEdit.getTutorials());
+        newTutorials.add(tutorial);
+        Person editedPerson = new Person(
+                personToEdit.getName(),
+                personToEdit.getPhone(),
+                personToEdit.getEmail(),
+                personToEdit.getAddress(),
+                personToEdit.getTags(),
+                newTutorials
+        );
+
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(generateSuccessMessage(editedPerson));
+    }
+
+    /**
+     * Generates a command execution success message based on whether
+     * the remark is added to or removed from
+     * {@code personToEdit}.
+     */
+    private String generateSuccessMessage(Person personToEdit) {
+        String message = !tutorial.tutorial.isEmpty() ? MESSAGE_ADD_MARK_SUCCESS : MESSAGE_DELETE_MARK_SUCCESS;
+        return String.format(message, Messages.format(personToEdit));
     }
 
     @Override
