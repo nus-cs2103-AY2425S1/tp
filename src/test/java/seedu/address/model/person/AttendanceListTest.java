97817f0f78942cb43ee2652e48b7cb1629784bc5
izruff <rafaelky.dev@gmail.com>
2024-10-10 23:13:47 +0800

    Add AttendanceList class

---
+++ src/test/java/seedu/address/model/person/AttendanceListTest.java
@@ -0,0 +1,28 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class AttendanceListTest {
+
+    @Test
+    public void addAttendance_nullAttendance_throwsNullPointerException() {
+        AttendanceList attendanceList = new AttendanceList();
+        assertThrows(NullPointerException.class, () -> attendanceList.addAttendance(null));
+    }
+
+    @Test
+    public void addAttendance_duplicateAttendance_throwsDuplicateAttendanceException() {
+        // TODO: Adding an attendance with the same datetime should throw DuplicateAttendanceException
+    }
+
+    @Test
+    public void removeAttendance_indexOutOfBounds_throwsAttendanceNotFoundException() {
+        // TODO: Removing an index greater than the list size or less than 0 should throw AttendanceNotFoundException
+    }
+}
