fb9bf02eb357299155659decf913ec71d0495594
Bryan <bryanchenyu616@gmail.com>
2024-10-10 23:42:26 +0800

    Remove whitespace

--- src/main/java/keycontacts/model/lesson/MakeupLesson.java
+++ src/main/java/keycontacts/model/lesson/MakeupLesson.java
@@ -1,7 +1,7 @@
 package keycontacts.model.lesson;
 
 public class MakeupLesson extends RegularLesson {
-    
+
     public MakeupLesson(Day lessonDay, Time startTime, Time endTime) {
         super(lessonDay, startTime, endTime);
     }
