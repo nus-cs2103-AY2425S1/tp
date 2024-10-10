3c074b02e082f2256a67a8da1a4cb0ce83d19eee
xqtann <xqxinquan@gmail.com>
2024-10-10 18:47:40 +0800

    Add javadoc to fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddBuyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddBuyCommandParser.java
@@ -3,6 +3,9 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/**
+ * Parses input arguments and creates a new AddBuyCommand object
+ */
 public class AddBuyCommandParser implements Parser<Command> {
     /**
      * Parses the given {@code String} of arguments in the context of the AddBuyCommand
