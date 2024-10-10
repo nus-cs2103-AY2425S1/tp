8e8ad527b4debdb2c84ce6772e0faacbcc777902
S-K-Y-Light <s84915656@gmail.com>
2024-10-10 21:33:34 +0800

    Add Courses to GUI
    PersonCard will display Courses Student is taking

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import seedu.address.model.course.Course;
 import seedu.address.model.person.Person;
 
 /**
@@ -40,6 +41,8 @@ public class PersonCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
+    @FXML
+    private FlowPane courses;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -55,5 +58,8 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        person.getCourses().stream()
+                .sorted(Comparator.comparing(course -> course.courseCode))
+                .forEach(course -> courses.getChildren().add(new Label(course.courseCode)));
     }
 }
