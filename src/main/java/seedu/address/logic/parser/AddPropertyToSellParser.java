26cbfd270d7de029042365001e1d23c875c64539
OrangeCatLoves <e1121707@u.nus.edu>
2024-10-10 22:42:08 +0800

    Updated some files to pass checkstyle

--- src/main/java/seedu/address/logic/parser/AddPropertyToSellParser.java
+++ src/main/java/seedu/address/logic/parser/AddPropertyToSellParser.java
@@ -1,11 +1,17 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.*;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
 import seedu.address.logic.commands.AddPropertyToSellCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Apartment;
 import seedu.address.model.person.Bto;
 import seedu.address.model.person.Condo;
 import seedu.address.model.person.Hdb;
-import seedu.address.model.person.Apartment;
 import seedu.address.model.person.HousingType;
 import seedu.address.model.person.OtherProperty;
 import seedu.address.model.person.PostalCode;
@@ -14,24 +20,30 @@ import seedu.address.model.person.Property;
 import seedu.address.model.person.UnitNumber;
 import seedu.address.model.tag.Tag;
 
-import java.util.Set;
-import java.util.stream.Stream;
-
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
-
+/**
+ * Parses input arguments and creates a new AddPropertyToSellCommand object
+ */
 public class AddPropertyToSellParser implements Parser<AddPropertyToSellCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddPropertyToSellCommand
+     * and returns an AddPropertyToSellCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public AddPropertyToSellCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_HOUSING_TYPE, PREFIX_SELLING_PRICE, PREFIX_POSTAL_CODE, PREFIX_UNIT_NUMBER, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_HOUSING_TYPE, PREFIX_SELLING_PRICE,
+                        PREFIX_POSTAL_CODE, PREFIX_UNIT_NUMBER, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_HOUSING_TYPE, PREFIX_SELLING_PRICE, PREFIX_POSTAL_CODE, PREFIX_UNIT_NUMBER)
+        if (!arePrefixesPresent(argMultimap, PREFIX_HOUSING_TYPE, PREFIX_SELLING_PRICE,
+                PREFIX_POSTAL_CODE, PREFIX_UNIT_NUMBER)
                 || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPropertyToSellCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    AddPropertyToSellCommand.MESSAGE_USAGE));
         }
         // Create a new Property object here and pass it to AddPropertyToSellCommand(Property property);
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_HOUSING_TYPE, PREFIX_SELLING_PRICE, PREFIX_POSTAL_CODE, PREFIX_UNIT_NUMBER);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_HOUSING_TYPE,
+                PREFIX_SELLING_PRICE, PREFIX_POSTAL_CODE, PREFIX_UNIT_NUMBER);
         HousingType housingType = ParserUtil.parseHousingType(argMultimap.getValue(PREFIX_HOUSING_TYPE).get());
         Price sellingPrice = ParserUtil.parseSellingPrice(argMultimap.getValue(PREFIX_SELLING_PRICE).get());
         PostalCode postalCode = ParserUtil.parsePostalCode(argMultimap.getValue(PREFIX_POSTAL_CODE).get());
@@ -51,12 +63,12 @@ public class AddPropertyToSellParser implements Parser<AddPropertyToSellCommand>
     private static Property getSpecificPropertyObject(HousingType housingType, Price sellingPrice,
                                                       PostalCode postalCode, UnitNumber unitNumber, Set<Tag> tagList) {
         return switch (housingType) {
-            case CONDO -> new Condo(postalCode, unitNumber, sellingPrice, tagList);
-            case HDB -> new Hdb(postalCode, unitNumber, sellingPrice, tagList);
-            case APARTMENT -> new Apartment(postalCode, unitNumber, sellingPrice, tagList);
-            case BTO -> new Bto(postalCode, unitNumber, sellingPrice, tagList);
-            case OTHERS -> new OtherProperty(postalCode, unitNumber, sellingPrice, tagList);
-            default -> null;
+        case CONDO -> new Condo(postalCode, unitNumber, sellingPrice, tagList);
+        case HDB -> new Hdb(postalCode, unitNumber, sellingPrice, tagList);
+        case APARTMENT -> new Apartment(postalCode, unitNumber, sellingPrice, tagList);
+        case BTO -> new Bto(postalCode, unitNumber, sellingPrice, tagList);
+        case OTHERS -> new OtherProperty(postalCode, unitNumber, sellingPrice, tagList);
+        default -> null;
         };
     }
 }
--- src/main/java/seedu/address/model/person/HousingType.java
+++ src/main/java/seedu/address/model/person/HousingType.java
@@ -1,21 +1,36 @@
 package seedu.address.model.person;
 
+/**
+ * Represents the type of housing that a person resides in.
+ */
 public enum HousingType {
     CONDO, HDB, APARTMENT, BTO, OTHERS, INVALID_HOUSING_TYPE;
 
+    /**
+     * Checks if the given housing type is valid.
+     *
+     * @param housingType The housing type to be checked.
+     * @return True if the housing type is valid, false otherwise.
+     */
     public static boolean isValidHousingType(String housingType) {
         return housingType.equals("a") || housingType.equals("c") || housingType.equals("b")
                 || housingType.equals("h") || housingType.equals("o");
     }
 
+    /**
+     * Returns the HousingType enum corresponding to the given housing type.
+     *
+     * @param housingType The housing type to be converted.
+     * @return The HousingType enum corresponding to the given housing type.
+     */
     public static HousingType getHousingType(String housingType) {
         return switch (housingType) {
-            case "a" -> APARTMENT;
-            case "c" -> CONDO;
-            case "b" -> BTO;
-            case "h" -> HDB;
-            case "o" -> OTHERS;
-            default -> INVALID_HOUSING_TYPE;
+        case "a" -> APARTMENT;
+        case "c" -> CONDO;
+        case "b" -> BTO;
+        case "h" -> HDB;
+        case "o" -> OTHERS;
+        default -> INVALID_HOUSING_TYPE;
         };
     }
 }
