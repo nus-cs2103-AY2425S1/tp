adce82d1d29f3739f3d13a18c54a2789f9172aa5
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-10-10 22:52:05 +0800

    Create functionality for PaidCommandParser class

--- src/main/java/seedu/address/logic/parser/PaidCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PaidCommandParser.java
@@ -1,3 +1,33 @@
 package seedu.address.logic.parser;
-public class PaidCommandParser implemets Parser<PaidCommand> {
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.PaidCommand;
+
+import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+
+/**
+ * Parses input arguments and creates a new PaidCommand object
+ */
+public class PaidCommandParser implements Parser<PaidCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the PaidCommand
+     * and returns a PaidCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public PaidCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+            editPersonDescriptor.setHasPaid(true);
+            return new PaidCommand(index, editPersonDescriptor);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, PaidCommand.MESSAGE_USAGE), pe);
+        }
+    }
 }
