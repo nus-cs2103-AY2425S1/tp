7618561f6d5e98f77c455cc7b82ec77d3922c3e3
jolwnn <zhuolinwee@gmail.com>
2024-10-10 23:17:37 +0800

    Add tests for command and parser classes for delete property

---
+++ src/test/java/seedu/address/logic/parser/DeletePropertyCommandParserTest.java
@@ -0,0 +1,74 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_POSTALCODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_UNITNUMBER;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DeletePropertyCommand;
+import seedu.address.model.property.PostalCode;
+import seedu.address.model.property.Unit;
+
+public class DeletePropertyCommandParserTest {
+
+    private DeletePropertyCommandParser parser = new DeletePropertyCommandParser();
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+        // Test for valid postal code and unit number
+        String userInput = " " + PREFIX_POSTALCODE + "118420 " + PREFIX_UNITNUMBER + "08-08";
+        PostalCode validPostalCode = new PostalCode("118420");
+        Unit validUnitNumber = new Unit("08-08");
+        DeletePropertyCommand expectedCommand = new DeletePropertyCommand(validPostalCode, validUnitNumber);
+
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_missingPostalCode_failure() {
+        // Test for missing postal code
+        String userInput = " " + PREFIX_UNITNUMBER + "08-08";
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePropertyCommand.MESSAGE_USAGE);
+
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_missingUnitNumber_failure() {
+        // Test for missing unit number
+        String userInput = " " + PREFIX_POSTALCODE + "118420";
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePropertyCommand.MESSAGE_USAGE);
+
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_invalidPostalCodeFormat_failure() {
+        // Test for invalid postal code format
+        String userInput = " " + PREFIX_POSTALCODE + "abc123" + " " + PREFIX_UNITNUMBER + "08-08";
+        String expectedMessage = PostalCode.MESSAGE_CONSTRAINTS;
+
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_invalidUnitNumberFormat_failure() {
+        // Test for invalid unit number format
+        String userInput = " " + PREFIX_POSTALCODE + "118420" + " " + PREFIX_UNITNUMBER + "808";
+        String expectedMessage = Unit.MESSAGE_CONSTRAINTS;
+
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_extraFields_failure() {
+        // Test with extraneous information in the command
+        String userInput = "extra " + PREFIX_POSTALCODE + "118420 " + PREFIX_UNITNUMBER + "08-08";
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeletePropertyCommand.MESSAGE_USAGE);
+
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+}
