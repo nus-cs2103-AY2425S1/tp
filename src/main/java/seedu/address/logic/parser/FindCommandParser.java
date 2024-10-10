f76d03396c92339b98d082d98dafe016d4c05406
hooyushing <94732346+hooyushing@users.noreply.github.com>
2024-10-10 11:11:32 +0800

    Update comment

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -16,7 +16,9 @@ public class FindCommandParser implements Parser<FindCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns a FindCommand object for execution.
+     * and returns a FindCommand object for execution, n/ input finds for name, c/ input finds
+     * for classId
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
