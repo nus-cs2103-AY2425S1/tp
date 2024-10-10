6899acc1b5cba53d5eb7e9748f44cadb7ba9ff60
Nigeltzy <nigel.teo.zy@gmail.com>
2024-10-10 17:31:30 +0800

    Update Doctor.java conflicts

--- src/main/java/seedu/address/model/person/Doctor.java
+++ src/main/java/seedu/address/model/person/Doctor.java
@@ -1,5 +1,7 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
+
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.TreeMap;
@@ -7,57 +9,93 @@ import java.util.TreeMap;
 import seedu.address.model.tag.Tag;
 
 /**
- * Represents a Doctor in the address book.
- * A Doctor is a type of Person with a schedule that stores appointment histories.
+ * Encapsulates the code required to represent a target user of the application.
  */
-public class Doctor extends Person {
-    private static int currentId;
-    private final int id;
-    private TreeMap<LocalDateTime, History> schedule; // TreeMap to store the schedule
-
+public class Doctor extends Person{
     /**
-     * Constructs a Doctor object.
+     * Placeholder Appointment class while waiting for
+     * Appointment implementation PR.
+     */
+    public class History {
+        private TreeMap<LocalDateTime, Pair<Id, String>> db;
+        private final Id id;
+
+        public History(Id id) {
+            this.id = id;
+        }
+
+        public void add(LocalDateTime time, Pair<Id, String> description) {
+            // TODO: relevant extra code like conditional checks, dupe checking
+            db.put(time, description);
+            // TODO: need to do something with doctor schedule also
+        }
+
+        public void update(LocalDateTime time, Pair<Id, String> description) {
+            // TODO, separate functionality by Patient or Doctor (probably need extra param)
+        }
+
+        public String getAll(Id id) {
+            // TODO, separate functionality by Patient or Doctor
+            return "";
+        }
+
+        public void get(LocalDateTime dateTime) {
+        }
+    }
+
+    private History history;
+    private Id id;
+    /**
+     * Every field must be present and not null.
      *
-     * @param name Name of the doctor.
-     * @param phone Phone number of the doctor.
-     * @param email Email address of the doctor.
-     * @param address Residential address of the doctor.
-     * @param remark Additional remarks or notes.
-     * @param tags Set of tags associated with the doctor.
+     * @param name
+     * @param phone
+     * @param email
+     * @param address
+     * @param remark
+     * @param tags
      */
     public Doctor(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
         super(name, phone, email, address, remark, tags);
-        this.id = currentId;;
-        currentId++;
-        this.schedule = new TreeMap<>(); // Initialize the TreeMap
+        id = new Id(this.getClass());
+        history = new Doctor.History(id);
     }
 
     /**
      * Adds an entry to the doctor's schedule.
      *
      * @param dateTime Date and time of the appointment or event.
-     * @param history The appointment or event history to be added to the schedule.
+     * TODO
      */
-    public void addScheduleEntry(LocalDateTime dateTime, History history) {
-        schedule.put(dateTime, history);
+    public void addAppointment(LocalDateTime dateTime, Pair<Id, String> description) {
+        requireNonNull(dateTime);
+        requireNonNull(description);
+        history.add(dateTime, description);
+    }
+
+    // Method to update appointment
+    public void updateAppointment(LocalDateTime time, Pair<Id, String> description) {
+        requireNonNull(time);
+        requireNonNull(description);
+        history.update(time, description);
     }
 
     /**
      * Retrieves the full schedule of the doctor.
      *
-     * @return A TreeMap containing the schedule, with the LocalDateTime as the key and History as the value.
+     * @return A TODO representing the TreeMap containing the doctor's schedule,
+     * with the LocalDateTime as the key and History as the value.
      */
-    public TreeMap<LocalDateTime, History> getSchedule() {
-        return schedule;
+    public String getAllAppointments(Id id) {
+        return history.getAll(id);
     }
-
     /**
-     * Retrieves a specific schedule entry based on the date and time.
+     * Retrieves a specific schedule entry based on the date and time. TODO.
      *
      * @param dateTime The date and time of the desired schedule entry.
-     * @return The History object associated with the specified date and time, or null if not found.
+     * @return Pair<Id, String> The History object associated with the specified date and time, or null if not found.
      */
-    public History getScheduleEntry(LocalDateTime dateTime) {
-        return schedule.get(dateTime);
+    public void getScheduleEntry(LocalDateTime dateTime) {
+        // return history.get(dateTime);
     }
 }
