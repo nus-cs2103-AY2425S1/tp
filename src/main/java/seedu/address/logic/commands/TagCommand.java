08be9ed4bd28047e36eed61cca7c266d63018d52
hyxnnii <E1156838@u.nus.edu>
2024-10-10 16:00:18 +0800

    Adhere to coding standards

--- src/main/java/seedu/address/logic/commands/TagCommand.java
+++ src/main/java/seedu/address/logic/commands/TagCommand.java
@@ -1,18 +1,23 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.List;
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 
-import java.util.List;
-import java.util.Set;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
-
+/**
+ * Adds tags to a person.
+ */
 public class TagCommand extends Command {
 
     public static final String COMMAND_WORD = "tag";
@@ -25,10 +30,17 @@ public class TagCommand extends Command {
     public static final String MESSAGE_SUCCESS = "New tag added";
     private final Index targetIndex;
     private final Set<Tag> tagList;
+
+    /**
+     * Creates a TagCommand to add the specified {@code Set<Tag>}
+     * to the person of specified {@code Index}
+     */
     public TagCommand(Index targetIndex, Set<Tag> tagList) {
         this.targetIndex = targetIndex;
         this.tagList = tagList;
     }
+
+    @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
@@ -42,4 +54,26 @@ public class TagCommand extends Command {
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(person)));
     }
 
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof TagCommand)) {
+            return false;
+        }
+
+        TagCommand otherTagCommand = (TagCommand) other;
+        return tagList.equals(otherTagCommand.tagList)
+                && targetIndex.equals(otherTagCommand.targetIndex);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("tags", tagList)
+                .toString();
+    }
 }
--- src/main/java/seedu/address/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -1,21 +1,28 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.tag.Tag;
 
-import java.util.List;
-import java.util.Set;
-
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
-
+/**
+ * Parses input arguments and creates a new TagCommand object
+ */
 public class TagCommandParser implements Parser<TagCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the TagCommand
+     * and returns a TagCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
     public TagCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
