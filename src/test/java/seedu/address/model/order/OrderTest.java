267629f5328c0e635f7d8a817a0bbcf8e235a57c
rimgik <karon503.cpp@gmail.com>
2024-10-10 17:32:40 +0800

    Remove unused import

--- src/test/java/seedu/address/model/order/OrderTest.java
+++ src/test/java/seedu/address/model/order/OrderTest.java
@@ -1,7 +1,6 @@
 package seedu.address.model.order;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.HashMap;
