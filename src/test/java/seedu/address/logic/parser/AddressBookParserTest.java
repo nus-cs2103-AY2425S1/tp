bf0ea8999540c8da28cb6006a497008997e7b6fd
OrangeCatLoves <e1121707@u.nus.edu>
2024-10-10 23:07:22 +0800

    Changed some files to pass checkstyle

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -18,18 +18,18 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Condo;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Price;
-import seedu.address.model.person.Property;
 import seedu.address.model.person.PostalCode;
+import seedu.address.model.person.Property;
 import seedu.address.model.person.UnitNumber;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
@@ -103,7 +103,8 @@ public class AddressBookParserTest {
         Set<Tag> tagList = ParserUtil.parseTags(Arrays.asList("Extremely spacious", "Near MRT"));
 
         Property property = new Condo(postalCode, unitNumber, sellingPrice, tagList);
-        //AddPropertyToSellCommand command = (AddPropertyToSellCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
+        //AddPropertyToSellCommand command = (AddPropertyToSellCommand)
+        // parser.parseCommand(PersonUtil.getAddCommand(person));
         //assertEquals(new AddPropertyToSellCommand(), command);
         assertEquals(property, property);
     }
