5b08c49fb75300c7fa82770efa5deedced678b44
siriousguy <raylisr@gmail.com>
2024-10-10 23:47:36 +0800

    Fix formatting

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -5,10 +5,10 @@ import java.util.Set;
 
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Job;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Job;
 import seedu.address.model.person.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
