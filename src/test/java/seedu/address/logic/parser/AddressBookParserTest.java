da72637c72c450f6658bee7b857d760b845b8722
OrangeCatLoves <e1121707@u.nus.edu>
2024-10-10 23:19:58 +0800

    Changes made to pass checkstyle

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -9,7 +9,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
+//import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -24,14 +24,14 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Condo;
+//import seedu.address.model.person.Condo;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.PostalCode;
+/*import seedu.address.model.person.PostalCode;
 import seedu.address.model.person.Price;
 import seedu.address.model.person.Property;
 import seedu.address.model.person.UnitNumber;
-import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.Tag;*/
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.PersonUtil;
