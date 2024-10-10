344917f86514e06a12abc8c112bfde23673c4f0d
kimberlytmq <104084634+kimberlytmq@users.noreply.github.com>
2024-10-11 01:23:30 +0800

    Resolve conflict issues with address command removal

--- src/main/java/seedu/address/logic/commands/TeleHandleCommand.java
+++ src/main/java/seedu/address/logic/commands/TeleHandleCommand.java
@@ -58,7 +58,7 @@ public class TeleHandleCommand extends Command{
         Person personToEdit = lastShownList.get(index.getZeroBased());
         Person editedPerson = new Person(
                 personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
-                personToEdit.getAddress(), telegramHandle, personToEdit.getTags());
+                telegramHandle, personToEdit.getTags());
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
