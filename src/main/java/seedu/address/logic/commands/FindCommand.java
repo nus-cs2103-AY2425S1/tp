c9486052dbfc5111b67bf42a0e8ca04126705248
daron <daronohchundai@gmail.com>
2024-10-10 22:29:35 +0800

    Bug fix minor typo

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -18,7 +18,7 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all patients whose field contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: FIELD/ KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " n/ C8 D7";
+            + "Example: " + COMMAND_WORD + " n/ Amy Dave";
 
     private final FieldContainsKeywordsPredicate predicate;
 
