5ead03fb497762071158816d6e9d7cbb52c9e8e0
Sparsh Kumar <e1121161@u.nus.edu>
2024-10-10 19:20:44 +0800

    Update Parser test for alt delete command

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -54,6 +54,13 @@ public class AddressBookParserTest {
     }
 
     @Test
+    public void parseCommand_delete_alt() throws Exception {
+        DeleteCommand command = (DeleteCommand) parser.parseCommand(
+                DeleteCommand.ALT_COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
+    }
+
+    @Test
     public void parseCommand_edit() throws Exception {
         Person person = new PersonBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
