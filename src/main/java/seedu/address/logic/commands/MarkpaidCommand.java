359bab14ec78984b62279bf0b151539acdbf8839
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-10-10 14:02:26 +0800

    Remove prefix for markpaid command

--- src/main/java/seedu/address/logic/commands/MarkpaidCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkpaidCommand.java
@@ -1,18 +1,17 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PAID;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 public class MarkpaidCommand extends Command {
     public static final String COMMAND_WORD = "markpaid";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
+    /*public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
             + "by the index number used in the last person listing. "
             + "Existing remark will be overwritten by the input.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + PREFIX_PAID + "[REMARK]\n"
             + "Example: " + COMMAND_WORD + " 1 "
-            + PREFIX_PAID + "Likes to swim.";
+            + PREFIX_PAID + "Likes to swim.";*/
 
     public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
