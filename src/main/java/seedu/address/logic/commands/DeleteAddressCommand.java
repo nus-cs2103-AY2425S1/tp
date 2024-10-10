a2f3dfc54807506941409eda0686248d5db64b00
TheRareFox <zhang.yuxiang290902@gmail.com>
2024-10-10 19:56:16 +0800

    Reformat code for consistency

--- src/main/java/seedu/address/logic/commands/DeleteAddressCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteAddressCommand.java
@@ -1,10 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-
-import java.util.List;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
@@ -12,6 +7,10 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.Prefix;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Deletes the person's public address identified by the index number used in the displayed person list and their crypto network.
