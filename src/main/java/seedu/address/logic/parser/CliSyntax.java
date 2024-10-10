f817962f15ccb633ba23a364913804ebae5f1eff
rachelyeohm <rachelyeohm@gmail.com>
2024-10-10 23:53:55 +0800

    Fix CheckStyle

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,11 +6,11 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("--name ");
-    public static final Prefix PREFIX_PHONE = new Prefix("--phone ");
-    public static final Prefix PREFIX_EMAIL = new Prefix("--email ");
-    public static final Prefix PREFIX_LOCATION = new Prefix("--location ");
-    public static final Prefix PREFIX_TAG = new Prefix("--tag ");
-    public static final Prefix PREFIX_REMARK = new Prefix("--remark ");
+    public static final Prefix PREFIX_NAME = new Prefix("n/ ");
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_LOCATION = new Prefix("l/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_REMARK = new Prefix("r/");
 
 }
