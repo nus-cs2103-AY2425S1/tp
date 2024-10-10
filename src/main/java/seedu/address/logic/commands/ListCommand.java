794570713c31507fc9560fc0f61d9c3c9392c8db
Apollo <shixintan01@gmail.com>
2024-10-10 22:27:58 +0800

    Add rudimentary support for list command
    Add ListCommandParser to parse arguments and create the correct ListCommand associated with the correct key
    Update ListCommand to store the key and act accordingly
    Update AddressBookParser to parse the new version of the ListCommand appropriately
    Update CliSyntax to account for new command prefix KEY

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -10,15 +10,67 @@ import seedu.address.model.Model;
  */
 public class ListCommand extends Command {
 
+    public static enum Key {
+        BUYERS,
+        SELLERS,
+        CLIENTS,
+        PROPERTIES;
+    }
+
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all persons";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all existing properties, sellers, buyers, or clients in the database.\n"
+            + "Command format: " + COMMAND_WORD + " k/KEY\n"
+            + "Example commands:\n"
+            + "1. List all the buyers: " + COMMAND_WORD + " k/buyers\n"
+            + "2. List all the properties: " + COMMAND_WORD + " k/properties\n"
+            + "\n"
+            + "Parameter considerations:\n"
+            + "The key must be one of the following: \"buyers\", \"sellers\", \"clients\", or \"properties\".\n"
+            + "Only these four types of records are stored in the database.\n";
 
+    //TODO modify this to actually show the correct thing
+    public static final String MESSAGE_SUCCESS = "Listed all %1$s";
+
+    private final Key key;
+
+    // TODO:
+    // 1. Store the String key to keep track of the thing (actually can use enum as well honestly)
+    // 2. So your ListCommandParser will take the arguments and produce
+    // 3. Have a boolean isInvalidKey or something as true, because if that's the case, then return nothing
+    // 4. Otherwise, all you need to do it to fetch the correct String and then print the string out as pernormal
+    //  (just call their toString honestly)
+    // 2. Create a bunch of helper methods that aim to resolve this issue
+    // 3. If there's an error, that is if the key doesn't exist or something, then gg throw error
+    // 4. Otherwise, parse as per usual!
+
+    public ListCommand(Key key) {
+        this.key = key;
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        switch (key) {
+            case BUYERS:
+                // Logic to list buyers
+                System.out.println("Listing all buyers");
+                break;
+            case SELLERS:
+                // Logic to list sellers
+                System.out.println("Listing all sellers");
+                break;
+            case CLIENTS:
+                // Logic to list clients
+                System.out.println("Listing all clients");
+                break;
+            case PROPERTIES:
+                // Logic to list properties
+                System.out.println("Listing all properties");
+                break;
+            default:
+                throw new AssertionError("Unexpected key: " + key);
+        }
+        return new CommandResult(String.format(ListCommand.MESSAGE_SUCCESS, this.key.toString().toLowerCase()));
     }
 }
