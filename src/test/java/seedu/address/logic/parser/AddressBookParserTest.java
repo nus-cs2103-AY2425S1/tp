3ba9b8ccf75ace25be1b42d8809f62d1f6c2a517
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-10-11 22:23:20 +0800

    Fix test error

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.logic.commands.CommandTestUtil.PERSON_ENTITY_STRING;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.logic.parser.ParserUtil.PERSON_ENTITY_STRING;
 
 
 import java.util.Arrays;
