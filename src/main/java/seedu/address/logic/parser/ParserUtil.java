f219925d21b5c1c506a56369c3ec4eccee20efed
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-10-11 10:49:20 +0800

    Implement parseRoles for multiple strings of roles

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -163,4 +163,19 @@ public class ParserUtil {
             throw new ParseException(RoleHandler.MESSAGE_CONSTRAINTS);
         }
     }
+
+    /**
+     * Parses {@code Collection<String> roles} into a {@code Set<Role>}.
+     * @param roles Collection of roles to be parsed
+     * @return Set of roles to be added
+     * @throws ParseException if one of the roles is invalid
+     */
+    public static Set<Role> parseRoles(Collection<String> roles) throws ParseException {
+        requireNonNull(roles);
+        final Set<Role> roleSet = new HashSet<>();
+        for (String role : roles) {
+            roleSet.add(parseRole(role));
+        }
+        return roleSet;
+    }
 }
