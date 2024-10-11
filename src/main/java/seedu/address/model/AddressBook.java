7996d22d7a0ce714014a1acab0a7b400bf644130
C5hives <e1122945@u.nus.edu>
2024-10-11 22:02:49 +0800

    Fix deletion of removeVendor method

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -99,6 +99,14 @@ public class AddressBook implements ReadOnlyAddressBook {
         vendors.setVendor(target, editedVendor);
     }
 
+    /**
+     * Removes {@code key} from this {@code AddressBook}.
+     * {@code key} must exist in the address book.
+     */
+    public void removeVendor(Vendor key) {
+        vendors.remove(key);
+    }
+
     //// event-level operations
 
     /**
