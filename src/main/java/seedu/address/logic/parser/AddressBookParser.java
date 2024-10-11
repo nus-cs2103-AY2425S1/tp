edae9453c37e3cdc9facdbd416808a544f87823d
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-10-11 12:03:06 +0800

    Fix conflicts with view command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -17,9 +17,9 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.MarkAttendanceCommand;
 import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.logic.commands.MarkAttendanceCommand;
 
 
 /**
