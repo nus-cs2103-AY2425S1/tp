8de06e469f449b3cd8db4c03bc067b955462dd38
IzN432 <isaac.ng432@gmail.com>
2024-10-11 22:07:15 +0800

    Add pianoPieces to hashCode

--- src/main/java/keycontacts/model/student/Student.java
+++ src/main/java/keycontacts/model/student/Student.java
@@ -191,7 +191,7 @@ public class Student {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags, regularLesson);
+        return Objects.hash(name, phone, email, address, tags, pianoPieces, regularLesson);
     }
 
     @Override
