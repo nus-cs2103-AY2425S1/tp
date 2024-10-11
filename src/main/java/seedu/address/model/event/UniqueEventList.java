da7aa1cd935d6c97d2bc6c32c82ab72962dbf168
C5hives <e1122945@u.nus.edu>
2024-10-11 22:27:41 +0800

    Add overloaded setEvents method

--- src/main/java/seedu/address/model/event/UniqueEventList.java
+++ src/main/java/seedu/address/model/event/UniqueEventList.java
@@ -49,6 +49,11 @@ public class UniqueEventList implements Iterable<Event> {
         }
     }
 
+    public void setEvents(UniqueEventList replacement) {
+        requireNonNull(replacement);
+        internalList.setAll(replacement.internalList);
+    }
+
     /**
      * Replaces the contents of this list with {@code vendors}.
      * {@code vendors} must not contain duplicate vendors.
