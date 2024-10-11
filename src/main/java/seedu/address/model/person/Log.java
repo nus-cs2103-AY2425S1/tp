d2d9e12b55fd4d28201ebcfc646a17ce2c35b18a
jeanyang04 <jeanyang.chen@gmail.com>
2024-10-11 16:23:21 +0800

    Add new line at end of file

--- src/main/java/seedu/address/model/person/Log.java
+++ src/main/java/seedu/address/model/person/Log.java
@@ -1,2 +1,39 @@
-package seedu.address.model.person;public class Log {
+package seedu.address.model.person;
+
+import java.util.Objects;
+
+public class Log {
+    private final String entry;
+
+    public Log(String entry) {
+        this.entry = entry;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Log)) {
+            return false;
+        }
+
+        Log otherLog = (Log) other;
+        return entry.equals(otherLog.entry);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(entry);
+    }
+
+    @Override
+    public String toString() {
+        return entry;
+    }
 }
