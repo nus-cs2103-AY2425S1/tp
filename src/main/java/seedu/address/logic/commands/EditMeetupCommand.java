d7579be7b37a2221c74366c7627a7859ea7f33ad
Solomon0519 <ng.eswen1@gmail.com>
2024-10-11 14:16:11 +0800

    Change edit meetup command class
    Minor adjustments to pass checkstyle tests.

--- src/main/java/seedu/address/logic/commands/EditMeetupCommand.java
+++ src/main/java/seedu/address/logic/commands/EditMeetupCommand.java
@@ -1,20 +1,17 @@
 package seedu.address.logic.commands;
-import seedu.address.commons.core.index.Index;
+
 import seedu.address.commons.util.CollectionUtil;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.meetup.MeetUp;
-import seedu.address.model.person.*;
-import seedu.address.model.tag.Tag;
+import seedu.address.model.person.Person;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEETUP_INFO;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
 
 /**
  * Edits the details of an existing meetup in the address book.
