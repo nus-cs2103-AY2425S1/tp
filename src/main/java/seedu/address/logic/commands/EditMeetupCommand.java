79657e4a6f83c07779ff05f14195aa589f2ecca4
Solomon0519 <ng.eswen1@gmail.com>
2024-10-11 14:09:42 +0800

    Change edit command to edit meet ups
    Edited the prototype command on editing a meetup. Created new edit meet-up descriptor class to replicate the edit person descriptor class.

--- src/main/java/seedu/address/logic/commands/EditMeetupCommand.java
+++ src/main/java/seedu/address/logic/commands/EditMeetupCommand.java
@@ -51,23 +51,7 @@ public class EditMeetupCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Person> lastShownList = model.getFilteredPersonList();
-
-        if (index.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-        }
-
-        Person personToEdit = lastShownList.get(index.getZeroBased());
-        Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
-
-        if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
-            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
-        }
-
-        model.setPerson(personToEdit, editedPerson);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson)));
+        return null;
     }
 
 
