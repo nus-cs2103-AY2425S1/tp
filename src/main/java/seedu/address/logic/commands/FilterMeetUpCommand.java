bbc5b1bb07c21b94ac7d0efcc56797cc6c30aac0
CreditableX <creditablecreates@gmail.com>
2024-10-11 15:31:25 +0800

    add to filter meetup command

--- src/main/java/seedu/address/logic/commands/FilterMeetUpCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterMeetUpCommand.java
@@ -1,10 +1,7 @@
 package seedu.address.logic.commands;
-import seedu.address.model.meetup.MeetUp;
-import seedu.address.model.schedule.Schedule;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
@@ -21,16 +18,23 @@ public class FilterMeetUpCommand extends Command {
             + "Parameters: KEYWORD yyyy-mm-dd\n"
             + "Example: " + COMMAND_WORD + "2022-12-12";
 
+    
     // public static List<MeetUp> getMeetUpList(Schedule schedule) {
     //     for (int i = 0; )
     // }
 
+    private final NameContainsKeywordsPredicate predicate;
+
+    public FilterMeetUpCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
     @Override
-    // public CommandResult execute(Model model) {
-    //     requireNonNull(model);
-    //     model.updateFilteredPersonList(predicate);
-    //     return new CommandResult(
-    //             String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
-    // }
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
 
 }
