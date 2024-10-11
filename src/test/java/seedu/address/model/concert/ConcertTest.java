2623d7e1cec4dbe6749ce0ecf9f129e475d53df8
wesley <wesleywong08@gmail.com>
2024-10-12 01:22:30 +0800

    Add Concert tests

--- src/test/java/seedu/address/model/concert/ConcertTest.java
+++ src/test/java/seedu/address/model/concert/ConcertTest.java
@@ -1,24 +1,56 @@
 package seedu.address.model.concert;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_GLASTONBURY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_GLASTONBURY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_COACHELLA;
+import static seedu.address.testutil.TypicalConcerts.COACHELLA;
+import static seedu.address.testutil.TypicalConcerts.GLASTONBURY;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.commons.Address;
-import seedu.address.model.commons.Name;
+import seedu.address.testutil.ConcertBuilder;
 
 public class ConcertTest {
-    // TODO: extract to typical concerts
-    private final Concert c1 = new Concert(new Name("Concert C1"), new Address("C1"),
-            new ConcertDate("2002-10-10 1000"));
 
     @Test
     public void isSameConcert() {
         // same object -> returns true
-        assertTrue(c1.isSameConcert(c1));
+        assertTrue(COACHELLA.isSameConcert(COACHELLA));
 
         // null -> returns false
-        assertFalse(c1.isSameConcert(null));
+        assertFalse(COACHELLA.isSameConcert(null));
+
+        // same name, all other different -> returns false
+        Concert editedCoachella = new ConcertBuilder(COACHELLA)
+                .withAddress(VALID_ADDRESS_GLASTONBURY).withDate(VALID_DATE_GLASTONBURY).build();
+        assertFalse(COACHELLA.isSameConcert(editedCoachella));
+
+        // name differs in case, all other attributes same -> returns false
+        editedCoachella = new ConcertBuilder().withName(VALID_NAME_COACHELLA.toLowerCase()).build();
+        assertFalse(COACHELLA.isSameConcert(editedCoachella));
+
+        // name has trailing spaces, all other attributes same -> returns false
+        String nameWithTrailingSpaces = VALID_NAME_COACHELLA + " ";
+        editedCoachella = new ConcertBuilder(COACHELLA).withName(nameWithTrailingSpaces).build();
+        assertFalse(COACHELLA.isSameConcert(editedCoachella));
+    }
+
+    @Test
+    public void equals() {
+        // same return true
+        assertTrue(COACHELLA.equals(COACHELLA));
+
+        // different return false
+        assertFalse(GLASTONBURY.equals(COACHELLA));
+    }
+
+    @Test
+    public void toStringMethod() {
+        String expected = Concert.class.getCanonicalName() + "{name=" + COACHELLA.getName()
+                + ", address=" + COACHELLA.getAddress() + ", date=" + COACHELLA.getDate() + "}";
+        assertEquals(COACHELLA.toString(), expected);
     }
 }
