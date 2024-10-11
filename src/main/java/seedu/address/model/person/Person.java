0728fd93e95dbe77e8cdb3f7270209192599b90b
JJtan <95962077+JJtan2002@users.noreply.github.com>
2024-10-12 01:51:51 +0800

    Fix coding quality error

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -104,7 +104,7 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
-                && tags.equals(otherPerson.tags);
+                && tags.equals(otherPerson.tags)
                 && games.equals(otherPerson.games);
 
     }
