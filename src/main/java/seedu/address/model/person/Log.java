134ff75cd5d988862cf28ebebdd9c628789a25f0
jeanyang04 <jeanyang.chen@gmail.com>
2024-10-11 16:27:36 +0800

    Add java docs comments

--- src/main/java/seedu/address/model/person/Log.java
+++ src/main/java/seedu/address/model/person/Log.java
@@ -5,14 +5,26 @@ import java.util.Objects;
 public class Log {
     private final String entry;
 
+    /**
+     * Constructs a {@code Log}.
+     *
+     * @param entry A valid log entry.
+     */
     public Log(String entry) {
         this.entry = entry;
     }
 
+    /**
+     * Returns the log entry.
+     */
     public String getEntry() {
         return entry;
     }
 
+    /**
+     * Returns true if both logs have the same entry.
+     * This defines a weaker notion of equality between two logs.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -27,11 +39,17 @@ public class Log {
         return entry.equals(otherLog.entry);
     }
 
+    /**
+     * Returns the hashcode of the log entry.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(entry);
     }
 
+    /**
+     * Returns the log entry as a string.
+     */
     @Override
     public String toString() {
         return entry;
--- src/main/java/seedu/address/model/person/LogsList.java
+++ src/main/java/seedu/address/model/person/LogsList.java
@@ -7,18 +7,31 @@ import java.util.Objects;
 public class LogsList {
     private final List<Log> logs;
 
+    /**
+     * Constructs a {@code LogsList}.
+     */
     public LogsList() {
         this.logs = new ArrayList<>();
     }
 
+    /**
+     * Adds a log to the list.
+     */
     public void addLog(Log log) {
         logs.add(log);
     }
 
+    /**
+     * Returns the list of logs.
+     */
     public List<Log> getLogs() {
         return new ArrayList<>(logs);
     }
 
+    /**
+     * Returns true if both logs lists have the same logs.
+     * This defines a weaker notion of equality between two logs lists.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -32,11 +45,17 @@ public class LogsList {
         return logs.equals(otherLogsList.logs);
     }
 
+    /**
+     * Returns the hashcode of the logs list.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(logs);
     }
 
+    /**
+     * Returns the logs list as a string.
+     */
     @Override
     public String toString() {
         return logs.toString();
