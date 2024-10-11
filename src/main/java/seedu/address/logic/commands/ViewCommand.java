2072ec7faacf13fd8dc856fca4b5b886fe3440bf
superb-sushi <caleb.ang.zx@gmail.com>
2024-10-11 12:28:57 +0800

    Update Code

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * View all information of specified contact
