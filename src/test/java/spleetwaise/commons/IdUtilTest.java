52d5e23c0a8d1c60a28cca0881758bf58f8033fe
Sean Lim <seanlimdev856@gmail.com>
2024-10-11 14:12:54 +0800

    Add `IdUtil` instantiation for codecov

--- src/test/java/spleetwaise/commons/IdUtilTest.java
+++ src/test/java/spleetwaise/commons/IdUtilTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 
 public class IdUtilTest {
 
+    private IdUtil idUtil = new IdUtil();
+
     @Test
     public void getId_deterministic() {
         IdUtil.setDeterminate(true);
