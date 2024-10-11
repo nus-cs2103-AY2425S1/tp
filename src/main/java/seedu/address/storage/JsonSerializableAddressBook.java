ab9d6bee1894c3b32c56b367c5be968cd4dcaff0
fusin <fusin02@gmail.com>
2024-10-11 11:15:48 +0800

    Remove print statement

--- src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
+++ src/main/java/seedu/address/storage/JsonSerializableAddressBook.java
@@ -45,7 +45,6 @@ class JsonSerializableAddressBook {
     public JsonSerializableAddressBook(ReadOnlyAddressBook source) {
         persons.addAll(source.getPersonList().stream().map(JsonAdaptedPerson::new).collect(Collectors.toList()));
         events.addAll(source.getEventList().stream().map(JsonAdaptedEvent::new).collect(Collectors.toList()));
-        System.out.println(events.size());
     }
 
     /**
