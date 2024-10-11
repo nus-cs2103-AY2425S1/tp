b67846d5249e5772b8291eba9962043c2206b5f6
JJtan <95962077+JJtan2002@users.noreply.github.com>
2024-10-12 01:46:51 +0800

    Add Game field to Person model

---
+++ src/main/java/seedu/address/model/game/Game.java
@@ -0,0 +1,68 @@
+package seedu.address.model.game;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Game in the address book.
+ * Guarantees: immutable; name is valid as declared in {@link #isValidgameName(String)}
+ */
+public class Game {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Games should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String gameName;
+
+    /**
+     * Constructs a {@code Game}.
+     *
+     * @param gameName A valid Game name.
+     */
+    public Game(String gameName) {
+        requireNonNull(gameName);
+        checkArgument(isValidgameName(gameName), MESSAGE_CONSTRAINTS);
+        this.gameName = gameName;
+    }
+
+    /**
+     * Returns true if a given string is a valid Game name.
+     */
+    public static boolean isValidgameName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Game)) {
+            return false;
+        }
+
+        Game otherGame = (Game) other;
+        return gameName.equals(otherGame.gameName);
+    }
+
+    @Override
+    public int hashCode() {
+        return gameName.hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    public String toString() {
+        return '[' + gameName + ']';
+    }
+
+}
