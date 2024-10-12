b62c71668a7bf44eebb89b7aa2dee860b8fbaf40
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-10-12 10:42:04 +0800

    Integrate mark command with payment

--- src/main/java/seedu/address/logic/commands/MarkPaidCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkPaidCommand.java
@@ -44,7 +44,8 @@ public class MarkPaidCommand extends Command {
 
         Person personToMarkPayment = lastShownList.get(targetIndex.getZeroBased());
         Person markedPerson = new Person(personToMarkPayment.getName(), personToMarkPayment.getPhone(),
-                personToMarkPayment.getEmail(), personToMarkPayment.getAddress(), personToMarkPayment.getTags());
+                personToMarkPayment.getEmail(), personToMarkPayment.getAddress(),
+                personToMarkPayment.getPayment(), personToMarkPayment.getTags());
 
         model.setPerson(personToMarkPayment, markedPerson);
         model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
