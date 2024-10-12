c468d0c0322c98a3fd2ed9d3594b0a45f4691aad
Chin Sek Yi <sekyichin@gmail.com>
2024-10-12 15:25:20 +0800

    Fix duplicate contacts error

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -70,8 +70,12 @@ public class Person {
             return true;
         }
 
-        return otherPerson != null
-                && otherPerson.getName().equals(getName());
+        if (otherPerson == null) {
+            return false;
+        }
+
+        return name.equals(otherPerson.name)
+                && (phone.equals(otherPerson.phone) || email.equals(otherPerson.email));
     }
 
     /**
@@ -89,10 +93,8 @@ public class Person {
         }
 
         Person otherPerson = (Person) other;
-        return (name.equals(otherPerson.name)
-                && phone.equals(otherPerson.phone))
-                || (name.equals(otherPerson.name)
-                && email.equals(otherPerson.email));
+        return name.equals(otherPerson.name)
+                && (phone.equals(otherPerson.phone) || email.equals(otherPerson.email));
     }
 
     @Override
