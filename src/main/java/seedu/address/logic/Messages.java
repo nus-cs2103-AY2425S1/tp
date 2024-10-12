08a0030c66010703f9e0069524de71872c23d341
Incogdino <javiertmw@gmail.com>
2024-10-12 13:23:20 +0800

    Update success message for add command
    Success message now includes details of GitHub username that is
    visible to the user.

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -46,6 +46,8 @@ public class Messages {
                 .append(person.getAddress())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
+        builder.append("; Github: ")
+                .append(person.getGithub());
         return builder.toString();
     }
 
