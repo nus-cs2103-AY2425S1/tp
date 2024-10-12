e79585adf91af1938d7db0e3d856b684814a600a
rayray39 <raynerlimfy@gmail.com>
2024-10-12 15:14:22 +0800

    ResultDisplay.java: Add styles to show success, failure, default results

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -2,6 +2,7 @@ package seedu.address.ui;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
@@ -11,8 +12,8 @@ import javafx.scene.layout.Region;
  */
 public class ResultDisplay extends UiPart<Region> {
 
-    public static final String ERROR_STYLE_CLASS = "error_result";
-    public static final String SUCCESS_STYLE_CLASS = "success_result";
+    public static final String ERROR_STYLE_CLASS = "error-result";
+    public static final String SUCCESS_STYLE_CLASS = "success-result";
     private static final String FXML = "ResultDisplay.fxml";
 
     @FXML
@@ -20,21 +21,53 @@ public class ResultDisplay extends UiPart<Region> {
 
     public ResultDisplay() {
         super(FXML);
-    }
-
-    public void setFeedbackToUser(String feedbackToUser) {
-        requireNonNull(feedbackToUser);
-        resultDisplay.setText(feedbackToUser);
+        resultDisplay.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
     }
 
     public void setSuccessFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
         resultDisplay.setText(feedbackToUser);
+        setStyleToIndicateCommandSuccess();
     }
 
     public void setErrorFeedbackToUser(String feedbackToUser) {
         requireNonNull(feedbackToUser);
         resultDisplay.setText(feedbackToUser);
+        setStyleToIndicateCommandFailure();
+    }
+
+    /**
+     * Sets the result text area style to use the default style.
+     */
+    private void setStyleToDefault() {
+        resultDisplay.getStyleClass().remove(ERROR_STYLE_CLASS);
+        resultDisplay.getStyleClass().remove(SUCCESS_STYLE_CLASS);
+    }
+
+    /**
+     * Sets the result text area style to indicate a failed result.
+     */
+    private void setStyleToIndicateCommandFailure() {
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if (styleClass.contains(ERROR_STYLE_CLASS)) {
+            return;
+        }
+
+        styleClass.add(ERROR_STYLE_CLASS);
+    }
+
+    /**
+     * Sets the result text area style to indicate a successful result.
+     */
+    private void setStyleToIndicateCommandSuccess() {
+        ObservableList<String> styleClass = resultDisplay.getStyleClass();
+
+        if (styleClass.contains(SUCCESS_STYLE_CLASS)) {
+            return;
+        }
+
+        styleClass.add(SUCCESS_STYLE_CLASS);
     }
 
 }
