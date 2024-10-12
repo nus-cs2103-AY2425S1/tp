d5b3b73fa37f67a50241c460f2ce21c0887cc29d
Yong Khee Hou <yongkheehou@gmail.com>
2024-10-12 10:49:51 +0800

    Make Client an abstract class

--- src/main/java/seedu/address/model/client/Client.java
+++ src/main/java/seedu/address/model/client/Client.java
@@ -2,20 +2,16 @@ package seedu.address.model.client;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Objects;
-
-import seedu.address.commons.util.ToStringBuilder;
-
 /**
  * Represents a Client in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
-public class Client {
+public abstract class Client {
 
     // Identity fields
-    private final Name name;
-    private final Phone phone;
-    private final Email email;
+    protected final Name name;
+    protected final Phone phone;
+    protected final Email email;
 
     /**
      * Every field must be present and not null.
@@ -39,52 +35,5 @@ public class Client {
         return email;
     }
 
-    /**
-     * Returns true if both clients have the same name.
-     * This defines a weaker notion of equality between two clients.
-     */
-    public boolean isSameClient(Client otherClient) {
-        if (otherClient == this) {
-            return true;
-        }
-
-        return otherClient != null
-                && otherClient.getName().equals(getName());
-    }
-
-    /**
-     * Returns true if both clients have the same identity and data fields.
-     * This defines a stronger notion of equality between two clients.
-     */
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof Client)) {
-            return false;
-        }
-
-        Client otherPerson = (Client) other;
-        return name.equals(otherPerson.name)
-                && phone.equals(otherPerson.phone)
-                && email.equals(otherPerson.email);
-    }
-
-    @Override
-    public int hashCode() {
-        // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email);
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .add("name", name)
-                .add("phone", phone)
-                .add("email", email)
-                .toString();
-    }
+    public abstract boolean isSameClient(Client otherClient);
 }
