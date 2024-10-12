c8810fcbc97aa4f9b355040cc66f5a887348be63
Lim Yixiang <limyixiang2@gmail.com>
2024-10-13 02:03:34 +0800

    Add more test cases for UniqueEventListTest

--- src/test/java/seedu/address/model/event/UniqueEventListTest.java
+++ src/test/java/seedu/address/model/event/UniqueEventListTest.java
@@ -8,6 +8,10 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EVENT_DESCRIPTI
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EVENT_FROM_DATE_1;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EVENT_TO_DATE_1;
 import static seedu.address.testutil.TypicalEvents.MEETING;
+import static seedu.address.testutil.TypicalEvents.WORKSHOP;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -68,5 +72,95 @@ public class UniqueEventListTest {
         UniqueEventList expectedUniqueEventList = new UniqueEventList();
         expectedUniqueEventList.add(MEETING);
         assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvent_editedEventHasSameIdentity_success() {
+        uniqueEventList.add(MEETING);
+        Event editedMeeting = new EventBuilder(MEETING).withEventDescription(VALID_EVENT_DESCRIPTION_WORKSHOP)
+                .withEventDuration(VALID_EVENT_FROM_DATE_1, VALID_EVENT_TO_DATE_1).build();
+        uniqueEventList.setEvent(MEETING, editedMeeting);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(editedMeeting);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvent_editedEventHasDifferentIdentity_success() {
+        uniqueEventList.add(MEETING);
+        uniqueEventList.setEvent(MEETING, WORKSHOP);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(WORKSHOP);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvent_editedEventHasNonUniqueIdentity_throwsDuplicateEventException() {
+        uniqueEventList.add(MEETING);
+        uniqueEventList.add(WORKSHOP);
+        assertThrows(DuplicateEventException.class, () -> uniqueEventList.setEvent(MEETING, WORKSHOP));
+    }
+
+    @Test
+    public void remove_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.remove(null));
+    }
+
+    @Test
+    public void remove_eventDoesNotExist_throwsEventNotFoundException() {
+        assertThrows(EventNotFoundException.class, () -> uniqueEventList.remove(MEETING));
+    }
+
+    @Test
+    public void remove_existingEvent_removesEvent() {
+        uniqueEventList.add(MEETING);
+        uniqueEventList.remove(MEETING);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvents_nullUniqueEventList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.setEvents((UniqueEventList) null));
+    }
+
+    @Test
+    public void setEvents_uniqueEventList_replacesOwnListWithProvidedUniqueEventList() {
+        uniqueEventList.add(MEETING);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(WORKSHOP);
+        uniqueEventList.setEvents(expectedUniqueEventList);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvents_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueEventList.setEvents((UniqueEventList) null));
+    }
+
+    @Test
+    public void setEvents_list_replacesOwnListWithProvidedList() {
+        uniqueEventList.add(MEETING);
+        UniqueEventList expectedUniqueEventList = new UniqueEventList();
+        expectedUniqueEventList.add(WORKSHOP);
+        uniqueEventList.setEvents(expectedUniqueEventList);
+        assertEquals(expectedUniqueEventList, uniqueEventList);
+    }
+
+    @Test
+    public void setEvents_listWithDuplicateEvents_throwsDuplicateEventException() {
+        List<Event> listWithDuplicateEvents = Arrays.asList(MEETING, MEETING);
+        assertThrows(DuplicateEventException.class, () -> uniqueEventList.setEvents(listWithDuplicateEvents));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () ->
+            uniqueEventList.asUnmodifiableObservableList().remove(0));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(uniqueEventList.asUnmodifiableObservableList().toString(), uniqueEventList.toString());
     }
 }
