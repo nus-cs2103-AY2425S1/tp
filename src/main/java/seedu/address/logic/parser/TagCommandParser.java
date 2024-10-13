9bf75130490e4fba9e05ffd0d8c2c13c4c63506e
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-13 19:54:25 +0800

    Update TagCommandParser to catch illegal arguments

--- src/main/java/seedu/address/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -4,7 +4,9 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.TagCommand;
@@ -24,14 +26,28 @@ public class TagCommandParser implements Parser<TagCommand> {
         ArgumentMultimap multimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
         try {
             Index index = ParserUtil.parseIndex(multimap.getPreamble());
-            Set<Tag> tagList = ParserUtil.parseTags(multimap.getAllValues(PREFIX_TAG)); //the list of tags to be added
-            if (tagList.isEmpty()) {
+            String tags = multimap.getValue(PREFIX_TAG).orElse("");
+            Set<Tag> tagSet = stringToTagSet(tags);
+
+            if (tagSet.isEmpty()) {
                 throw new ParseException("No tags specified");
             }
-            return new TagCommand(index, tagList);
+            return new TagCommand(index, tagSet);
 
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE), pe);
         }
+    }
+
+    /**
+     * Utility method to parse tags delimited by spaces into a Set of Tags
+     * @param tagString Input string to parse
+     * @return
+     */
+    private static Set<Tag> stringToTagSet(String tagString) {
+        String[] strArr = tagString.split("\\s+"); // regex to catch multiple spaces
+        System.out.println(Arrays.toString(strArr));
+        Set<Tag> tagSet = Arrays.stream(strArr).map(s -> new Tag(s)).collect(Collectors.toSet());
+        return tagSet;
     }
 }
