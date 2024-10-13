2cddc30df720c77c81e36cb25494dbd29141d0c7
ckaayy <e1122115@u.nus.edu>
2024-10-13 17:20:43 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -7,7 +7,6 @@ import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.model.person.Person;
 import seedu.address.model.policy.Policy;
-import seedu.address.model.policy.PolicyExpiryDatePredicate;
 
 /**
  * The API of the Model component.
