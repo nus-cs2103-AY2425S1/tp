40b160404bde4099b4b918ef4c8fd144c7130bee
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-10-13 23:03:15 +0800

    Modify FilterCommand
    - Added in functionality to filter by multiple tags
    - Added corresponding test cases

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -10,6 +10,8 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.tag.Tag;
 
 public class FilterCommandTest {
     private Model model;
@@ -28,8 +31,10 @@ public class FilterCommandTest {
 
     @Test
     public void equals() {
+        Set<Tag> expectedTags = new HashSet<>();
+        expectedTags.add(new Tag("friend"));
         FilterCommand filterFirstCommand = new FilterCommand("first", null);
-        FilterCommand filterSecondCommand = new FilterCommand(null, "second");
+        FilterCommand filterSecondCommand = new FilterCommand(null, expectedTags);
 
         // same object -> returns true
         assertTrue(filterFirstCommand.equals(filterFirstCommand));
@@ -47,47 +52,60 @@ public class FilterCommandTest {
         // different person -> returns false
         assertFalse(filterFirstCommand.equals(filterSecondCommand));
     }
-    //Test case for filtering using name only
+
+    // Test case for filtering using name only
     @Test
     public void execute_nameKeyword_personsFound() {
-        String expectedMessage = FilterCommand.constructSuccessMessage("Meier", null);
-        FilterCommand command = new FilterCommand("Meier", null);
+        Set<Tag> expectedTags = new HashSet<>();
+        String expectedMessage = FilterCommand.constructSuccessMessage("Meier", expectedTags);
+        FilterCommand command = new FilterCommand("Meier", expectedTags);
         CommandResult result = command.execute(model);
         assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(Arrays.asList(BENSON, DANIEL), model.getFilteredPersonList());
     }
-    //Test case for filtering using tag only
+
+    // Test case for filtering using tag only
     @Test
     public void execute_tagKeyword_personsFound() {
-        String expectedMessage = FilterCommand.constructSuccessMessage(null, "owesMoney");
-        FilterCommand command = new FilterCommand(null, "owesMoney");
+        Set<Tag> expectedTags = new HashSet<>();
+        expectedTags.add(new Tag("owesMoney"));
+        String expectedMessage = FilterCommand.constructSuccessMessage(null, expectedTags);
+        FilterCommand command = new FilterCommand(null, expectedTags);
         CommandResult result = command.execute(model);
         assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(Arrays.asList(BENSON), model.getFilteredPersonList());
     }
-    //Test case for fitlering usign both
+
+    // Test case for filtering using both name and tags
     @Test
-    public void execute_nameAndTagKeywords_personFound() {
-        String expectedMessage = FilterCommand.constructSuccessMessage("Meier", "friends");
-        FilterCommand command = new FilterCommand("Meier", "friends");
+    public void execute_nameAndTagKeywords_personsFound() {
+        Set<Tag> expectedTags = new HashSet<>();
+        expectedTags.add(new Tag("friends"));
+        String expectedMessage = FilterCommand.constructSuccessMessage("Meier", expectedTags);
+        FilterCommand command = new FilterCommand("Meier", expectedTags);
         CommandResult result = command.execute(model);
         assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(Arrays.asList(BENSON, DANIEL), model.getFilteredPersonList());
     }
-    //Test case for when there is no one after filtering
+
+    // Test case for when there is no one after filtering
     @Test
     public void execute_noMatchingNameOrTag_allPersonsReturned() {
         String expectedMessage = FilterCommand.MESSAGE_NO_CONTACT_FOUND;
-        FilterCommand command = new FilterCommand("Damith", "2103");
+        Set<Tag> tags = new HashSet<>();
+        tags.add(new Tag("2103"));
+        FilterCommand command = new FilterCommand("Damith", tags);
         CommandResult result = command.execute(model);
         assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(Collections.emptyList(), model.getFilteredPersonList());
     }
-    //Test case to test parital string fitlering
+
+    // Test case to test partial string filtering
     @Test
     public void execute_partialNameMatch_personsFound() {
-        String expectedMessage = FilterCommand.constructSuccessMessage("er", null);
-        FilterCommand command = new FilterCommand("er", null);
+        Set<Tag> expectedTags = new HashSet<>();
+        String expectedMessage = FilterCommand.constructSuccessMessage("er", expectedTags);
+        FilterCommand command = new FilterCommand("er", expectedTags);
         CommandResult result = command.execute(model);
         assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(Arrays.asList(BENSON, DANIEL, ELLE), model.getFilteredPersonList());
@@ -95,8 +113,10 @@ public class FilterCommandTest {
 
     @Test
     public void toStringMethod() {
-        FilterCommand filterCommand = new FilterCommand("John", "client");
-        String expected = FilterCommand.class.getCanonicalName() + "{name=John, tagName=client}";
+        Set<Tag> tags = new HashSet<>();
+        tags.add(new Tag("client"));
+        FilterCommand filterCommand = new FilterCommand("John", tags);
+        String expected = FilterCommand.class.getCanonicalName() + "{name=John, tags=[[client]]}";
         assertEquals(expected, filterCommand.toString());
     }
 }
