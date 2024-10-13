bd32effb7934a4374a8bd09ebb1e26a3550cb4d6
somethingfishyfishy <ernestycr@gmail.com>
2024-10-13 14:28:53 +0800

    Add error messages for duplicate input fields

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -40,7 +40,7 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
-    public static final String MESSAGE_DUPLICATE_FIELD = "This person has a duplicate field as another person";
+    public static final String MESSAGE_DUPLICATE_FIELDS = "This person has a duplicate field as another person";
 
     private final Person toAdd;
 
@@ -61,7 +61,7 @@ public class AddCommand extends Command {
         }
 
         if (model.hasDuplicateFields(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_FIELD);
+            throw new CommandException(MESSAGE_DUPLICATE_FIELDS);
         }
 
         model.addPerson(toAdd);
--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -52,6 +52,8 @@ public class EditCommand extends Command {
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_FIELDS =
+            "One of the fields you want to add conflict with another person.";
 
     private final Index index;
     private final EditPersonDescriptor editPersonDescriptor;
@@ -82,6 +84,10 @@ public class EditCommand extends Command {
 
         if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        if (!personToEdit.isSamePerson(editedPerson) && model.hasDuplicateFields(editedPerson)) {
+            throw new CommandException(MESSAGE_DUPLICATE_FIELDS);
         }
 
         model.setPerson(personToEdit, editedPerson);
