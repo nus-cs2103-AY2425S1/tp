73d825fb0c13cb126e48cc7598e41db9ee0bd085
teamotitan <timothy.tanzijing@gmail.com>
2024-10-13 21:18:45 +0800

    Fixed bugs and style errors

--- src/test/java/seedu/address/logic/commands/IncomeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/IncomeCommandTest.java
@@ -1,25 +1,34 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+
 
 public class IncomeCommandTest {
+    Model model = new ModelManager();
     @Test
     public void emptyAddressBook_noPaidAmount_noOwedAmount() {
-        Model model = new ModelManager();
         CommandResult commandResult = new IncomeCommand().execute(model);
         assertEquals(commandResult.getFeedbackToUser(), "Total Paid: 0.0   Total Owed: 0.0");
     }
 
-//    @Test
-//    public void addPerson_Updated
+    @Test
+    public void addPerson_showUpdated_PaidAndOwed() {
+        Person validPerson = new PersonBuilder().build();
+        model.addPerson(validPerson);
+        CommandResult commandResult = new IncomeCommand().execute(model);
+
+        String expectedOutput = "Total Paid: " + validPerson.getPaid().value
+                + "   Total Owed: " + validPerson.getOwedAmount().value;
+
+        assertEquals(commandResult.getFeedbackToUser(), expectedOutput);
+
+
+    }
 }
