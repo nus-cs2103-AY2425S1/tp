d818ef912ac95e6c5659d359904cc099d6f3b0be
bmanara <bmanaranus@gmail.com>
2024-10-13 18:47:01 +0800

    Reformat code to follow style

--- src/main/java/seedu/address/logic/commands/ListDoctorCommand.java
+++ src/main/java/seedu/address/logic/commands/ListDoctorCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.doctor.DoctorPredicate;
 
--- src/main/java/seedu/address/model/doctor/DoctorPredicate.java
+++ src/main/java/seedu/address/model/doctor/DoctorPredicate.java
@@ -4,9 +4,12 @@ import java.util.function.Predicate;
 
 import seedu.address.model.person.Person;
 
+/**
+ * Tests that a {@code Person} is an instanceof Doctor.
+ */
 public class DoctorPredicate implements Predicate<Person> {
-        @Override
-        public boolean test(Person person) {
-            return person instanceof Doctor;
-        }
+    @Override
+    public boolean test(Person person) {
+        return person instanceof Doctor;
+    }
 }
