382b4dcc071bd171f9695fad126bf81a46a4358d
Gilbb <gilbert.iskandar2002@gmail.com>
2024-10-13 23:20:53 +0800

    Remove extra line break

--- src/main/java/seedu/address/logic/commands/EventCommand.java
+++ src/main/java/seedu/address/logic/commands/EventCommand.java
@@ -23,7 +23,7 @@ public class EventCommand extends Command {
             + PREFIX_DATE + "DATE (yyyy-mm-dd) \n"
             + "Example: "
             + PREFIX_NAME + "New Years Party "
-            + PREFIX_DATE + "2025-01-01 \n";
+            + PREFIX_DATE + "2025-01-01";
 
     public static final String MESSAGE_SUCCESS = "New event added: %1$s";
     public static final String MESSAGE_DUPLICATE_EVENT = "This event already exists in the address book";
