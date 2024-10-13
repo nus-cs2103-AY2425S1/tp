19fc9c535022a1afa75a1f34a0187adb909a3650
Jabez Tho <jabeztho@gmail.com>
2024-10-14 02:29:05 +0800

    Fix model stub not overriding

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -179,6 +179,11 @@ public class AddCommandTest {
         public ObservableList<Event> getFilteredEventList() {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public void updateFilteredEventList(Predicate<Event> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
