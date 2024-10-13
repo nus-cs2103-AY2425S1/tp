61d777402eea39e04a499336aab8c2cac7f7e844
teamotitan <timothy.tanzijing@gmail.com>
2024-10-13 21:41:52 +0800

    Fixed style issues

--- src/main/java/seedu/address/logic/commands/IncomeCommand.java
+++ src/main/java/seedu/address/logic/commands/IncomeCommand.java
@@ -14,16 +14,16 @@ import seedu.address.model.person.Person;
 public class IncomeCommand extends Command {
     public static final String COMMAND_WORD = "income";
 
-    public static final String MESSAGE_SUCCESS = "Showed total money earned and owed";
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         double totalOwed = 0;
         double totalPaid = 0;
         ObservableList<Person> personList = model.getFilteredPersonList();
+
         for (Person person: personList) {
-        totalOwed += person.getOwedAmount().value;
-        totalPaid += person.getPaid().value;
+            totalOwed += person.getOwedAmount().value;
+            totalPaid += person.getPaid().value;
         }
         return new CommandResult("Total Paid: " + totalPaid + "   Total Owed: " + totalOwed);
     }
--- src/test/java/seedu/address/logic/commands/IncomeCommandTest.java
+++ src/test/java/seedu/address/logic/commands/IncomeCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import org.junit.jupiter.api.Test;
 
