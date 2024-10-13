133bdd10388df8e144a4d4f542fe1743aba6df70
Jabez Tho <jabeztho@gmail.com>
2024-10-14 01:16:39 +0800

    Update parser to suppport deleting event

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,11 +2,13 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_VENDOR;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT;
 
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteEventCommand;
 import seedu.address.logic.commands.DeleteVendorCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -23,22 +25,57 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_VENDOR);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_VENDOR, PREFIX_EVENT);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_VENDOR)
+        final boolean isEventDelete = arePrefixesPresent(argMultimap, PREFIX_EVENT);
+        final boolean isVendorDelete = arePrefixesPresent(argMultimap, PREFIX_VENDOR);
+
+        // exactly one prefix should be present
+        if (!(isVendorDelete ^ isEventDelete)
                 || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteVendorCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_VENDOR);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_VENDOR, PREFIX_EVENT);
 
+        if (isEventDelete) {
+            return parseEventDelete(argMultimap);
+        } else {
+            return parseVendorDelete(argMultimap);
+        }
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * DeleteVendorCommand
+     * and returns a DeleteVendorCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    private DeleteVendorCommand parseVendorDelete(ArgumentMultimap argMultimap) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_VENDOR).get());
-            // default to vendor since event deletion is not supported
             return new DeleteVendorCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteVendorCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the
+     * DeleteEventCommand
+     * and returns a DeleteEventCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    private DeleteEventCommand parseEventDelete(ArgumentMultimap argMultimap) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_EVENT).get());
+            return new DeleteEventCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE), pe);
         }
     }
 
