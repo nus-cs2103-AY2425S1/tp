d17627ee8ee72e025d0823e2977762e53f9faa2f
PeterHW963 <peterhadiwijaya963@gmail.com>
2024-10-13 14:58:49 +0800

    Improve UI for DetailPanel.
    Need Meetings class to be implemented properly to finish

--- src/main/java/seedu/address/ui/DetailPanel.java
+++ src/main/java/seedu/address/ui/DetailPanel.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
+import seedu.address.model.person.Major;
 import seedu.address.model.person.Person;
 
 /**
@@ -59,12 +60,6 @@ public class DetailPanel extends UiPart<Region> implements SelectionListener {
         // Set up further interactions or styling
     }
 
-    public void setPerson(Person person, int displayedIndex) {
-        this.person = person;
-        this.displayedIndex = displayedIndex;
-        updateDetails();
-    }
-
     /**
      * Sets the person details on the DetailPanel.
      * This method updates the text of the labels with the provided person's details.
@@ -77,12 +72,35 @@ public class DetailPanel extends UiPart<Region> implements SelectionListener {
         address.setText("Address: " + person.getAddress().value);
         email.setText("Email: " + person.getEmail().value);
         role.setText("Role: " + person.getRole());
-        major.setText("Major: " + person.getMajor());
+        major.setText("Major: " + getMajorFullName(person.getMajor()));
         // Update other fields as needed
     }
 
     @Override
     public void onPersonSelected(Person person, int index) {
         setPerson(person, index);
+    }
+
+    public void setPerson(Person person, int index) {
+        this.person = person;
+        this.displayedIndex = index + 1;
+        updateDetails();
+    }
+
+    private String getMajorFullName(Major major) {
+        switch (major.toString()) {  // Using toLowerCase to handle case variations
+        case "cs":
+            return "Computer Science";
+        case "bza":
+            return "Business Analytics";
+        case "isys":
+            return "Information System";
+        case "isec":
+            return "Information Security";
+        case "ceg":
+            return "Computer Engineering";
+        default:
+            return "Unknown Major";  // Default case if the major code doesn't match known ones
+        }
     }
 }
