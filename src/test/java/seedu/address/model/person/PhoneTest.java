f34bc72e2dd12dece1574ea088ecb8f5a27b0440
EdaYeo <wye.yeo@gmail.com>
2024-10-14 23:23:21 +0800

    Fix area code

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -30,6 +30,7 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
+        assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits without country code
         assertFalse(Phone.isValidPhone("+ 90111234")); // indicates empty country code
         assertFalse(Phone.isValidPhone("+53  90111234")); // indicates empty area code
 
