3e7ac83e0651d8fde390f6d5ec8b60e83d700b9a
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-15 01:57:23 +0800

    Change ListTags to display in alphabetical order

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -3,16 +3,27 @@ package seedu.address.logic.commands;
 import seedu.address.model.Model;
 import seedu.address.model.tag.Tag;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
 
+/**
+ * Outputs the currently used tags in the AddressBook
+ */
 public class ListTagsCommand extends Command {
     public static final String COMMAND_WORD = "taglist";
 
     public static final String MESSAGE_SUCCESS = "Listed tags currently in use: %1$s";
+
     @Override
     public CommandResult execute(Model model) {
-        HashMap<Tag, Integer> activeTags = model.getActiveTags();
-        String tagString = Tag.tagSetToString(activeTags.keySet());
-        return new CommandResult(String.format(MESSAGE_SUCCESS, tagString));
+        HashMap<Tag, Integer> tagMap = model.getActiveTags();
+        Set<Tag> tags = tagMap.keySet();    //Getting the unique Tags
+        String[] tagStrings = tags.stream().map(Tag::toString).toArray(String[]::new); //convert Set to Array
+        Arrays.sort(tagStrings);    //Sort the tag strings in alphabetical order
+
+        String resultString = String.join(", ", tagStrings);
+
+        return new CommandResult(String.format(MESSAGE_SUCCESS, resultString));
     }
 }
