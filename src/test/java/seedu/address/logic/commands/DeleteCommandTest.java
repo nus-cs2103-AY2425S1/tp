913ef7a75f05b599a51cc55076086278d821aae5
ritvi12 <ritvi0205@gmail.com>
2024-10-14 23:36:13 +0800

    Update checkstyle

--- src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteCommandTest.java
@@ -1,15 +1,16 @@
 package seedu.address.logic.commands;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -17,8 +18,6 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.commons.core.index.Index;
-
-import java.util.Optional;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
