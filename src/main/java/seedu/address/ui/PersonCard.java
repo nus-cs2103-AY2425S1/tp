c0e2b3762c1616422d0cc49722d76ff8a4fe3b3e
jasmiinee <jasmiine.lje@gmail.com>
2024-10-14 17:18:21 +0800

    Fix bugs

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -50,7 +50,7 @@ public class PersonCard extends UiPart<Region> {
     private FlowPane tags;
 
     /**
-     * Creates a {@code PersonCode} with the given {@code Person} and index to display
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
@@ -60,6 +60,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+
         if (person.getSchedule().toString().isEmpty()) {
             schedule.setText(person.getSchedule().toString());
         } else {
@@ -73,15 +74,21 @@ public class PersonCard extends UiPart<Region> {
             note.setText("Notes: " + person.getSchedule().getNotes());
         }
         if (person.getReminder() != null && !person.getReminder().toString().isEmpty()) {
-            String formattedDateTime = LocalDateTime.parse(person.getReminder().getAppointmentDateTime(),
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"))
+            String formattedDateTime = LocalDateTime.parse(
+                            person.getReminder().getAppointmentDateTime(),
+                            DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"))
                     .format(DateTimeFormatter.ofPattern("MMM d yyyy, h:mm a"));
-            reminder.setText(String.format("Reminder on %s, %s before", formattedDateTime,
+
+            // Combine formatted date-time with the reminder time
+            reminder.setText(String.format("Reminder on %s, %s before",
+                    formattedDateTime,
                     person.getReminder().getReminderTime()));
         } else {
             reminder.setText("");
         }
-        person.getTags().stream().sorted(Comparator.comparing(tag -> tag.tagName))
+
+        person.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 }
