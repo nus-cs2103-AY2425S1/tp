e5c5d7f94d4f35c949bd8ba16ec1647e5cc87c91
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-15 01:33:09 +0800

    Add ListTagsCommand class

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -1,4 +1,18 @@
 package seedu.address.logic.commands;
 
-public class ListTagsCommand {
+import seedu.address.model.Model;
+import seedu.address.model.tag.Tag;
+
+import java.util.HashMap;
+
+public class ListTagsCommand extends Command {
+    public static final String COMMAND_WORD = "taglist";
+
+    public static final String MESSAGE_SUCCESS = "Listed tags currently in use: %1$s";
+    @Override
+    public CommandResult execute(Model model) {
+        HashMap<Tag, Integer> activeTags = model.getActiveTags();
+        String tagString = Tag.tagSetToString(activeTags.keySet());
+        return new CommandResult(String.format(MESSAGE_SUCCESS, tagString));
+    }
 }
