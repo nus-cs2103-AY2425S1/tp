43a7bdbca0a60108d422affb0fdedeb92bcabace
annabellekk <yuyien.khoo@gmail.com>
2024-10-14 23:52:02 +0800

    Show new fields ic and classes in a person's contact card

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -39,6 +39,10 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private Label ic;
+    @FXML
+    private Label classes;
+    @FXML
     private FlowPane tags;
 
     /**
@@ -52,6 +56,7 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
+        ic.setText(person.getIc().value);
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
