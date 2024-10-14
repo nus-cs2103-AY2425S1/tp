a7edbf643a5930897a2b692a6786f393956aebd7
Jianwen <jianwenlei2003@gmail.com>
2024-10-14 21:01:51 +0800

    Update the UserPref to have the path for archived file of the Address book
    This is a preparation work for the implementation of archive command

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -15,6 +15,7 @@ public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
     private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path archivedAddressBookFilePath = Paths.get("archived", "archivedaddressbook.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
@@ -36,6 +37,7 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         requireNonNull(newUserPrefs);
         setGuiSettings(newUserPrefs.getGuiSettings());
         setAddressBookFilePath(newUserPrefs.getAddressBookFilePath());
+        setArchivedAddressBookFilePath(newUserPrefs.getArchivedAddressBookFilePath());
     }
 
     public GuiSettings getGuiSettings() {
@@ -54,6 +56,14 @@ public class UserPrefs implements ReadOnlyUserPrefs {
     public void setAddressBookFilePath(Path addressBookFilePath) {
         requireNonNull(addressBookFilePath);
         this.addressBookFilePath = addressBookFilePath;
+    }
+
+    public Path getArchivedAddressBookFilePath() {
+        return archivedAddressBookFilePath;
+    }
+
+    public void setArchivedAddressBookFilePath(Path archivedAddressBookFilePath) {
+        this.archivedAddressBookFilePath = archivedAddressBookFilePath;
     }
 
     @Override
