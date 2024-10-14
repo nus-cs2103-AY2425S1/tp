d133359f8de8f2e4914e25c829cd4d0ce855ab4b
OrangeCatLoves <e1121707@u.nus.edu>
2024-10-14 21:35:47 +0800

    Made some changes to pass JavaCI

--- src/test/java/seedu/address/logic/commands/AddPropertyToSellCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPropertyToSellCommandTest.java
@@ -1,17 +1,17 @@
 
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.exceptions.CommandException;
+//import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
