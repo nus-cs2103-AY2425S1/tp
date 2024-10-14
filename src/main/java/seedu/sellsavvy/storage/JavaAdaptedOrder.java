c4244eeccd3bdc9dade06728c7f84dabea6a9ab2
kaixuan477 <kxuan477@gmail.com>
2024-10-14 21:54:01 +0800

    Add JavaAdaptedOrder.java for storing orders

---
+++ src/main/java/seedu/sellsavvy/storage/JavaAdaptedOrder.java
@@ -0,0 +1,76 @@
+package seedu.sellsavvy.storage;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.sellsavvy.commons.exceptions.IllegalValueException;
+import seedu.sellsavvy.model.order.Count;
+import seedu.sellsavvy.model.order.Date;
+import seedu.sellsavvy.model.order.Item;
+import seedu.sellsavvy.model.order.Order;
+
+/**
+ * Jackson-friendly version of {@link Order}.
+ */
+class JsonAdaptedOrder {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Order's %s field is missing!";
+
+    private final String count;
+    private final String date;
+    private final String item;
+
+    /**
+     * Constructs a {@code JsonAdaptedOrder} with the given order details.
+     */
+    @JsonCreator
+    public JsonAdaptedOrder(@JsonProperty("item") String item, @JsonProperty("count") String count,
+                             @JsonProperty("date") String date) {
+        this.date = date;
+        this.item = item;
+        this.count = count;
+    }
+
+    /**
+     * Converts a given {@code Order} into this class for Jackson use.
+     */
+    public JsonAdaptedOrder(Order source) {
+        item = source.getItem().fullDescription;
+        date = source.getDate().value;
+        count = source.getCount().value;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted order object into the model's {@code Order} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted order.
+     */
+    public Order toModelType() throws IllegalValueException {
+        if (item == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Item.class.getSimpleName()));
+        }
+        if (!Item.isValidItem(item)) {
+            throw new IllegalValueException(Item.MESSAGE_CONSTRAINTS);
+        }
+        final Item modelItem = new Item(item);
+
+        if (date == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Date.class.getSimpleName()));
+        }
+        if (!Date.isValidDate(date)) {
+            throw new IllegalValueException(Date.MESSAGE_CONSTRAINTS);
+        }
+        final Date modelDate = new Date(date);
+
+        if (count == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Count.class.getSimpleName()));
+        }
+        if (!Count.isValidCount(count)) {
+            throw new IllegalValueException(Count.MESSAGE_CONSTRAINTS);
+        }
+        final Count modelCount = new Count(count);
+
+        return new Order(modelItem, modelCount, modelDate);
+    }
+
+}
--- src/test/java/seedu/sellsavvy/testutil/TypicalPersons.java
+++ src/test/java/seedu/sellsavvy/testutil/TypicalPersons.java
@@ -10,6 +10,12 @@ import static seedu.sellsavvy.logic.commands.personcommands.PersonCommandTestUti
 import static seedu.sellsavvy.logic.commands.personcommands.PersonCommandTestUtil.VALID_PHONE_BOB;
 import static seedu.sellsavvy.logic.commands.personcommands.PersonCommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.sellsavvy.logic.commands.personcommands.PersonCommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.sellsavvy.testutil.TypicalOrders.ABACUS;
+import static seedu.sellsavvy.testutil.TypicalOrders.ATLAS;
+import static seedu.sellsavvy.testutil.TypicalOrders.BLOCKS;
+import static seedu.sellsavvy.testutil.TypicalOrders.BOTTLE;
+import static seedu.sellsavvy.testutil.TypicalOrders.CAMERA;
+import static seedu.sellsavvy.testutil.TypicalOrders.DAGGER;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,32 +31,35 @@ public class TypicalPersons {
 
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("94351253")
-            .withTags("friends").build();
+            .withPhone("94351253").withTags("friends").withOrders(ABACUS).build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
-            .withTags("owesMoney", "friends").build();
+            .withTags("owesMoney", "friends").withOrders(ABACUS).build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").build();
+            .withEmail("heinz@example.com").withAddress("wall street").withOrders(ABACUS).build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").build();
+            .withEmail("cornelia@example.com").withAddress("10th street")
+            .withTags("friends").withOrders(ABACUS).build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").build();
+            .withEmail("werner@example.com").withAddress("michegan ave").withOrders(ABACUS).build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo").withOrders(ABACUS).build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").build();
+            .withEmail("anna@example.com").withAddress("4th street").withOrders(ABACUS).build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").build();
+            .withEmail("stefan@example.com").withAddress("little india")
+            .withOrders(ABACUS, BLOCKS, CAMERA, DAGGER).build();
     public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").build();
+            .withEmail("hans@example.com").withAddress("chicago ave")
+            .withOrders(ATLAS, BOTTLE).build();
 
     // Manually added - Person's details found in {@code PersonCommandTestUtil}
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
+            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND)
+            .build();
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
             .build();
