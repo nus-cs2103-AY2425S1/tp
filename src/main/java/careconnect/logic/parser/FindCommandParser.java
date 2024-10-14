a4c48eb4887b1f87ba310e181e3ece71b8880eaa
celeschai <celes.chai@gmail.com>
2024-10-15 00:20:52 +0800

    style: Fix indentation

--- src/main/java/careconnect/logic/parser/FindCommandParser.java
+++ src/main/java/careconnect/logic/parser/FindCommandParser.java
@@ -16,7 +16,7 @@ public class FindCommandParser implements Parser<FindCommand> {
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format or if
-     * the search term is too short
+     *     the search term is too short
      */
     public FindCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
