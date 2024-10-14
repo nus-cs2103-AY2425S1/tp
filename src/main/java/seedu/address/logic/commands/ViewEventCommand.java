3502ad89ead6ee08830003c74aada5977fd71cfc
fusin <fusin02@gmail.com>
2024-10-14 21:47:36 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/ViewEventCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewEventCommand.java
@@ -31,7 +31,8 @@ public class ViewEventCommand extends ViewCommand {
         requireNonNull(model);
         model.updateFilteredEventList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredEventList().size()), false, false, false, true);
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredEventList().size()),
+                false, false, false, true);
 
     }
 
