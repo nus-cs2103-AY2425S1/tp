480dd8838633b83a48d74c498533a0e39dde18c5
hogretup <leechewie13@gmail.com>
2024-10-14 21:17:34 +0800

    refactor import issues

--- src/main/java/seedu/address/logic/parser/SearchCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchCommandParser.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 import seedu.address.logic.commands.SearchCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.TagContainsKeywordsPredicate;
+import seedu.address.model.types.common.TagContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new SearchCommand object
