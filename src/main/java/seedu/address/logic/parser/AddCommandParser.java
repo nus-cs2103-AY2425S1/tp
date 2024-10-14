c2147e047e4f67ae300bd97bae018a363278d9f3
Daryl Koh <daryl.koh.hb@gmail.com>
2024-10-14 21:32:48 +0800

    Amend repeated prefix

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -37,7 +37,7 @@ public class AddCommandParser implements Parser<AddCommand> {
                         PREFIX_TAG);
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_STUDENT_ID,
-                PREFIX_PHONE, PREFIX_EMAIL, PREFIX_STUDENT_ID) || !argMultimap.getPreamble().isEmpty()) {
+                PREFIX_PHONE, PREFIX_EMAIL) || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
