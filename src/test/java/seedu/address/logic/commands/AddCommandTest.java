2fb272043560cd0f35f90090fe92d8ac3e47320f
EMMANUEL\Emmanuel <emmanueler97@gmail.com>
2024-10-14 17:14:15 +0800

    Update code to meet Checkstyle requirements

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -22,7 +22,6 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.assignment.Assignment;
 import seedu.address.model.student.Student;
 import seedu.address.testutil.StudentBuilder;
 
