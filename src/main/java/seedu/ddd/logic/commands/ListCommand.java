1121b3e7c748c18ff9dc68f636c917146fd1f651
Teddayz <mervynneo02@gmail.com>
2024-10-14 14:12:25 +0800

    Add filter by name to list command

--- src/main/java/seedu/ddd/logic/commands/ListCommand.java
+++ src/main/java/seedu/ddd/logic/commands/ListCommand.java
@@ -1,6 +1,7 @@
 package seedu.ddd.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.function.Predicate;
@@ -19,11 +20,16 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all contacts.\n"
-            + "If tags are specified, all contacts that match any of "
+    public static final String TAG_MESSAGE_USAGE = COMMAND_WORD + " " + PREFIX_TAG
+            + " : All contacts that match any of "
             + "the specified tags (case-insensitive) will be displayed as a list with index numbers.\n"
             + "Parameters: " + PREFIX_TAG + " TAG...\n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_TAG + " catering " + PREFIX_TAG + " photography";
+    public static final String NAME_MESSAGE_USAGE = COMMAND_WORD + " " + PREFIX_NAME + " : All contacts with the "
+            + "specified name (case-insensitive) will be displayed as a list with index numbers. \n"
+            + "Parameters: " + PREFIX_NAME + "NAME...\n"
+            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + " jane ";
+
 
     private final Predicate<Contact> predicate;
     public ListCommand(Predicate<Contact> predicate) {
--- src/main/java/seedu/ddd/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/ddd/logic/parser/ListCommandParser.java
@@ -1,14 +1,20 @@
 package seedu.ddd.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.ddd.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_CLIENT;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_VENDOR;
 
+import java.util.Arrays;
 import java.util.Set;
 
 import seedu.ddd.logic.commands.ListCommand;
 import seedu.ddd.logic.parser.exceptions.ParseException;
 import seedu.ddd.model.Model;
 import seedu.ddd.model.contact.common.ContactContainsTagPredicate;
+import seedu.ddd.model.contact.common.NameContainsKeywordsPredicate;
 import seedu.ddd.model.tag.Tag;
 
 /**
@@ -21,17 +27,25 @@ public class ListCommandParser implements Parser<ListCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListCommand parse(String args) throws ParseException {
+        requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG, PREFIX_CLIENT, PREFIX_VENDOR);
         if (!argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
-        } else if (argMultimap.getValue(PREFIX_TAG).isEmpty()) {
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.TAG_MESSAGE_USAGE));
+        } else if (argMultimap.getValue(PREFIX_TAG).isPresent()) {
+            Set<Tag> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+            return new ListCommand(new ContactContainsTagPredicate(tagSet));
+        } else if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
+            String trimmedArgs = argMultimap.getValue(PREFIX_NAME).get().trim();
+            if (trimmedArgs.isEmpty()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.NAME_MESSAGE_USAGE));
+            }
+            String[] nameKeywords = trimmedArgs.split("\\s+");
+            return new ListCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        } else {
             return new ListCommand(Model.PREDICATE_SHOW_ALL_CONTACTS);
         }
-
-        Set<Tag> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
-
-        return new ListCommand(new ContactContainsTagPredicate(tagSet));
     }
 }
