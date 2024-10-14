72b7502ba1cff9af02fb7613a347c15fcdab28da
Raghava <chinnu4148@gmail.com>
2024-10-14 22:48:59 +0800

    Modify clear command

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -9,22 +9,25 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.internshipapplication.InternshipApplication;
 
 public class ClearCommandTest {
 
     @Test
     public void execute_emptyAddressBook_success() {
-        Model model = new ModelManager();
-        Model expectedModel = new ModelManager();
+        Model<InternshipApplication> model = new ModelManager<>();
+        Model<InternshipApplication> expectedModel = new ModelManager<>();
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
     @Test
     public void execute_nonEmptyAddressBook_success() {
-        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        expectedModel.setAddressBook(new AddressBook());
+        Model<InternshipApplication> model =
+                new ModelManager<InternshipApplication>(getTypicalAddressBook(), new UserPrefs());
+        Model<InternshipApplication> expectedModel =
+                new ModelManager<InternshipApplication>(getTypicalAddressBook(), new UserPrefs());
+        expectedModel.setAddressBook(new AddressBook<>());
 
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
