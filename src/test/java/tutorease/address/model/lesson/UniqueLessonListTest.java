62fa30672506d19819f4ae60758faef90fe11e54
ethantyh692 <ethantyh692@gmail.com>
2024-10-14 22:00:39 +0800

    Remove unused imports

--- src/test/java/tutorease/address/model/lesson/UniqueLessonListTest.java
+++ src/test/java/tutorease/address/model/lesson/UniqueLessonListTest.java
@@ -7,9 +7,7 @@ import static tutorease.address.testutil.Assert.assertThrows;
 
 import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
