7f95d2f15f2de0302f3911056ceb46201913bee6
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-10-14 18:58:56 +0800

    Change new person initialisation under 'MarkPaidCommand'
    Directly set Payment as true, to comply with the purpose of markPaid

--- src/main/java/seedu/address/logic/commands/MarkPaidCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkPaidCommand.java
@@ -9,6 +9,7 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Payment;
 import seedu.address.model.person.Person;
 
 /**
@@ -45,7 +46,7 @@ public class MarkPaidCommand extends Command {
         Person personToMarkPayment = lastShownList.get(targetIndex.getZeroBased());
         Person markedPerson = new Person(personToMarkPayment.getName(), personToMarkPayment.getPhone(),
                 personToMarkPayment.getEmail(), personToMarkPayment.getAddress(),
-                personToMarkPayment.getPayment(), personToMarkPayment.getTags());
+                new Payment(true), personToMarkPayment.getTags());
 
         model.setPerson(personToMarkPayment, markedPerson);
         model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
