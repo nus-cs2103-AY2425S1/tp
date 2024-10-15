a16273e8b256304c1605dbc693a9a4241e10a6b6
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-10-15 17:22:55 +0800

    Add PersonContainsKeywordsPredicate class
    There is currently no Predicate class for identifying Persons
    whose attributes may match any of the keywords or search query given.
    
    Let's add a new Predicate class PersonContainsKeywordsPredicate
    which first checks the search query for what keywords to check
    in which attribute categories.
    
    The Predicate's test method then checks a Person if any of their
    attributes matches any of the specified keywords.

---
+++ src/main/java/seedu/address/model/person/PersonContainsKeywordsPredicate.java
@@ -0,0 +1,74 @@
+package seedu.address.model.person;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Tests that a {@code Person} matches any of the keywords given.
+ */
+public class PersonContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> nameKeywords = new ArrayList<>();
+    private final List<String> phoneKeywords = new ArrayList<>();
+    private final List<String> emailKeywords = new ArrayList<>();
+    private final List<String> addressKeywords = new ArrayList<>();
+    private final List<String> tagsKeywords = new ArrayList<>();
+
+    // TODO: Missing keywords lists for subject and classes
+
+    public PersonContainsKeywordsPredicate(List<String> keywords) throws ParseException {
+        Map<Prefix, List<String>> keywordMap = new HashMap<>();
+        keywordMap.put(PREFIX_NAME, nameKeywords);
+        keywordMap.put(PREFIX_PHONE, phoneKeywords);
+        keywordMap.put(PREFIX_EMAIL, emailKeywords);
+        keywordMap.put(PREFIX_ADDRESS, addressKeywords);
+        keywordMap.put(PREFIX_TAG, tagsKeywords);
+
+        List<String> currentList = null;
+
+        for (String keyword : keywords) {
+            if (keywordMap.containsKey(new Prefix(keyword))) {
+                currentList = keywordMap.get(new Prefix(keyword));
+            } else if (currentList != null) {
+                currentList.add(keyword);
+            } else {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ClearCommand.MESSAGE_USAGE));
+            }
+        }
+    }
+
+    @Override
+    public boolean test(Person person) {
+        boolean hasMatchingName = nameKeywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+
+        boolean hasMatchingPhone = phoneKeywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getPhone().value, keyword));
+
+        boolean hasMatchingEmail = emailKeywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getEmail().value, keyword));
+
+        boolean hasMatchingAddress = addressKeywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getAddress().value, keyword));
+
+        boolean hasMatchingTags = tagsKeywords.stream()
+                .anyMatch(keyword -> person.getTags().stream()
+                        .anyMatch(tag -> StringUtil.containsWordIgnoreCase(tag.tagName, keyword)));
+
+        return hasMatchingName || hasMatchingPhone || hasMatchingEmail || hasMatchingAddress || hasMatchingTags;
+    }
+}
