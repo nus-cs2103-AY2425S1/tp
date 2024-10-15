24f76b35873f724cc1f7a0fc16fe2e1bd306863e
yihuididi <tohyihui11@yahoo.com>
2024-10-16 04:02:42 +0800

    Add a expiryDate field in Policy

---
+++ src/main/java/seedu/address/commons/core/dateformatter/DateFormatter.java
@@ -0,0 +1,11 @@
+package seedu.address.commons.core.dateformatter;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * A class to store the available DateFormatters used by Prudy.
+ */
+public class DateFormatter {
+    public static final DateTimeFormatter MM_DD_YYYY_FORMATTER = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    // Insert more formatters here
+}
--- src/main/java/seedu/address/model/policy/HealthPolicy.java
+++ src/main/java/seedu/address/model/policy/HealthPolicy.java
@@ -1,17 +1,37 @@
 package seedu.address.model.policy;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+import seedu.address.model.person.Person;
+
 /**
  * A Policy of type Health.
  */
 public class HealthPolicy extends Policy {
     private static final double DEFAULT_PREMIUM_AMOUNT = 200;
     private static final double DEFAULT_COVERAGE_AMOUNT = 20000;
+    private static final Period DEFAULT_EXPIRY_DATE_PERIOD = Period.ofYears(10);
 
     /**
-     * Constructor for a new HealthPolicy using the default premium amount and coverage amount.
+     * Constructor for a new HealthPolicy initialized with defaults.
      */
     public HealthPolicy() {
-        super(DEFAULT_PREMIUM_AMOUNT, DEFAULT_COVERAGE_AMOUNT);
+        super(DEFAULT_PREMIUM_AMOUNT, DEFAULT_COVERAGE_AMOUNT, LocalDate.now().plus(DEFAULT_EXPIRY_DATE_PERIOD));
+    }
+
+    /**
+     * Constructor for a new HealthPolicy with all its fields initialized.
+     *
+     * @param premiumAmount the price of the policy, paid per month.
+     * @param coverageAmount the maximum amount that can be claimed under this policy.
+     * @param expiryDate the date of Policy's expiry.
+     * @param insuree the policy insuree.
+     * @throws IllegalArgumentException if the premiumAmount or coverageAmount is negative.
+     * @throws NullPointerException if the given expiryDate or insuree is null.
+     */
+    public HealthPolicy(double premiumAmount, double coverageAmount, LocalDate expiryDate, Person insuree) {
+        super(premiumAmount, coverageAmount, expiryDate, insuree);
     }
 
     @Override
