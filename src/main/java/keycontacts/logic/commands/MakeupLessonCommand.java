280c6836e457732d385d159801873bd6437d9aa8
Bryan <bryanchenyu616@gmail.com>
2024-10-16 00:04:21 +0800

    Add command parser for makeup lesson

---
+++ src/main/java/keycontacts/logic/commands/MakeupLessonCommand.java
@@ -0,0 +1,71 @@
+package keycontacts.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static keycontacts.logic.parser.CliSyntax.PREFIX_DATE;
+import static keycontacts.logic.parser.CliSyntax.PREFIX_END_TIME;
+import static keycontacts.logic.parser.CliSyntax.PREFIX_START_TIME;
+
+import java.util.List;
+
+import keycontacts.commons.core.index.Index;
+import keycontacts.logic.Messages;
+import keycontacts.logic.commands.exceptions.CommandException;
+import keycontacts.model.Model;
+import keycontacts.model.lesson.MakeupLesson;
+import keycontacts.model.student.Student;
+
+/**
+ * Creates a new makeup lesson for a student identified using its displayed
+ * index from the student directory
+ */
+
+public class MakeupLessonCommand extends Command {
+
+    public static final String COMMAND_WORD = "makeup";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Creates a new makeup lesson "
+            + "for a student identified by the index number used in the displayed student list. "
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_DATE + "DATE "
+            + PREFIX_START_TIME + "START_TIME "
+            + PREFIX_END_TIME + "END_TIME\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_DATE + "06-07-2022 "
+            + PREFIX_START_TIME + "12:00 "
+            + PREFIX_END_TIME + "13:00";
+
+    private final Index targetIndex;
+    private final MakeupLesson makeupLesson;
+
+    /**
+     * @param date      of the makeup lesson
+     * @param startTime of the makeup lesson
+     * @param endTime   of the makeup lesson
+     */
+    public MakeupLessonCommand(Index targetIndex, MakeupLesson makeupLesson) {
+        this.targetIndex = targetIndex;
+        this.makeupLesson = makeupLesson;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Student> lastShownList = model.getFilteredStudentList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX);
+        }
+
+        Student studentToUpdate = lastShownList.get(targetIndex.getZeroBased());
+
+        return new CommandResult("Makeup lesson created.");
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof MakeupLessonCommand // instanceof handles nulls
+                        && makeupLesson.equals(((MakeupLessonCommand) other).makeupLesson));
+    }
+
+}
--- src/main/java/keycontacts/model/lesson/MakeupLesson.java
+++ src/main/java/keycontacts/model/lesson/MakeupLesson.java
@@ -1,11 +1,63 @@
 package keycontacts.model.lesson;
 
+import static keycontacts.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import keycontacts.commons.util.ToStringBuilder;
+
 /**
  * Represents a makeup lesson which happens if a student misses a lesson.
-*/
+ */
 public class MakeupLesson extends Lesson {
 
-    public MakeupLesson(Time startTime, Time endTime) {
+    private final LocalDate lessonDate;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public MakeupLesson(LocalDate lessonDate, Time startTime, Time endTime) {
         super(startTime, endTime);
+        requireAllNonNull(lessonDate);
+        this.lessonDate = lessonDate;
+    }
+
+    public LocalDate getLessonDate() {
+        return lessonDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lessonDate, getStartTime(), getEndTime());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof MakeupLesson)) {
+            return false;
+        }
+
+        MakeupLesson otherLesson = (MakeupLesson) other;
+        return super.equals(otherLesson) && lessonDate.equals(otherLesson.lessonDate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("lessonDate", lessonDate)
+                .add("startTime", getStartTime())
+                .add("endTime", getEndTime())
+                .toString();
+    }
+
+    @Override
+    public String toDisplay() {
+        return lessonDate.toString() + " " + super.toDisplay();
     }
 }
