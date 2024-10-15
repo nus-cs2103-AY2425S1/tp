e6bf457f15af44ad28f34706658b54d8444d85a8
yanqiyqh <yanqi.yqh@gmail.com>
2024-10-15 20:08:34 +0800

    Add ListClaimsCommand command word to AddressBookParser

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListClaimsCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -88,6 +89,9 @@ public class AddressBookParser {
 
         case AddClaimCommand.COMMAND_WORD:
             return new AddClaimCommandParser().parse(arguments);
+
+            case ListClaimsCommand.COMMAND_WORD:
+                return new ListClaimsCommandParser().parse(arguements);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
