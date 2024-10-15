efe1e96e26a94cd7497673c7974371b4bed955a3
Dinoman44 <prakamya.singh08@gmail.com>
2024-10-15 15:19:17 +0800

    Update person equality comparator method javadoc

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -83,7 +83,7 @@ public class Person {
     }
 
     /**
-     * Returns true if both persons have the same name.
+     * Returns true if both persons have the same name, email and phone.
      * This defines a weaker notion of equality between two persons.
      */
     public boolean isSamePerson(Person otherPerson) {
