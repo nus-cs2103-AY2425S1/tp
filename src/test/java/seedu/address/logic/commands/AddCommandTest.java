b183d2972289af3ed55fe25d8001e0ff66ce1840
Kevin Pek <kevinpek@r-34-98-25-172.comp.nus.edu.sg>
2024-10-15 20:09:30 +0800

    Update model stub in AddCommandTest to align with new model

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -184,6 +184,16 @@ public class AddCommandTest {
         public void updateFilteredEventList(Predicate<Event> predicate) {
             throw new AssertionError("This method should not be called.");
         }
+
+        @Override
+        public boolean isVendorAssignedToEvent(Vendor vendor, Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void assignVendorToEvent(Vendor vendor, Event event) {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
