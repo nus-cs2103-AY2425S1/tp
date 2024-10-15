e4ce9464ae84e99674342a79ccd191e3ce901ca4
wrjgold <trexwrj@gmail.com>
2024-10-15 14:21:37 +0800

    Add predicate to check matching celebrity name in Event

---
+++ src/main/java/seedu/address/model/event/EventCelebrityMatchesKeywordPredicate.java
@@ -0,0 +1,39 @@
+package seedu.address.model.event;
+
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+public class EventCelebrityMatchesKeywordPredicate implements Predicate<Event> {
+    private final String keyword;
+
+    public EventCelebrityMatchesKeywordPredicate(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public boolean test(Event event) {
+        return keyword.equalsIgnoreCase(event.getCelebrity().getName().toString());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EventCelebrityMatchesKeywordPredicate)) {
+            return false;
+        }
+
+        EventCelebrityMatchesKeywordPredicate otherEventCelebrityContainsKeywordPredicate =
+                (EventCelebrityMatchesKeywordPredicate) other;
+        return keyword.equals(otherEventCelebrityContainsKeywordPredicate.keyword);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keyword", keyword).toString();
+    }
+}
