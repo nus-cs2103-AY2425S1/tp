6b539d24367c2e904f436f9928a4ae5e242bbe72
rimgik <karon503.cpp@gmail.com>
2024-10-15 15:41:06 +0800

    Update test case and fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/AddOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddOrderCommandTest.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -22,7 +22,8 @@ class AddOrderCommandTest {
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         String name = "cake";
         expectedModel.addOrder(new Order(name));
-        assertCommandSuccess(new AddOrderCommand(name), model, String.format(AddOrderCommand.MESSAGE_SUCCESS, name), expectedModel);
+        assertCommandSuccess(new AddOrderCommand(name), model,
+                String.format(AddOrderCommand.MESSAGE_SUCCESS, name), expectedModel);
     }
 
     @Test
--- src/test/java/seedu/address/logic/commands/ListOrderCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListOrderCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
