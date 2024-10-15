9b7487d673b4c5501d15d206ec91e867bfc727a8
LeeZeHao <zehaolee2004@gmail.com>
2024-10-16 04:25:16 +0800

    Add FindProjectCommandParser.java and related tests

---
+++ src/test/java/seedu/address/logic/parser/FindProjectCommandParserTest.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.FindProjectCommand;
+import seedu.address.model.project.ProjectNameContainsKeywordsPredicate;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class FindProjectCommandParserTest {
+
+    private FindProjectCommandParser parser = new FindProjectCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindProjectCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindProjectCommand() {
+        // no leading and trailing whitespaces
+        FindProjectCommand expectedFindProjectCommand =
+                new FindProjectCommand(new ProjectNameContainsKeywordsPredicate(Arrays.asList("Alpha", "Beta")));
+        assertParseSuccess(parser, "Alpha Beta", expectedFindProjectCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, " \n Alpha \n \t Beta  \t", expectedFindProjectCommand);
+    }
+
+}
