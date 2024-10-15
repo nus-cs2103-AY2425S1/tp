57bfcb7e0021e510a6a3639782770ff716097ad3
sarahteonin <E1249329@u.nus.edu>
2024-10-16 00:28:30 +0800

    Update imports

--- src/test/java/seedu/address/model/person/ScheduleTest.java
+++ src/test/java/seedu/address/model/person/ScheduleTest.java
@@ -1,7 +1,8 @@
 package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
