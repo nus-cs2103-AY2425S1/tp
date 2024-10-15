78f885d61c8f9909a2c651b192d533e0fa55d6da
Ming Jun <evan.zhangmingjun@gmail.com>
2024-10-16 02:11:56 +0800

    chore: remove unused imports and variable

--- src/test/java/careconnect/logic/commands/ViewCommandTest.java
+++ src/test/java/careconnect/logic/commands/ViewCommandTest.java
@@ -2,26 +2,18 @@ package careconnect.logic.commands;
 
 import static careconnect.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static careconnect.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static careconnect.testutil.TypicalPersons.getTypicalAddressBook;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import careconnect.commons.core.index.Index;
-import careconnect.model.Model;
-import careconnect.model.ModelManager;
-import careconnect.model.UserPrefs;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
  * {@code ViewCommand}.
  */
 public class ViewCommandTest {
-
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
     @Test
     public void equals() {
         ViewCommand command = new ViewCommand(INDEX_FIRST_PERSON);
