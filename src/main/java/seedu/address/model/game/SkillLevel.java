3e9d72ddd61eb267c31eb46ad9a932ff2a7a4c4f
JJtan <95962077+JJtan2002@users.noreply.github.com>
2024-10-15 23:16:20 +0800

    Add SkillLevel in Game

---
+++ src/main/java/seedu/address/model/game/SkillLevel.java
@@ -0,0 +1,73 @@
+package seedu.address.model.game;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Describes the Person's skill level in a Game.
+ */
+public class SkillLevel {
+
+    private static final String MESSAGE_CONSTRAINTS =
+            "Skill Level should not be blank";
+
+    /*
+     * Regex expression matches Strings that contain at least one non-whitespace character.
+     */
+    private static final String VALIDATION_REGEX = "^(?!\\s*$).+";
+
+    private final String skillLevel;
+
+    /**
+     * Constructs a {@code SkillLevel}.
+     *
+     * @param skillLevel a valid skill level or rank.
+     */
+    public SkillLevel(String skillLevel) {
+        requireNonNull(skillLevel);
+        checkArgument(isValidSkillLevel(skillLevel), MESSAGE_CONSTRAINTS);
+        this.skillLevel = skillLevel;
+    }
+
+    /**
+     * Returns true if a given string is a valid SkillLevel.
+     */
+    public static boolean isValidSkillLevel(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    /**
+     * Getter for skillLevel field.
+     */
+    public String getSkillLevel() {
+        return skillLevel;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof SkillLevel)) {
+            return false;
+        }
+
+        SkillLevel otherName = (SkillLevel) other;
+        return skillLevel.equals(otherName.skillLevel);
+    }
+
+    @Override
+    public int hashCode() {
+        return skillLevel.hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    public String toString() {
+        return skillLevel;
+    }
+
+}
