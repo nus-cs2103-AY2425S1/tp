b60b01fabac9ead4e01e3ff75b0bf808f860fe24
myrilla12 <myrilla12@gmail.com>
2024-10-15 20:51:19 +0800

    Update existing tests

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -16,7 +16,7 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_AGE = "23";
+    public static final String DEFAULT_AGE = "24";
     public static final String DEFAULT_SEX = "Female";
 
     private Name name;
