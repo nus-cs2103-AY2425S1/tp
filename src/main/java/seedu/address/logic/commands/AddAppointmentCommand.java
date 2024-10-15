2244b7d09446ae7480b9bed51730b1919866553f
Vangmay Sachan <vangmay.sachan16@gmail.com>
2024-10-15 23:54:19 +0800

    Add addAppointmentCommand

---
+++ src/main/java/seedu/address/logic/commands/AddAppointmentCommand.java
@@ -0,0 +1,118 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT_TYPE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SICKNESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MEDICINE;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.appointment.Appointment;
+
+/**
+ * Adds an appointment to the appointment book.
+ */
+public class AddAppointmentCommand extends AddCommand {
+    public static final String COMMAND_WORD = "add appt";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds an appointment to the appointment book. "
+            + "Parameters: "
+            + PREFIX_APPOINTMENT_TYPE + "Appointment_TYPE"
+            + PREFIX_PERSON_ID + "PersonId"
+            + PREFIX_SICKNESS + "Sickness"
+            + PREFIX_MEDICINE + "Medicine"
+            + "Example: " + COMMAND_WORD
+            + PREFIX_APPOINTMENT_TYPE + "Check up"
+            + PREFIX_PERSON_ID + "1"
+            + PREFIX_SICKNESS + "Common Cold"
+            + PREFIX_MEDICINE + "Paracetamol";
+
+    public static final String MESSAGE_SUCCESS = "New appointment added: %1$s";
+    public static final String MESSAGE_DUPLICATE_APPOINTMENT = "This appointment already exists in the address book";
+
+    private final Appointment toAdd;
+
+    /**
+     * Creates an AddAppointmentCommand to add the specified {@code Appointment}
+     */
+    public AddAppointmentCommand(Appointment appointment) {
+        requireNonNull(appointment);
+        toAdd = appointment;
+    }
+
+    /*
+     * Checks if the entity being added to model already exists.
+     */
+    @Override
+    protected boolean alreadyExists(Model model) {
+        return model.hasAppointment(toAdd);
+    };
+
+    /*
+     * Adds the entity to the model.
+     */
+    @Override
+    protected void addEntity(Model model) {
+        model.addAppointment(toAdd);
+    };
+
+    /*
+     * Returns success message to display upon adding entity.
+     */
+    @Override
+    protected String getSuccessMessage() {
+        return MESSAGE_SUCCESS;
+    };
+
+    /*
+     * Returns the message to display when there is a duplicate.
+     */
+    @Override
+    protected String getDuplicateEntityMessage() {
+        return MESSAGE_DUPLICATE_APPOINTMENT;
+    };
+
+    /**
+     * Formats the entity for displaying in the success message.
+     */
+    @Override
+    protected String formatEntity() {
+        return Messages.formatAppointment(toAdd);
+    };
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (alreadyExists(model)) {
+            throw new CommandException(getDuplicateEntityMessage());
+        }
+
+        addEntity(model);
+        return new CommandResult(String.format(getSuccessMessage(), formatEntity()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddAppointmentCommand otherAddAppointmentCommand)) {
+            return false;
+        }
+
+        return toAdd.equals(otherAddAppointmentCommand.toAdd);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("toAdd", toAdd)
+                .toString();
+    }
+}
--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,6 +12,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 
+    public static final Prefix PREFIX_PERSON_ID = new Prefix("i/");
     public static final Prefix PREFIX_APPOINTMENT_TYPE = new Prefix("t/");
     public static final Prefix PREFIX_MEDICINE = new Prefix("m/");
     public static final Prefix PREFIX_SICKNESS = new Prefix("s/");
