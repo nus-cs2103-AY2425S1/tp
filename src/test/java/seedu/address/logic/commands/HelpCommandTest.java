198954357411faefc045b35bd72160010c04e39d
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-10-16 01:07:44 +0800

    Add test cases for help

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
 
@@ -16,5 +17,11 @@ public class HelpCommandTest {
     public void execute_help_success() {
         CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false, false);
         assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void helpMessageUsage_correctFormat() {
+        String expectedMessageUsage = "help: Shows program usage instructions.\nExample: help";
+        assertEquals(expectedMessageUsage, HelpCommand.MESSAGE_USAGE);
     }
 }
