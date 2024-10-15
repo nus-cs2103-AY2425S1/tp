1cc1558a1da7b294382a5e65cbadaf78a9ea9347
C5hives <e1122945@u.nus.edu>
2024-10-15 17:22:53 +0800

    Add EventUtils to generate test commands

--- src/test/java/seedu/address/testutil/EventUtil.java
+++ src/test/java/seedu/address/testutil/EventUtil.java
@@ -1,2 +1,41 @@
-package seedu.address.testutil;public class EventUtil {
+package seedu.address.testutil;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_VENDOR;
+
+import java.util.Set;
+
+import seedu.address.logic.commands.CreateVendorCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.model.event.Event;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.vendor.Vendor;
+
+/**
+ * A utility class for Event.
+ */
+public class EventUtil {
+
+    /**
+     * Returns a create event command string for adding the {@code event}.
+     */
+    public static String getCreateEventCommand(Event event) {
+        return CreateVendorCommand.COMMAND_WORD + " " + PREFIX_EVENT + " " + getEventDetails(event);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code vendor}'s details.
+     */
+    public static String getEventDetails(Event event) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + event.getName().fullName + " ");
+        sb.append(PREFIX_DATE + event.getDate().toString() + " ");
+        return sb.toString();
+    }
+
 }
