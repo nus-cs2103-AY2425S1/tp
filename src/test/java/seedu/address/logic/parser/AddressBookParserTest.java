13e6d37d0489f14bb1e4ee2bb1306a0fe6fb07cb
rimgik <109288765+rimgik@users.noreply.github.com>
2024-10-15 18:09:44 +0800

    Update AddressBookParserTest.java

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -9,6 +9,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
