b7227eb2a7e1e5126b04c030df4e6e32dae57b0a
Zac <zacheryngky@gmail.com>
2024-10-15 21:14:34 +0800

    Rename FindCommand to FindPersonCommand

--- src/main/java/seedu/address/logic/commands/FindPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/FindPersonCommand.java
@@ -11,9 +11,9 @@ import seedu.address.model.commons.NameContainsKeywordsPredicate;
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
-public class FindCommand extends Command {
+public class FindPersonCommand extends Command {
 
-    public static final String COMMAND_WORD = "find";
+    public static final String COMMAND_WORD = "findp";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
@@ -22,7 +22,7 @@ public class FindCommand extends Command {
 
     private final NameContainsKeywordsPredicate predicate;
 
-    public FindCommand(NameContainsKeywordsPredicate predicate) {
+    public FindPersonCommand(NameContainsKeywordsPredicate predicate) {
         this.predicate = predicate;
     }
 
@@ -41,12 +41,12 @@ public class FindCommand extends Command {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof FindCommand)) {
+        if (!(other instanceof FindPersonCommand)) {
             return false;
         }
 
-        FindCommand otherFindCommand = (FindCommand) other;
-        return predicate.equals(otherFindCommand.predicate);
+        FindPersonCommand otherFindPersonCommand = (FindPersonCommand) other;
+        return predicate.equals(otherFindPersonCommand.predicate);
     }
 
     @Override
