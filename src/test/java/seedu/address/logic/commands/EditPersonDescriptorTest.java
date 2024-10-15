e046f668d8fde83107884d44508cd1ca215738e5
rayray39 <raynerlimfy@gmail.com>
2024-10-15 20:45:35 +0800

    Update VALID_EMAIL_BOB to VALID_DATEOFLASTVISIT_BOB.

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATEOFLASTVISIT_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
@@ -57,7 +58,7 @@ public class EditPersonDescriptorTest {
         assertFalse(DESC_AMY.equals(editedAmy));
 
         // different dateOfLastVisit -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withDateOfLastVisit(VALID_EMAIL_BOB).build();
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withDateOfLastVisit(VALID_DATEOFLASTVISIT_BOB).build();
         assertFalse(DESC_AMY.equals(editedAmy));
     }
 
