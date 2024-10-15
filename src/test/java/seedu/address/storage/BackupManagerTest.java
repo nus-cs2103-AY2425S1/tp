9bed68c15ee5ecab3f305cf7aa14797f5fc81d7d
JellyPenguinnn <>
2024-10-15 14:38:12 +0800

    Optimize imports

--- src/test/java/seedu/address/storage/BackupManagerTest.java
+++ src/test/java/seedu/address/storage/BackupManagerTest.java
@@ -1,11 +1,19 @@
 package seedu.address.storage;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test class for BackupManager.
