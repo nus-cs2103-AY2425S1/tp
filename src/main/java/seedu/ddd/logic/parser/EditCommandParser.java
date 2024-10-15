714b66f7ad5f74c39f4cbb9e7782efb6dc885c6d
Leslie Yip <leslieyip02@gmail.com>
2024-10-15 09:16:29 +0800

    Add abilit to edit id

--- src/main/java/seedu/ddd/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/ddd/logic/parser/EditCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.ddd.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_ID;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.ddd.logic.parser.CliSyntax.PREFIX_SERVICE;
@@ -36,7 +37,7 @@ public class EditCommandParser implements Parser<EditCommand> {
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                PREFIX_ADDRESS, PREFIX_DATE, PREFIX_SERVICE, PREFIX_TAG);
+                PREFIX_ADDRESS, PREFIX_DATE, PREFIX_SERVICE, PREFIX_TAG, PREFIX_ID);
 
         Index index;
 
@@ -49,7 +50,7 @@ public class EditCommandParser implements Parser<EditCommand> {
         // Should not contain duplicates prefixes.
         // Should either specify date for client or service for vendor.
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_DATE,
-                PREFIX_SERVICE);
+                PREFIX_SERVICE, PREFIX_ID);
         argMultimap.verifyNoExclusivePrefixesFor(PREFIX_DATE, PREFIX_SERVICE);
 
         EditContactDescriptor editContactDescriptor = parseArgumentMultimap(argMultimap);
@@ -86,6 +87,9 @@ public class EditCommandParser implements Parser<EditCommand> {
             editContactDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
         }
         parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editContactDescriptor::setTags);
+        if (argMultimap.getValue(PREFIX_ID).isPresent()) {
+            editContactDescriptor.setId(ParserUtil.parseId(argMultimap.getValue(PREFIX_ID).get()));
+        }
 
         if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
             EditClientDescriptor editClientDescriptor = new EditClientDescriptor(editContactDescriptor);
