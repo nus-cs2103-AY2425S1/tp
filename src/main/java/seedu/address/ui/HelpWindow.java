8cb6ac739eec473dff3b2c2c27e4dd318fb3411a
btbrandon <tzhbrandon@gmail.com>
2024-10-15 14:58:37 +0800

    Style changes

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -19,55 +19,56 @@ public class HelpWindow extends UiPart<Stage> {
 
     public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
     public static final String HELP_MESSAGE =
-            "============================================================================================\n" +
-            "   H E L P   M E N U\n" +
-            "============================================================================================\n" +
-            "\n1. Add Student\n" +
-            "   - Purpose: Adds a student and their details to the address book.\n" +
-            "   - Command Format:\n" +
-            "       add id/ [STUDENT_ID] n/ [STUDENT_NAME] p/ [PHONE_NUMBER] a/ [ADDRESS] c/ [COURSE] t/ [TAGS]\n" +
-            "   - Example:\n" +
-            "       add id/ 12345678 n/ John Doe p/ 99999999 a/ 123 Jane Doe Road c/ Computer Science t/ Student\n" +
-            "\n2. Add a Student Grade\n" +
-            "   - Purpose: Adds a grade for a student in a module.\n" +
-            "   - Command Format:\n" +
-            "       grade id/ [STUDENT_ID] m/ [MODULE] g/ [GRADE]\n" +
-            "   - Example:\n" +
-            "       grade id/ 12345678 m/ CS2103T g/ A\n" +
-            "\n3. Edit Student\n" +
-            "   - Purpose: Edits a student's details according to the fields specified.\n" +
-            "   - Command Format:\n" +
-            "       edit [STUDENT_ID] [FIELD_TO_EDIT_PREFIX] [NEW_VALUE]\n" +
-            "   - Editable Fields:\n" +
-            "       n/ [STUDENT_NAME], p/ [PHONE_NUMBER], e/ [EMAIL], a/ [ADDRESS], c/ [COURSE], t/ [TAG]\n" +
-            "   - Example:\n" +
-            "       edit 12345678 n/ Jane Doe p/ 88888888 e/ janedoe@gmail.com a/ 456 John Doe Road c/ Physics t/ Student\n" +
-            "\n4. Delete Student\n" +
-            "   - Purpose: Removes a student from the address book.\n" +
-            "   - Command Format:\n" +
-            "       delete id/ [STUDENT_ID]\n" +
-            "   - Example:\n" +
-            "       delete id/ 12345678\n" +
-            "\n5. List Students\n" +
-            "   - Purpose: Displays all students currently stored in the address book.\n" +
-            "   - Command Format:\n" +
-            "       list\n" +
-            "   - Example:\n" +
-            "       list\n" +
-            "\n6. Clear Data\n" +
-            "   - Purpose: Clears all student data from the address book.\n" +
-            "   - Command Format:\n" +
-            "       clear\n" +
-            "   - Example:\n" +
-            "       clear\n" +
-            "\n7. Exit Application\n" +
-            "   - Purpose: Exits the application.\n" +
-            "   - Command Format:\n" +
-            "       exit\n" +
-            "   - Example:\n" +
-            "       exit\n" +
-            "\n============================================================================================\n" +
-            "For more details, refer to the user guide:";
+            "============================================================================================\n"
+            + "   H E L P   M E N U\n"
+            + "============================================================================================\n"
+            + "\n1. Add Student\n"
+            + "   - Purpose: Adds a student and their details to the address book.\n"
+            + "   - Command Format:\n"
+            + "       add id/ [STUDENT_ID] n/ [STUDENT_NAME] p/ [PHONE_NUMBER] a/ [ADDRESS] c/ [COURSE] t/ [TAGS]\n"
+            + "   - Example:\n"
+            + "       add id/ 12345678 n/ John Doe p/ 99999999 a/ 123 Jane Doe Road c/ Computer Science t/ Student\n"
+            + "\n2. Add a Student Grade\n"
+            + "   - Purpose: Adds a grade for a student in a module.\n"
+            + "   - Command Format:\n"
+            + "       grade id/ [STUDENT_ID] m/ [MODULE] g/ [GRADE]\n"
+            + "   - Example:\n"
+            + "       grade id/ 12345678 m/ CS2103T g/ A\n"
+            + "\n3. Edit Student\n"
+            + "   - Purpose: Edits a student's details according to the fields specified.\n"
+            + "   - Command Format:\n"
+            + "       edit [STUDENT_ID] [FIELD_TO_EDIT_PREFIX] [NEW_VALUE]\n"
+            + "   - Editable Fields:\n"
+            + "       n/ [STUDENT_NAME], p/ [PHONE_NUMBER], e/ [EMAIL], a/ [ADDRESS], c/ [COURSE], t/ [TAG]\n"
+            + "   - Example:\n"
+            + "       edit 12345678 n/ Jane Doe p/ 88888888 e/ janedoe@gmail.com "
+            +         "a/ 456 John Doe Road c/ Physics t/ Student\n"
+            + "\n4. Delete Student\n"
+            + "   - Purpose: Removes a student from the address book.\n"
+            + "   - Command Format:\n"
+            + "       delete id/ [STUDENT_ID]\n"
+            + "   - Example:\n"
+            + "       delete id/ 12345678\n"
+            + "\n5. List Students\n"
+            + "   - Purpose: Displays all students currently stored in the address book.\n"
+            + "   - Command Format:\n"
+            + "       list\n"
+            + "   - Example:\n"
+            + "       list\n"
+            + "\n6. Clear Data\n"
+            + "   - Purpose: Clears all student data from the address book.\n"
+            + "   - Command Format:\n"
+            + "       clear\n"
+            + "   - Example:\n"
+            + "       clear\n"
+            + "\n7. Exit Application\n"
+            + "   - Purpose: Exits the application.\n"
+            + "   - Command Format:\n"
+            + "       exit\n"
+            + "   - Example:\n"
+            + "       exit\n"
+            + "\n============================================================================================\n"
+            + "For more details, refer to the user guide:";
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
@@ -92,6 +93,13 @@ public class HelpWindow extends UiPart<Stage> {
     }
 
     /**
+     * Creates a new HelpWindow.
+     */
+    public HelpWindow() {
+        this(new Stage());
+    }
+
+    /**
      * Opens the user guide in the default web browser when the hyperlink is clicked.
      *
      * @param event The mouse event triggered when the hyperlink is clicked.
@@ -104,13 +112,6 @@ public class HelpWindow extends UiPart<Stage> {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Creates a new HelpWindow.
-     */
-    public HelpWindow() {
-        this(new Stage());
     }
 
     /**
