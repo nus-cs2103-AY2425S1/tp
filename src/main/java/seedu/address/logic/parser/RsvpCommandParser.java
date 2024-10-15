15b27ba6dd01e404f31ae6776d22d621ecc8f17a
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-10-15 20:46:22 +0800

    Ensure RSVP not showing error when input command - Left implementation to work on

--- src/main/java/seedu/address/logic/parser/RsvpCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RsvpCommandParser.java
@@ -4,6 +4,8 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.RsvpCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Parses input arguments and creates a new RsvpCommand object
  */
@@ -13,29 +15,21 @@ public class RsvpCommandParser implements Parser<RsvpCommand> {
      * Parses the given {@code String} of arguments in the context of the RsvpCommand
      */
     public RsvpCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(RsvpCommand.MESSAGE_INVALID_FORMAT));
-        }
-
-
         // Split the trimmedArgs into commandWord and index
-        String[] splitArgs = trimmedArgs.split("\\s+");
+        String[] splitArgs = args.split("\\s+");
         if (splitArgs.length != 2) {
             throw new ParseException(RsvpCommand.MESSAGE_INVALID_FORMAT);
         }
 
-        // Check if the index is a valid number
         String commandWord = splitArgs[0];
         Index index;
         try {
             index = ParserUtil.parseIndex(splitArgs[1]);
+            boolean isRsvp = commandWord.equals(RsvpCommand.RSVP_COMMAND_WORD);
+            return new RsvpCommand(index, isRsvp);
         } catch (ParseException pe) {
             throw new ParseException(RsvpCommand.MESSAGE_INVALID_INDEX);
         }
 
-        boolean isRsvp = commandWord.equals(RsvpCommand.RSVP_COMMAND_WORD);
-        return new RsvpCommand(index, isRsvp);
     }
 }
