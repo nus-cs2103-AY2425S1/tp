6f8df7712f2e7f64c071878b4b1d3f47fdd89760
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-10-15 22:58:41 +0800

    Fix ArchiveCommand test to initialise addressbook file

--- src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
@@ -4,14 +4,29 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.util.FileUtil;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class ArchiveCommandTest {
+    @BeforeEach
+    public void setUp() throws IOException {
+        FileUtil.createIfMissing(new UserPrefs().getAddressBookFilePath());
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        Files.deleteIfExists(new UserPrefs().getAddressBookFilePath());
+    }
+
     @Test
     public void execute_emptyAddressBook_success() {
         Model model = new ModelManager();
