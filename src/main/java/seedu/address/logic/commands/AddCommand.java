e62175cd4ca977311ec4966438692e1c0d70b595
myrilla12 <myrilla12@gmail.com>
2024-10-15 16:33:48 +0800

    Update AddCommand message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -22,14 +22,16 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
+            + PREFIX_AGE + "AGE "
+            + PREFIX_SEX + "SEX "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_SEX + "Male "
             + PREFIX_AGE + "23 "
+            + PREFIX_SEX + "Male "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
