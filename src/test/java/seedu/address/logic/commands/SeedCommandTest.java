a8bd78b4d3ae4ee25d210a426f017e2bda3300d6
liauzhanyi <liauzhanyi@gmail.com>
2024-10-15 17:29:21 +0800

    test: Add test for seed command

--- src/test/java/seedu/address/logic/commands/SeedCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SeedCommandTest.java
@@ -1,18 +1,32 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.util.SampleDataUtil;
 
 public class SeedCommandTest {
 
     private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(new AddressBook(), new UserPrefs());
+        ReadOnlyAddressBook sampleData = SampleDataUtil.getSampleAddressBook();
+        expectedModel = new ModelManager(sampleData, new UserPrefs());
+    }
 
     @Test
-    public void execute() {
-        assertCommandFailure(new SeedCommand(), model, SeedCommand.MESSAGE_NOT_IMPLEMENTED_YET);
+    public void execute_emptySocialBook_showPopulatedSocialBook() {
+        assertCommandSuccess(new SeedCommand(), model, SeedCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
 }
