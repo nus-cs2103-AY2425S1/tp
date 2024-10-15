92cfc253ff20273c9dfd163419bcd98110c5e368
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-10-16 03:32:44 +0800

    Update and add tests for FilterCommandParser

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -4,10 +4,16 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.predicates.AddressContainsSubstringPredicate;
+import seedu.address.model.person.predicates.CombinedPredicate;
 import seedu.address.model.person.predicates.EmailContainsSubstringPredicate;
 import seedu.address.model.person.predicates.JobContainsSubstringPredicate;
 import seedu.address.model.person.predicates.NameContainsSubstringPredicate;
@@ -20,60 +26,98 @@ public class FilterCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_validArgs_returnsFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new NameContainsSubstringPredicate("Alice"));
-        assertParseSuccess(parser, " n/ Alice", expectedFilterCommand);
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new NameContainsSubstringPredicate("Alice"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
 
-        // multiple whitespaces between flag and substring
-        assertParseSuccess(parser, " n/ \n  Alice \t", expectedFilterCommand);
+        assertParseSuccess(parser, " n/ Alice", expectedFilterCommand);
     }
 
     @Test
     public void parse_addressFlag_returnsAddressFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new AddressContainsSubstringPredicate("Block 123"));
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new AddressContainsSubstringPredicate("Block 123"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
         assertParseSuccess(parser, " a/ Block 123", expectedFilterCommand);
     }
 
     @Test
     public void parse_emailFlag_returnsEmailFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new EmailContainsSubstringPredicate("alice@hello.com"));
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new EmailContainsSubstringPredicate("alice@hello.com"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
         assertParseSuccess(parser, " e/ alice@hello.com", expectedFilterCommand);
     }
 
     @Test
     public void parse_jobFlag_returnsRemarkFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new JobContainsSubstringPredicate("Software Engineer"));
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new JobContainsSubstringPredicate("Software Engineer"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
         assertParseSuccess(parser, " j/ Software Engineer", expectedFilterCommand);
     }
 
     @Test
     public void parse_nameFlag_returnsNameFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new NameContainsSubstringPredicate("Alice"));
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new NameContainsSubstringPredicate("Alice"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
         assertParseSuccess(parser, " n/ Alice", expectedFilterCommand);
     }
 
-
     @Test
     public void parse_phoneFlag_returnsPhoneFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new PhoneContainsSubstringPredicate("91112222"));
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new PhoneContainsSubstringPredicate("91112222"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
         assertParseSuccess(parser, " p/ 91112222", expectedFilterCommand);
     }
 
 
     @Test
     public void parse_remarkFlag_returnsRemarkFilterCommand() {
-        FilterCommand expectedFilterCommand =
-                new FilterCommand(new RemarkContainsSubstringPredicate("is a celebrity"));
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new RemarkContainsSubstringPredicate("is a celebrity"));
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
         assertParseSuccess(parser, " r/ is a celebrity", expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_validMultipleArgs_returnsFilterCommand() {
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new NameContainsSubstringPredicate("Alice"));
+        expectedPredicates.add(new PhoneContainsSubstringPredicate("91112222"));
+        expectedPredicates.add(new EmailContainsSubstringPredicate("alice@example.com"));
+        expectedPredicates.add(new AddressContainsSubstringPredicate("Block 123"));
+        expectedPredicates.add(new JobContainsSubstringPredicate("Software Engineer"));
+        expectedPredicates.add(new RemarkContainsSubstringPredicate("is a celebrity"));
+
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
+        assertParseSuccess(parser, " n/ Alice p/ 91112222 e/ alice@example.com a/ Block 123 "
+                + "j/ Software Engineer r/ is a celebrity", expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_validMultipleArgsWithWhitespace_returnsFilterCommand() {
+        List<Predicate<Person>> expectedPredicates = new ArrayList<>();
+        expectedPredicates.add(new NameContainsSubstringPredicate("Alice"));
+        expectedPredicates.add(new PhoneContainsSubstringPredicate("91112222"));
+
+        FilterCommand expectedFilterCommand = new FilterCommand(new CombinedPredicate(expectedPredicates));
+
+        assertParseSuccess(parser, " n/  \t Alice \n p/  \t 91112222 ", expectedFilterCommand);
     }
 }
