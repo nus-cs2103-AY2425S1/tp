1c23a021366822f5d9bdc67849b0abf964033a6c
Johnwz123 <112536096+Johnwz123@users.noreply.github.com>
2024-10-15 23:02:28 +0800

    Fix style

--- src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ArchiveCommandTest.java
@@ -5,7 +5,6 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
