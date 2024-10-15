e676e05b3e0b938f78b8d27353b5711c7d4dc79f
ITZXITZX <isaacteozixian@hotmail.com>
2024-10-16 03:41:34 +0800

    Update AddressBookParser to handle DeletePropertyToBuyCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,18 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddPropertyToBuyCommand;
-import seedu.address.logic.commands.AddPropertyToSellCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindNameCommand;
-import seedu.address.logic.commands.FindPhoneNumberCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -86,6 +75,9 @@ public class AddressBookParser {
             return new AddPropertyToSellParser().parse(arguments);
 
         case AddPropertyToBuyCommand.COMMAND_WORD:
+            return new AddPropertyToBuyParser().parse(arguments);
+
+        case DeletePropertyToBuyCommand.COMMAND_WORD:
             return new AddPropertyToBuyParser().parse(arguments);
 
         default:
