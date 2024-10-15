a608f295596e34f35e2b07371e50a7076aa1854d
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-15 15:49:53 +0800

    Add new test cases for Tag related classes

--- src/test/java/seedu/address/logic/parser/TagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/TagCommandParserTest.java
@@ -1,12 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.TagCommand;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
@@ -16,9 +18,45 @@ public class TagCommandParserTest {
     @Test
     public void parse_oneTag_success() {
         TagCommandParser parser = new TagCommandParser();
-        String userInput = INDEX_FIRST_PERSON.getOneBased() + " " + "t/testTag1";
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + " t/testTag1";
         Set<Tag> addedTags = SampleDataUtil.getTagSet("testTag1");
         TagCommand expectedCommand = new TagCommand(INDEX_FIRST_PERSON, addedTags);
         assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_threeTags_success() {
+        TagCommandParser parser = new TagCommandParser();
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + " t/ testTag1 testTag2 testTag3";
+        Set<Tag> addedTags = SampleDataUtil.getTagSet("testTag1", "testTag2", "testTag3");
+        TagCommand expectedCommand = new TagCommand(INDEX_FIRST_PERSON, addedTags);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_noPrefix_failure() {
+        TagCommandParser parser = new TagCommandParser();
+        String userInput = String.valueOf(INDEX_FIRST_PERSON.getOneBased());
+        String expectedMessage = String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, TagCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_duplicatePrefixes_failure() {
+        TagCommandParser parser = new TagCommandParser();
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + " t/tag1 t/tag2 t/tag3";
+        String expectedMessage = String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT,
+                "Please only include one prefix t/ !");
+
+        assertParseFailure(parser, userInput, expectedMessage);
+    }
+
+    @Test
+    public void parse_emptyTagArgument_failure() {
+        TagCommandParser parser = new TagCommandParser();
+        String userInput = INDEX_FIRST_PERSON.getOneBased() + " t/";
+        String expectedMessage = String.format(Messages.MESSAGE_INVALID_COMMAND_FORMAT, "No tags specified!");
+
+        assertParseFailure(parser, userInput, expectedMessage);
     }
 }
--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,8 +1,12 @@
 package seedu.address.model.tag;
 
 import static seedu.address.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TagTest {
 
@@ -21,6 +25,13 @@ public class TagTest {
     public void isValidTagName() {
         // null tag name
         assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+    }
+
+    @Test
+    public void stringToTagSet_emptyString_emptySet() {
+        String userInput = "";
+        Set<Tag> tagSet = Tag.stringToTagSet(userInput);
+        assertEquals(tagSet, new HashSet<Tag>());
     }
 
 }
