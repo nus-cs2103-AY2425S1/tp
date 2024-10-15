637590a6523d9cf21a617306da5769f95b357a0c
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-10-15 20:16:33 +0800

    Add tests for parsing prefixes in filter command

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -7,7 +7,12 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FilterCommand;
+import seedu.address.model.person.predicates.AddressContainsSubstringPredicate;
+import seedu.address.model.person.predicates.EmailContainsSubstringPredicate;
+import seedu.address.model.person.predicates.JobContainsSubstringPredicate;
 import seedu.address.model.person.predicates.NameContainsSubstringPredicate;
+import seedu.address.model.person.predicates.PhoneContainsSubstringPredicate;
+import seedu.address.model.person.predicates.RemarkContainsSubstringPredicate;
 
 public class FilterCommandParserTest {
 
@@ -26,5 +31,49 @@ public class FilterCommandParserTest {
 
         // multiple whitespaces between flag and substring
         assertParseSuccess(parser, " n/ \n  Alice \t", expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_addressFlag_returnsAddressFilterCommand() {
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new AddressContainsSubstringPredicate("Block 123"));
+        assertParseSuccess(parser, " a/ Block 123", expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_emailFlag_returnsEmailFilterCommand() {
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new EmailContainsSubstringPredicate("alice@hello.com"));
+        assertParseSuccess(parser, " e/ alice@hello.com", expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_jobFlag_returnsRemarkFilterCommand() {
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new JobContainsSubstringPredicate("Software Engineer"));
+        assertParseSuccess(parser, " j/ Software Engineer", expectedFilterCommand);
+    }
+
+    @Test
+    public void parse_nameFlag_returnsNameFilterCommand() {
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new NameContainsSubstringPredicate("Alice"));
+        assertParseSuccess(parser, " n/ Alice", expectedFilterCommand);
+    }
+
+
+    @Test
+    public void parse_phoneFlag_returnsPhoneFilterCommand() {
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new PhoneContainsSubstringPredicate("91112222"));
+        assertParseSuccess(parser, " p/ 91112222", expectedFilterCommand);
+    }
+
+
+    @Test
+    public void parse_remarkFlag_returnsRemarkFilterCommand() {
+        FilterCommand expectedFilterCommand =
+                new FilterCommand(new RemarkContainsSubstringPredicate("is a celebrity"));
+        assertParseSuccess(parser, " r/ is a celebrity", expectedFilterCommand);
     }
 }
