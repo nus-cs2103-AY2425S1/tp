9dcb937c58a02e7a4a74ce9d67033c564528b4dd
Anvita2110 <e1070535@u.nus.edu>
2024-10-16 03:20:00 +0800

    add test code for help command

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -25,19 +25,19 @@ public class HelpCommandTest {
         assert commandResult.isShowHelp() : "Expected help window to be shown";
     }
 
-    @Test
-    public void messageUsage_isCorrect() {
-        String expectedMessageUsage = "help: Shows program usage instructions.\n"
-                + "Example: help";
-        assertEquals(expectedMessageUsage, HelpCommand.MESSAGE_USAGE, "MESSAGE_USAGE is not correct");
-    }
-
     // Test to verify SHOWING_HELP_MESSAGE variable
     @Test
     public void showingHelpMessage_isCorrect() {
         String expectedHelpMessage = "Opened help window with link to user guide.";
         assertEquals(expectedHelpMessage, HelpCommand.SHOWING_HELP_MESSAGE,
                 "SHOWING_HELP_MESSAGE is not correct");
+    }
+
+    @Test
+    public void messageUsage_isCorrect() {
+        String expectedMessageUsage = "help: Shows program usage instructions.\n"
+                + "Example: help";
+        assertEquals(expectedMessageUsage, HelpCommand.MESSAGE_USAGE, "MESSAGE_USAGE is not correct");
     }
 
 }
