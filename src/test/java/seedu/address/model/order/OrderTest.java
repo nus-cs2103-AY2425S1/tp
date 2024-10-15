2b4239d8cbbdd53fb7783ca4638fcb382d778cd7
rimgik <karon503.cpp@gmail.com>
2024-10-15 14:59:57 +0800

    Update test case for Order

--- src/test/java/seedu/address/model/order/OrderTest.java
+++ src/test/java/seedu/address/model/order/OrderTest.java
@@ -1,11 +1,20 @@
 package seedu.address.model.order;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
+    @Test
+    public void equalsTest() {
+        Order order = new Order("Cake");
+        Order order2 = new Order("Test");
+        Order order3 = new Order("Cake");
+        assertEquals(order, order3);
+        assertNotEquals(order, order2);
+    }
     @Test
     public void toStringTest() {
         Order order = new Order("Cake");
