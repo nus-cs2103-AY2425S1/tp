cc3d202e15dff0e9507b4da2f585b9a2c7785621
Isaac Pang <isaac.pangth@gmail.com>
2024-10-15 21:00:45 +0800

    Add Student class

---
+++ src/main/java/seedu/address/model/person/Student.java
@@ -0,0 +1,70 @@
+package seedu.address.model.person;
+
+import java.util.Objects;
+import java.util.Set;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
+
+public class Student extends Person {
+
+    private final Name parentName;
+    private final Phone parentPhone;
+    private final Email parentEmail;
+
+    public Student(Name name, Phone phone, Email email, Address address, Name parentName, Phone parentPhone,
+            Email parentEmail, Set<Tag> tags) {
+        super(name, phone, email, address, tags);
+        this.parentName = parentName;
+        this.parentPhone = parentPhone;
+        this.parentEmail = parentEmail;
+    }
+
+    public Name getParentName() {
+        return parentName;
+    }
+
+    public Phone getParentPhone() {
+        return parentPhone;
+    }
+
+    public Email getParentEmail() {
+        return parentEmail;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Student)) {
+            return false;
+        }
+
+        Student otherStudent = (Student) other;
+        return super.equals(otherStudent) && parentName.equals(otherStudent.parentName) &&
+                parentPhone.equals(otherStudent.parentPhone) && parentEmail.equals(otherStudent.parentEmail);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getName(), this.getPhone(), this.getEmail(), this.getAddress(),
+                this.getParentName(), this.getParentPhone(), this.getParentEmail(), this.getTags());
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", this.getName())
+                .add("phone", this.getPhone())
+                .add("email", this.getEmail())
+                .add("address", this.getAddress())
+                .add("parentName", this.getParentName())
+                .add("parentPhone", this.getParentPhone())
+                .add("parentEmail", this.getParentEmail())
+                .add("tags", this.getTags())
+                .toString();
+    }
+}
