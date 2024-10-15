cdc528d28db6e51407b0f8b9999cb6d77d1ecbe7
Bryan Chen <68541293+bryancheny@users.noreply.github.com>
2024-10-15 15:08:35 +0800

    Modify MakeupLesson to extend Lesson

--- src/main/java/keycontacts/model/lesson/MakeupLesson.java
+++ src/main/java/keycontacts/model/lesson/MakeupLesson.java
@@ -3,7 +3,7 @@ package keycontacts.model.lesson;
 /**
  * Represents a makeup lesson which happens if a student misses a lesson.
 */
-public class MakeupLesson extends RegularLesson {
+public class MakeupLesson extends Lesson {
 
     public MakeupLesson(Day lessonDay, Time startTime, Time endTime) {
         super(lessonDay, startTime, endTime);
