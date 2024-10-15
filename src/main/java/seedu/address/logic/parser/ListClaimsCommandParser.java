7d44c6daffa94eee531616c5b685bf066fcb5988
yanqiyqh <yanqi.yqh@gmail.com>
2024-10-15 20:12:10 +0800

    Modify ListClaimsCommandParser with reference to DeleteCommandParser

--- src/main/java/seedu/address/logic/parser/ListClaimsCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListClaimsCommandParser.java
@@ -1,25 +1,20 @@
 package seedu.address.logic.parser;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.ListClaimsCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class ListClaimsCommandParser implements Parser<ListClaimsCommand> {
 
     public ListClaimsCommand parse(String args) throws ParseException {
-        requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
-        Index index;
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
-        } catch (IllegalValueException ive) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListClaimsCommand.MESSAGE_USAGE),
-                    ive);
+            Index index = ParserUtil.parseIndex(args);
+            return new ListClaimsCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListClaimsCommand.MESSAGE_USAGE), pe);
         }
-        return new ListClaimsCommand(index);
     }
 }
