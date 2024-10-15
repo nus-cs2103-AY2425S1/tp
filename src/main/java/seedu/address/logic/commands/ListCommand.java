c677c261a056b80ef3c98b839d136bb051cfd4d9
Tan Wen Xuan <>
2024-10-16 03:36:38 +0800

    Improve CLI further, update ListCommand to accommodate for empty list and add more tests in LogicMangerTest

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -11,14 +12,15 @@ import seedu.address.model.Model;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
-
     public static final String MESSAGE_SUCCESS = "These are your clients!";
-
-
+    public static final String MESSAGE_NO_CLIENT_IN_LIST = "You currently have no clients in the list.";
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        if (model.getFilteredPersonList().isEmpty()) {
+            throw new CommandException(MESSAGE_NO_CLIENT_IN_LIST);
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
