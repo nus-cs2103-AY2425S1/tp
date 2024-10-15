0aa85ddc4c55939f17a8256db16a8f3003b2a990
Varun Shukla <vslpahm@gmail.com>
2024-10-15 22:50:57 +0800

    Add application class

---
+++ src/main/java/seedu/internbuddy/model/application/exceptions/ApplicationNotFoundException.java
@@ -0,0 +1,6 @@
+package seedu.internbuddy.model.application.exceptions;
+
+/**
+ * Signals that the operation is unable to find the specified application.
+ */
+public class ApplicationNotFoundException {}
