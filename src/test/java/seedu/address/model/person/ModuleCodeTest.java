b5c54d6ac308c588fbe0fae8d4254842b2f165ab
MAOXIONGKAI <conan7153@gmail.com>
2024-10-15 13:57:45 +0800

    Add test cases for isValidModuleCode method

--- src/test/java/seedu/address/model/person/ModuleCodeTest.java
+++ src/test/java/seedu/address/model/person/ModuleCodeTest.java
@@ -1,9 +1,11 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ModuleCodeTest {
@@ -11,6 +13,49 @@ public class ModuleCodeTest {
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new ModuleCode(null));
+    }
+
+    @Test
+    public void constructor_emptyString_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ModuleCode("");
+        });
+    }
+
+    @Test
+    public void constructor_moduleCodeWithSpace_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ModuleCode("CS 1101S");
+        });
+    }
+
+    @Test
+    public void constructor_moduleCodeWithWrongOrder_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new ModuleCode("1101CS");
+        });
+    }
+
+    @Test
+    public void constructor_validModuleCodeWithNoLetterBehind() {
+        assertDoesNotThrow(() -> {
+            new ModuleCode("MA1522");
+        });
+    }
+
+    @Test
+    public void constructor_validModuleCodeWithLetterBehind() {
+        assertDoesNotThrow(() -> {
+            new ModuleCode("CS1231S");
+        });
+    }
+
+    @Test
+    public void constructor_validModuleCodeWithLowerCaseLetter() {
+        assertDoesNotThrow(() -> {
+            new ModuleCode("ma1522");
+            new ModuleCode("cs1231s");
+        });
     }
 
     @Test
