58c7ecc5bf91fa00108c677cfb9b234e22bd3b9c
MITHUNA\10mit <e1155351@u.nus.edu>
2024-10-15 13:28:05 +0800

    Revert to before deleting by phone number

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Phone;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
@@ -15,21 +14,16 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
         try {
-            // check if arg is a phone number and initializes Delete Command with args
-            Phone phone = ParserUtil.parsePhone(args);
-            return new DeleteCommand(phone);
+            Index index = ParserUtil.parseIndex(args);
+            return new DeleteCommand(index);
         } catch (ParseException pe) {
-            try {
-                Index index = ParserUtil.parseIndex(args);
-                return new DeleteCommand(index);
-            } catch (ParseException pe1) {
-                throw new ParseException(
-                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe1);
-            }
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
         }
     }
 }
