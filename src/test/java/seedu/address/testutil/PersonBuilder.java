5505300b7b0298081c863a3bcc5be0996bd736eb
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-15 16:30:16 +0800

    Fix boolean naming

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -99,11 +99,12 @@ public class PersonBuilder {
         this.email = new Email(email);
         return this;
     }
+
     /**
      * Sets the {@code Payment} of the {@code Person} that we are building.
      */
-    public PersonBuilder withPayment(Boolean payment) {
-        this.payment = new Payment(payment);
+    public PersonBuilder withPayment(Boolean hasPaid) {
+        this.payment = new Payment(hasPaid);
         return this;
     }
 
@@ -114,6 +115,7 @@ public class PersonBuilder {
         this.attendance = new Attendance(isPresent);
         return this;
     }
+
 
     public Person build() {
         return new Person(name, phone, email, address, payment, attendance, tags);
