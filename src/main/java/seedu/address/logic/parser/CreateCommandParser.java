4007bee4c2bcdf52dd1cbe66419e40d5f3a6a42a
C5hives <e1122945@u.nus.edu>
2024-10-15 18:20:53 +0800

    Remove unused method arePrefixesPresent in parser

--- src/main/java/seedu/address/logic/parser/CreateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CreateCommandParser.java
@@ -38,13 +38,4 @@ public class CreateCommandParser implements Parser<CreateCommand> {
         }
     }
 
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values
-     * in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -77,13 +77,4 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
         }
     }
 
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values
-     * in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
