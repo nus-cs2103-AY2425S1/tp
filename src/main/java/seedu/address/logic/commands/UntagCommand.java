77a3e2b4f9f44d329e789a187d5eaa5a3ffc182c
yxshrk <yrk17122002@gmail.com>
2024-10-15 11:18:42 +0800

    Store changes for untag command

--- src/main/java/seedu/address/logic/commands/UntagCommand.java
+++ src/main/java/seedu/address/logic/commands/UntagCommand.java
@@ -24,8 +24,8 @@ public class UntagCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": untags specific contact \n"
-            + "Parameters: 'index t/ [TAG TO REMOVE]' OR 'index t/ all' (this removes everything)\n"
-            + "Examples: " + COMMAND_WORD + "1 t/ friends," + " " + COMMAND_WORD + " 1 t/ all";
+            + "Parameters: 'index t/[TAGS TO REMOVE]' OR 'index t/all' (this removes everything)\n"
+            + "Examples: " + COMMAND_WORD + " 1 t/friends colleagues," + " " + COMMAND_WORD + " 1 t/all";
 
     public static final String MESSAGE_UNTAG_SUCCESS = "Untagged the person: %1$s";
     private final Index index;
