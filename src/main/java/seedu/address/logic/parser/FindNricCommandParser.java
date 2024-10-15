2c4eed404ae2e7b892f1317808328a546cb1c4e6
suhayl13 <suhayl.pervez@gmail.com>
2024-10-15 15:34:21 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/FindNricCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindNricCommandParser.java
@@ -1,12 +1,14 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
 import seedu.address.logic.commands.FindNricCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NricContainsKeywordsPredicate;
 
-import java.util.Arrays;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 /**
  * Parses input arguments and creates a new FindNricCommand object
@@ -16,7 +18,7 @@ public class FindNricCommandParser implements Parser<FindNricCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the FindNricCommand
      * and returns a FindNricCommand object for execution.
-     * @throws seedu.address.logic.parser.exceptions.ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform the expected format
      */
     public FindNricCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
