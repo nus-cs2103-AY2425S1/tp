8c93bd8327fe18a0e55b1c28d0d8f81d89c686cd
beginner-web-developer <e1156242@u.nus.edu>
2024-10-15 09:12:41 +0800

    Add transactions to GUI

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -9,6 +9,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Transaction;
 
 /**
  * API of the Logic component
@@ -33,6 +34,9 @@ public interface Logic {
     /** Returns an unmodifiable view of the filtered list of persons */
     ObservableList<Person> getFilteredPersonList();
 
+    /** Returns an unmodifiable view of the transactions */
+    ObservableList<Transaction> getTransactionList();
+
     /**
      * Returns the user prefs' address book file path.
      */
@@ -47,4 +51,9 @@ public interface Logic {
      * Set the user prefs' GUI settings.
      */
     void setGuiSettings(GuiSettings guiSettings);
+
+    /**
+     * Determines if transaction view or person view is used.
+     */
+    boolean isViewTransactions();
 }
