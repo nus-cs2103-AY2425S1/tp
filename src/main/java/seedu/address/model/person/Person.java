69ea13d33bf58571537056ce8eb42076aa4765eb
myrilla12 <myrilla12@gmail.com>
2024-10-15 15:59:34 +0800

    Update Person to include age and sex

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -23,17 +23,21 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final Age age;
+    private final Sex sex;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Address address, Age age, Sex sex, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, age, sex, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.age = age;
+        this.sex = sex;
         this.tags.addAll(tags);
     }
 
@@ -51,6 +55,14 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public Age getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
     }
 
     /**
@@ -94,13 +106,15 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
+                && age.equals(otherPerson.age)
+                && sex.equals(otherPerson.sex)
                 && tags.equals(otherPerson.tags);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, age, sex, tags);
     }
 
     @Override
@@ -110,6 +124,8 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
+                .add("age", age)
+                .add("sex", sex)
                 .add("tags", tags)
                 .toString();
     }
