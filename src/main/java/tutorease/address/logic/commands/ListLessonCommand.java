bc00be471e8249d68a28036393b621cd3cf08f86
NorbertLoh <lohnorbert@gmail.com>
2024-10-15 11:17:58 +0800

    Add command into command parser

--- src/main/java/tutorease/address/logic/commands/ListLessonCommand.java
+++ src/main/java/tutorease/address/logic/commands/ListLessonCommand.java
@@ -1,14 +1,15 @@
 package tutorease.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+
 import tutorease.address.logic.commands.exceptions.CommandException;
 import tutorease.address.model.Model;
 
 /**
  * Lists all lessons in the lesson list.
  */
-public class ListLessonCommand extends Command{
-    public static final String SUB_COMMAND_WORD = "list";
+public class ListLessonCommand extends Command {
+    public static final String COMMAND_WORD = "list";
     public static final String MESSAGE_SUCCESS = "Listed all lessons";
     @Override
     public CommandResult execute(Model model) throws CommandException {
