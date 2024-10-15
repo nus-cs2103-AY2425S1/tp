e2f357cc846b358acfa5f131d05ff35aba9e22ec
S-K-Y-Light <s84915656@gmail.com>
2024-10-15 21:38:44 +0800

    Fix Style Check for JavaCI

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,8 +14,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX = "The student provided at index %1$d is invalid";
-
+    public static final String MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX =
+            "The student provided at index %1$d is invalid";
     public static final String MESSAGE_INVALID_INDEX_SHOWN = "The student provided at index/indices %1$s is invalid";
     public static final String MESSAGE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
