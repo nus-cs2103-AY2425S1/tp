a505455d59d3074e78dd1ade18db16e5242330a5
Solomon0519 <ng.eswen1@gmail.com>
2024-10-16 18:19:32 +0800

    Change files again to follow checkstyle rules
    Edited address book parser class such that it doesn't use * for importing.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -8,7 +8,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.commands.*;
+import seedu.address.logic.commands.AddPersonCommand;
+import seedu.address.logic.commands.AddMeetUpCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteMeetUpCommand;
+import seedu.address.logic.commands.DeletePersonCommand;
+import seedu.address.logic.commands.EditMeetUpCommand;
+import seedu.address.logic.commands.EditPersonCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindPersonCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ViewMeetUpCommand;
+import seedu.address.logic.commands.ViewPersonCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
