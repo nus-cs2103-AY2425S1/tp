86c2c1396f7e47b81a47ccbaf3439f1d264ea8c8
saajidshaik02 <saajidshaik02@gmail.com>
2024-10-16 18:47:51 +0800

    More Test Cases

--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,13 +11,21 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.testutil.TypicalPersons;
 
 class UndoCommandTest {
 
     private Model model;
+    private Model expectedModel;
+
+
     @BeforeEach
     public void setUp() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+
     }
 
     @Test
@@ -29,6 +38,33 @@ class UndoCommandTest {
 
         // Verify that the failure message is returned
         assertEquals(UndoCommand.MESSAGE_FAILURE, result.getFeedbackToUser());
+    }
+    @Test
+    public void execute_multipleUndo_success() throws Exception {
+        // Add two people to the model
+        Person person1 = new PersonBuilder().withName("Alice").build();
+        Person person2 = new PersonBuilder().withName("Bob").build();
+
+        model.addPerson(person1);
+        model.addPerson(person2);
+
+        expectedModel.addPerson(person1);
+        expectedModel.addPerson(person2);
+
+        // Ensure multiple undos are possible
+        assertTrue(model.canUndoAddressBook());
+
+        // Undo the last action (adding Bob)
+        CommandResult result1 = new UndoCommand().execute(model);
+        expectedModel.undoAddressBook();
+        assertEquals(UndoCommand.MESSAGE_SUCCESS, result1.getFeedbackToUser());
+        assertEquals(expectedModel, model);
+
+        // Undo the second action (adding Alice)
+        CommandResult result2 = new UndoCommand().execute(model);
+        expectedModel.undoAddressBook();
+        assertEquals(UndoCommand.MESSAGE_SUCCESS, result2.getFeedbackToUser());
+        assertEquals(expectedModel, model);
     }
 
 
