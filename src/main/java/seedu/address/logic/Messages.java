d6ed74a36460ccbf79f453af8e5a35ea4f2d00eb
weiseng18 <tws.hci@gmail.com>
2024-10-16 23:43:00 +0800

    Add comment

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -45,6 +45,8 @@ public class Messages {
                 .append(person.getPhone())
                 .append("; Email: ")
                 .append(person.getEmail());
+
+        // Only display Address: if present
         person.getAddress().ifPresent(address -> builder.append("; Address: ")
                 .append(person.getAddress().map(Object::toString).orElse("")));
 
