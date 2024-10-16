72dd23172ee71c651ae8211899055778bc4c4b10
keerthigkaarthik <120106412+keerthigkaarthik@users.noreply.github.com>
2024-10-16 18:32:08 +0800

    Add VolunteerDeleteCommand and corresponding Parser

--- src/main/java/seedu/address/logic/parser/VolunteerParserUtil.java
+++ src/main/java/seedu/address/logic/parser/VolunteerParserUtil.java
@@ -2,6 +2,8 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.volunteer.Date;
 import seedu.address.model.volunteer.Email;
@@ -16,6 +18,7 @@ import seedu.address.model.volunteer.Time;
 public class VolunteerParserUtil {
 
     public static final String MESSAGE_CONSTRAINTS = "Volunteer parsing error";
+    public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
 
     /**
      * Parses a given string as a {@code Name} and returns it after validation.
@@ -31,6 +34,19 @@ public class VolunteerParserUtil {
             throw new ParseException(Name.MESSAGE_CONSTRAINTS);
         }
         return new Name(trimmedName);
+    }
+
+    /**
+     * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
+     * trimmed.
+     * @throws ParseException if the specified index is invalid (not non-zero unsigned integer).
+     */
+    public static Index parseIndex(String oneBasedIndex) throws ParseException {
+        String trimmedIndex = oneBasedIndex.trim();
+        if (!StringUtil.isNonZeroUnsignedInteger(trimmedIndex)) {
+            throw new ParseException(MESSAGE_INVALID_INDEX);
+        }
+        return Index.fromOneBased(Integer.parseInt(trimmedIndex));
     }
 
     /**
