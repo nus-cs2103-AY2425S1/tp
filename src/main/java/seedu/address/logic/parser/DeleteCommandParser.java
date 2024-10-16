800253e6a349cf75d44951df0b1af8647e0c39a0
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-10-16 23:10:43 +0800

    Update DeleteCommandParser to accept multiple indices

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,8 +1,13 @@
 package seedu.address.logic.parser;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.core.index.IndexComparator;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -18,8 +23,13 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      */
     public DeleteCommand parse(String args) throws ParseException {
         try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            ArrayList<String> stringIndices = new ArrayList<>(new HashSet<String>((Arrays.asList(args.split("\\s*")))));
+            ArrayList<Index> indices = new ArrayList<>();
+            for (String s: stringIndices) {
+                indices.add(ParserUtil.parseIndex(s));
+            }
+            indices.sort(new IndexComparator()); //sort in descending order for smooth deletion
+            return new DeleteCommand(indices);
         } catch (ParseException pe) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
