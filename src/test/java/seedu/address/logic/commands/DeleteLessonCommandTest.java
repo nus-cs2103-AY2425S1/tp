73ccbceedece9c666d82a8006d0adbdb8e57d388
obrona <theodorelim2002@gmail.com>
2024-10-16 17:02:42 +0800

    Make some progess

---
+++ src/test/java/seedu/address/logic/commands/DeleteLessonCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showStudentAtIndex;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_STUDENT;
+import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.calendar.EdulogCalendar;
+import seedu.address.model.student.Student;
+
+public class DeleteLessonCommandTest {
+
+    @Test
+    public void deleteValidLesson() {
+        EdulogCalendar
+    }
+}
