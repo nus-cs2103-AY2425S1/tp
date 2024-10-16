77b49e70fce193035ac0a004c0049e6b1b2fc329
Rithik <rithiklenin@gmail.com>
2024-10-16 21:19:55 +0800

    Add logic to add policies to person and check duplicates

--- src/main/java/seedu/address/logic/commands/AddPolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/AddPolicyCommand.java
@@ -1,14 +1,20 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.policy.Policy;
 import seedu.address.model.policy.PolicySet;
 
 /**
- * Add Policy to an existing client in Prudy.
+ * Adds Policy to an existing client in Prudy.
  */
 public class AddPolicyCommand extends Command {
     public static final String COMMAND_WORD = "add-policy";
@@ -21,26 +27,73 @@ public class AddPolicyCommand extends Command {
             + "pt/[POLICY_TYPE]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + "pt/life";
+    public static final String POLICY_ADD_PERSON_SUCCESS = "Added Policy: %1$s";
 
     private final Index index;
     private final PolicySet policies;
 
     /**
-     * Creates an AddPolicyCommand to add the specified {@code PolicyMap} to the client.
+     * Creates an AddPolicyCommand to add the specified {@code PolicySet} to the client.
      *
      * @param index of the client in the filtered client list to add policy.
      * @param policies the set of policies to be added.
      */
     public AddPolicyCommand(Index index, PolicySet policies) {
-        requireAllNonNull(index, policies);
+        requireNonNull(index);
+        requireNonNull(policies);
 
         this.index = index;
         this.policies = policies;
     }
 
     @Override
-    public CommandResult execute(Model mode) throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), policies.toString()));
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        // Validate the index
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToEdit = lastShownList.get(index.getZeroBased());
+
+        // Add policies to the person, checking for duplicates
+        PolicySet editedPolicySet = addPoliciesToPerson(policies, personToEdit.getPolicySet());
+
+        // Create a new person with the updated policy set
+        Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), personToEdit.getTags(), editedPolicySet);
+
+        // Update the model with the edited person
+        model.setPerson(personToEdit, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        // Return success message
+        return new CommandResult(String.format(POLICY_ADD_PERSON_SUCCESS, Messages.format(editedPerson)));
+    }
+
+    /**
+     * Adds policies to the person's policy set. Throws a CommandException if any of the policies already exist.
+     *
+     * @param policiesToAdd The set of policies to add.
+     * @param existingPolicies The person's current policy set.
+     * @return A new PolicySet with the added policies.
+     * @throws CommandException if there are duplicate policies.
+     */
+    private PolicySet addPoliciesToPerson(PolicySet policiesToAdd, PolicySet existingPolicies) throws CommandException {
+        PolicySet updatedPolicySet = new PolicySet();
+        updatedPolicySet.addAll(existingPolicies);
+
+        // Check for duplicates and add new policies
+        for (Policy policy : policiesToAdd) {
+            if (existingPolicies.contains(policy)) { // Checking for duplicates
+                throw new CommandException(MESSAGE_DUPLICATES);
+            }
+            updatedPolicySet.add(policy);
+        }
+
+        return updatedPolicySet;
     }
 
     @Override
