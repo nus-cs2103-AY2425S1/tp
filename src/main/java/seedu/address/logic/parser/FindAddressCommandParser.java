5e48d71f98dfd657c3bbd3b74d907377d0132a97
harithhh <e1156421@u.nus.edu>
2024-10-16 12:40:02 +0800

    Add FindAddress Command

---
+++ src/main/java/seedu/address/logic/parser/FindAddressCommandParser.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.address.logic.commands.FindAddressCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.AddressContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindAddressCommand object
+ */
+public class FindAddressCommandParser implements Parser<FindAddressCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindAddressCommand
+     * and returns a FindAddressCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindAddressCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindAddressCommand.MESSAGE_USAGE));
+        }
+
+        String[] addressKeywords = trimmedArgs.split("\\s+");
+
+        return new FindAddressCommand(new AddressContainsKeywordsPredicate(Arrays.asList(addressKeywords)));
+    }
+
+
+}
