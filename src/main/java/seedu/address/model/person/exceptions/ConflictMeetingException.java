43c99adaf44650c683c49bea9ca33961afb96c93
tata32000 <tata32000@hotmail.com>
2024-10-16 14:37:05 +0800

    Fix styling and add javadoc for ConflictMeetingException.java

--- src/main/java/seedu/address/model/person/exceptions/ConflictMeetingException.java
+++ src/main/java/seedu/address/model/person/exceptions/ConflictMeetingException.java
@@ -1,5 +1,8 @@
 package seedu.address.model.person.exceptions;
 
+/**
+ * Signals that an operation would result in conflicting meetings.
+ */
 public class ConflictMeetingException extends RuntimeException {
     public ConflictMeetingException() {
         super("Operation would result in conflicting meetings");
