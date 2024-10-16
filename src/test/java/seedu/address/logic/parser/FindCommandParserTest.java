befd7803fad17d201db6980372b71d7b72fc12c2
jevan <e1121960@u.nus.edu>
2024-10-16 12:49:51 +0800

    Add test to FindCommandParserTest for invalid arguments
    - Asserts failure for invalid argument, e.g. "find /a john"

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -21,6 +21,11 @@ public class FindCommandParserTest {
     }
 
     @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "/a john", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
