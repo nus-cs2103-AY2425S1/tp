4bdaeb66888022ba0c296b3d0571bffb7c853a8f
yizhong187 <yizhong187@gmail.com>
2024-10-16 16:18:53 +0800

    Replace AddCommand
    Remove original AddCommand class and renamed AddContactCommand into
    AddCommand. Update all other occurrences as well.

--- src/main/java/seedu/ddd/logic/parser/AddContactCommandParser.java
+++ src/main/java/seedu/ddd/logic/parser/AddContactCommandParser.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import seedu.ddd.logic.commands.AddContactCommand;
+import seedu.ddd.logic.commands.AddCommand;
 import seedu.ddd.logic.parser.exceptions.ParseException;
 import seedu.ddd.model.AddressBook;
 import seedu.ddd.model.contact.client.Client;
@@ -36,14 +36,14 @@ import seedu.ddd.model.tag.Tag;
 /**
  * Parses input arguments and creates a new AddCommand object
  */
-public class AddContactCommandParser implements Parser<AddContactCommand> {
+public class AddContactCommandParser implements Parser<AddCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public AddContactCommand parse(String args) throws ParseException {
+    public AddCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
@@ -62,18 +62,18 @@ public class AddContactCommandParser implements Parser<AddContactCommand> {
             }
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddContactCommand.MESSAGE_USAGE), pe);
+                    AddCommand.MESSAGE_USAGE), pe);
         }
 
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_SERVICE)
                 && contactType == VENDOR) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddContactCommand.VENDOR_MESSAGE_USAGE));
+                    AddCommand.VENDOR_MESSAGE_USAGE));
         } else if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS,
                 PREFIX_PHONE, PREFIX_EMAIL, PREFIX_DATE)
                 && contactType == CLIENT) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddContactCommand.CLIENT_MESSAGE_USAGE));
+                    AddCommand.CLIENT_MESSAGE_USAGE));
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
@@ -95,7 +95,7 @@ public class AddContactCommandParser implements Parser<AddContactCommand> {
             contact = new Client(name, phone, email, address, date, tagList, id);
         }
         AddressBook.incrementNextId();
-        return new AddContactCommand(contact);
+        return new AddCommand(contact);
     }
 
     /**
