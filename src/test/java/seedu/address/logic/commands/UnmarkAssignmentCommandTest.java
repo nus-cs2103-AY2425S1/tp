f8c6d98d7399b423ed0e893082642f8ed19dc5d2
avrilmohh <avrilmoh1234@gmail.com>
2024-10-16 21:56:05 +0800

    Add test cases for unmark assignment

--- src/test/java/seedu/address/logic/commands/UnmarkAssignmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkAssignmentCommandTest.java
@@ -1,6 +1,6 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ASSIGNMENT;
@@ -21,54 +21,53 @@ import seedu.address.model.student.Student;
 import seedu.address.testutil.AssignmentBuilder;
 import seedu.address.testutil.StudentBuilder;
 
-
-public class MarkAssignmentCommandTest {
+public class UnmarkAssignmentCommandTest {
 
     private Model model = new ModelManager(new AddressBook(), new UserPrefs());
 
     @Test
-    public void execute_validStudentAndAssignmentIndex_markSuccessful() {
+    public void execute_validStudentAndAssignmentIndex_unmarkSuccessful() {
         Assignment assignment = new AssignmentBuilder()
+                .withHasSubmitted(true)
                 .build();
         Student student = new StudentBuilder().withAssignments(new ArrayList<>()).build().addAssignment(assignment);
         model.addStudent(student);
-        MarkAssignmentCommand markAssignmentCommand = new MarkAssignmentCommand(
+        UnmarkAssignmentCommand unmarkAssignmentCommand = new UnmarkAssignmentCommand(
                 INDEX_FIRST_STUDENT, INDEX_FIRST_ASSIGNMENT);
-        String expectedMessage = String.format(MarkAssignmentCommand.MESSAGE_MARK_SUCCESS,
-                "Math Homework", student.getName());
+        String expectedMessage = String.format(UnmarkAssignmentCommand.MESSAGE_UNMARK_SUCCESS,
+                "Math Homework", student.getName().fullName);
         Model expectedModel = new ModelManager(new AddressBook(), model.getUserPrefs());
         Assignment expectedAssignment = new AssignmentBuilder()
-                .withHasSubmitted(true)
                 .build();
         Student expectedStudent = new StudentBuilder().build().addAssignment(expectedAssignment);
         expectedModel.addStudent(expectedStudent);
-        assertCommandSuccess(markAssignmentCommand, model, expectedMessage, expectedModel);
-        assertTrue(student.getAssignmentList().get(INDEX_FIRST_ASSIGNMENT.getZeroBased()).getHasSubmitted());
+        assertCommandSuccess(unmarkAssignmentCommand, model, expectedMessage, expectedModel);
+        assertEquals(false, student.getAssignmentList().get(INDEX_FIRST_ASSIGNMENT.getZeroBased()).getHasSubmitted());
+        assertEquals(0, student.getAssignmentList().get(INDEX_FIRST_ASSIGNMENT.getZeroBased()).getScore());
     }
 
     @Test
-    public void execute_invalidStudentIndex_markFailure() {
+    public void execute_invalidStudentIndex_throwsCommandException() {
         Assignment assignment = new AssignmentBuilder()
+                .withHasSubmitted(true)
                 .build();
-        Student student = new StudentBuilder().build().addAssignment(assignment);
+        Student student = new StudentBuilder().withAssignments(new ArrayList<>()).build().addAssignment(assignment);
         model.addStudent(student);
-        MarkAssignmentCommand markAssignmentCommand = new MarkAssignmentCommand(
-                Index.fromOneBased(2),
-                INDEX_FIRST_ASSIGNMENT);
-        String expectedMessage = Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX;
-        assertCommandFailure(markAssignmentCommand, model, expectedMessage);
+        UnmarkAssignmentCommand unmarkAssignmentCommand = new UnmarkAssignmentCommand(
+                Index.fromOneBased(2), INDEX_FIRST_ASSIGNMENT);
+        assertCommandFailure(unmarkAssignmentCommand, model, Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX);
     }
 
     @Test
-    public void execute_invalidAssignmentIndex_markFailure() {
+    public void execute_invalidAssignmentIndex_throwsCommandException() {
         Assignment assignment = new AssignmentBuilder()
+                .withHasSubmitted(true)
                 .build();
-        Student student = new StudentBuilder().build().addAssignment(assignment);
+        Student student = new StudentBuilder().withAssignments(new ArrayList<>()).build().addAssignment(assignment);
         model.addStudent(student);
-        MarkAssignmentCommand markAssignmentCommand = new MarkAssignmentCommand(
-                INDEX_FIRST_STUDENT,
-                Index.fromOneBased(2));
-        String expectedMessage = Messages.MESSAGE_INVALID_ASSIGNMENT_INDEX;
-        assertCommandFailure(markAssignmentCommand, model, expectedMessage);
+        UnmarkAssignmentCommand unmarkAssignmentCommand = new UnmarkAssignmentCommand(
+                INDEX_FIRST_STUDENT, Index.fromOneBased(2));
+        assertCommandFailure(unmarkAssignmentCommand, model, Messages.MESSAGE_INVALID_ASSIGNMENT_INDEX);
     }
+
 }
