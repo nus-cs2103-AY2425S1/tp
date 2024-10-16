efcad73768388471ea453a93ffdb421b655e20d4
lr <lruniapp1020@gmail.com>
2024-10-16 21:05:16 +0800

    Add FindNameCommand test Add FindNameCommand test

--- src/main/java/seedu/address/logic/parser/FindNameCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindNameCommandParser.java
@@ -9,13 +9,13 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments and creates a new FindNameCommand object
  */
 public class FindNameCommandParser implements Parser<FindNameCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns a FindCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the FindNameCommand
+     * and returns a FindNameCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindNameCommand parse(String args) throws ParseException {
