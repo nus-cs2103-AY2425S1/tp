0c302ff7329ddbae003f50cd5b3092a7c2b6aed3
lr <lruniapp1020@gmail.com>
2024-10-16 12:18:28 +0800

    Update find name command Add FindNameCommand class and edit relevant files

--- src/main/java/seedu/address/logic/parser/ClientHubParser.java
+++ src/main/java/seedu/address/logic/parser/ClientHubParser.java
@@ -15,6 +15,7 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindNameCommand;
 import seedu.address.logic.commands.FindPhoneCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
@@ -80,6 +81,9 @@ public class ClientHubParser {
 
         case FindPhoneCommand.COMMAND_WORD:
             return new FindPhoneCommandParser().parse(arguments);
+
+        case FindNameCommand.COMMAND_WORD:
+            return new FindNameCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
