d891e6d636f7f6976f97f2cc2f12464decb85249
shotnothing <shotnothing@protonmail.ch>
2024-10-16 17:01:15 +0800

    Tidy up imports

--- src/test/java/seedu/address/model/person/PriceTest.java
+++ src/test/java/seedu/address/model/person/PriceTest.java
@@ -2,9 +2,9 @@ package seedu.address.model.person;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
-
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class PriceTest {
 
