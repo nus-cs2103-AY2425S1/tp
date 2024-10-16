1a6a767a5df49195c321cbfba83aec4391986cd6
stephaneleong <stephaneleong@gmail.com>
2024-10-16 22:53:37 +0800

    Implement GoodStorage interface

---
+++ src/main/java/seedu/address/storage/GoodsStorage.java
@@ -0,0 +1,49 @@
+package seedu.address.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataLoadingException;
+import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.goodsReceipt.GoodsReceipt;
+
+
+/**
+ * Represents a storage for {@link seedu.address.model.goods.Goods}.
+ */
+public interface GoodsStorage {
+
+    /**
+     * Returns the file path of the data file.
+     */
+    Path getGoodsFilePath();
+
+    /**
+     * Returns Goods data as a {@link GoodsReceipt}.
+     * Returns {@code Optional.empty()} if storage file is not found.
+     *
+     * @throws DataLoadingException if loading the data from storage failed.
+     */
+    Optional<List<GoodsReceipt>> readGoods() throws DataLoadingException;
+
+    /**
+     * @see #getGoodsFilePath()
+     */
+    Optional<List<GoodsReceipt>> readGoods(Path filePath) throws DataLoadingException;
+
+    /**
+     * TODO: Add ReadOnly Feature to the parameter
+     * Saves the given {@link ReadOnlyAddressBook} to the storage.
+     * @param goods cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveGoods(List<GoodsReceipt> goods) throws IOException;
+
+    /**
+     * TODO: Add ReadOnly Feature to the parameter
+     * @see #saveGoods(List)
+     */
+    void saveGoods(List<GoodsReceipt> goods, Path filePath) throws IOException;
+}
