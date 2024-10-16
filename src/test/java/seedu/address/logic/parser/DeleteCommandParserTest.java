68561ce955dc8007a3c86909cdd1af79ee197406
wassilililily <e0725370@u.nus.edu>
2024-10-16 13:24:21 +0800

    Adds JUnit tests for DeleteCompanyCommand

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -5,9 +5,11 @@ import static seedu.address.logic.Messages.MESSAGE_MISSING_INDEX;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_COMPANY;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.DeleteCompanyCommand;
 import seedu.address.logic.commands.DeleteContactCommand;
 
 /**
@@ -27,11 +29,23 @@ public class DeleteCommandParserTest {
     }
 
     @Test
+    public void parse_validCompanyArgs_returnsDeleteCompanyCommand() {
+        assertParseSuccess(parser, "company 1", new DeleteCompanyCommand(INDEX_FIRST_COMPANY));
+    }
+
+    @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "contact", String.format(MESSAGE_MISSING_INDEX,
                 DeleteContactCommand.MESSAGE_USAGE));
 
         assertParseFailure(parser, "contact a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 DeleteContactCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidArgsCompany_throwsParseException() {
+        assertParseFailure(parser, "company", MESSAGE_MISSING_INDEX); // No index provided
+        assertParseFailure(parser, "company a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteCompanyCommand.MESSAGE_USAGE)); // Non-numeric index
     }
 }
