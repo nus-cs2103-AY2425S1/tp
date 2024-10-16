26078b6475226bada97521ed32b538a3c2ee6b2f
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-10-16 17:16:44 +0800

    Person.java: Edit to include Last Seen date

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -23,17 +23,19 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final LastSeen lastSeen;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+    public Person(Name name, Phone phone, Email email, Address address, LastSeen lastSeen, Set<Tag> tags) {
+        requireAllNonNull(name, phone, email, address, lastSeen, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.lastSeen = lastSeen;
         this.tags.addAll(tags);
     }
 
@@ -51,6 +53,9 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+    public LastSeen getLastSeen() {
+        return lastSeen;
     }
 
     /**
@@ -94,13 +99,14 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
+                && lastSeen.equals(otherPerson.lastSeen)
                 && tags.equals(otherPerson.tags);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, lastSeen, tags);
     }
 
     @Override
@@ -110,6 +116,7 @@ public class Person {
                 .add("phone", phone)
                 .add("email", email)
                 .add("address", address)
+                .add("last seen", lastSeen)
                 .add("tags", tags)
                 .toString();
     }
