f5336d6f2a4665e8c954195ba53fe6ded5dd3713
yu7ong <yutonggg0131@gmail.com>
2024-10-16 18:46:20 +0800

    Correct checkstyle

--- src/test/java/seedu/address/logic/commands/AssignProductCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AssignProductCommandTest.java
@@ -1,7 +1,15 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PRODUCT_APPLE_PIE;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalSuppliers.getTypicalAddressBook;
+
 import java.util.Set;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
@@ -12,12 +20,6 @@ import seedu.address.model.supplier.Name;
 import seedu.address.model.supplier.Supplier;
 import seedu.address.testutil.ProductBuilder;
 import seedu.address.testutil.SupplierBuilder;
-
-import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_PRODUCT_APPLE_PIE;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalSuppliers.getTypicalAddressBook;
 
 public class AssignProductCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -32,7 +34,8 @@ public class AssignProductCommandTest {
                 new ProductName(VALID_PRODUCT_APPLE_PIE),
                 new Name(VALID_NAME_AMY));
 
-        String expectedMessage = String.format(AssignProductCommand.MESSAGE_SUCCESS, VALID_PRODUCT_APPLE_PIE, VALID_NAME_AMY);
+        String expectedMessage = String.format(AssignProductCommand.MESSAGE_SUCCESS, VALID_PRODUCT_APPLE_PIE,
+                VALID_NAME_AMY);
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.setSupplier(supplier, new SupplierBuilder(supplier).withProducts(Set.of(product)).build());
 
