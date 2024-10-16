b96dd6cccbf7bf682a8a110c5bb1f7d75b1f1938
thenosewizard <abicharan.b@gmail.com>
2024-10-16 17:21:02 +0800

    Fix import order

--- src/test/java/seedu/address/model/goodsReceipt/GoodsReceiptTest.java
+++ src/test/java/seedu/address/model/goodsReceipt/GoodsReceiptTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Objects;
+
+import org.junit.jupiter.api.Test;
 
 import seedu.address.model.goods.Goods;
 import seedu.address.model.goods.GoodsCategories;
