1a8d1682c03ca91864649de06910add66996c24c
hovanleong <hovanleong1@gmail.com>
2024-10-16 17:35:56 +0800

    Create and implement EditVendorDescriptorBuilder

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -18,11 +18,13 @@ import java.util.List;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.commands.util.EditGuestDescriptor;
+import seedu.address.logic.commands.util.EditVendorDescriptor;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.EditGuestDescriptorBuilder;
+import seedu.address.testutil.EditVendorDescriptorBuilder;
 
 /**
  * Contains helper methods for testing commands.
