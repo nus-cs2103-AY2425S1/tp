bbf4449ac582480aa8d2b6302f7c621a1f803772
EdaYeo <wye.yeo@gmail.com>
2024-10-16 23:52:59 +0800

    Update sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,26 +21,29 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new RoomNumber("01-0123"), new Address("Blk 30 Geylang Street 29, #06-40"),
+            new Person(new Name("Alex Yeoh"), new Phone("+65 87438807"), new Email("alexyeoh@example.com"),
+                new RoomNumber("01-0123"), new Address("Apple Dorm Block A"),
                 new EmergencyContact(new Name("Aiken"), new Phone("81234567")),
-                getTagSet("friends")),
+                getTagSet("Floor1")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new RoomNumber("01-0124"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), null,
-                getTagSet("colleagues", "friends")),
+                new RoomNumber("01-0124"), new Address("Apple Dorm Block B"),
+                new EmergencyContact(new Name("Aiken"), new Phone("+20 81234567")),
+                getTagSet("Floor1", "volleyball")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new RoomNumber("02-0123"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                new EmergencyContact(new Name("Dueet"), new Phone("12345678")),
-                getTagSet("neighbours")),
+                new RoomNumber("02-0123"), new Address("Mango Dorm Block C"),
+                new EmergencyContact(new Name("Dueet"), new Phone("+123 23 12345678")),
+                getTagSet("Floor10")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new RoomNumber("08-8888"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), null,
-                getTagSet("family")),
+                new RoomNumber("08-8888"), new Address("Pear Dorm Block A"),
+                new EmergencyContact(new Name("Benny Yao"), null),
+                getTagSet("Floor3")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new RoomNumber("04-4444"), new Address("Blk 47 Tampines Street 20, #17-35"), null,
-                getTagSet("classmates")),
+                new RoomNumber("04-4444"), new Address("Grape Dorm Block A"),
+                new EmergencyContact(null, new Phone("+20 81234567")),
+                getTagSet("Floor5")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new RoomNumber("11-1111"), new Address("Blk 45 Aljunied Street 85, #11-31"), null,
-                getTagSet("colleagues"))
+                null, new Address("Grape Dorm Block A"), null,
+                getTagSet("Floor3"))
         };
     }
 
