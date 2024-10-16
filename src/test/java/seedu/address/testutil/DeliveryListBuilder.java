a4b009616aa44c1337955fbc1515d2bd8440bae0
linedoestrolling <yiheng.yh.zhao@gmail.com>
2024-10-16 15:46:43 +0800

    Fix style issues

--- src/test/java/seedu/address/testutil/DeliveryListBuilder.java
+++ src/test/java/seedu/address/testutil/DeliveryListBuilder.java
@@ -5,16 +5,31 @@ import seedu.address.model.delivery.DeliveryList;
 
 import java.util.ArrayList;
 
+/**
+ * A utility class to help with building DeliveryList objects.
+ */
 public class DeliveryListBuilder {
     private ArrayList<Delivery> deliveryArrayList;
 
+    /**
+     * Constructor to do nothing
+     */
     public DeliveryListBuilder() {}
 
+    /**
+     * Adds deliveries to the arraylist of deliveries
+     * @param delivery Delivery object to be added
+     */
     public DeliveryListBuilder withNewDelivery(Delivery delivery) {
         deliveryArrayList.add(delivery);
         return this;
     }
 
+    /**
+     * Builds the {@code DeliveryList} object
+     *
+     * @return The built DeliveryList object.
+     */
     public DeliveryList build() {
         DeliveryList deliveryList = new DeliveryList();
         for (Delivery delivery: deliveryArrayList) {
