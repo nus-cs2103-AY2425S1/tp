d17986b17fb7a433546d1f6814b286861fececcc
ulricolo7 <ulricoco39lo7@gmail.com>
2024-10-17 00:05:41 +0800

    Update JsonAdaptedMeeting.java

--- src/main/java/seedu/address/storage/JsonAdaptedMeeting.java
+++ src/main/java/seedu/address/storage/JsonAdaptedMeeting.java
@@ -7,8 +7,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Meeting;
+import seedu.address.model.person.Name;
 
 /**
  * Jackson-friendly version of {@link Meeting}.
@@ -60,7 +60,8 @@ class JsonAdaptedMeeting {
         final Name modelName = new Name(person);
 
         if (startTime == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, LocalDateTime.class.getSimpleName()));
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    LocalDateTime.class.getSimpleName()));
         }
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
@@ -68,7 +69,8 @@ class JsonAdaptedMeeting {
         final LocalDateTime modelStartTime = LocalDateTime.parse(startTime, formatter);
 
         if (endTime == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, LocalDateTime.class.getSimpleName()));
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    LocalDateTime.class.getSimpleName()));
         }
 
         final LocalDateTime modelEndTime = LocalDateTime.parse(endTime, formatter);
