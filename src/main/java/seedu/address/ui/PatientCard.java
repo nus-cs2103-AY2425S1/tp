3a9934501e586d7f7399bfa190cea19062212ea6
sam-theman88 <samuel.lee9525@gmail.com>
2024-10-16 14:05:56 +0800

    Update UI to display patients as a PatientCard

--- src/main/java/seedu/address/ui/PatientCard.java
+++ src/main/java/seedu/address/ui/PatientCard.java
@@ -1,2 +1,35 @@
-package seedu.address.ui;public class PatientCard {
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import seedu.address.model.patient.Patient;
+
+/**
+ * A UI component that displays information of a {@code Patient}.
+ */
+public class PatientCard extends PersonCard {
+
+    private static final String FXML = "PatientListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    @FXML
+    private Label dob;
+    @FXML
+    private Label gender;
+
+    /**
+     * Creates a {@code PatientCode} with the given {@code Patient} and index to display.
+     */
+    public PatientCard(Patient patient, int displayedIndex) {
+        super(patient, displayedIndex);
+        dob.setText(patient.getDateOfBirth().value);
+        gender.setText(patient.getGender().value);
+    }
 }
