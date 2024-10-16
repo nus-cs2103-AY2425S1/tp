824e045e4d8f07f7ade81abde3265f9de4c60757
tata32000 <tata32000@hotmail.com>
2024-10-16 14:26:55 +0800

    Fix styling and add javadoc for JsonSerializableScheduleStorage.java

--- src/main/java/seedu/address/storage/JsonSerializableScheduleStorage.java
+++ src/main/java/seedu/address/storage/JsonSerializableScheduleStorage.java
@@ -7,17 +7,21 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.ReadOnlyScheduleList;
 import seedu.address.model.ScheduleList;
 import seedu.address.model.schedule.Meeting;
 
+/**
+ * An Immutable ScheduleList that is serializable to JSON format.
+ */
 @JsonRootName(value = "schedulelist")
 public class JsonSerializableScheduleStorage {
     public static final String MEETING_DUPLICATE_PERSON = "Schedule list contains duplicate meeting(s).";
-    
+
     private final List<JsonAdaptedMeeting> meetings = new ArrayList<>();
-    
+
     /**
      * Constructs a {@code JsonSerializableScheduleStorage} with the given persons.
      */
@@ -25,16 +29,16 @@ public class JsonSerializableScheduleStorage {
     public JsonSerializableScheduleStorage(@JsonProperty("meetings") List<JsonAdaptedMeeting> meetings) {
         this.meetings.addAll(meetings);
     }
-    
+
     /**
      * Converts a given {@code ReadOnlyScheduleList} into this class for Jackson use.
      *
      * @param source future changes to this will not affect the created {@code JsonSerializableScheduleStorage}.
      */
-    public  JsonSerializableScheduleStorage(ReadOnlyScheduleList source) {
+    public JsonSerializableScheduleStorage(ReadOnlyScheduleList source) {
         meetings.addAll(source.getMeetingList().stream().map(JsonAdaptedMeeting::new).collect(Collectors.toList()));
     }
-    
+
     /**
      * Converts this schedule list into the model's {@code ScheduleList} object.
      *
@@ -51,5 +55,4 @@ public class JsonSerializableScheduleStorage {
         }
         return scheduleList;
     }
-    
 }
