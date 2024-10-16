44b3f1e3fb3bae04ede0e56f558fd370651be83d
Kitty-001 <jqm002@hotmail.com>
2024-10-16 15:06:25 +0800

    Update Day REGEX and add test for validation

--- src/main/java/seedu/address/model/preferredtime/Day.java
+++ src/main/java/seedu/address/model/preferredtime/Day.java
@@ -8,10 +8,15 @@ public class Day {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Day should only contain alphanumeric characters and it should not be blank.\n"
-            + "It should be one of the {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}.";
+            + "It should be one of the {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}.\n"
+            + "Or corresponding abbreviations {Mon, Tue, Wed, Thu, Fri, Sat, Sun}";
 
     // TODO: change the REGEX
-    public static final String VALIDATION_REGEX = "^(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)$";
+    public static final String REGEX_DAY_COMPLETE = "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday";
+    public static final String REGEX_DAY_ABBR = "Mon|Tue|Wed|Thu|Fri|Sat|Sun";
+    // public static final String VALIDATION_REGEX = "^(?i)(Mon|Tue|Wed|Thu|Fri|Sat|Sun)(day)?$";
+    public static final String VALIDATION_REGEX = "^(?i)(" + REGEX_DAY_COMPLETE + "|"
+            + REGEX_DAY_ABBR + ")$";
 
 
     /**
--- src/test/java/seedu/address/model/preferredtime/DayTest.java
+++ src/test/java/seedu/address/model/preferredtime/DayTest.java
@@ -1,5 +1,7 @@
 package seedu.address.model.preferredtime;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -9,9 +11,18 @@ public class DayTest {
 
     @Test
     public void isValidDay() {
-        // TODO: implement test for Day format
-
         // null day
         assertThrows(NullPointerException.class, () -> Day.isValidDay(null));
+
+        // invalid day
+        assertFalse(Day.isValidDay("")); // empty String
+        assertFalse(Day.isValidDay(" ")); // spaces only
+        assertFalse(Day.isValidDay("WEdnes")); // wrong abbreviation
+
+        // valid day
+        assertTrue(Day.isValidDay("Wednesday")); // normal input
+        assertTrue(Day.isValidDay("wednesday")); // all lower case
+        assertTrue(Day.isValidDay("WEDNESDAY")); // all upper case
+        assertTrue(Day.isValidDay("mon")); // lower case abbreviation
     }
 }
