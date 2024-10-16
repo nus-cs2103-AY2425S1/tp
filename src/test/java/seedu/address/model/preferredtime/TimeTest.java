add724a012aeec4454db284782c9fa7d5b0dc336
Kitty-001 <jqm002@hotmail.com>
2024-10-16 15:12:02 +0800

    Add tests for Time validation

--- src/test/java/seedu/address/model/preferredtime/TimeTest.java
+++ src/test/java/seedu/address/model/preferredtime/TimeTest.java
@@ -1,5 +1,7 @@
 package seedu.address.model.preferredtime;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -11,7 +13,21 @@ public class TimeTest {
     public void isValidTime() {
         // TODO: implement test for time format
 
-        // null tag name
+        // null time
         assertThrows(NullPointerException.class, () -> Time.isValidTime(null));
+
+        // invalid time
+        assertFalse(Time.isValidTime("")); // empty string
+        assertFalse(Time.isValidTime(" ")); // space only
+        assertFalse(Time.isValidTime("12")); // < 4 places
+        assertFalse(Time.isValidTime("123456")); // > 4 places
+        assertFalse(Time.isValidTime("4567")); // out of range
+        assertFalse(Time.isValidTime("2400")); // end edge
+
+        // valid input
+        assertTrue(Time.isValidTime("1230")); // normal input
+        assertTrue(Time.isValidTime("0000")); // start edge
+        assertTrue(Time.isValidTime("2359")); // end edge
+
     }
 }
