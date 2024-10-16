93ab2ed9343875e43ea4d442118907a100010db7
oneBoz <dingyihe@DYHDYHDYH.local>
2024-10-16 15:22:26 +0800

    Add FindTagCommandParserTest

--- src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
@@ -15,10 +15,10 @@ import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.person.TagContainsKeywordsPredicate;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 
 /**
