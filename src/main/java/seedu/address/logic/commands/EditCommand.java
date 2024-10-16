224824c1ddd503726cf066ceb4272ded105b527c
starbucksventi <ravirajtalgeri@gmail.com>
2024-10-16 19:48:37 +0800

    Refine code style for EditCommand

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -9,7 +9,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -219,7 +218,7 @@ public class EditCommand extends Command {
         }
 
         public Optional<Birthday> getBirthday() {
-            return Optional.ofNullable(birthday).filter(date -> !date.getValue().equals(LocalDate.MIN));
+            return Optional.ofNullable(birthday);
         }
 
         /**
