899cbf8c2962af79e98abda92a8505c7d9746702
hovanleong <hovanleong1@gmail.com>
2024-10-16 17:43:50 +0800

    Add static variables to CommandTestUtil

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -75,6 +75,8 @@ public class CommandTestUtil {
 
     public static final EditGuestDescriptor DESC_AMY;
     public static final EditGuestDescriptor DESC_BOB;
+    public static final EditVendorDescriptor DESC_VENDOR_AMY;
+    public static final EditVendorDescriptor DESC_VENDOR_BOB;
 
     static {
         DESC_AMY = new EditGuestDescriptorBuilder().withName(VALID_NAME_AMY)
@@ -83,6 +85,13 @@ public class CommandTestUtil {
         DESC_BOB = new EditGuestDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
                 .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).withRsvp(VALID_RSVP_PENDING).build();
+        DESC_VENDOR_AMY = new EditVendorDescriptorBuilder().withName(VALID_NAME_AMY)
+                .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
+                .withTags(VALID_TAG_FRIEND).withCompany(VALID_COMPANY_AMY).build();
+        DESC_VENDOR_BOB = new EditVendorDescriptorBuilder().withName(VALID_NAME_BOB)
+                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
+                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).withCompany(VALID_COMPANY_BOB).build();
+
     }
 
     /**
