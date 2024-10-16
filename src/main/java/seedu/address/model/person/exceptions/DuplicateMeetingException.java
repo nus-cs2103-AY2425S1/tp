3a28299bb9ca758edb1c8ba21af76b999a4b0afc
tata32000 <tata32000@hotmail.com>
2024-10-16 14:37:32 +0800

    Fix styling and add javadoc for DuplicateMeetingException.java

--- src/main/java/seedu/address/model/person/exceptions/DuplicateMeetingException.java
+++ src/main/java/seedu/address/model/person/exceptions/DuplicateMeetingException.java
@@ -1,5 +1,8 @@
 package seedu.address.model.person.exceptions;
 
+/**
+ * Signals that an operation would result in duplicate meetings.
+ */
 public class DuplicateMeetingException extends RuntimeException {
     public DuplicateMeetingException() {
         super("Operation would result in duplicate meetings");
