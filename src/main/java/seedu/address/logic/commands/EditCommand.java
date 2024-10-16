fc9472c615f111b4461a2b8a7f878f9265bdce1e
IanCheah <iancheah24@gmail.com>
2024-10-16 12:29:31 +0800

    Fix style error

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -102,11 +102,11 @@ public class EditCommand extends Command {
         Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
         Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
         Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
-        Organisation updatedOrganisation = editPersonDescriptor.getOrganisation().
-                orElse(personToEdit.getOrganisation());
+        Organisation updatedOrganisation = editPersonDescriptor.getOrganisation()
+                .orElse(personToEdit.getOrganisation());
         Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
 
-        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress,  updatedOrganisation, updatedTags);
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedOrganisation, updatedTags);
     }
 
     @Override
