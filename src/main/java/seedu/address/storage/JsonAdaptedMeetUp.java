58562fbef3180e76feeb1cefd9bfd36b0ee3a0ce
KennyLewi <kennylewi27@gmail.com>
2024-10-16 17:32:03 +0800

    Add storage test cases

--- src/main/java/seedu/address/storage/JsonAdaptedMeetUp.java
+++ src/main/java/seedu/address/storage/JsonAdaptedMeetUp.java
@@ -56,11 +56,17 @@ class JsonAdaptedMeetUp {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     Name.class.getSimpleName()));
         }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+        }
         final Name modelName = new Name(name);
 
         if (info == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     Info.class.getSimpleName()));
+        }
+        if (!Info.isValidInfo(info)) {
+            throw new IllegalValueException(Info.MESSAGE_CONSTRAINTS);
         }
         final Info modelInfo = new Info(info);
 
@@ -68,11 +74,17 @@ class JsonAdaptedMeetUp {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     From.class.getSimpleName()));
         }
+        if (!From.isValidMeetUpFromTime(from)) {
+            throw new IllegalValueException(From.MESSAGE_CONSTRAINTS);
+        }
         final From modelFrom = new From(from);
 
         if (to == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
                     To.class.getSimpleName()));
+        }
+        if (!To.isValidMeetUpToTime(to)) {
+            throw new IllegalValueException(To.MESSAGE_CONSTRAINTS);
         }
         final To modelTo = new To(to);
 
