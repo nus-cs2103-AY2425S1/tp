07249f0e5e330d09770f8849e3b8dcc8633adc19
nathanshew <92252099+nathanshew@users.noreply.github.com>
2024-10-16 20:00:20 +0800

    Create AddLessonCommandTest.java

---
+++ src/test/java/seedu/address/logic/commands/AddLessonCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.calendar.Lesson;
+
+public class AddLessonCommandTest {
+
+    @Test
+    public void addValidLesson() throws CommandException {
+        Lesson lesson = new Lesson("Math", DayOfWeek.MONDAY, LocalTime.of(12, 0), LocalTime.of(13, 0));
+        Model model = new ModelManager();
+        model.addLesson(lesson);
+
+        AddLessonCommand command = new AddLessonCommand(lesson);
+        CommandResult result = command.execute(model);
+        assertEquals(result.getFeedbackToUser(),
+                String.format(AddLessonCommand.MESSAGE_SUCCESS, lesson));
+        assertTrue(model.hasLesson(lesson));
+    }
+}
