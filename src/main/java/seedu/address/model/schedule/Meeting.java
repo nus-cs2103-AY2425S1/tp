c2d0f90367a7d76a9a6597a331e08c557247f3d4
tata32000 <tata32000@hotmail.com>
2024-10-16 14:12:34 +0800

    Fix styling and add javadoc for Meeting.java

--- src/main/java/seedu/address/model/schedule/Meeting.java
+++ src/main/java/seedu/address/model/schedule/Meeting.java
@@ -1,20 +1,32 @@
 package seedu.address.model.schedule;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
+/**
+ * Represents a Meeting in the schedule.
+ * Guarantees: details are present and not null, and fields are validated.
+ */
 public class Meeting {
-    
+
     // Data Fields
     private List<Integer> contactIndexes;
     private String meetingName;
     private LocalDate meetingDate;
     private LocalTime meetingTime;
-    
+
+    /**
+     * Constructs a {@code Meeting}.
+     *
+     * @param contactIndexes A list of indexes representing the contacts involved in the meeting.
+     * @param meetingName    Name of the meeting.
+     * @param meetingDate    Date of the meeting. Cannot be null.
+     * @param meetingTime    Time of the meeting. Cannot be null.
+     */
     public Meeting(List<Integer> contactIndexes, String meetingName, LocalDate meetingDate, LocalTime meetingTime) {
         requireAllNonNull(meetingDate, meetingTime);
         this.contactIndexes = contactIndexes;
@@ -22,44 +34,84 @@ public class Meeting {
         this.meetingDate = meetingDate;
         this.meetingTime = meetingTime;
     }
-    
+
+    /**
+     * Returns the list of contact indexes involved in the meeting.
+     *
+     * @return A list of contact indexes.
+     */
     public List<Integer> getContactIndexes() {
         return contactIndexes;
     }
+
+    /**
+     * Converts the list of contact indexes to a string, with indexes separated by commas.
+     *
+     * @param inputContactIndexes List of contact indexes to be converted.
+     * @return A comma-separated string of contact indexes.
+     */
     public String convertContactIndexesToString(List<Integer> inputContactIndexes) {
         return inputContactIndexes.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(","));
     }
-    
-    
+
+    /**
+     * Returns the name of the meeting.
+     *
+     * @return Meeting name.
+     */
     public String getMeetingName() {
         return meetingName;
     }
-    
+
+    /**
+     * Returns the date of the meeting.
+     *
+     * @return Meeting date.
+     */
     public LocalDate getMeetingDate() {
         return meetingDate;
     }
-    
+
+    /**
+     * Returns the time of the meeting.
+     *
+     * @return Meeting time.
+     */
     public LocalTime getMeetingTime() {
         return meetingTime;
     }
-    
+
+    /**
+     * Checks if the current meeting has a time conflict with another meeting.
+     * Meetings are considered conflicting if they occur at the same date and time.
+     *
+     * @param otherMeeting Another meeting to compare against.
+     * @return True if the meetings conflict, false otherwise.
+     */
     public boolean hasConflictMeeting(Meeting otherMeeting) {
         if (otherMeeting == this) {
             return true;
         }
-        
+
         return otherMeeting != null
                 && otherMeeting.getMeetingDate().equals(getMeetingDate())
                 && otherMeeting.getMeetingTime().equals(getMeetingTime());
     }
-    
+
+    /**
+     * Checks if two meetings are the same.
+     * Meetings are considered the same if they have the same date, time, and name.
+     *
+     * @param otherMeeting Another meeting to compare against.
+     * @return True if the meetings are the same, false otherwise.
+     */
     public boolean isSameMeeting(Meeting otherMeeting) {
         if (otherMeeting == this) {
             return true;
         }
-    
+
         return otherMeeting != null
                 && otherMeeting.getMeetingDate().equals(getMeetingDate())
                 && otherMeeting.getMeetingTime().equals(getMeetingTime())
