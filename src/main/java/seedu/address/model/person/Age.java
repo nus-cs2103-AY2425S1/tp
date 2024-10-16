df70bc84a97797c20e2919f236ec02321c79cae4
sannie-beep-admin <sanchita.dh7@gmail.com>
2024-10-16 15:59:43 +0800

    Update Age class to check age constraints and tests for invalid age over limit

--- src/main/java/seedu/address/model/person/Age.java
+++ src/main/java/seedu/address/model/person/Age.java
@@ -1,64 +1,54 @@
 package seedu.address.model.person;
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
-
 /**
  * Represents a Person's age in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAge(String)}
  */
 public class Age {
-
-    public static final String MESSAGE_CONSTRAINTS = "Age should only contain numeric values";
-
-    /*
-     * The age must be a non-negative integer.
-     */
-    public static final String VALIDATION_REGEX = "\\d+";
-
+    public static final String MESSAGE_CONSTRAINTS =
+            "Age should only contain a number between 0 and 150 ";
     public final String value;
-
     /**
      * Constructs a {@code Age}.
      *
-     * @param age A valid age (numeric value).
+     * @param age A valid age.
      */
     public Age(String age) {
         requireNonNull(age);
         checkArgument(isValidAge(age), MESSAGE_CONSTRAINTS);
-        this.value = age;
+        value = age;
     }
-
     /**
      * Returns true if a given string is a valid age.
+     * A valid age should be a number between 0 and 150 (inclusive).
      */
     public static boolean isValidAge(String test) {
-        return test.matches(VALIDATION_REGEX);
+        try {
+            int age = Integer.parseInt(test);
+            return age >= 0 && age <= 150;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
-
     @Override
     public String toString() {
         return value;
     }
-
     @Override
     public boolean equals(Object other) {
         if (other == this) {
             return true;
         }
-
         // instanceof handles nulls
         if (!(other instanceof Age)) {
             return false;
         }
-
         Age otherAge = (Age) other;
         return value.equals(otherAge.value);
     }
-
     @Override
     public int hashCode() {
         return value.hashCode();
     }
-
 }
