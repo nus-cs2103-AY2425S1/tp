bb3baeb3bd92ff1d146ea35574fc6ffc475790eb
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-10-16 20:14:17 +0800

    Messages.java: Add support for last seen

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -43,6 +43,8 @@ public class Messages {
                 .append(person.getEmail())
                 .append("; Address: ")
                 .append(person.getAddress())
+                .append("; Last Seen: ")
+                .append(person.getLastSeen())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
