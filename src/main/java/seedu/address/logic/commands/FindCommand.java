1229cbe119b1f07afc06ce3482d6628c7fc3e9c3
tingxuanp <peh.tingx@gmail.com>
2024-10-16 18:20:32 +0800

    Update description for find command

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,6 +1,6 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -10,10 +10,12 @@ public abstract class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " " + PREFIX_PHONE + " alice charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons based on the specified keywords "
+            + "(case-insensitive) after the prefix representing the field, "
+            + "and displays them as a list with index numbers.\n"
+            + "Use 'n/' to search by name, 'a/' to search by address etc. \n"
+            + "Parameters: PREFIX/ KEYWORDS [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " " + PREFIX_NAME + " alice charlie";
 
     public static final String MESSAGE_FIND_PERSON_UNSUCCESSFUL = "No contacts found.";
 
