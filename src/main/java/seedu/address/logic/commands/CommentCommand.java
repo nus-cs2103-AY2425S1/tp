caf8d8949b9ef315b62811eafd377d07d93f43ad
Judy1x4 <e1129873@u.nus.edu>
2024-10-16 21:49:14 +0800

    Add CommentCommandParser class and Test class

--- src/main/java/seedu/address/logic/commands/CommentCommand.java
+++ src/main/java/seedu/address/logic/commands/CommentCommand.java
@@ -1,14 +1,62 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
+/**
+ * Changes the comment of an existing person identified using the displayed index from the address book.
+ */
 public class CommentCommand extends Command {
 
-    public static final String COMMAND_WORD = "remark";
+    public static final String COMMAND_WORD = "comment";
 
-    @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from comment");
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the comment of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing comment will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [COMMENT]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "r/ Likes to swim.";
+
+    public static final String MESSAGE_ARGUMENTS = " Index: %1$d, Comemnt: %2$s";
+
+    private final Index index;
+    private final String comment;
+
+    /**
+     * @param index of the person in the filtered person list to edit the comment
+     * @param comment of the person to be updated to
+     */
+    public CommentCommand(Index index, String comment) {
+        requireAllNonNull(index, comment);
+
+        this.index = index;
+        this.comment = comment;
     }
 
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(
+                String.format(MESSAGE_ARGUMENTS, index.getOneBased(), comment));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof CommentCommand)) {
+            return false;
+        }
+
+        CommentCommand e = (CommentCommand) other;
+        return index.equals(e.index)
+                && comment.equals(e.comment);
+    }
 }
