cc09341b83581f45c8b331da5e963e29c1c95591
keerthigkaarthik <120106412+keerthigkaarthik@users.noreply.github.com>
2024-10-16 18:51:17 +0800

    Fix import order

--- src/main/java/seedu/address/logic/parser/eventcommandparser/EventDeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/eventcommandparser/EventDeleteCommandParser.java
@@ -4,8 +4,8 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.eventcommands.EventDeleteCommand;
-import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.EventParserUtil;
+import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
