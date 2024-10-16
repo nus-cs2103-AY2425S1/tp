7d82a2cfc9332ae6b440598093003b763f360671
gavinsoh <gavinsoh.02@gmail.com>
2024-10-16 20:30:03 +0800

    Add Testcases to PriorityCommandParserTest

--- src/test/java/seedu/address/logic/parser/PriorityCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/PriorityCommandParserTest.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,9 +20,35 @@ public class PriorityCommandParserTest {
 
     @Test
     public void parse_extraArgs_throwsParseException() {
-        // Too many arguments
         assertParseFailure(parser, " 1 id 5 level 2 extra",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, PriorityCommand.MESSAGE_USAGE));
     }
 
+    @Test
+    public void parse_validDeletePriority_success() {
+        String userInput = "deletelevel 1";
+        PriorityCommand expectedCommand = new PriorityCommand(1, 3, true);
+        assertParseSuccess(parser, userInput, expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidIndex_throwsParseException() {
+        assertParseFailure(parser, "abc /level 2",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, PriorityCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, "-1 /level 2",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, PriorityCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingLevel_throwsParseException() {
+        assertParseFailure(parser, "1",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, PriorityCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_incorrectCommandSyntax_throwsParseException() {
+        assertParseFailure(parser, "1 /level notANumber",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, PriorityCommand.MESSAGE_USAGE));
+    }
 }
