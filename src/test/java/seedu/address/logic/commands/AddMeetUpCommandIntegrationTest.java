c366c377245793855b033e9ae0166accecf97006
KennyLewi <kennylewi27@gmail.com>
2024-10-16 15:19:09 +0800

    Fix addMeetUp test case checkstyle

--- src/test/java/seedu/address/logic/commands/AddMeetUpCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddMeetUpCommandIntegrationTest.java
@@ -13,9 +13,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.meetup.MeetUp;
-import seedu.address.model.person.Person;
 import seedu.address.testutil.MeetUpBuilder;
-import seedu.address.testutil.PersonBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code AddMeetUpCommand}.
