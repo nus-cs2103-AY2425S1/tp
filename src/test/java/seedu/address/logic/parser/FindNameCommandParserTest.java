a425788289cdc3e8c72f4265bfe3f20ed70b9d21
lr <lruniapp1020@gmail.com>
2024-10-16 21:18:36 +0800

    Update FindNameCommand test Update FindNameCommand test

--- src/test/java/seedu/address/logic/parser/FindNameCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindNameCommandParserTest.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.FindNameCommand;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 
@@ -18,7 +17,8 @@ public class FindNameCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                FindNameCommand.MESSAGE_USAGE));
     }
 
     @Test
