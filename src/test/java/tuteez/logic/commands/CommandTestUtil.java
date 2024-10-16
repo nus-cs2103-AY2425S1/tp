0a7b1393a27a767cb6bc4eca07a7f6f6e8bc5f54
weijian <weijian9091@gmail.com>
2024-10-16 19:31:33 +0800

    Modify CommandTestUtil to use lessons

--- src/test/java/tuteez/logic/commands/CommandTestUtil.java
+++ src/test/java/tuteez/logic/commands/CommandTestUtil.java
@@ -67,10 +67,10 @@ public class CommandTestUtil {
     static {
         DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
                 .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withTags(VALID_TAG_FRIEND).build();
+                .withTags(VALID_TAG_FRIEND).withLessons(VALID_LESSON_DAY_AND_TME).build();
         DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
                 .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).withLessons(VALID_LESSON_DAY_AND_TME).build();
     }
 
     /**
