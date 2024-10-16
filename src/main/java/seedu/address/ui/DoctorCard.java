b8eefaa39260fbeab6af492895c56726e7bfbe21
sam-theman88 <samuel.lee9525@gmail.com>
2024-10-16 17:43:27 +0800

    Update GUI to have a flag for doctor contacts

--- src/main/java/seedu/address/ui/DoctorCard.java
+++ src/main/java/seedu/address/ui/DoctorCard.java
@@ -1,13 +1,18 @@
 package seedu.address.ui;
 
+import java.util.Comparator;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import seedu.address.model.doctor.Doctor;
 
 /**
  * A UI component that displays information of a {@code Doctor}.
  */
-public class DoctorCard extends PersonCard {
+public class DoctorCard extends UiPart<Region> {
 
     private static final String FXML = "DoctorListCard.fxml";
 
@@ -19,14 +24,42 @@ public class DoctorCard extends PersonCard {
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
 
+    public final Doctor doctor;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label doctorFlag;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
     @FXML
     private Label specialty;
+    @FXML
+    private FlowPane tags;
 
     /**
      * Creates a {@code DoctorCode} with the given {@code Doctor} and index to display.
      */
     public DoctorCard(Doctor doctor, int displayedIndex) {
-        super(doctor, displayedIndex);
+        super(FXML);
+        this.doctor = doctor;
+        id.setText(displayedIndex + ". ");
+        doctorFlag.setText("[DOCTOR]");
+        name.setText(doctor.getName().fullName);
+        phone.setText(doctor.getPhone().value);
+        address.setText(doctor.getAddress().value);
+        email.setText(doctor.getEmail().value);
         specialty.setText(doctor.getSpeciality().value);
+        doctor.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 }
