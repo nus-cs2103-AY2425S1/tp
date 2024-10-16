fbbdb3f9a22fb41161fc6c55912350b262852b20
saajidshaik02 <saajidshaik02@gmail.com>
2024-10-16 17:22:34 +0800

    Fix CodeCoverage issues

--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.testutil.PersonBuilder;
+import seedu.address.model.person.Person;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for UndoCommand.
@@ -27,14 +29,18 @@ public class UndoCommandTest {
 
     @Test
     public void execute_undoAvailable_success() {
-        // Simulate a state that can be undone by adding a person
-        model.addPerson(getTypicalAddressBook().getPersonList().get(0));
+        // Use a unique person to avoid DuplicatePersonException
+        Person uniquePerson = new PersonBuilder().build(); // Create a unique person
+
+        // Simulate adding a person to the model (which can then be undone)
+        model.addPerson(uniquePerson);
         model.saveAddressBook();
 
-        expectedModel.addPerson(getTypicalAddressBook().getPersonList().get(0));
+        // Do the same for expectedModel
+        expectedModel.addPerson(uniquePerson);
         expectedModel.saveAddressBook();
 
-        // Execute undo command
+        // Perform undo in both the model and expectedModel
         model.undoAddressBook();
         expectedModel.undoAddressBook();
 
@@ -43,7 +49,6 @@ public class UndoCommandTest {
 
     @Test
     public void execute_noUndoableState_failure() {
-        // Try to undo when there is no available state to undo
         assertCommandFailure(new UndoCommand(), model, UndoCommand.MESSAGE_FAILURE);
     }
 }
