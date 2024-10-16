78716948acf4eeaa1bd80631ddf65ed04cce7ad0
yixianggg <e1186210@u.nus.edu>
2024-10-16 23:18:41 +0800

    Fix checkstyle in DeleteTaskCommandParserTest

--- src/test/java/seedu/address/logic/parser/DeleteTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteTaskCommandParserTest.java
@@ -20,6 +20,7 @@ public class DeleteTaskCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteTaskCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteTaskCommand.MESSAGE_USAGE));
     }
 }
