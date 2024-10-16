a35393e904ad85dcbcd7e9dd869c5d5c7e672844
hovanleong <hovanleong1@gmail.com>
2024-10-16 16:44:39 +0800

    Create and implement EditVendorCommand and EditVendorDescriptor

---
+++ src/main/java/seedu/address/logic/commands/util/EditVendorDescriptor.java
@@ -0,0 +1,69 @@
+package seedu.address.logic.commands.util;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.Company;
+
+/**
+ * Stores the details to edit the vendor with. Each non-empty field value will replace the
+ * corresponding field value of the vendor.
+ */
+public class EditVendorDescriptor extends EditPersonDescriptor {
+    private Company company;
+
+    public EditVendorDescriptor() {
+    }
+
+    /**
+     * Copy constructor.
+     * A defensive copy of {@code tags} is used internally.
+     */
+    public EditVendorDescriptor(EditVendorDescriptor toCopy) {
+        super(toCopy);
+        setCompany(toCopy.company);
+    }
+
+    /**
+     * Returns true if at least one field is edited.
+     */
+    @Override
+    public boolean isAnyFieldEdited() {
+        return super.isAnyFieldEdited() || CollectionUtil.isAnyNonNull(company);
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Optional<Company> getCompany() {
+        return Optional.ofNullable(company);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof EditVendorDescriptor)) {
+            return false;
+        }
+        EditVendorDescriptor otherEditVendorDescriptor = (EditVendorDescriptor) other;
+        return super.equals(other)
+                && Objects.equals(company, otherEditVendorDescriptor.company);
+    }
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", getName().orElse(null))
+                .add("phone", getPhone().orElse(null))
+                .add("email", getEmail().orElse(null))
+                .add("address", getAddress().orElse(null))
+                .add("tags", getTags().orElse(null))
+                .add("company", company)
+                .toString();
+    }
+}
