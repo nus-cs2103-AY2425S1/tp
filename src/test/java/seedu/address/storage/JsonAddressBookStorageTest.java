3ecd2024def81c9c4c85723a93f9ac5374056449
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-10-16 18:34:11 +0800

    Update src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java

--- src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
+++ src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
@@ -105,6 +105,7 @@ public class JsonAddressBookStorageTest {
         } catch (IOException ioe) {
             throw new AssertionError("There should not be an error writing to the file.", ioe);
         }
+        
     }
     @Test
     public void saveAddressBook_nullFilePath_throwsNullPointerException() {
