014f91af002b81c404e5603292ae45852bd782d2
Leslie Yip <leslieyip02@gmail.com>
2024-10-17 14:35:54 +0800

    Change persons to contacts

--- src/main/java/seedu/ddd/logic/Messages.java
+++ src/main/java/seedu/ddd/logic/Messages.java
@@ -24,7 +24,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_CONTACT_DISPLAYED_INDEX = "The contact index provided is invalid";
     public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX = "The client index provided is invalid";
     public static final String MESSAGE_INVALID_VENDOR_DISPLAYED_INDEX = "The vendor index provided is invalid";
-    public static final String MESSAGE_CONTACTS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_CONTACTS_LISTED_OVERVIEW = "%1$d contacts listed!";
     public static final String MESSAGE_CLIENTS_LISTED_OVERVIEW = "%1$d clients listed!";
     public static final String MESSAGE_VENDORS_LISTED_OVERVIEW = "%1$d vendors listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
