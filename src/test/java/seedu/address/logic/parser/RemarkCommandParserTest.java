10df10994e4a82e7e948f0a31b47ffbb57990f5d
Liu Yinze <>
2024-10-17 23:56:30 +0800

    Delete unused importing lines

--- src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemarkCommandParserTest.java
@@ -2,13 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.RemarkCommand;
-import seedu.address.model.person.Remark;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
