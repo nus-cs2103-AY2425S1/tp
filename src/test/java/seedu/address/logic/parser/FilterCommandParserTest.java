d950168df461eb3f6e59f73362e7ca2a89cfd67c
Youngseo Park <98391497+youngseopark05@users.noreply.github.com>
2024-10-18 00:31:41 +0800

    Update FilterCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -1,110 +1,41 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.ALICE;
-import static seedu.address.testutil.TypicalPersons.BENSON;
-import static seedu.address.testutil.TypicalPersons.DANIEL;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
+import seedu.address.logic.commands.FilterCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.role.RoleContainsKeywordsPredicate;
 
-public class FilterCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+public class FilterCommandParserTest {
+
+    private final FilterCommandParser parser = new FilterCommandParser();
 
     @Test
-    public void equals() {
-        RoleContainsKeywordsPredicate firstPredicate =
-                new RoleContainsKeywordsPredicate(List.of("first"));
-        RoleContainsKeywordsPredicate secondPredicate =
-                new RoleContainsKeywordsPredicate(List.of("second"));
-
-        FilterCommand filterFirstCommand = new FilterCommand(firstPredicate);
-        FilterCommand filterSecondCommand = new FilterCommand(secondPredicate);
-
-        // same object -> returns true
-        assertTrue(filterFirstCommand.equals(filterFirstCommand));
-
-        // same values -> returns true
-        FilterCommand filterFirstCommandCopy = new FilterCommand(firstPredicate);
-        assertTrue(filterFirstCommand.equals(filterFirstCommandCopy));
-
-        // different types -> returns false
-        assertFalse(filterFirstCommand.equals(1));
-
-        // null -> returns false
-        assertFalse(filterFirstCommand.equals(null));
-
-        // different person -> returns false
-        assertFalse(filterFirstCommand.equals(filterSecondCommand));
+    public void parse_emptyArgs_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse(""));
     }
 
     @Test
-    public void execute_zeroKeywords_noPersonFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
-        RoleContainsKeywordsPredicate predicate = preparePredicate(" ");
-        FilterCommand command = new FilterCommand(predicate);
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    public void parse_validArgs_returnsFilterCommand() throws ParseException {
+        FilterCommand command = parser.parse("admin manager");
+        assertEquals(new FilterCommand(new RoleContainsKeywordsPredicate(Arrays.asList("admin", "manager"))), command);
     }
 
     @Test
-    public void execute_multipleKeywords_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-        RoleContainsKeywordsPredicate predicate = new RoleContainsKeywordsPredicate(
-                List.of("Athlete", "Volunteer", "Sponsor"));
-        FilterCommand command = new FilterCommand(predicate);
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(List.of(ALICE, BENSON, DANIEL), model.getFilteredPersonList());
-    }
-
-    private RoleContainsKeywordsPredicate preparePredicate(String userInput) {
-        return new RoleContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    public void parse_argsWithExtraSpaces_returnsFilterCommand() throws ParseException {
+        FilterCommand command = parser.parse("  admin   manager  ");
+        assertEquals(new FilterCommand(new RoleContainsKeywordsPredicate(Arrays.asList("admin", "manager"))), command);
     }
 
     @Test
-    public void execute_singleKeyword_singlePersonFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
-        RoleContainsKeywordsPredicate predicate = preparePredicate("Volunteer");
-        FilterCommand command = new FilterCommand(predicate);
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(List.of(BENSON), model.getFilteredPersonList());
-    }
-
-    @Test
-    public void execute_nonMatchingKeyword_noPersonFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
-        RoleContainsKeywordsPredicate predicate = preparePredicate("NonExistentRole");
-        FilterCommand command = new FilterCommand(predicate);
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
-    }
-
-    @Test
-    public void execute_caseInsensitiveKeywords_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-        RoleContainsKeywordsPredicate predicate = new RoleContainsKeywordsPredicate(
-                List.of("athlete", "volunteer", "sponsor"));
-        FilterCommand command = new FilterCommand(predicate);
-        expectedModel.updateFilteredPersonList(predicate);
-        assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(List.of(ALICE, BENSON, DANIEL), model.getFilteredPersonList());
+    public void parse_singleKeyword_returnsFilterCommand() throws ParseException {
+        FilterCommand command = parser.parse("admin");
+        assertEquals(new FilterCommand(new RoleContainsKeywordsPredicate(List.of("admin"))), command);
     }
 }
