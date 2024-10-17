32e90231014e45f598b90256c620f4196bfd5391
harithhh <e1156421@u.nus.edu>
2024-10-17 17:31:59 +0800

    Update checkstyle

--- src/test/java/seedu/address/logic/commands/FindAddressCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindAddressCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.DANIEL;
 import static seedu.address.testutil.TypicalPersons.GEORGE;
 import static seedu.address.testutil.TypicalPersons.getTypicalClientHub;
