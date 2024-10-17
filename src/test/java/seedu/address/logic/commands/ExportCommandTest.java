596a5e9d426de1c81b4b00ca8ed96bcbb9a21c0f
Shaun <Hundin231@gmail.com>
2024-10-17 16:27:24 +0800

    Fix bugs in export command and add more tests

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +17,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+
 
 /**
  * Test class for export command
@@ -28,8 +31,6 @@ public class ExportCommandTest {
                                                                         .resolve("exported_data.csv");
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
-
 
     @Test
     public void execute_exportCommand_successfulExport() throws CommandException {
@@ -53,5 +54,58 @@ public class ExportCommandTest {
             // Handle any IO exception
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void csvFormatTest() throws CommandException {
+        try {
+            Path importPath = projectRootPath.resolve("src").resolve("test").resolve("data")
+                                .resolve("JsonExportTest").resolve("typicalPersonsAddressBook.json");
+            ExportCommand exportCommand = new ExportCommand();
+            CommandResult commandResult = exportCommand.execute(model, importPath , exportFilePath);
+            assertEquals(String.format(ExportCommand.MESSAGE_SUCCESS),
+                    commandResult.getFeedbackToUser());
+            List<String> lines = Files.readAllLines(exportFilePath);
+            final String[] header = {"Name", "Class", "Phone number", "Tags"};
+            final String[] alice = {"Alice Pauline", "4A", "94351253", "friends"};
+            final String[] benson = {"Benson Meier", "6B", "98765432", "owesMoney friends"};
+            final String[] carl = {"Carl Kurz", "4A", "95352563", ""};
+            final String[][] testStuds = {header, alice, benson, carl};
+            for (int i = 0; i < lines.size() - 1; i++) {
+                String[] columns = lines.get(i).split(",");
+                for (int j = 0; j < lines.size(); j++) {
+                    System.out.println(testStuds[i][j]);
+                    System.out.println(columns[j]);
+                    assertEquals(testStuds[i][j], columns[j]);
+                }
+            }
+
+            // Clean up the exported file
+            Files.deleteIfExists(exportFilePath);
+        } catch (IOException e) {
+            // Handle any IO exception
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Requires the file to be unreadable. Thus, must be set unreadable locally.
+     * Use terminal command `chmod 000 <file>` to set file to unreadable.
+     * @throws IOException
+     */
+    @Test
+    void saveJsonFile_accessDenied_throwsCommandException() throws IOException {
+        Path importPath = projectRootPath.resolve("src").resolve("test").resolve("data")
+                                .resolve("JsonExportTest").resolve("accessDenied.json");
+
+        exportFilePath.toFile().setWritable(true);
+        ExportCommand exportCommand = new ExportCommand();
+
+        // Execute Test and Assert Outcome
+        CommandException thrown = assertThrows(CommandException.class, () -> {
+            exportCommand.execute(model , importPath, exportFilePath);
+        });
+        // Clean Up
+        Files.deleteIfExists(exportFilePath);
     }
 }
