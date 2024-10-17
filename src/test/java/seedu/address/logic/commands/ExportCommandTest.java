8bc43cae2cfadfb10a5560d4ff388bcea4cbe3a7
Nicholas Chua <e1114227@u.nus.edu>
2024-10-17 14:39:58 +0800

    Fix checkstyle rule violations

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
