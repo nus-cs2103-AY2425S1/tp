df9facb15f6a375561d542f2186b8a1af0114ec8
nigellqx <lownigel79@gmail.com>
2024-10-17 15:12:47 +0800

    Update AddCommand.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -37,7 +37,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_ORGANISATION + "National University of Singapore"
+            + PREFIX_ORGANISATION + "National University of Singapore "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney "
             + PREFIX_PRIORITY + "low";
