121dfb84d68955458803c8881cc8b2395a93af88
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-10-17 20:15:53 +0800

    Fix style

--- src/main/java/seedu/address/model/person/predicates/TierStartsWithSubstringPredicate.java
+++ src/main/java/seedu/address/model/person/predicates/TierStartsWithSubstringPredicate.java
@@ -1,10 +1,10 @@
 package seedu.address.model.person.predicates;
 
+import java.util.function.Predicate;
+
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Person;
-
-import java.util.function.Predicate;
 
 /**
  * Tests that a {@code Person}'s assigned {@code Tier} starts with a specified String.
--- src/test/java/seedu/address/model/person/predicates/TierStartsWithSubstringPredicateTest.java
+++ src/test/java/seedu/address/model/person/predicates/TierStartsWithSubstringPredicateTest.java
@@ -1,12 +1,13 @@
 package seedu.address.model.person.predicates;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import seedu.address.testutil.PersonBuilder;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PersonBuilder;
 
 public class TierStartsWithSubstringPredicateTest {
 
