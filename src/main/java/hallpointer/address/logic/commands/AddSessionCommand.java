9492dc7dd5f7a2e9e07e84468216c24b624b058e
QinHaichen12 <haichensurfs@gmail.com>
2024-10-17 11:10:58 +0800

    add unique check for adding session

--- src/main/java/hallpointer/address/logic/commands/AddSessionCommand.java
+++ src/main/java/hallpointer/address/logic/commands/AddSessionCommand.java
@@ -7,6 +7,7 @@ import static hallpointer.address.logic.parser.CliSyntax.PREFIX_SESSION_NAME;
 import static hallpointer.address.model.Model.PREDICATE_SHOW_ALL_MEMBERS;
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import hallpointer.address.commons.core.index.Index;
@@ -22,7 +23,8 @@ import hallpointer.address.model.session.Session;
 public class AddSessionCommand extends Command {
     public static final String COMMAND_WORD = "add_session";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a session to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds a session to the address book. "
             + "Parameters: "
             + PREFIX_SESSION_NAME + "NAME "
             + PREFIX_DATE + "DATE "
@@ -56,27 +58,31 @@ public class AddSessionCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        if (model.hasSession(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_SESSION);
-        }
-
         List<Member> lastShownList = model.getFilteredMemberList();
-
+        List<Member> memberToUpdate = new ArrayList<>();
         for (Index index : memberIndexes) {
             if (index.getZeroBased() >= lastShownList.size()) {
                 throw new CommandException(MESSAGE_INVALID_INDEX);
             }
             Member member = lastShownList.get(index.getZeroBased());
+            if (member.hasSession(toAdd)) {
+                throw new CommandException(MESSAGE_DUPLICATE_SESSION);
+            }
+            memberToUpdate.add(member);
+        }
+        for (Member member : memberToUpdate) {
             member.addSession(toAdd);
             model.setMember(member, member);
         }
 
         model.updateFilteredMemberList(PREDICATE_SHOW_ALL_MEMBERS);
-        return new CommandResult(String.format(MESSAGE_SUCCESS,
-                toAdd.getSessionName().toString(),
-                toAdd.getDate().toString(),
-                toAdd.getPoints().getValue(),
-                memberIndexes.size()));
+        return new CommandResult(
+                String.format(
+                        MESSAGE_SUCCESS,
+                        toAdd.getSessionName().toString(),
+                        toAdd.getDate().toString(),
+                        toAdd.getPoints().getValue(),
+                        memberIndexes.size()));
     }
 
     @Override
@@ -95,9 +101,7 @@ public class AddSessionCommand extends Command {
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this)
-                .add("toAdd", toAdd)
-                .toString();
+        return new ToStringBuilder(this).add("toAdd", toAdd).toString();
     }
 
 }
