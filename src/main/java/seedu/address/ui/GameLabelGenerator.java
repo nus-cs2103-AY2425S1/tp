13ab22994ffb5016989a03b407476c884bdb7af2
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-10-17 16:23:14 +0800

    Update GameLabelGenerator.java

--- src/main/java/seedu/address/ui/GameLabelGenerator.java
+++ src/main/java/seedu/address/ui/GameLabelGenerator.java
@@ -2,14 +2,26 @@ package seedu.address.ui;
 
 import javafx.scene.control.Label;
 import seedu.address.model.game.Game;
+import seedu.address.model.game.Role;
+import seedu.address.model.game.SkillLevel;
+import seedu.address.model.game.Username;
 
 public class GameLabelGenerator {
     public static Label gameLabel(Game game) {
         StringBuilder sb = new StringBuilder();
-        sb.append("").append(game.getGameName()).append("\n")
-          .append("Username: ").append(game.getUsername()).append("\n")
-                .append("Skill Lvl: ").append(game.getSkillLevel()).append("\n")
-                .append("Role: ").append(game.getRole());
+        Username username = game.getUsername();
+        SkillLevel skillLevel = game.getSkillLevel();
+        Role role = game.getRole();
+        sb.append(game.getGameName()).append("\n");
+        if (username != null) {
+            sb.append("Username: ").append(game.getUsername()).append("\n");
+        }
+        if (skillLevel !=null) {
+            sb.append("Skill Lvl: ").append(game.getSkillLevel()).append("\n");
+        }
+        if (role != null) {
+            sb.append("Role: ").append(game.getRole());
+        }
         return new Label(sb.toString());
     }
 }
