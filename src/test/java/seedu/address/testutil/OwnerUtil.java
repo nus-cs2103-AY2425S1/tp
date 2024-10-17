836a4641237e0d8db8e8c5e529622e4789d4d569
davidgohzk <david.gohzhekai@gmail.com>
2024-10-17 02:33:23 -0500

    fix checkstyle

--- src/test/java/seedu/address/testutil/OwnerUtil.java
+++ src/test/java/seedu/address/testutil/OwnerUtil.java
@@ -4,13 +4,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
-import java.util.Set;
 
 import seedu.address.logic.commands.AddOwnerCommand;
 import seedu.address.model.owner.Owner;
-import seedu.address.model.tag.Tag;
 
 /**
  * A utility class for Owner.
