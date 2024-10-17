bb124f4e310be60e444ff2b27b64a54b8c1817ce
kellywsq03 <kellywsq03@gmail.com>
2024-10-17 16:46:51 +0800

    Add test cases for EmergencyContact

--- src/test/java/seedu/address/storage/JsonAdaptedEmergencyContactTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedEmergencyContactTest.java
@@ -1,2 +1,90 @@
-package seedu.address.storage;public class JsonAdaptedEmergencyContactTest {
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedPerson.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.EmergencyContact;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.Relationship;
+
+public class JsonAdaptedEmergencyContactTest {
+    private static final String INVALID_ECNAME = "R@chel";
+    private static final String INVALID_ECPHONE = "+651234";
+    private static final String INVALID_ECRS = "Pet";
+    private static final String INVALID_ECRS_CHARACTERS = "@s5!ho";
+    private static final String VALID_ECNAME = BENSON.getEmergencyContact().getName().toString();
+    private static final String VALID_ECPHONE = BENSON.getEmergencyContact().getPhone().toString();
+    private static final String VALID_ECRS = BENSON.getEmergencyContact().getRelationship().toString();
+
+    @Test
+    public void toModelType_validEmergencyContactDetails_returnsEmergencyContact() throws Exception {
+        EmergencyContact emergencyContact = BENSON.getEmergencyContact();
+        JsonAdaptedEmergencyContact jsonAdaptedEmergencyContact = new JsonAdaptedEmergencyContact(emergencyContact);
+        assertEquals(jsonAdaptedEmergencyContact.toModelType(),
+                emergencyContact);
+    }
+
+    @Test
+    public void toModelType_invalidName_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact emergencyContact =
+                        new JsonAdaptedEmergencyContact(INVALID_ECNAME, VALID_ECPHONE, VALID_ECRS);
+        String expectedMessage = Name.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, emergencyContact::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullName_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact person = new JsonAdaptedEmergencyContact(null, VALID_ECPHONE, VALID_ECRS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidPhone_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact emergencyContact =
+                        new JsonAdaptedEmergencyContact(VALID_ECNAME, INVALID_ECPHONE, VALID_ECRS);
+        String expectedMessage = Phone.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, emergencyContact::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullPhone_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact emergencyContact =
+                new JsonAdaptedEmergencyContact(VALID_ECNAME, null, VALID_ECRS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, emergencyContact::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidRelationship_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact emergencyContact =
+            new JsonAdaptedEmergencyContact(VALID_ECNAME, VALID_ECPHONE, INVALID_ECRS);
+        String expectedMessage = Relationship.RELATIONSHIP_TYPE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, emergencyContact::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidAlphanumericRelationship_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact emergencyContact =
+                new JsonAdaptedEmergencyContact(VALID_ECNAME, VALID_ECPHONE, INVALID_ECRS_CHARACTERS);
+        String expectedMessage = Relationship.ALPHANUMERIC_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, emergencyContact::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullRelationship_throwsIllegalValueException() {
+        JsonAdaptedEmergencyContact emergencyContact =
+            new JsonAdaptedEmergencyContact(VALID_ECNAME, VALID_ECPHONE, null);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Relationship.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, emergencyContact::toModelType);
+    }
 }
