03837506eda3742a38a3f3a0769864384c7a8ae0
zacharyxzz <xiebawang@hotmail.com>
2024-10-17 20:14:48 +0800

    Fix style errors

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -21,7 +21,6 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.UnassignProductCommand;
-
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
