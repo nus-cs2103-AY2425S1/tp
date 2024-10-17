c21888dfa828c330f4cb623287eb3812c877e7bb
Isaac Chan <42537694+ozervesh@users.noreply.github.com>
2024-10-17 10:53:42 +0800

    Remove redundant imports in ExportCommandTest

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -2,9 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.ExportCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.logic.commands.ExportCommand.SUCCESS_MESSAGE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
