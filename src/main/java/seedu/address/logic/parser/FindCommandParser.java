38d31278bbbe237c1d2cb626c4e14d531c8100d7
Jing Hong Tee <e1122714@u.nus.edu>
2024-10-17 10:46:41 +0800

    Update variable name

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -25,9 +25,9 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String[] keywords = trimmedArgs.split("\\s+");
 
-        return new FindCommand(new DetailContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        return new FindCommand(new DetailContainsKeywordsPredicate(Arrays.asList(keywords)));
     }
 
 }
