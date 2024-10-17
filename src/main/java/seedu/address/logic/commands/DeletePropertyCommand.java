475ca2c0ac53d944145c6f28e5fa3f4bfc6ce5d8
lrongyi <iygnormal@gmail.com>
2024-10-17 22:21:00 +0800

    Create and test DeletePropertyCommand

--- src/main/java/seedu/address/logic/commands/DeletePropertyCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePropertyCommand.java
@@ -1,2 +1,71 @@
-package seedu.address.logic.commands;public class DeletePropertyCommand {
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Property;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class DeletePropertyCommand extends Command {
+    public static final String COMMAND_WORD = "delprop";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the client's property corresponding to the client's name.\n"
+            + "Parameters: CLIENT_NAME (case-insensitive)\n"
+            + "Example: " + COMMAND_WORD + " n/Tan Wen Xuan";
+
+    public static final String MESSAGE_DELETE_PROPERTY_SUCCESS = "Sucessfully deleted property from %1$s";
+
+    private final Name targetName;
+
+    public DeletePropertyCommand(Name targetName) {
+        this.targetName = targetName;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        Person personToDeleteProperty = model.getPersonByName(targetName);
+        if (!lastShownList.contains(personToDeleteProperty)) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_INPUT);
+        }
+
+        Person personWithoutProperty = new Person(personToDeleteProperty.getName(),
+                personToDeleteProperty.getPhone(), personToDeleteProperty.getEmail(),
+                personToDeleteProperty.getTags(), personToDeleteProperty.getAppointment(), new Property(""));
+        model.setPerson(personToDeleteProperty, personWithoutProperty);
+
+        return new CommandResult(String.format(MESSAGE_DELETE_PROPERTY_SUCCESS,
+                personToDeleteProperty.getName()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeletePropertyCommand)) {
+            return false;
+        }
+
+        DeletePropertyCommand otherDeletePropertyCommand = (DeletePropertyCommand) other;
+        return targetName.equals(otherDeletePropertyCommand.targetName);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("targetName", targetName)
+                .toString();
+    }
 }
--- src/main/java/seedu/address/logic/parser/DeletePropertyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePropertyCommandParser.java
@@ -1,2 +1,39 @@
-package seedu.address.logic.parser;public class DeletePropertyCommandParser {
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.DeletePropertyCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MISSING_CLIENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+public class DeletePropertyCommandParser implements Parser<DeletePropertyCommand> {
+    public DeletePropertyCommand parse(String args) throws ParseException {
+        String namePrefix = PREFIX_NAME.toString();
+        String trimmedArgs = args.trim();
+
+        // Check if the input contains the "n/" prefix
+        if (!trimmedArgs.startsWith(namePrefix)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeletePropertyCommand.MESSAGE_USAGE));
+        }
+
+        // Extract the name after "n/"
+        String nameString = trimmedArgs.substring(namePrefix.length()).trim();
+
+        // Check if the name is empty
+        if (nameString.isEmpty()) {
+            throw new ParseException(MISSING_CLIENT_NAME);
+        }
+
+        try {
+            // Parse the name and return the DeleteClientProfileCommand
+            Name name = ParserUtil.parseName(nameString);
+            return new DeletePropertyCommand(name);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeletePropertyCommand.MESSAGE_USAGE), pe);
+        }
+    }
 }
