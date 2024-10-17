edf8d5a08482cf9a9865b6c9d068dac1c37e5155
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-10-17 16:35:25 +0800

    Update src/main/java/seedu/address/MainApp.java

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -52,6 +52,7 @@ public class MainApp extends Application {
     public boolean getIsLoadError() {
         return isLoadError;
     }
+    
     @Override
     public void init() throws Exception {
         logger.info("=============================[ Initializing AddressBook ]===========================");
