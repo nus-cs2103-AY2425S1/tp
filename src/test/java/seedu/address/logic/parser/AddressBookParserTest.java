d28db833e6408c9cd3e0f7ac1d6ba756b06863f4
Zac <zacheryngky@gmail.com>
2024-10-17 19:34:29 +0800

    Add tests for find concert command

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -23,6 +23,7 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindConcertCommand;
 import seedu.address.logic.commands.FindPersonCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.LinkCommand;
@@ -100,7 +101,17 @@ public class AddressBookParserTest {
                 + PREFIX_NAME + keywords.stream().collect(Collectors.joining(" "));
         FindPersonCommand command = (FindPersonCommand) parser.parseCommand(
                 userInput);
-        assertEquals(new FindPersonCommand(new NameContainsKeywordsPredicate<>(keywords)), command);
+        assertEquals(new FindPersonCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    }
+
+    @Test
+    public void parseCommand_findConcert() throws Exception {
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        String userInput = FindConcertCommand.COMMAND_WORD + " "
+                + PREFIX_NAME + keywords.stream().collect(Collectors.joining(" "));
+        FindConcertCommand command = (FindConcertCommand) parser.parseCommand(
+                userInput);
+        assertEquals(new FindConcertCommand(new NameContainsKeywordsPredicate<>(keywords)), command);
     }
 
     @Test
