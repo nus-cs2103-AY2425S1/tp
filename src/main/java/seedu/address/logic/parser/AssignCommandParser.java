4f140134c463b681fcbccd7786944894b4d09dc7
Kan_Jun_Hong <junhongkannot@gmail.com>
2024-10-17 20:36:22 +0800

    Modify AssignCommand
    AssignCommand now uses the index of events and volunteers as
    identifiers instead of their id.

--- src/main/java/seedu/address/logic/parser/AssignCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AssignCommandParser.java
@@ -6,6 +6,7 @@ import static seedu.address.logic.parser.CliSyntax.ASSIGN_VOLUNTEER_PREFIX_NAME;
 
 import java.util.stream.Stream;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AssignCommand;
 import seedu.address.logic.commands.volunteercommands.VolunteerAddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -32,19 +33,19 @@ public class AssignCommandParser {
         // Ensure no duplicate prefixes are provided
         argMultimap.verifyNoDuplicatePrefixesFor(ASSIGN_VOLUNTEER_PREFIX_NAME, ASSIGN_EVENT_PREFIX_NAME);
 
-        int volunteerId;
-        int eventId;
+        Index volunteerId;
+        Index eventId;
 
         try {
-            volunteerId = Integer.parseInt(argMultimap.getValue(ASSIGN_VOLUNTEER_PREFIX_NAME)
+            volunteerId = VolunteerParserUtil.parseIndex(argMultimap.getValue(ASSIGN_VOLUNTEER_PREFIX_NAME)
                     .orElseThrow(() -> new ParseException("Volunteer ID is required.")
             ));
 
-            eventId = Integer.parseInt(argMultimap.getValue(ASSIGN_EVENT_PREFIX_NAME)
+            eventId = EventParserUtil.parseIndex(argMultimap.getValue(ASSIGN_EVENT_PREFIX_NAME)
                     .orElseThrow(() -> new ParseException("Event ID is required.")
             ));
         } catch (NumberFormatException e) {
-            throw new ParseException("ID must be a valid integer.");
+            throw new ParseException("Index must be a positive integer.");
         }
 
         return new AssignCommand(volunteerId, eventId);
