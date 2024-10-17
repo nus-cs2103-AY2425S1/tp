1200b5a8a725abc627fcc79db20b4ab368bdf6bd
Liu Yinze <>
2024-10-17 23:28:19 +0800

    Fix checkstyle problem about importing

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_HANDSOME;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_REMARK_PRETTY;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
@@ -14,8 +13,8 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
