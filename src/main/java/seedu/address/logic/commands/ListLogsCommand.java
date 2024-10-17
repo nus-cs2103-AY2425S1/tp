bd1e1c2d530f10a4e83ce2a9a64b2126f2b76849
Ze Rui <133195329+slidings@users.noreply.github.com>
2024-10-17 16:25:55 +0800

    Fix checkstsyle

--- src/main/java/seedu/address/logic/commands/ListLogsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListLogsCommand.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.IdentityNumber;
