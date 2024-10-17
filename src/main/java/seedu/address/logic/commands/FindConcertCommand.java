d0e47dd64698bed3a42c4ff108d874a78c26b0b1
Zac <zacheryngky@gmail.com>
2024-10-17 20:22:06 +0800

    Fix typo

--- src/main/java/seedu/address/logic/commands/FindConcertCommand.java
+++ src/main/java/seedu/address/logic/commands/FindConcertCommand.java
@@ -12,7 +12,7 @@ import seedu.address.model.commons.NameContainsKeywordsPredicate;
 import seedu.address.model.concert.Concert;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Finds and lists all concerts in address book whose name contains any of the argument keywords.
  * Keyword matching is case-insensitive.
  */
 public class FindConcertCommand extends Command {
