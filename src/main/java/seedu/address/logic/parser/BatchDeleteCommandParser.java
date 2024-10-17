2330f3a53b96de729c8b36afb3b2d1203a4d2815
MinkhantEain <khantkyaw.pkk@gmail.com>
2024-10-17 15:34:42 +0800

    Add batch delete command parser

---
+++ src/main/java/seedu/address/logic/parser/BatchDeleteCommandParser.java
@@ -0,0 +1,39 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.BatchDeleteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.PersonContainsTagsPredicate;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+public class BatchDeleteCommandParser implements Parser<BatchDeleteCommand> {
+    @Override
+    public BatchDeleteCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_TAG)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, BatchDeleteCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TAG);
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+
+        return new BatchDeleteCommand(tagList, new PersonContainsTagsPredicate(tagList));
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
