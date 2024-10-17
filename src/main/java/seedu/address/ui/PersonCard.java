ae79fb4900247e5b668cc0d67cd2022cd16915e1
Nicholascyx <nicholas.chongg@gmail.com>
2024-10-17 22:27:58 +0800

    Change variable names and remove certain imports

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import seedu.address.model.addresses.PublicAddress;
 import seedu.address.model.person.Person;
 
 import static seedu.address.model.addresses.Network.BTC;
