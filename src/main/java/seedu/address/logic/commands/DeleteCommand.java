86af377aa3e08542fa44f0f9fcf5e7d778fb64f9
t-leongchuan <156800445+t-leongchuan@users.noreply.github.com>
2024-10-17 22:28:00 +0800

    Revert "Add "delete contacts" feature"

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -4,11 +4,11 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.List;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.IdentityNumber;
 import seedu.address.model.person.Person;
 
 /**
@@ -25,31 +25,22 @@ public class DeleteCommand extends Command {
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    private final IdentityNumber identityNumber;
+    private final Index targetIndex;
 
-    public DeleteCommand(IdentityNumber identityNumber) {
-        this.identityNumber = identityNumber;
+    public DeleteCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
-        Person personToDelete = null;
 
-        // Find the person by identity number
-        for (Person person : lastShownList) {
-            if (person.getIdentityNumber().equals(identityNumber)) {
-                personToDelete = person;
-                break;
-            }
-        }
-
-        // If person was not found, throw an exception
-        if (personToDelete == null) {
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
+        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
@@ -66,13 +57,13 @@ public class DeleteCommand extends Command {
         }
 
         DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-        return identityNumber.equals(otherDeleteCommand.identityNumber);
+        return targetIndex.equals(otherDeleteCommand.targetIndex);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("identityNumber", identityNumber)
+                .add("targetIndex", targetIndex)
                 .toString();
     }
 }
