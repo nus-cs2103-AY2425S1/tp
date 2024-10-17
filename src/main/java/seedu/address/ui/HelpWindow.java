4a04c870b0b6347e90536e9df7a6b9b85f8edbc5
Shantaa <shanetay4425@gmail.com>
2024-10-17 20:56:12 +0800

    remove trailing whitespaces

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -85,37 +85,37 @@ public class HelpWindow extends UiPart<Stage> {
      */
     private void setHelpContent() {
         String userGuideContent = """
-            Financial Assurance Revolutionary Telemarketer (FART) is a **desktop app for managing contacts just for you 
-            Financial Advisors, optimized for use via a Command Line Interface** (CLI) 
-            while still having the benefits of a Graphical User Interface (GUI). 
-            If you can type fast, FART can get your contact management tasks done faster than traditional GUI apps.
+            Financial Assurance Revolutionary Telemarketer (FART) is a **desktop app for managing contacts just for you
+             Financial Advisors, optimized for use via a Command Line Interface** (CLI)
+             while still having the benefits of a Graphical User Interface (GUI).
+             If you can type fast, FART can get your contact management tasks done faster than traditional GUI apps.
 
             ## Quick start
 
             1. Ensure you have Java `17` or above installed in your Computer.
-               1. Should you require help, [here](https://www.oracle.com/java/technologies/downloads/#java17) is 
-               the download link to Java '17'.
-               2. After accessing the website, 
-               please choose the right download link for your operating system (Linux, macOS, or Windows).
+               1. Should you require help, [here](https://www.oracle.com/java/technologies/downloads/#java17) is
+                the download link to Java '17'.
+               2. After accessing the website,
+                please choose the right download link for your operating system (Linux, macOS, or Windows).
 
             2. Download the latest `.jar` file from [here](https://github.com/se-edu/addressbook-level3/releases).
 
             3. Copy the file to the folder you want to use as the _home folder_ for your FART.
                A good home folder would be the Downloads folder as it makes the next step easier.
 
-            4. Open a command terminal, `cd` into the folder you put the jar file in, 
-            and use the `java -jar fart_in_a.jar` command to run the application.
+            4. Open a command terminal, `cd` into the folder you put the jar file in,
+             and use the `java -jar fart_in_a.jar` command to run the application.
                A GUI similar to the below should appear in a few seconds. Note how the app contains some sample data.
                ![Ui](images/Ui.png)
 
-            5. Type the command in the command box and press Enter to execute it. e.g. typing **`help`** 
-            and pressing Enter will open the help window.
+            5. Type the command in the command box and press Enter to execute it. e.g. typing **`help`**
+             and pressing Enter will open the help window.
 
             Some example commands you can try:
 
                * `list` : Lists all contacts.
-               * `add n/John Doe p/98765432 e/johnd@example.com a/John street, block 123, #01-01 b/11 09 2001` : 
-               Adds a contact named `John Doe` to the FART book and automatically marks them as unpaid.
+               * `add n/John Doe p/98765432 e/johnd@example.com a/John street, block 123, #01-01 b/11 09 2001` :
+                Adds a contact named `John Doe` to the FART book and automatically marks them as unpaid.
                * `delete 3` : Deletes the 3rd contact shown in the current list.
                * `paid 3` : Marks the 3rd contact shown in the current list as paid.
                * `clear` : Deletes all contacts.
@@ -164,8 +164,8 @@ public class HelpWindow extends UiPart<Stage> {
 
             ### Saving the data
 
-            FART data is saved in the hard disk automatically after any command that changes the data. 
-            There is no need to save manually.
+            FART data is saved in the hard disk automatically after any command that changes the data.
+             There is no need to save manually.
 
             ### Editing the data file
 
@@ -175,13 +175,13 @@ public class HelpWindow extends UiPart<Stage> {
 
             Action | Format, Examples
             --------|------------------
-            **Add** | `add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS b/BIRTHDAY [t/TAG]…​` <br> 
-            e.g., `add n/James Ho p/22224444 e/jamesho@example.com a/123, 
-            Clementi Rd, 1234665 b/11 09 2001 t/friend t/colleague`
+            **Add** | `add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS b/BIRTHDAY [t/TAG]…​` <br>
+             e.g., `add n/James Ho p/22224444 e/jamesho@example.com a/123,
+             Clementi Rd, 1234665 b/11 09 2001 t/friend t/colleague`
             **Clear** | `clear`
             **Delete** | `delete INDEX`<br> e.g., `delete 3`
-            **Edit** | `edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG]…​`<br> 
-            e.g.,`edit 2 n/James Lee e/jameslee@example.com`
+            **Edit** | `edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG]…​`<br>
+             e.g.,`edit 2 n/James Lee e/jameslee@example.com`
             **Find** | `find KEYWORD [MORE_KEYWORDS]`<br> e.g., `find James Jake`
             **List** | `list`
             **Help** | `help`
