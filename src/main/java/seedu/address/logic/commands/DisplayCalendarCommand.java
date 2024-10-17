6ea18219f447587c3968e47336cc55e9e8c3f0b8
kimxw <kimaya.wanjari@nuscomputing.com>
2024-10-17 22:26:43 +0800

    Create a calendar command and add to parse cases

---
+++ src/main/java/seedu/address/logic/commands/DisplayCalendarCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_STUDENTS;
+
+/**
+ * Lists all students in the address book to the user.
+ */
+public class DisplayCalendarCommand extends Command {
+
+    public static final String COMMAND_WORD = "dc";
+
+    public static final String MESSAGE_SUCCESS = "Displayed all lessons";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.DisplayCalendarCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -78,6 +79,9 @@ public class AddressBookParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case DisplayCalendarCommand.COMMAND_WORD:
+            return new DisplayCalendarCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
