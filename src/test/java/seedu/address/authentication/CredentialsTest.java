9539bca7f6f2bf4f02182f7a7dade917bf4f68a7
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-10-17 18:53:23 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/authentication/CredentialsTest.java
+++ src/test/java/seedu/address/authentication/CredentialsTest.java
@@ -1,10 +1,5 @@
 package seedu.address.authentication;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
 public class CredentialsTest {
     // @Test
     // public void testFindUser() {
