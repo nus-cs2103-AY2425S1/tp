61c531527eae3261fc1bcafb65c82e22d04f3048
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2024-10-17 22:53:51 +0800

    Update Credential Test

--- src/test/java/seedu/address/authentication/CredentialsTest.java
+++ src/test/java/seedu/address/authentication/CredentialsTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class CredentialsTest {
-     @Test
-     public void testFindUser() {
-         Credentials testing = new Credentials("/config/test.txt");
-         assertTrue(testing.findUser("test1", "password"));
-         assertTrue(testing.findUser("funnyfella", "heeheehaha@123"));
-         assertFalse(testing.findUser("johndee", "password"));
-     }
+    @Test
+    public void testFindUser() {
+        Credentials testing = new Credentials("/config/test.txt");
+        assertTrue(testing.findUser("test1", "password"));
+        assertTrue(testing.findUser("funnyfella", "heeheehaha@123"));
+        assertFalse(testing.findUser("johndee", "password"));
+    }
 }
