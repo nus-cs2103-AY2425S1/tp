a30dfd0741b4a833b058911751e513d67455d773
ckclion <118067503+ckclion@users.noreply.github.com>
2024-10-17 11:59:06 +0800

    Add more tests

--- src/test/java/seedu/address/model/tag/GradeTest.java
+++ src/test/java/seedu/address/model/tag/GradeTest.java
@@ -1,8 +1,13 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Name;
 
 public class GradeTest {
 
@@ -18,8 +23,39 @@ public class GradeTest {
     }
 
     @Test
+    public void constructor_validGradeIndex() {
+        String validGradeIndex = "1";
+        assertEquals(Grade.class, new Grade(validGradeIndex).getClass());
+    }
+
+    @Test
     public void isValidGradeName() {
         // null tag name
         assertThrows(NullPointerException.class, () -> Grade.isValidGradeName(null));
+    }
+
+    @Test
+    public void equals() {
+        Grade grade = new Grade("2");
+
+        // same values -> returns true
+        assertTrue(grade.equals(new Grade("2")));
+
+        // same object -> returns true
+        assertTrue(grade.equals(grade));
+
+        // null -> returns false
+        assertFalse(grade.equals(null));
+
+        // different types -> returns false
+        assertFalse(grade.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(grade.equals(new Name("3")));
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("[Failing]", new Grade("4").toString());
     }
 }
