e9041d7984856cfe4505fb210e3d8aedcce0b4ce
zienxu <zienxu.ang@gmail.com>
2024-10-18 14:40:06 +0800

    Fix bugs in test cases

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -29,25 +29,25 @@ public class TypicalPersons {
             .withEmail("alice@example.com")
             .withPhone("94351253")
             .withTelegramHandle("@alicepauline")
-            .withTags("friends").build();
+            .withTags("friends").withContactType("Work").build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withEmail("johnd@example.com").withPhone("98765432")
             .withTelegramHandle("@bensonmeier")
-            .withTags("owesMoney", "friends").withContactType("Personal").build();
+            .withTags("owesMoney", "friends").withContactType("Work").build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withTelegramHandle("@carlkurz").withContactType("Personal").build();
+            .withEmail("heinz@example.com").withTelegramHandle("@carlkurz").withContactType("Work").build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com").withTelegramHandle("@danielmeier").withTags("friends")
             .withContactType("Personal").build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com").withTelegramHandle("@ellemeyer")
-            .withContactType("Work").build();
+            .withContactType("Personal").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
             .withEmail("lydia@example.com").withTelegramHandle("@fionakunz")
-            .withContactType("Work").build();
+            .withContactType("Personal").build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withTelegramHandle("@georgebest")
-            .withContactType("Work").build();
+            .withContactType("Personal").build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
