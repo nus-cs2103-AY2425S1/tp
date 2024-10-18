5b1034ec6eb63c58442ecdbd542346c054a99ab3
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-10-19 01:35:02 +0800

    fix Checkstyle

--- src/test/java/seedu/address/ui/CommandHistoryTest.java
+++ src/test/java/seedu/address/ui/CommandHistoryTest.java
@@ -1,9 +1,12 @@
 package seedu.address.ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 public class CommandHistoryTest {
 
