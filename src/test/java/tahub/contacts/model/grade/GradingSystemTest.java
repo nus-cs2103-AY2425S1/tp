2616e2cab507b24bbc109fc2ca909b758b6f2d83
devivy <divyalath.nus@gmail.com>
2024-10-18 12:21:25 +0800

    Imporve build pt5: Add Changes in ordering

--- src/test/java/tahub/contacts/model/grade/GradingSystemTest.java
+++ src/test/java/tahub/contacts/model/grade/GradingSystemTest.java
@@ -1,9 +1,11 @@
 package tahub.contacts.model.grade;
 
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.Map;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class GradingSystemTest {
 
