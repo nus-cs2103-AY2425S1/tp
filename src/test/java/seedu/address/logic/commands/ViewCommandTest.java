b4eb78ea3916c5abf19ffe0f996ca90c7f8b8b98
Anvita2110 <e1070535@u.nus.edu>
2024-10-18 12:46:04 +0800

    Add test cases for view command

---
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -0,0 +1,93 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.Messages.MESSAGE_PRODUCTS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalProducts.APPLE;
+import static seedu.address.testutil.TypicalProducts.DATE;
+import static seedu.address.testutil.TypicalProducts.HONEYDEW;
+import static seedu.address.testutil.TypicalSuppliers.getTypicalAddressBook;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.product.ProductNameContainsKeywordsPredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code ViewCommand}.
+ */
+public class ViewCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        ProductNameContainsKeywordsPredicate firstPredicate =
+                new ProductNameContainsKeywordsPredicate(Collections.singletonList("first"));
+        ProductNameContainsKeywordsPredicate secondPredicate =
+                new ProductNameContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        ViewCommand findFirstCommand = new ViewCommand(firstPredicate);
+        ViewCommand findSecondCommand = new ViewCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        ViewCommand findFirstCommandCopy = new ViewCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(findFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(findFirstCommand.equals(null));
+
+        // different supplier -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
+
+    @Test
+    public void execute_zeroKeywords_noProductFound() {
+        String expectedMessage = String.format(MESSAGE_PRODUCTS_LISTED_OVERVIEW, 0);
+        ProductNameContainsKeywordsPredicate predicate = preparePredicate(" ");
+        ViewCommand command = new ViewCommand(predicate);
+        expectedModel.updateFilteredProductList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredProductList());
+    }
+
+    @Test
+    public void execute_multipleKeywords_multipleProductsFound() {
+        String expectedMessage = String.format(MESSAGE_PRODUCTS_LISTED_OVERVIEW, 3);
+        ProductNameContainsKeywordsPredicate predicate = preparePredicate("APPLE HONEYDEW DATE");
+        ViewCommand command = new ViewCommand(predicate);
+        expectedModel.updateFilteredProductList(predicate);
+        System.out.println(command.execute(model).toString());
+        System.out.println(expectedMessage);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(APPLE, HONEYDEW, DATE), model.getFilteredProductList());
+    }
+
+    @Test
+    public void toStringMethod() {
+        ProductNameContainsKeywordsPredicate predicate = new ProductNameContainsKeywordsPredicate(Arrays.asList("keyword"));
+        ViewCommand viewCommand = new ViewCommand(predicate);
+        String expected = ViewCommand.class.getCanonicalName() + "{predicate=" + predicate + "}";
+        assertEquals(expected, viewCommand.toString());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code ProductNameContainsKeywordsPredicate}.
+     */
+    private ProductNameContainsKeywordsPredicate preparePredicate(String userInput) {
+        return new ProductNameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+}
