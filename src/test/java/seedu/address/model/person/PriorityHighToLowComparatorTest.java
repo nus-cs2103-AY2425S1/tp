9c6387aa1693b37600d8523dad052eadd778b2d0
Sparsh Kumar <e1121161@u.nus.edu>
2024-10-19 20:56:50 +0800

    Add more test cases

---
+++ src/test/java/seedu/address/model/person/PriorityHighToLowComparatorTest.java
@@ -0,0 +1,19 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PriorityHighToLowComparatorTest {
+
+    @Test
+    public void equals() {
+        PriorityHighToLowComparator highToLowComparator = new PriorityHighToLowComparator();
+        PriorityLowToHighComparator lowToHighComparator = new PriorityLowToHighComparator();
+
+        assertTrue(highToLowComparator.equals(new PriorityHighToLowComparator()));
+
+        assertFalse(highToLowComparator.equals(lowToHighComparator));
+    }
+}
