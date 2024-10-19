17f49cb2c3af3900153102378729591d877bbf4e
Kan_Jun_Hong <junhongkannot@gmail.com>
2024-10-19 18:30:00 +0800

    Rename output jar file name
    changed output jar file name to Volunsync. Also updated the default
    list of events and volunteers generated for a new user.

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -20,25 +20,31 @@ public class SampleDataUtil {
         return new Volunteer[] {
             new Volunteer(new Name("Alex Yeoh"), new Phone("12345678"), new Email("AlexYeoh@gmail.com"),
                     new Date("2022-02-02"), new Time("00:00"), new Time("00:01")),
-            new Volunteer(new Name("Dominic"), new Phone("23456789"), new Email("dom@gmail.com"),
+            new Volunteer(new Name("Bob Tan"), new Phone("23456789"), new Email("bob@gmail.com"),
                     new Date("2022-03-02"), new Time("23:00"), new Time("00:32")),
-            new Volunteer(new Name("Carry"), new Phone("12345633"), new Email("carry@gmail.com"),
+            new Volunteer(new Name("Cathy Lee"), new Phone("12345633"), new Email("cathy@gmail.com"),
                     new Date("2022-02-23"), new Time("00:00"), new Time("12:34")),
-            new Volunteer(new Name("Dingsel"), new Phone("33345678"), new Email("dingsel@gmail.com"),
+            new Volunteer(new Name("David Ng"), new Phone("33345678"), new Email("david@gmail.com"),
                     new Date("2022-04-02"), new Time("00:55"), new Time("12:01")),
-            new Volunteer(new Name("Bagel"), new Phone("12232678"), new Email("bagel@gmail.com"),
+            new Volunteer(new Name("Emily Lim"), new Phone("12232678"), new Email("emily@gmail.com"),
                     new Date("2022-02-02"), new Time("00:00"), new Time("00:01")),
         };
     }
 
     public static Event[] getSampleEvents() {
         return new Event[] {
-            new Event(new EventName("birthday"), new Location("12 Crescent Road"),
+            new Event(new EventName("Charity Run"), new Location("12 Crescent Road"),
                     new seedu.address.model.event.Date("2002-02-02"), new seedu.address.model.event.Time("00:01"),
                     new seedu.address.model.event.Time("12:23")),
-            new Event(new EventName("graduation"), new Location("23 Springfield Avenue"),
+            new Event(new EventName("Food Donation Drive"), new Location("Bishan Park"),
                     new seedu.address.model.event.Date("2004-03-02"), new seedu.address.model.event.Time("12:01"),
                     new seedu.address.model.event.Time("12:23")),
+            new Event(new EventName("Blood Donation Drive"), new Location("Red Cross Centre"),
+                    new seedu.address.model.event.Date("2014-04-02"), new seedu.address.model.event.Time("15:00"),
+                    new seedu.address.model.event.Time("17:23")),
+            new Event(new EventName("Graduation"), new Location("LT 28"),
+                    new seedu.address.model.event.Date("2024-03-02"), new seedu.address.model.event.Time("07:00"),
+                    new seedu.address.model.event.Time("14:23")),
         };
     }
 
