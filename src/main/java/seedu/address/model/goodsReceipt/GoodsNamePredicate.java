12d3208bc9af984730d57b40ddb53c739772ef19
jialegitgud <jiale.official@gmail.com>
2024-10-19 11:04:19 +0800

    Add JavaDocs for new predicates

--- src/main/java/seedu/address/model/goodsReceipt/GoodsNamePredicate.java
+++ src/main/java/seedu/address/model/goodsReceipt/GoodsNamePredicate.java
@@ -2,6 +2,9 @@ package seedu.address.model.goodsReceipt;
 
 import java.util.function.Predicate;
 
+/**
+ * Tests that a {@code GoodsReceipt}'s {@code Goods} partially or fully matches the given {@code GoodsName}.
+ */
 public class GoodsNamePredicate implements Predicate<GoodsReceipt> {
     private final String name;
 
--- src/main/java/seedu/address/model/goodsReceipt/SupplierNamePredicate.java
+++ src/main/java/seedu/address/model/goodsReceipt/SupplierNamePredicate.java
@@ -4,6 +4,9 @@ import java.util.function.Predicate;
 
 import seedu.address.model.person.Name;
 
+/**
+ * Tests that a {@code GoodsReceipt} belongs to a supplier by {@code Name}.
+ */
 public class SupplierNamePredicate implements Predicate<GoodsReceipt> {
     private final Name name;
 
