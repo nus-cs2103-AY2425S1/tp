52b011dcad12ac6449b907c282226e8e2eae0713
dasha3412 <dashasychova777@gmail.com>
2024-10-20 00:24:05 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -9,7 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
-import seedu.address.model.wedding.Wedding;
 
 /**
  * Panel containing the list of persons.
