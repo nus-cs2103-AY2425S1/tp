2dd31cf3be1ea5b1cb2f3910177ec4b1e88d5229
jialegitgud <jiale.official@gmail.com>
2024-10-19 18:14:26 +0800

    Add missing test for equals

--- src/test/java/seedu/address/logic/commands/ListGoodsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListGoodsCommandTest.java
@@ -1,13 +1,21 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalGoods.getTypicalGoodsReceipts;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.parser.ListGoodsCommandParser;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.goods.GoodsCategories;
+import seedu.address.model.goodsReceipt.CategoryPredicate;
+import seedu.address.model.goodsReceipt.GoodsReceipt;
 
 /**
  * Contains integration tests for {@code ListGoodsCommand}
@@ -44,5 +52,21 @@ public class ListGoodsCommandTest {
     @Test
     public void execute_multipleKeywords_noGoodsReturned() {
         // TODO: Implement Test
+    }
+
+    @Test
+    public void equals() {
+        ListGoodsCommand cmd = new ListGoodsCommand(ListGoodsCommandParser.DUMMY_PREDICATE);
+        Predicate<GoodsReceipt> testPredicate = new CategoryPredicate(GoodsCategories.CONSUMABLES);
+        ListGoodsCommand unequalCmd = new ListGoodsCommand(testPredicate);
+
+        // Check if the object is equal to itself
+        assertTrue(cmd.equals(cmd));
+
+        // Check if the object is equal to command with different predicate
+        assertFalse(cmd.equals(unequalCmd));
+
+        // Check if the object is equal to an unrelated object
+        assertFalse(cmd.equals(1.0));
     }
 }
