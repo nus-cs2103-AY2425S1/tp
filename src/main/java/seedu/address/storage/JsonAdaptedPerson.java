afcbed938d65359bb7f2ed62bd5e50816776bd6f
izruff <rafaelky.dev@gmail.com>
2024-10-19 23:47:52 +0800

    Fix checkstyle

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -2,7 +2,6 @@ package seedu.address.storage;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
