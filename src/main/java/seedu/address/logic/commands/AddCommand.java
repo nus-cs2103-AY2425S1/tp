361fee2dd225cbb1397282b555f15dd98305783d
pradyuprasad <pradyu.sp@gmail.com>
2024-10-19 16:02:15 +0800

    amend example message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -36,7 +36,7 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "back pain "
+            + PREFIX_TAG + "backPain "
             + PREFIX_ROLE + "patient";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
