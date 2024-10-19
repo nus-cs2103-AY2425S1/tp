8c2b9c5964acda9d01251d81a9cb5038a96af8f4
CliffKoes <cliffkoes@gmail.com>
2024-10-19 21:14:23 +0800

    Fix invalid tutorial number showing wrong error message

--- src/main/java/seedu/address/logic/parser/MarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkCommandParser.java
@@ -2,9 +2,13 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
 
+import java.util.stream.Stream;
+
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.MarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Tutorial;
@@ -20,19 +24,22 @@ public class MarkCommandParser implements Parser<MarkCommand> {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
                 PREFIX_TUTORIAL);
 
-        Index index;
-        Tutorial tutorial;
-        try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
-            tutorial = new Tutorial(argMultimap.getValue(PREFIX_TUTORIAL).orElse(""));
-        } catch (ParseException ive) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    MarkCommand.MESSAGE_USAGE), ive);
-        } catch (IllegalArgumentException e) {
-            throw new ParseException(e.getMessage());
+        if (!arePrefixesPresent(argMultimap, PREFIX_TUTORIAL)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
         }
-
+        
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TUTORIAL);
+        Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        Tutorial tutorial = ParserUtil.parseTutorial(argMultimap.getValue(PREFIX_TUTORIAL).get());
 
         return new MarkCommand(index, tutorial);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
