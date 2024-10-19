1e32255749c0760fc7de846d985a7933c3d45215
haleemairfan <haleemairfan04@gmail.com>
2024-10-20 00:31:11 +0800

    Add partial search

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,30 +1,45 @@
 package seedu.address.model.person;
 
-import java.util.List;
 import java.util.function.Predicate;
-
-import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code Name} starts with the specified keyword.
+ * The comparison is case-insensitive.
  */
 public class NameContainsKeywordsPredicate implements Predicate<Person> {
+
+    /** The keyword to match against the person's name. */
     private final String keywords;
 
+    /**
+     * Constructs a {@code NameContainsKeywordsPredicate} with the specified keyword.
+     *
+     * @param keywords The keyword to match against a {@code Person}'s name.
+     */
     public NameContainsKeywordsPredicate(String keywords) {
         this.keywords = keywords.toLowerCase();
     }
 
+    /**
+     * Evaluates this predicate on the given {@code Person}.
+     *
+     * @param person The person whose name is to be tested.
+     * @return {@code true} if the {@code Person}'s full name starts with the specified keyword; {@code false} otherwise.
+     */
     @Override
     public boolean test(Person person) {
-        Name name = person.getName();
-        String fullName = name.fullName;
-        fullName = fullName.toLowerCase();
+        String fullName = person.getName().fullName.toLowerCase();
         return fullName.startsWith(this.keywords);
     }
 
-     @Override
+    /**
+     * Checks if this predicate is equal to another object.
+     *
+     * @param other The other object to compare against.
+     * @return {@code true} if the other object is the same instance or has the same keyword; {@code false} otherwise.
+     */
+    @Override
     public boolean equals(Object other) {
         if (other == this) {
             return true;
@@ -39,6 +54,11 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
         return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
     }
 
+    /**
+     * Returns a string representation of the {@code NameContainsKeywordsPredicate}.
+     *
+     * @return A string that includes the class name and the keyword.
+     */
     @Override
     public String toString() {
         return new ToStringBuilder(this).add("keywords", keywords).toString();
