523892eb06c41c2e6ac2becfcbea12e35b4c4db2
appleshill <eliot@u.nus.edu>
2024-10-19 22:49:53 +0800

    Fix issue with checkstyle

--- src/test/java/seedu/address/ui/HelpWindowTest.java
+++ src/test/java/seedu/address/ui/HelpWindowTest.java
@@ -1,6 +1,5 @@
 package seedu.address.ui;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
