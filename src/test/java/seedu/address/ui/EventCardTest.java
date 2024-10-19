0b3de8356f566d4cdc51b4f3635966638de3abf9
Lim Yixiang <limyixiang2@gmail.com>
2024-10-19 17:23:39 +0800

    Fix test case in EventCardTest

--- src/test/java/seedu/address/ui/EventCardTest.java
+++ src/test/java/seedu/address/ui/EventCardTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.EventDescription;
 import seedu.address.model.event.EventDuration;
@@ -20,15 +23,23 @@ public class EventCardTest extends ApplicationTest {
     private Event event;
     private EventCard eventCard;
 
-    @BeforeEach
-    public void setUp() {
+    @Override
+    public void start(Stage stage) {
         event = new Event(new EventName("Meeting"), new EventDescription("Project discussion"),
                 new EventDuration(LocalDate.parse("2024-01-01"), LocalDate.parse("2024-01-02")));
         eventCard = new EventCard(event, 1);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(eventCard.getRoot());
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     @Test
     public void display() {
+        // Wait for JavaFX thread to complete
+        WaitForAsyncUtils.waitForFxEvents();
+
         // Verify that the event details are correctly displayed
         assertNotNull(eventCard);
         assertEquals("1. ", ((Label) eventCard.getRoot().lookup("#id")).getText());
