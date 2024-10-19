dedb5e16e87f751e2c7157089c00b54543741fe3
ckaayy <e1122115@u.nus.edu>
2024-10-19 19:03:36 +0800

    Allow ListExpiringPolicies Command to filter based on user input time frame
    ListExpiringPoliciesCommand can now take in an optional daysToExpiry
    argument.

--- src/main/java/seedu/address/logic/parser/ListExpiringPoliciesCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListExpiringPoliciesCommandParser.java
@@ -1,32 +1,44 @@
 package seedu.address.logic.parser;
 
+
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DAYS_FROM_EXPIRY;
 
 import seedu.address.logic.commands.ListExpiringPoliciesCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Parses input arguments and creates a new ListExpiringPoliciesCommand object
- * Currently ListExpiringPoliciesCommand has no arguments but this parser will support future
- * iterations when arguments are passed to ListExpiringPoliciesCommand to customise the time frame
  */
 public class ListExpiringPoliciesCommandParser implements Parser<ListExpiringPoliciesCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the ListExpiringPoliciesCommand
      * and returns a ListExpiringPoliciesCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format.
+     * @throws ParseException if the user input does not conform the expected format
      */
     public ListExpiringPoliciesCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_DAYS_FROM_EXPIRY);
 
-        // checks if input empty since no args
-        if (!argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    ListExpiringPoliciesCommand.MESSAGE_USAGE));
+        int days = 30; // Default value
+
+        // Check if the 'd/' prefix is provided
+        if (argMultimap.getValue(PREFIX_DAYS_FROM_EXPIRY).isPresent()) {
+            String daysString = argMultimap.getValue(PREFIX_DAYS_FROM_EXPIRY).get();
+            try {
+                days = Integer.parseInt(daysString);
+            } catch (NumberFormatException e) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        ListExpiringPoliciesCommand.MESSAGE_USAGE));
+            }
+
+            if (days <= 0) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        ListExpiringPoliciesCommand.MESSAGE_USAGE));
+            }
         }
 
-        return new ListExpiringPoliciesCommand();
+        // Create and return the command with the parsed days or default
+        return new ListExpiringPoliciesCommand(days);
     }
 }
-
