d3e776bbc857b9b99ed9f58782c9c185dd65ed6b
CliffKoes <cliffkoes@gmail.com>
2024-10-19 21:16:51 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/parser/MarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkCommandParser.java
@@ -2,13 +2,11 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
 
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.MarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Tutorial;
@@ -27,7 +25,7 @@ public class MarkCommandParser implements Parser<MarkCommand> {
         if (!arePrefixesPresent(argMultimap, PREFIX_TUTORIAL)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE));
         }
-        
+
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TUTORIAL);
         Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
         Tutorial tutorial = ParserUtil.parseTutorial(argMultimap.getValue(PREFIX_TUTORIAL).get());
