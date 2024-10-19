15e3d5f9655d408b31c449ce70282547df2a62ff
jolwnn <zhuolinwee@gmail.com>
2024-10-19 19:16:12 +0800

    Add tests for meeting book

--- src/test/java/seedu/address/testutil/TypicalMeetings.java
+++ src/test/java/seedu/address/testutil/TypicalMeetings.java
@@ -19,6 +19,15 @@ public class TypicalMeetings {
     public static final Meeting MEETING_BEDOK = new MeetingBuilder()
             .withMeetingTitle("Bedok Villa Finalizing Purchase Agreement")
             .withMeetingDate("11-06-2024").build();
+
+    // Manually added
+    public static final Meeting MEETING_CLEMENTI = new MeetingBuilder()
+            .withMeetingTitle("Clementi Apartment Lease Agreement Discussion")
+            .withMeetingDate("01-01-2025").build();
+
+    public static final Meeting MEETING_DOVER = new MeetingBuilder()
+            .withMeetingTitle("Dover condo client viewing")
+            .withMeetingDate("02-01-2025").build();
     private TypicalMeetings() {} // prevents instantiation
     /**
      * Returns an {@code MeetingBook} with all the typical persons.
