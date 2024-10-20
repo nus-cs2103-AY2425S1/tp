0ce30511e7cdb8fbecadf6924ea4c9a513d58ae9
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-20 15:07:54 +0800

    Resolve style issues

--- src/main/java/seedu/address/model/tutorial/UniqueTutorialList.java
+++ src/main/java/seedu/address/model/tutorial/UniqueTutorialList.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import seedu.address.model.tutorial.exceptions.DuplicateTutorialException;
 import seedu.address.model.tutorial.exceptions.TutorialNotFoundException;
 /**
