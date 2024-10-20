e43d0807f7da59b27d85149cb5de451610ff551a
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-10-20 22:27:33 +0800

    Implement sort functionality and ensure existing test cases pass

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,21 +1,26 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
+import static java.util.Objects.requireNonNull;
+
+import java.util.Comparator;
+
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
- * Sorts all contacts by name in the all contact list.
+ * Sorts all contacts by name in the contact list.
  */
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
-    public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": sorts contacts in the contact list by name";
-
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
-            "Sort command not implemented yet";
+    public static final String MESSAGE_SUCCESS = "Sorted all persons by name";
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException(MESSAGE_NOT_IMPLEMENTED_YET);
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        // Call the model's sortPersonList method with a comparator that sorts by name
+        model.sortPersonList(Comparator.comparing(person -> person.getName().toString()));
+
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
