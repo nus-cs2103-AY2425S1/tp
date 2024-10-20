2258d8b764da9c2b4d6a5812869b52cc4e703b5c
miloaisdino <miloaisdino@gmail.com>
2024-10-21 01:57:41 +0800

    refactor tests

--- src/test/java/tahub/contacts/AppParametersTest.java
+++ src/test/java/tahub/contacts/AppParametersTest.java
@@ -1,6 +1,9 @@
 package tahub.contacts;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
