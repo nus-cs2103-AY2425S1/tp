f04748970e1af489a59fea19d34d419d13a1294e
lm-44 <yauchengyong.literallyme@gmail.com>
2024-10-20 17:27:32 +0800

    Fix help message of view_student command

--- src/main/java/seedu/address/logic/parser/AddAssignmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAssignmentCommandParser.java
@@ -38,7 +38,7 @@ public class AddAssignmentCommandParser implements Parser<AddAssignmentCommand> 
         try {
             studentIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_STUDENT_INDEX).get());
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddAssignmentCommand.MESSAGE_USAGE), pe);
         }
         AssignmentName assignmentName = ParserUtil.parseAssignmentName(argMultimap.getValue(PREFIX_ASSIGNMENT_NAME)
                 .get());
