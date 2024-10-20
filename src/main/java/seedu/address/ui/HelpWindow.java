dc153a4f6edfc5a230074d1dd47a24e5a82b4022
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-10-21 01:48:33 +0800

    Add TableView to help window

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -5,46 +5,40 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.commands.HelpCommand;
 
 /**
  * Controller for a help page
  */
 public class HelpWindow extends UiPart<Stage> {
 
+    private static class HelpCommand {
+        private final String command;
+        private final String description;
+
+        public HelpCommand(String command, String description) {
+            this.command = command;
+            this.description = description;
+        }
+
+        public String getHelpCommand() {
+            return command;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
     public static final String USERGUIDE_URL = "https://ay2425s1-cs2103t-t12-4.github.io/tp/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide for more info: " + USERGUIDE_URL
-            + "\n **Add**    \n" +
-            "`add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS [g/GAME]… [t/TAG]…\u200B`  \n" +
-            "e.g., `add n/James Ho p/22224444 e/jamesho@example.com a/123, Clementi Rd, 1234665 g/Overwatch t/friend t/colleague`\n" +
-            "\n" +
-            "**Clear**  \n" +
-            "`clear`\n" +
-            "\n" +
-            "**Delete**  \n" +
-            "`delete INDEX`  \n" +
-            "e.g., `delete 3`\n" +
-            "\n" +
-            "**Edit**  \n" +
-            "`edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [g/Game]… [t/TAG]…\u200B`  \n" +
-            "e.g., `edit 2 n/James Lee e/jameslee@example.com`\n" +
-            "\n" +
-            "**Edit Game**  \n" +
-            "`editgame INDEX g/GAME [u/USERNAME] [s/SKILLLEVEL] [r/ROLE]\u200B`  \n" +
-            "e.g., `editgame 1 g/Overwatch u/Potato`\n" +
-            "\n" +
-            "**Find**  \n" +
-            "`find KEYWORD [MORE_KEYWORDS]`  \n" +
-            "e.g., `find James Jake`\n" +
-            "\n" +
-            "**List**  \n" +
-            "`list`\n" +
-            "\n" +
-            "**Help**  \n" +
-            "`help`\n";
+    public static final String HELP_MESSAGE = "Refer to the user guide for more info: " + USERGUIDE_URL;
 
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
@@ -55,6 +49,8 @@ public class HelpWindow extends UiPart<Stage> {
 
     @FXML
     private Label helpMessage;
+    @FXML
+    private TableView<HelpCommand> helpTable;
 
     /**
      * Creates a new HelpWindow.
@@ -63,7 +59,59 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
+        TableView<HelpCommand> table = new TableView<>();
+
+        TableColumn<HelpCommand, String> commandColumn = new TableColumn<>("HelpCommand");
+        commandColumn.setCellValueFactory(new PropertyValueFactory<>("helpCommand"));
+
+        TableColumn<HelpCommand, String> descriptionColumn = new TableColumn<>("Description");
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+        table.getColumns().add(commandColumn);
+        table.getColumns().add(descriptionColumn);
+
+        // Add data to the table
+        table.getItems().addAll(
+                new HelpCommand("Add", "`add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS [g/GAME]… [t/TAG]…`"),
+                new HelpCommand("Clear", "`clear`"),
+                new HelpCommand("Delete", "`delete INDEX` e.g., `delete 3`"),
+                new HelpCommand("Edit", "`edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [g/Game]… [t/TAG]…`"),
+                new HelpCommand("Edit Game", "`editgame INDEX g/GAME [u/USERNAME] [s/SKILLLEVEL] [r/ROLE]​`"),
+                new HelpCommand("Find", "`find KEYWORD [MORE_KEYWORDS]` e.g., `find James Jake`"),
+                new HelpCommand("List", "`list`"),
+                new HelpCommand("Help", "`help`")
+        );
+
+        helpTable = helpTable();
         helpMessage.setText(HELP_MESSAGE);
+    }
+
+
+    @FXML
+    private TableView<HelpCommand> helpTable() {
+        TableView<HelpCommand> table = new TableView<>();
+
+        TableColumn<HelpCommand, String> commandColumn = new TableColumn<>("HelpCommand");
+        commandColumn.setCellValueFactory(new PropertyValueFactory<>("helpCommand"));
+
+        TableColumn<HelpCommand, String> descriptionColumn = new TableColumn<>("Description");
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+        table.getColumns().add(commandColumn);
+        table.getColumns().add(descriptionColumn);
+
+        // Add data to the table
+        table.getItems().addAll(
+                new HelpCommand("Add", "`add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS [g/GAME]… [t/TAG]…`"),
+                new HelpCommand("Clear", "`clear`"),
+                new HelpCommand("Delete", "`delete INDEX` e.g., `delete 3`"),
+                new HelpCommand("Edit", "`edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [g/Game]… [t/TAG]…​`"),
+                new HelpCommand("Edit Game", "`editgame INDEX g/GAME [u/USERNAME] [s/SKILLLEVEL] [r/ROLE]​`"),
+                new HelpCommand("Find", "`find KEYWORD [MORE_KEYWORDS]` e.g., `find James Jake`"),
+                new HelpCommand("List", "`list`"),
+                new HelpCommand("Help", "`help`")
+        );
+        return table;
     }
 
     /**
