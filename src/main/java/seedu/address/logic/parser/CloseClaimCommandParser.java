ab301e1fce8f02650a3bd0449a9116df8ffcfa19
matthewyip1511 <e0938566@u.nus.ed>
2024-10-20 22:54:26 +0800

    Add main files for CloseClaimCommand Most of the previous commits were work-in-progress and done as I worked on the files This commit contains the working version of the CloseClaimCommand with the right files in place

--- src/main/java/seedu/address/logic/parser/CloseClaimCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CloseClaimCommandParser.java
@@ -1,15 +1,19 @@
 package seedu.address.logic.parser;
 
-import seedu.address.commons.core.index.Index;
-import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.AddClaimCommand;
-import seedu.address.logic.commands.CloseClaimCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLAIM_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INSURANCE_ID;
 
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.CloseClaimCommand;
+import seedu.address.logic.commands.DeleteClaimCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Creates an {@code CloseClaimCommand} object after parsing the user inputs.
+ */
 public class CloseClaimCommandParser implements Parser<CloseClaimCommand> {
     /**
      * Parses {@code userInput} into a command and returns it for execution.
@@ -20,30 +24,26 @@ public class CloseClaimCommandParser implements Parser<CloseClaimCommand> {
     public CloseClaimCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_INSURANCE_ID,
-                PREFIX_CLAIM_ID, PREFIX_CLAIM_AMOUNT);
+                PREFIX_CLAIM_ID);
 
         Index index;
         int insuranceId;
         String claimId;
-        int claimAmount;
 
         try {
             if (argMultimap.getValue(PREFIX_INSURANCE_ID).isEmpty()
-                    || argMultimap.getValue(PREFIX_CLAIM_ID).isEmpty()
-                    || argMultimap.getValue(PREFIX_CLAIM_AMOUNT).isEmpty()) {
+                    || argMultimap.getValue(PREFIX_CLAIM_ID).isEmpty()) {
                 throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                        AddClaimCommand.MESSAGE_USAGE));
+                        DeleteClaimCommand.MESSAGE_USAGE));
             }
 
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
             insuranceId = ParserUtil.parseInsurancePlan(argMultimap.getValue(PREFIX_INSURANCE_ID).get());
-
             claimId = ParserUtil.parseClaimId(argMultimap.getValue(PREFIX_CLAIM_ID).get());
-            claimAmount = ParserUtil.parseClaimAmount(argMultimap.getValue(PREFIX_CLAIM_AMOUNT).get());
         } catch (IllegalValueException ive) {
-            throw new ParseException(String.format(ive.getMessage(), AddClaimCommand.MESSAGE_USAGE), ive);
+            throw new ParseException(String.format(ive.getMessage(), DeleteClaimCommand.MESSAGE_USAGE), ive);
         }
 
-        return new CloseClaimCommand();
+        return new CloseClaimCommand(index, insuranceId, claimId);
     }
 }
