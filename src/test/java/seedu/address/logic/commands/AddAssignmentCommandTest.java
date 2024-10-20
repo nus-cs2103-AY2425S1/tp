85671ae1e3aa9596255fb4725059836c57c294d3
lm-44 <yauchengyong.literallyme@gmail.com>
2024-10-20 17:25:24 +0800

    Add more test cases

--- src/test/java/seedu/address/logic/commands/AddAssignmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddAssignmentCommandTest.java
@@ -1,13 +1,17 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ASSIGNMENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
 import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -35,18 +39,48 @@ public class AddAssignmentCommandTest {
                 new AddAssignmentCommand.AssignmentDescriptor(AssignmentBuilder.DEFAULT_MAX_SCORE,
                         new AssignmentName(AssignmentBuilder.DEFAULT_ASSIGNMENT_NAME));
         AddAssignmentCommand addAssignmentCommand = new AddAssignmentCommand(INDEX_FIRST_STUDENT, toAddDescriptor);
-        CommandResult commandResult = addAssignmentCommand.execute(model);
+        Student studentToAddAssignmentTo = model.getFilteredStudentList().get(INDEX_FIRST_STUDENT.getZeroBased());
+        Assignment expectedAssignment = new AssignmentBuilder().withStudent(studentToAddAssignmentTo).build();
+        Student studentWithAssignment = studentToAddAssignmentTo.addAssignment(expectedAssignment);
 
-        Student studentToAssignAssignmentTo = model.getFilteredStudentList().get(0);
-        Assignment expectedAssignment = new AssignmentBuilder().withStudent(studentToAssignAssignmentTo).build();
-        studentToAssignAssignmentTo = studentToAssignAssignmentTo.addAssignment(expectedAssignment);
-        expectedAssignment = studentToAssignAssignmentTo.getAssignmentList().get(0);
         String expectedMessage = String.format(AddAssignmentCommand.MESSAGE_SUCCESS,
                 Messages.format(expectedAssignment));
 
-        assertEquals(expectedMessage, commandResult.getFeedbackToUser());
-        // Fails because of bug
-        assertEquals(expectedAssignment, commandResult.getStudent().getAssignmentList()
-                .get(0));
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setStudent(studentToAddAssignmentTo, studentWithAssignment);
+
+        assertCommandSuccess(addAssignmentCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_duplicateAssignment_throwsCommandException() throws Exception {
+        AddAssignmentCommand.AssignmentDescriptor toAddDescriptor =
+                new AddAssignmentCommand.AssignmentDescriptor(AssignmentBuilder.DEFAULT_MAX_SCORE,
+                        new AssignmentName(AssignmentBuilder.DEFAULT_ASSIGNMENT_NAME));
+        AddAssignmentCommand addAssignmentCommand = new AddAssignmentCommand(INDEX_FIRST_STUDENT, toAddDescriptor);
+        addAssignmentCommand.execute(model);
+
+        AddAssignmentCommand.AssignmentDescriptor duplicateToAddDescriptor =
+                new AddAssignmentCommand.AssignmentDescriptor(AssignmentBuilder.DEFAULT_MAX_SCORE,
+                        new AssignmentName(AssignmentBuilder.DEFAULT_ASSIGNMENT_NAME));
+        AddAssignmentCommand duplicateAddAssignmentCommand = new AddAssignmentCommand(INDEX_FIRST_STUDENT, duplicateToAddDescriptor);
+
+        assertCommandFailure(duplicateAddAssignmentCommand, model, AddAssignmentCommand.MESSAGE_DUPLICATE_ASSIGNMENT);
+    }
+
+    @Test
+    public void execute_emojiAssignment_throwsCommandException() throws Exception {
+        AddAssignmentCommand.AssignmentDescriptor toAddDescriptor =
+                new AddAssignmentCommand.AssignmentDescriptor(AssignmentBuilder.DEFAULT_MAX_SCORE,
+                        new AssignmentName(new String(Character.toChars(0x1F349))));
+        AddAssignmentCommand addAssignmentCommand = new AddAssignmentCommand(INDEX_FIRST_STUDENT, toAddDescriptor);
+        addAssignmentCommand.execute(model);
+
+        AddAssignmentCommand.AssignmentDescriptor duplicateToAddDescriptor =
+                new AddAssignmentCommand.AssignmentDescriptor(AssignmentBuilder.DEFAULT_MAX_SCORE,
+                        new AssignmentName(AssignmentBuilder.DEFAULT_ASSIGNMENT_NAME));
+        AddAssignmentCommand duplicateAddAssignmentCommand = new AddAssignmentCommand(INDEX_FIRST_STUDENT, duplicateToAddDescriptor);
+
+        assertCommandFailure(duplicateAddAssignmentCommand, model, AddAssignmentCommand.MESSAGE_DUPLICATE_ASSIGNMENT);
     }
 }
