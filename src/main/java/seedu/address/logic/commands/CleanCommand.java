7f541860efdbe33b67a4d2f2f1da65dc2e0a8e2c
EdaYeo <wye.yeo@gmail.com>
2024-10-20 23:06:42 +0800

    Add clean command class

---
+++ src/main/java/seedu/address/logic/commands/CleanCommand.java
@@ -0,0 +1,72 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+
+
+/**
+ * Deletes a person identified using its displayed index from the address book.
+ */
+public class CleanCommand extends Command {
+
+    public static final String COMMAND_WORD = "clean";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the people past their graduation date in the displayed person list.\n"
+            + "No parameters\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_CLEAN_SUCCESS = "Deleted graduated people";
+
+    private final Predicate<Person> predicate;
+
+    public CleanCommand() {
+        // TO DO: CHANGE THIS TO USE A GradDate Predicate comparing to LocalDateTime.now()
+        this.predicate = p -> true;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        List<Person> lastShownList = model.getFilteredPersonList();
+        for (int i = 0; i < lastShownList.size(); i++) {
+            model.deletePerson(lastShownList.get(i));
+        }
+
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        return new CommandResult(MESSAGE_CLEAN_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof CleanCommand)) {
+            return false;
+        }
+
+        CleanCommand otherCleanCommand = (CleanCommand) other;
+        return this.predicate.equals(otherCleanCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("graduation date predicate", predicate)
+                .toString();
+    }
+}
