a69f7932fca8eb7ee63e05be604f713075ad05ca
DennieDan <e1129903@u.nus.edu>
2024-10-20 22:50:16 +0800

    Add tests for OweCommand class

--- src/test/java/seedu/address/logic/commands/OweCommandTest.java
+++ src/test/java/seedu/address/logic/commands/OweCommandTest.java
@@ -1,11 +1,16 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_OWED_AMOUNT_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_OWED_AMOUNT_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showStudentAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_OUT_OF_BOUNDS;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_STUDENT;
 import static seedu.address.testutil.TypicalStudents.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -65,5 +70,36 @@ public class OweCommandTest {
         showStudentAtIndex(model, INDEX_FIRST_STUDENT);
         OweCommand oweCommand = new OweCommand(Index.fromOneBased(2), new OwedAmount(VALID_OWED_AMOUNT_BOB));
         assertCommandFailure(oweCommand, model, Messages.MESSAGE_INVALID_STUDENT_DISPLAYED_INDEX);
+    }
+    
+    @Test
+    public void equals() {
+        OwedAmount standardOwedAmount = new OwedAmount(VALID_OWED_AMOUNT_BOB);
+        final OweCommand standardCommand = new OweCommand(INDEX_FIRST_STUDENT, standardOwedAmount);
+        
+        // same values -> returns true
+        OweCommand commandWithSameValues = new OweCommand(INDEX_FIRST_STUDENT, new OwedAmount(VALID_OWED_AMOUNT_BOB));
+        assertTrue(standardCommand.equals(commandWithSameValues));
+        
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+        
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+        
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new OweCommand(INDEX_SECOND_STUDENT, standardOwedAmount)));
+        
+        // different owedAmount -> returns false
+        assertFalse(standardCommand.equals(new OweCommand(INDEX_FIRST_STUDENT, new OwedAmount(VALID_OWED_AMOUNT_AMY))));
+    }
+    
+    @Test
+    public void toStringMethod() {
+        Index index = Index.fromOneBased(1);
+        OwedAmount updatedOwedAmount = new OwedAmount(VALID_OWED_AMOUNT_AMY);
+        OweCommand oweCommand = new OweCommand(index, updatedOwedAmount);
+        String expected = OweCommand.class.getCanonicalName() + "{index=" + index + ", updatedOwedAmount=" + updatedOwedAmount + "}";
+        assertEquals(expected, oweCommand.toString());
     }
 }
