85a4c8a8e99cf4cb5dff7831ed62d2c3761a3439
Jaehyeok Choi <77jack0105@gmail.com>
2024-10-20 16:59:38 +0800

    Add Clear Appointment class and test

---
+++ src/main/java/seedu/address/logic/commands/ClearAppointmentCommand.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.AppointmentBook;
+import seedu.address.model.Model;
+
+/**
+ * Clears the appointment book.
+ */
+public class ClearAppointmentCommand extends ClearCommand {
+    public static final String MESSAGE_SUCCESS = "Appointment has been cleared!";
+
+    protected void clearEntity(Model model) {
+        model.setAppointmentBook(new AppointmentBook());
+    }
+
+    /*
+     * Returns success message to display upon adding entity.
+     */
+    protected String getSuccessMessage() {
+        return MESSAGE_SUCCESS;
+    }
+}
