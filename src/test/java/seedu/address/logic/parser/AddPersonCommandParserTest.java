1b34c1ca2fd24ecfb6b3c45bd5c1229903c11486
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-10-20 21:17:54 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/AddPersonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddPersonCommandParserTest.java
@@ -26,7 +26,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_STATUS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
