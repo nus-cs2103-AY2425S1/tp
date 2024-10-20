f3b3b09611a5d8ba72d428225f813ecdc78b5a8e
TYiFan-hub <tyifan123@gmail.com>
2024-10-20 20:05:58 +0800

    Add logging to code

--- src/main/java/seedu/address/logic/parser/PayCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PayCommandParser.java
@@ -5,6 +5,9 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.PayCommand.MESSAGE_USAGE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_HOUR;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.PayCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -14,13 +17,12 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class PayCommandParser implements Parser<PayCommand> {
 
-    /**
-     * Parses the given {@code String} of arguments in the context of the PayCommand
-     * and returns a PayCommand object for execution.
-     * @throws ParseException if the user input does not conform to the expected format.
-     */
+    private static final Logger logger = Logger.getLogger(PayCommandParser.class.getName());
+
     public PayCommand parse(String args) throws ParseException {
         requireNonNull(args);
+        logger.log(Level.INFO, "Parsing PayCommand with arguments: " + args);
+
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_HOUR);
 
@@ -30,18 +32,31 @@ public class PayCommandParser implements Parser<PayCommand> {
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
+            logger.log(Level.WARNING, "Failed to parse index. Invalid command format.", pe);
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_HOUR);
+        try {
+            argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_HOUR);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Duplicate hour prefix found in the input.", e);
+        }
 
         if (argMultimap.getValue(PREFIX_HOUR).isPresent()) {
-            hour = ParserUtil.parseHoursPaid(argMultimap.getValue(PREFIX_HOUR).get());
+            try {
+                hour = ParserUtil.parseHoursPaid(argMultimap.getValue(PREFIX_HOUR).get());
+            } catch (ParseException pe) {
+                logger.log(Level.WARNING, "Failed to parse hours paid. Invalid value.", pe);
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE), pe);
+            }
         } else {
+            logger.log(Level.WARNING, "Failed to parse hours paid. Invalid parameters.");
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
         }
 
+        logger.log(Level.INFO,"Successfully parsed PayCommand.");
         return new PayCommand(index, hour);
     }
+
 
 }
