a4d1a04fcf48cc2295e5fccb02a4636811c97ad0
EdaYeo <wye.yeo@gmail.com>
2024-10-20 23:33:22 +0800

    Add clean command test

--- src/test/java/seedu/address/logic/commands/CleanCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CleanCommandTest.java
@@ -1,2 +1,31 @@
-package seedu.address.logic.commands;public class CleanCommandTest {
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CleanCommand.MESSAGE_CLEAN_SUCCESS;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+import java.util.List;
+
+public class CleanCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    //private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_clean_success() {
+        CommandResult expectedCommandResult = new CommandResult(MESSAGE_CLEAN_SUCCESS);
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        List<Person> lastShownList = expectedModel.getFilteredPersonList();
+        for (int i = lastShownList.size() - 1; i >= 0; i--) {
+            expectedModel.deletePerson(lastShownList.get(i));
+        }
+
+        assertCommandSuccess(new CleanCommand(), model, expectedCommandResult, expectedModel);
+    }
 }
