31ff0b5e57d250fca450ca36c6e1d3b4c336521f
S-K-Y-Light <s84915656@gmail.com>
2024-10-20 09:36:05 +0800

    EditCommand.java: Disallow EditCommand from resetting student list display

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -82,7 +82,6 @@ public class EditCommand extends Command {
         }
 
         model.setStudent(studentToEdit, editedStudent);
-        model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
         return new CommandResult(String.format(MESSAGE_EDIT_STUDENT_SUCCESS, Messages.format(editedStudent)));
     }
 
