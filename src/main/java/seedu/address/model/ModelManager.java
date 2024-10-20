c73db64747cd91d14b1caaa9a657be37f7300c85
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-20 22:02:14 +0800

    Implement Wedding operations in ModelManager

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
@@ -13,6 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.ActiveTags;
+import seedu.address.model.wedding.Wedding;
 
 /**
  * Represents the in-memory model of the address book data.
@@ -23,6 +25,7 @@ public class ModelManager implements Model {
     private final AddressBook addressBook;
     private final UserPrefs userPrefs;
     private final FilteredList<Person> filteredPersons;
+    private final FilteredList<Wedding> filteredWeddings;
     private ActiveTags activeTags; //Stores all currently used tags
     /**
      * Initializes a ModelManager with the given addressBook and userPrefs.
@@ -35,6 +38,7 @@ public class ModelManager implements Model {
         this.addressBook = new AddressBook(addressBook);
         this.userPrefs = new UserPrefs(userPrefs);
         filteredPersons = new FilteredList<>(this.addressBook.getPersonList());
+        filteredWeddings = new FilteredList<>(this.addressBook.getWeddingList());
         activeTags = new ActiveTags(this.addressBook.findTagOccurrences());
     }
 
@@ -111,6 +115,47 @@ public class ModelManager implements Model {
         requireAllNonNull(target, editedPerson);
 
         addressBook.setPerson(target, editedPerson);
+    }
+
+    @Override
+    public void addWedding(Wedding wedding) {
+        requireNonNull(wedding);
+
+        addressBook.addWedding(wedding);
+        System.out.println(Arrays.toString(addressBook.getWeddingList().toArray()));
+    }
+
+    @Override
+    public void removeWedding(Wedding wedding) {
+        requireNonNull(wedding);
+
+        addressBook.removeWedding(wedding);
+    }
+
+    @Override
+    public void setWedding(Wedding wedding, Wedding editedWedding) {
+        requireAllNonNull(wedding, editedWedding);
+
+        addressBook.setWedding(wedding, editedWedding);
+    }
+
+    @Override
+    public void assignPerson(Wedding wedding, Person person) {
+        requireAllNonNull(wedding, person);
+
+        addressBook.assignPerson(wedding, person);
+    }
+
+    @Override
+    public void unassignPerson(Wedding wedding, Person person) {
+        requireAllNonNull(wedding, person);
+
+        addressBook.unassignPerson(wedding, person);
+    }
+
+    @Override
+    public ObservableList<Wedding> getFilteredWeddingList() {
+        return filteredWeddings;
     }
 
     //=========== Filtered Person List Accessors =============================================================
