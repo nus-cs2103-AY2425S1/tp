bd0b725d205d8160fe483d2a51698917aa28ee21
Curtis <curtischang2510@gmail.com>
2024-10-21 12:10:13 +0800

    Fix checkstyle in `LessonManagerTest`
    - Added a newline at the end of the file to meet formatting standards.
    - Corrected the order of import statements

--- src/test/java/tuteez/model/person/lesson/LessonManagerTest.java
+++ src/test/java/tuteez/model/person/lesson/LessonManagerTest.java
@@ -1,10 +1,9 @@
 package tuteez.model.person.lesson;
 
-
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class LessonManagerTest {
 
