6a533117796b95774c32c6acde11a9dcf8cb3fce
Jalenleeruixian <e1122262@u.nus.edu>
2024-10-21 22:36:52 +0800

    Add assert

--- src/test/java/seedu/address/logic/commands/CountCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CountCommandTest.java
@@ -1,74 +1,62 @@
-//package seedu.address.logic.commands;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.util.Optional;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.address.logic.commands.exceptions.CommandException;
-//import seedu.address.model.Model;
-//import seedu.address.model.ModelManager;
-//
-//public class CountCommandTest {
-//
-//    @Test
-//    public void executeSuccessNoFilter() throws CommandException {
-//        Model model = new ModelManager();
-//        int expectedCount = model.getFilteredPersonList().size();
-//
-//        // Create CountCommand with no filters
-//        CountCommand countCommand = new CountCommand(Optional.empty(), Optional.empty());
-//        CommandResult result = countCommand.execute(model);
-//
-//        assertEquals(String.format(CountCommand.MESSAGE_COUNT_PERSONS, expectedCount), result.getFeedbackToUser());
-//    }
-//
-//    @Test
-//    public void executeSuccessWithNamePrefix() throws CommandException {
-//        Model model = new ModelManager();
-//        String namePrefix = "Al";
-//
-//        // Manually filter list for expected count
-//        int expectedCount = (int) model.getFilteredPersonList().stream()
-//                .filter(person -> person.getName().fullName.startsWith(namePrefix))
-//                .count();
-//
-//        // Create CountCommand with name prefix filter
-//        CountCommand countCommand = new CountCommand(Optional.of(namePrefix), Optional.empty());
-//        CommandResult result = countCommand.execute(model);
-//
-//        assertEquals(String.format(CountCommand.MESSAGE_COUNT_PERSONS, expectedCount), result.getFeedbackToUser());
-//    }
-//
-//    @Test
-//    public void executeSuccessWithTag() throws CommandException {
-//        Model model = new ModelManager();
-//        String tag = "friend";
-//
-//        // Manually filter list for expected count
-//        int expectedCount = (int) model.getFilteredPersonList().stream()
-//                .filter(person -> person.getTags().stream()
-//                        .anyMatch(t -> t.tagName.equals(tag)))
-//                .count();
-//
-//        // Create CountCommand with tag filter
-//        CountCommand countCommand = new CountCommand(Optional.empty(), Optional.of(tag));
-//        CommandResult result = countCommand.execute(model);
-//
-//        assertEquals(String.format(CountCommand.MESSAGE_COUNT_PERSONS, expectedCount), result.getFeedbackToUser());
-//    }
-//
-//    @Test
-//    public void executeNoMatches() throws CommandException {
-//        Model model = new ModelManager();
-//        String namePrefix = "Zzz"; // unlikely to match
-//
-//        // Create CountCommand with a prefix that yields no matches
-//        CountCommand countCommand = new CountCommand(Optional.of(namePrefix), Optional.empty());
-//        CommandResult result = countCommand.execute(model);
-//
-//        assertEquals(CountCommand.MESSAGE_NO_MATCHES, result.getFeedbackToUser());
-//    }
-//}
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.CountCommand;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.TypicalPersons;
+import seedu.address.model.UserPrefs;
+
+public class CountCommandTest {
+
+    @Test
+    public void execute_noFilter_success() {
+        Model model = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+
+        CountCommand command = new CountCommand(Optional.empty(), Optional.empty());
+        String expectedMessage = String.format(CountCommand.MESSAGE_COUNT_PERSONS, model.getFilteredPersonList().size());
+
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_namePrefixFilter_success() {
+        Model model = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+
+        Optional<String> namePrefix = Optional.of("Al");
+        CountCommand command = new CountCommand(namePrefix, Optional.empty());
+
+        expectedModel.updateFilteredPersonList(person ->
+                person.getName().fullName.toLowerCase().startsWith(namePrefix.get().toLowerCase()));
+        String expectedMessage = String.format(CountCommand.MESSAGE_COUNT_PERSONS, expectedModel.getFilteredPersonList().size());
+
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_tagFilter_success() {
+        Model model = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(TypicalPersons.getTypicalAddressBook(), new UserPrefs());
+
+        Optional<String> tag = Optional.of("friend");
+        CountCommand command = new CountCommand(Optional.empty(), tag);
+
+        expectedModel.updateFilteredPersonList(person ->
+                person.getTags().stream().anyMatch(t -> t.tagName.equalsIgnoreCase(tag.get())));
+        String expectedMessage = String.format(CountCommand.MESSAGE_COUNT_PERSONS, expectedModel.getFilteredPersonList().size());
+
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+    }
+}
+
+
 
