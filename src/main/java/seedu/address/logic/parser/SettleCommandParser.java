ebb560b621a91668e70e8097775d9c64f91f7fec
Soumyadip <soumyadip1107@gmail.com>
2024-10-22 00:20:19 +0800

    Add logging in settleCommandParser

--- src/main/java/seedu/address/logic/parser/SettleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SettleCommandParser.java
@@ -4,15 +4,20 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_AMOUNT;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.SettleCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.logging.Logger;
 
 
 /**
  * Parses input arguments and creates a new SettleCommand object
  */
 public class SettleCommandParser implements Parser<SettleCommand> {
+
+    private static final Logger logger = LogsCenter.getLogger(SettleCommandParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the SettleCommand
@@ -28,18 +33,23 @@ public class SettleCommandParser implements Parser<SettleCommand> {
 
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            logger.info("Parsed index: " + index.getOneBased());  // Log the parsed index
         } catch (ParseException pe) {
+            logger.warning("Failed to parse index from arguments: " + args);  // Log a warning when parsing the index fails
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SettleCommand.MESSAGE_USAGE), pe);
         }
 
         if (!argMultimap.getValue(PREFIX_AMOUNT).isPresent()) {
+            logger.warning("No amount value provided in the arguments: " + args);  // Log missing amount prefix
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SettleCommand.MESSAGE_USAGE));
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_AMOUNT);
 
         double amount = ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get());
+        logger.info("Parsed amount: " + amount);  // Log the parsed amount
 
+        logger.info("Successfully parsed SettleCommand with index " + index.getOneBased() + " and amount " + amount);
         return new SettleCommand(index, amount);
     }
 }
