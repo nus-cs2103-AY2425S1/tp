12f86f4e24f82da85e2a6f53db2588227c2c4a52
raageth <chaygareth@gmail.com>
2024-10-21 22:11:32 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnmarkCommandTest.java
@@ -14,8 +14,8 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
