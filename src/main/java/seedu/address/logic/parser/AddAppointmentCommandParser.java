ef2830b5aafa3dc7738c46daf5859b1f206730eb
clarud <clarencejw@gmail.com>
2024-10-22 01:47:55 +0800

    Fix implementation of add appointment parser

--- src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddAppointmentCommandParser.java
@@ -1,15 +1,18 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
 
+
 import java.time.LocalDateTime;
-import java.util.stream.Stream;
+
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddAppointmentCommand;
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.appointment.Appointment;
 
@@ -24,29 +27,21 @@ public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddAppointmentCommand parse(String args) throws ParseException {
-        String[] indexMultimapSplit = args.split(" ", 2);
-        // Ensure the input can be split into exactly two parts (index and the rest)
-        if (indexMultimapSplit.length < 2) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddAppointmentCommand.MESSAGE_USAGE));
-        }
+        requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(indexMultimapSplit[1], PREFIX_APPOINTMENT, PREFIX_START, PREFIX_END);
-        if (!arePrefixesPresent(argMultimap, PREFIX_APPOINTMENT, PREFIX_START, PREFIX_END)
-                || !argMultimap.getPreamble().isEmpty()
-                || indexMultimapSplit.length < 2) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddAppointmentCommand.MESSAGE_USAGE));
-        }
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_APPOINTMENT, PREFIX_START, PREFIX_END);
+                ArgumentTokenizer.tokenize(args, PREFIX_APPOINTMENT, PREFIX_START, PREFIX_END);
 
         Index index;
+
         try {
-            index = ParserUtil.parseIndex(indexMultimapSplit[0]);
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddAppointmentCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_APPOINTMENT, PREFIX_START,
+                PREFIX_END);
+
         String appointmentDescription = ParserUtil.parseAppointmentDescription(
                 argMultimap.getValue(PREFIX_APPOINTMENT).get());
         LocalDateTime startDateTime = ParserUtil.parseLocalDateTime(
@@ -58,13 +53,5 @@ public class AddAppointmentCommandParser implements Parser<AddAppointmentCommand
         }
         Appointment appointment = new Appointment(appointmentDescription, startDateTime, endDateTime, null);
         return new AddAppointmentCommand(appointment, index);
-    }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
