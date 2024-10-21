a897d6a348cb619ad21bf29cebe3219f42e98b6d
jia xin <jjiiaaxxiinn@gmail.com>
2024-10-21 17:40:57 +0800

    Add test cases for mass deletetag function

--- src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteTagCommandParserTest.java
@@ -23,7 +23,17 @@ public class DeleteTagCommandParserTest {
         Tag expectedTag = TypicalTags.VALID_TAG_BRIDES_FRIEND;
         List<Tag> expectedTags = new ArrayList<>();
         expectedTags.add(expectedTag);
-        assertParseSuccess(parser, "bride's friend", new DeleteTagCommand(expectedTags));
+        assertParseSuccess(parser, " t/bride's friend", new DeleteTagCommand(expectedTags));
+    }
+
+    @Test
+    public void parse_validArgsNoLeadingSpace_throwsParseException() {
+        assertParseFailure(parser, "t/bride's friend", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgsNoPrefix_throwsParseException() {
+        assertParseFailure(parser, "bride's friend", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 
     @Test
@@ -31,25 +41,35 @@ public class DeleteTagCommandParserTest {
         Tag expectedTag = TypicalTags.VALID_TAG_BRIDES_FRIEND;
         List<Tag> expectedTags = new ArrayList<>();
         expectedTags.add(expectedTag);
-        assertParseSuccess(parser, "   bride's friend   ", new DeleteTagCommand(expectedTags));
+        assertParseSuccess(parser, "  t/ bride's friend   ", new DeleteTagCommand(expectedTags));
     }
 
     @Test
     public void parse_emptyArgs_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        assertParseFailure(parser, " t/     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_AtMaxLength_returnsDeleteTagCommand() {
+        String maxLengthTagName = "a".repeat(50); // 50 characters
+        Tag expectedTag = new Tag(maxLengthTagName);
+        List<Tag> expectedTags = new ArrayList<>();
+        expectedTags.add(expectedTag);
+
+        assertParseSuccess(parser, " t/" + maxLengthTagName, new DeleteTagCommand(expectedTags));
     }
 
     @Test
     public void parse_exceedsMaxLength_throwsParseException() {
-        String longTag = "a".repeat(51); // 51 characters, exceeding the 50-character limit.
-        assertParseFailure(parser, longTag, String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        String longTagName = "a".repeat(51); // 51 characters, exceeding the 50-character limit.
+        assertParseFailure(parser, " t/" + longTagName, String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 
     @Test
     public void parse_invalidCharacters_throwsParseException() {
-        assertParseFailure(parser, ";%<>}{", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-        assertParseFailure(parser, "¡£™", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-        assertParseFailure(parser, "¶¢", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        assertParseFailure(parser, "t/;%<>}{", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        assertParseFailure(parser, "t/¡£™", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        assertParseFailure(parser, "t/¶¢", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 
     @Test
@@ -57,6 +77,31 @@ public class DeleteTagCommandParserTest {
         Tag expectedTag = new Tag("friend");
         List<Tag> expectedTags = new ArrayList<>();
         expectedTags.add(expectedTag);
-        assertParseSuccess(parser, "  FRIEND  ", new DeleteTagCommand(expectedTags));
+        assertParseSuccess(parser, " t/FRIEND", new DeleteTagCommand(expectedTags));
+    }
+
+    @Test
+    public void parse_multipleValidArgs_returnsDeleteTagCommand() {
+        Tag expectedTagBride = TypicalTags.BRIDES_SIDE;
+        Tag expectedTagGroom = TypicalTags.GROOMS_SIDE;
+        List<Tag> expectedTags = new ArrayList<>();
+        expectedTags.add(expectedTagBride);
+        expectedTags.add(expectedTagGroom);
+        assertParseSuccess(parser, " t/bride's side t/groom's side", new DeleteTagCommand(expectedTags));
+    }
+
+    @Test
+    public void parse_multipleValidArgsWithoutSpace_returnsDeleteTagCommand() {
+        assertParseFailure(parser, " t/bride's sidet/groom's side", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_multipleArgsOneInvalid_returnsDeleteTagCommand() {
+        assertParseFailure(parser, " t/bride's side t/^@%", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_multipleArgsOneExceedsMaxLength_returnsDeleteTagCommand() {
+        assertParseFailure(parser, " t/bride's side t/" + "a".repeat(51), String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 }
