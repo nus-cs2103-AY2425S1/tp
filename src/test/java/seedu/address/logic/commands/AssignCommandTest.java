b981c37db1f6e2a4b8baed3f80a304c115cfabf8
ItsMeNengYi <20040408gry@gmail.com>
2024-10-21 10:21:40 +0800

    Fix checkstyle issue

--- src/test/java/seedu/address/logic/commands/AssignCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AssignCommandTest.java
@@ -13,15 +13,12 @@ import static seedu.address.testutil.TypicalProjects.BETA;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.logic.Messages;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
