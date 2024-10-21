6f3ec06c241805be181c8f5dc849283b3ec26a6d
pradyuprasad <pradyu.sp@gmail.com>
2024-10-21 12:17:17 +0800

    Fix variable name to match style check

--- src/main/java/seedu/address/model/Appointment/Appointment.java
+++ src/main/java/seedu/address/model/Appointment/Appointment.java
@@ -1,12 +1,16 @@
-package seedu.address.model.Appointment;
+package seedu.address.model.appointment;
 
 import java.time.LocalDateTime;
 
 import seedu.address.model.person.Nric;
 
+/**
+ * Represents a patient's appointment in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 public class Appointment {
 
-    private final String Name;
+    private final String name;
     private final Nric nric;
 
     private final LocalDateTime startTime;
@@ -14,47 +18,74 @@ public class Appointment {
 
     private boolean isCompleted = false;
 
+    /**
+     * Constructs an appointment with the given details.
+     *
+     * @param name the name of the appointment
+     * @param nric the nric of the patient
+     * @param startTime the start time of the appointment
+     * @param endTime the end time of the appointment
+     */
     public Appointment(String name, Nric nric, LocalDateTime startTime, LocalDateTime endTime) {
-        this.Name = name;
+        this.name = name;
         this.nric = nric;
         this.startTime = startTime;
         this.endTime = endTime;
     }
 
+    /**
+     * @return the name of the appointment
+     */
     public String getName() {
-        return Name;
+        return name;
     }
 
+    /**
+     * @return the nric of the patient
+     */
     public Nric getNric() {
         return nric;
     }
 
+    /**
+     * @return the start time of the appointment
+     */
     public LocalDateTime getStartTime() {
         return startTime;
-    }   
+    }
 
+    /**
+     * @return the end time of the appointment
+     */
     public LocalDateTime getEndTime() {
         return endTime;
     }
 
+    /**
+     * @return true if the appointment has been completed, false otherwise
+     */
     public boolean isCompleted() {
         return isCompleted;
     }
 
+    /**
+     * Marks the appointment as completed.
+     */
     public void markAsCompleted() {
         isCompleted = true;
     }
 
+    /**
+     * Returns a string representation of the appointment.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Appointment: " + Name + "\n");
+        sb.append("Appointment: " + name + "\n");
         sb.append("Nric: " + nric.maskNric() + "\n");
         sb.append("Start Time: " + startTime + "\n");
         sb.append("End Time: " + endTime + "\n");
         return sb.toString();
     }
 
-
-    
 }
