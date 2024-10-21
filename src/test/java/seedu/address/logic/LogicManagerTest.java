352440d62b391ec089579f382e100594636b2c24
S-K-Y-Light <s84915656@gmail.com>
2024-10-21 23:03:52 +0800

    LogicManager & ModelManager: Add Test Cases

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -94,6 +94,11 @@ public class LogicManagerTest {
         assertThrows(UnsupportedOperationException.class, () -> logic.getFilteredStudentList().remove(0));
     }
 
+    @Test
+    public void getFilteredConsultationList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> logic.getFilteredConsultationList().remove(0));
+    }
+
     /**
      * Executes the command and confirms that
      * - no exceptions are thrown <br>
---
+++ src/test/java/seedu/address/testutil/TypicalConsultations.java
@@ -0,0 +1,8 @@
+package seedu.address.testutil;
+
+import seedu.address.model.consultation.Consultation;
+
+public class TypicalConsultations {
+    public static final Consultation DEFAULT_CONSULTATION =
+            new ConsultationBuilder().build();
+}
