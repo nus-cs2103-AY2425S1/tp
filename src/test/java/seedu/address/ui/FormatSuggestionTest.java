e9df89b596f6f248ad4aa6d49f187f8b31762388
Sim GUan Yi <e1130009@u.nus.edu>
2024-10-21 23:36:26 +0800

    add more FormatSuggestionTest

--- src/test/java/seedu/address/ui/FormatSuggestionTest.java
+++ src/test/java/seedu/address/ui/FormatSuggestionTest.java
@@ -117,4 +117,99 @@ public class FormatSuggestionTest {
         assertEquals("",
                 editSuggestion.getRemainingFormat("edit abc"));
     }
+
+    // New test methods below
+
+    @Test
+    public void testGetRemainingFormat_findCommand() {
+        FormatSuggestion findSuggestion = new FormatSuggestion(
+                "find",
+                "find NAME",
+                new String[]{}
+        );
+
+        // Test just command
+        assertEquals(" NAME", findSuggestion.getRemainingFormat("find"));
+
+        // Test with invalid name (contains numbers)
+        assertEquals("", findSuggestion.getRemainingFormat("find John123"));
+
+        // Test with valid name
+        assertEquals("", findSuggestion.getRemainingFormat("find John"));
+    }
+
+    @Test
+    public void testGetRemainingFormat_mixedCommands() {
+        FormatSuggestion suggestion = new FormatSuggestion(
+                "search",
+                "search INDEX/NAME [t/TAG] [s/SORT]",
+                new String[]{"t/", "s/"}
+        );
+
+        // Test with numeric index
+        assertEquals(" [t/TAG] [s/SORT]", suggestion.getRemainingFormat("search 1"));
+
+        // Test with alphabetic name
+        assertEquals(" [t/TAG] [s/SORT]", suggestion.getRemainingFormat("search John"));
+
+        // Test with alphanumeric input
+        assertEquals(" [t/TAG] [s/SORT]", suggestion.getRemainingFormat("search John123"));
+
+        // Test with one optional parameter
+        assertEquals(" [s/SORT]", suggestion.getRemainingFormat("search 1 t/friend"));
+    }
+
+    @Test
+    public void testGetRemainingFormat_specialCharacters() {
+        FormatSuggestion addSuggestion = new FormatSuggestion(
+                "add",
+                "add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS [t/TAG]",
+                new String[]{"n/", "p/", "e/", "a/", "t/"}
+        );
+
+        // Test with special characters in input
+        assertEquals(" p/PHONE_NUMBER e/EMAIL a/ADDRESS [t/TAG]", addSuggestion.getRemainingFormat("add n/John@Doe"));
+
+        // Test with multiple tags containing special characters
+        assertEquals(" a/ADDRESS",
+                addSuggestion.getRemainingFormat("add n/John p/12345 e/john@example.com t/friend t/co-worker"));
+    }
+
+    @Test
+    public void testGetRemainingFormat_complexCommand() {
+        FormatSuggestion complexSuggestion = new FormatSuggestion(
+                "filter",
+                "filter INDEX/NAME [d/DATE] [p/PRIORITY] [s/STATUS] [t/TAG]",
+                new String[]{"d/", "p/", "s/", "t/"}
+        );
+
+        // Test with mixed optional parameters in different orders
+        assertEquals(" [d/DATE] [p/PRIORITY] [t/TAG]",
+                complexSuggestion.getRemainingFormat("filter 1 s/pending"));
+
+        // Test with all optional parameters
+        assertEquals("",
+                complexSuggestion.getRemainingFormat(
+                        "filter John d/2024-01-01 p/high s/pending t/urgent"));
+
+        // Test with duplicate parameters
+        assertEquals(" [d/DATE] [p/PRIORITY] [s/STATUS]",
+                complexSuggestion.getRemainingFormat(
+                        "filter 1 t/urgent t/important"));
+    }
+
+    @Test
+    public void testGetRemainingFormat_caseInsensitivity() {
+        FormatSuggestion suggestion = new FormatSuggestion(
+                "ADD",
+                "ADD n/NAME p/PHONE_NUMBER",
+                new String[]{"n/", "p/"}
+        );
+
+        // Test with mixed case command
+        assertEquals("", suggestion.getRemainingFormat("AdD"));
+
+        // Test with mixed case parameters
+        assertEquals(" n/NAME p/PHONE_NUMBER", suggestion.getRemainingFormat("ADD N/John"));
+    }
 }
