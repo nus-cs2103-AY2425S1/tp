0f608f0fe839f4e3997a173ca37a9a9c10325fce
ItsMeNengYi <20040408gry@gmail.com>
2024-10-21 10:19:42 +0800

    Comment out some test case

--- src/test/java/seedu/address/logic/commands/AssignCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AssignCommandTest.java
@@ -13,12 +13,15 @@ import static seedu.address.testutil.TypicalProjects.BETA;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
@@ -38,27 +41,27 @@ public class AssignCommandTest {
         assertThrows(NullPointerException.class, () -> new AssignCommand(null));
     }
 
-//     @Test
-//     public void execute_assignmentAcceptedByModel_addSuccessful() throws Exception {
-//         ModelStubAcceptingAssignmentAdded modelStub = new ModelStubAcceptingAssignmentAdded();
-//         Assignment validAssignment = new AssignmentBuilder().build();
-//
-//         CommandResult commandResult = new AssignCommand(validAssignment).execute(modelStub);
-//
-//         assertEquals(String.format(AssignCommand.MESSAGE_SUCCESS, Messages.format(validAssignment)),
-//                 commandResult.getFeedbackToUser());
-//         assertEquals(Arrays.asList(validAssignment), modelStub.assignmentsAdded);
-//     }
-//
-//     @Test
-//     public void execute_duplicateAssignment_throwsCommandException() {
-//         Assignment validAssignment = new AssignmentBuilder().build();
-//         AssignCommand assignCommand = new AssignCommand(validAssignment);
-//         ModelStub modelStub = new ModelStubWithAssignment(validAssignment);
-//
-//         assertThrows(CommandException.class,
-//                 AssignCommand.MESSAGE_DUPLICATE_ASSIGNMENT, () -> assignCommand.execute(modelStub));
-//     }
+    // @Test
+    // public void execute_assignmentAcceptedByModel_addSuccessful() throws Exception {
+    //      ModelStubAcceptingAssignmentAdded modelStub = new ModelStubAcceptingAssignmentAdded();
+    //      Assignment validAssignment = new AssignmentBuilder().build();
+
+    // CommandResult commandResult = new AssignCommand(validAssignment).execute(modelStub);
+
+    //      assertEquals(String.format(AssignCommand.MESSAGE_SUCCESS, Messages.format(validAssignment)),
+    //              commandResult.getFeedbackToUser());
+    //      assertEquals(Arrays.asList(validAssignment), modelStub.assignmentsAdded);
+    // }
+
+    // @Test
+    // public void execute_duplicateAssignment_throwsCommandException() {
+    //     Assignment validAssignment = new AssignmentBuilder().build();
+    //     AssignCommand assignCommand = new AssignCommand(validAssignment);
+    //     ModelStub modelStub = new ModelStubWithAssignment(validAssignment);
+
+    //     assertThrows(CommandException.class,
+    //            AssignCommand.MESSAGE_DUPLICATE_ASSIGNMENT, () -> assignCommand.execute(modelStub));
+    // }
 
     @Test
     public void equals() {
