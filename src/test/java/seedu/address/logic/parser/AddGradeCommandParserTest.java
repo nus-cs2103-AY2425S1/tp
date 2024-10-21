14c91affd6a177f12c1f54f650d5cd4eae44d47f
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-10-21 13:06:54 +0800

    Fix test error messages

--- src/test/java/seedu/address/logic/parser/AddGradeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddGradeCommandParserTest.java
@@ -1,12 +1,19 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SCORE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_WEIGHTAGE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
+import static seedu.address.model.person.Grade.MESSAGE_SCORE_CONSTRAINTS;
+import static seedu.address.model.person.Grade.MESSAGE_WEIGHTAGE_CONSTRAINTS;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.AddGradeCommand;
 import seedu.address.model.person.Grade;
 
@@ -36,11 +43,16 @@ public class AddGradeCommandParserTest {
 
         // Wrong arguments structure
         assertParseFailure(parser, "15 Midterm 95.6 1", errorMessage);
-        assertParseFailure(parser, "Adam n/Midterm s/95.6 w/30", errorMessage);
-        assertParseFailure(parser, "15 n/Midterm s/A w/30", errorMessage);
-        assertParseFailure(parser, "15 n/Midterm s/95.6 w/FM", errorMessage);
+        assertParseFailure(parser, "Adam n/Midterm s/95.6 w/30", MESSAGE_INVALID_INDEX);
+        assertParseFailure(parser, "15 n/Midterm s/A w/30", MESSAGE_SCORE_CONSTRAINTS);
+        assertParseFailure(parser, "15 n/Midterm s/95.6 w/FM", MESSAGE_WEIGHTAGE_CONSTRAINTS);
 
         // Too many arguments
-        assertParseFailure(parser, "15 n/Midterm s/95.000 w/35.000 777", errorMessage);
+        assertParseFailure(parser, "15 n/Midterm s/95.000 w/35.000 w/777",
+                           Messages.getErrorMessageForDuplicatePrefixes(PREFIX_WEIGHTAGE));
+        assertParseFailure(parser, "15 n/Midterm s/95.000 s/35.000 w/777",
+                           Messages.getErrorMessageForDuplicatePrefixes(PREFIX_SCORE));
+        assertParseFailure(parser, "15 n/Midterm n/Finals s/35.000 w/777",
+                           Messages.getErrorMessageForDuplicatePrefixes(PREFIX_NAME));
     }
 }
