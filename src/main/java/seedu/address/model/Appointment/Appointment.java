25f31e5b0701706adf2f102406550828ef382b1f
pradyuprasad <pradyu.sp@gmail.com>
2024-10-21 12:12:55 +0800

    Add getters for appointment
    Add getters and toString for appointment

--- src/main/java/seedu/address/model/Appointment/Appointment.java
+++ src/main/java/seedu/address/model/Appointment/Appointment.java
@@ -12,11 +12,47 @@ public class Appointment {
     private final LocalDateTime startTime;
     private final LocalDateTime endTime;
 
+    private boolean isCompleted = false;
+
     public Appointment(String name, Nric nric, LocalDateTime startTime, LocalDateTime endTime) {
         this.Name = name;
         this.nric = nric;
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Nric getNric() {
+        return nric;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }   
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void markAsCompleted() {
+        isCompleted = true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Appointment: " + Name + "\n");
+        sb.append("Nric: " + nric.maskNric() + "\n");
+        sb.append("Start Time: " + startTime + "\n");
+        sb.append("End Time: " + endTime + "\n");
+        return sb.toString();
     }
 
 
