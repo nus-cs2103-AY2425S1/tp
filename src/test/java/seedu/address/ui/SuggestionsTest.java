5e02aec0ba9a469dc9197cf863424a3d7eb10926
Sim GUan Yi <e1130009@u.nus.edu>
2024-10-21 20:20:15 +0800

    checkstyle

--- src/test/java/seedu/address/ui/SuggestionsTest.java
+++ src/test/java/seedu/address/ui/SuggestionsTest.java
@@ -1,8 +1,9 @@
 package seedu.address.ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit tests for the Suggestions class.
