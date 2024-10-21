e4202ee1e12cae6c354989a62403eddb01ead9b6
aditig0305 <E1136813@u.nus.edu>
2024-10-21 18:17:57 +0800

    Add date test cases

--- src/test/java/seedu/address/model/person/DateTest.java
+++ src/test/java/seedu/address/model/person/DateTest.java
@@ -1,9 +1,8 @@
 package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DateTest {
     @Test
@@ -22,4 +21,37 @@ public class DateTest {
         Date differentDate = new Date("1/9/2025");
         assertFalse(date.equals(differentDate));
     }
+
+    @Test
+    public void hashCode_sameValues_sameHashCode() {
+        // Two Date objects with the same value should have the same hash code
+        Date date1 = new Date("2024-01-01");
+        Date date2 = new Date("2024-01-01");
+        assertEquals(date1.hashCode(), date2.hashCode());
+    }
+
+    @Test
+    public void hashCode_differentValues_differentHashCode() {
+        // Two Date objects with different values should have different hash codes
+        Date date1 = new Date("2024-01-01");
+        Date date2 = new Date("2024-12-31");
+        assertEquals(false, date1.hashCode() == date2.hashCode());
+    }
+
+    @Test
+    public void hashCode_nullValue_throwsNullPointerException() {
+        try {
+            Date date = new Date(null);
+        } catch (NullPointerException e) {
+            assertEquals(true, e instanceof NullPointerException);
+        }
+    }
+
+    @Test
+    public void hashCode_consistentHashCode() {
+        Date date = new Date("2024-01-01");
+        int initialHashCode = date.hashCode();
+        assertEquals(initialHashCode, date.hashCode());
+    }
+
 }
