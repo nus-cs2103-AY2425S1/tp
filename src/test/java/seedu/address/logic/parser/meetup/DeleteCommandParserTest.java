730a6d19c988c502170410afb8f7775447364623
Solomon0519 <ng.eswen1@gmail.com>
2024-10-21 21:17:49 +0800

    Close issue #120
    Added two new Parser Tests for Add Command and Delete Command for meet ups.

--- src/test/java/seedu/address/logic/parser/meetup/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/meetup/DeleteCommandParserTest.java
@@ -1,5 +1,34 @@
 package seedu.address.logic.parser.meetup;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MEETUP;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.meetup.DeleteCommand;
+
+/**
+ * As we are only doing white-box testing, our test cases do not cover path variations
+ * outside of the DeleteCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteCommand, and therefore we test only one of them.
+ * The path variation for those two cases occur inside the ParserUtil, and
+ * therefore should be covered by the ParserUtilTest.
+ */
 public class DeleteCommandParserTest {
-    // TO-DO
+
+    private seedu.address.logic.parser.meetup.DeleteCommandParser parser = new DeleteCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteCommand() {
+        assertParseSuccess(parser, "1",
+                new seedu.address.logic.commands.meetup.DeleteCommand(INDEX_FIRST_MEETUP));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "f", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteCommand.MESSAGE_USAGE));
+    }
 }
