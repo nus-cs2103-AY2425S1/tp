23c8dfd1a4013e352d93e9e1a02262a450231806
WuJinhan1 <e1075517@u.nus.edu>
2024-10-21 16:28:10 +0800

    fix chechstyle

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
-
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Name;
