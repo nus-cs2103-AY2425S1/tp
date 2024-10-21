21c0f49ffe154a050b2e832e4d5b9d13e7253bd8
pradyuprasad <pradyu.sp@gmail.com>
2024-10-21 14:26:13 +0800

    Add javadoc for InvalidAppointmentException

--- src/main/java/seedu/address/model/Appointment/exceptions/InvalidAppointmentException.java
+++ src/main/java/seedu/address/model/Appointment/exceptions/InvalidAppointmentException.java
@@ -1,9 +1,13 @@
 package seedu.address.model.appointment.exceptions;
 
+/**
+ * Signals that an operation is invalid due to the appointment timing being
+ * invalid.
+ */
 public class InvalidAppointmentException extends RuntimeException {
 
     public InvalidAppointmentException() {
         super("Invalid Appointment: Start time must be before end time");
     }
-    
+
 }
