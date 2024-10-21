bc07aa8e1cebac94afcffd0c3b79c624f64c4f38
neohengkai <neohengkai2002@gmail.com>
2024-10-21 22:08:43 +0800

    Add javadoc for comparator

--- src/main/java/seedu/address/model/person/PersonNameComparator.java
+++ src/main/java/seedu/address/model/person/PersonNameComparator.java
@@ -3,9 +3,12 @@ package seedu.address.model.person;
 import java.util.Comparator;
 
 /**
- * Compares two persons based on their names.
+ * A Comparator object that compares two persons based on their name.
  */
 public class PersonNameComparator implements Comparator<Person> {
+    /**
+     * Compares two persons based on their names.
+     */
     public int compare(Person p1, Person p2) {
         String p1Name = p1.getName().fullName.toLowerCase();
         String p2Name = p2.getName().fullName.toLowerCase();
