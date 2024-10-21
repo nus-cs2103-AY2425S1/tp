8eb0b73dd3993490eb43c690789ad619517a5495
Wu Zengfu <wuzengfu666@gmail.com>
2024-10-21 20:09:14 +0800

    Add totalCount Label to StatusBarFooter

--- src/main/java/seedu/address/ui/StatusBarFooter.java
+++ src/main/java/seedu/address/ui/StatusBarFooter.java
@@ -3,9 +3,12 @@ package seedu.address.ui;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import seedu.address.model.person.Person;
 
 /**
  * A ui for the status bar that is displayed at the footer of the application.
@@ -17,12 +20,21 @@ public class StatusBarFooter extends UiPart<Region> {
     @FXML
     private Label saveLocationStatus;
 
+    @FXML
+    private Label personListStatus;
+
     /**
      * Creates a {@code StatusBarFooter} with the given {@code Path}.
      */
-    public StatusBarFooter(Path saveLocation) {
+    public StatusBarFooter(Path saveLocation, ObservableList<Person> personList) {
         super(FXML);
         saveLocationStatus.setText(Paths.get(".").resolve(saveLocation).toString());
+        setPersonListStatus(personList.size());
+        personList.addListener((ListChangeListener<? super Person>) unused -> setPersonListStatus(personList.size()));
     }
 
+    private void setPersonListStatus(int totalCount) {
+        String personListStatusPrefix = "Total Count: ";
+        personListStatus.setText(personListStatusPrefix + totalCount);
+    }
 }
