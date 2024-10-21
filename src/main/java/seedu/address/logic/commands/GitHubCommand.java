de26a1e86debd7318760a80bb289a0b0e0f99cd4
Incogdino <javiertmw@gmail.com>
2024-10-21 12:34:54 +0800

    Add tests for GitHubCommand
    Comprehensive test of GitHubCommand to ensure that the methods performs
    as expected, correct errors are thrown.

--- src/main/java/seedu/address/logic/commands/GitHubCommand.java
+++ src/main/java/seedu/address/logic/commands/GitHubCommand.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import java.io.IOException;
 
 import seedu.address.commons.core.Browser;
-import seedu.address.commons.core.FunctionalBrowser;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
