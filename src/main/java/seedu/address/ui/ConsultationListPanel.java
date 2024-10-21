993065344864a31cc3db818bde7934e9894a0ee8
S-K-Y-Light <s84915656@gmail.com>
2024-10-21 21:02:55 +0800

    Consultation GUI: Fix JavaCI Style Check

--- src/main/java/seedu/address/ui/ConsultationListPanel.java
+++ src/main/java/seedu/address/ui/ConsultationListPanel.java
@@ -1,5 +1,7 @@
 package seedu.address.ui;
 
+import java.util.logging.Logger;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
@@ -8,8 +10,9 @@ import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.consultation.Consultation;
 
-import java.util.logging.Logger;
-
+/**
+ * Panel containing the list of consultations.
+ */
 public class ConsultationListPanel extends UiPart<Region> {
     private static final String FXML = "ConsultationListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(ConsultationListPanel.class);
