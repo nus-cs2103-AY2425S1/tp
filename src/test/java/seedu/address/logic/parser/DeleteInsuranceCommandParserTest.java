e4bc8faea6bbeb3b18836671118977a036f9b294
yanqiyqh <yanqi.yqh@gmail.com>
2024-10-21 23:40:51 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/DeleteInsuranceCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteInsuranceCommandParserTest.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INSURANCE_ID;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FOURTH_CLIENT;
 
 import org.junit.jupiter.api.Test;
