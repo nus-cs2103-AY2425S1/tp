214b314c279fedb2bb8059fa08c593ba6fd57741
Yifeng Li <charlesliyifeng@gmail.com>
2024-10-22 10:20:17 +0800

    Add no commands message to undo

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -11,9 +12,10 @@ public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
 
     public static final String MESSAGE_SUCCESS = "Undo successful";
+    public static final String MESSAGE_NO_COMMANDS = "No commands to undo";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         //model.undo();
         return new CommandResult(MESSAGE_SUCCESS);
