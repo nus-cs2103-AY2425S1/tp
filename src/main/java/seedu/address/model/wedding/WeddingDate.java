c7b716edfbcc13834c9dc4ce486677ca38a1eb58
dovlin <dovlincs01@gmail.com>
2024-10-22 16:43:51 +0800

    Add WeddingDate class skeleton

---
+++ src/main/java/seedu/address/model/wedding/WeddingDate.java
@@ -0,0 +1,9 @@
+package seedu.address.model.wedding;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+
+public class WeddingDate {
+
+}
