cfaf6745d4526f02ed0254e651ae87a73e4742ef
thony-ui <anthonyhermanto1@gmail.com>
2024-10-22 20:33:59 +0800

    Add defensive programming for delete client and filter client command

--- src/main/java/seedu/address/logic/commands/FilterClientCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterClientCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.client.Name;
@@ -34,6 +37,7 @@ public class FilterClientCommand extends Command {
 
     /** Message used to format the arguments passed to the command. */
     public static final String MESSAGE_ARGUMENTS = "Name: %1$s";
+    private static final Logger logger = LogsCenter.getLogger(FilterClientCommand.class);
 
     /** The client whose name is being used for filtering. */
     private final Name name;
@@ -47,6 +51,7 @@ public class FilterClientCommand extends Command {
     public FilterClientCommand(Name name) {
         requireAllNonNull(name);
         this.name = name;
+        assert this.name != null : "Name should not be null";
     }
 
     /**
@@ -58,7 +63,9 @@ public class FilterClientCommand extends Command {
      */
     @Override
     public CommandResult execute(Model model) throws CommandException {
+        logger.info("Filtering clients with name starting with: " + name);
         model.updateFilteredClientList(client -> client.getName().toString().matches("(?i)^" + name.toString() + ".*"));
+        logger.info("Displaying clients with name starting with: " + name);
         model.setDisplayClients();
         return new CommandResult(String.format(MESSAGE_SUCCESS + " with name starting with: " + name.toString()));
     }
