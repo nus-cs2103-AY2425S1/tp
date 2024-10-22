0476b66dd987ad0646432bb559c105c21f90b6f6
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-10-22 22:30:54 +0800

    Update command to include Frequency usage

--- src/main/java/seedu/address/logic/commands/PaidCommand.java
+++ src/main/java/seedu/address/logic/commands/PaidCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_FREQUENCY;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
@@ -8,10 +9,12 @@ import java.util.Objects;
 import java.util.Optional;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Frequency;
 import seedu.address.model.person.Person;
 
 
@@ -25,14 +28,15 @@ public class PaidCommand extends Command {
     public static final String COMMAND_WORD = "paid";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Marks the person identified by the index number to have made payment.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Marks the person identified by the index number to have made "
+            + "payment and updates the policy renewal frequency.\n"
+            + "Parameters: INDEX (must be a positive integer) " + "[" + PREFIX_FREQUENCY + "FREQUENCY]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_FREQUENCY + "3";
 
+    public static final String MESSAGE_NO_FREQUENCY = "Please provide a policy renewal frequency";
     public static final String MESSAGE_PAID_PERSON_SUCCESS = "Marked Person: %1$s";
-
     private final Index index;
-
     private final PaidPersonDescriptor paidPersonDescriptor;
 
     /**
@@ -44,7 +48,7 @@ public class PaidCommand extends Command {
         requireNonNull(paidPersonDescriptor);
 
         this.index = index;
-        this.paidPersonDescriptor = new PaidCommand.PaidPersonDescriptor(paidPersonDescriptor);
+        this.paidPersonDescriptor = new PaidPersonDescriptor(paidPersonDescriptor);
     }
 
     @Override
@@ -72,9 +76,11 @@ public class PaidCommand extends Command {
         assert personToPay != null;
 
         Boolean updatedHasPaid = true;
+        Frequency updatedFrequency = paidPersonDescriptor.getFrequency().orElse(personToPay.getFrequency());
 
         return new Person(personToPay.getName(), personToPay.getPhone(), personToPay.getEmail(),
-                personToPay.getAddress(), personToPay.getBirthday(), personToPay.getTags(), updatedHasPaid);
+                personToPay.getAddress(), personToPay.getBirthday(), personToPay.getTags(),
+                updatedHasPaid, updatedFrequency);
     }
 
     @Override
@@ -105,6 +111,7 @@ public class PaidCommand extends Command {
      */
     public static class PaidPersonDescriptor {
         private Boolean hasPaid;
+        private Frequency frequency;
 
         public PaidPersonDescriptor() {}
 
@@ -113,7 +120,8 @@ public class PaidCommand extends Command {
          * A defensive copy of {@code tags} is used internally.
          */
         public PaidPersonDescriptor(PaidPersonDescriptor toCopy) {
-            toCopy.setHasPaid();
+            setHasPaid();
+            setFrequency(toCopy.frequency);
         }
 
         public void setHasPaid() {
@@ -124,6 +132,19 @@ public class PaidCommand extends Command {
             return Optional.ofNullable(hasPaid);
         }
 
+        public void setFrequency(Frequency frequency) {
+            this.frequency = frequency;
+        }
+        public Optional<Frequency> getFrequency() {
+            return Optional.ofNullable(frequency);
+        }
+
+        /**
+         * Returns true if frequency is present.
+         */
+        public boolean isFrequencyPresent() {
+            return CollectionUtil.isAnyNonNull(frequency);
+        }
         @Override
         public boolean equals(Object other) {
             if (other == this) {
@@ -136,13 +157,15 @@ public class PaidCommand extends Command {
             }
 
             PaidCommand.PaidPersonDescriptor otherPaidPersonDescriptor = (PaidCommand.PaidPersonDescriptor) other;
-            return Objects.equals(hasPaid, otherPaidPersonDescriptor.hasPaid);
+            return Objects.equals(hasPaid, otherPaidPersonDescriptor.hasPaid)
+                   && Objects.equals(frequency, otherPaidPersonDescriptor.frequency);
         }
 
         @Override
         public String toString() {
             return new ToStringBuilder(this)
                     .add("hasPaid", hasPaid)
+                    .add("frequency", frequency)
                     .toString();
         }
     }
