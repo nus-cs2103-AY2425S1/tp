7df8c7b4d330de1476acbc4ee7bc14d11d5fbda3
DESKTOP-RRESQ08\User <nicchan0207@gmail.com>
2024-10-23 04:09:35 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/delivery/DeliveryList.java
+++ src/main/java/seedu/address/model/delivery/DeliveryList.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import javafx.collections.FXCollections;
