959c06fcd1faa20b4915d334e366af879c4a2df6
josh1248 <joshthoo@gmail.com>
2024-10-23 01:24:35 +0800

    Remove misleading test class headers

--- src/test/java/seedu/edulog/logic/parser/MarkCommandParserTest.java
+++ src/test/java/seedu/edulog/logic/parser/MarkCommandParserTest.java
@@ -13,13 +13,6 @@ import seedu.edulog.logic.commands.MarkIndexCommand;
 import seedu.edulog.logic.commands.MarkNameCommand;
 
 
-/**
- * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the MarkCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the MarkCommand, and therefore we test only one of them.
- * The path variation for those two cases occur inside the ParserUtil, and
- * therefore should be covered by the ParserUtilTest.
- */
 public class MarkCommandParserTest {
 
     private MarkCommandParser parser = new MarkCommandParser();
