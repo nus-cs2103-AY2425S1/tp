cd7217738000f27dd6b8e9f41202a96d671a2a29
Dinoman44 <prakamya.singh08@gmail.com>
2024-10-22 13:39:26 +0800

    Mark overdue projects with the [OVERDUE] flag

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -63,7 +63,7 @@ public class PersonCard extends UiPart<Region> {
         projectStatus.setText("Project status: " + person.getProjectStatus().toString());
         paymentStatus.setText("Payment status: " + person.getPaymentStatus().toString());
         clientStatus.setText("Client Status: " + person.getClientStatus().toString());
-        deadline.setText("Deadline: " + person.getDeadline().toString());
+        deadline.setText("Deadline: " + person.checkAndGetDeadline());
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
