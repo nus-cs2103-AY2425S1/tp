6f6945a7a208630e440ea628c78ed5bc054be307
wesley <wesleywong08@gmail.com>
2024-10-22 22:12:10 +0800

    Update command word

--- src/main/java/seedu/address/logic/commands/EditPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/EditPersonCommand.java
@@ -36,7 +36,7 @@ import seedu.address.model.tag.Tag;
  */
 public class EditPersonCommand extends Command {
 
-    public static final String COMMAND_WORD = "edit";
+    public static final String COMMAND_WORD = "editp";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
             + "by the index number used in the displayed person list. "
