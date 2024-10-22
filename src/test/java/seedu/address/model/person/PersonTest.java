f69d5bb937f8e3be3cbc1c210f1eb70a5e87ab48
seandias <seankdias@gmail.com>
2024-10-22 18:19:55 +0800

    Fix small bug

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -39,7 +39,7 @@ public class PersonTest {
         editedAlice = new PersonBuilder(ALICE).withName(VALID_NAME_BOB).buildBuyer();
         assertFalse(ALICE.isSamePerson(editedAlice));
 
-        // name differs in case, all other attributes same -> returns false
+        // name differs in case, all other attributes same -> returns true
         Person editedBob = new PersonBuilder(BOB).withName(VALID_NAME_BOB.toLowerCase()).buildBuyer();
         assertTrue(BOB.isSamePerson(editedBob));
 
