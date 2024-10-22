354fabe0213a33c92ddf7051e37ce9e1b775b2d6
yxshrk <yrk17122002@gmail.com>
2024-10-23 00:27:19 +0800

    Fix import order

--- src/main/java/seedu/address/logic/commands/AssignContactToWeddingCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignContactToWeddingCommand.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonId;
