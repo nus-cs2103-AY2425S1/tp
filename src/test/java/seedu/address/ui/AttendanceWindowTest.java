3a38b3b551fade7ec1e9dde9f85017a0ab6383a6
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-10-22 23:13:43 +0800

    fix checkstyle

--- src/test/java/seedu/address/ui/AttendanceWindowTest.java
+++ src/test/java/seedu/address/ui/AttendanceWindowTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-
 import seedu.address.model.student.Student;
 import seedu.address.model.student.TutorialGroup;
 import seedu.address.testutil.StudentBuilder;
