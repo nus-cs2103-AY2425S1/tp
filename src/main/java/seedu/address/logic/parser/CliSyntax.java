f992418a5a7cca235ad86421a9cd6e696e5d32c0
volleyballkickedme <89293409+volleyballkickedme@users.noreply.github.com>
2024-10-22 22:37:56 +0800

    Update EditCommand to support editing of appointment date

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,5 +13,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+    public static final Prefix PREFIX_APPOINTMENT = new Prefix("app/");
 
 }
