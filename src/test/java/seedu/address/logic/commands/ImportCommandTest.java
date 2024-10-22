8119abc1e9f82cba234729471933c7eae800d12f
tohjh <h1710141@nushigh.edu.sg>
2024-10-23 05:44:20 +0800

    Fix test issues

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -18,14 +20,16 @@ public class ImportCommandTest {
         new UserPrefs(),
         SampleAssignmentsUtil.getSamplePredefinedAssignments());
 
-
     @Test
     public void validImportCommandExecution_success() throws CommandException {
-        ImportCommand command = new ImportCommand(
-            "C:\\Users\\User\\Documents\\tp\\src\\test\\data\\testImport.csv");
+        String projectDir = System.getProperty("user.dir");
+        String filePath = projectDir + "/src/test/data/testImport.csv";
+        filePath = filePath.replace("/", File.separator);
+        ImportCommand command = new ImportCommand(filePath);
         CommandResult commandResult = command.execute(model);
         assertEquals("Successfully imported 1 persons.", commandResult.getFeedbackToUser());
     }
+
 
     @Test
     public void invalidImportCommandExecution_fail() {
