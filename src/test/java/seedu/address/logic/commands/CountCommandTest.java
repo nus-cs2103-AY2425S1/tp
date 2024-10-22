2370ea918632f97ab3616319ea4040cc5599c51f
Jalenleeruixian <e1122262@u.nus.edu>
2024-10-22 13:30:46 +0800

    Add checkstyle

--- src/test/java/seedu/address/logic/commands/CountCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CountCommandTest.java
@@ -2,15 +2,15 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
 
 public class CountCommandTest {
     @Test
     public void toStringMethod() {
         Optional<String> tag = Optional.of("friends");
-        CountCommand countCommand = new CountCommand(null,  tag);
+        CountCommand countCommand = new CountCommand(null, tag);
         String expected = CountCommand.class.getCanonicalName() + "{tag=" + tag + "}";
         assertEquals(expected, countCommand.toString());
     }
