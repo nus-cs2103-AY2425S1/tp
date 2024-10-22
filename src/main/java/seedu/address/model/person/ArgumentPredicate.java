be772120495762d12d0c6246a2103068a9aa8123
jktang14 <tangboy14@gmail.com>
2024-10-22 23:07:56 +0800

    Edit code for code quality

--- src/main/java/seedu/address/model/person/ArgumentPredicate.java
+++ src/main/java/seedu/address/model/person/ArgumentPredicate.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,42 +16,23 @@ import static java.util.Objects.requireNonNull;
  * Tests that a {@code Person}'s arguments matches all the requirements
  */
 public class ArgumentPredicate implements Predicate<Person> {
-    private final String name;
-    private final String phone;
-    private final String email;
-    private final String address;
-    private final Set<Tag> tagList;
-    private final String projectStatus;
-    private final String paymentStatus;
-    private final String clientStatus;
-    private final String deadline;
+    private final Person toFind;
 
-    public ArgumentPredicate(String name, String phone, String email, String address, Set<Tag>tagList,
-                             String projectStatus, String paymentStatus, String clientStatus,
-                             String deadline) {
-        requireNonNull(name);
-        requireNonNull(phone);
-        requireNonNull(email);
-        requireNonNull(address);
-        requireNonNull(tagList);
-        requireNonNull(projectStatus);
-        requireNonNull(paymentStatus);
-        requireNonNull(clientStatus);
-        requireNonNull(deadline);
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.tagList = tagList;
-        this.projectStatus = projectStatus;
-        this.paymentStatus = paymentStatus;
-        this.clientStatus = clientStatus;
-        this.deadline = deadline;
+    public ArgumentPredicate(Person person) {
+        requireNonNull(person);
+        toFind = person;
     }
 
     private void addIfPresent(Supplier<String> fieldSupplier, Predicate<Person> predicate,
                               Person person, List<Boolean> validParameters) {
         if (!fieldSupplier.get().isEmpty()) {
+            validParameters.add(predicate.test(person));
+        }
+    }
+
+    private void addIfDeadlinePresent(Supplier<String> fieldSupplier, Predicate<Person> predicate,
+                                      Person person, List<Boolean> validParameters) {
+        if (!fieldSupplier.get().equals(LocalDate.MIN.format(Deadline.OUTPUT_FORMATTER))) {
             validParameters.add(predicate.test(person));
         }
     }
@@ -65,31 +47,31 @@ public class ArgumentPredicate implements Predicate<Person> {
     private List<Boolean> getValidParameters(Person person) {
         List<Boolean> validParameters = new ArrayList<>();
 
-            addIfPresent(() -> name, p -> {
-                String[] nameKeywords = name.split("\\s+");
+            addIfPresent(() -> toFind.getName().toString(), p -> {
+                String[] nameKeywords = toFind.getName().toString().split("\\s+");
                 NameContainsKeywordsPredicate namePredicate = new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords));
                 return namePredicate.test(p);
             }, person, validParameters);
 
-            addIfPresent(() -> phone, p -> phone.equals(p.getPhone().toString()), person, validParameters);
+            addIfPresent(() -> toFind.getPhone().toString(), p -> toFind.getPhone().equals(p.getPhone()), person, validParameters);
 
-            addIfPresent(() -> email, p -> email.equals(p.getEmail().toString()), person, validParameters);
+            addIfPresent(() -> toFind.getEmail().toString(), p -> toFind.getEmail().equals(p.getEmail()), person, validParameters);
 
-            addIfPresent(() -> address, p -> address.equals(p.getAddress().toString()), person, validParameters);
+            addIfPresent(() -> toFind.getAddress().toString(), p -> toFind.getAddress().equals(p.getAddress()), person, validParameters);
 
-            addIfPresent(() -> projectStatus, p -> projectStatus.equals(p.getProjectStatus().toString()),
+            addIfPresent(() -> toFind.getProjectStatus().toString(), p -> toFind.getProjectStatus().equals(p.getProjectStatus()),
                     person, validParameters);
 
-            addIfPresent(() -> paymentStatus, p -> paymentStatus.equals(p.getPaymentStatus().toString()),
+            addIfPresent(() -> toFind.getPaymentStatus().toString(), p -> toFind.getPaymentStatus().equals(p.getPaymentStatus()),
                     person, validParameters);
 
-            addIfPresent(() -> clientStatus, p -> paymentStatus.equals(p.getClientStatus().toString()),
+            addIfPresent(() -> toFind.getClientStatus().toString(), p -> toFind.getClientStatus().equals(p.getClientStatus()),
                     person, validParameters);
 
-            addIfPresent(() -> deadline, p -> deadline.equals(p.getDeadline().toString()),
+            addIfDeadlinePresent(() -> toFind.getDeadline().toString(), p -> toFind.getDeadline().equals(p.getDeadline()),
                     person, validParameters);
 
-            addIfTagPresent(tagList, p -> tagList.stream().anyMatch(p.getTags()::contains), person, validParameters);
+            addIfTagPresent(toFind.getTags(), p -> toFind.getTags().stream().anyMatch(p.getTags()::contains), person, validParameters);
 
         return validParameters;
     }
@@ -100,23 +82,23 @@ public class ArgumentPredicate implements Predicate<Person> {
         return validParameters.stream().allMatch(Boolean::booleanValue);
     }
 
-//    @Override
-//    public boolean equals(Object other) {
-//        if (other == this) {
-//            return true;
-//        }
-//
-//        // instanceof handles nulls
-//        if (!(other instanceof ArgumentPredicate)) {
-//            return false;
-//        }
-//
-//        ArgumentPredicate argumentPredicate = (ArgumentPredicate) other;
-//        return toFind.equals(argumentPredicate.toFind);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return new ToStringBuilder(this).add("person", toFind).toString();
-//    }
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ArgumentPredicate)) {
+            return false;
+        }
+
+        ArgumentPredicate argumentPredicate = (ArgumentPredicate) other;
+        return toFind.equals(argumentPredicate.toFind);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("person", toFind).toString();
+    }
 }
