8bf7e0a8cc1f219e56b94e572529d4aa25162ced
keithxun <keithonghx@gmail.com>
2024-10-22 10:59:37 +0800

    Add points hovering functionality

--- src/main/java/hallpointer/address/ui/MemberCard.java
+++ src/main/java/hallpointer/address/ui/MemberCard.java
@@ -3,11 +3,14 @@ package hallpointer.address.ui;
 import java.util.Comparator;
 
 import hallpointer.address.model.member.Member;
+import hallpointer.address.model.session.Session;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.util.Duration;
 
 /**
  * An UI component that displays information of a {@code Member}.
@@ -15,14 +18,6 @@ import javafx.scene.layout.Region;
 public class MemberCard extends UiPart<Region> {
 
     private static final String FXML = "MemberListCard.fxml";
-
-    /**
-     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
-     * As a consequence, UI elements' variable names cannot be set to such keywords
-     * or an exception will be thrown by JavaFX during runtime.
-     *
-     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
-     */
 
     public final Member member;
 
@@ -44,7 +39,7 @@ public class MemberCard extends UiPart<Region> {
     private FlowPane sessions;
 
     /**
-     * Creates a {@code MemberCode} with the given {@code Member} and index to display.
+     * Creates a {@code MemberCard} with the given {@code Member} and index to display.
      */
     public MemberCard(Member member, int displayedIndex) {
         super(FXML);
@@ -61,6 +56,15 @@ public class MemberCard extends UiPart<Region> {
 
         member.getSessions().stream()
                 .sorted(Comparator.comparing(session -> session.getDate().fullDate))
-                .forEach(session -> sessions.getChildren().add(new Label(session.getSessionName().toString() + " " + session.getDate().toString())));
+                .forEach(session -> {
+                    Label sessionLabel = new Label(session.getSessionName().toString() + " " + session.getDate().toString());
+
+                    // Create a Tooltip to show points on hover
+                    Tooltip sessionTooltip = new Tooltip("Points: " + session.getPoints().toString());
+                    sessionTooltip.setShowDelay(Duration.seconds(0.1));
+                    Tooltip.install(sessionLabel, sessionTooltip);
+
+                    sessions.getChildren().add(sessionLabel);
+                });
     }
 }
