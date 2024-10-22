d0b81688b19739f28ed2b23edf77703e9e363969
tanwartushar <>
2024-10-22 16:07:10 +0800

    Tag.java: fix message constraints

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -9,7 +9,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Tag {
 
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric. It may contain spaces.";
+    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric and may contain spaces.";
     public static final String VALIDATION_REGEX = "[\\p{Alnum} ]+";
 
     public final String tagName;
