89bd1e70278b005cae7276ccfd8788087fdf3d61
Trisha Patodia <trishapatodia@r-121-108-25-172.comp.nus.edu.sg>
2024-10-22 13:21:09 +0800

    Update to remove * import

---
+++ src/main/java/seedu/address/logic/commands/PinCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.model.Model;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class PinCommand extends Command {
+
+    public static final String COMMAND_WORD = "pin";
+
+    public static final String MESSAGE_SUCCESS = "Pinned person";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
