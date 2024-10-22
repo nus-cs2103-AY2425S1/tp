526b2e5d7cf66fcba5c95f7c3c4cab1640b48de6
Rithik <rithiklenin@gmail.com>
2024-10-22 16:25:44 +0800

    Edit functionality for edit policy command class

--- src/main/java/seedu/address/logic/commands/EditPolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/EditPolicyCommand.java
@@ -10,15 +10,19 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-import seedu.address.model.policy.PolicySet;
 import seedu.address.model.policy.EditPolicyDescriptor;
+import seedu.address.model.policy.Policy;
+import seedu.address.model.policy.PolicySet;
+import seedu.address.model.policy.PolicyType;
+
 /**
  * Updates an existing policy for a client in Prudy.
  */
 public class EditPolicyCommand extends Command {
     public static final String COMMAND_WORD = "update-policy";
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Policy updated to:\n%2$s";
-    public static final String MESSAGE_POLICY_NOT_FOUND = "Policy not found.";
+    public static final String MESSAGE_NOT_EDITED = "At least one field needs to be updated";
+    public static final String MESSAGE_POLICY_NOT_FOUND = "Policy of specified type not found.";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Updates the specified policy for the person identified "
             + "by the index number used in the last person listing. \n"
@@ -33,13 +37,13 @@ public class EditPolicyCommand extends Command {
      * Creates an UpdatePolicyCommand to update the specified {@code PolicyMap} of a client.
      *
      * @param index   Index of the client in the filtered client list to update the policy.
-     * @param policies The new policy to be set.
+     * @param editPolicyDescriptor The new optional fields in policy to be set.
      */
     public EditPolicyCommand(Index index, EditPolicyDescriptor editPolicyDescriptor) {
-        requireAllNonNull(index, policies);
+        requireAllNonNull(index, editPolicyDescriptor);
 
         this.index = index;
-        this.policies = policies;
+        this.editPolicyDescriptor = editPolicyDescriptor;
     }
 
     @Override
@@ -53,14 +57,48 @@ public class EditPolicyCommand extends Command {
 
         Person personToEdit = lastShownList.get(index.getZeroBased());
         PolicySet personPolicies = personToEdit.getPolicySet();
-        try {
 
+        PolicyType policyTypeToEdit = editPolicyDescriptor.getPolicyType();
+        Policy policyToRemove = findPolicyByType(personPolicies, policyTypeToEdit);
+
+        if (policyToRemove == null) {
+            throw new CommandException(MESSAGE_POLICY_NOT_FOUND);
         }
 
+        personPolicies.remove(policyToRemove);
+        Policy editedPolicy = createEditedPolicy(policyToRemove, editPolicyDescriptor);
+        personPolicies.add(editedPolicy);
 
+        Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                personToEdit.getAddress(), personToEdit.getTags(), personPolicies);
 
-        return new CommandResult(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), policies));
+        model.setPerson(personToEdit, editedPerson);
+        return new CommandResult(String.format("Edited policy of type %s for %s", policyTypeToEdit, personToEdit.getName()));
 
+    }
+
+    /**
+     * Finds a policy by the given type in the provided set of policies.
+     */
+    private Policy findPolicyByType(PolicySet policySet, PolicyType policyType) {
+        for (Policy policy : policySet) {
+            if (policy.getType().equals(policyType)) {
+                return policy;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Creates a new Policy by copying the old policy and applying the edited fields.
+     */
+    private Policy createEditedPolicy(Policy policyToEdit, EditPolicyDescriptor descriptor) {
+        PremiumAmount updatedPremiumAmount = descriptor.getPremiumAmount().orElse(policyToEdit.getPremiumAmount());
+        CoverageAmount updatedCoverageAmount = descriptor.getCoverageAmount().orElse(policyToEdit.getCoverageAmount());
+        ExpiryDate updatedExpiryDate = descriptor.getExpiryDate().orElse(policyToEdit.getExpiryDate());
+        Policy policy = Policy.makePolicy(descriptor.getPolicyType(),updatedPremiumAmount,
+                updatedCoverageAmount, updatedExpiryDate);
+        return policy;
     }
 
     @Override
@@ -73,7 +111,8 @@ public class EditPolicyCommand extends Command {
             return false;
         }
 
-        EditPolicyCommand upc = (EditPolicyCommand) other;
-        return index.equals(upc.index) && policies.equals(upc.policies);
+        EditPolicyCommand otherCommand = (EditPolicyCommand) other;
+        return index.equals(otherCommand.index)
+                && editPolicyDescriptor.equals(otherCommand.editPolicyDescriptor);
     }
 }
