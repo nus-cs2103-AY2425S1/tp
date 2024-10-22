e48536eb06bb7b2e4ecc2c5ad40a50befbcf6409
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-10-22 22:17:14 +0800

    Add constructor using UUID for Person

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -46,6 +46,23 @@ public class Person implements Comparable<Person> {
         this.uniqueId = UUID.randomUUID(); // Generate a unique ID
     }
 
+    /**
+     * Constructor to create Person from memory using UUID
+     */
+    public Person(Name name, Phone phone, Email email, Address address,
+                  TelegramUsername telegramUsername, Set<Role> roles,
+                   UUID uniqueId) {
+        requireAllNonNull(name, phone, email, address, telegramUsername, roles);
+
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.telegramUsername = telegramUsername;
+        this.roles.addAll(roles);
+        this.uniqueId = uniqueId; // use pre-existing UUID
+    }
+
     public Name getName() {
         return name;
     }
