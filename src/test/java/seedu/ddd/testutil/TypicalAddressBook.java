eadbdf15208e782d2624fd7cfe79d0ec94d022cc
Casper Ng <casperngeen@gmail.com>
2024-10-22 23:20:53 +0800

    Refactor TestUtil to include Events

---
+++ src/test/java/seedu/ddd/testutil/TypicalAddressBook.java
@@ -0,0 +1,30 @@
+package seedu.ddd.testutil;
+
+import static seedu.ddd.testutil.TypicalContacts.getTypicalContacts;
+import static seedu.ddd.testutil.TypicalEvents.getTypicalEvents;
+
+import seedu.ddd.model.AddressBook;
+import seedu.ddd.model.contact.common.Contact;
+import seedu.ddd.model.event.Event;
+
+/**
+ * A utility class containing a list of {@code AddressBook} objects to be used in tests.
+ */
+public class TypicalAddressBook {
+    private TypicalAddressBook() {
+    } // Prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical contacts.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Contact contact : getTypicalContacts()) {
+            ab.addContact(contact);
+        }
+        for (Event event : getTypicalEvents()) {
+            ab.addEvent(event);
+        }
+        return ab;
+    }
+}
