fed24a0a7dcafbd8fbdd13b6e937b7a47ba10a44
xGladiate <sharonsohxuanhui@hotmail.com>
2024-10-22 22:03:44 +0800

    Add classes for unarchive command

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -142,6 +142,16 @@ public class Person {
      * {@code targetIndex} must be a valid index in the deliveryList.
      * The identity of {@code editedDelivery} must not be the same as another existing delivery in the list.
      */
+    public void unarchiveDelivery(Index targetIndex, Delivery unarchivedDelivery) {
+        deleteDelivery(targetIndex);
+        addDelivery(getFirstArchivedIndex(), unarchivedDelivery);
+    }
+
+    /**
+     * Replaces the given delivery {@code target} in the list with {@code editedDelivery}.
+     * {@code targetIndex} must be a valid index in the deliveryList.
+     * The identity of {@code editedDelivery} must not be the same as another existing delivery in the list.
+     */
     public Index getFirstArchivedIndex() {
         return deliveryList.getFirstArchivedIndex();
     }
