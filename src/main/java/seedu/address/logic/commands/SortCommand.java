e1d3561e5cc551caa68f69b631f390fe4df51cd4
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-10-23 01:03:43 +0800

    Add toString() and equals() to SortCommand

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
@@ -42,5 +43,29 @@ public class SortCommand extends Command {
         requireNonNull(model);
         model.sortFilteredPersonList(sortParameter, isAscendingOrder);
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof AddCommand)) {
+            return false;
+        }
+
+        SortCommand otherSortCommand = (SortCommand) other;
+        return (sortParameter.equals(otherSortCommand.sortParameter) &&
+                (isAscendingOrder == otherSortCommand.isAscendingOrder));
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("sortParameter", sortParameter)
+                .add("sortOrder", isAscendingOrder ? "ascending" : "descending")
+                .toString();
     }
 }
