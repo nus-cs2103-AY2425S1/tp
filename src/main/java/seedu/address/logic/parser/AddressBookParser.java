d3518a894935073eb12add6e63179d443b0f9b96
Gilbb <gilbert.iskandar2002@gmail.com>
2024-10-22 14:06:57 +0800

    Create split list command to person and event

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,7 +18,7 @@ import seedu.address.logic.commands.EventCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListPersonCommand;
 import seedu.address.logic.commands.UpdateCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -74,8 +74,8 @@ public class AddressBookParser {
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
 
-        case ListCommand.COMMAND_WORD:
-            return new ListCommand();
+        case ListPersonCommand.COMMAND_WORD:
+            return new ListPersonCommand();
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
