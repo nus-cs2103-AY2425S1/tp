7e86d4e3f1bf99dae8a88b3ac3138a2774af5930
rachelyeohm <rachelyeohm@gmail.com>
2024-10-23 02:15:25 +0800

    Move parsing function to ParserUtil

--- src/main/java/seedu/address/model/util/FunctionWithException.java
+++ src/main/java/seedu/address/model/util/FunctionWithException.java
@@ -1,5 +1,8 @@
 package seedu.address.model.util;
 
+/**
+ * A functional interface similar to Function<T,R> but allows for a specified exception to be thrown.
+ */
 public interface FunctionWithException<S, T, R extends Throwable> {
-    public T apply(S value) throws R;
+    T apply(S value) throws R;
 }
