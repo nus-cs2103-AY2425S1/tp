4d50f8f59d02a63dab1db2efd3981ab5af72e630
Rithik <rithiklenin@gmail.com>
2024-10-22 16:26:23 +0800

    Add package for parser class

--- src/main/java/seedu/address/logic/parser/EditPolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditPolicyCommandParser.java
@@ -10,9 +10,8 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.EditPolicyCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.policy.Policy;
 import seedu.address.model.policy.PolicyType;
-import seedu.address.model.policy.Edit
+import seedu.address.model.policy.EditPolicyDescriptor;
 
 /**
  * Parses input arguments and creates a new UpdatePolicyCommand object.
@@ -45,7 +44,6 @@ public class EditPolicyCommandParser implements Parser<EditPolicyCommand> {
         PolicyType policyType = ParserUtil.parsePolicyType(argMultimap.getValue(PREFIX_POLICY_TYPE).get());
         EditPolicyDescriptor editPolicyDescriptor = new EditPolicyDescriptor(policyType);
 
-        // Set optional fields (these fields can be edited)
         if (argMultimap.getValue(PREFIX_POLICY_PREMIUM_AMOUNT).isPresent()) {
             editPolicyDescriptor.setPremiumAmount(ParserUtil.parsePolicyAmount(argMultimap.getValue(PREFIX_POLICY_PREMIUM_AMOUNT).get()));
         }
@@ -65,46 +63,4 @@ public class EditPolicyCommandParser implements Parser<EditPolicyCommand> {
     }
 }
 
-//public class AddPolicyCommandParser implements Parser<AddPolicyCommand> {
-//    /**
-//     * Parses the given {@code String} of arguments in the context of the {@code AddPolicyCommand}
-//     * and returns an {@code AddPolicyCommand} object for execution.
-//     *
-//     * @throws ParseException if the user input does not conform to the expected format.
-//     */
-//    public AddPolicyCommand parse(String args) throws ParseException {
-//        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_POLICY_TYPE,
-//                PREFIX_POLICY_PREMIUM_AMOUNT, PREFIX_POLICY_COVERAGE_AMOUNT, PREFIX_POLICY_EXPIRY_DATE);
-//
-//        // Ensure there is at least one policy and that the preamble is not empty
-//        if (!(argMultimap.getValue(PREFIX_POLICY_TYPE).isPresent()) || argMultimap.getPreamble().isEmpty()) {
-//            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPolicyCommand.MESSAGE_USAGE));
-//        }
-//
-//        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_POLICY_TYPE, PREFIX_POLICY_PREMIUM_AMOUNT,
-//                PREFIX_POLICY_COVERAGE_AMOUNT, PREFIX_POLICY_EXPIRY_DATE);
-//
-//        Index index;
-//        try {
-//            // Parse the preamble as the index of the person
-//            index = ParserUtil.parseIndex(argMultimap.getPreamble());
-//        } catch (IllegalValueException ive) {
-//            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-//                    AddPolicyCommand.MESSAGE_USAGE), ive);
-//        }
-//
-//        assert argMultimap.getValue(PREFIX_POLICY_TYPE).isPresent() : "Expected value for 'pt/' but none found.";
-//        PolicyType policyType = ParserUtil.parsePolicyType(
-//                argMultimap.getValue(PREFIX_POLICY_TYPE).get());
-//        double premiumAmount = ParserUtil.parsePolicyAmount(
-//                argMultimap.getValue(PREFIX_POLICY_PREMIUM_AMOUNT).orElse(""));
-//        double coverageAmount = ParserUtil.parsePolicyAmount(
-//                argMultimap.getValue(PREFIX_POLICY_COVERAGE_AMOUNT).orElse(""));
-//        LocalDate expiryDate = ParserUtil.parseExpiryDate(
-//                argMultimap.getValue(PREFIX_POLICY_EXPIRY_DATE).orElse(""));
-//
-//        Policy policy = Policy.makePolicy(policyType, premiumAmount, coverageAmount, expiryDate);
-//        return new AddPolicyCommand(index, policy);
-//    }
-//}
 
