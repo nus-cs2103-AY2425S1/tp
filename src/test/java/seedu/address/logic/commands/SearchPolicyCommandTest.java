8176a2daeb727bb16bd94e5aa204dab93f0a11fd
M-kew <e0958776@u.nus.edu>
2024-10-22 20:41:32 +0800

    Resolve checkstyle issues

--- src/test/java/seedu/address/logic/commands/SearchPolicyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SearchPolicyCommandTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static org.junit.jupiter.api.Assertions.fail;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
