66fdcf36f34cb8634d833f00fdadb8e90cea4b70
IanCheah <iancheah24@gmail.com>
2024-10-22 11:37:34 +0800

    Add SaveSortPreference command

---
+++ src/main/java/seedu/address/logic/parser/SaveSortPreferenceCommandParser.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.SaveSortPreferenceCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new {@code SaveSortPreference} object
+ */
+public class SaveSortPreferenceCommandParser implements Parser<SaveSortPreferenceCommand> {
+    @Override
+    public SaveSortPreferenceCommand parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+
+        String args = userInput.trim().toLowerCase();
+
+        switch (args) {
+        case "high":
+            return new SaveSortPreferenceCommand("high");
+        case "low":
+            return new SaveSortPreferenceCommand("low");
+        case "default":
+            // empty string denotes the original
+            return new SaveSortPreferenceCommand("default");
+        default:
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, SaveSortPreferenceCommand.MESSAGE_USAGE));
+        }
+    }
+}
