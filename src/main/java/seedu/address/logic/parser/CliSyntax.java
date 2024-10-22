c8ad69bfc60f017f6f6e2cc3b5660ec614d06640
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-10-22 22:26:22 +0800

    create new prefix for frequency for use in paid command

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -12,4 +12,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_BIRTHDAY = new Prefix("b/");
+    public static final Prefix PREFIX_FREQUENCY = new Prefix("f/");
 }
