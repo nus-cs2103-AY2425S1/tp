f4536907021cd6d1bc1ff3f6f32f960e1c6d2de0
YANYAN-w <e0773610@u.nus.edu>
2024-10-22 23:50:28 +0800

    Add more test cases for edit parsers

--- src/test/java/seedu/address/logic/parser/EditTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditTaskCommandParserTest.java
@@ -1,8 +1,9 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.TEAM_FIVE;
-import static seedu.address.logic.commands.CommandTestUtil.TEAM_FOUR;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TASK_DEADLINE;
@@ -15,9 +16,11 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
 import seedu.address.logic.commands.editcommands.EditTaskCommand;
 import seedu.address.logic.commands.editcommands.EditTaskCommand.EditTaskDescriptor;
 import seedu.address.logic.parser.editcommands.EditTaskCommandParser;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.group.GroupName;
 import seedu.address.model.task.Deadline;
 import seedu.address.model.task.TaskName;
@@ -46,13 +49,16 @@ public class EditTaskCommandParserTest {
             EditTaskCommand.MESSAGE_USAGE));
 
         String userInputMissingGroupName = " " + PREFIX_INDEX + "1";
-        assertParseFailure(parser, userInputMissingGroupName, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-            EditTaskCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, userInputMissingGroupName,
+            String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditTaskCommand.MESSAGE_USAGE));
     }
     @Test
     public void parse_noFieldsEdited_failure() {
-        String userInput = " " + PREFIX_GROUP_NAME + TEAM_FOUR + " " + PREFIX_INDEX + "1";
-
-        assertParseFailure(parser, userInput, EditTaskCommand.MESSAGE_NOT_EDITED);
+        String userInput = " " + PREFIX_GROUP_NAME + TEAM_FIVE + " " + PREFIX_INDEX + "1 "
+            + PREFIX_TASK_NAME + "TP " + PREFIX_TASK_DEADLINE + "2025-12-31 2300 "
+            + PREFIX_EMAIL + "e0000000@u.nus.edu";
+        assertThrows(ParseException.class, ()->parser.parse(userInput),
+            String.format(Messages.MESSAGE_ILLEGAL_PREFIX_USED, EditTaskCommand.MESSAGE_USAGE));
     }
+
 }
