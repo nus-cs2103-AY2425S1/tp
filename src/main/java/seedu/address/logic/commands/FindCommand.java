8119ac9e3d91eada166472dba4de029e8a5bf935
jevan <e1121960@u.nus.edu>
2024-10-22 21:32:00 +0800

    Update code style to pass check style
    - Fixed error of + on a new line

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -19,8 +19,8 @@ public class FindCommand extends Command {
     public static final String ARGUMENT_WORD = "all";
 
     public static final String COMMAND_WORD = "find";
-    public static final String MESSAGE_PURPOSE = "Finds all employees and/or potential hires whose names " +
-            "contain any of the specified keywords (case-insensitive) and displays them as a list with";
+    public static final String MESSAGE_PURPOSE = "Finds all employees and/or potential hires whose names "
+            + "contain any of the specified keywords (case-insensitive) and displays them as a list with";
     public static final String MESSAGE_FORMAT = COMMAND_WORD + " (" + FindEmployeeCommand.ARGUMENT_WORD
         + "/" + FindPotentialCommand.ARGUMENT_WORD + "/" + FindCommand.ARGUMENT_WORD + ") KEYWORD [MORE_KEYWORDS]";
     public static final String MESSAGE_EXAMPLE = COMMAND_WORD + " " + ARGUMENT_WORD + "n/alice bob e/alice@gmail.com";
