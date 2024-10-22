02c5b58e9f8953a98e2e510d78ee8858b91f25a0
kvernnn <156299531+kvernnn@users.noreply.github.com>
2024-10-22 13:14:26 +0800

    Change layout of the TagListPanel to be horizontal using FlowPane

--- src/main/java/seedu/address/ui/TagListPanel.java
+++ src/main/java/seedu/address/ui/TagListPanel.java
@@ -4,8 +4,7 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import seedu.address.model.tag.Tag;
 
@@ -15,45 +14,26 @@ import seedu.address.model.tag.Tag;
 public class TagListPanel extends UiPart<Region> {
     private static final String FXML = "TagListPanel.fxml";
     @FXML
-    private ListView<Tag> tagListView;
+    private FlowPane tagListFlowPane;
 
     /**
      * Creates a {@code TagListPanel} with the given {@code ObservableList}.
      */
     public TagListPanel(ObservableList<Tag> tags) {
         super(FXML);
-        tagListView.setItems(tags);
-        System.out.println(tagListView.getItems());
-        tagListView.setCellFactory(listView -> new TagListViewCell());
+        updateTagsDisplay(tags);
 
         tags.addListener((ListChangeListener<Tag>) c -> {
-            System.out.println("TagListPanel: Detected change in tag list.");
-            while (c.next()) {
-                if (c.wasAdded() || c.wasRemoved()) {
-                    System.out.println("Tags added/removed.");
-                    tagListView.refresh();
-                }
-            }
+            updateTagsDisplay(tags);
         });
     }
 
-    /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Tag} using a {@code Label}.
-     */
-    class TagListViewCell extends ListCell<Tag> {
-        @Override
-        protected void updateItem(Tag tag, boolean empty) {
-            super.updateItem(tag, empty);
-
-            if (empty || tag == null) {
-                setGraphic(null);
-                setText(null);
-            } else {
-                Label tagLabel = new Label(tag.getTagName());
-                tagLabel.getStyleClass().add("tag-label");
-                setGraphic(tagLabel);
-            }
+    private void updateTagsDisplay(ObservableList<Tag> tags) {
+        tagListFlowPane.getChildren().clear();
+        for (Tag tag : tags) {
+            Label tagLabel = new Label(tag.getTagName());
+            tagLabel.getStyleClass().add("tag-label");
+            tagListFlowPane.getChildren().add(tagLabel);
         }
     }
-
 }
