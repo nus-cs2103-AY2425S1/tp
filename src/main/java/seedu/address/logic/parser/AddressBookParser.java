087b230252729e99b9a7df4ee582178c3b47d567
Soumyadip <soumyadip1107@gmail.com>
2024-10-22 22:07:07 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,8 +18,8 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.IncomeCommand;
 import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.SettleCommand;
 import seedu.address.logic.commands.PayCommand;
+import seedu.address.logic.commands.SettleCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
