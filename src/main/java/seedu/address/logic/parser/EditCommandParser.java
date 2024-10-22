55f5e1caf8f5ab555025916e0815985f14c525c1
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-10-22 11:48:43 +0800

    Fix format in edit command parser

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -32,9 +32,13 @@ public class EditCommandParser implements Parser<EditCommand> {
      */
     public EditCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_NRIC, PREFIX_PHONE,
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+                PREFIX_NAME,
+                PREFIX_NRIC,
+                PREFIX_PHONE,
                 PREFIX_EMAIL,
-                PREFIX_ADDRESS, PREFIX_TAG);
+                PREFIX_ADDRESS,
+                PREFIX_TAG);
 
         Nric nric;
 
