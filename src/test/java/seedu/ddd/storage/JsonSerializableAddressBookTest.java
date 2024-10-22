7c3b49f9300cbd140f87fafe5cdf03e49aecc8c7
Casper Ng <casperngeen@gmail.com>
2024-10-22 23:24:56 +0800

    Update test cases to include Events

--- src/test/java/seedu/ddd/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/ddd/storage/JsonSerializableAddressBookTest.java
@@ -11,23 +11,25 @@ import org.junit.jupiter.api.Test;
 import seedu.ddd.commons.exceptions.IllegalValueException;
 import seedu.ddd.commons.util.JsonUtil;
 import seedu.ddd.model.AddressBook;
-import seedu.ddd.testutil.TypicalContacts;
+import seedu.ddd.testutil.TypicalAddressBook;
 
 public class JsonSerializableAddressBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-    private static final Path TYPICAL_CONTACTS_FILE = TEST_DATA_FOLDER.resolve("typicalContactsAddressBook.json");
+    private static final Path TYPICAL_CONTACTS_FILE = TEST_DATA_FOLDER.resolve("typicalAddressBook.json");
     private static final Path INVALID_CLIENT_FILE = TEST_DATA_FOLDER.resolve("invalidClientAddressBook.json");
     private static final Path INVALID_VENDOR_FILE = TEST_DATA_FOLDER.resolve("invalidVendorAddressBook.json");
+    private static final Path INVALID_EVENT_FILE = TEST_DATA_FOLDER.resolve("invalidEventAddressBook.json");
     private static final Path DUPLICATE_CLIENTS_FILE = TEST_DATA_FOLDER.resolve("duplicateClientsAddressBook.json");
     private static final Path DUPLICATE_VENDORS_FILE = TEST_DATA_FOLDER.resolve("duplicateVendorsAddressBook.json");
+    private static final Path DUPLICATE_EVENTS_FILE = TEST_DATA_FOLDER.resolve("duplicateEventsAddressBook.json");
 
     @Test
     public void toModelType_typicalContactsFile_success() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_CONTACTS_FILE,
                 JsonSerializableAddressBook.class).get();
         AddressBook addressBookFromFile = dataFromFile.toModelType();
-        AddressBook typicalContactsAddressBook = TypicalContacts.getTypicalAddressBook();
+        AddressBook typicalContactsAddressBook = TypicalAddressBook.getTypicalAddressBook();
         assertEquals(addressBookFromFile, typicalContactsAddressBook);
     }
 
@@ -46,6 +48,13 @@ public class JsonSerializableAddressBookTest {
     }
 
     @Test
+    public void toModelType_invalidEventFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_EVENT_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
     public void toModelType_duplicateClients_throwsIllegalValueException() throws Exception {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_CLIENTS_FILE,
                 JsonSerializableAddressBook.class).get();
@@ -58,6 +67,14 @@ public class JsonSerializableAddressBookTest {
         JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_VENDORS_FILE,
                 JsonSerializableAddressBook.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_VENDOR,
+                dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_duplicateEvents_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_EVENTS_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_EVENT,
                 dataFromFile::toModelType);
     }
 }
