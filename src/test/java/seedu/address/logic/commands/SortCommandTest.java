f53ae5b4f4c09ec76d95864a9e6a3e8e0184fa94
DESKTOP-RRESQ08\User <nicchan0207@gmail.com>
2024-10-22 23:39:42 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,16 +1,15 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class SortCommandTest {
     private static final String VALID_ATTRIBUTE = "address";
