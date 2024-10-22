317ae981ac54fe55834b4aedc94e16b185f13285
Kan_Jun_Hong <junhongkannot@gmail.com>
2024-10-23 00:42:33 +0800

    Update AddressBookParserTest
    Updated AddressBookParserTest to test the new commands created for
    VolunSync.
    
    Currently the code does not compile because it was written to
    accommodate changes in PR #109 that have not been merged.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -7,13 +7,99 @@ import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.AssignCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.UnassignCommand;
+import seedu.address.logic.commands.eventcommands.EventDeleteCommand;
+import seedu.address.logic.commands.eventcommands.EventNewCommand;
+import seedu.address.logic.commands.eventcommands.EventViewCommand;
+import seedu.address.logic.commands.eventcommands.FindEventCommand;
+import seedu.address.logic.commands.volunteercommands.FindVolunteerCommand;
+import seedu.address.logic.commands.volunteercommands.VolunteerAddCommand;
+import seedu.address.logic.commands.volunteercommands.VolunteerDeleteCommand;
+import seedu.address.logic.commands.volunteercommands.VolunteerViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.volunteer.Volunteer;
 
 public class AddressBookParserTest {
 
     private final AddressBookParser parser = new AddressBookParser();
+
+    private String createCommand(String indicator, String commandWord) {
+        return indicator + " " + commandWord;
+    }
+    @Test
+    public void parseCommand_newEvent() throws Exception {
+        assertTrue(
+                parser.parseCommand(createCommand(AddressBookParser.EVENT_COMMAND_INDICATOR,
+                        EventNewCommand.COMMAND_WORD)) instanceof EventNewCommand
+        );
+    }
+
+    @Test
+    public void parseCommand_deleteEvent() throws Exception {
+        assertTrue(
+                parser.parseCommand(createCommand(AddressBookParser.EVENT_COMMAND_INDICATOR,
+                        EventDeleteCommand.COMMAND_WORD)) instanceof EventDeleteCommand
+        );
+    }
+
+    @Test
+    public void parseCommand_viewEvent() throws Exception {
+        assertTrue(
+            parser.parseCommand(createCommand(AddressBookParser.EVENT_COMMAND_INDICATOR,
+                  EventViewCommand.COMMAND_WORD)) instanceof EventViewCommand
+        );
+    }
+
+    @Test
+    public void parseCommand_findEvent() throws Exception {
+        assertTrue(
+            parser.parseCommand(createCommand(AddressBookParser.EVENT_COMMAND_INDICATOR,
+                    FindEventCommand.COMMAND_WORD)) instanceof FindEventCommand
+        );
+    }
+    @Test
+    public void parseCommand_newVolunteer() throws Exception {
+        assertTrue(
+            parser.parseCommand(createCommand(AddressBookParser.VOLUNTEER_COMMAND_INDICATOR,
+            VolunteerNewCommand.COMMAND_WORD)) instanceof VolunteerNewCommand
+        );
+    }
+
+    @Test
+    public void parseCommand_deleteVolunteer() throws Exception {
+        assertTrue(
+            parser.parseCommand(createCommand(AddressBookParser.VOLUNTEER_COMMAND_INDICATOR,
+                VolunteerDeleteCommand.COMMAND_WORD)) instanceof VolunteerDeleteCommand
+        );
+    }
+
+    @Test
+    public void parseCommand_viewVolunteer() throws Exception {
+        assertTrue(
+            parser.parseCommand(createCommand(AddressBookParser.VOLUNTEER_COMMAND_INDICATOR,
+            VolunteerViewCommand.COMMAND_WORD)) instanceof VolunteerViewCommand
+        );
+    }
+
+    @Test void parseCommand_findVolunteer() throws Exception {
+        assertTrue(
+            parser.parseCommand(createCommand(AddressBookParser.VOLUNTEER_COMMAND_INDICATOR,
+                    FindVolunteerCommand.COMMAND_WORD)) instanceof FindVolunteerCommand
+        );
+    }
+
+    @Test
+    public void parseCommand_assign() throws Exception {
+        assertTrue(parser.parseCommand(AssignCommand.COMMAND_WORD) instanceof AssignCommand);
+    }
+
+    @Test
+    public void parseCommand_unassign() throws Exception {
+        assertTrue(parser.parseCommand(UnassignCommand.COMMAND_WORD) instanceof UnassignCommand);
+    }
 
     @Test
     public void parseCommand_exit() throws Exception {
