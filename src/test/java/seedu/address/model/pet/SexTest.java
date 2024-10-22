30ca1700c7b2fd957331090cb3c6e1c41e0b571c
Runus01 <rodriguesj.aaron@gmail.com>
2024-10-22 16:07:31 +0800

    Add test for pet's sex::toString() method

--- src/test/java/seedu/address/model/pet/SexTest.java
+++ src/test/java/seedu/address/model/pet/SexTest.java
@@ -1,10 +1,14 @@
 package seedu.address.model.pet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.PetBuilder;
 
 public class SexTest {
 
@@ -36,6 +40,30 @@ public class SexTest {
         assertTrue(Name.isValidName("M")); // one character only and is 'M' or 'F'
         assertTrue(Name.isValidName("f")); // lowercase 'm' or 'f'
         assertTrue(Name.isValidName("m")); // lowercase 'm' or 'f'
+    }
+
+    @Test
+    public void testToString() {
+        Pet validFemalePet1 = new PetBuilder().withName("Fluffy").withSpecies("Dog").withBreed("Golden Retriever")
+                .withAge("5").withSex("F").build();
+        Pet validFemalePet2 = new PetBuilder().withName("Fluffy").withSpecies("Dog").withBreed("Golden Retriever")
+                .withAge("5").withSex("f").build();
+        Pet validMalePet1 = new PetBuilder().withName("Tubby").withSpecies("Turtle").withBreed("Pelomedusidae")
+                .withAge("2").withSex("M").build();
+        Pet validMalePet2 = new PetBuilder().withName("Tubby").withSpecies("Turtle").withBreed("Pelomedusidae")
+                .withAge("2").withSex("m").build();
+
+        // valid toString output
+        assertEquals("Female", validFemalePet1.getSex().toString());
+        assertEquals("Female", validFemalePet2.getSex().toString());
+        assertEquals("Male", validMalePet1.getSex().toString());
+        assertEquals("Male", validMalePet2.getSex().toString());
+
+        // invalid toString output
+        assertNotEquals("F", validFemalePet1.getSex().toString());
+        assertNotEquals("f", validFemalePet2.getSex().toString());
+        assertNotEquals("M", validMalePet1.getSex().toString());
+        assertNotEquals("m", validMalePet2.getSex().toString());
     }
 
     @Test
