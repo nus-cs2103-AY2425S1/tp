2f7bc0bf139c71957006cd5cb978e2af96d271a4
myrilla12 <myrilla12@gmail.com>
2024-10-22 13:04:03 +0800

    Implement export function
    Reminder, works for addressbook.jar only

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -2,6 +2,9 @@ package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
 
+import java.io.File;
+import java.io.IOException;
+
 import static java.util.Objects.requireNonNull;
 
 /**
@@ -11,11 +14,33 @@ public class ExportCommand extends Command {
 
     public static final String COMMAND_WORD = "export";
 
-    public static final String MESSAGE_EXPORT_ACKNOWLEDGEMENT = "Exporting addressbook.json under file path ...";
+    public static final String MESSAGE_EXPORT_ACKNOWLEDGEMENT = "Exporting addressbook.json under "
+            + "file path data/addressbook.json in whichever folder addressbook.jar is in";
+    private static final String ADDRESSBOOK_FILE_PATH = "data/addressbook.json";
+    private static final String MESSAGE_EXPORT_FAIL = "Export failed!";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+
+        try {
+            File file = new File(ADDRESSBOOK_FILE_PATH);
+            File dir = file.getParentFile();
+
+            // if directory does not exist, make it
+            if (dir != null && !dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // if file doesn't exist, make it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new CommandResult(MESSAGE_EXPORT_FAIL);
+        }
+
         return new CommandResult(MESSAGE_EXPORT_ACKNOWLEDGEMENT);
     }
 
