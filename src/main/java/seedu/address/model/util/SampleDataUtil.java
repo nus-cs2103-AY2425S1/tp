c6e7597337589a4e63e775170320f6557703e3f5
gladwintan <gladwintan@hotmail.sg>
2024-10-23 21:13:01 +0800

    Update sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,15 +21,11 @@ import seedu.address.model.tutorial.Tutorial;
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
-    public static Person alex = new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-            new Address("Blk 30 Geylang Street 29, #06-40"), new Payment("200"),
-            new ArrayList<Participation>(), getTagSet("friends"));
-
     public static Person[] getSamplePersons() {
-        alex.getParticipation().add(new Participation(alex, new Tutorial("Math")));
-        alex.getParticipation().add(new Participation(alex, new Tutorial("Math")));
         return new Person[] {
-            alex, 
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                    new Address("Blk 30 Geylang Street 29, #06-40"), new Payment("200"),
+                    new ArrayList<Participation>(), getTagSet("friends")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Payment("300"),
                     new ArrayList<Participation>(), getTagSet("colleagues", "friends")),
