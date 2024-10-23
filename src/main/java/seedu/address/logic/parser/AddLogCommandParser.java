bc51a693237b77e89b3112997b6319376dcb4d36
junyi73 <lerjunyi@gmail.com>
2024-10-23 23:37:22 +0800

    Change syntax for AddLogCommand

--- src/main/java/seedu/address/logic/parser/AddLogCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddLogCommandParser.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_IDENTITY_NUMBER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LOG;
 
 import seedu.address.logic.commands.AddLogCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -18,18 +20,21 @@ public class AddLogCommandParser implements Parser<AddLogCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddLogCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_IDENTITY_NUMBER, PREFIX_LOG, PREFIX_DATE);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_IDENTITY_NUMBER, PREFIX_LOG, PREFIX_DATE);
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_IDENTITY_NUMBER, PREFIX_LOG, PREFIX_DATE);
 
-        if (argMultimap.getValue(PREFIX_IDENTITY_NUMBER).isEmpty() || argMultimap.getValue(PREFIX_LOG).isEmpty() || argMultimap.getValue(PREFIX_DATE).isEmpty()) {
+        if (argMultimap.getValue(PREFIX_IDENTITY_NUMBER).isEmpty() || argMultimap.getValue(PREFIX_LOG).isEmpty()
+                || argMultimap.getValue(PREFIX_DATE).isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddLogCommand.MESSAGE_USAGE));
         }
 
         try {
             // Parse identity number
-            IdentityNumber identityNumber = ParserUtil.parseIdentityNumber(argMultimap.getValue(PREFIX_IDENTITY_NUMBER).get());
+            IdentityNumber identityNumber = ParserUtil.parseIdentityNumber(
+                    argMultimap.getValue(PREFIX_IDENTITY_NUMBER).get());
 
             // Parse date
             String date = argMultimap.getValue(PREFIX_DATE).get();
