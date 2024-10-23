6889073f5d45ae32e456e09021c15a566623fd7d
SherChew27 <shermanczj@gmail.com>
2024-10-23 18:42:31 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -9,18 +9,7 @@ import seedu.address.logic.commands.FilterCommand;
 
 public class FilterCommandParserTest {
     private FilterCommandParser parser = new FilterCommandParser();
-
-//    @Test
-//    public void parse_validargs_returnsFilterCommand() {
-//        Tag tag = new Tag("black");
-//        assertParseSuccess(parser, "t/black", new FilterTagCommand(new TagContainsKeywordsPredicate(tag)));
-//    }
-
-//    @Test
-//    public void parse_invalidFilterField_throwsParseException() {
-//        assertParseFailure(parser, "name", MESSAGE_INVALID_FILTER_CRITERIA);
-//    }
-
+    
     @Test
     public void parse_noFilterField_throwsParseException() {
         assertParseFailure(parser, " ",
