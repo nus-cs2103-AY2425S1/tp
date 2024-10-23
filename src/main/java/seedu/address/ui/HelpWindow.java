1d3a891e6a314c5b7819bc3cf86645a36654df7d
Shantaa <shanetay4425@gmail.com>
2024-10-24 00:02:35 +0800

    add more javadocs

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -1,8 +1,8 @@
 package seedu.address.ui;
 
 import java.util.logging.Logger;
-import javafx.fxml.FXML;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -40,6 +40,11 @@ public class HelpWindow extends UiPart<Stage> {
     private HelpContentManager contentManager;
     private HelpLogicManager logicManager;
 
+    /**
+     * Creates a new HelpWindow.
+     *
+     * @param root Stage to use as the root of the HelpWindow.
+     */
     public HelpWindow(Stage root) {
         super(FXML, root);
         this.contentManager = new HelpContentManager();
@@ -50,21 +55,47 @@ public class HelpWindow extends UiPart<Stage> {
     public HelpWindow() {
         this(new Stage());
     }
-
+    /**
+     * Shows the help window.
+     * @throws IllegalStateException
+     *     <ul>
+     *         <li>
+     *             if this method is called on a thread other than the JavaFX Application Thread.
+     *         </li>
+     *         <li>
+     *             if this method is called during animation or layout processing.
+     *         </li>
+     *         <li>
+     *             if this method is called on the primary stage.
+     *         </li>
+     *         <li>
+     *             if {@code dialogStage} is already showing.
+     *         </li>
+     *     </ul>
+     */
     public void show() {
         logger.fine("Showing help page about the application.");
         getRoot().show();
         getRoot().centerOnScreen();
     }
 
+    /**
+     * Returns true if the help window is currently being shown.
+     */
     public boolean isShowing() {
         return getRoot().isShowing();
     }
 
+    /**
+     * Hides the help window.
+     */
     public void hide() {
         getRoot().hide();
     }
 
+    /**
+     * Focuses on the help window.
+     */
     public void focus() {
         getRoot().requestFocus();
     }
@@ -101,6 +132,7 @@ public class HelpWindow extends UiPart<Stage> {
         commandSummaryTable.setVisible(false);
     }
 
+    /** Displays the command summary in the TableView. */
     public void displayCommandSummary() {
         helpContentFlow.getChildren().clear();
         commandSummaryTable.setVisible(true);
