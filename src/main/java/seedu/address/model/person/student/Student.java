f639c7c274ca461ff9ad5d17f39e36aea8cfc218
Siow Rui Ming <siowruiming@gmail.com>
2024-10-23 09:22:59 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/person/student/Student.java
+++ src/main/java/seedu/address/model/person/student/Student.java
@@ -42,7 +42,6 @@ public class Student extends Person {
     public StudentID getStudentID() {
         return studentID;
     }
-    
     /**
      * Return null as student does not have an industry
      * @return null
