732dd24261c2eccf40667fa109daac766bddd3bf
blackpanther9229 <hoangnguyen09022004@gmail.com>
2024-10-24 00:20:24 +0800

    More check style error fixed

--- src/main/java/seedu/address/logic/commands/AddTagCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
