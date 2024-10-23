7a7525be69745a397b52cc1a8aeb77f3f94751ca
MAOXIONGKAI <conan7153@gmail.com>
2024-10-23 22:50:03 +0800

    Change help command syntax and provide more features

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -1,5 +1,10 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_HELP_KEYWORD;
+import static seedu.address.logic.Messages.getOrderedListString;
+
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -9,13 +14,76 @@ public class HelpCommand extends Command {
 
     public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + " <Command Keyword>"
+            + ": Shows usage of the given command keyword\n"
+            + "Example: " + COMMAND_WORD + " add\n"
+            + COMMAND_WORD + ": Shows full list of keywords and usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String[] CURRENT_SUPPORTED_INSTRUCTION_USAGES = new String[]{
+        AddCommand.MESSAGE_USAGE,
+        DeleteCommand.MESSAGE_USAGE,
+        EditCommand.MESSAGE_USAGE,
+        FindCommand.MESSAGE_USAGE,
+        ListCommand.MESSAGE_USAGE,
+        HelpCommand.MESSAGE_USAGE,
+        ClearCommand.MESSAGE_USAGE,
+        ExitCommand.MESSAGE_USAGE
+    };
+
+    public static final String FULL_HELP_MESSAGE = getOrderedListString(CURRENT_SUPPORTED_INSTRUCTION_USAGES);
+
+    private final String keyword;
+
+    public HelpCommand(String keyword) {
+        this.keyword = keyword.trim().toLowerCase();
+    }
 
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        requireNonNull(keyword);
+        switch (keyword) {
+        case "add":
+            return new CommandResult(AddCommand.MESSAGE_USAGE, false, false);
+        case "delete":
+            return new CommandResult(DeleteCommand.MESSAGE_USAGE, false, false);
+        case "edit":
+            return new CommandResult(EditCommand.MESSAGE_USAGE, false, false);
+        case "find":
+            return new CommandResult(FindCommand.MESSAGE_USAGE, false, false);
+        case "list":
+            return new CommandResult(ListCommand.MESSAGE_USAGE, false, false);
+        case "help":
+            return new CommandResult(MESSAGE_USAGE, false, false);
+        case "clear":
+            return new CommandResult(ClearCommand.MESSAGE_USAGE, false, false);
+        case "exit":
+            return new CommandResult(ExitCommand.MESSAGE_USAGE, false, false);
+        case "":
+            return new CommandResult(FULL_HELP_MESSAGE, true, false);
+        default:
+            throw new CommandException(String.format(MESSAGE_INVALID_HELP_KEYWORD, keyword));
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof HelpCommand)) {
+            return false;
+        }
+
+        HelpCommand otherHelpCommand = (HelpCommand) other;
+        return this.keyword.equals(otherHelpCommand.keyword);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("help %s", keyword);
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/HelpCommandParser.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new HelpCommand object
+ */
+public class HelpCommandParser implements Parser<HelpCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the HelpCommand
+     * and returns a HelpCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    @Override
+    public HelpCommand parse(String keyword) throws ParseException {
+        String trimmedLowercaseKeyword = keyword.trim().toLowerCase();
+        if (!trimmedLowercaseKeyword.isEmpty()
+            && !trimmedLowercaseKeyword.equals(AddCommand.COMMAND_WORD)
+            && !trimmedLowercaseKeyword.equals(DeleteCommand.COMMAND_WORD)
+            && !trimmedLowercaseKeyword.equals(EditCommand.COMMAND_WORD)
+            && !trimmedLowercaseKeyword.equals(FindCommand.COMMAND_WORD)
+            && !trimmedLowercaseKeyword.equals(ListCommand.COMMAND_WORD)
+            && !trimmedLowercaseKeyword.equals(ClearCommand.COMMAND_WORD)
+            && !trimmedLowercaseKeyword.equals(ExitCommand.COMMAND_WORD)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                trimmedLowercaseKeyword));
+        }
+        return new HelpCommand(trimmedLowercaseKeyword);
+    }
+}
