270a7b8a00655a2e978429b04feae232ad52f2bc
weibinlim <limweibin0816@gmail.com>
2024-10-23 16:34:00 +0800

    Refactor Rsvp Class
    - Replaced the array of RSVP statuses with an enum for better
      type safety and clarity.
    - Improved code readability and maintainability by using enums
      for predefined status values.

--- src/main/java/seedu/address/model/person/Rsvp.java
+++ src/main/java/seedu/address/model/person/Rsvp.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents an RSVP status for a person.
+ * Represents the RSVP status for a person.
  * The RSVP status can be one of the following: "PENDING", "ACCEPTED", "DECLINED".
  * This class enforces the following constraints:
  * RSVP Status should be either "PENDING", "ACCEPTED", or "DECLINED".</li>
@@ -15,13 +15,15 @@ public class Rsvp {
     public static final String MESSAGE_CONSTRAINTS =
             "RSVP Status should be 'PENDING', 'ACCEPTED' or 'DECLINED'";
 
+    public enum Status {
+        PENDING, ACCEPTED, DECLINED
+    }
+
     /**
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
-
-    private static final String[] RSVP_STATUS = {"PENDING", "ACCEPTED", "DECLINED"};
 
     public final String value;
 
@@ -50,8 +52,8 @@ public class Rsvp {
             return false;
         }
 
-        for (String status : RSVP_STATUS) {
-            if (status.equalsIgnoreCase(str)) {
+        for (Status status : Status.values()) {
+            if (status.name().equalsIgnoreCase(str)) {
                 return true;
             }
         }
@@ -82,11 +84,10 @@ public class Rsvp {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof Rsvp)) {
+        if (!(other instanceof Rsvp otherName)) {
             return false;
         }
 
-        Rsvp otherName = (Rsvp) other;
         return value.equals(otherName.value);
     }
 
