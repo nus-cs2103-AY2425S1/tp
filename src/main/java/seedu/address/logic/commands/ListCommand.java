22c2e9ef7f76d971e73a06632f4b673bbb62643b
jovensoh <34702990+JovenSoh@users.noreply.github.com>
2024-10-23 17:24:48 +0800

    Update User Guide

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -7,14 +7,14 @@ import seedu.address.model.Model;
 /**
  * Resets volunteer and events display.
  */
-public class ResetDisplayListsCommand extends Command {
+public class ListCommand extends Command {
 
-    public static final String COMMAND_WORD = "reset";
+    public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Resets volunteer and event display lists.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all volunteers and events.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Successfully reset volunteer and event display lists.";
+    public static final String MESSAGE_SUCCESS = "Successfully listed volunteers and events.";
 
     @Override
     public CommandResult execute(Model model) {
