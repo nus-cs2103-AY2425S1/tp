ce471eac73973144c87d3206c56d569488729628
Chua Tse Hui <htcylsg@gmail.com>
2024-10-23 20:26:52 +0800

    Remove magic numbers

--- src/main/java/seedu/address/model/ComparatorName.java
+++ src/main/java/seedu/address/model/ComparatorName.java
@@ -4,10 +4,13 @@ import seedu.address.model.person.Person;
 
 import java.util.Comparator;
 
+// javadoc comments?
 public class ComparatorName implements Comparator<Person> {
+    final int EQUAL_STATUS = 0;
+
     public int compare(Person contact1, Person contact2) {
         int compareNumberByName = contact1.getName().compareTo(contact2.getName());
-        if (compareNumberByName == 0) {
+        if (compareNumberByName == EQUAL_STATUS) {
             return contact1.getNickname().compareTo(contact2.getNickname());
         }
         return compareNumberByName;
