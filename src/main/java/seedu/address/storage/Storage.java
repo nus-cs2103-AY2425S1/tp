66de9ba979e908b47f2d6fa963682d91c5c52d6c
ywllowsensor <isyazani@gmail.com>
2024-10-23 22:16:34 +0800

    Add methods that work to store weddings into storage

--- src/main/java/seedu/address/storage/Storage.java
+++ src/main/java/seedu/address/storage/Storage.java
@@ -7,12 +7,13 @@ import java.util.Optional;
 import seedu.address.commons.exceptions.DataLoadingException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
+import seedu.address.model.ReadOnlyWeddingBook;
 import seedu.address.model.UserPrefs;
 
 /**
  * API of the Storage component
  */
-public interface Storage extends AddressBookStorage, UserPrefsStorage {
+public interface Storage extends AddressBookStorage, UserPrefsStorage, WeddingBookStorage {
 
     @Override
     Optional<UserPrefs> readUserPrefs() throws DataLoadingException;
@@ -28,5 +29,14 @@ public interface Storage extends AddressBookStorage, UserPrefsStorage {
 
     @Override
     void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
+
+    @Override
+    Path getWeddingBookFilePath();
+
+    @Override
+    Optional<ReadOnlyWeddingBook> readWeddingBook() throws DataLoadingException;
+
+    @Override
+    void saveWeddingBook(ReadOnlyWeddingBook weddingBook) throws IOException;
 
 }
