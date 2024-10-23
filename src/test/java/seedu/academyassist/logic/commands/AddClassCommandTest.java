058ade8a56dcc23b455e12a168746302cafdf787
annabellekk <yuyien.khoo@gmail.com>
2024-10-23 19:12:03 +0800

    Fix styling issue

--- src/test/java/seedu/academyassist/logic/commands/AddClassCommandTest.java
+++ src/test/java/seedu/academyassist/logic/commands/AddClassCommandTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 
 import javafx.collections.ObservableList;
 import seedu.academyassist.commons.core.GuiSettings;
-import seedu.academyassist.logic.Messages;
 import seedu.academyassist.logic.commands.exceptions.CommandException;
 import seedu.academyassist.model.Model;
 import seedu.academyassist.model.ReadOnlyAcademyAssist;
