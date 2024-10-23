0487fdaea1e4a1611af646b843f74d6234eb2b30
JumpyJay <iuswtric1909@gmail.com>
2024-10-24 00:45:37 +0800

    Postpone test cases

--- src/test/java/seedu/address/logic/commands/ClientUtilTest.java
+++ src/test/java/seedu/address/logic/commands/ClientUtilTest.java
@@ -1,35 +1,35 @@
-package seedu.address.logic.commands;
-
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import java.nio.file.Path;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-
-import seedu.address.logic.Logic;
-import seedu.address.logic.LogicManager;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.storage.JsonAddressBookStorage;
-import seedu.address.storage.JsonUserPrefsStorage;
-import seedu.address.storage.Storage;
-import seedu.address.storage.StorageManager;
-
-public class ClientUtilTest {
-
-    @TempDir
-    public Path temporaryFolder;
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    JsonAddressBookStorage addressBookStorage =
-            new JsonAddressBookStorage(temporaryFolder.resolve("addressBook.json"));
-    JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(temporaryFolder.resolve("userPrefs.json"));
-    StorageManager storage = new StorageManager(addressBookStorage, userPrefsStorage);
-    private Logic logic = new LogicManager(model, storage);
-    @Test
-    public void findViewPerson() {
-
-        //
-    }
-}
+//package seedu.address.logic.commands;
+//
+//import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+//
+//import java.nio.file.Path;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.io.TempDir;
+//
+//import seedu.address.logic.Logic;
+//import seedu.address.logic.LogicManager;
+//import seedu.address.model.Model;
+//import seedu.address.model.ModelManager;
+//import seedu.address.model.UserPrefs;
+//import seedu.address.storage.JsonAddressBookStorage;
+//import seedu.address.storage.JsonUserPrefsStorage;
+//import seedu.address.storage.Storage;
+//import seedu.address.storage.StorageManager;
+//
+//public class ClientUtilTest {
+//
+//    @TempDir
+//    public Path temporaryFolder;
+//    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+//    JsonAddressBookStorage addressBookStorage =
+//            new JsonAddressBookStorage(temporaryFolder.resolve("addressBook.json"));
+//    JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(temporaryFolder.resolve("userPrefs.json"));
+//    StorageManager storage = new StorageManager(addressBookStorage, userPrefsStorage);
+//    private Logic logic = new LogicManager(model, storage);
+//    @Test
+//    public void findViewPerson() {
+//
+//        //
+//    }
+//}
