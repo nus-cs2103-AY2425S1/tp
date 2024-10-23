d9d447a0512ff96ac571c63f3d21e9d854abea01
Solomon0519 <ng.eswen1@gmail.com>
2024-10-23 18:48:57 +0800

    Close issue #139
    Amended Find Command Parser for meetups so that it takes in a n/ tag, also rewrote the find command parser tests to account for this new addition of tag.

--- src/main/java/seedu/address/logic/commands/meetup/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/meetup/FindCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands.meetup;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
@@ -20,7 +21,8 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all meet-up whose meet-up names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " john ron tom ";
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "KEYWORD ";
 
     private final MeetUpContainsKeywordsPredicate predicate;
 
