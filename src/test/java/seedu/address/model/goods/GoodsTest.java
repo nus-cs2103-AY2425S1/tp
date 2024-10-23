3e06bbf83b3eca0652a87638ba0b78b6066bffe1
thenosewizard <abicharan.b@gmail.com>
2024-10-23 15:21:44 +0800

    Fix import error

--- src/test/java/seedu/address/model/goods/GoodsTest.java
+++ src/test/java/seedu/address/model/goods/GoodsTest.java
@@ -2,6 +2,7 @@ package seedu.address.model.goods;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
