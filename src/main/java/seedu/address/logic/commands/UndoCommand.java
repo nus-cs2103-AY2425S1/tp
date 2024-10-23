eef4069e6d25f6441b5ae45cad4bf435c0dfb20c
Chen Lei Yu <128605764+ChenLeiyu@users.noreply.github.com>
2024-10-23 19:22:44 +0800

    Add undo functionality for 'clear' command

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 /**
@@ -12,11 +14,15 @@ public class UndoCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undoes previous user command.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String MESSAGE_SUCCESS = "Undid previous command: ";
+    public static final String MESSAGE_SUCCESS = "Undid previous command.";
+    public static final String MESSAGE_INVALID_PREVIOUS_COMMAND = "No Command found to undo";
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         Command previousCommand = model.getPreviousCommand();
+        if (previousCommand == null) {
+            throw new CommandException(MESSAGE_INVALID_PREVIOUS_COMMAND);
+        }
         previousCommand.undo(model);
         return new CommandResult(MESSAGE_SUCCESS);
     }
