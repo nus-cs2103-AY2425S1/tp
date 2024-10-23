c9b0860aff1348f7d1296c14909552ad00df41b8
jerrelllee <leejerrell337@gmail.com>
2024-10-24 01:25:42 +0800

    Update AddressBookParser and its test

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -25,6 +25,7 @@ import seedu.address.logic.commands.GetAttendanceCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.MarkAttendanceCommand;
+import seedu.address.logic.commands.MarkPresentAllCommand;
 import seedu.address.logic.commands.ViewStudentCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -113,6 +114,9 @@ public class AddressBookParser {
 
         case GetAttendanceCommand.COMMAND_WORD:
             return new GetAttendanceCommandParser().parse(arguments);
+
+        case MarkPresentAllCommand.COMMAND_WORD:
+            return new MarkPresentAllCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
