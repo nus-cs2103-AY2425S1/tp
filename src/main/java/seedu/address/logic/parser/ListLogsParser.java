6ed39072b07bee691c8a172a66900f6ab2c4d78d
slidings <ngzerui@gmail.com>
2024-10-24 03:19:10 +0800

    Refactor ListLogsParser to utilise CliSyntax

--- src/main/java/seedu/address/logic/parser/ListLogsParser.java
+++ src/main/java/seedu/address/logic/parser/ListLogsParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_IDENTITY_NUMBER;
 
 import seedu.address.logic.commands.ListLogsCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -16,27 +17,26 @@ public class ListLogsParser implements Parser<ListLogsCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ListLogsCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_IDENTITY_NUMBER);
 
-        // Ensure the input is exactly 11 characters long
-        // eg: i/SxxxxxxxA
-        if (trimmedArgs.length() != 11) {
+        // Ensure the prefix and value are present
+        if (argMultimap.getValue(PREFIX_IDENTITY_NUMBER).isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListLogsCommand.MESSAGE_USAGE));
         }
 
-        // Extract the 9-character field after "/i"
-        String arg = trimmedArgs.substring(2);
-        IdentityNumber id;
-
         try {
-            // Try to initialise id
-            id = new IdentityNumber(arg);
+            // Parse the identity number
+            IdentityNumber id = ParserUtil.parseIdentityNumber(
+                    argMultimap.getValue(PREFIX_IDENTITY_NUMBER).get());
+
+            // Return the ListLogsCommand with the parsed identity number
+            return new ListLogsCommand(id);
+
         } catch (IllegalArgumentException e) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListLogsCommand.MESSAGE_USAGE));
         }
-
-        return new ListLogsCommand(id);
     }
 }
