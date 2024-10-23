089f80a11b1b206d306dce15bf5e6fd643baed78
MAOXIONGKAI <conan7153@gmail.com>
2024-10-23 22:51:13 +0800

    Create test cases for the new help command code

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -1,7 +1,9 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_HELP_KEYWORD;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+import static seedu.address.logic.commands.HelpCommand.FULL_HELP_MESSAGE;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +16,61 @@ public class HelpCommandTest {
 
     @Test
     public void execute_help_success() {
-        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
-        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+        CommandResult expectedCommandResult = new CommandResult(FULL_HELP_MESSAGE, true, false);
+        assertCommandSuccess(new HelpCommand(""), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpAdd_success() {
+        CommandResult expectedCommandResult = new CommandResult(AddCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("add"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpDelete_success() {
+        CommandResult expectedCommandResult = new CommandResult(DeleteCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("delete"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpEdit_success() {
+        CommandResult expectedCommandResult = new CommandResult(EditCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("edit"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpFind_success() {
+        CommandResult expectedCommandResult = new CommandResult(FindCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("find"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpList_success() {
+        CommandResult expectedCommandResult = new CommandResult(ListCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("list"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpHelp_success() {
+        CommandResult expectedCommandResult = new CommandResult(HelpCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("help"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpClear_success() {
+        CommandResult expectedCommandResult = new CommandResult(ClearCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("clear"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpExit_success() {
+        CommandResult expectedCommandResult = new CommandResult(ExitCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand("exit"), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_helpInvalidKeyword_throwsCommandException() {
+        assertCommandFailure(new HelpCommand("nonsense"), model,
+            String.format(MESSAGE_INVALID_HELP_KEYWORD, "nonsense"));
     }
 }
