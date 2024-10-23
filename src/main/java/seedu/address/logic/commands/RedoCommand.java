ad07be76d4b2f1139b404d42b099400553092a2f
Christopher Tan <phertan02@gmail.com>
2024-10-23 16:20:09 +0800

    Add new Redo command with basic tests

---
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+
+/**
+ * Recovers previously undone versions of CampusConnect.
+ */
+public class RedoCommand extends Command {
+    public static final String COMMAND_WORD = "redo";
+    public static final String MESSAGE_SUCCESS = "Campus Connect has gone back to the future!";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalCampusConnect;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+
+public class RedoCommandTest {
+    private Model model = new ModelManager(getTypicalCampusConnect(), new UserPrefs());
+
+    @Test
+    public void execute_emptyFutureStack_success() {
+        ModelManager expectedModel = new ModelManager(model.getCampusConnect(), new UserPrefs());
+
+        assertCommandSuccess(new RedoCommand(), model, RedoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
