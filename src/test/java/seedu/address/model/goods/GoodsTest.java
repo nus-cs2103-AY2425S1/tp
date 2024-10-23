cef361c3a386e0840d2f462660e0eb027ae82851
thenosewizard <abicharan.b@gmail.com>
2024-10-23 15:19:16 +0800

    Style

--- src/test/java/seedu/address/model/goods/GoodsTest.java
+++ src/test/java/seedu/address/model/goods/GoodsTest.java
@@ -1,8 +1,10 @@
 package seedu.address.model.goods;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class GoodsTest {
 
