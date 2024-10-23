a69fec521d51a628001b7e039973ec3955597d74
uniqly <simontong09@gmail.com>
2024-10-23 17:46:56 +0800

    Remove redudant AddCommandParser#arePrefixesPresent()

--- src/main/java/seedu/internbuddy/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/internbuddy/logic/parser/AddCommandParser.java
@@ -56,13 +56,4 @@ public class AddCommandParser implements Parser<AddCommand> {
 
         return new AddCommand(company);
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
 }
