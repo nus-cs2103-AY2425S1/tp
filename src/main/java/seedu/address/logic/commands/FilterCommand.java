3e1104061654208a2d8dd9be0615ced70f363721
SherChew27 <shermanczj@gmail.com>
2024-10-23 18:31:26 +0800

    Add multi-filter for tags

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -1,11 +1,45 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * Filters the address book by a given prefix and displays the filtered list to the user
  * Users can only filter by one field at a time
  */
-public abstract class FilterCommand extends Command {
+public class FilterCommand extends Command {
     public static final String COMMAND_WORD = "filter";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters the address book with the given criteria\n"
             + "Parameters: [prefix]/[filter critera]\n" + "Example: " + COMMAND_WORD + " t/friends";
+
+    public Set<Predicate<Person>> predicateSet;
+    public Predicate<Person> predicate = new Predicate<Person>() {
+        @Override
+        public boolean test(Person person) {
+            return true;
+        }
+    };
+
+    public FilterCommand(Set<Predicate<Person>> predicateSet) {
+        this.predicateSet = predicateSet;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        for(Predicate<Person> predicateToAdd: predicateSet) {
+            predicate = predicate.and(predicateToAdd);
+        }
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
 }
