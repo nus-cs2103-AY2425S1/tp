6975cc05d60c13a2f24f468655550249825c3f09
Shantaa <shanetay4425@gmail.com>
2024-10-23 15:11:33 +0800

    remove more whitespaces

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -133,33 +133,35 @@ public class HelpWindow extends UiPart<Stage> {
 
         contentMap.put("Introduction", """
                 Introduction\n
-                Financial Assurance Revolutionary Telemarketer (FART) is a desktop app for managing contacts just for you.
-                Financial Advisors, optimized for use via a Command Line Interface (CLI) while still having the benefits of a Graphical User Interface (GUI). If you can type fast, FART can get your contact management tasks done faster than traditional GUI apps.\n
+                Financial Assurance Revolutionary Telemarketer (FART) is a desktop app for managing
+                contacts just for you. Financial Advisors, optimized for use via a Command Line Interface (CLI)
+                while still having the benefits of a Graphical User Interface (GUI). If you can type fast, FART
+                can get your contact management tasks done faster than traditional GUI apps.\n
                 Navigate to the appropriate issue via the panel on the left.
                 """);
 
         contentMap.put("Quick Start", """
-            Quick start\n
-            1. Ensure you have Java `17` or above installed in your Computer.
-            (Visit the Oracle website for Java 17 installation.)\n
-            2. Download the latest `.jar` file from the GitHub releases page.\n
-            3. Copy the `.jar` file to your desired folder and open a terminal.\n
-            4. Run the application using `java -jar fart_in_a.jar`.\n
-            5. Type commands in the command box and press Enter.
-            """);
+                Quick start\n
+                1. Ensure you have Java `17` or above installed in your Computer.
+                (Visit the Oracle website for Java 17 installation.)\n
+                2. Download the latest `.jar` file from the GitHub releases page.\n
+                3. Copy the `.jar` file to your desired folder and open a terminal.\n
+                4. Run the application using `java -jar fart_in_a.jar`.\n
+                5. Type commands in the command box and press Enter.
+                """);
 
         contentMap.put("Command Format Guidelines", """
                 Command Format Guidelines\n
-                1. Words in UPPER_CASE are parameters to be supplied by the user.
-                 For example, in `add n/NAME`, `NAME` is a parameter which can be used as `add n/John Doe`.\n
-                2. Items in square brackets are optional.
-                For example, `n/NAME [t/TAG]` can be used as `n/John Doe t/friend` or as `n/John Doe`.\n
-                3. Items with …​ after them can be used multiple times, including zero times.
-                For example, `[t/TAG]…​` can be used zero times (i.e. `add n/John Doe`), or multiple times, such as `t/friend`, `t/friend t/family`, etc.\n
-                4. Parameters can be in any order.
-                For example, if the command specifies `n/NAME p/PHONE_NUMBER`, it can also be written as `p/PHONE_NUMBER n/NAME`.\n
-                5. Extraneous parameters for commands that do not take in parameters (such as `help`, `list`, `exit`, and `clear`) will be ignored.
-                For example, if the command specifies `help 123`, it will be interpreted as `help`.     
+                1. Words in UPPER_CASE are parameters to be supplied by the user. For example, in `add n/NAME`, 
+                `NAME` is a parameter which can be used as `add n/John Doe`.\n
+                2. Items in square brackets are optional. For example, `n/NAME [t/TAG]` can be used as `n/John Doe t/friend` 
+                or as `n/John Doe`.\n
+                3. Items with …​ after them can be used multiple times, including zero times. For example, `[t/TAG]…​` can be used 
+                zero times (i.e. `add n/John Doe`), or multiple times, such as `t/friend`, `t/friend t/family`, etc.\n
+                4. Parameters can be in any order. For example, if the command specifies `n/NAME p/PHONE_NUMBER`, 
+                it can also be written as `p/PHONE_NUMBER n/NAME`.\n
+                5. Extraneous parameters for commands that do not take in parameters (such as `help`, `list`, `exit`, and `clear`) 
+                will be ignored. For example, if the command specifies `help 123`, it will be interpreted as `help`.
                 """);
 
         contentMap.put("Adding a Contact", """
@@ -187,37 +189,36 @@ public class HelpWindow extends UiPart<Stage> {
                 When editing tags, the existing tags of the person will be removed (i.e., adding tags is not cumulative).
                 You can remove all the person’s tags by typing `t/` without specifying any tags after it.\n
                 Examples:
-                `edit 1 p/91234567 e/johndoe@example.com`: Edits the phone number and email address of the 1st person to be `91234567` and `johndoe@example.com`, respectively.
+                `edit 1 p/91234567 e/johndoe@example.com`: Edits the phone number and email address of the 1st person to be 
+                `91234567` and `johndoe@example.com`, respectively.
                 `edit 2 n/Betsy Crower t/`: Edits the name of the 2nd person to `Betsy Crower` and clears all existing tags.
                 """);
-
 
         contentMap.put("Finding Contacts", """
                 Finding Contacts\n
                 Finds persons whose attributes contain any of the given keywords.\n
                 Format: `find KEYWORD [MORE_KEYWORDS]`\n
-                The search is case-insensitive. For example, `hans` will match `Hans`.
-                Keywords are matched against Name, Phone, Address, Email, Birthday, and Tag.
-                The order of the keywords does not matter. For example, `Hans Bo` will match `Bo Hans`.
-                Persons matching at least one keyword will be returned (i.e., OR search). For example, `Hans Bo` will return `Hans Gruber`, `Bo Yang`.
-                Partial matches of a keyword will also be returned. For example, `9123` will return the person with the phone number `91234567`.\n
+                The search is case-insensitive. For example, `hans` will match `Hans`. Keywords are matched against Name, 
+                Phone, Address, Email, Birthday, and Tag. The order of the keywords does not matter. For example, `Hans Bo` 
+                will match `Bo Hans`. Persons matching at least one keyword will be returned (i.e., OR search). 
+                For example, `Hans Bo` will return `Hans Gruber`, `Bo Yang`. Partial matches of a keyword will also be 
+                returned. For example, `9123` will return the person with the phone number `91234567`.\n
                 Examples:
                 `find John` returns `john` and `John Doe`.
                 `find alex david` returns `Alex Yeoh`, `David Li`.
                 """);
 
-
         contentMap.put("Deleting a Contact", """
                 Deleting a Contact\n
                 Deletes the specified person from the FART book.\n
                 Format: `delete INDEX`\n
-                Deletes the person at the specified INDEX.
-                The **INDEX** refers to the index number shown in the displayed person list.
-                The index must be a positive integer (1, 2, 3, …​).\n
+                Deletes the person at the specified INDEX. The **INDEX** refers to the index number shown in the 
+                displayed person list. The index must be a positive integer (1, 2, 3, …​).\n
                 Examples:
-                `list` followed by `delete 2` deletes the 2nd person in the FART book.
-                `find Betsy` followed by `delete 1` deletes the 1st person in the results of the find command.\n
-                Note: As the delete command cannot be undone, the FART Book will first prompt you to confirm that the contact you want to delete is correct. If you select Cancel, the deletion will be aborted.
+                `list` followed by `delete 2` deletes the 2nd person in the FART book. `find Betsy` followed by 
+                `delete 1` deletes the 1st person in the results of the find command.\n
+                Note: As the delete command cannot be undone, the FART Book will first prompt you to confirm that the 
+                contact you want to delete is correct. If you select Cancel, the deletion will be aborted.
                 """);
 
         contentMap.put("Clearing All Entries", """
@@ -231,9 +232,8 @@ public class HelpWindow extends UiPart<Stage> {
                 Marking a Person as Paid/Unpaid\n
                 Marks the specified person from the FART book as paid/unpaid.\n
                 Format: `paid INDEX` / `unpaid INDEX`\n
-                Marks the person at the specified INDEX as paid/unpaid.
-                The INDEX refers to the index number shown in the displayed person list.
-                The index must be a positive integer (1, 2, 3, …​).\n
+                Marks the person at the specified INDEX as paid/unpaid. The INDEX refers to the index number 
+                shown in the displayed person list. The index must be a positive integer (1, 2, 3, …​).\n
                 Examples:
                 `list` followed by `paid 2` marks the 2nd person in the FART book as paid.
                 `find Betsy` followed by `paid 1` marks the 1st person in the results of the find command as paid.
@@ -241,11 +241,10 @@ public class HelpWindow extends UiPart<Stage> {
                 `find Betsy` followed by `unpaid 1` marks the 1st person in the results of the find command as unpaid.
                 """);
 
-
         contentMap.put("Saving and Editing Data", """
                 Saving and Editing Data\n
-                FART data is automatically saved after any command.
-                Data is stored in a JSON file at `[JAR file location]/data/addressbook.json`.
+                FART data is automatically saved after any command. Data is stored in a JSON file at 
+                `[JAR file location]/data/addressbook.json`.
                 """);
 
         contentMap.put("Exiting the Program", """
@@ -257,8 +256,8 @@ public class HelpWindow extends UiPart<Stage> {
                 Command Summary\n
                 | Action | Format | Examples |
                 |--------|--------|----------|
-                | Add    | `add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS b/BIRTHDAY [t/TAG]…​` | `add n/James Ho 
-                p/22224444 e/jamesho@example.com a/123, Clementi Rd, 1234665 b/11 09 2001 t/friend t/colleague` |
+                | Add    | `add n/NAME p/PHONE_NUMBER e/EMAIL a/ADDRESS b/BIRTHDAY [t/TAG]…​` | `add n/James Ho p/22224444 
+                e/jamesho@example.com a/123, Clementi Rd, 1234665 b/11 09 2001 t/friend t/colleague` |
                 | Clear  | `clear`     | - |
                 | Delete | `delete INDEX` | `delete 3` |
                 | Edit   | `edit INDEX [n/NAME] [p/PHONE_NUMBER] [e/EMAIL] [a/ADDRESS] [t/TAG]…​` | `edit 2 n/James Lee 
@@ -269,6 +268,7 @@ public class HelpWindow extends UiPart<Stage> {
                 | Paid   | `paid INDEX` | `paid 3` |
                 | Unpaid | `unpaid INDEX` | `unpaid 3` |
                 """);
+
 
         initializeCommandSummaryData();
 
@@ -291,17 +291,23 @@ public class HelpWindow extends UiPart<Stage> {
      * Initializes the command summary data for the TableView.
      */
     private void initializeCommandSummaryData() {
-        actionColumn.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue()[0]));
-        formatColumn.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue()[1]));
-        exampleColumn.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue()[2]));
+        actionColumn.setCellValueFactory
+                (cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue()[0]));
+        formatColumn.setCellValueFactory
+                (cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue()[1]));
+        exampleColumn.setCellValueFactory
+                (cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue()[2]));
 
         commandSummaryData.clear();
 
         commandSummaryData.addAll(
-                new String[]{"Add", "add n/NAME p/PHONE_NUMBER\n e/EMAIL a/ADDRESS b/BIRTHDAY [t/TAG]…​", "add n/James Ho p/22224444 \ne/jamesho@example.com a/123, \nClementi Rd, 1234665 b/11 09 2001 \nt/friend t/colleague"},
+                new String[]{"Add", "add n/NAME p/PHONE_NUMBER\n " +
+                        "e/EMAIL a/ADDRESS b/BIRTHDAY [t/TAG]…​", "add n/James Ho p/22224444 \n" +
+                        "e/jamesho@example.com a/123, \nClementi Rd, 1234665 b/11 09 2001 \nt/friend t/colleague"},
                 new String[]{"Clear", "clear", ""},
                 new String[]{"Delete", "delete INDEX", "delete 3"},
-                new String[]{"Edit", "edit INDEX [n/NAME] [p/PHONE_NUMBER]\n [e/EMAIL] [a/ADDRESS] [t/TAG]…​", "edit 2 n/James Lee\n e/jameslee@example.com"},
+                new String[]{"Edit", "edit INDEX [n/NAME] [p/PHONE_NUMBER]\n " +
+                        "[e/EMAIL] [a/ADDRESS] [t/TAG]…​", "edit 2 n/James Lee\n e/jameslee@example.com"},
                 new String[]{"Find", "find KEYWORD [MORE_KEYWORDS]", "find James Jake"},
                 new String[]{"List", "list", ""},
                 new String[]{"Help", "help", ""},
@@ -317,7 +323,7 @@ public class HelpWindow extends UiPart<Stage> {
     }
 
     /**
-     * Updates the TextFlow with bold, large titles, regular body text, hyperlinks, and code formatting for text in backticks.
+     * Updates TextFlow with bold, large titles, hyperlinks, and code formatting for text in backticks.
      */
     private void updateTextFlow(String content) {
         helpContentFlow.getChildren().clear();
