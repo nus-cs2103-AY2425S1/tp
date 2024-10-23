67b7c9f42b326a40671505208c1f139eceb9df76
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-10-23 23:05:05 +0800

    Update src/main/java/seedu/address/logic/commands/ExportCommand.java
    Co-authored-by: Sanchita Dhareshwar <81695359+sannie-beep@users.noreply.github.com>

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -27,7 +27,7 @@ public class ExportCommand extends Command {
     public static final String MESSAGE_EMPTY = "No participants to export";
     public static final String FILE_WRITE_ERROR = "Unable export participants' emails due to unexpected I/O error";
     public static final String FILE_WRITE_PERMISSION_ERROR =
-            "Unable export participants' emails due to denied export file access";
+            "Unable to export participants' emails due to denied export file access";
 
     private final File exportFile;
 
