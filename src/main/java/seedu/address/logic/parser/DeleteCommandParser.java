86529d3cc835f012ea052bf17f12b593e4d1f99e
Ng Kay Hian <e1113766@u.nus.edu>
2024-10-24 00:04:28 +0800

    Delete command: change error message
    Application displays an internal error message instead of a
    user-oriented message.
    
    Let's restore it to the user-oriented message.

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,8 +16,9 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the
+     * DeleteCommand and returns a DeleteCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
@@ -33,10 +36,8 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
                     .collect(Collectors.toList());
             return new DeleteCommand(indices);
         } catch (RuntimeException e) {
-            if (e.getCause() instanceof ParseException) {
-                throw (ParseException) e.getCause();
-            }
-            throw e;
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), e);
         }
     }
 }
