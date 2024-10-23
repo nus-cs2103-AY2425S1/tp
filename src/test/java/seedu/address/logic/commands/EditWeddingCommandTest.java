a94e290b5797c1a90a44c8d634b931f58bb83f3d
dovlin <dovlincs01@gmail.com>
2024-10-23 20:19:08 +0800

    Update test

--- src/test/java/seedu/address/logic/commands/EditWeddingCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditWeddingCommandTest.java
@@ -1,4 +1,12 @@
 package seedu.address.logic.commands;
 
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 public class EditWeddingCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
 }
