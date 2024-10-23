19ac446e61bf6cc294464f5383d9b2ddd343cc8d
slidings <ngzerui@gmail.com>
2024-10-24 03:21:00 +0800

    Remove changes in other files for this pr

--- src/main/java/seedu/address/Main.java
+++ src/main/java/seedu/address/Main.java
@@ -34,6 +34,7 @@ public class Main {
         // The warning however, can be safely ignored. Thus, the following log informs
         // the user (if looking at the log output) that the said warning appearing in the log
         // can be ignored.
+
         logger.warning("The warning about Unsupported JavaFX configuration below (if any) can be ignored.");
         Application.launch(MainApp.class, args);
     }
