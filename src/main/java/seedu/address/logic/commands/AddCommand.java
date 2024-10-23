a74e7247424d93539fca951a198f75dd4a704c8c
George <156641986+george-yeo@users.noreply.github.com>
2024-10-23 11:50:13 +0800

    Allow duplicate Persons

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -36,7 +36,6 @@ public class AddCommand extends Command {
             + PREFIX_TAG + "owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "'%s' already exists in the address book";
 
     private final Person toAdd;
 
@@ -49,24 +48,11 @@ public class AddCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
+    public CommandResult execute(Model model) {
         requireNonNull(model);
-
-        if (model.hasPerson(toAdd)) {
-            throw new CommandException(generateDuplicatePersonMessage());
-        }
 
         model.addPerson(toAdd);
         return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
-    }
-
-    /**
-     * Generates a message for duplicate person with the specified {@code Person}
-     *
-     * @return message with person's name
-     */
-    public String generateDuplicatePersonMessage() {
-        return String.format(MESSAGE_DUPLICATE_PERSON, toAdd.getName());
     }
 
     @Override
