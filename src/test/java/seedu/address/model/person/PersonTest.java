b6cb1b1a3c4faca5c74309549766c62d447f4f52
O-Darren <137365365+O-Darren@users.noreply.github.com>
2024-10-23 20:54:46 +0800

    PersonTest.java: Edit toStringMethod() test

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -102,7 +102,8 @@ public class PersonTest {
         String expected = Person.class.getCanonicalName() + "{name=" + ALICE.getName() + ", phone=" + ALICE.getPhone()
                 + ", email=" + ALICE.getEmail() + ", address=" + ALICE.getAddress() + ", organisation="
                 + ALICE.getOrganisation() + ", last seen=" + ALICE.getLastSeen() + ", tags=" + ALICE.getTags()
-                + ", priority=" + ALICE.getPriority() + ", remark=" + ALICE.getRemark() + "}";
+                + ", priority=" + ALICE.getPriority() + ", remark=" + ALICE.getRemark()
+                + ", reminders=" + ALICE.getReminderList() + "}";
         assertEquals(expected, ALICE.toString());
     }
 }
