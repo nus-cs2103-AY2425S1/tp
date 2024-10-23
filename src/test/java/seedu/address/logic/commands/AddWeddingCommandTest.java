6f2715d4803fd7cb2b1acf61659ea3d091088dc9
Aditya Ghosh <adityaghosh2002@gmail.com>
2024-10-24 00:04:43 +0800

    Update test cases for AddWeddingCommand

--- src/test/java/seedu/address/logic/commands/AddWeddingCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddWeddingCommandTest.java
@@ -7,30 +7,36 @@ import static seedu.address.testutil.TypicalWeddings.WEDDING_ONE;
 import static seedu.address.testutil.TypicalWeddings.WEDDING_TWO;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.person.Person;
 import seedu.address.model.wedding.Wedding;
+import seedu.address.testutil.TypicalPersons;
 
 public class AddWeddingCommandTest {
 
     private Model model;
+    private Set<Index> contactIndexes;
 
     @BeforeEach
     public void setUp() {
         model = new ModelManager();
+        contactIndexes = new HashSet<>();
     }
 
     @Test
-    public void execute_newWedding_success() throws Exception {
+    public void execute_newWeddingWithoutContacts_success() throws Exception {
         Wedding validWedding = WEDDING_ONE;
         AddWeddingCommand addWeddingCommand = new AddWeddingCommand(new Wedding(WEDDING_ONE.getWeddingName(),
-                WEDDING_ONE.getWeddingDate()), new HashSet<>());
+                WEDDING_ONE.getWeddingDate()), contactIndexes);
 
         CommandResult commandResult = addWeddingCommand.execute(model);
 
@@ -39,14 +45,45 @@ public class AddWeddingCommandTest {
     }
 
     @Test
+    public void execute_newWeddingWithContacts_success() throws Exception {
+        Person person1 = TypicalPersons.ALICE;
+        Person person2 = TypicalPersons.BOB;
+        model.addPerson(person1);
+        model.addPerson(person2);
+
+        contactIndexes.add(Index.fromOneBased(1));
+        contactIndexes.add(Index.fromOneBased(2));
+
+        Wedding validWedding = WEDDING_TWO;
+        AddWeddingCommand addWeddingCommand = new AddWeddingCommand(new Wedding(WEDDING_TWO.getWeddingName(),
+                WEDDING_TWO.getWeddingDate()), contactIndexes);
+
+        CommandResult commandResult = addWeddingCommand.execute(model);
+
+        assertEquals(String.format(AddWeddingCommand.MESSAGE_SUCCESS, Messages.formatWedding(validWedding)),
+                commandResult.getFeedbackToUser());
+    }
+
+
+    @Test
     public void execute_duplicateWedding_throwsCommandException() {
         Wedding duplicateWedding = WEDDING_ONE;
         model.addWedding(duplicateWedding);
 
         AddWeddingCommand addWeddingCommand = new AddWeddingCommand(new Wedding(WEDDING_ONE.getWeddingName(),
-                WEDDING_ONE.getWeddingDate()), new HashSet<>());
+                WEDDING_ONE.getWeddingDate()), contactIndexes);
 
         assertThrows(CommandException.class, AddWeddingCommand.MESSAGE_DUPLICATE_WEDDING, () ->
+                addWeddingCommand.execute(model));
+    }
+
+    @Test
+    public void execute_invalidContactIndex_throwsCommandException() {
+        contactIndexes.add(Index.fromOneBased(999));
+        AddWeddingCommand addWeddingCommand = new AddWeddingCommand(new Wedding(WEDDING_ONE.getWeddingName(),
+                WEDDING_ONE.getWeddingDate()), contactIndexes);
+
+        assertThrows(CommandException.class, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, () ->
                 addWeddingCommand.execute(model));
     }
 
@@ -54,16 +91,16 @@ public class AddWeddingCommandTest {
     public void equals() {
 
         AddWeddingCommand addWeddingCommand1 = new AddWeddingCommand(new Wedding(WEDDING_ONE.getWeddingName(),
-                WEDDING_ONE.getWeddingDate()), new HashSet<>());
+                WEDDING_ONE.getWeddingDate()), contactIndexes);
         AddWeddingCommand addWeddingCommand2 = new AddWeddingCommand(new Wedding(WEDDING_TWO.getWeddingName(),
-                WEDDING_TWO.getWeddingDate()), new HashSet<>());
+                WEDDING_TWO.getWeddingDate()), contactIndexes);
 
         // Same object -> returns true
         assertTrue(addWeddingCommand1.equals(addWeddingCommand1));
 
         // Same values -> returns true
         AddWeddingCommand addWeddingCommand1Copy = new AddWeddingCommand(new Wedding(WEDDING_ONE.getWeddingName(),
-                WEDDING_ONE.getWeddingDate()), new HashSet<>());
+                WEDDING_ONE.getWeddingDate()), contactIndexes);
         assertTrue(addWeddingCommand1.equals(addWeddingCommand1Copy));
 
         // Different types -> returns false
