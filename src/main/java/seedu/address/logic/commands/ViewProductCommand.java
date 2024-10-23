4db7b9ce01ec51a48a550e6dbfa50cbf39b6a395
zacharyxzz <xiebawang@hotmail.com>
2024-10-23 21:04:13 +0800

    Fix import formatting errors

--- src/main/java/seedu/address/logic/commands/ViewProductCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewProductCommand.java
@@ -2,12 +2,12 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.function.Predicate;
+
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
 import seedu.address.model.product.Product;
-
-import java.util.function.Predicate;
 
 /**
  * Finds and lists all suppliers in address book whose name contains any of the argument keywords.
