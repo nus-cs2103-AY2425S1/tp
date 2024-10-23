fefaa1f3600b4b40805ab5ef35cfe0a798ad6a0d
weibinlim <limweibin0816@gmail.com>
2024-10-23 15:04:51 +0800

    Revert "Add Relation class for new Relation field in Guest object"
    This reverts commit 204a994470c12c13640552f9dc4209b1bcf9ada5.

--- src/main/java/seedu/address/model/person/Guest.java
+++ src/main/java/seedu/address/model/person/Guest.java
@@ -12,30 +12,10 @@ import seedu.address.model.tag.Tag;
 public class Guest extends Person {
 
     private final Rsvp rsvp;
-    private final Relation relation;
 
     /**
      * Creates a Guest with the specified details.
      * Every field must be present and not null.
-     *
-     * @param name     The name of the guest.
-     * @param phone    The phone number of the guest.
-     * @param email    The email address of the guest.
-     * @param address  The address of the guest.
-     * @param tags     The tags associated with the guest.
-     * @param rsvp     The RSVP status of the guest.
-     * @param relation The relationship of the guest to the husband or wife.
-     */
-    public Guest(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Rsvp rsvp, Relation relation) {
-        super(name, phone, email, address, tags);
-        this.rsvp = rsvp;
-        this.relation = relation;
-    }
-
-    /**
-     * Creates a Guest with the specified details.
-     * Every field must be present and not null.
-     * The Relation status will be set to U for unknown by default if not specified.
      *
      * @param name    The name of the guest.
      * @param phone   The phone number of the guest.
@@ -47,31 +27,12 @@ public class Guest extends Person {
     public Guest(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Rsvp rsvp) {
         super(name, phone, email, address, tags);
         this.rsvp = rsvp;
-        this.relation = new Relation("U");
     }
+
 
     /**
      * Creates a Guest with the specified details.
      * The RSVP status will be set to PENDING by default if not specified.
-     * Every field must be present and not null.
-     *
-     * @param name     The name of the guest.
-     * @param phone    The phone number of the guest.
-     * @param email    The email address of the guest.
-     * @param address  The address of the guest.
-     * @param tags     The tags associated with the guest.
-     * @param relation The relationship of the guest to the husband or wife.
-     */
-    public Guest(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Relation relation) {
-        super(name, phone, email, address, tags);
-        this.rsvp = new Rsvp("PENDING");
-        this.relation = relation;
-    }
-
-    /**
-     * Creates a Guest with the specified details.
-     * The RSVP status will be set to PENDING by default if not specified.
-     * The Relation status will be set to U for unknown by default if not specified
      * Every field must be present and not null.
      *
      * @param name    The name of the guest.
@@ -83,7 +44,6 @@ public class Guest extends Person {
     public Guest(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
         this.rsvp = new Rsvp("PENDING");
-        this.relation = new Relation("U");
     }
 
     /**
@@ -102,15 +62,6 @@ public class Guest extends Person {
      */
     public Rsvp getRsvp() {
         return this.rsvp;
-    }
-
-    /**
-     * Returns the guest's relation to the wedding.
-     *
-     * @return The guest's relation to the wedding.
-     */
-    public Relation getRelation() {
-        return this.relation;
     }
 
     /**
@@ -144,8 +95,7 @@ public class Guest extends Person {
         }
 
         Guest otherGuest = (Guest) other;
-        return super.equals(other) && getRsvp().equals(otherGuest.getRsvp())
-                && getRelation().equals(otherGuest.getRelation());
+        return super.equals(other) && getRsvp().equals(otherGuest.getRsvp());
     }
 
     /**
@@ -156,12 +106,12 @@ public class Guest extends Person {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(getName(), getPhone(), getEmail(), getAddress(), getTags(), getRsvp(), getRelation());
+        return Objects.hash(getName(), getPhone(), getEmail(), getAddress(), getTags(), getRsvp());
     }
 
     /**
      * Returns a string representation of the guest, including their details
-     * RSVP status, and relation to the wedding.
+     * and RSVP status.
      *
      * @return A string representation of the guest.
      */
@@ -169,7 +119,6 @@ public class Guest extends Person {
     public String toString() {
         return super.toStringBuilder()
                 .add("RSVP", rsvp)
-                .add("Relation", relation)
                 .toString();
     }
 
