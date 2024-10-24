125e59221f59e38f0165e4184c608467e4dc499e
Daryl Koh <daryl.koh.hb@gmail.com>
2024-10-24 17:19:44 +0800

    Fix minor sorting bug

--- src/main/java/seedu/address/model/person/AttendanceStatus.java
+++ src/main/java/seedu/address/model/person/AttendanceStatus.java
@@ -12,11 +12,11 @@ public enum AttendanceStatus {
 
     public static Comparator<AttendanceStatus> getComparator(boolean isAscendingOrder) {
         return (s1, s2) -> {
-            if (s1 == NOT_TAKEN_PLACE) {
-                return 1;
-            }
             if (s2 == NOT_TAKEN_PLACE) {
                 return -1;
+            }
+            if (s1 == NOT_TAKEN_PLACE) {
+                return 1;
             }
             return isAscendingOrder ? s1.compareTo(s2) : s2.compareTo(s1);
         };
