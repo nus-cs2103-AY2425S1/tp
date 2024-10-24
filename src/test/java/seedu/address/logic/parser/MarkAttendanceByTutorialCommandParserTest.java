46b04e0c18321349773f3e5d58da7ee0dfe88a02
gladwintan <gladwintan@hotmail.sg>
2024-10-24 16:44:15 +0800

    Create test for parser

---
+++ src/test/java/seedu/address/logic/parser/MarkAttendanceByTutorialCommandParserTest.java
@@ -0,0 +1,33 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.MarkAttendanceByStudentCommand;
+import seedu.address.logic.commands.MarkAttendanceByTutorialCommand;
+import seedu.address.model.person.Attendance;
+import seedu.address.model.tutorial.Tutorial;
+
+public class MarkAttendanceByTutorialCommandParserTest {
+
+    private MarkAttendanceByTutorialCommandParser parser = new MarkAttendanceByTutorialCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsMarkAttendanceByTutorialCommand() {
+        MarkAttendanceByTutorialCommand expectedCommand = new MarkAttendanceByTutorialCommand(
+                new Tutorial("Math"), "12/12/2024");
+        assertParseSuccess(parser, "attend/12/12/2024 tut/Math", expectedCommand);
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkAttendanceByStudentCommand.MESSAGE_USAGE));
+    }
+}
