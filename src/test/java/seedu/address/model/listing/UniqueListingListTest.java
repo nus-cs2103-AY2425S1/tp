37b198a158e2fdc1ad0217387321c3608aa030b6
lrongyi <iygnormal@gmail.com>
2024-10-24 15:36:27 +0800

    Add UniqueListingListTest
    Add tests that are similar to the UniquePersonListTest
    due to the similarity between the two Lists

--- src/test/java/seedu/address/model/listing/UniqueListingListTest.java
+++ src/test/java/seedu/address/model/listing/UniqueListingListTest.java
@@ -2,13 +2,19 @@ package seedu.address.model.listing;
 
 import org.junit.jupiter.api.Test;
 import seedu.address.model.listing.exceptions.DuplicateListingException;
+import seedu.address.model.listing.exceptions.ListingNotFoundException;
 import seedu.address.testutil.ListingBuilder;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalListings.PASIR_RIS;
+import static seedu.address.testutil.TypicalListings.TAMPINES;
 
 public class UniqueListingListTest {
 
@@ -60,7 +66,7 @@ public class UniqueListingListTest {
 
     @Test
     public void setListing_targetListingNotInList_throwsListingNotFoundException() {
-        assertThrows(NullPointerException.class, () -> uniqueListingList.setListing(PASIR_RIS, PASIR_RIS));
+        assertThrows(ListingNotFoundException.class, () -> uniqueListingList.setListing(PASIR_RIS, PASIR_RIS));
     }
 
     @Test
@@ -82,4 +88,77 @@ public class UniqueListingListTest {
         assertEquals(uniqueListingList, expectedUniqueListingList);
     }
 
+    @Test
+    public void setListing_editedListingHasDifferentIdentity_success() {
+        uniqueListingList.add(PASIR_RIS);
+        uniqueListingList.setListing(PASIR_RIS, TAMPINES);
+        UniqueListingList expectedUniqueListingList = new UniqueListingList();
+        expectedUniqueListingList.add(TAMPINES);
+        assertEquals(uniqueListingList, expectedUniqueListingList);
+    }
+
+    @Test
+    public void setListing_editedListingHasNonUniqueIdentity_throwsDuplicateListingException() {
+        uniqueListingList.add(PASIR_RIS);
+        uniqueListingList.add(TAMPINES);
+        assertThrows(DuplicateListingException.class, () -> uniqueListingList.setListing(PASIR_RIS, TAMPINES));
+    }
+
+    @Test
+    public void remove_nullListing_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueListingList.remove(null));
+    }
+
+    @Test
+    public void remove_listingDoesNotExist_throwsListingNotFoundException() {
+        assertThrows(ListingNotFoundException.class, () -> uniqueListingList.remove(PASIR_RIS));
+    }
+
+    @Test
+    public void remove_existingListing_removesListing() {
+        uniqueListingList.add(PASIR_RIS);
+        uniqueListingList.remove(PASIR_RIS);
+        UniqueListingList expectedUniqueListingList = new UniqueListingList();
+        assertEquals(expectedUniqueListingList, uniqueListingList);
+    }
+
+    @Test
+    public void setListings_nullUniqueListingList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueListingList.setListings((UniqueListingList) null));
+    }
+
+    @Test
+    public void setListings_uniqueListingList_replacesOwnListWithProvidedUniqueListingList() {
+        uniqueListingList.add(PASIR_RIS);
+        UniqueListingList expectedUniqueListingList = new UniqueListingList();
+        expectedUniqueListingList.add(TAMPINES);
+        uniqueListingList.setListings(expectedUniqueListingList);
+        assertEquals(expectedUniqueListingList, uniqueListingList);
+    }
+
+    @Test
+    public void setListings_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueListingList.setListings((List<Listing>) null));
+    }
+
+    @Test
+    public void setListings_list_replacesOwnListWithProvidedList() {
+        uniqueListingList.add(PASIR_RIS);
+        List<Listing> listingList = Collections.singletonList(TAMPINES);
+        uniqueListingList.setListings(listingList);
+        UniqueListingList expectedUniqueListingList = new UniqueListingList();
+        expectedUniqueListingList.add(TAMPINES);
+        assertEquals(expectedUniqueListingList, uniqueListingList);
+    }
+
+    @Test
+    public void setListings_listWithDuplicateListings_throwsDuplicateListingException() {
+        List<Listing> listWithDuplicateListings = Arrays.asList(PASIR_RIS, PASIR_RIS);
+        assertThrows(DuplicateListingException.class, () -> uniqueListingList.setListings(listWithDuplicateListings));
+    }
+
+    @Test
+    public void toStringMethod() {
+        assertEquals(uniqueListingList.asUnmodifiableObservableList().toString(), uniqueListingList.toString());
+    }
 }
