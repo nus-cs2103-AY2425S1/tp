78cb579d063b4e690633b47b78aab4b73c57eb75
engyuhan <e1136866@u.nus.edu>
2024-10-24 20:30:01 +0800

    Add SearchTagCommand

---
+++ src/main/java/seedu/address/logic/commands/SearchTagCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Finds and lists all customers in the list whose any tag contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class SearchTagCommand extends Command {
+    public static final String COMMAND_WORD = "searchtag";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all customers whose any tag contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " new_customer non_english_speaker";
+
+    private final TagContainsKeywordsPredicate predicate;
+
+    public SearchTagCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof SearchCommand)) {
+            return false;
+        }
+
+        SearchTagCommand otherFindCommand = (SearchTagCommand) other;
+        return predicate.equals(otherFindCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
