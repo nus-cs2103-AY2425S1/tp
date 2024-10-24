0abf43269a9f7151923a33b75ecb2277675fbddd
Joel Tio <joeltiojinhon@gmail.com>
2024-10-24 16:00:30 +0800

    Rename ExportFormat to Exporter

---
+++ src/main/java/seedu/address/logic/exporter/Exporter.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.exporter;
+
+import java.nio.file.Path;
+
+import seedu.address.model.ReadOnlyAddressBook;
+
+/**
+ * Represents a class that can export an address book to a particular format
+ */
+public interface Exporter {
+    /**
+     * Exports the address book
+     */
+    public void exportAddressBook(ReadOnlyAddressBook addressBook);
+
+    /**
+     * Returns the path that the addressbook will be exported to
+     */
+    public Path getExportPath();
+}
