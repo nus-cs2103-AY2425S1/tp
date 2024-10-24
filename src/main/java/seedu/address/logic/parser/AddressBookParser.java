7e93962b5bcd3caa2eb9ba0e8064da296364e307
Jing Hong <46739337+RadieonAjax@users.noreply.github.com>
2024-10-24 23:28:18 +0800

    Remove whitespace

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -95,13 +95,13 @@ public class AddressBookParser {
 
         case FilterCommand.COMMAND_WORD:
             return new FilterCommandParser().parse(arguments);
-            
+
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
-            
+
         case GradeCommand.COMMAND_WORD:
             return new GradeCommandParser().parse(arguments);
-            
+
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
