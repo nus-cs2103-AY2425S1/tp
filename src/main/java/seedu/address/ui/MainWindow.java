cd328beba73f2b42784c199b6459efeaa7dd6347
JumpyJay <iuswtric1909@gmail.com>
2024-10-24 12:54:57 +0800

    Update Ui UML diagram image

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -1,7 +1,5 @@
 package seedu.address.ui;
 
-import static seedu.address.logic.commands.ClientUtil.findViewPerson;
-
 import java.util.logging.Logger;
 
 import javafx.event.ActionEvent;
@@ -15,6 +13,7 @@ import javafx.stage.Stage;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
+import seedu.address.logic.commands.ClientUtil;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -195,7 +194,7 @@ public class MainWindow extends UiPart<Stage> {
             }
 
             if (commandResult.isShowClient()) {
-                Person targetClient = findViewPerson(commandText, logic);
+                Person targetClient = ClientUtil.findViewPerson(commandText, logic);
                 handleShowClient(targetClient);
             }
 
