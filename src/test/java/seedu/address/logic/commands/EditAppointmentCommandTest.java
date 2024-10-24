67a2548b238079aa85cbb4206372a19291deddeb
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-10-24 09:43:12 +0800

    Add equality test cases for edit appointment

--- src/test/java/seedu/address/logic/commands/EditAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditAppointmentCommandTest.java
@@ -1,8 +1,11 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_APPOINTMENT_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_APPOINTMENT_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NRIC_BOB;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -27,5 +30,20 @@ public class EditAppointmentCommandTest {
         EditAppointmentCommand commandWithSameValues = new EditAppointmentCommand(new Nric(VALID_NRIC_AMY),
                 copyDescriptor);
         assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different nric -> returns false
+        assertFalse(standardCommand.equals(new EditAppointmentCommand(new Nric(VALID_NRIC_BOB), DESC_APPOINTMENT_AMY)));
+
+        // different descriptor -> returns false
+        assertFalse(standardCommand.equals(new EditAppointmentCommand(new Nric(VALID_NRIC_AMY), DESC_APPOINTMENT_BOB)));
     }
 }
