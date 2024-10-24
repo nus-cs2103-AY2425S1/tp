bc5dc6f730345ac8ea48fd0b28f93f523ea53330
Leslie Yip <leslieyip02@gmail.com>
2024-10-24 20:25:35 +0800

    Update docs

--- src/main/java/seedu/ddd/logic/commands/ListCommand.java
+++ src/main/java/seedu/ddd/logic/commands/ListCommand.java
@@ -4,6 +4,16 @@ import static java.util.Objects.requireNonNull;
 import static seedu.ddd.logic.Messages.MESSAGE_CONTACTS_LISTED_OVERVIEW;
 import static seedu.ddd.logic.parser.CliFlags.FLAG_CLIENT;
 import static seedu.ddd.logic.parser.CliFlags.FLAG_EVENT;
+import static seedu.ddd.logic.parser.CliFlags.FLAG_VENDOR;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_DESC;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_ID;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_SERVICE;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.function.Predicate;
 
@@ -19,15 +29,36 @@ import seedu.ddd.model.event.common.Event;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
+
+    public static final String COMMAND_DESCRIPTION = COMMAND_WORD + ": lists contacts with optional filters";
+    public static final String COMMAND_USAGE = "usage: " + COMMAND_WORD + " [{"
+            + FLAG_CLIENT + " | "
+            + FLAG_VENDOR + " | "
+            + FLAG_EVENT +  "}] "
+            + "[" + PREFIX_NAME + "NAME] "
+            + "[" + PREFIX_PHONE + "PHONE] "
+            + "[" + PREFIX_EMAIL + "EMAIL] "
+            + "[" + PREFIX_ADDRESS + "ADDRESS] "
+            + "{[" + PREFIX_DATE + "DATE] | "
+            + "[" + PREFIX_SERVICE + "SERVICE]} "
+            + "[" + PREFIX_TAG + "TAG ...] "
+            + "[" + PREFIX_ID + "ID]";
+    public static final String CLIENT_EXAMPLE_USAGE = "example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "Jane";
+    public static final String VENDOR_EXAMPLE_USAGE = "example: " + COMMAND_WORD + " "
+            + FLAG_VENDOR + " "
+            + PREFIX_SERVICE + "catering";
+    public static final String EVENT_EXAMPLE_USAGE = "example: " + COMMAND_WORD + " "
+            + FLAG_EVENT + " "
+            + PREFIX_DESC + "wedding";
+
+    public static final String MESSAGE_USAGE = COMMAND_DESCRIPTION + "\n"
+        + COMMAND_USAGE + "\n"
+        + CLIENT_EXAMPLE_USAGE + "\n"
+        + VENDOR_EXAMPLE_USAGE + "\n"
+        + EVENT_EXAMPLE_USAGE + "\n";
+
     public static final String MESSAGE_SUCCESS = "Listed all contacts";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + " -CONTACT_FLAG (either `-v` or `-c`)"
-            + ": Lists all clients in the addressbook.\n"
-            + "Example: " + COMMAND_WORD + " " + FLAG_CLIENT
-            + " n/Jane , lists all clients with name Jane in the addressbook.";
-    public static final String MESSAGE_EVENT_USAGE = COMMAND_WORD + " " + FLAG_EVENT
-            + ": Lists all events in the addressbook.\n"
-                    + "Example: " + COMMAND_WORD + " " + FLAG_CLIENT
-            + " des/George and Amy, lists all events with description George and Amy in the addressbook.";
 
     private static final Predicate<Event> CLEAR_EVENTS = any -> false;
 
--- src/main/java/seedu/ddd/model/event/common/predicate/EventPredicateBuilder.java
+++ src/main/java/seedu/ddd/model/event/common/predicate/EventPredicateBuilder.java
@@ -1,15 +1,14 @@
 package seedu.ddd.model.event.common.predicate;
 
-import static seedu.ddd.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.ddd.logic.parser.CliSyntax.PREFIX_DESC;
-import static seedu.ddd.logic.parser.CliSyntax.PREFIX_ID;
-
 import java.util.Arrays;
 import java.util.function.Predicate;
 
 import seedu.ddd.commons.util.ToStringBuilder;
+import static seedu.ddd.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.ddd.logic.commands.ListEventCommand;
 import seedu.ddd.logic.parser.ArgumentMultimap;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_DESC;
+import static seedu.ddd.logic.parser.CliSyntax.PREFIX_ID;
 import seedu.ddd.logic.parser.exceptions.ParseException;
 import seedu.ddd.model.event.common.Event;
 import seedu.ddd.model.event.common.EventId;
@@ -45,7 +44,7 @@ public class EventPredicateBuilder {
             String trimmedArgs = argMultimap.getValue(PREFIX_DESC).get().trim();
             if (trimmedArgs.isEmpty()) {
                 throw new ParseException(
-                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListEventCommand.MESSAGE_EVENT_USAGE));
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListEventCommand.MESSAGE_USAGE));
             }
             String[] descriptionKeywords = trimmedArgs.split("\\s+");
             combinedPredicate = combinedPredicate.and(
@@ -59,7 +58,7 @@ public class EventPredicateBuilder {
             String trimmedArgs = argMultimap.getValue(PREFIX_ID).get();
             if (trimmedArgs.isEmpty()) {
                 throw new ParseException(
-                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListEventCommand.MESSAGE_EVENT_USAGE));
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListEventCommand.MESSAGE_USAGE));
             }
             EventId eventId = new EventId(trimmedArgs);
             combinedPredicate = combinedPredicate.and(new EventIdPredicate(eventId));
