d7bf2040274e8dca7fce43ead3f208a8b8b78094
Dinoman44 <prakamya.singh08@gmail.com>
2024-10-24 14:51:02 +0800

    Fix checkstyle issues (import order)

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -18,8 +18,8 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.WhitelistCommand;
 import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.commands.WhitelistCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
