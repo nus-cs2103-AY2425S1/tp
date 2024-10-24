896a3c0cdc09b50a81f9dd12d88f911d68d761d1
tyxiangs <157029145+tyxiangs@users.noreply.github.com>
2024-10-24 21:10:17 +0800

    Add CheckAppointmentTest
    Half-complete

--- src/test/java/seedu/address/logic/commands/CheckAppointmentCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CheckAppointmentCommandTest.java
@@ -20,267 +20,238 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
-import seedu.address.model.person.Doctor;
 import seedu.address.model.person.Id;
-import seedu.address.model.person.Patient;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
 
 public class CheckAppointmentCommandTest {
 
 
-//    private final LocalDateTime appointmentTime1 = LocalDateTime.of(2024, 12, 31, 12, 0);
-//    private final LocalDateTime appointmentTime2 = LocalDateTime.of(2024, 12, 31, 13, 0);
-//    private final LocalDate appointmentDate = LocalDate.of(2024, 12, 31);
-//    private final String appointmentRemark = "Follow-up check";
-//
-//    @Test
-//    public void checkAppointment_success() throws Exception {
-//        // Create a ModelStub that can accept appointments
-//        ModelStubAcceptingAppointmentAdded modelStub = new ModelStubAcceptingAppointmentAdded();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//        Person validPatient = new PersonBuilder().buildPatient();
-//        Person validDoctor = new PersonBuilder().buildDoctor();
-//
-//        // Add doctor and patient to the model
-//        modelStub.addPersonToList(validPatient);
-//        modelStub.addPersonToList(validDoctor);
-//
-//        // Add an appointment to the patient
-//        validDoctor.addAppointment(appointmentTime1, validPatient.getId(), validDoctor.getId(), appointmentRemark);
-//
-//        // Execute the ViewHistoryCommand
-//        CheckAppointmentCommand checkAppointmentCommand =
-//                new CheckAppointmentCommand(validDoctor.getId(), appointmentDate);
-//        CommandResult commandResult = checkAppointmentCommand.execute(modelStub);
-//
-//        // Validate that the appointment was retrieved correctly
-//        String expectedMessage = String.format("DateTime: %s Appointment: %s (patient id) with %s (doctor id). "
-//                        + "Remarks: %s" + "\n", appointmentTime1.format(formatter), validPatient.getId(),
-//                validDoctor.getId(), appointmentRemark);
-//        assertEquals(expectedMessage, commandResult.getFeedbackToUser());
-//    }
-//
-//    @Test
-//    public void execute_checkMultipleValidAppointment_success() throws Exception {
-//        // Create a ModelStub that accepts appointments
-//        ModelStubAcceptingAppointmentAdded modelStub = new ModelStubAcceptingAppointmentAdded();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//        Person validPatient = new PersonBuilder().buildPatient();
-//        Person validDoctor = new PersonBuilder().buildDoctor();
-//
-//        // Add patient and doctor to model
-//        modelStub.addPersonToList(validPatient);
-//        modelStub.addPersonToList(validDoctor);
-//
-//        // Add an appointment to patient
-//        validPatient.addAppointment(appointmentTime1, validPatient.getId(), validDoctor.getId(), appointmentRemark);
-//        validPatient.addAppointment(appointmentTime2, validPatient.getId(), validDoctor.getId(), appointmentRemark);
-//
-//        // Create a CheckAppointmentCommand and execute it
-//        CheckAppointmentCommand checkAppointmentCommand =
-//                new CheckAppointmentCommand(validDoctor.getId(), appointmentDate);
-//        CommandResult commandResult = checkAppointmentCommand.execute(modelStub);
-//
-//        // Expected output message
-//        String expectedMessage = String.format("DateTime: %s Appointment: %s (patient id) "
-//                        + "with %s (doctor id). Remarks: %s\n"
-//                        + "DateTime: %s Appointment: %s (patient id) "
-//                        + "with %s (doctor id). Remarks: %s\n",
-//                appointmentTime1.format(formatter), validPatient.getId(), validDoctor.getId(), appointmentRemark,
-//                appointmentTime2.format(formatter), validPatient.getId(), validDoctor.getId(), appointmentRemark);
-//        assertEquals(expectedMessage, commandResult.getFeedbackToUser());
-//    }
-//
-//    @Test
-//    public void execute_checkNonexistentAppointment_throwsCommandException() {
-//        // Create a ModelStub with no appointments
-//        ModelStubAcceptingAppointmentAdded modelStub = new ModelStubAcceptingAppointmentAdded();
-//        Person validDoctor = new PersonBuilder().buildDoctor();
-//        Person validPatient = new PersonBuilder().buildPatient();
-//
-//        modelStub.addPersonToList(validPatient);
-//        modelStub.addPersonToList(validDoctor);
-//
-//        // Create a CheckAppointmentCommand for a nonexistent appointment
-//        CheckAppointmentCommand checkAppointmentCommand =
-//                new CheckAppointmentCommand(validDoctor.getId(), appointmentDate);
-//
-//        // Verify that an exception is thrown when no appointment is found
-//        assertThrows(CommandException.class, String.format(MESSAGE_NO_APPOINTMENT_FOUND, validDoctor.getName()), () ->
-//                checkAppointmentCommand.execute(modelStub));
-//    }
-//
-//    private class ModelStub implements Model {
-//        @Override
-//        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public ReadOnlyUserPrefs getUserPrefs() {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public GuiSettings getGuiSettings() {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public void setGuiSettings(GuiSettings guiSettings) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public Path getAddressBookFilePath() {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public void setAddressBookFilePath(Path addressBookFilePath) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public void addPerson(Person person) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public void setAddressBook(ReadOnlyAddressBook newData) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public ReadOnlyAddressBook getAddressBook() {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public boolean hasPerson(Person person) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public void deletePerson(Person target) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public void setPerson(Person target, Person editedPerson) {
-//            throw new AssertionError("This method should not be called.");
-//        }
-//
-//        @Override
-//        public ObservableList<Person> getFilteredPersonList() {
-//            return null;
-//        }
-//
-//        @Override
-//        public ObservableList<Person> getFilteredPersonById(Id id) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Patient getFilteredPatientById(ObservableList<Person> allPersons, Id id) {
-//            return null;
-//        }
-//
-//        @Override
-//        public Doctor getFilteredDoctorById(ObservableList<Person> allPersons, Id id) {
-//            return null;
-//        }
-//
-//        @Override
-//        public void updateFilteredPersonList(Predicate<Person> predicate) {
-//        }
-//    }
-//    /**
-//     * A Model stub that contains a single person.
-//     */
-//    private class ModelStubWithAppointment extends CheckAppointmentCommandTest.ModelStub {
-//        private final Patient patient;
-//        private final Doctor doctor;
-//
-//        ModelStubWithAppointment(Patient patient, Doctor doctor) {
-//            requireNonNull(patient);
-//            requireNonNull(doctor);
-//            this.patient = patient;
-//            this.doctor = doctor;
-//        }
-//
-//        @Override
-//        public boolean hasPerson(Person person) {
-//            requireNonNull(person);
-//            return person.isSamePerson(patient) || person.isSamePerson(doctor);
-//        }
-//
-//        @Override
-//        public ObservableList<Person> getFilteredPersonList() {
-//            return javafx.collections.FXCollections.observableArrayList(patient, doctor);
-//        }
-//
-//        @Override
-//        public Patient getFilteredPatientById(ObservableList<Person> allPersons, Id id) {
-//            return patient.getId().equals(id) ? patient : null;
-//        }
-//
-//        @Override
-//        public Doctor getFilteredDoctorById(ObservableList<Person> allPersons, Id id) {
-//            return doctor.getId().equals(id) ? doctor : null;
-//        }
-//    }
-//
-//    /**
-//     * A Model stub that always accept the appointment being added.
-//     */
-//    public class ModelStubAcceptingAppointmentAdded extends CheckAppointmentCommandTest.ModelStub {
-//        final ArrayList<Person> personsAdded = new ArrayList<>();
-//
-//        private final ArrayList<Person> personList = new ArrayList<>();
-//
-//        @Override
-//        public boolean hasPerson(Person person) {
-//            requireNonNull(person);
-//            return personsAdded.stream().anyMatch(person::isSamePerson);
-//        }
-//
-//        @Override
-//        public void addPerson(Person person) {
-//            requireNonNull(person);
-//            personsAdded.add(person);
-//        }
-//
-//        @Override
-//        public Patient getFilteredPatientById(ObservableList<Person> allPersons, Id id) {
-//            // Search for a patient with the specified ID
-//            for (Person person : allPersons) {
-//                if (person.getId().equals(id) && person instanceof Patient) {
-//                    return (Patient) person;
-//                }
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        public Doctor getFilteredDoctorById(ObservableList<Person> allPersons, Id id) {
-//            // Search for a doctor with the specified ID
-//            for (Person person : allPersons) {
-//                if (person.getId().equals(id) && person instanceof Doctor) {
-//                    return (Doctor) person;
-//                }
-//            }
-//            return null;
-//        }
-//        @Override
-//        public ObservableList<Person> getFilteredPersonList() {
-//            return javafx.collections.FXCollections.observableArrayList(personList);
-//        }
-//        public void addPersonToList(Person person) {
-//            personList.add(person);
-//        }
-//    }
+    private final LocalDateTime appointmentTime1 = LocalDateTime.of(2024, 12, 31, 12, 0);
+    private final LocalDateTime appointmentTime2 = LocalDateTime.of(2024, 12, 31, 13, 0);
+    private final LocalDate appointmentDate = LocalDate.of(2024, 12, 31);
+    private final String appointmentRemark = "Follow-up check";
+
+    @Test
+    public void execute_validDoctorWithAppointment_returnsAppointments() throws Exception {
+        ModelStubAcceptingAppointmentAdded modelStub = new ModelStubAcceptingAppointmentAdded();
+        modelStub.clearList();
+
+        Person validPatient = new PersonBuilder().buildPatient();
+        Person validDoctor = new PersonBuilder().buildDoctor();
+        modelStub.addPerson(validDoctor);
+        modelStub.addPerson(validPatient);
+
+        LocalDate appointmentDate = LocalDate.of(2024, 12, 31);
+        validPatient.addAppointment(appointmentTime1, validPatient.getId(), validDoctor.getId(), appointmentRemark);
+
+        CheckAppointmentCommand command = new CheckAppointmentCommand(validDoctor.getId(), appointmentDate);
+        CommandResult result = command.execute(modelStub);
+
+        String expectedMessage = String.format(MESSAGE_APPOINTMENT_FOUND, doctor.getName(),
+                validDoctor.getStringAppointmentsForDay(appointmentDate));
+
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_doctorWithNoAppointments_throwsCommandException() {
+        ModelStubWithDoctorAndAppointments modelStub = new ModelStubWithDoctorAndAppointments();
+
+        // Create a doctor without appointments
+        Person doctor = new PersonBuilder().withName("Dr. John Doe").buildDoctor();
+        modelStub.addDoctor(doctor);
+
+        CheckAppointmentCommand command = new CheckAppointmentCommand(doctor.getId(), LocalDate.of(2024, 12, 31));
+
+        CommandException thrown = org.junit.jupiter.api.Assertions.assertThrows(CommandException.class, () -> {
+            command.execute(modelStub);
+        });
+
+        assertEquals(String.format(MESSAGE_NO_APPOINTMENT_FOUND, doctor.getName()), thrown.getMessage());
+    }
+
+
+
+    private class ModelStub implements Model {
+        @Override
+        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyUserPrefs getUserPrefs() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public GuiSettings getGuiSettings() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setGuiSettings(GuiSettings guiSettings) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public Path getAddressBookFilePath() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBookFilePath(Path addressBookFilePath) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setAddressBook(ReadOnlyAddressBook newData) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ReadOnlyAddressBook getAddressBook() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deletePerson(Person target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void setPerson(Person target, Person editedPerson) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            return null;
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonById(int id) {
+            return null;
+        }
+
+        @Override
+        public Person getFilteredPersonById(ObservableList<Person> allPersons, int id) {
+            return null; // TODO?
+        }
+
+        @Override
+        public Person getFilteredPatientById(ObservableList<Person> allPersons, int id) {
+            return null;
+        }
+
+        @Override
+        public Person getFilteredDoctorById(ObservableList<Person> allPersons, int id) {
+            return null;
+        }
+
+        @Override
+        public void updateFilteredPersonList(Predicate<Person> predicate) {
+        }
+    }
+    /**
+     * A Model stub that contains a single person.
+     */
+    private class ModelStubWithAppointment extends CheckAppointmentCommandTest.ModelStub {
+        private final Person patient;
+        private final Person doctor;
+
+        ModelStubWithAppointment(Person patient, Person doctor) {
+            requireNonNull(patient);
+            requireNonNull(doctor);
+            this.patient = patient;
+            this.doctor = doctor;
+        }
+
+        @Override
+        public boolean hasPerson(Person person) {
+            requireNonNull(person);
+            return person.isSamePerson(patient) || person.isSamePerson(doctor);
+        }
+
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            return javafx.collections.FXCollections.observableArrayList(patient, doctor);
+        }
+
+        @Override
+        public Person getFilteredPatientById(ObservableList<Person> allPersons, int id) {
+            return patient.getId() == (id) ? patient : null;
+        }
+
+        @Override
+        public Person getFilteredDoctorById(ObservableList<Person> allPersons, int id) {
+            return doctor.getId() == (id) ? doctor : null;
+        }
+    }
+
+    /**
+     * A Model stub that always accept the appointment being added.
+     */
+    public class ModelStubAcceptingAppointmentAdded extends CheckAppointmentCommandTest.ModelStub {
+
+        private final ArrayList<Person> personList = new ArrayList<>();
+
+
+        @Override
+        public boolean hasPerson(Person person) {
+            requireNonNull(person);
+            return personList.stream().anyMatch(person::isSamePerson);
+        }
+
+        @Override
+        public void addPerson(Person person) {
+            requireNonNull(person);
+            personList.add(person);
+        }
+
+        public void clearList() {
+            personList.clear();
+        }
+
+        @Override
+        public Person getFilteredPatientById(ObservableList<Person> allPersons, int id) {
+            // Search for a patient with the specified ID
+            for (Person person : allPersons) {
+                if (person.getId() == (id) && person instanceof Person) {
+                    return person;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public Person getFilteredDoctorById(ObservableList<Person> allPersons, int id) {
+            // Search for a doctor with the specified ID
+            for (Person person : allPersons) {
+                if (person.getId() == (id) && person instanceof Person) {
+                    return person;
+                }
+            }
+            return null;
+        }
+        @Override
+        public ObservableList<Person> getFilteredPersonList() {
+            return javafx.collections.FXCollections.observableArrayList(personList);
+        }
+        public void addPersonToList(Person person) {
+            personList.add(person);
+        }
+    }
 
 }
 
