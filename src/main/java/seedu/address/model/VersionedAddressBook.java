e938d5f455bd08def268a6c455de6dc2dfcc1705
estellelim <estlim@hotmail.com>
2024-10-24 22:09:06 +0800

    Remove unintended changes

--- src/main/java/seedu/address/model/VersionedAddressBook.java
+++ src/main/java/seedu/address/model/VersionedAddressBook.java
@@ -87,9 +87,7 @@ public class VersionedAddressBook extends AddressBook {
         }
 
         VersionedAddressBook otherVersionedAddressBook = (VersionedAddressBook) other;
-        System.out.println("ab state: " + addressBookStateList.equals(otherVersionedAddressBook.addressBookStateList));
-        System.out.println(addressBookStateList);
-        System.out.println(otherVersionedAddressBook.addressBookStateList);
+
         // state check
         return super.equals(otherVersionedAddressBook)
                 && addressBookStateList.equals(otherVersionedAddressBook.addressBookStateList)
