43ea40ea6a7a20e15fa351fbecff2195db01847b
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2024-10-24 20:20:14 +0530

    Update Student.java

--- src/main/java/seedu/edulog/model/student/Student.java
+++ src/main/java/seedu/edulog/model/student/Student.java
@@ -28,7 +28,7 @@ public class Student {
     private final Fee fee;
 
     /**
-     * Every field must be present and not null except isPresent.
+     * Every field must be present and not null except hasPaid.
      */
     public Student(Name name, Phone phone, Email email, Address address, Set<Tag> tags, Fee fee) {
         requireAllNonNull(name, phone, email, address, tags);
