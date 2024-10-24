096b08de0e94902e6936a837a6c0e734a7b610fe
slye20 <samuellim2011@gmail.com>
2024-10-24 17:56:19 +0800

    Fix checkstyle

--- src/test/java/seedu/address/testutil/TypicalVendors.java
+++ src/test/java/seedu/address/testutil/TypicalVendors.java
@@ -2,6 +2,8 @@ package seedu.address.testutil;
 
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_BUDGET_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_BUDGET_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_COMPANY_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_COMPANY_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
@@ -12,9 +14,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_BUDGET_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_BUDGET_BOB;
-
 
 import java.util.ArrayList;
 import java.util.Arrays;
