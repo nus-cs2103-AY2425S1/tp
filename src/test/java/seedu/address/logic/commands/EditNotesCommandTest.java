b948b74243caadf6ea6d5f3ae72ce4403424fe7e
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-10-24 21:03:13 +0800

    Fix edit notes test

--- src/test/java/seedu/address/logic/commands/EditNotesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditNotesCommandTest.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_NOTED_INDEX;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_NOTE;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -17,11 +17,13 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for
@@ -32,14 +34,30 @@ public class EditNotesCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_editNotesCommand_success() throws Exception {
-        Person testPerson = model.getFilteredPersonList().get(0);
-        CommandResult commandResult = new EditNotesCommand(INDEX_FIRST_PERSON, INDEX_FIRST_NOTE, TYPICAL_NOTE)
-                .execute(model);
+    public void execute_editNotesCommand_success() {
+        // Create the person and expected models as in the original
+        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+
+        // Define the edited person with updated notes
+        Person editedPerson = new PersonBuilder(personToEdit).withNotes(
+                TYPICAL_NOTE.getNote(), "Likes dumplings").build();
+
+        // Define the EditNotesCommand
+        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_FIRST_PERSON, INDEX_FIRST_NOTE, TYPICAL_NOTE);
+
+        // Define the expected message
         String expectedMessage = String.format(EditNotesCommand.MESSAGE_EDIT_NOTES_SUCCESS,
-                Messages.format(testPerson));
-        assertEquals(expectedMessage, commandResult.getFeedbackToUser());
+                Messages.format(editedPerson));
+
+        // Create the expected model
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(personToEdit, editedPerson);
+        expectedModel.getFocusedPerson().set(editedPerson);
+        model.setPerson(personToEdit, personToEdit);
+
+        assertCommandSuccess(editNotesCommand, model, expectedMessage, expectedModel);
     }
+
 
     @Test
     public void execute_duplicateNote_throwsCommandException() {
@@ -59,8 +77,6 @@ public class EditNotesCommandTest {
     public void execute_invalidNoteIndex_throwsCommandException() {
         EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_SECOND_PERSON, INDEX_OUTOFBOUND_NOTE,
                 TYPICAL_NOTE);
-        assertThrows(CommandException.class, MESSAGE_INVALID_NOTED_INDEX, () ->
-                editNotesCommand.execute(model));
+        assertThrows(CommandException.class, MESSAGE_INVALID_NOTED_INDEX, () -> editNotesCommand.execute(model));
     }
-
 }
