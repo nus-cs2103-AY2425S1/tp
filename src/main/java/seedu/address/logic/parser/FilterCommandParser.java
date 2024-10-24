7ea9fe21d0315d04cde16ff25ddf46d96908a87b
SherChew27 <shermanczj@gmail.com>
2024-10-24 23:44:08 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FilterCommandParser.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_RSVP_STATUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
