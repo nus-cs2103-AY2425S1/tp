c4fcd64a9b6403aa325d7e88615f6c1c6e014594
Chan Guan Yu <changuanyu2904@gmail.com>
2024-10-24 18:46:20 +0800

    Remove unnecessary import statement

--- src/main/java/seedu/address/logic/commands/SearchCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_FIELD;
 
 import java.util.function.Predicate;
 
