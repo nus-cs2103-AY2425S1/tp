6192966af37654e24cc03c20f550c339196c03fc
chenle228 <e1156512@u.nus.edu>
2024-10-24 23:31:49 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/parser/FindByInterestCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindByInterestCommandParser.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import java.util.Arrays;
+
 import seedu.address.logic.commands.FindByInterestCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.InterestContainsKeywordsPredicate;
