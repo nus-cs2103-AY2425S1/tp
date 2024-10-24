21262cb8928bf3797f62c2992afba7b3bdf1ba5a
SamuelFoo <fooenzesamuel@gmail.com>
2024-10-24 23:05:27 +0800

    Remove unneeded override

--- src/main/java/seedu/address/logic/commands/listcommands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/listcommands/ListCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands.listcommands;
 
 import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.CommandResult;
-import seedu.address.model.Model;
 
 /**
  * Represents a command that lists data in the application.
@@ -12,12 +10,4 @@ public abstract class ListCommand extends Command {
     // Common prefix for all list commands
     public static final String COMMAND_PREFIX = "list";
 
-    /**
-     * Executes the list command and returns the result message.
-     *
-     * @param model {@code Model} which the command should operate on.
-     * @return feedback message of the operation result for display
-     */
-    @Override
-    public abstract CommandResult execute(Model model);
 }
