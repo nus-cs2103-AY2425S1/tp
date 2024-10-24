75dbcb9710e9a18c68ca2014417107d28d0de76f
sirmentos <e0958801@u.nus.edu>
2024-10-24 22:27:54 +0800

    code cov test

--- src/test/java/seedu/address/logic/parser/AssignPolicyCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AssignPolicyCommandParserTest.java
@@ -3,14 +3,21 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_POLICY_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.INVALID_ASSIGN_POLICY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ASSIGN_POLICY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_1;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_2;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_INSURANCE_AMOUNT_DUE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_INSURANCE_PAYMENT_DATE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_POLICY_NAME_LIFE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.AssignPolicyCommand;
 import seedu.address.model.person.Payment;
-
+import seedu.address.model.person.Policy;
+import seedu.address.commons.core.index.Index;
 public class AssignPolicyCommandParserTest {
 
     private AssignPolicyCommandParser parser = new AssignPolicyCommandParser();
@@ -28,5 +35,14 @@ public class AssignPolicyCommandParserTest {
         //invalid insurance field
         assertParseFailure(parser, "1" + INVALID_ASSIGN_POLICY, String.format(Payment.MESSAGE_CONSTRAINTS + "\n"
                 + AssignPolicyCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void assignPolicySuccess() {
+        Policy policy = new Policy(VALID_POLICY_NAME_LIFE, VALID_DATE_1, VALID_DATE_2,
+                VALID_INSURANCE_PAYMENT_DATE + " " + VALID_INSURANCE_AMOUNT_DUE);
+        Index targetIndex = Index.fromZeroBased(0);
+        AssignPolicyCommand assignPolicyCommand = new AssignPolicyCommand(targetIndex, policy);
+        assertParseSuccess(parser, "1" + VALID_ASSIGN_POLICY, assignPolicyCommand);
     }
 }
