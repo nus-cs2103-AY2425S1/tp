c7dc682c8435809d710817d6b535cfa11d681d10
he shi <e1312287@u.nus.edu>
2024-10-24 21:52:36 +0800

    Change FindCommandParserTest

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -6,6 +6,8 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.RoomNumber;
 
 public class FindCommandParserTest {
 
@@ -15,6 +17,15 @@ public class FindCommandParserTest {
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "     ",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidValue_throwsParseException() {
+        assertParseFailure(parser, " r/123", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                RoomNumber.MESSAGE_CONSTRAINTS));
+
+        assertParseFailure(parser, " p/47", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                Phone.MESSAGE_CONSTRAINTS));
     }
 
 }
