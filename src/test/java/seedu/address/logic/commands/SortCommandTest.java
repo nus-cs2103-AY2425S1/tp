9efb03b18d42c9be74d06389bb1c8ffc3298ac70
miuna9890 <156643085+miuna9890@users.noreply.github.com>
2024-10-24 10:49:11 +0800

    Add seperate typical person to test for unsorted list

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -2,7 +2,7 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.UnsortedTypicalPersons.getTypicalAddressBook;
 
 import java.util.Comparator;
 import java.util.List;
