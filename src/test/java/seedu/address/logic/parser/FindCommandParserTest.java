637968abe1acb2d753dd9a958908dc2654c56e30
SlothyCat <slothycatcoder@gmail.com>
2024-10-24 21:09:55 +0800

    Update FindCommandParserTest for TelegramHandle

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -5,11 +5,13 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailur
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.TelegramHandleContainsKeywordsPredicate;
 
 public class FindCommandParserTest {
 
@@ -21,7 +23,7 @@ public class FindCommandParserTest {
     }
 
     @Test
-    public void parse_validArgs_returnsFindCommand() {
+    public void parse_validName_returnsFindCommand() {
         // no leading and trailing whitespaces
         FindCommand expectedFindCommand =
                 new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")));
@@ -31,4 +33,26 @@ public class FindCommandParserTest {
         assertParseSuccess(parser, " \n Alice \n \t Bob  \t", expectedFindCommand);
     }
 
+    @Test
+    public void parse_validSingleTelegramHandle_returnsFindCommand() {
+        // Expected command with Telegram handle predicate
+        FindCommand expectedFindCommand =
+            new FindCommand(new TelegramHandleContainsKeywordsPredicate(List.of("@amy123")));
+
+        // Test with the correct input format
+        assertParseSuccess(parser, "/h @amy123", expectedFindCommand);
+    }
+
+    @Test
+    public void parse_validMultipleTelegramHandle_returnsFindCommand() {
+        // Expected command with Telegram handle predicate
+        FindCommand expectedFindCommand =
+            new FindCommand(new TelegramHandleContainsKeywordsPredicate(Arrays.asList("@amy123", "@bob321")));
+
+        // Test with the correct input format
+        assertParseSuccess(parser, "/h @amy123 @bob321", expectedFindCommand);
+
+        // multiple whitespaces between keywords
+        assertParseSuccess(parser, "/h \n @amy123 \n \t @bob321  \t", expectedFindCommand);
+    }
 }
