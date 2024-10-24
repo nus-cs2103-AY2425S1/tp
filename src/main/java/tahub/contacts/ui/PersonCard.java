e190e71cfd962fb8c8eea186b2e05192ef7809fe
devivy <divyalath.nus@gmail.com>
2024-10-24 19:13:27 +0800

    Correct chkeckstyles

--- src/main/java/tahub/contacts/ui/PersonCard.java
+++ src/main/java/tahub/contacts/ui/PersonCard.java
@@ -3,8 +3,8 @@ package tahub.contacts.ui;
 import java.util.Comparator;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
--- src/test/java/tahub/contacts/storage/JsonAdaptedStudentCourseAssociationTest.java
+++ src/test/java/tahub/contacts/storage/JsonAdaptedStudentCourseAssociationTest.java
@@ -11,7 +11,6 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 import tahub.contacts.commons.exceptions.IllegalValueException;
-import tahub.contacts.model.grade.GradingSystem;
 import tahub.contacts.model.studentcourseassociation.StudentCourseAssociation;
 import tahub.contacts.testutil.AttendanceExamples;
 
