7e60049ca2564a748286051b1bfb51809e863b62
zzibo <zibozzb@gmail.com>
2024-10-24 21:46:31 +0800

    Update help window message

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -21,7 +21,9 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String COMMAND_FUNCTION = COMMAND_WORD + ": Adds a person to the address book. ";
+
+    public static final String MESSAGE_USAGE = COMMAND_FUNCTION
             + "Parameters: "
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
