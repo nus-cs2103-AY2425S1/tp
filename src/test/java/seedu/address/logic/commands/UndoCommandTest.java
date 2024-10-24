462ead89e5d2eed53525525bc57c649729ed4d32
Jing Hong Tee <e1122714@u.nus.edu>
2024-10-24 21:43:56 +0800

    Update import order

--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -1,16 +1,17 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class UndoCommandTest {
     private static final Integer FIRST_INDEX = 1;
