01adda4703d092fbec9e30d426a0df572b07649d
sheenkerr <{46549752}+{sheenkerr}@users.noreply.github.com>
2024-10-24 18:44:22 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/EditNotesCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditNotesCommandTest.java
@@ -1,6 +1,20 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_NOTED_INDEX;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_NOTE;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_OUTOFBOUND_NOTE;
+import static seedu.address.testutil.TypicalIndexes.INDEX_OUTOFBOUND_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalNotes.DUPLICATE_NOTE;
+import static seedu.address.testutil.TypicalNotes.TYPICAL_NOTE;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -9,46 +23,44 @@ import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Note;
 import seedu.address.model.person.Person;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalIndexes.*;
-import static seedu.address.testutil.TypicalNotes.DUPLICATE_NOTE;
-import static seedu.address.testutil.TypicalNotes.TYPICAL_NOTE;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-
 /**
- * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * EditCommand.
  */
 public class EditNotesCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_EditNotesCommand_success()  throws Exception {
+    public void execute_editNotesCommand_success() throws Exception {
         Person testPerson = model.getFilteredPersonList().get(0);
-        CommandResult commandResult = new EditNotesCommand(INDEX_FIRST_PERSON,INDEX_FIRST_NOTE, TYPICAL_NOTE).execute(model);
-        String expectedMessage = String.format(EditNotesCommand.MESSAGE_EDIT_NOTES_SUCCESS, Messages.format(testPerson));
+        CommandResult commandResult = new EditNotesCommand(INDEX_FIRST_PERSON, INDEX_FIRST_NOTE, TYPICAL_NOTE)
+                .execute(model);
+        String expectedMessage = String.format(EditNotesCommand.MESSAGE_EDIT_NOTES_SUCCESS,
+                Messages.format(testPerson));
         assertEquals(expectedMessage, commandResult.getFeedbackToUser());
     }
 
     @Test
     public void execute_duplicateNote_throwsCommandException() {
-        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_SECOND_PERSON,INDEX_FIRST_NOTE, DUPLICATE_NOTE);
+        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_SECOND_PERSON, INDEX_FIRST_NOTE, DUPLICATE_NOTE);
         assertThrows(CommandException.class, Note.DUPLICATE_MESSAGE_CONSTRAINTS, () -> editNotesCommand.execute(model));
     }
+
     @Test
     public void execute_invalidPersonIndex_throwsCommandException() {
-        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_OUTOFBOUND_PERSON,INDEX_FIRST_NOTE, DUPLICATE_NOTE);
-        assertThrows(CommandException.class, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, () -> editNotesCommand.execute(model));
+        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_OUTOFBOUND_PERSON, INDEX_FIRST_NOTE,
+                DUPLICATE_NOTE);
+        assertThrows(CommandException.class, MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, () ->
+                editNotesCommand.execute(model));
     }
-   @Test
+
+    @Test
     public void execute_invalidNoteIndex_throwsCommandException() {
-        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_FIRST_PERSON,INDEX_OUTOFBOUND_NOTE, TYPICAL_NOTE);
-        assertThrows(CommandException.class, Messages.MESSAGE_INVALID_NOTED_INDEX, () -> editNotesCommand.execute(model));
+        EditNotesCommand editNotesCommand = new EditNotesCommand(INDEX_SECOND_PERSON, INDEX_OUTOFBOUND_NOTE,
+                TYPICAL_NOTE);
+        assertThrows(CommandException.class, MESSAGE_INVALID_NOTED_INDEX, () ->
+                editNotesCommand.execute(model));
     }
-
-
-
 
 }
--- src/test/java/seedu/address/testutil/TypicalNotes.java
+++ src/test/java/seedu/address/testutil/TypicalNotes.java
@@ -1,7 +1,11 @@
 package seedu.address.testutil;
 
 import seedu.address.model.person.Note;
+
+/**
+ * A utility class containing a list of {@code Notes} objects to be used in tests.
+ */
 public class TypicalNotes {
-    public static final Note TYPICAL_NOTE =  new Note("Test");
-    public static final Note DUPLICATE_NOTE =  new Note("Likes dumplings");
+    public static final Note TYPICAL_NOTE = new Note("Test");
+    public static final Note DUPLICATE_NOTE = new Note("Likes dumplings");
 }
