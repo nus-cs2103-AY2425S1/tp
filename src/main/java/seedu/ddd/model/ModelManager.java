41ab6bbb1a779c7bcac5f795c33d936bda615a70
Leslie Yip <leslieyip02@gmail.com>
2024-10-24 12:43:19 +0800

    Change reference to Person

--- src/main/java/seedu/ddd/model/ModelManager.java
+++ src/main/java/seedu/ddd/model/ModelManager.java
@@ -176,7 +176,7 @@ public class ModelManager implements Model {
     //=========== Filtered Data List Accessors =============================================================
 
     /**
-     * Returns an unmodifiable view of the list of {@code Person} backed by the internal list of
+     * Returns an unmodifiable view of the list of {@code Contact} backed by the internal list of
      * {@code versionedAddressBook}
      */
     @Override
