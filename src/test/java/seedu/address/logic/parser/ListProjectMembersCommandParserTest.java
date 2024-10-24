595941918574b92ba33f8084b8c5c22d0fae615d
LeeZeHao <zehaolee2004@gmail.com>
2024-10-24 10:59:03 +0800

    Add ListProjectMembersCommandParserTest.java

---
+++ src/test/java/seedu/address/logic/parser/ListProjectMembersCommandParserTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.ListProjectMembersCommand;
+import seedu.address.model.assignment.AssignmentProjectPredicate;
+
+import org.junit.jupiter.api.Test;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+public class ListProjectMembersCommandParserTest {
+
+    private ListProjectMembersCommandParser parser = new ListProjectMembersCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListProjectMembersCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_validArgs_returnsFindCommand() {
+        // no leading and trailing whitespaces
+        ListProjectMembersCommand expectedListCommand =
+                new ListProjectMembersCommand(new AssignmentProjectPredicate("Project Alpha"));
+        assertParseSuccess(parser, "Project Alpha", expectedListCommand);
+    }
+
+}
