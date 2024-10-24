9b80e6adaa6da5ac233b1a5b6fefcd21c9e7fe4f
Tseng Tzu Che <tzuche2406@gmail.com>
2024-10-24 16:40:10 +0800

    Update equals method in Name

--- src/main/java/seedu/address/model/volunteer/Name.java
+++ src/main/java/seedu/address/model/volunteer/Name.java
@@ -57,7 +57,7 @@ public class Name {
         }
 
         Name otherName = (Name) other;
-        return fullName.equals(otherName.fullName);
+        return fullName.equalsIgnoreCase(otherName.fullName);
     }
 
     /**
