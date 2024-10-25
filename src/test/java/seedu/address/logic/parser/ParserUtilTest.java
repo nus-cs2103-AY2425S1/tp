073d4b87e6477a7bad1d2b30e937493879307c21
DennieDan <e1129903@u.nus.edu>
2024-10-25 10:50:01 +0800

    Add tests for ParserUtil#parseRate

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -17,7 +17,7 @@ import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
 import seedu.address.model.student.OwedAmount;
 import seedu.address.model.student.Phone;
-
+import seedu.address.model.student.Rate;
 
 
 public class ParserUtilTest {
@@ -25,14 +25,14 @@ public class ParserUtilTest {
     private static final String INVALID_PHONE = "+651234";
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
-    private static final String INVALID_OWED_AMOUNT = "+123.12";
+    private static final String INVALID_COST_AMOUNT = "+123.12";
     private static final String INVALID_HOUR = "+0.75";
 
     private static final String VALID_NAME = "Rachel Walker";
     private static final String VALID_PHONE = "91234567";
     private static final String VALID_ADDRESS = "123 Main Street #0505";
     private static final String VALID_EMAIL = "rachel@example.com";
-    private static final String VALID_OWED_AMOUNT = "300.25";
+    private static final String VALID_COST_AMOUNT = "300.25";
     private static final String VALID_HOUR = "12.5";
 
     private static final String WHITESPACE = " \t\r\n";
@@ -151,25 +151,47 @@ public class ParserUtilTest {
     }
 
     @Test
+    public void parseRate_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseRate((String) null));
+    }
+
+    @Test
+    public void parseRate_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseRate(INVALID_COST_AMOUNT));
+    }
+
+    @Test
+    public void parseRate_validValueWithoutWhitespace_returnsRate() throws Exception {
+        Rate expectedRate = new Rate(VALID_COST_AMOUNT);
+        assertEquals(expectedRate, ParserUtil.parseRate(VALID_COST_AMOUNT));
+    }
+
+    @Test
+    public void parseRate_validValueWithWhitespace_returnsTrimmedRate() throws Exception {
+        Rate expectedRate = new Rate(VALID_COST_AMOUNT);
+        assertEquals(expectedRate, ParserUtil.parseRate(WHITESPACE + VALID_COST_AMOUNT));
+    }
+
+    @Test
     public void parseOwedAmount_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> ParserUtil.parseOwedAmount((String) null));
     }
 
     @Test
     public void parseOwedAmount_invalidValue_throwsParseException() {
-        assertThrows(ParseException.class, () -> ParserUtil.parseOwedAmount(INVALID_OWED_AMOUNT));
+        assertThrows(ParseException.class, () -> ParserUtil.parseOwedAmount(INVALID_COST_AMOUNT));
     }
 
     @Test
     public void parseOwedAmount_validValueWithWhitespace_returnsTrimmedOwedAmount() throws Exception {
-        OwedAmount expectedOwedAmount = new OwedAmount(VALID_OWED_AMOUNT);
-        assertEquals(expectedOwedAmount, ParserUtil.parseOwedAmount(VALID_OWED_AMOUNT + WHITESPACE));
+        OwedAmount expectedOwedAmount = new OwedAmount(VALID_COST_AMOUNT);
+        assertEquals(expectedOwedAmount, ParserUtil.parseOwedAmount(VALID_COST_AMOUNT + WHITESPACE));
     }
 
     @Test
     public void parseOwedAmount_validValueWithoutWhitespace_returnsOwedAmount() throws Exception {
-        OwedAmount expectedOwedAmount = new OwedAmount(VALID_OWED_AMOUNT);
-        assertEquals(expectedOwedAmount, ParserUtil.parseOwedAmount(VALID_OWED_AMOUNT));
+        OwedAmount expectedOwedAmount = new OwedAmount(VALID_COST_AMOUNT);
+        assertEquals(expectedOwedAmount, ParserUtil.parseOwedAmount(VALID_COST_AMOUNT));
     }
 
     @Test
