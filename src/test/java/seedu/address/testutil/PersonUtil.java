cf3813c67c81dca71efa8e30740d567b81a28936
SherniceSng <shernice.syx@gmail.com>
2024-10-25 18:07:54 +0800

    Make minor changes

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -34,11 +34,8 @@ public class PersonUtil {
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
 
         if (!person.getTags().isEmpty()) {
-            sb.append(PREFIX_TAG + person.getTag().tagName + " ");
+            sb.append(PREFIX_TAG + person.getRole().tagName + " ");
         }
-//        person.getTags().stream().forEach(
-//            s -> sb.append(PREFIX_TAG + s.tagName + " ")
-//        );
 
         person.getWeddingJobs().stream().forEach(
                 s -> sb.append(PREFIX_WEDDING + s.getName().toString() + " ")
@@ -55,14 +52,6 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
-//        if (descriptor.getTags().isPresent()) {
-//            Set<Tag> tags = descriptor.getTags().get();
-//            if (tags.isEmpty()) {
-//                sb.append(PREFIX_TAG);
-//            } else {
-//                tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
-//            }
-//        }
         return sb.toString();
     }
 }
