ee435c4bfdba8f00a77ef26108252ef39e37ce60
meerkatboy <sohjunanerwin@gmail.com>
2024-10-25 09:24:38 +0800

    Add logger to the FindCommandParser
    Without a logger, it may be difficult to track where errors occur and
    document them properly to aid with debugging in the future.
    
    Having a logger will help speed up the process of debugging.

--- src/main/java/seedu/internbuddy/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/internbuddy/logic/parser/FindCommandParser.java
@@ -3,6 +3,8 @@ package seedu.internbuddy.logic.parser;
 import static seedu.internbuddy.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import seedu.internbuddy.logic.commands.FindCommand;
 import seedu.internbuddy.logic.parser.exceptions.ParseException;
@@ -13,14 +15,18 @@ import seedu.internbuddy.model.company.NameContainsKeywordsPredicate;
  */
 public class FindCommandParser implements Parser<FindCommand> {
 
+    private static Logger logger = Logger.getLogger("Foo");
+
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
      * and returns a FindCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
+        logger.log(Level.INFO, "trimming argument");
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
+            logger.log(Level.WARNING, "string is empty!");
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
