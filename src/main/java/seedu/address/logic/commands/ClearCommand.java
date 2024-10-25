b949e7777b4a19aad204786d30feb34a43f30769
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-10-25 15:43:00 +0800

    Add clear command for filtered list functionality

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,8 +2,10 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.ObservableList;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Clears the address book.
@@ -11,13 +13,21 @@ import seedu.address.model.Model;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Filtered participants have been deleted";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setAddressBook(new AddressBook());
+        ObservableList<Person> participantList = model.getFilteredPersonList();
+        if (participantList.equals(model.getAddressBook().getPersonList())) {
+            model.setAddressBook(new AddressBook());
+        } else {
+            while (!participantList.isEmpty()) {
+                Person personToDelete = participantList.get(0);
+                model.deletePerson(personToDelete);
+            }
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -3,12 +3,16 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.predicates.GenderMatchesKeywordsPredicate;
 
 public class ClearCommandTest {
 
@@ -29,4 +33,17 @@ public class ClearCommandTest {
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
+    @Test
+    public void execute_nonEmptyAddressBookFilteredList_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        model.updateFilteredPersonList(new GenderMatchesKeywordsPredicate(Arrays.asList("m")));
+        expectedModel.updateFilteredPersonList(new GenderMatchesKeywordsPredicate(Arrays.asList("m")));
+        while (!expectedModel.getFilteredPersonList().isEmpty()) {
+            Person personToDelete = expectedModel.getFilteredPersonList().get(0);
+            expectedModel.deletePerson(personToDelete);
+        }
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
 }
