4f656bce5c00cafe6cb2f67bb31638438cb11423
SherniceSng <shernice.syx@gmail.com>
2024-10-25 17:16:48 +0800

    Edit PersonUtil.java

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -5,13 +5,11 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-
-import java.util.Set;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_WEDDING;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Person;
-import seedu.address.model.tag.Tag;
 
 /**
  * A utility class for Person.
@@ -34,8 +32,16 @@ public class PersonUtil {
         sb.append(PREFIX_PHONE + person.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
-        person.getTags().stream().forEach(
-            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+
+        if (!person.getTags().isEmpty()) {
+            sb.append(PREFIX_TAG + person.getTag().tagName + " ");
+        }
+//        person.getTags().stream().forEach(
+//            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+//        );
+
+        person.getWeddingJobs().stream().forEach(
+                s -> sb.append(PREFIX_WEDDING + s.getName().toString() + " ")
         );
         return sb.toString();
     }
@@ -49,14 +55,14 @@ public class PersonUtil {
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
-        if (descriptor.getTags().isPresent()) {
-            Set<Tag> tags = descriptor.getTags().get();
-            if (tags.isEmpty()) {
-                sb.append(PREFIX_TAG);
-            } else {
-                tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
-            }
-        }
+//        if (descriptor.getTags().isPresent()) {
+//            Set<Tag> tags = descriptor.getTags().get();
+//            if (tags.isEmpty()) {
+//                sb.append(PREFIX_TAG);
+//            } else {
+//                tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
+//            }
+//        }
         return sb.toString();
     }
 }
