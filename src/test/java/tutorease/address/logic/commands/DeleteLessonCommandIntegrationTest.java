499c0d5b8f77ed8c629a17dc85bbf7301dec8fe6
ethantyh692 <ethantyh692@gmail.com>
2024-10-25 15:12:49 +0800

    Add test case to test delete after find

--- src/test/java/tutorease/address/logic/commands/DeleteLessonCommandIntegrationTest.java
+++ src/test/java/tutorease/address/logic/commands/DeleteLessonCommandIntegrationTest.java
@@ -1,8 +1,18 @@
 package tutorease.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tutorease.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static tutorease.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static tutorease.address.testutil.TypicalLessons.GEOGRAPHY_LESSON;
+import static tutorease.address.testutil.TypicalLessons.HISTORY_LESSON;
+import static tutorease.address.testutil.TypicalLessons.MATH_LESSON;
+import static tutorease.address.testutil.TypicalLessons.SCIENCE_LESSON;
 import static tutorease.address.testutil.TypicalStudents.getTypicalTutorEase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,9 +24,11 @@ import tutorease.address.model.Model;
 import tutorease.address.model.ModelManager;
 import tutorease.address.model.UserPrefs;
 import tutorease.address.model.lesson.Lesson;
+import tutorease.address.model.lesson.LessonContainsNamesPredicate;
 import tutorease.address.model.lesson.StudentId;
 import tutorease.address.model.person.Person;
 import tutorease.address.testutil.LessonBuilder;
+import tutorease.address.testutil.TypicalLessons;
 import tutorease.address.testutil.TypicalStudents;
 
 public class DeleteLessonCommandIntegrationTest {
@@ -37,7 +49,8 @@ public class DeleteLessonCommandIntegrationTest {
 
     @Test
     public void execute_deleteLesson_success() throws ParseException {
-        Model expectedModel = new ModelManager(model.getTutorEase(), new UserPrefs(), model.getLessonSchedule());
+        Model expectedModel = new ModelManager(model.getTutorEase(), new UserPrefs(),
+                model.getLessonSchedule());
         Lesson validLesson = new LessonBuilder().withName(validPerson)
                 .withStartDateTime(startDateTime)
                 .withEndDateTime(endDateTime)
@@ -56,7 +69,8 @@ public class DeleteLessonCommandIntegrationTest {
 
     @Test
     public void execute_invalidIndex_throwsCommandException() throws ParseException {
-        Model expectedModel = new ModelManager(model.getTutorEase(), new UserPrefs(), model.getLessonSchedule());
+        Model expectedModel = new ModelManager(model.getTutorEase(), new UserPrefs(),
+                model.getLessonSchedule());
         Lesson validLesson = new LessonBuilder().withName(validPerson)
                 .withStartDateTime(startDateTime)
                 .withEndDateTime(endDateTime)
@@ -70,5 +84,42 @@ public class DeleteLessonCommandIntegrationTest {
 
         assertCommandFailure(new DeleteLessonCommand(Index.fromOneBased(10)), model,
                 DeleteLessonCommand.MESSAGE_INVALID_INDEX);
+    }
+
+    @Test
+    public void execute_AfterFind_success() {
+        Model expectedModel = new ModelManager(model.getTutorEase(), new UserPrefs(),
+                model.getLessonSchedule());
+
+        // Add lessons for Alice and Bob
+        List<Lesson> typicalLessons = TypicalLessons.getTypicalLessonsList();
+        for (Lesson lesson : typicalLessons) {
+            expectedModel.addLesson(lesson);
+            assertTrue(expectedModel.getFilteredLessonList().contains(lesson));
+        }
+
+        assertEquals(expectedModel.getFilteredLessonList().size(), 7);
+
+        // Only find lessons for Alice
+        List<String> keywords = Arrays.asList("Alice");
+        expectedModel.updateFilteredLessonList(new LessonContainsNamesPredicate(keywords));
+        assertEquals(expectedModel.getFilteredLessonList().size(), 4);
+        List<Lesson> expectedRemainingLessons = Arrays.asList(
+                MATH_LESSON,
+                SCIENCE_LESSON,
+                HISTORY_LESSON,
+                GEOGRAPHY_LESSON);
+        expectedRemainingLessons.sort(Lesson::compareTo);
+        assertEquals(expectedRemainingLessons, expectedModel.getFilteredLessonList());
+
+        // Since there are only 4 lessons now, should not be able to delete lesson at index 5
+        assertCommandFailure(new DeleteLessonCommand(Index.fromOneBased(5)), expectedModel,
+                DeleteLessonCommand.MESSAGE_INVALID_INDEX);
+
+        // Deleting lesson at index 4 should be Alice's History Lesson instead of Alice's Geography Lesson,
+        // which was the lesson at index 4 before finding
+        assertCommandSuccess(new DeleteLessonCommand(Index.fromOneBased(4)), expectedModel,
+                String.format(DeleteLessonCommand.MESSAGE_SUCCESS, HISTORY_LESSON),
+                expectedModel);
     }
 }
