2603bc8b255076fbd5b65d1d927c5d12a664d428
gladwintan <gladwintan@hotmail.sg>
2024-10-26 15:46:21 +0800

    Add participationList field to update UI

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -110,7 +110,8 @@ public class MainWindow extends UiPart<Stage> {
      * Fills up all the placeholders of this window.
      */
     void fillInnerParts() {
-        personListPanel = new PersonListPanel(logic.getFilteredPersonList());
+        personListPanel = new PersonListPanel(
+                logic.getFilteredPersonList(), logic.getAddressBook().getParticipationList());
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -1,13 +1,19 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.participation.Participation;
 import seedu.address.model.person.Person;
 
 /**
@@ -17,16 +23,26 @@ public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
+    private final ObservableList<Person> personList;
+    private final ObservableList<Participation> participationList;
+    private final Map<Person, ObservableList<Participation>> participationMap;
+
     @FXML
     private ListView<Person> personListView;
 
     /**
      * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
      */
-    public PersonListPanel(ObservableList<Person> personList) {
+    public PersonListPanel(ObservableList<Person> personList, ObservableList<Participation> participationList) {
         super(FXML);
         personListView.setItems(personList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
+
+        this.personList = personList;
+        this.participationList = participationList;
+        participationMap = groupParticipationByPerson(personList, participationList);
+
+        addListeners();
     }
 
     /**
@@ -46,4 +62,53 @@ public class PersonListPanel extends UiPart<Region> {
         }
     }
 
+    private HashMap<Person, ObservableList<Participation>> groupParticipationByPerson(
+            ObservableList<Person> personList, ObservableList<Participation> participationList) {
+        HashMap<Person, ObservableList<Participation>> participationMap = new HashMap<>();
+
+        for (Person person : personList) {
+            participationMap.put(person, FXCollections.observableArrayList());
+        }
+
+        for (Participation participation : participationList) {
+            ObservableList<Participation> participations = participationMap.get(participation.getStudent());
+            if (participations != null) {
+                participations.add(participation);
+            }
+        }
+
+        return participationMap;
+    }
+
+    private void addListeners() {
+        // Listener for changes in the personList
+        personList.addListener((ListChangeListener<Person>) change -> {
+            while (change.next()) {
+                for (Person removedPerson : change.getRemoved()) {
+                    participationMap.remove(removedPerson);
+                }
+                for (Person addedPerson : change.getAddedSubList()) {
+                    participationMap.put(addedPerson, FXCollections.observableArrayList());
+                }
+            }
+        });
+
+        // Listener for changes in the participationList
+        participationList.addListener((ListChangeListener<Participation>) change -> {
+            while (change.next()) {
+                for (Participation removedParticipation : change.getRemoved()) {
+                    ObservableList<Participation> participations = participationMap.get(removedParticipation.getStudent());
+                    if (participations != null) {
+                        participations.remove(removedParticipation);
+                    }
+                }
+                for (Participation addedParticipation : change.getAddedSubList()) {
+                    ObservableList<Participation> participations = participationMap.get(addedParticipation.getStudent());
+                    if (participations != null) {
+                        participations.add(addedParticipation);
+                    }
+                }
+            }
+        });
+    }
 }
