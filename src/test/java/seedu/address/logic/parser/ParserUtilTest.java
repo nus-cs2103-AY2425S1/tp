164b473d0f3687a4cb8ecc55c4c0dda2ceea3ddb
TYiFan-hub <tyifan123@gmail.com>
2024-10-26 15:04:47 +0800

    Add test cases
    Add test cases for parseSchedule.

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -16,7 +16,7 @@ import seedu.address.model.student.Days;
 import seedu.address.model.student.Email;
 import seedu.address.model.student.Name;
 import seedu.address.model.student.Phone;
-
+import seedu.address.model.student.Schedule;
 
 
 public class ParserUtilTest {
@@ -25,12 +25,14 @@ public class ParserUtilTest {
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_HOUR = "+0.75";
+    private static final String INVALID_SCHEDULE = "Monday 1600-1800";
 
     private static final String VALID_NAME = "Rachel Walker";
     private static final String VALID_PHONE = "91234567";
     private static final String VALID_ADDRESS = "123 Main Street #0505";
     private static final String VALID_EMAIL = "rachel@example.com";
     private static final String VALID_HOUR = "12.5";
+    private static final String VALID_SCHEDULE = "Monday-1600-1800";
 
     private static final String WHITESPACE = " \t\r\n";
 
@@ -169,6 +171,30 @@ public class ParserUtilTest {
         double expectedHour = Double.parseDouble(VALID_HOUR);
         assertEquals(expectedHour, ParserUtil.parseHour(hourWithWhitespace));
     }
+
+    @Test
+    public void parseSchedule_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseSchedule((String) null));
+    }
+
+    @Test
+    public void parseSchedule_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseSchedule(INVALID_SCHEDULE));
+    }
+
+    @Test
+    public void parseSchedule_validValueWithourWhitespace_returnsSchedule() throws Exception {
+        Schedule expectedSchedule = ParserUtil.parseSchedule(VALID_SCHEDULE);
+        assertEquals(expectedSchedule, ParserUtil.parseSchedule(VALID_SCHEDULE));
+    }
+
+    @Test
+    public void parseSchedule_validValueWithWhitespace_returnsTrimmedSchedule() throws Exception {
+        String scheduleWithWhitespace = WHITESPACE + VALID_SCHEDULE + WHITESPACE;
+        Schedule expectedSchedule = ParserUtil.parseSchedule(VALID_SCHEDULE);
+        assertEquals(expectedSchedule, ParserUtil.parseSchedule(scheduleWithWhitespace));
+    }
+
     @Test
     public void parseAmount_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> ParserUtil.parseAmount(null));
