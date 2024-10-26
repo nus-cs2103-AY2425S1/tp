3f0747f0aa2aaab3b19d404fce03d4d433246c9a
nws321 <nws321@gmail.com>
2024-10-27 02:00:11 +0800

    Add test cases for adda and deletea

--- src/test/java/seedu/address/logic/commands/UndoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UndoCommandTest.java
@@ -5,18 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_ADD;
+import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_ADD_APPOINTMENT;
 import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_CLEAR;
 import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_COMMAND_SUCCESS;
 import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_DELETE;
+import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_DELETE_APPOINTMENT;
 import static seedu.address.logic.commands.UndoCommand.MESSAGE_UNDO_EDIT;
+import static seedu.address.testutil.TypicalPastCommands.ADDA_COMMAND_LAST;
 import static seedu.address.testutil.TypicalPastCommands.ADD_COMMAND_LAST;
 import static seedu.address.testutil.TypicalPastCommands.CLEAR_COMMAND_LAST;
+import static seedu.address.testutil.TypicalPastCommands.DELETEA_COMMAND_LAST;
 import static seedu.address.testutil.TypicalPastCommands.DELETE_COMMAND_LAST;
 import static seedu.address.testutil.TypicalPastCommands.EDIT_COMMAND_LAST;
 import static seedu.address.testutil.TypicalPastCommands.EMPTY_COMMAND_LAST;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook2;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +33,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.appointment.Appointment;
 import seedu.address.testutil.TypicalPersons;
 
 public class UndoCommandTest {
@@ -84,6 +92,35 @@ public class UndoCommandTest {
 
         String expectedMessage = String.format(MESSAGE_UNDO_COMMAND_SUCCESS,
                 String.format(MESSAGE_UNDO_EDIT, TypicalPersons.BENSON.getName()));
+        assertCommandSuccess(undoCommand, model, expectedMessage, expectedModel);
+
+    }
+    @Test
+    public void execute_undoAddAppointmentCommand() throws CommandException {
+        ADDA_COMMAND_LAST.getCommandHistory().get(ADDA_COMMAND_LAST.getSize() - 1).execute(model);
+
+        UndoCommand undoCommand = new UndoCommand(ADDA_COMMAND_LAST);
+
+        String expectedMessage = String.format(MESSAGE_UNDO_COMMAND_SUCCESS,
+                String.format(MESSAGE_UNDO_ADD_APPOINTMENT, model.getFilteredAppointmentList().get(0)));
+        assertCommandSuccess(undoCommand, model, expectedMessage, expectedModel);
+
+    }
+
+    @Test
+    public void execute_undoDeleteAppointmentCommand() throws CommandException {
+        Appointment testAppointment = new Appointment(TypicalPersons.BENSON.getName(),
+                LocalDate.of(2024, 12, 31),
+                LocalTime.of(14, 30), LocalTime.of(16, 30));
+        List<Appointment> temp = new ArrayList<>();
+        temp.add(testAppointment);
+        model = new ModelManager(getTypicalAddressBook2(), temp, new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), temp, new UserPrefs());
+        DELETEA_COMMAND_LAST.getCommandHistory().get(DELETEA_COMMAND_LAST.getSize() - 1).execute(model);
+        UndoCommand undoCommand = new UndoCommand(DELETEA_COMMAND_LAST);
+
+        String expectedMessage = String.format(MESSAGE_UNDO_COMMAND_SUCCESS,
+                String.format(MESSAGE_UNDO_DELETE_APPOINTMENT, testAppointment));
         assertCommandSuccess(undoCommand, model, expectedMessage, expectedModel);
 
     }
--- src/test/java/seedu/address/testutil/TypicalPastCommands.java
+++ src/test/java/seedu/address/testutil/TypicalPastCommands.java
@@ -2,14 +2,21 @@ package seedu.address.testutil;
 
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.AddAppointmentCommand;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteAppointmentCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.person.Name;
+
 
 /**
  * A utility class containing a list of past commands to be used in tests.
@@ -24,6 +31,13 @@ public class TypicalPastCommands {
     public static final EditCommand EXAMPLE_EDIT = new EditCommand(INDEX_FIRST_PERSON,
             settingName(NEW_NAME, EDIT_PERSON_DESCRIPTOR));
 
+    public static final AddAppointmentCommand EXAMPLE_ADDA = new AddAppointmentCommand(Index.fromZeroBased(0),
+            LocalDate.of(2024, 12, 31),
+            LocalTime.of(14, 30), LocalTime.of(16, 30));
+
+    public static final DeleteAppointmentCommand EXAMPLE_DELETEA =
+            new DeleteAppointmentCommand(Index.fromZeroBased(0));
+
     public static final CommandHistory EMPTY_COMMAND_LAST = createPastCommands();
 
     public static final CommandHistory CLEAR_COMMAND_LAST =
@@ -37,6 +51,13 @@ public class TypicalPastCommands {
 
     public static final CommandHistory EDIT_COMMAND_LAST =
             createPastCommands(EXAMPLE_CLEAR, EXAMPLE_ADD, EXAMPLE_DELETE, EXAMPLE_EDIT);
+
+    public static final CommandHistory ADDA_COMMAND_LAST =
+            createPastCommands(EXAMPLE_CLEAR, EXAMPLE_ADD, EXAMPLE_DELETE, EXAMPLE_ADDA);
+
+    public static final CommandHistory DELETEA_COMMAND_LAST =
+            createPastCommands(EXAMPLE_CLEAR, EXAMPLE_ADD, EXAMPLE_DELETE, EXAMPLE_DELETEA);
+
 
     private static CommandHistory CURR_PAST_COMMANDS = new CommandHistory();
     /**
