39f6cef8ec82f0623fd0c98c7394cbd3cb5ab52a
gladwintan <gladwintan@hotmail.sg>
2024-10-26 19:05:11 +0800

    Create view to show other attendance on click

--- src/main/java/seedu/address/ui/AttendanceCard.java
+++ src/main/java/seedu/address/ui/AttendanceCard.java
@@ -3,10 +3,16 @@ package seedu.address.ui;
 import java.time.LocalDate;
 import java.time.temporal.IsoFields;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import seedu.address.model.person.Attendance;
 
 /**
@@ -20,9 +26,13 @@ public class AttendanceCard extends UiPart<Region> {
     public final String tutorial;
 
     @FXML
+    private VBox attendanceCard;
+    @FXML
     private Label subject;
     @FXML
     private Label attendance;
+    @FXML
+    private Label otherAttendance;
 
     /**
      * Creates a {@code AttendanceCard} of the given tutorial
@@ -32,13 +42,30 @@ public class AttendanceCard extends UiPart<Region> {
         this.attendanceList = attendanceList;
         this.tutorial = tutorial;
 
+        attendanceCard.setOnMouseClicked((MouseEvent event) -> {
+            otherAttendance.setManaged(!otherAttendance.isManaged());
+            otherAttendance.setVisible(!otherAttendance.isVisible());
+        });
         subject.setText(tutorial);
-        Attendance currentWeekAttendance = getCurrentWeekAttendance();
 
-        if (currentWeekAttendance == null) {
+        setAttendance();
+    }
+
+    private void setAttendance() {
+        Attendance currentWeekAttendance = getCurrentWeekAttendance();
+        if (attendanceList.isEmpty()) {
             attendance.setText(": not attended");
+            otherAttendance.setText("No more attendance to show");
+        } else if (currentWeekAttendance == null) {
+            attendance.setText(": not attended");
+            otherAttendance.setText(formatOtherAttendance(attendanceList));
         } else {
-            attendance.setText(": " + currentWeekAttendance.toDisplayString());
+            attendance.setText(" Attended on: " + currentWeekAttendance.toDisplayString());
+            List<Attendance> otherAttendanceList = attendanceList
+                    .stream()
+                    .filter(attendance -> !attendance.equals(currentWeekAttendance))
+                    .toList();
+            otherAttendance.setText(formatOtherAttendance(otherAttendanceList));
         }
     }
 
@@ -53,4 +80,18 @@ public class AttendanceCard extends UiPart<Region> {
         return null;
     }
 
+    private String formatOtherAttendance(List<Attendance> attendanceList) {
+        if (attendanceList.isEmpty()) {
+            return "No more attendance to show";
+        }
+
+        StringBuilder attendance = new StringBuilder();
+        for (int i = 0; i < attendanceList.size(); i++) {
+            attendance.append(attendanceList.get(i).toDisplayString());
+            if (i != attendanceList.size() - 1) {
+                attendance.append(", ");
+            }
+        }
+        return "Other attendance: " + attendance;
+    }
 }
