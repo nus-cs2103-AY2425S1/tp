58c479085c25e52af0fcf824a7a4689c663fb8ef
Plishh <101051455+Plishh@users.noreply.github.com>
2024-10-26 18:01:57 +0800

    Add Subject to personCard

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -6,14 +6,14 @@ package seedu.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("\\n ");
-    public static final Prefix PREFIX_PHONE = new Prefix("\\p ");
-    public static final Prefix PREFIX_HOURS = new Prefix("\\h ");
-    public static final Prefix PREFIX_EMAIL = new Prefix("\\e ");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("\\a ");
+    public static final Prefix PREFIX_NAME = new Prefix("\\n");
+    public static final Prefix PREFIX_PHONE = new Prefix("\\p");
+    public static final Prefix PREFIX_HOURS = new Prefix("\\h");
+    public static final Prefix PREFIX_EMAIL = new Prefix("\\e");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("\\a");
     public static final Prefix PREFIX_TAG = new Prefix("\\t");
-    public static final Prefix PREFIX_SUBJECT = new Prefix("\\s ");
-    public static final Prefix PREFIX_FILEPATH = new Prefix("\\f ");
+    public static final Prefix PREFIX_SUBJECT = new Prefix("\\s");
+    public static final Prefix PREFIX_FILEPATH = new Prefix("\\f");
 
 
 }
