8742440a15a9af5beced5fd910c02236c6d06bb2
JumpyJay <iuswtric1909@gmail.com>
2024-10-27 22:44:03 +0800

    Fix checkstyles

--- src/test/java/seedu/address/logic/commands/ViewClientCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewClientCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
