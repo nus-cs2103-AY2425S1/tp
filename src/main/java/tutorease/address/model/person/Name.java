fbae34f40dd85fcff2690eeaaf4c63af488f6a48
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-10-27 22:03:52 +0800

    Give customised error message when names contain slashes

--- src/main/java/tutorease/address/model/person/Name.java
+++ src/main/java/tutorease/address/model/person/Name.java
@@ -12,11 +12,14 @@ public class Name {
     public static final String MESSAGE_CONSTRAINTS =
             "Names should only contain alphanumeric characters and spaces, and it should not be blank";
 
+    public static final String MESSAGE_CONSTRAINTS_NO_SLASHES = "Names should not contain slashes ('/').";
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public static final String REGEX_NO_SLASHES = "^[^/]*$";
 
     public final String fullName;
 
@@ -27,6 +30,7 @@ public class Name {
      */
     public Name(String name) {
         requireNonNull(name);
+        checkArgument(hasNoSlash(name), MESSAGE_CONSTRAINTS_NO_SLASHES);
         checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
         fullName = name;
     }
@@ -36,6 +40,13 @@ public class Name {
      */
     public static boolean isValidName(String test) {
         return test.matches(VALIDATION_REGEX);
+    }
+
+    /**
+     * Returns true if a given string does not contain slashes.
+     */
+    public static boolean hasNoSlash(String test) {
+        return test.matches(REGEX_NO_SLASHES);
     }
 
 
