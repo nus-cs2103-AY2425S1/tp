e1f2e52a4591b2e5e17b52ee2673b7c6b07801c4
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-10-27 23:03:18 +0800

    Make names support brackets

--- src/main/java/tutorease/address/model/person/Name.java
+++ src/main/java/tutorease/address/model/person/Name.java
@@ -10,14 +10,14 @@ import static tutorease.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain alphanumeric characters, spaces and brackets, and it should not be blank";
 
     public static final String MESSAGE_CONSTRAINTS_NO_SLASHES = "Names should not contain slashes ('/').";
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}()][\\p{Alnum}() ]*";
 
     public static final String REGEX_NO_SLASHES = "^[^/]*$";
 
