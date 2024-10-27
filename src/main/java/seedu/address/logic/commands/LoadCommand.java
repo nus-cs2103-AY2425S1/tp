7612abda37d28348ce8874fe117bff9e5cfcf3b4
Jianwen <jianwenlei2003@gmail.com>
2024-10-27 15:44:59 +0800

    Update the archive command and load command
    These two commands now can only accept one path input.
    Multiple path provided will results in an error message

--- src/main/java/seedu/address/logic/commands/LoadCommand.java
+++ src/main/java/seedu/address/logic/commands/LoadCommand.java
@@ -16,6 +16,7 @@ public class LoadCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + " " + PREFIX_PATH + "FileName\n"
             + "The file name should be a path of an json file containing an address book "
             + "and not contain any slash `/` \n"
+            + "There should be only one path provided.\n"
             + "Example: " + COMMAND_WORD + " " + PREFIX_PATH + "mybook.json";
     public static final String MESSAGE_SUCCESS = "Loading file from: %1$s";
 
