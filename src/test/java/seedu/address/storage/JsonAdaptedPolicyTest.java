a6fc6ac7bb30854e8e1543e1ed9b3b8c97f2bfc7
Justincjr <jchoojr@hotmail.com>
2024-10-27 22:34:54 +0800

    Fix checkstyle

--- src/test/java/seedu/address/storage/JsonAdaptedPolicyTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPolicyTest.java
@@ -3,7 +3,6 @@ package seedu.address.storage;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
