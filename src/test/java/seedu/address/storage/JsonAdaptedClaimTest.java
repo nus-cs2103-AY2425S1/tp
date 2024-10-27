237906e44a844ed5aed537994921906e10a18494
Justincjr <jchoojr@hotmail.com>
2024-10-27 22:27:31 +0800

    Fix test style check

--- src/test/java/seedu/address/storage/JsonAdaptedClaimTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedClaimTest.java
@@ -1,11 +1,11 @@
 package seedu.address.storage;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.claim.Claim;
 import seedu.address.model.claim.ClaimStatus;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class JsonAdaptedClaimTest {
 
@@ -20,15 +20,15 @@ class JsonAdaptedClaimTest {
         JsonAdaptedClaim adaptedClaim = new JsonAdaptedClaim(VALID_STATUS, VALID_DESCRIPTION);
         Claim claim = adaptedClaim.toModelType();
 
-        assertEquals(ClaimStatus.PENDING, claim.getStatus());
-        assertEquals(VALID_DESCRIPTION, claim.getClaimDescription());
+        Assertions.assertEquals(ClaimStatus.PENDING, claim.getStatus());
+        Assertions.assertEquals(VALID_DESCRIPTION, claim.getClaimDescription());
     }
 
     @Test
     void toModelType_nullStatus_throwsIllegalValueException() {
         JsonAdaptedClaim adaptedClaim = new JsonAdaptedClaim(null, VALID_DESCRIPTION);
 
-        assertThrows(IllegalValueException.class, adaptedClaim::toModelType,
+        Assertions.assertThrows(IllegalValueException.class, adaptedClaim::toModelType,
                 String.format(JsonAdaptedClaim.MISSING_FIELD_MESSAGE_FORMAT,
                         ClaimStatus.class.getSimpleName()));
     }
@@ -37,30 +37,31 @@ class JsonAdaptedClaimTest {
     void toModelType_invalidStatus_throwsIllegalValueException() {
         JsonAdaptedClaim adaptedClaim = new JsonAdaptedClaim(INVALID_STATUS, VALID_DESCRIPTION);
 
-        IllegalValueException exception = assertThrows(IllegalValueException.class, adaptedClaim::toModelType);
-        assertEquals("Invalid claim status: " + INVALID_STATUS, exception.getMessage());
+        IllegalValueException exception = Assertions.assertThrows(IllegalValueException.class,
+                adaptedClaim::toModelType);
+        Assertions.assertEquals("Invalid claim status: " + INVALID_STATUS, exception.getMessage());
     }
 
     @Test
     void toModelType_nullDescription_throwsIllegalValueException() {
         JsonAdaptedClaim adaptedClaim = new JsonAdaptedClaim(VALID_STATUS, null);
 
-        assertThrows(IllegalValueException.class, adaptedClaim::toModelType,
+        Assertions.assertThrows(IllegalValueException.class, adaptedClaim::toModelType,
                 String.format(JsonAdaptedClaim.MISSING_FIELD_MESSAGE_FORMAT, "Description"));
     }
 
     @Test
     void toModelType_invalidDescription_throwsIllegalValueException() {
         JsonAdaptedClaim adaptedClaim = new JsonAdaptedClaim(VALID_STATUS, INVALID_DESCRIPTION);
-
-        IllegalValueException exception = assertThrows(IllegalValueException.class, adaptedClaim::toModelType);
-        assertEquals(Claim.MESSAGE_CONSTRAINTS, exception.getMessage());
+        IllegalValueException exception = Assertions.assertThrows(IllegalValueException.class,
+                adaptedClaim::toModelType);
+        Assertions.assertEquals(Claim.MESSAGE_CONSTRAINTS, exception.getMessage());
     }
     @Test
     void jsonAdaptedClaim_constructorFromClaimObject_correctFieldValues() {
         Claim claim = new Claim(ClaimStatus.PENDING, VALID_DESCRIPTION);
         JsonAdaptedClaim adaptedClaim = new JsonAdaptedClaim(claim);
-        assertEquals(VALID_STATUS, adaptedClaim.getStatus());
-        assertEquals(VALID_DESCRIPTION, adaptedClaim.getDescription());
+        Assertions.assertEquals(VALID_STATUS, adaptedClaim.getStatus());
+        Assertions.assertEquals(VALID_DESCRIPTION, adaptedClaim.getDescription());
     }
 }
