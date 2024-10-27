a1576cca5d0058a3ef1d11e8f454dd329da3a31b
lutfir-cpu <lutfir.rn@gmail.com>
2024-10-28 05:02:18 +0800

    Implement archive, unarchive and listarchive

--- src/main/java/seedu/address/logic/commands/UnarchiveCommand.java
+++ src/main/java/seedu/address/logic/commands/UnarchiveCommand.java
@@ -1,34 +1,34 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
-/**
- * Unpins a person identified using its displayed index.
- */
-public class PinCommand extends Command {
+import java.util.ArrayList;
+import java.util.List;
 
-    public static final String COMMAND_WORD = "pin";
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Archives a person identified using its displayed index.
+ */
+public class ArchiveCommand extends Command {
+
+    public static final String COMMAND_WORD = "archive";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Pins the persons identified by the index numbers used in the displayed persons list.\n"
+            + ": Archives the persons identified by the index numbers used in the displayed persons list.\n"
             + "Parameters: INDEX (one or more, all must be positive integers)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_PIN_PERSON_SUCCESS = "Pinned Person: %1$s";
-    public static final String MESSAGE_PIN_PEOPLE_SUCCESS = "Pinned People: \n%1$s";
+    public static final String MESSAGE_ARCHIVE_PERSON_SUCCESS = "Archived Person: %1$s";
+    public static final String MESSAGE_ARCHIVE_PEOPLE_SUCCESS = "Archived People: \n%1$s";
 
     private final List<Index> targetIndices;
 
-    public PinCommand(List<Index> targetIndices) {
+    public ArchiveCommand(List<Index> targetIndices) {
         this.targetIndices = targetIndices;
     }
 
@@ -37,32 +37,30 @@ public class PinCommand extends Command {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
 
-        List<Person> peopleToPin = new ArrayList<>();
+        List<Person> peopleToArchive = new ArrayList<>();
         for (Index index : targetIndices) {
             if (index.getZeroBased() >= lastShownList.size()) {
                 throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
             }
 
-            Person personToPin = lastShownList.get(index.getZeroBased());
-            peopleToPin.add(personToPin);
+            Person personToArchive = lastShownList.get(index.getZeroBased());
+            peopleToArchive.add(personToArchive);
         }
 
-        assert peopleToPin.size() == targetIndices.size();
+        assert peopleToArchive.size() == targetIndices.size();
 
         List<String> resultMessages = new ArrayList<>();
-        for (Person person : peopleToPin) {
-            model.pinPerson(person);
+        for (Person person : peopleToArchive) {
+            model.archivePerson(person);
             resultMessages.add(Messages.format(person));
         }
 
         assert resultMessages.size() == targetIndices.size();
 
-        model.sortByPin();
-
         if (resultMessages.size() == 1) {
-            return new CommandResult(String.format(MESSAGE_PIN_PERSON_SUCCESS, resultMessages.get(0)));
+            return new CommandResult(String.format(MESSAGE_ARCHIVE_PERSON_SUCCESS, resultMessages.get(0)));
         } else {
-            return new CommandResult(String.format(MESSAGE_PIN_PEOPLE_SUCCESS,
+            return new CommandResult(String.format(MESSAGE_ARCHIVE_PEOPLE_SUCCESS,
                     String.join("\n", resultMessages)));
         }
     }
@@ -74,11 +72,11 @@ public class PinCommand extends Command {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof PinCommand)) {
+        if (!(other instanceof ArchiveCommand)) {
             return false;
         }
 
-        PinCommand otherPinCommand = (PinCommand) other;
-        return targetIndices.equals(otherPinCommand.targetIndices);
+        ArchiveCommand otherArchiveCommand = (ArchiveCommand) other;
+        return targetIndices.equals(otherArchiveCommand.targetIndices);
     }
 }
---
+++ src/main/java/seedu/address/logic/parser/UnarchiveCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ArchiveCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.List;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+/**
+ * Parses input arguments and creates a new PinCommand object
+ */
+public class ArchiveCommandParser implements Parser<ArchiveCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the PinCommand
+     * and returns a PinCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ArchiveCommand parse(String args) throws ParseException {
+        try {
+            List<Index> indices = ParserUtil.parseIndexes(args);
+            return new ArchiveCommand(indices);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ArchiveCommand.MESSAGE_USAGE), pe);
+        }
+    }
+
+}
