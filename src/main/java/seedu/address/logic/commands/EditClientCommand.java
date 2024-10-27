bf47ece19116db04c743f3aa99a7bd99bb352afd
calebyyy <caleb.ng.yy@gmail.com>
2024-10-27 15:32:36 +0800

    Remove print statements

--- src/main/java/seedu/address/logic/commands/EditClientCommand.java
+++ src/main/java/seedu/address/logic/commands/EditClientCommand.java
@@ -112,8 +112,6 @@ public class EditClientCommand extends Command {
             if (carToEdit.equals(editedCar) && isCarEdited) {
                 throw new CommandException(MESSAGE_FIELD_VALUES_SAME);
             }
-            System.out.println(!carToEdit.hasMatchingVrnOrVin(editedCar));
-            System.out.println(model.hasCar(editedCar));
             if ((!carToEdit.hasMatchingVrnAndVin(editedCar)) && model.hasCar(editedCar) && isCarEdited) {
                 throw new CommandException(MESSAGE_DUPLICATE_CAR);
             }
