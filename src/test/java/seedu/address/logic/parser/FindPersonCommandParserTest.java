7bbc50c2af8992ea8cbfacea5143780268acaa78
Rachel Tai Ke Jia <e0969575@u.nus.edu>
2024-10-27 17:10:27 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/parser/FindPersonCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindPersonCommandParserTest.java
@@ -1,10 +1,8 @@
 package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import org.junit.jupiter.api.Test;
 
