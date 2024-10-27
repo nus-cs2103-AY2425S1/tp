4dfa9b75ec04d5b270c9e4e9936627e8e19a85e4
ckaayy <e1122115@u.nus.edu>
2024-10-28 00:59:57 +0800

    Add DeleteClaimsCommand class

---
+++ src/main/java/seedu/address/logic/commands/DeleteClaimsCommand.java
@@ -0,0 +1,131 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLAIM_DESC;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CLAIM_STATUS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_POLICY_TYPE;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.claim.Claim;
+import seedu.address.model.claim.ClaimStatus;
+import seedu.address.model.person.Person;
+import seedu.address.model.policy.Policy;
+import seedu.address.model.policy.PolicySet;
+import seedu.address.model.policy.PolicyType;
+
+/**
+ * Deletes a claim from an existing policy in the address book.
+ */
+public class DeleteClaimsCommand extends Command {
+
+    public static final String COMMAND_WORD = "delete-claim";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes a claim from the person identified by the index number used in the displayed person list. \n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + PREFIX_POLICY_TYPE + "POLICY_TYPE " + PREFIX_CLAIM_STATUS + "CLAIM_STATUS "
+            + PREFIX_CLAIM_DESC + "CLAIM_DESCRIPTION\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + PREFIX_POLICY_TYPE + "health "
+            + PREFIX_CLAIM_STATUS + "PENDING "
+            + PREFIX_CLAIM_DESC + "Hospitalization";
+
+    public static final String MESSAGE_DELETE_CLAIM_SUCCESS = "Claim deleted for policy type '%1$s' of person: %2$s\n\n"
+            + "Deleted Claim Details:\nStatus: %3$s | Description: %4$s";
+    public static final String MESSAGE_NO_CLAIM_FOUND = "No claim matching the specified status "
+            + "and description was found.";
+    public static final String MESSAGE_INVALID_PERSON_INDEX = "The person index provided is invalid.";
+    public static final String MESSAGE_NO_POLICY_OF_TYPE = "No policy of type '%1$s' found for person: %2$s";
+
+    private final Index personIndex;
+    private final PolicyType policyType;
+    private final ClaimStatus claimStatus;
+    private final String claimDescription;
+
+    /**
+     * Creates a DeleteClaimsCommand to delete the specified claim.
+     *
+     * @param personIndex The index of the person in the filtered person list.
+     * @param policyType The type of the policy whose claim is to be deleted.
+     * @param claimStatus The status of the claim.
+     * @param claimDescription The description of the claim.
+     */
+    public DeleteClaimsCommand(Index personIndex, PolicyType policyType, ClaimStatus claimStatus,
+                               String claimDescription) {
+        requireAllNonNull(personIndex, policyType, claimStatus, claimDescription);
+        this.personIndex = personIndex;
+        this.policyType = policyType;
+        this.claimStatus = claimStatus;
+        this.claimDescription = claimDescription;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (personIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(MESSAGE_INVALID_PERSON_INDEX);
+        }
+
+        Person person = lastShownList.get(personIndex.getZeroBased());
+
+        Policy policy = findPolicyByType(person, policyType);
+
+        Claim claimToDelete = new Claim(claimStatus, claimDescription);
+        boolean claimRemoved = policy.getClaimList().remove(claimToDelete);
+
+        if (!claimRemoved) {
+            throw new CommandException(MESSAGE_NO_CLAIM_FOUND);
+        }
+
+        PolicySet updatedPolicySet = new PolicySet();
+        updatedPolicySet.addAll(person.getPolicies());
+
+        Person updatedPerson = new Person(person.getName(), person.getPhone(), person.getEmail(),
+                person.getAddress(), person.getTags(), updatedPolicySet);
+
+        model.setPerson(person, updatedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        String successMessage = String.format(
+                MESSAGE_DELETE_CLAIM_SUCCESS, policyType, person.getName(), claimStatus, claimDescription);
+
+        return new CommandResult(successMessage);
+    }
+
+
+    private Policy findPolicyByType(Person person, PolicyType policyType) throws CommandException {
+        Optional<Policy> policyOptional = person.getPolicies().stream()
+                .filter(policy -> policy.getType().equals(policyType))
+                .findFirst();
+
+        if (policyOptional.isEmpty()) {
+            throw new CommandException(String.format(MESSAGE_NO_POLICY_OF_TYPE, policyType, person.getName()));
+        }
+        return policyOptional.get();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof DeleteClaimsCommand)) {
+            return false;
+        }
+        DeleteClaimsCommand otherCommand = (DeleteClaimsCommand) other;
+        return personIndex.equals(otherCommand.personIndex)
+                && policyType.equals(otherCommand.policyType)
+                && claimStatus.equals(otherCommand.claimStatus)
+                && claimDescription.equalsIgnoreCase(otherCommand.claimDescription);
+    }
+}
