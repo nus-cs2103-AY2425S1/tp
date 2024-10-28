1a63136eff715730d7d1add45eda1d70c1a71dc3
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-10-28 19:42:53 +0800

    Implement deletion of events

---
+++ src/main/java/seedu/address/logic/commands/DeleteEventCommand.java
@@ -0,0 +1,62 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.event.Event;
+
+
+/**
+ * Deletes an event identified using its displayed index from the database.
+ */
+public class DeleteEventCommand extends Command {
+    public static final String COMMAND_WORD = "deleteevent";
+    public static final String MESSAGE_SUCCESS = "Deleted Event: %1$s";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the event identified by the index number used in the displayed event list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    private final Index targetIndex;
+
+    /**
+     * Creates an AddEventCommand to add the specified {@code Event}
+     */
+    public DeleteEventCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Event> lastShownList = model.getFilteredEventList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+        }
+
+        Event eventToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteEvent(eventToDelete);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.formatEvent(eventToDelete)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof DeleteEventCommand)) {
+            return false;
+        }
+
+        DeleteEventCommand otherDeleteEventCommand = (DeleteEventCommand) other;
+        return targetIndex.equals(otherDeleteEventCommand.targetIndex);
+    }
+}
