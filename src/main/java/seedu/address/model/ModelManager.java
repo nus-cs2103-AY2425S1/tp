86e6728fa30709338b9ceb2fb92b706575f9aa5d
rimgik <karon503.cpp@gmail.com>
2024-10-28 14:44:01 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
