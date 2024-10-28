9fa43d6785f665e85cd1640ea5fc55106ee389cc
Shoujin <sugimotoshoujin@gmail.com>
2024-10-28 09:45:21 +0800

    Fix bugs

--- src/main/java/seedu/address/ui/PersonDetails.java
+++ src/main/java/seedu/address/ui/PersonDetails.java
@@ -8,8 +8,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import seedu.address.model.person.Person;
 
-
-
 /**
  * Controller class for displaying a person's details in the UI. Encapsulates
  * all features for the person object, and displays the details line by line.
@@ -25,21 +23,14 @@ public class PersonDetails {
     private FlowPane tags;
     @FXML
     private Label nameLabel;
-
     @FXML
     private Label phoneLabel;
-
     @FXML
     private Label emailLabel;
-
     @FXML
     private Label addressLabel;
-
     @FXML
     private VBox propertyList;
-
-
-
 
     /**
      * Sets the details of the specified {@code Person} in the respective UI labels.
@@ -50,16 +41,18 @@ public class PersonDetails {
     public void setPersonDetails(Person person) {
         nameLabel.setText(person.getName().fullName);
         phoneLabel.setText(person.getPhone().value);
-        emailLabel.setT     ext(person.getEmail().value);
+        emailLabel.setText(person.getEmail().value);
         addressLabel.setText(person.getAddress().value);
         birthdayLabel.setText(person.getBirthday().value.toString());
         remarkLabel.setText(person.getRemark().value);
-        person.ge
+
+
 
         person.getHistory().getHistoryEntries().forEach((date, activities) -> {
             Label historyLabel = new Label(date.toString());
             historyLabel.setStyle("-fx-background-color: #293f3f; -fx-text-fill: #D9B08C; -fx-padding: 5");
             history.getChildren().add(historyLabel);
+
             activities.forEach(entry -> {
                 Label activityLabel = new Label("\t - " + entry);
                 activityLabel.setStyle("-fx-font-size: 1em; -fx-text-fill: #D9B08C; -fx-padding: 2");
@@ -70,12 +63,10 @@ public class PersonDetails {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName)).forEach(tag -> {
                     Label tagLabel = new Label(tag.tagName);
-                    // Check if the tag contains "favourite" and add a style class
                     if (tag.tagName.equalsIgnoreCase("favourite")) {
                         tagLabel.getStyleClass().add("favourite-tag");
                     }
                     tags.getChildren().add(tagLabel);
                 });
-
     }
 }
