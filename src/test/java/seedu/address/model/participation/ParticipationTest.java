9c80e8edcd4c36f543a1be7d6b824ef7702c28a7
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-10-29 00:51:11 +0800

    Add import statement

--- src/test/java/seedu/address/model/participation/ParticipationTest.java
+++ src/test/java/seedu/address/model/participation/ParticipationTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
