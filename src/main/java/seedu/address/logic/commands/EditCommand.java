d75b917a771337d525a3537c1ef6c171898b7fbf
Kevin Pek <kevinpek@37.8.126.129.static.m1net.com.sg>
2024-10-28 12:57:21 +0800

    Remove changes made to EditCommand

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.commands.EditVendorCommand.MESSAGE_EDIT_VENDOR_SUCCESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EVENT;
@@ -9,23 +8,9 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_VENDOR;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_VENDORS;
-
-import java.util.List;
-import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.Messages;
-import seedu.address.logic.commands.EditVendorCommand.EditVendorDescriptor;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-import seedu.address.model.commons.name.Name;
-import seedu.address.model.commons.tag.Tag;
-import seedu.address.model.vendor.Description;
-import seedu.address.model.vendor.Phone;
-import seedu.address.model.vendor.Vendor;
-import seedu.address.storage.JsonSerializableAddressBook;
 
 /**
  * Parent abstract class for edit commands.
@@ -53,7 +38,6 @@ public abstract class EditCommand extends Command {
     public static final String MESSAGE_NOT_EDITED = "At least one valid field to edit must be provided.";
 
     protected final Index index;
-    private EditVendorDescriptor editVendorDescriptor;
 
     /**
      * @param targetIndex in the filtered list to edit
@@ -61,70 +45,6 @@ public abstract class EditCommand extends Command {
     public EditCommand(Index targetIndex) {
         requireNonNull(targetIndex);
         this.index = targetIndex;
-    }
-
-    /**
-     * @param targetIndex in the filtered list to edit
-     * @param editVendorDescriptor
-     */
-    public EditCommand(Index index, EditVendorDescriptor editVendorDescriptor) {
-        requireNonNull(index);
-        requireNonNull(editVendorDescriptor);
-
-        this.index = index;
-        this.editVendorDescriptor = new EditVendorDescriptor(editVendorDescriptor);
-    }
-
-    @Override
-    public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Vendor> lastShownList = model.getFilteredVendorList();
-
-        if (index.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_VENDOR_DISPLAYED_INDEX);
-        }
-
-        Vendor vendorToEdit = lastShownList.get(index.getZeroBased());
-        Vendor editedVendor = createEditedVendor(vendorToEdit, editVendorDescriptor);
-
-        if (!vendorToEdit.isSameVendor(editedVendor) && model.hasVendor(editedVendor)) {
-            throw new CommandException(JsonSerializableAddressBook.MESSAGE_DUPLICATE_VENDOR);
-        }
-
-        model.setVendor(vendorToEdit, editedVendor);
-        model.updateFilteredVendorList(PREDICATE_SHOW_ALL_VENDORS);
-        return new CommandResult(String.format(MESSAGE_EDIT_VENDOR_SUCCESS, Messages.format(editedVendor)));
-    }
-
-    /**
-     * Creates and returns a {@code Vendor} with the details of {@code vendorToEdit}
-     * edited with {@code editVendorDescriptor}.
-     */
-    private static Vendor createEditedVendor(Vendor vendorToEdit, EditVendorDescriptor editVendorDescriptor) {
-        assert vendorToEdit != null;
-
-        Name updatedName = editVendorDescriptor.getName().orElse(vendorToEdit.getName());
-        Phone updatedPhone = editVendorDescriptor.getPhone().orElse(vendorToEdit.getPhone());
-        Description updatedDescription = editVendorDescriptor.getDescription().orElse(vendorToEdit.getDescription());
-        Set<Tag> updatedTags = editVendorDescriptor.getTags().orElse(vendorToEdit.getTags());
-
-        return new Vendor(vendorToEdit.getId(), updatedName, updatedPhone, updatedDescription, updatedTags);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        // instanceof handles nulls
-        if (!(other instanceof EditCommand)) {
-            return false;
-        }
-
-        EditCommand otherEditCommand = (EditCommand) other;
-        return index.equals(otherEditCommand.index)
-                && editVendorDescriptor.equals(otherEditCommand.editVendorDescriptor);
     }
 
     @Override
