2a0badb4dc086c886e1b45ac1eb3d07900c71950
CherngYuen <arrengithub11111@gmail.com>
2024-10-28 10:35:13 +0800

    Add assertions to view command

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -28,6 +28,7 @@ public class ViewCommand extends Command {
 
     public ViewCommand(Nric nric) {
         this.nric = nric;
+        assert nric != null : "Nric should not be null";
     }
 
     @Override
@@ -45,6 +46,7 @@ public class ViewCommand extends Command {
         }
 
         Patient patient = optionalPatient.get();
+        assert patient != null : "Patient should not be null after being found";
 
         return new CommandResult(generateSuccessMessage(patient), null, false, patient,
                 true, false);
@@ -66,6 +68,7 @@ public class ViewCommand extends Command {
     }
 
     private String generateSuccessMessage(Patient patient) {
+        assert patient != null : "Patient should not be null when generating success message";
         return String.format(MESSAGE_VIEW_SUCCESS, patient.getName());
     }
 
