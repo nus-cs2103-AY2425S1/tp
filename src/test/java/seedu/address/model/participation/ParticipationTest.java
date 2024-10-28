c2a95b29d119c7eefe6696d58dfb8ea2291d9cce
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-10-29 00:48:44 +0800

    Reformat import statements

--- src/test/java/seedu/address/model/participation/ParticipationTest.java
+++ src/test/java/seedu/address/model/participation/ParticipationTest.java
@@ -10,7 +10,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 
 class ParticipationTest {
