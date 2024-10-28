bb5a1a286841598406134fe0d81a417af35b74b1
shotnothing <shotnothing@protonmail.ch>
2024-10-29 01:27:49 +0800

    Update sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -26,22 +26,29 @@ public class SampleDataUtil {
         return new Restaurant[] {
             new Restaurant(new Name("Swenswen"), new Phone("87438807"), new Email("sowhen@idk.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), new Rating(null),
-                getTagSet("friends", "$$")),
+                getTagSet("western", "family", "$$")),
             new Restaurant(new Name("Hai shang lao"), new Phone("99272758"), new Email("ilovexjp@ccpno1.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Rating(1),
-                getTagSet("colleagues", "friends", "$$$$")),
+                getTagSet("chinese", "family", "$$$$")),
             new Restaurant(new Name("Mala Hideaway"), new Phone("93210283"), new Email("buyaola@buyaoma.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Rating(2),
-                getTagSet("neighbours", "$")),
+                getTagSet("chinese", "$")),
             new Restaurant(new Name("Heavens Kitchen"), new Phone("91031282"), new Email("idiotsandwich@gr.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Rating(3),
-                getTagSet("family", "$$$")),
+                getTagSet("chinese", "family", "$$$")),
             new Restaurant(new Name("McDonkey"), new Phone("92492021"), new Email("parapapapa@mcdonkey.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), new Rating(4),
-                getTagSet("classmates", "$")),
+                getTagSet("fastfood", "family", "halal", "$")),
             new Restaurant(new Name("Mookantang"), new Phone("92624417"), new Email("moomoo@moomookt.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"), new Rating(5),
-                getTagSet("colleagues", "$$$"))
+                getTagSet("thai", "$$$")),
+
+            new Restaurant(new Name("Pizza Heart"), new Phone("82242099"), new Email("pizza@pheart.com"),
+                new Address("78 Airport Boulevard, #03-01"), new Rating(5),
+                getTagSet("fastfood", "family", "halal", "$$")),
+            new Restaurant(new Name("EightyFive Tampanies"), new Phone("62454523"), new Email("parapapapa@mcdonkey.com"),
+                new Address("Blk 85 Tampines Street, #01-09"), new Rating(1),
+                getTagSet("western", "halal", "$$$$")),
         };
     }
 
