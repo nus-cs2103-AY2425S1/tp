fb6d99430c7b24a7c18495785c6729c7c2c25599
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-28 15:39:48 +0800

    Add JsonAdaptedAttendance test case

--- src/test/java/seedu/address/storage/JsonAdaptedAttendanceTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedAttendanceTest.java
@@ -1,5 +1,18 @@
 package seedu.address.storage;
 
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.Attendance;
+
 public class JsonAdaptedAttendanceTest {
 
+    @Test
+    public void toModelType_invalidAttendance_throwsIllegalValueException() throws Exception {
+        JsonAdaptedAttendance jsonAdaptedAttendance = new JsonAdaptedAttendance("1/1");
+        String expectedMessage = Attendance.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, jsonAdaptedAttendance::toModelType);
+    }
 }
