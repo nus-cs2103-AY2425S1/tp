de8e6a08408c3c08ea6ae2c3d4457355e19e1171
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-28 13:32:28 +0800

    Add JsonAdaptedParticipationTest

---
+++ src/test/java/seedu/address/storage/JsonAdaptedParticipationTest.java
@@ -0,0 +1,85 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedParticipation.MISSING_ADDRESSBOOK_OBJECT_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.MATH;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.AddressBook;
+import seedu.address.model.participation.Participation;
+import seedu.address.model.person.Person;
+import seedu.address.model.tutorial.Tutorial;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class JsonAdaptedParticipationTest {
+
+    @Test
+    public void toModelType_validParticipationDetails_returnsParticipation() throws Exception {
+        Participation participation = new Participation(BENSON, MATH, new ArrayList<>());
+        JsonAdaptedParticipation jsonAdaptedParticipation = new JsonAdaptedParticipation(participation);
+        AddressBookStubWithPersonAndTutorial model = new AddressBookStubWithPersonAndTutorial(BENSON, MATH);
+        assertEquals(jsonAdaptedParticipation.toModelType(model), participation);
+    }
+
+    @Test
+    public void toModelType_missingPerson_throwsIllegalValueException() {
+        Participation participation = new Participation(BENSON, MATH, new ArrayList<>());
+        JsonAdaptedParticipation jsonAdaptedParticipation = new JsonAdaptedParticipation(participation);
+        AddressBookStubWithPersonAndTutorial model = new AddressBookStubWithPersonAndTutorial(null, MATH);
+        String expectedMessage = String.format(
+                MISSING_ADDRESSBOOK_OBJECT_MESSAGE_FORMAT, Person.class.getSimpleName());
+
+        // Using an Anonymous Class to fit the assertThrows method descriptor
+        Executable toModelTypeMethod = () -> jsonAdaptedParticipation.toModelType(model);
+        assertThrows(IllegalValueException.class, expectedMessage, toModelTypeMethod);
+    }
+
+    @Test
+    public void toModelType_missingTutorial_throwsIllegalValueException() {
+        Participation participation = new Participation(BENSON, MATH, new ArrayList<>());
+        JsonAdaptedParticipation jsonAdaptedParticipation = new JsonAdaptedParticipation(participation);
+        AddressBookStubWithPersonAndTutorial model = new AddressBookStubWithPersonAndTutorial(BENSON, null);
+        String expectedMessage = String.format(
+                MISSING_ADDRESSBOOK_OBJECT_MESSAGE_FORMAT, Tutorial.class.getSimpleName());
+
+        // Using an Anonymous Class to fit the assertThrows method descriptor
+        Executable toModelTypeMethod = () -> jsonAdaptedParticipation.toModelType(model);
+        assertThrows(IllegalValueException.class, expectedMessage, toModelTypeMethod);
+    }
+
+    // TODO fix this portion.
+    // Not exactly correct to use it like this, since AddressBook is not an Interface
+    private class AddressBookStubWithPersonAndTutorial extends AddressBook {
+        private final Person person;
+        private final Tutorial tutorial;
+
+        AddressBookStubWithPersonAndTutorial(Person person, Tutorial tutorial) {
+            this.person = person;
+            this.tutorial = tutorial;
+        }
+
+        @Override
+        public ObservableList<Person> getPersonList() {
+            if (person == null) {
+                return FXCollections.observableList(Collections.EMPTY_LIST);
+            }
+            return FXCollections.observableList(Collections.singletonList(person));
+        }
+        @Override
+        public ObservableList<Tutorial> getTutorialList() {
+            if (tutorial == null) {
+                return FXCollections.observableList(Collections.EMPTY_LIST);
+            }
+            return FXCollections.observableList(Collections.singletonList(tutorial));
+        }
+    }
+}
