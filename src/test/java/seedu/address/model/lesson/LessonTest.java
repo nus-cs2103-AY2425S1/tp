7b02309ba16604645342b82b16b641109e00c2db
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-10-28 15:14:26 +0800

    Add testcases for Lesson.java

--- src/test/java/seedu/address/model/lesson/LessonTest.java
+++ src/test/java/seedu/address/model/lesson/LessonTest.java
@@ -1,6 +1,7 @@
 package seedu.address.model.lesson;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.consultation.Date;
 import seedu.address.model.consultation.Time;
 import seedu.address.model.student.Student;
+import seedu.address.model.student.exceptions.StudentNotFoundException;
 import seedu.address.testutil.StudentBuilder;
 
 public class LessonTest {
@@ -46,6 +48,12 @@ public class LessonTest {
     }
 
     @Test
+    public void constructor_studentNotInAttendanceMap_throwsAssertionError() {
+        assertThrows(AssertionError.class, () -> new Lesson(date, time,
+                List.of(student1), Collections.emptyMap()));
+    }
+
+    @Test
     public void getDate_success() {
         Lesson lesson = new Lesson(date, time, Collections.emptyList(), Collections.emptyMap());
         assertEquals(date, lesson.getDate());
@@ -66,6 +74,14 @@ public class LessonTest {
     }
 
     @Test
+    public void getStudents_modifyAttendanceMap_throwsUnsupportedOperationException() {
+        Lesson lesson = new Lesson(date, time, Arrays.asList(student1, student2),
+                Map.of(student1, false, student2, false));
+        Map<Student, Boolean> attendanceMap = lesson.getAttendanceMap();
+        assertThrows(UnsupportedOperationException.class, () -> attendanceMap.put(student1, true));
+    }
+
+    @Test
     public void addStudent_success() {
         Lesson lesson = new Lesson(date, time, Collections.emptyList(), Collections.emptyMap());
         lesson.addStudent(student1);
@@ -73,11 +89,57 @@ public class LessonTest {
     }
 
     @Test
+    public void addStudent_alsoAddToAttendanceMap() {
+        Lesson lesson = new Lesson(date, time, Collections.emptyList(), Collections.emptyMap());
+        lesson.addStudent(student1);
+        assertTrue(lesson.getAttendanceMap().containsKey(student1));
+    }
+
+    @Test
     public void removeStudent_success() {
         Lesson lesson = new Lesson(date, time, Arrays.asList(student1, student2),
                 Map.of(student1, false, student2, false));
         lesson.removeStudent(student1);
-        assertTrue(!lesson.getStudents().contains(student1));
+        assertFalse(lesson.getStudents().contains(student1));
+    }
+
+    @Test
+    public void removeStudent_alsoRemoveFromAttendanceMap() {
+        Lesson lesson = new Lesson(date, time, Arrays.asList(student1, student2),
+                Map.of(student1, false, student2, false));
+        lesson.removeStudent(student1);
+        assertFalse(lesson.getAttendanceMap().containsKey(student1));
+    }
+
+    @Test
+    public void getAttendance_studentExists_success() {
+        boolean attendance = true;
+        Lesson lesson = new Lesson(date, time, List.of(student1), Map.of(student1, attendance));
+        assertEquals(lesson.getAttendance(student1), attendance);
+    }
+
+    @Test
+    public void getAttendance_studentDoesNotExist_throwsStudentNotFoundException() {
+        boolean attendance = true;
+        Lesson lesson = new Lesson(date, time, List.of(student1), Map.of(student1, attendance));
+        assertFalse(lesson.getStudents().contains(student2));
+        assertThrows(StudentNotFoundException.class, () -> lesson.getAttendance(student2));
+    }
+
+    @Test
+    public void setAttendance_studentExists_success() {
+        Lesson lesson = new Lesson(date, time, List.of(student1), Map.of(student1, false));
+        boolean newAttendance = true;
+        lesson.setAttendance(student1, newAttendance);
+        assertEquals(lesson.getAttendance(student1), newAttendance);
+    }
+
+    @Test
+    public void setAttendance_studentDoesNotExist_throwsStudentNotFoundException() {
+        Lesson lesson = new Lesson(date, time, List.of(student1), Map.of(student1, false));
+        boolean newAttendance = true;
+        assertFalse(lesson.getStudents().contains(student2));
+        assertThrows(StudentNotFoundException.class, () -> lesson.setAttendance(student2, newAttendance));
     }
 
     @Test
