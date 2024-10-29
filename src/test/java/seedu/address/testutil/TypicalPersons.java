378721f4e413f1a752935dc3ac51f875d756a461
verakohh <vrkwjacad@gmail.com>
2024-10-29 14:46:44 +0800

    Update logic manager tests

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -75,7 +75,7 @@ public class TypicalPersons {
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY)
             .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
             .withWorkExp(VALID_WORKEXP_AMY).withTags(VALID_TAG_FRIEND).withUniversity("NUS")
-            .withMajor("Computer Science").withInterests("Swimming", "Reading").build();
+            .withMajor("Computer Science").withInterests("Reading").build();
 
     public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB)
             .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
