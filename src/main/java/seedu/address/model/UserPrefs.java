3ef6b338d98c7aa7700114a2528254f86ce5b2a2
Ignatius Oey <oey.ignatius@gmail.com>
2024-10-29 17:14:15 +0800

    Add listeners for tagColorMap

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 import java.util.Objects;
 
 import seedu.address.commons.core.GuiSettings;
@@ -16,14 +15,6 @@ public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
     private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
-    public Map<String, String> colorTagMap = Map.ofEntries(
-            Map.entry("photographer", "#a5494f"),
-            Map.entry("florist", "#ad7e48"),
-            Map.entry("videographer", "a7a15a"),
-            Map.entry("caterer", "#48864d"),
-            Map.entry("musician", "#4a57ba"),
-            Map.entry("bernice", "#5f388b")
-    );
 
     /**
      * Creates a {@code UserPrefs} with default values.
@@ -64,8 +55,6 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         requireNonNull(addressBookFilePath);
         this.addressBookFilePath = addressBookFilePath;
     }
-
-    public Map<String, String> getColorTagMap() { return colorTagMap; }
 
     @Override
     public boolean equals(Object other) {
