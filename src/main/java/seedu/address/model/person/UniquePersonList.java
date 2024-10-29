75028aa74a6235ff0c513ba1321e150104c8a54e
reginateh <e1312290@u.nus.edu>
2024-10-29 21:16:50 +0800

    Remove changes that should be in another PR

--- src/main/java/seedu/address/model/person/UniquePersonList.java
+++ src/main/java/seedu/address/model/person/UniquePersonList.java
@@ -3,7 +3,6 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -47,7 +46,6 @@ public class UniquePersonList implements Iterable<Person> {
             throw new DuplicatePersonException();
         }
         internalList.add(toAdd);
-        internalList.sort(Comparator.comparing(p -> p.getName().fullName));
     }
 
     /**
