69ae1a587af8190cf5da4c43d7a2d68a6c8ec044
notnotmax <156508404+notnotmax@users.noreply.github.com>
2024-10-29 16:48:57 +0800

    Update test JSON

--- src/test/java/seedu/address/storage/JsonAdaptedLessonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedLessonTest.java
@@ -2,7 +2,6 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
