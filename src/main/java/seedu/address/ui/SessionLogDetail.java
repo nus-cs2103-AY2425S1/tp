34fb7ba5c1f053e227907300114d4bb78dff0c8b
t-leongchuan <156800445+t-leongchuan@users.noreply.github.com>
2024-10-29 16:49:27 +0800

    Fix checkstyle errors for SessionLogDetail

--- src/main/java/seedu/address/ui/SessionLogDetail.java
+++ src/main/java/seedu/address/ui/SessionLogDetail.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.model.log.Log;
 
+/**
+ * A UI component that displays detailed information of a {@code Log}.
+ */
 public class SessionLogDetail extends UiPart<Region> {
 
     private static final String FXML = "SessionLogDetail.fxml";
@@ -15,13 +18,20 @@ public class SessionLogDetail extends UiPart<Region> {
     @FXML
     private Label detailSessionEntry;
 
+    /**
+     * Creates a {@code SessionLogDetail} with the given {@code Log}.
+     */
     public SessionLogDetail(Log log) {
         super(FXML);
         setLogDetails(log);
     }
 
+    /**
+     * Sets the FXML labels with the given {@code Log}
+     */
     public void setLogDetails(Log log) {
         detailDate.setText(log.getAppointmentDate().toString());
         detailSessionEntry.setText(log.toDetailedString());
     }
 }
+
