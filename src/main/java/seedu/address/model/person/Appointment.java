6a2348d8f874670317acee547d97a95cda8fd3fe
Dominic-Khoo <dominickhooyx@gmail.com>
2024-10-29 19:01:22 +0800

    Add popup showing Today's Appointments

--- src/main/java/seedu/address/model/person/Appointment.java
+++ src/main/java/seedu/address/model/person/Appointment.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,15 +9,21 @@ import java.time.format.DateTimeFormatter;
  */
 public class Appointment {
 
+    /** Message constraints that indicate the valid format for appointments. */
     public static final String MESSAGE_CONSTRAINTS = "Appointments should be of the format DD-MM-YYYY HH:MM";
+
+    /** Regex for validating appointment format. */
     public static final String VALIDATION_REGEX =
             "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4}) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$";
+
+    /** Formatter for displaying and parsing appointment date and time. */
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
+    /** The date and time value of the appointment. */
     public final LocalDateTime value;
 
     /**
-     * Constructs an {@code Appointment}.
+     * Constructs an {@code Appointment} with the specified date and time string.
      *
      * @param dateTimeString A valid appointment date string.
      */
@@ -29,17 +36,32 @@ public class Appointment {
     }
 
     /**
-     * Returns true if a given string is a valid appointment date.
+     * Returns true if a given string is a valid appointment date according to {@code VALIDATION_REGEX}.
+     *
+     * @param test The string to be tested for validity as an appointment date.
+     * @return {@code true} if the string is a valid appointment date, otherwise {@code false}.
      */
     public static boolean isValidAppointment(String test) {
         return test.matches(VALIDATION_REGEX);
     }
 
+    /**
+     * Returns a string representation of the appointment date and time in the specified format.
+     *
+     * @return The formatted appointment date and time.
+     */
     @Override
     public String toString() {
         return value.format(FORMATTER);
     }
 
+    /**
+     * Checks if this appointment is equal to another appointment.
+     *
+     * @param other The other object to compare to.
+     * @return {@code true} if the other object is an {@code Appointment} with the same date and time,
+     *         otherwise {@code false}.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -54,9 +76,35 @@ public class Appointment {
         return (value.toString()).equals(otherAppointment.value.toString());
     }
 
+    /**
+     * Returns the hash code of the appointment.
+     *
+     * @return The hash code of the appointment.
+     */
     @Override
     public int hashCode() {
         return value.hashCode();
     }
 
+    /**
+     * Returns the time of the appointment as a {@code LocalDateTime} object.
+     *
+     * @return The appointment time.
+     */
+    public LocalDateTime getTime() {
+        return this.value;
+    }
+
+    /**
+     * Checks if the appointment is on the same date as the specified {@code LocalDate}.
+     *
+     * @param currentDate The date to compare with the appointment date.
+     * @return {@code true} if the appointment is on the same date, otherwise {@code false}.
+     */
+    public boolean isSameDate(LocalDate currentDate) {
+        if (value == null) {
+            return false;
+        }
+        return value.toLocalDate().equals(currentDate);
+    }
 }
--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -35,6 +35,7 @@ public class MainWindow extends UiPart<Stage> {
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
     private ViewWindow viewWindow;
+    private AppointmentPopup appointmentPopup;
 
     @FXML
     private StackPane commandBoxPlaceholder;
@@ -57,17 +58,15 @@ public class MainWindow extends UiPart<Stage> {
     public MainWindow(Stage primaryStage, Logic logic) {
         super(FXML, primaryStage);
 
-        // Set dependencies
         this.primaryStage = primaryStage;
         this.logic = logic;
 
-        // Configure the UI
         setWindowDefaultSize(logic.getGuiSettings());
-
         setAccelerators();
 
         helpWindow = new HelpWindow();
         viewWindow = null;
+        appointmentPopup = null;
     }
 
     public Stage getPrimaryStage() {
@@ -86,19 +85,8 @@ public class MainWindow extends UiPart<Stage> {
         menuItem.setAccelerator(keyCombination);
 
         /*
-         * TODO: the code below can be removed once the bug reported here
+         * Workaround for handling function keys in certain text input controls
          * https://bugs.openjdk.java.net/browse/JDK-8131666
-         * is fixed in later version of SDK.
-         *
-         * According to the bug report, TextInputControl (TextField, TextArea) will
-         * consume function-key events. Because CommandBox contains a TextField, and
-         * ResultDisplay contains a TextArea, thus some accelerators (e.g F1) will
-         * not work when the focus is in them because the key event is consumed by
-         * the TextInputControl(s).
-         *
-         * For now, we add following event filter to capture such key events and open
-         * help window purposely so to support accelerators even when focus is
-         * in CommandBox or ResultDisplay.
          */
         getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
             if (event.getTarget() instanceof TextInputControl && keyCombination.match(event)) {
@@ -123,6 +111,22 @@ public class MainWindow extends UiPart<Stage> {
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
+
+        handleAppointmentPopup();
+    }
+
+    /**
+     * Handles the popup for appointments today.
+     */
+    public void handleAppointmentPopup() {
+        if (appointmentPopup == null) {
+            appointmentPopup = new AppointmentPopup(logic.getFilteredPersonList());
+        }
+        if (!appointmentPopup.isShowing() && appointmentPopup.hasAppointmentsToday()) {
+            appointmentPopup.show();
+        } else {
+            appointmentPopup.hide();
+        }
     }
 
     /**
