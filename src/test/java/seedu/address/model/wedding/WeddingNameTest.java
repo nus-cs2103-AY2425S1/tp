777f2f761e2ff0ea1d8f0a5889d640d0bc51a84e
dovlin <dovlincs01@gmail.com>
2024-10-29 21:06:00 +0800

    Fix style issues

--- src/test/java/seedu/address/model/wedding/WeddingNameTest.java
+++ src/test/java/seedu/address/model/wedding/WeddingNameTest.java
@@ -1,12 +1,12 @@
 package seedu.address.model.wedding;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.person.Name;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.person.Name;
 public class WeddingNameTest {
     @Test
     public void constructor_null_throwsNullPointerException() {
