cfa383ce54d8f2d0d4c28da964bcd00366a49bdf
amitha2210 <amithavuputuri@yahoo.com.sg>
2024-10-29 14:50:28 +0800

    Add class for unmarking attendance

---
+++ src/main/java/seedu/address/logic/parser/UnmarkAttendanceByStudentCommandParser.java
@@ -0,0 +1,42 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.UnmarkAttendanceByStudentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Attendance;
+import seedu.address.model.tutorial.Tutorial;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
+
+/**
+ * Parses input arguments and creates a new UnmarkAttendanceByStudentCommand object.
+ */
+public class UnmarkAttendanceByStudentCommandParser implements Parser<UnmarkAttendanceByStudentCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the UnmarkAttendanceByStudentCommand
+     * and returns an UnmarkAttendanceByStudentCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UnmarkAttendanceByStudentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_ATTENDANCE, PREFIX_TUTORIAL);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException e) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    UnmarkAttendanceByStudentCommand.MESSAGE_USAGE), e);
+        }
+
+        if (argMultimap.getValue(PREFIX_ATTENDANCE).isEmpty() || argMultimap.getValue(PREFIX_TUTORIAL).isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnmarkAttendanceByStudentCommand.MESSAGE_USAGE));
+        }
+
+        Attendance attendance = ParserUtil.parseAttendance(argMultimap.getValue(PREFIX_ATTENDANCE).get());
+        Tutorial tutorial = ParserUtil.pa
+    }
+}
