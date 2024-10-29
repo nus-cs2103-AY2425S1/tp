e2bcd5dc298c135eb4d49dc7adc806dce8ea2fde
leong <leongweizhi03@gmail.com>
2024-10-29 22:15:45 +0800

    Add log message for archive command

--- src/main/java/seedu/address/logic/commands/ArchiveCommand.java
+++ src/main/java/seedu/address/logic/commands/ArchiveCommand.java
@@ -3,9 +3,11 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
+import java.util.logging.*;
 
+import seedu.address.commons.core.*;
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
+import seedu.address.logic.*;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
@@ -34,13 +36,14 @@ public class ArchiveCommand extends Command {
             "This person is already unarchived in the address book.";
 
     private final Index index;
-
     private final boolean isArchive;
+
+    private final Logger logger = LogsCenter.getLogger(LogicManager.class);
 
     private Person personToModify;
 
     /**
-     * @param index of the person in the filtered person list to archive
+     * @param index of the person in the filtered person list to archive/unarchive
      */
     public ArchiveCommand(Index index, boolean isArchive) {
         requireNonNull(index);
@@ -68,13 +71,22 @@ public class ArchiveCommand extends Command {
 
         Person modifiedPerson = modifyPerson(personToModify);
         model.setPerson(personToModify, modifiedPerson);
+        logArchiveInfo(modifiedPerson);
         return generateCommandResult(modifiedPerson);
     }
 
     private CommandResult generateCommandResult(Person modifiedPerson) {
         return new CommandResult(String.format(
                 isArchive ? MESSAGE_ARCHIVE_PERSON_SUCCESS : MESSAGE_UNARCHIVE_PERSON_SUCCESS,
-                modifiedPerson));
+                Messages.format(modifiedPerson)));
+    }
+
+    private void logArchiveInfo(Person modifiedPerson) {
+        logger.info(String.format(
+                "%s %s",
+                isArchive ? "Archived" : "Unarchived",
+                Messages.format(modifiedPerson)
+        ));
     }
 
     private Person modifyPerson(Person personToModify) {
