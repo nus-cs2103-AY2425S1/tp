047c2dbef7859f5dfc6f24dfb804644d27acca55
Rithik <rithiklenin@gmail.com>
2024-10-29 20:20:11 +0800

    Add code to make class functional

--- src/main/java/seedu/address/model/claim/EditClaimDescriptor.java
+++ src/main/java/seedu/address/model/claim/EditClaimDescriptor.java
@@ -1,5 +1,57 @@
 package seedu.address.model.claim;
 
-public class EditClaimDescriptor {
+import java.util.Optional;
 
+public class EditClaimDescriptor {
+    private ClaimStatus status;
+    private String description;
+
+    public EditClaimDescriptor() {}
+
+    public EditClaimDescriptor(EditClaimDescriptor toCopy) {
+        this.status = toCopy.status;
+        this.description = toCopy.description;
+    }
+
+    public boolean isStatusEdited() {
+        return status != null;
+    }
+
+    public boolean isDescriptionEdited() {
+        return description != null;
+    }
+
+    public boolean isAnyFieldEdited() {
+        return isDescriptionEdited() || isStatusEdited();
+    }
+
+    public Optional<ClaimStatus> getStatus() {
+        return Optional.ofNullable(status);
+    }
+
+    public void setStatus(ClaimStatus status) {
+        this.status = status;
+    }
+
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(description);
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof EditClaimDescriptor)) {
+            return false;
+        }
+        EditClaimDescriptor e = (EditClaimDescriptor) other;
+        return getStatus().equals(e.getStatus())
+                && getDescription().equals(e.getDescription());
+    }
 }
+
