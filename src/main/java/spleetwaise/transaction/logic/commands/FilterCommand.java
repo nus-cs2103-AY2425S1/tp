5fbe26ea6930fae6d869a25922f2e0723129138b
Dong Qianbo <pencilate@outlook.com>
2024-10-29 19:03:45 +0800

    Implement FilterCommandPredicate

--- src/main/java/spleetwaise/transaction/logic/commands/FilterCommand.java
+++ src/main/java/spleetwaise/transaction/logic/commands/FilterCommand.java
@@ -1,22 +1,17 @@
 package spleetwaise.transaction.logic.commands;
 
+import static java.util.Objects.requireNonNull;
 import static spleetwaise.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static spleetwaise.transaction.logic.parser.CliSyntax.PREFIX_AMOUNT;
 import static spleetwaise.transaction.logic.parser.CliSyntax.PREFIX_DATE;
 import static spleetwaise.transaction.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 
-import java.util.function.Predicate;
-
 import spleetwaise.address.commons.util.ToStringBuilder;
-import spleetwaise.address.model.person.Person;
 import spleetwaise.commons.logic.commands.Command;
 import spleetwaise.commons.logic.commands.CommandResult;
 import spleetwaise.commons.logic.commands.exceptions.CommandException;
 import spleetwaise.commons.model.CommonModel;
-import spleetwaise.transaction.model.transaction.Amount;
-import spleetwaise.transaction.model.transaction.Date;
-import spleetwaise.transaction.model.transaction.Description;
-import spleetwaise.transaction.model.transaction.Transaction;
+import spleetwaise.transaction.model.FilterCommandPredicate;
 
 /**
  * This class represents a command for filtering transactions.
@@ -43,28 +38,17 @@ public class FilterCommand extends Command {
                     + "At least one of the above filtering criteria is needed\n"
                     + "Example: " + COMMAND_WORD + " " + PREFIX_PHONE + "88888888";
 
-    private final Person contact;
-    private final Amount amount;
-    private final Description description;
-    private final Date date;
+    private final FilterCommandPredicate filterPredicate;
 
     /**
      * Creates an AddCommand to add the specified {@code Transaction}.
      *
-     * @param contact     The person to filter the transaction by.
-     * @param amount      The amount to filter the transaction by.
-     * @param description The description to filter the transaction by.
-     * @param date        The date to filter the transaction by.
+     * @param predicate the predicate to filter the transactions.
      */
-    public FilterCommand(Person contact, Amount amount, Description description, Date date) {
-        if (contact == null && amount == null && description == null && date == null) {
-            throw new NullPointerException();
-        }
+    public FilterCommand(FilterCommandPredicate predicate) {
+        requireNonNull(predicate);
 
-        this.contact = contact;
-        this.amount = amount;
-        this.description = description;
-        this.date = date;
+        filterPredicate = predicate;
     }
 
     /**
@@ -76,28 +60,6 @@ public class FilterCommand extends Command {
     public CommandResult execute() throws CommandException {
         CommonModel model = CommonModel.getInstance();
 
-        Predicate<Transaction> filterPredicate = (txn) -> {
-            if (contact != null && !txn.getPerson().equals(contact)) {
-                return false;
-            }
-
-            if (amount != null && !txn.getAmount().equals(amount)) {
-                return false;
-            }
-
-            if (description != null
-                    && !txn.getDescription().toString().toLowerCase()
-                    .contains(description.toString().toLowerCase())) {
-                return false;
-            }
-
-            if (date != null && !txn.getDate().equals(date)) {
-                return false;
-            }
-
-            return true;
-        };
-
         model.updateFilteredTransactionList(filterPredicate);
         return new CommandResult(MESSAGE_SUCCESS);
     }
@@ -108,25 +70,17 @@ public class FilterCommand extends Command {
             return true;
         }
 
-        if (!(other instanceof FilterCommand)) {
+        if (!(other instanceof FilterCommand otherFilterCommand)) {
             return false;
         }
 
-        FilterCommand otherFilterCommand = (FilterCommand) other;
-        return (contact == null ? otherFilterCommand.contact == null : contact.equals(otherFilterCommand.contact))
-                && (amount == null ? otherFilterCommand.amount == null : amount.equals(otherFilterCommand.amount))
-                && (description == null ? otherFilterCommand.description == null
-                : description.equals(otherFilterCommand.description))
-                && (date == null ? otherFilterCommand.date == null : date.equals(otherFilterCommand.date));
+        return filterPredicate.equals(otherFilterCommand.filterPredicate);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("contact", contact)
-                .add("amount", amount)
-                .add("description", description)
-                .add("date", date)
+                .add("predicate", filterPredicate)
                 .toString();
     }
 }
