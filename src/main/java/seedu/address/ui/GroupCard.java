f2fb5f6d6a06eaa7afcac7aa60da002c7423963f
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-10-30 16:12:02 +0800

    Add GroupCard class

---
+++ src/main/java/seedu/address/ui/GroupCard.java
@@ -0,0 +1,54 @@
+package seedu.address.ui;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Group;
+import seedu.address.model.person.Person;
+
+/**
+ * A UI component that displays information of a {@code Group}.
+ */
+public class GroupCard extends UiPart<Region> {
+
+    private static final String FXML = "GroupListCard.fxml";
+
+    public final Group group;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label groupName;
+    @FXML
+    private Label id;
+
+    @FXML
+    private FlowPane membersPane; // A FlowPane to display members
+
+    /**
+     * Creates a {@code GroupCard} with the given {@code Group} and index to display.
+     */
+    public GroupCard(Group group, int displayedIndex) {
+        super(FXML);
+        this.group = group;
+        id.setText(displayedIndex + ". ");
+        groupName.setText(group.getName());
+
+        // Retrieve the first 3 members of the group
+        List<Person> firstThreeMembers = group.asUnmodifiableObservableList().stream()
+                .limit(3)
+                .collect(Collectors.toList());
+
+        // Display each member in the FlowPane
+        firstThreeMembers.forEach(member -> {
+            Label memberLabel = new Label(member.getName().fullName);
+            membersPane.getChildren().add(memberLabel);
+        });
+    }
+}
