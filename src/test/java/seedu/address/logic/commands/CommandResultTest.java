35f0ed47e88f757e70bc10c0eedc509b5e3daf2b
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-10-30 23:31:01 +0800

    Update tests for CommandResult

--- src/test/java/seedu/address/logic/commands/CommandResultTest.java
+++ src/test/java/seedu/address/logic/commands/CommandResultTest.java
@@ -5,71 +5,107 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 
 import org.junit.jupiter.api.Test;
 
 public class CommandResultTest {
+    private static final CommandResult NORMAL_COMMAND_RESULT = new CommandResult("normal feedback");
+
+    private static final CommandResult VIEW_COMMAND_RESULT = new CommandResult("view feedback",
+            INDEX_SECOND_PERSON, false, false);
+
+    private static final CommandResult HELP_COMMAND_RESULT = new CommandResult("help feedback",
+            CommandResult.NO_INDEX_TO_VIEW, true, false);
+
+    private static final CommandResult EXIT_COMMAND_RESULT = new CommandResult("exit feedback",
+            CommandResult.NO_INDEX_TO_VIEW, false, true);
+
     @Test
     public void equals() {
-        CommandResult commandResult = new CommandResult("feedback");
 
         // same values -> returns true
-        assertTrue(commandResult.equals(new CommandResult("feedback")));
-        assertTrue(commandResult.equals(new CommandResult("feedback",
+        assertTrue(NORMAL_COMMAND_RESULT.equals(new CommandResult("normal feedback")));
+        assertTrue(NORMAL_COMMAND_RESULT.equals(new CommandResult("normal feedback",
                 CommandResult.NO_INDEX_TO_VIEW, false, false)));
 
         // same object -> returns true
-        assertTrue(commandResult.equals(commandResult));
+        assertTrue(NORMAL_COMMAND_RESULT.equals(NORMAL_COMMAND_RESULT));
 
         // null -> returns false
-        assertFalse(commandResult.equals(null));
+        assertFalse(NORMAL_COMMAND_RESULT.equals(null));
 
         // different types -> returns false
-        assertFalse(commandResult.equals(0.5f));
+        assertFalse(NORMAL_COMMAND_RESULT.equals(0.5f));
 
         // different feedbackToUser value -> returns false
-        assertFalse(commandResult.equals(new CommandResult("different")));
+        assertFalse(NORMAL_COMMAND_RESULT.equals(new CommandResult("different feedback")));
 
         // different indexToView value -> returns false
-        assertFalse(commandResult.equals(new CommandResult("feedback",
+        assertFalse(NORMAL_COMMAND_RESULT.equals(new CommandResult("normal feedback",
                 INDEX_FIRST_PERSON, false, false)));
 
         // different showHelp value -> returns false
-        assertFalse(commandResult.equals(new CommandResult("feedback",
+        assertFalse(NORMAL_COMMAND_RESULT.equals(new CommandResult("normal feedback",
                 CommandResult.NO_INDEX_TO_VIEW, true, false)));
 
         // different exit value -> returns false
-        assertFalse(commandResult.equals(new CommandResult("feedback",
+        assertFalse(NORMAL_COMMAND_RESULT.equals(new CommandResult("normal feedback",
                 CommandResult.NO_INDEX_TO_VIEW, false, true)));
     }
 
     @Test
-    public void hashcode() {
-        CommandResult commandResult = new CommandResult("feedback");
+    public void isViewCommand() {
+        assertFalse(NORMAL_COMMAND_RESULT.isViewCommand());
+        assertTrue(VIEW_COMMAND_RESULT.isViewCommand());
+        assertFalse(HELP_COMMAND_RESULT.isViewCommand());
+        assertFalse(EXIT_COMMAND_RESULT.isViewCommand());
+    }
 
+    @Test
+    public void isShowHelp() {
+        assertFalse(NORMAL_COMMAND_RESULT.isShowHelp());
+        assertFalse(VIEW_COMMAND_RESULT.isShowHelp());
+        assertTrue(HELP_COMMAND_RESULT.isShowHelp());
+        assertFalse(EXIT_COMMAND_RESULT.isShowHelp());
+    }
+
+    @Test
+    public void isExit() {
+        assertFalse(NORMAL_COMMAND_RESULT.isExit());
+        assertFalse(VIEW_COMMAND_RESULT.isExit());
+        assertFalse(HELP_COMMAND_RESULT.isExit());
+        assertTrue(EXIT_COMMAND_RESULT.isExit());
+    }
+
+    @Test
+    public void hashcode() {
         // same values -> returns same hashcode
-        assertEquals(commandResult.hashCode(), new CommandResult("feedback").hashCode());
+        assertEquals(NORMAL_COMMAND_RESULT.hashCode(), new CommandResult("normal feedback").hashCode());
 
         // different feedbackToUser value -> returns different hashcode
-        assertNotEquals(commandResult.hashCode(), new CommandResult("different").hashCode());
+        assertNotEquals(NORMAL_COMMAND_RESULT.hashCode(), new CommandResult("different").hashCode());
+
+        // different indexToView value -> returns different hashcode
+        assertNotEquals(NORMAL_COMMAND_RESULT.hashCode(), new CommandResult("normal feedback",
+                INDEX_FIRST_PERSON, false, false).hashCode());
 
         // different showHelp value -> returns different hashcode
-        assertNotEquals(commandResult.hashCode(), new CommandResult("feedback",
+        assertNotEquals(NORMAL_COMMAND_RESULT.hashCode(), new CommandResult("normal feedback",
                 CommandResult.NO_INDEX_TO_VIEW, true, false).hashCode());
 
         // different exit value -> returns different hashcode
-        assertNotEquals(commandResult.hashCode(), new CommandResult("feedback",
+        assertNotEquals(NORMAL_COMMAND_RESULT.hashCode(), new CommandResult("normal feedback",
                 CommandResult.NO_INDEX_TO_VIEW, false, true).hashCode());
     }
 
     @Test
     public void toStringMethod() {
-        CommandResult commandResult = new CommandResult("feedback");
         String expected = CommandResult.class.getCanonicalName()
-                + "{feedbackToUser=" + commandResult.getFeedbackToUser()
-                + ", indexToView=" + commandResult.getIndexToView()
-                + ", showHelp=" + commandResult.isShowHelp()
-                + ", shouldExit=" + commandResult.isExit() + "}";
-        assertEquals(expected, commandResult.toString());
+                + "{feedbackToUser=" + NORMAL_COMMAND_RESULT.getFeedbackToUser()
+                + ", indexToView=" + NORMAL_COMMAND_RESULT.getIndexToView()
+                + ", showHelp=" + NORMAL_COMMAND_RESULT.isShowHelp()
+                + ", shouldExit=" + NORMAL_COMMAND_RESULT.isExit() + "}";
+        assertEquals(expected, NORMAL_COMMAND_RESULT.toString());
     }
 }
