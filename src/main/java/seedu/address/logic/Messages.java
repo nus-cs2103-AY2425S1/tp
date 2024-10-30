28a37d1a62df87a20aa6d0a6813dcd762ac0ce98
BlazeChron <51698874+BlazeChron@users.noreply.github.com>
2024-10-30 10:36:51 +0800

    Remove ParticipationList from Person Tutorial

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -61,8 +61,6 @@ public class Messages {
                 .append(person.getAddress())
                 .append("; Payment: ")
                 .append(person.getPayment())
-                .append("; Attendance: ")
-                .append(person.getParticipation())
                 .append("; Tags: ");
         person.getTags().forEach(builder::append);
         return builder.toString();
@@ -74,9 +72,7 @@ public class Messages {
     public static String formatTutorial(Tutorial tutorial) {
         final StringBuilder builder = new StringBuilder();
         builder.append("Tutorial Class Name: ")
-                .append(tutorial.getSubject())
-                .append("; Participation: ")
-                .append(tutorial.getParticipationList());
+                .append(tutorial.getSubject());
         return builder.toString();
     }
 
