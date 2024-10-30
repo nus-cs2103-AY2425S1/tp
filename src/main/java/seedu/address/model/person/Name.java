305e168aa10ce26e2d3d62cac671541e2114d55c
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-10-31 02:06:26 +0800

    Duplicate detection limitation
    - Initially we can add 2 people with the same name but different case
    - Which is not expected behavior
    - So changed comparator for Name class

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -55,7 +55,7 @@ public class Name {
         }
 
         Name otherName = (Name) other;
-        return fullName.equals(otherName.fullName);
+        return fullName.equalsIgnoreCase(otherName.fullName);
     }
 
     @Override
