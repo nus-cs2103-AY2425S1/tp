1f811e38d2aadea34ecdb9d2a2ae23ab446bf203
sooyitao <sooyitao@gmail.com>
2024-10-30 17:19:22 +0800

    Fix test case

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -90,8 +90,8 @@ public class EditCommand extends Command {
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         //to be moved to notes command
-        NotesWindow notesWindow = new NotesWindow();
-        String newNotes = notesWindow.showNotesWindow(editedPerson);
+        //NotesWindow notesWindow = new NotesWindow();
+        //String newNotes = notesWindow.showNotesWindow(editedPerson);
         return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson)));
     }
 
