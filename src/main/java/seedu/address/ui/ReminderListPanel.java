48f0f9de73e22f0e8cc103f9485a62de9758081d
Jeremy <e1155908@u.nus.edu>
2024-10-30 23:49:42 +0800

    Update ReminderUI
    Link Reminder logic to the UI

--- src/main/java/seedu/address/ui/ReminderListPanel.java
+++ src/main/java/seedu/address/ui/ReminderListPanel.java
@@ -1,51 +1,51 @@
 package seedu.address.ui;
 
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.reminder.Reminder;
 
 /**
  * Panel containing the list of reminders.
  */
 public class ReminderListPanel extends UiPart<Region> {
     private static final String FXML = "ReminderListPanel.fxml";
-    private static final int NUMBER_OF_DUMMY_REMINDERS = 5;
+    private final Logger logger = LogsCenter.getLogger(ReminderListPanel.class);
 
     @FXML
-    private ListView<Integer> reminderListView;
+    private ListView<Reminder> reminderListView;
 
     /**
      * Creates a {@code ReminderListPanel} with the given {@code ObservableList}.
      */
-    public ReminderListPanel() {
+    public ReminderListPanel(ObservableList<Reminder> reminderList) {
         super(FXML);
-        // Create a list of integers from 1 to NUMBER_OF_DUMMY_REMINDERS
-        reminderListView.setItems(FXCollections.observableArrayList(
-                IntStream.rangeClosed(1, NUMBER_OF_DUMMY_REMINDERS)
-                        .boxed()
-                        .collect(Collectors.toList())
-        ));
+        reminderListView.setItems(reminderList);
         reminderListView.setCellFactory(listView -> new ReminderListViewCell());
+
     }
 
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Reminder} using a {@code ReminderCard}.
      */
-    class ReminderListViewCell extends ListCell<Integer> {
+    class ReminderListViewCell extends ListCell<Reminder> {
         @Override
-        protected void updateItem(Integer index, boolean empty) {
-            super.updateItem(index, empty);
+        protected void updateItem(Reminder reminder, boolean empty) {
+            super.updateItem(reminder, empty);
 
-            if (empty || index == null) {
+            if (empty || reminder == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new ReminderCard(index).getRoot());
+                setGraphic(new ReminderCard(reminder, getIndex() + 1).getRoot());
             }
         }
     }
