c1150eae6fb3e37c2ba753cc99554e991f4b0904
Andrew22Teoh <teoh.pekhong@gmail.com>
2024-10-30 13:24:10 +0800

    Update exit and help commands with new signature

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -16,6 +16,6 @@ public class HelpCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, false, true, false);
+        return new CommandResult(SHOWING_HELP_MESSAGE, CommandResult.NO_INDEX_TO_VIEW, true, false);
     }
 }
