240ac141618ea213786a01c206ff45bb8a6600df
SViswa02 <sviswa2002@gmail.com>
2024-10-30 11:57:32 +0800

    Edit UI and user guide for easier reference

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -187,8 +187,8 @@ public class ParserUtil {
 
         try {
             int parsedWeek = Integer.parseInt(trimmedWeek);
-            if (parsedWeek < 0) {
-                throw new ParseException("Week number must be a non-negative integer.");
+            if (parsedWeek <= 0) {
+                throw new ParseException("Week number must be a positive integer.");
             }
             if (parsedWeek > MAX_WEEK) {
                 throw new ParseException("Week number cannot exceed 13.");
