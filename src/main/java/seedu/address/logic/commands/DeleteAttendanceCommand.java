670d609571e43663ce10906dc611f0c800035a1a
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-10-30 21:30:47 +0800

    Add Duplicate handling for getat

--- src/main/java/seedu/address/logic/commands/DeleteAttendanceCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteAttendanceCommand.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.time.format.DateTimeFormatter;
 
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -11,6 +11,9 @@ import seedu.address.model.Model;
 import seedu.address.model.attendance.Attendance;
 import seedu.address.model.person.Name;
 import seedu.address.model.student.Student;
+import seedu.address.model.student.StudentNumber;
+
+import static seedu.address.logic.parser.CliSyntax.*;
 
 /**
  * Deletes the attendance of a student for a specific date.
@@ -23,15 +26,25 @@ public class DeleteAttendanceCommand extends Command {
             + ": Deletes the attendance of a student for a specific date.\n"
             + "Parameters: "
             + PREFIX_NAME + "NAME "
-            + PREFIX_DATE + "DATE\n"
+            + PREFIX_DATE + "DATE "
+            + PREFIX_STUDENT_NUMBER + "STUDENT NUMBER (Optional)\n"
             + "Example: "
             + COMMAND_WORD + " "
-            + PREFIX_NAME + " John Doe "
-            + PREFIX_DATE + "2019-10-09 ";
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_DATE + "2019-10-09 "
+            + PREFIX_STUDENT_NUMBER + "A1234567B";
 
     public static final String MESSAGE_SUCCESS = "Attendance deleted: %1$s on %2$s";
+
+    public static final String MESSAGE_DUPLICATE_STUDENT = "There is more than 1 student of the same name.\n"
+            + "Their student numbers are as follows: %s" + "\n"
+            + "Use the following command: " + COMMAND_WORD + " " + PREFIX_NAME + "%s "
+            + PREFIX_STUDENT_NUMBER + "STUDENT NUMBER "
+            + PREFIX_DATE + "DATE "
+            + "to delete the Attendance for the correct Student.";
     private final Name name;
     private final LocalDate date;
+    private final Optional<StudentNumber> studentNumber;
     private Attendance previousAttendance;
 
     /**
@@ -39,18 +52,39 @@ public class DeleteAttendanceCommand extends Command {
      * @param name The name of the student.
      * @param date The date for which to delete attendance.
      */
-    public DeleteAttendanceCommand(Name name, LocalDate date) {
+    public DeleteAttendanceCommand(Name name, LocalDate date, Optional<StudentNumber> studentNumber) {
         this.name = name;
         this.date = date;
+        this.studentNumber = studentNumber;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        Student student = model.getStudentByName(name);
-        if (student == null) {
+        List<Student> studentList = model.getAllStudentsByName(name);
+
+        if (studentList.isEmpty()) {
             throw new CommandException("Student not found: " + name);
         }
 
+        Student student;
+        if (studentNumber.isPresent()) {
+            List<Student> filteredStudentList = studentList.stream()
+                    .filter(s -> s.getStudentNumber().equals(studentNumber.get()))
+                    .collect(Collectors.toList());
+            if (filteredStudentList.isEmpty()) {
+                throw new CommandException("Student not found: " + name + " with student number: "
+                        + studentNumber.get());
+            }
+            student = filteredStudentList.get(0);
+        } else {
+            if (studentList.size() > 1) {
+                String duplicateStudentNumbers = studentList.stream()
+                        .map(s -> s.getStudentNumber().toString())
+                        .collect(Collectors.joining(", "));
+                throw new CommandException(String.format(MESSAGE_DUPLICATE_STUDENT, duplicateStudentNumbers, name));
+            }
+            student = studentList.get(0);
+        }
         previousAttendance = student.getAttendanceRecord().stream()
                 .filter(record -> record.getDate().equals(date))
                 .findFirst()
@@ -78,6 +112,21 @@ public class DeleteAttendanceCommand extends Command {
             student.markAttendance(date, previousAttendance.value);
         }
         return true;
+    }
+
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof DeleteAttendanceCommand)) {
+            return false;
+        }
+
+        DeleteAttendanceCommand otherCommand = (DeleteAttendanceCommand) other;
+        return name.equals(otherCommand.name) && date.equals(otherCommand.date);
     }
 
 }
