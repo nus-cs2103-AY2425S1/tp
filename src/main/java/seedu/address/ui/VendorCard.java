953b1b93fceaf1e8913cdecbfa34ffc933ec7200
C5hives <e1122945@u.nus.edu>
2024-10-30 16:57:23 +0800

    Rename address book to EventTory for Ui

--- src/main/java/seedu/address/ui/VendorCard.java
+++ src/main/java/seedu/address/ui/VendorCard.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.Region;
 import seedu.address.model.vendor.Vendor;
 
 /**
- * An UI component that displays information of a {@code Vendor}.
+ * A UI component that displays information of a {@code Vendor}.
  */
 public class VendorCard extends UiPart<Region> {
 
