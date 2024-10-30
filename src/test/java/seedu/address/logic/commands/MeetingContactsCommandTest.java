ed901067793ac23a72b5026025a4e2692965030b
LimKaiWei <E1122652@u.nus.edu>
2024-10-31 01:17:15 +0800

    Fix test coverage gaps previous commit and jsonadpatedmeeting class

--- src/test/java/seedu/address/logic/commands/MeetingContactsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MeetingContactsCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MEETING;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_MEETING;
 import static seedu.address.testutil.TypicalMeetings.getTypicalMeetings;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -67,6 +68,36 @@ public class MeetingContactsCommandTest {
 
         // command failure meeting has no contact uid.
         assertThrows(AssertionError.class, () -> meetingContactsCommand.execute(modelAnother));
+    }
+
+    @Test
+    public void equals() {
+        MeetingContactsCommand meetingContactsCommand =
+            new MeetingContactsCommand(INDEX_FIRST_MEETING);
+        // same object -> returns true
+        assert(meetingContactsCommand.equals(meetingContactsCommand));
+        // same values -> returns true
+        MeetingContactsCommand meetingContactsCommandCopy =
+            new MeetingContactsCommand(INDEX_FIRST_MEETING);
+        assert(meetingContactsCommand.equals(meetingContactsCommandCopy));
+        // different types -> returns false
+        assert(!meetingContactsCommand.equals(1));
+        // null -> returns false
+        assert(!meetingContactsCommand.equals(null));
+        // different index -> returns false
+        MeetingContactsCommand meetingContactsCommandDifferentIndex =
+            new MeetingContactsCommand(INDEX_SECOND_MEETING);
+        assert(!meetingContactsCommand.equals(meetingContactsCommandDifferentIndex));
+        assert(!meetingContactsCommand.equals("aaaa"));
+    }
+
+    @Test
+    public void toStringTest() {
+        MeetingContactsCommand meetingContactsCommand =
+                new MeetingContactsCommand(INDEX_FIRST_MEETING);
+        assert(meetingContactsCommand.toString()
+                .equals("MeetingContactsCommand{targetMeetingIndex="
+                + "seedu.address.commons.core.index.Index{zeroBasedIndex=0}}"));
     }
 
     public class IndexStub extends Index {
