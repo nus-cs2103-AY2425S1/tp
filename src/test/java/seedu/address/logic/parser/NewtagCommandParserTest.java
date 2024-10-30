3289d476667309084eb2a86c76b56a0d4c9e00bb
jia xin <jjiiaaxxiinn@gmail.com>
2024-10-30 16:19:47 +0800

    Make NewtagCommandParser tests neater

--- src/test/java/seedu/address/logic/parser/NewtagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/NewtagCommandParserTest.java
@@ -18,53 +18,34 @@ public class NewtagCommandParserTest {
 
     private NewtagCommandParser parser = new NewtagCommandParser();
 
+    /**
+     * EP: Single valid lowercase argument.
+     */
     @Test
     public void parse_validArgs_returnsNewtagCommand() {
         Tag expectedTag = TypicalTags.BRIDES_SIDE;
-        List<Tag> expectedTags = new ArrayList<>();
-        expectedTags.add(expectedTag);
+        List<Tag> expectedTags = List.of(expectedTag);
         assertParseSuccess(parser, " t/bride's side", new NewtagCommand(expectedTags));
+    }
+
+    @Test
+    public void parse_maximumLengthArgs_returnsNewtagCommand() {
+        String maxName = "a".repeat(50);
+        Tag maxTag = new Tag(maxName);
+        List<Tag> expectedTags = List.of(maxTag);
+        assertParseSuccess(parser, " t/" + maxName, new NewtagCommand(expectedTags));
     }
 
     @Test
     public void parse_leadingAndTrailingSpaces_returnsNewtagCommand() {
         Tag expectedTag = TypicalTags.BRIDES_SIDE;
-        List<Tag> expectedTags = new ArrayList<>();
-        expectedTags.add(expectedTag);
+        List<Tag> expectedTags = List.of(expectedTag);
         assertParseSuccess(parser, " t/   bride's side   ", new NewtagCommand(expectedTags));
     }
 
-    @Test
-    public void parse_emptyArgs_throwsParseException() {
-        assertParseFailure(parser, " t/    ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-    }
-
-    @Test
-    public void parse_missingPrefix_throwsParseException() {
-        assertParseFailure(parser, "bride's friend", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-    }
-
-    @Test
-    public void parse_exceedsMaxLength_throwsParseException() {
-        String longTag = "a".repeat(51); // 51 characters, exceeding the 50-character limit.
-        assertParseFailure(parser, longTag, String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-    }
-
-    @Test
-    public void parse_invalidCharacters_throwsParseException() {
-        assertParseFailure(parser, ";%<>}{", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-        assertParseFailure(parser, "¡£™", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-        assertParseFailure(parser, "¶¢", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
-    }
-
-    @Test
-    public void parse_caseInsensitiveTag_returnsNewtagCommand() {
-        Tag expectedTag = new Tag("friend");
-        List<Tag> expectedTags = new ArrayList<>();
-        expectedTags.add(expectedTag);
-        assertParseSuccess(parser, " t/FRIEND", new NewtagCommand(expectedTags));
-    }
-
+    /**
+     * EP: Multiple valid lowercase arguments.
+     */
     @Test
     public void parse_multipleValidArgs_returnsNewtagCommand() {
         Tag tagBridesFriend = TypicalTags.BRIDES_SIDE;
@@ -76,11 +57,19 @@ public class NewtagCommandParserTest {
     }
 
     @Test
-    public void parse_multipleValidArgsWithoutSpace_throwsParseException() {
-        assertParseFailure(parser, " t/bride's sidet/groom's side",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, NewtagCommand.MESSAGE_USAGE));
+    public void parse_multipleMaximumLengthArgs_returnsNewtagCommand() {
+        String maxNameA = "a".repeat(50);
+        String maxNameB = "b".repeat(50);
+        Tag maxTagA = new Tag(maxNameA);
+        Tag maxTagB = new Tag(maxNameB);
+        List<Tag> expectedTags = List.of(maxTagA, maxTagB);
+        assertParseSuccess(parser, " t/" + maxNameA + " t/" + maxNameB,
+                new NewtagCommand(expectedTags));
     }
 
+    /**
+     * EP: Mix of valid and invalid arguments.
+     */
     @Test
     public void parse_multipleArgsOneInvalid_throwsParseException() {
         assertParseFailure(parser, " t/bride's side t/^@%",
@@ -91,5 +80,55 @@ public class NewtagCommandParserTest {
     public void parse_multipleArgsOneExceedsMaxLength_throwsParseException() {
         assertParseFailure(parser, " t/bride's side t/" + "a".repeat(51),
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, NewtagCommand.MESSAGE_USAGE));
+    }
+
+    /**
+     * EP: Single valid uppercase argument (should be case-insensitive).
+     */
+    @Test
+    public void parse_caseInsensitiveTag_returnsNewtagCommand() {
+        Tag expectedTag = new Tag("friend");
+        List<Tag> expectedTags = new ArrayList<>();
+        expectedTags.add(expectedTag);
+        assertParseSuccess(parser, " t/FRIEND", new NewtagCommand(expectedTags));
+    }
+
+    /**
+     * EP: Formatting errors.
+     */
+    @Test
+    public void parse_emptyString_throwsParseException() {
+        assertParseFailure(parser, " ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+    @Test
+    public void parse_emptyArgs_throwsParseException() {
+        assertParseFailure(parser, " t/    ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingPrefix_throwsParseException() {
+        assertParseFailure(parser, "bride's side", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_multipleValidArgsWithoutSpace_throwsParseException() {
+        assertParseFailure(parser, " t/bride's sidet/groom's side",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, NewtagCommand.MESSAGE_USAGE));
+    }
+
+    /**
+     * EP: Invalid tag names.
+     */
+    @Test
+    public void parse_exceedsMaxLength_throwsParseException() {
+        String longTag = "a".repeat(51); // 51 characters, exceeding the 50-character limit.
+        assertParseFailure(parser, longTag, String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidCharacters_throwsParseException() {
+        assertParseFailure(parser, ";%<>}{", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        assertParseFailure(parser, "¡£™", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        assertParseFailure(parser, "¶¢", String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
     }
 }
