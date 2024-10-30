d620a88b55c4bb2ef4f47e968b322b64414f2bf2
YangQF2002 <yangqf18@gmail.com>
2024-10-31 01:58:45 +0800

    Minor updates to command descriptions

--- src/main/java/seedu/address/logic/commands/EditVendorCommand.java
+++ src/main/java/seedu/address/logic/commands/EditVendorCommand.java
@@ -35,7 +35,7 @@ import seedu.address.model.tag.Tag;
 public class EditVendorCommand extends Command {
     public static final String COMMAND_WORD = "edit_vendor";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the vendor identified "
-            + "by the index number used in the displayed guest list. "
+            + "by the index number used in the displayed vendor list. "
             + "Existing values will be overwritten by the input values.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + "[" + PREFIX_NAME + "NAME] "
