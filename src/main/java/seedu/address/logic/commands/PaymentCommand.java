c884f594124e588804ee33489921f38d622cdab4
InvisibLemon <guanpeng2@gmail.com>
2024-10-30 19:52:46 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/PaymentCommand.java
+++ src/main/java/seedu/address/logic/commands/PaymentCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PAID;
 
 import java.util.HashSet;
