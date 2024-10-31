e87ff9bfc2fcae639da868ec90ad98b1e01c3776
rubinnn <e1155263@u.nus.edu>
2024-10-31 10:58:08 +0800

    Update Reminder Tests
    Update Reminder Tests with more testcodes

---
+++ src/test/java/seedu/address/model/reminder/ReminderTest.java
@@ -0,0 +1,64 @@
+package seedu.address.model.reminder;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.model.person.Name;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class ReminderTest {
+
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Reminder(null, ));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidName = "";
+        assertThrows(IllegalArgumentException.class, () -> new Name(invalidName));
+    }
+
+    @Test
+    public void isValidName() {
+        // null name
+        assertThrows(NullPointerException.class, () -> Name.isValidName(null));
+
+        // invalid name
+        assertFalse(Name.isValidName("")); // empty string
+        assertFalse(Name.isValidName(" ")); // spaces only
+        assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
+        assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("12345")); // numbers only
+        assertFalse(Name.isValidName("peter the 2nd")); // contains numbers
+        assertFalse(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names with numbers
+
+        // valid name
+        assertTrue(Name.isValidName("peter jack")); // alphabets only
+        assertTrue(Name.isValidName("Capital Tan")); // with capital letters
+        assertTrue(Name.isValidName("David Roger Jackson Ray Jr")); // long names
+        assertTrue(Name.isValidName("David Roger Jackson Ray Jr (NUS)")); // long names with parenthesis
+    }
+
+    @Test
+    public void equals() {
+        Name name = new Name("Valid Name");
+
+        // same values -> returns true
+        assertTrue(name.equals(new Name("Valid Name")));
+
+        // same object -> returns true
+        assertTrue(name.equals(name));
+
+        // null -> returns false
+        assertFalse(name.equals(null));
+
+        // different types -> returns false
+        assertFalse(name.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(name.equals(new Name("Other Valid Name")));
+    }
+}
