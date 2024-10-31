9615125105edaf588e4ca0debeda47e0a7f81b0b
rayray39 <raynerlimfy@gmail.com>
2024-10-31 23:07:16 +0800

    Correct checkstyle errors

--- src/test/java/seedu/address/model/person/DateOfLastVisitTest.java
+++ src/test/java/seedu/address/model/person/DateOfLastVisitTest.java
@@ -4,9 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 
 public class DateOfLastVisitTest {
 
