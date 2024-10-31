6ed257b77add91d04b94c65cd1b759b1366c7508
Chronoxy <adriannpcc@gmail.com>
2024-10-31 21:21:05 +0800

    Update contacts prefix

--- src/main/java/tutorease/address/logic/parser/CliSyntax.java
+++ src/main/java/tutorease/address/logic/parser/CliSyntax.java
@@ -7,14 +7,14 @@ package tutorease.address.logic.parser;
 public class CliSyntax {
 
     /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("/n");
-    public static final Prefix PREFIX_PHONE = new Prefix("/p");
-    public static final Prefix PREFIX_EMAIL = new Prefix("/e");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("/a");
-    public static final Prefix PREFIX_TAG = new Prefix("/t");
+    public static final Prefix PREFIX_NAME = new Prefix("n/");
+    public static final Prefix PREFIX_PHONE = new Prefix("p/");
+    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
+    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
+    public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_STUDENT_ID = new Prefix("sid/");
     public static final Prefix PREFIX_FEE = new Prefix("f/");
     public static final Prefix PREFIX_START_DATE = new Prefix("d/");
     public static final Prefix PREFIX_DURATION = new Prefix("h/");
-    public static final Prefix PREFIX_ROLE = new Prefix("/r");
+    public static final Prefix PREFIX_ROLE = new Prefix("r/");
 }
