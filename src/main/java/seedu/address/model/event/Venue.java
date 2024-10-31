deb41974e087287d8bfa739b292da16f347d5e67
wrjgold <trexwrj@gmail.com>
2024-11-01 00:21:19 +0800

    Add validation check for Event venue

--- src/main/java/seedu/address/model/event/Venue.java
+++ src/main/java/seedu/address/model/event/Venue.java
@@ -1,10 +1,22 @@
 package seedu.address.model.event;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 /**
  * Represents an Event venue in the address book.
  * Guarantees: details are present and not null, field values are validated.
  */
 public class Venue {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Event venues should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the event venue must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
     private final String eventVenue;
 
     /**
@@ -13,7 +25,16 @@ public class Venue {
      * @param eventVenue A valid venue.
      */
     public Venue(String eventVenue) {
+        requireNonNull(eventVenue);
+        checkArgument(isValidName(eventVenue), MESSAGE_CONSTRAINTS);
         this.eventVenue = eventVenue;
+    }
+
+    /**
+     * Returns true if a given string is a valid event venue.
+     */
+    public static boolean isValidName(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     public String getVenue() {
