2ac9ff263854915dcdd4d7cc458aecccfe5aa959
Tanziyanghenry <e0725822@u.nus.edu>
2024-10-31 17:09:52 +0800

    Change the range of valid dates allowed

--- src/main/java/seedu/address/model/person/AbsentDate.java
+++ src/main/java/seedu/address/model/person/AbsentDate.java
@@ -5,7 +5,6 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 
 /**
  * Represents the date where the student is absent.
@@ -15,8 +14,8 @@ import java.time.format.DateTimeFormatter;
 public class AbsentDate {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "The absent date should be in the format DD-MM-YYYY, must be within the current year, "
-                    + "and must not exceed the current date. Ensure the day is valid for the specified month "
+            "The absent date should be in the format DD-MM-YYYY and must be within the current year. "
+                    + "Ensure the day is valid for the specified month "
                     + "(e.g., February should not exceed 29 days).";
     public static final int CURRENT_YEAR = LocalDate.now().getYear();
     public static final String VALIDATION_REGEX = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19\\d{2}|20[01]\\d|20"
@@ -48,15 +47,14 @@ public class AbsentDate {
         String[] dateParts = absentDate.split("-");
         int day = Integer.parseInt(dateParts[0]);
         int month = Integer.parseInt(dateParts[1]);
+        int year = Integer.parseInt(dateParts[2]);
         YearMonth yearMonth = YearMonth.of(CURRENT_YEAR, month);
         int maxDay = yearMonth.lengthOfMonth();
-        if (day > maxDay) {
+        if (day > maxDay || year != CURRENT_YEAR) {
             return false;
         }
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate parsedDate = LocalDate.parse(absentDate, formatter);
-        return !parsedDate.isAfter(LocalDate.now());
+        return true;
     }
 
     @Override
