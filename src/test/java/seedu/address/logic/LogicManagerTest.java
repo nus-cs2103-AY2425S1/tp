3031801d42567615db2a28ef348c2142fec00d31
Elinengu <Eline.Ngu2003@gmail.com>
2024-10-31 15:16:26 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -31,7 +31,6 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.history.HistoryCommand;
-import seedu.address.model.history.HistoryCommandList;
 import seedu.address.model.person.Person;
 import seedu.address.storage.JsonAddressBookStorage;
 import seedu.address.storage.JsonUserPrefsStorage;
