59215c54eeb76a2320792fe570efe131182a6455
xGladiate <sharonsohxuanhui@hotmail.com>
2024-11-01 00:17:03 +0800

    Resolve checkstyle errors and JUnit issues

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -21,11 +21,9 @@ import java.util.Optional;
 import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.EditCommand.EditDeliveryDescriptor;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.delivery.ItemName;
 import seedu.address.model.tag.Tag;
