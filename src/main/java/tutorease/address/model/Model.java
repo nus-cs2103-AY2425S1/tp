94f0c7f030043686f773c95047b66ca96919eb7b
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-10-31 13:41:57 +0800

    Modify delete contact to delete the lessons associated with the contact

--- src/main/java/tutorease/address/model/Model.java
+++ src/main/java/tutorease/address/model/Model.java
@@ -113,13 +113,12 @@ public interface Model {
      * @return A boolean indicating if the lesson is in the lessons schedule.
      */
     boolean hasLessons(Lesson lesson);
-
     /**
-     * Deletes the lesson at the given index.
+     * Deletes the lesson given the lesson as the inpyt.
      *
-     * @param index The index of the lesson to delete.
+     * @param lesson The lesson to delete.
      */
-    void deleteLesson(int index);
+    void deleteLesson(Lesson lesson);
 
     /**
      * Returns lesson at the specified index.
