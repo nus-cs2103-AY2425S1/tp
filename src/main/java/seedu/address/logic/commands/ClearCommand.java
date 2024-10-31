aade120e09a6658db80312902a3667c7ef182d19
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-10-31 16:24:16 +0800

    Implement Clear Command Popup Window
    - Implemented a popup window for confirmation when user wants to clear
    - Added testing
    - Modified FilterCommand predicate logic
    - Added FilterCommand testing for partial tags match

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -1,9 +1,11 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
+import java.util.Optional;
 
-import seedu.address.model.AddressBook;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import seedu.address.model.Model;
+import seedu.address.ui.AlertFactory;
 
 /**
  * Clears the address book.
@@ -12,12 +14,40 @@ public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String CANCEL_CLEAR = "Clear operation cancelled.";
 
+    private final AlertFactory alertFactory;
+
+    /**
+     * Creates a ClearCommand with a default AlertFactory.
+     */
+    public ClearCommand() {
+        this(new AlertFactory());
+    }
+
+    /**
+     * Creates a ClearCommand with the specified AlertFactory.
+     * This constructor is used for testing purposes.
+     *
+     * @param alertFactory The factory to create alert dialogs.
+     */
+    public ClearCommand(AlertFactory alertFactory) {
+        this.alertFactory = alertFactory;
+    }
 
     @Override
     public CommandResult execute(Model model) {
-        requireNonNull(model);
-        model.setAddressBook(new AddressBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        Alert alert = alertFactory.createAlert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Clear");
+        alert.setHeaderText("Are you sure you want to clear the address book?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            model.setAddressBook(new seedu.address.model.AddressBook());
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            return new CommandResult(CANCEL_CLEAR);
+        }
     }
 }
