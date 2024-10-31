cc007ec7fbc8525d5711ba157f01da17dcf19261
xGladiate <sharonsohxuanhui@hotmail.com>
2024-10-31 23:00:27 +0800

    Add testcase for UnarchiveCommandTest

--- src/test/java/seedu/address/logic/commands/UnarchiveCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UnarchiveCommandTest.java
@@ -3,8 +3,10 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_LIST;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_LIST;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalIndexes.*;
+import static seedu.address.testutil.TypicalIndexes.INDEX_THIRD_LIST;
+import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.ArrayList;
@@ -13,9 +15,14 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.parser.AddressBookParser;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
 
 
 /**
@@ -25,6 +32,29 @@ import seedu.address.model.UserPrefs;
 public class UnarchiveCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_validSingleIndexUnfilteredPersonList_success() {
+        Index lastIndex = Index.fromZeroBased(model.getFilteredPersonList().size());
+        List<Index> lastIndexList = new ArrayList<>();
+        lastIndexList.add(lastIndex);
+
+        Person archivedPerson = new PersonBuilder().withArchive("true").build();
+
+        Person unarchivedPerson = new PersonBuilder().build();
+
+        UnarchiveCommand unarchiveCommand = new UnarchiveCommand(lastIndexList);
+
+        String expectedMessage = String.format(UnarchiveCommand.MESSAGE_UNARCHIVED_PERSON_SUCCESS,
+                "\n" + Messages.format(archivedPerson));
+
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.addPerson(unarchivedPerson);
+
+        AddressBookParser.setInspect(false);
+        model.addPerson(archivedPerson);
+        assertCommandSuccess(unarchiveCommand, model, expectedMessage, expectedModel);
+    }
 
     @Test
     public void equals() {
