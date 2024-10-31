53c4e1231b84a1edc85e39b09650a5fd449f4dc1
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-10-31 11:39:32 +0800

    Fix checkStyle

--- src/main/java/seedu/address/model/person/TagMatchesKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/TagMatchesKeywordsPredicate.java
@@ -3,7 +3,6 @@ package seedu.address.model.person;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.model.tag.Tag;
 
 /**
  * Tests that a {@code Person}'s {@code tag} strictly matches the keywords given.
