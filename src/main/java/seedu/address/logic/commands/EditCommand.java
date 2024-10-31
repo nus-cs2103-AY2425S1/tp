7b8467f352041bbcb35bd1c2e85fd09379a11596
PresleyChew <E1115692@u.nus.edu>
2024-10-31 15:25:52 +0800

    Amend MESSAGEs in the various command classes

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -60,7 +60,7 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Patient: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This patient already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This patient already exists in MediBase3.";
     private static final Logger logger = LogsCenter.getLogger(EditCommand.class);
     private final NricMatchesPredicate predicate;
     private final EditPersonDescriptor editPersonDescriptor;
