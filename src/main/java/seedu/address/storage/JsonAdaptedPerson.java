f4b68cbff92dfdb20299950e2c6cbd657995b565
Mingyang <limingyanglmy10@gmail.com>
2024-11-01 01:35:42 +0800

    Fix checkstyle

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -123,6 +123,10 @@ public class JsonAdaptedPerson {
         return id;
     }
 
+    public void setId() {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
