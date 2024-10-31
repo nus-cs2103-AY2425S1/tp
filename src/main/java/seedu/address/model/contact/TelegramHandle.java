bef8a8987376b1f443a778cb4fb6735c91f2a6e6
Chua Tse Hui <htcylsg@gmail.com>
2024-10-31 10:08:10 +0800

    Rectify comments

--- src/main/java/seedu/address/model/contact/TelegramHandle.java
+++ src/main/java/seedu/address/model/contact/TelegramHandle.java
@@ -4,7 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's telegram handle in the address book.
+ * Represents a Contact's telegram handle in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidTelegramHandle(String)}
  */
 public class TelegramHandle {
