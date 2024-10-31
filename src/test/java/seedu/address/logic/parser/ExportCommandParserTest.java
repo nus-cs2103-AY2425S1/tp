657ca4ef0929f9eb56de57a0a52ee73a091aa359
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-11-01 00:31:02 +0800

    Fix parser test

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -9,30 +9,21 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import seedu.address.logic.commands.ExportCommand;
 
 public class ExportCommandParserTest {
 
-    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "ExportCommandTest");
-    @TempDir
-    public Path testFolder;
     private ExportCommandParser parser = new ExportCommandParser();
 
     @Test
     public void parse_validArgs_returnsExportCommand() throws IOException {
-        Path validPath = TEST_DATA_FOLDER.resolve("validExportFile");
-        Files.deleteIfExists(validPath);
-        File exportFile = validPath.toFile();
-
-        Path afterParsePath = TEST_DATA_FOLDER.resolve("validExportFile.txt");
-        Files.deleteIfExists(afterParsePath);
-        File afterParseExportFile = afterParsePath.toFile();
-
+        Files.deleteIfExists(Path.of("validExportFile"));
+        File exportFile = new File("validExportFile");
+        Files.deleteIfExists(Path.of("validExportFile.txt"));
+        File afterParseExportFile = new File("validExportFile.txt");
         assertParseSuccess(parser, exportFile.toString(), new ExportCommand(afterParseExportFile));
     }
 
