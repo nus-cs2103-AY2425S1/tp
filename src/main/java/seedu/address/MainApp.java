a74d7de3f2bd63b91b43ec988f719082e7b40559
tkaixiang <tkaixiang@gmail.com>
2024-10-31 18:43:26 +0800

    Rename addressbook.json + logger name

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -170,7 +170,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting PROperty " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
