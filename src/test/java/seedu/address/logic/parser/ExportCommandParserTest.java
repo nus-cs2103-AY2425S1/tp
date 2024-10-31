a6cc8b023be3ddb2d34f9c0c9b07a929736e71e8
PrishaVP <127843050+PrishaVP@users.noreply.github.com>
2024-11-01 00:20:53 +0800

    Fix export parser test

--- src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExportCommandParserTest.java
@@ -16,16 +16,13 @@ import seedu.address.logic.commands.ExportCommand;
 
 public class ExportCommandParserTest {
 
-    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "ExportCommandTest");
-    @TempDir
-    public Path testFolder;
     private ExportCommandParser parser = new ExportCommandParser();
 
     @Test
     public void parse_validArgs_returnsExportCommand() {
-        Path validPath = TEST_DATA_FOLDER.resolve("validExportFile");
-        File exportFile = validPath.toFile();
-        assertParseSuccess(parser, exportFile.toString(), new ExportCommand(exportFile));
+        File exportFile = new File("validExportFile");
+        File afterParseExportFile = new File("validExportFile.txt");
+        assertParseSuccess(parser, exportFile.toString(), new ExportCommand(afterParseExportFile));
     }
 
     @Test
