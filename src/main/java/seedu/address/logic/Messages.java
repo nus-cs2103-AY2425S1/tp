fa4a385b59a7f79648c5156ec72b375a0eadcad3
Tan Yi Long <yiiilonggg@gmail.com>
2024-10-31 22:51:42 +0800

    Init relaxation of add command
    Users only need to include one of phone, email,
    address, social media.
    Need to update:
    1. Test cases
    2. UI
    3. User guide

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 
 import seedu.address.logic.parser.Prefix;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.SocialMedia;
 
 /**
  * Container for user visible messages.
@@ -37,14 +38,19 @@ public class Messages {
     public static String format(Person person) {
         final StringBuilder builder = new StringBuilder();
         builder.append(person.getName())
-                .append("; Phone: ")
-                .append(person.getPhone())
-                .append("; Email: ")
-                .append(person.getEmail())
-                .append("; Address: ")
-                .append(person.getAddress())
-                .append("; Tags: ");
-        person.getTags().forEach(builder::append);
+                .append(person.getPhone().value.isEmpty() ? "" : String.format("; Phone: %s", person.getPhone()))
+                .append(person.getEmail().value.isEmpty() ? "" : String.format("; Email: %s", person.getEmail()))
+                .append(person.getAddress().value.isEmpty() ? "" : String.format("; Address: %s", person.getAddress()))
+                .append(person.getSchedule().toString().isEmpty()
+                        ? ""
+                        : String.format("; Schedule: %s", person.getSchedule()))
+                .append(person.getSocialMedia().getPlatform().equals(SocialMedia.Platform.UNNAMED)
+                        ? ""
+                        : String.format("; Social Media: %s", person.getSocialMedia()));
+        if (!person.getTags().isEmpty()) {
+            builder.append("; Tags: ");
+            person.getTags().forEach(builder::append);
+        }
         return builder.toString();
     }
 
