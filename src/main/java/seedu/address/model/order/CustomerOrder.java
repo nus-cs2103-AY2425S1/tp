caf88dc463a7f4fa65fd5c97518b058a906dc98c
Samuel Then <samuelthen@Samuels-MacBook-Pro.local>
2024-11-01 01:14:03 +0800

    Fix style and javadoc for model-order and ui

--- src/main/java/seedu/address/model/order/CustomerOrder.java
+++ src/main/java/seedu/address/model/order/CustomerOrder.java
@@ -1,12 +1,13 @@
 package seedu.address.model.order;
 
-import seedu.address.model.order.OrderStatus;
-import seedu.address.model.person.Person;
-import seedu.address.model.product.Pastry;
-import seedu.address.model.product.Product;
-
 import java.util.List;
 
+import seedu.address.model.person.Person;
+import seedu.address.model.product.Product;
+
+/**
+ * Class representing a customer order.
+ */
 public class CustomerOrder extends Order {
     public CustomerOrder(Person person, List<Product> items, OrderStatus status) {
         super(person, items, status);
