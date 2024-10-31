90d9bba7e11402297b1d3892b95b09fd4440c77b
SeanFoongjt <seanfoongjt@gmail.com>
2024-10-31 19:14:05 +0800

    Extract behaviour from Export to File Access

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -28,16 +28,6 @@ public class ExportCommand extends FileAccessCommand {
     private static final DateTimeFormatter EXPORT_FORMATTER = DateTimeFormatter.ofPattern("MM-dd-yyyy-hhmmssa");
 
     @Override
-    public CommandResult execute(Model model) {
-        // This version of execute should never be run
-        assert false;
-        requireNotExecuted();
-        requireNonNull(model);
-        isExecuted = true;
-        return new CommandResult(MESSAGE_SUCCESS);
-    }
-
-    @Override
     public CommandResult execute(Model model, Storage storage) throws CommandException {
         requireNotExecuted();
         requireNonNull(model);
