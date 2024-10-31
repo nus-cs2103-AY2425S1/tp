331b692a2f33b5fef5336752f875404d5442ad1a
fusin <fusin02@gmail.com>
2024-10-31 23:21:22 +0800

    Change Usage Message and TypicalPersons to reflect context

--- src/main/java/seedu/address/logic/commands/AddPersonCommand.java
+++ src/main/java/seedu/address/logic/commands/AddPersonCommand.java
@@ -31,12 +31,11 @@ public class AddPersonCommand extends AddCommand {
             + PREFIX_ADDRESS + "ADDRESS "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " " + COMMAND_FIELD + " "
-            + PREFIX_NAME + "John Doe "
+            + PREFIX_NAME + "Sydney Sweeney "
             + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_EMAIL + "sydney@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney";
+            + PREFIX_TAG + "Celebrity ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
