8ce9d0b28413187014308a1f37b46f0b0feb5fc0
Rachel <yyxxcairunxin@sina.cn>
2024-11-01 22:58:47 +0800

    Fix ListAttendanceCommandParser user input trim error

--- src/main/java/seedu/address/logic/parser/ListAttendanceCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListAttendanceCommandParser.java
@@ -11,7 +11,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class ListAttendanceCommandParser implements Parser<ListAttendanceCommand> {
     @Override
     public ListAttendanceCommand parse(String userInput) throws ParseException {
-        if (userInput != null) {
+        System.out.println("user input null? " + userInput + "    " + userInput.isEmpty());
+        if (!userInput.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_NO_ADDITIONAL_PARAMS, ListAttendanceCommand.COMMAND_WORD));
         }
         return new ListAttendanceCommand();
