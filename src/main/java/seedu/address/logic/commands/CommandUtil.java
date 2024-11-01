2327e79aba23b412e00a4ff7652de796b6d4df8c
yockcheng <e1121069@u.nus.edu>
2024-11-01 12:10:14 +0800

    Fix sorting bug

--- src/main/java/seedu/address/logic/commands/CommandUtil.java
+++ src/main/java/seedu/address/logic/commands/CommandUtil.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
@@ -33,5 +34,28 @@ public class CommandUtil {
             personList.add(currDisplayedList.get(index.getZeroBased()));
         }
         return personList;
+    }
+
+    /**
+     * Returns a {@code Comparator} that compares {@code Person} objects based on their current
+     * display order in {@code currDisplayedList}. This allows for sorting by the position
+     * in the current provided list.
+     *
+     * @param currDisplayedList the list of persons currently displayed
+     * @return a comparator that orders persons by their index in the list
+     * @throws AssertionError if a {@code Person} is not found in {@code currDisplayedList}
+     */
+    public static Comparator<Person> getComparatorByCurrentIndex(List<Person> currDisplayedList) {
+        assert currDisplayedList != null : "currDisplayedList should not be null";
+        ArrayList<Person> copyOfList = new ArrayList<>(currDisplayedList);
+        return (p1, p2) -> {
+            Integer index1 = copyOfList.stream().filter(p -> p.isSamePerson(p1))
+                    .findFirst().map(copyOfList :: indexOf)
+                    .orElseThrow(() -> new AssertionError("Person 1 not found in the list"));
+            Integer index2 = copyOfList.stream().filter(p -> p.isSamePerson(p2))
+                    .findFirst().map(copyOfList :: indexOf)
+                    .orElseThrow(() -> new AssertionError("Person 2 not found in the list"));
+            return Integer.compare(index1, index2);
+        };
     }
 }
