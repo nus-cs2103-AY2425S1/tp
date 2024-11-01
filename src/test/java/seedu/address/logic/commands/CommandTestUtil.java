0d75d4c0eb4c114769b4a0778e74c30eab386a30
DINESH1201 <dinesh1201@live.com.sg>
2024-11-01 16:56:21 +0800

    Add test cases for addf command

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -40,75 +40,134 @@ public class CommandTestUtil {
     // Valid fields
     public static final String VALID_NAME_AMY = "Amy Bee";
     public static final String VALID_NAME_BOB = "Bob Choo";
+    public static final String VALID_NAME_JAKE = "Jake Tio";
+    public static final String VALID_NAME_KEANU = "Keanu Reeves";
     public static final String VALID_NRIC_AMY = "T0123456A";
     public static final String VALID_NRIC_BOB = "S0123456B";
     public static final String VALID_NRIC_ALICE = "S1234567J";
+    public static final String VALID_NRIC_JAKE = "S9975484H";
+    public static final String VALID_NRIC_KEANU = "S9975483H";
     public static final String VALID_SEX_AMY = "F";
     public static final String VALID_SEX_BOB = "M";
+    public static final String VALID_SEX_JAKE = "M";
+    public static final String VALID_SEX_KEANU = "M";
     public static final String VALID_BIRTHDATE_AMY = "2001-05-10";
     public static final String VALID_BIRTHDATE_BOB = "2003-11-30";
+    public static final String VALID_BIRTHDATE_JAKE = "1972-12-19";
+    public static final String VALID_BIRTHDATE_KEANU = "1997-11-30";
     public static final String VALID_HEALTHSERVICE_VACCINATION = "Vaccination";
     public static final String VALID_HEALTHSERVICE_BLOOD_TEST = "Blood Test";
     public static final String VALID_PHONE_AMY = "91234567";
     public static final String VALID_PHONE_BOB = "81234567";
+    public static final String VALID_PHONE_JAKE = "86512312";
+    public static final String VALID_PHONE_KEANU = "86526969";
     public static final String VALID_EMAIL_AMY = "amybee10@gmail.com";
     public static final String VALID_EMAIL_BOB = "bobchoo30@gmail.com";
+    public static final String VALID_EMAIL_JAKE = "jakejaket@example.com";
+    public static final String VALID_EMAIL_KEANU = "keanureeves@example.com";
     public static final String VALID_ADDRESS_AMY = "8 Sungei Kadut Loop";
-    public static final String VALID_ADDRESS_BOB = "30 Bukit Batok Street 21 ,08-04";
+    public static final String VALID_ADDRESS_BOB = "30 Bukit Batok Street 21, 08-04";
+    public static final String VALID_ADDRESS_JAKE = "Blk 546, Clementi Road, 11-192";
+    public static final String VALID_ADDRESS_KEANU = "Blk 512 Ang Mo Kio Ave 2, 12-14";
     public static final String VALID_BLOODTYPE_AMY = "O-";
     public static final String VALID_BLOODTYPE_BOB = "A+";
+    public static final String VALID_BLOODTYPE_JAKE = "A+";
+    public static final String VALID_BLOODTYPE_KEANU = "O-";
     public static final String VALID_NOKNAME_AMY = "Bumble Bee";
     public static final String VALID_NOKNAME_BOB = "Chu Choo";
+    public static final String VALID_NOKNAME_JAKE = "Jamie Tio";
+    public static final String VALID_NOKNAME_KEANU = "Mila Kunis";
     public static final String VALID_NOKPHONE_AMY = "97654321";
     public static final String VALID_NOKPHONE_BOB = "87654321";
-    public static final String VALID_ALLERGY_AMY = "peanuts, eggs, seafood";
+    public static final String VALID_NOKPHONE_JAKE = "81231234";
+    public static final String VALID_NOKPHONE_KEANU = "84126990";
+    public static final String VALID_ALLERGY_AMY = "peanuts";
     public static final String VALID_ALLERGY_BOB = "ceterizine dihydrochloride";
+    public static final String VALID_ALLERGY_JAKE = "eggs";
+    public static final String VALID_ALLERGY_KEANU = "wheat";
     public static final String VALID_HEALTHRISK_AMY = "HIGH";
     public static final String VALID_HEALTHRISK_BOB = "LOW";
+    public static final String VALID_HEALTHRISK_JAKE = "MEDIUM";
+    public static final String VALID_HEALTHRISK_KEANU = "LOW";
     public static final String VALID_EXISTINGCONDITION_AMY = "diabetes, hypertension, dementia";
     public static final String VALID_EXISTINGCONDITION_BOB = "cancer";
+    public static final String VALID_EXISTINGCONDITION_JAKE = "cancer";
+    public static final String VALID_EXISTINGCONDITION_KEANU = "diabetes";
     public static final String VALID_APPOINTMENT_AMY = "2025-12-10T14:30";
     public static final String VALID_APPOINTMENT_BOB = "2025-12-11T14:00";
     public static final String VALID_NOTE_AMY = "Patient needs extra care";
     public static final String VALID_NOTE_BOB = "Patient is a fall risk";
+    public static final String VALID_NOTE_JAKE = "Patient is quick-tempered";
+    public static final String VALID_NOTE_KEANU = "Patient has previous gunshot wound to chest";
 
     // Valid fields with respective prefixes
     public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
     public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
+    public static final String NAME_DESC_JAKE = " " + PREFIX_NAME + VALID_NAME_JAKE;
+    public static final String NAME_DESC_KEANU = " " + PREFIX_NAME + VALID_NAME_KEANU;
     public static final String NRIC_DESC_AMY = " " + PREFIX_NRIC + VALID_NRIC_AMY;
     public static final String NRIC_DESC_BOB = " " + PREFIX_NRIC + VALID_NRIC_BOB;
+    public static final String NRIC_DESC_JAKE = " " + PREFIX_NRIC + VALID_NRIC_JAKE;
+    public static final String NRIC_DESC_KEANU = " " + PREFIX_NRIC + VALID_NRIC_KEANU;
     public static final String SEX_DESC_AMY = " " + PREFIX_SEX + VALID_SEX_AMY;
     public static final String SEX_DESC_BOB = " " + PREFIX_SEX + VALID_SEX_BOB;
+    public static final String SEX_DESC_JAKE = " " + PREFIX_SEX + VALID_SEX_JAKE;
+    public static final String SEX_DESC_KEANU = " " + PREFIX_SEX + VALID_SEX_KEANU;
     public static final String BIRTHDATE_DESC_AMY = " " + PREFIX_BIRTHDATE + VALID_BIRTHDATE_AMY;
     public static final String BIRTHDATE_DESC_BOB = " " + PREFIX_BIRTHDATE + VALID_BIRTHDATE_BOB;
+    public static final String BIRTHDATE_DESC_JAKE = " " + PREFIX_BIRTHDATE + VALID_BIRTHDATE_JAKE;
+    public static final String BIRTHDATE_DESC_KEANU = " " + PREFIX_BIRTHDATE + VALID_BIRTHDATE_KEANU;
     public static final String HEALTHSERVICE_DESC_VACCINATION = " " + PREFIX_HEALTHSERVICE
             + VALID_HEALTHSERVICE_VACCINATION;
     public static final String HEALTHSERVICE_DESC_BLOOD_TEST = " " + PREFIX_HEALTHSERVICE
             + VALID_HEALTHSERVICE_BLOOD_TEST;
     public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
     public static final String PHONE_DESC_BOB = " " + PREFIX_PHONE + VALID_PHONE_BOB;
+    public static final String PHONE_DESC_JAKE = " " + PREFIX_PHONE + VALID_PHONE_JAKE;
+    public static final String PHONE_DESC_KEANU = " " + PREFIX_PHONE + VALID_PHONE_KEANU;
     public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
     public static final String EMAIL_DESC_BOB = " " + PREFIX_EMAIL + VALID_EMAIL_BOB;
+    public static final String EMAIL_DESC_JAKE = " " + PREFIX_EMAIL + VALID_EMAIL_JAKE;
+    public static final String EMAIL_DESC_KEANU = " " + PREFIX_EMAIL + VALID_EMAIL_KEANU;
     public static final String ADDRESS_DESC_AMY = " " + PREFIX_ADDRESS + VALID_ADDRESS_AMY;
     public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
+    public static final String ADDRESS_DESC_JAKE = " " + PREFIX_ADDRESS + VALID_ADDRESS_JAKE;
+    public static final String ADDRESS_DESC_KEANU = " " + PREFIX_ADDRESS + VALID_ADDRESS_KEANU;
     public static final String BLOODTYPE_DESC_AMY = " " + PREFIX_BLOODTYPE + VALID_BLOODTYPE_AMY;
     public static final String BLOODTYPE_DESC_BOB = " " + PREFIX_BLOODTYPE + VALID_BLOODTYPE_BOB;
+    public static final String BLOODTYPE_DESC_JAKE = " " + PREFIX_BLOODTYPE + VALID_BLOODTYPE_JAKE;
+    public static final String BLOODTYPE_DESC_KEANU = " " + PREFIX_BLOODTYPE + VALID_BLOODTYPE_KEANU;
     public static final String NOKNAME_DESC_AMY = " " + PREFIX_NOKNAME + VALID_NOKNAME_AMY;
     public static final String NOKNAME_DESC_BOB = " " + PREFIX_NOKNAME + VALID_NOKNAME_BOB;
+    public static final String NOKNAME_DESC_JAKE = " " + PREFIX_NOKNAME + VALID_NOKNAME_JAKE;
+    public static final String NOKNAME_DESC_KEANU = " " + PREFIX_NOKNAME + VALID_NOKNAME_KEANU;
     public static final String NOKPHONE_DESC_AMY = " " + PREFIX_NOKPHONE + VALID_NOKPHONE_AMY;
     public static final String NOKPHONE_DESC_BOB = " " + PREFIX_NOKPHONE + VALID_NOKPHONE_BOB;
+    public static final String NOKPHONE_DESC_JAKE = " " + PREFIX_NOKPHONE + VALID_NOKPHONE_JAKE;
+    public static final String NOKPHONE_DESC_KEANU = " " + PREFIX_NOKPHONE + VALID_NOKPHONE_KEANU;
     public static final String ALLERGY_DESC_AMY = " " + PREFIX_ALLERGY + VALID_ALLERGY_AMY;
     public static final String ALLERGY_DESC_BOB = " " + PREFIX_ALLERGY + VALID_ALLERGY_BOB;
+    public static final String ALLERGY_DESC_JAKE = " " + PREFIX_ALLERGY + VALID_ALLERGY_JAKE;
+    public static final String ALLERGY_DESC_KEANU = " " + PREFIX_ALLERGY + VALID_ALLERGY_KEANU;
     public static final String HEALTHRISK_DESC_AMY = " " + PREFIX_HEALTHRISK + VALID_HEALTHRISK_AMY;
     public static final String HEALTHRISK_DESC_BOB = " " + PREFIX_HEALTHRISK + VALID_HEALTHRISK_BOB;
+    public static final String HEALTHRISK_DESC_JAKE = " " + PREFIX_HEALTHRISK + VALID_HEALTHRISK_JAKE;
+    public static final String HEALTHRISK_DESC_KEANU = " " + PREFIX_HEALTHRISK + VALID_HEALTHRISK_KEANU;
     public static final String EXISTINGCONDITION_DESC_AMY = " " + PREFIX_EXISTINGCONDITION
             + VALID_EXISTINGCONDITION_AMY;
     public static final String EXISTINGCONDITION_DESC_BOB = " " + PREFIX_EXISTINGCONDITION
             + VALID_EXISTINGCONDITION_BOB;
+    public static final String EXISTINGCONDITION_DESC_JAKE = " " + PREFIX_EXISTINGCONDITION
+            + VALID_EXISTINGCONDITION_JAKE;
+    public static final String EXISTINGCONDITION_DESC_KEANU = " " + PREFIX_EXISTINGCONDITION
+            + VALID_EXISTINGCONDITION_KEANU;
     public static final String APPOINTMENT_DESC_AMY = " " + PREFIX_APPOINTMENT + VALID_APPOINTMENT_AMY;
     public static final String APPOINTMENT_DESC_BOB = " " + PREFIX_APPOINTMENT + VALID_APPOINTMENT_BOB;
     public static final String NOTE_DESC_AMY = " " + PREFIX_NOTE + VALID_NOTE_AMY;
     public static final String NOTE_DESC_BOB = " " + PREFIX_NOTE + VALID_NOTE_BOB;
+    public static final String NOTE_DESC_JAKE = " " + PREFIX_NOTE + VALID_NOTE_JAKE;
+    public static final String NOTE_DESC_KEANU = " " + PREFIX_NOTE + VALID_NOTE_KEANU;
+
 
     // Invalid fields with respective prefixes
     public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
@@ -119,7 +178,9 @@ public class CommandTestUtil {
     public static final String INVALID_HEALTHSERVICE_DESC = " "
             + PREFIX_HEALTHSERVICE + "*"; // * not allowed for Health Services
     public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "22";
-
+    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "jaker@@gmail.com";
+    public static final String INVALID_BLOODTYPE_DESC = " " + PREFIX_BLOODTYPE + "a-";
+    public static final String INVALID_HEALTHRISK_DESC = " " + PREFIX_HEALTHRISK + "VERY HIGH";
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
 
