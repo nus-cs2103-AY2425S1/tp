01d02e0d821b29da96a8fdf77c4a2292c52ec501
yockcheng <e1121069@u.nus.edu>
2024-11-01 12:34:16 +0800

    Add tests for getComparatorByCurrentIndex

--- src/test/java/seedu/address/logic/commands/CommandUtilTest.java
+++ src/test/java/seedu/address/logic/commands/CommandUtilTest.java
@@ -3,8 +3,12 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.TypicalIndexes.INDEX_ALL;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalPersons.getShuffledTypicalAddressBook;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -17,6 +21,7 @@ import seedu.address.model.person.Person;
 
 public class CommandUtilTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model shuffledModel = new ModelManager(getShuffledTypicalAddressBook(), new UserPrefs());
 
     @Test
     public void filterPersonsByIndex_shouldFilterAll_success() {
@@ -41,5 +46,31 @@ public class CommandUtilTest {
         } catch (CommandException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    public void getComparatorByCurrentIndex_shouldSortByOriginalIndex_success() {
+        List<Person> originalList = this.model.getFilteredPersonList();
+        List<Person> shuffledList = new ArrayList<>(this.shuffledModel.getFilteredPersonList());
+        Comparator<Person> comparator = CommandUtil.getComparatorByCurrentIndex(originalList);
+
+        shuffledList.sort(comparator);
+
+        assertEquals(originalList, shuffledList, "Sorted order should match the original list order.");
+    }
+
+    @Test
+    public void getComparatorByCurrentIndex_unaffectedByOriginalChanges_success() {
+        List<Person> originalList = new ArrayList<>(this.model.getFilteredPersonList());
+        List<Person> originalListCopy = new ArrayList<>(originalList);
+        List<Person> shuffledList = new ArrayList<>(this.shuffledModel.getFilteredPersonList());
+        Comparator<Person> comparator = CommandUtil.getComparatorByCurrentIndex(originalList);
+
+        // changing order of originalList
+        Collections.shuffle(originalList);
+        shuffledList.sort(comparator);
+
+        assertEquals(originalListCopy, shuffledList,
+                "Sorted order should still match the original list order after changing original list.");
     }
 }
