54d5fac48805dbbf1d6db00c1c91f21b34cb09b4
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2024-11-01 13:12:05 +0800

    Rewrite tests to avoid checkstyle violations

--- src/test/java/seedu/address/logic/parser/DeleteEventCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteEventCommandParserTest.java
@@ -6,6 +6,7 @@ import seedu.address.logic.commands.DeleteEventCommand;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EVENT;
 
 /**
@@ -26,6 +27,8 @@ public class DeleteEventCommandParserTest {
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteEventCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT,
+                DeleteEventCommand.MESSAGE_USAGE));
     }
 }
