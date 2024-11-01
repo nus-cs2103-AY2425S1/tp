9bb1b6af23a3e6cda747bf867236e007daa61eee
jankai <android.agent.k@gmail.com>
2024-11-01 12:41:16 +0800

    Add testcase for searchmodecommandparser and remove wrapper class for predicates

--- src/test/java/seedu/address/logic/parser/SearchModeSearchCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SearchModeSearchCommandParserTest.java
@@ -4,14 +4,24 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.searchmode.SearchModeSearchCommand;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.predicates.AddressContainsKeywordsPredicate;
+import seedu.address.model.person.predicates.EmailContainsKeywordsPredicate;
 import seedu.address.model.person.predicates.NameContainsKeywordsPredicate;
-
+import seedu.address.model.person.predicates.PersonIsRolePredicate;
+import seedu.address.model.person.predicates.PhoneNumberContainsKeywordPredicate;
+import seedu.address.model.role.RoleHandler;
+import seedu.address.model.role.exceptions.InvalidRoleException;
 
 
 public class SearchModeSearchCommandParserTest {
@@ -41,5 +51,31 @@ public class SearchModeSearchCommandParserTest {
 
         // multiple keywords with leading and trailing whitespaces
         assertParseSuccess(parser, " n/  Amy Bob  ", expectedCommand);
+    }
+
+    @Test
+    public void parse_allFieldsSpecified_success() {
+
+        SearchModeSearchCommand expectedCommand = null;
+        try {
+            NameContainsKeywordsPredicate namePredicate = new NameContainsKeywordsPredicate(
+                    Collections.singletonList("Amy"));
+            PhoneNumberContainsKeywordPredicate phonePredicate = new PhoneNumberContainsKeywordPredicate(
+                    Collections.singletonList("1234567"));
+            EmailContainsKeywordsPredicate emailPredicate = new EmailContainsKeywordsPredicate(
+                    Collections.singletonList("test@gmail.com"));
+            AddressContainsKeywordsPredicate addressPredicate = new AddressContainsKeywordsPredicate(
+                    new ArrayList<>(Arrays.asList("123", "Road")));
+            PersonIsRolePredicate rolePredicate = new PersonIsRolePredicate(
+                    Collections.singletonList(RoleHandler.getRole("attendee")));
+            Set<Predicate<Person>> predicates = new HashSet<>(Arrays.asList(namePredicate, phonePredicate,
+                    emailPredicate, addressPredicate, rolePredicate));
+            expectedCommand = new SearchModeSearchCommand(predicates);
+        } catch (InvalidRoleException e) {
+            assert(false);
+        }
+
+        assertParseSuccess(parser, " n/Amy p/1234567 e/test@gmail.com"
+                + " r/attendee a/123 Road", expectedCommand);
     }
 }
