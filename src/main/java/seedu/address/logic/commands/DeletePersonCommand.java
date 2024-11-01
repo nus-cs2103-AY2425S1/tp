48ef808fc316afdceb6f76fc2a9cdfd23e78b7a8
fusin <fusin02@gmail.com>
2024-11-01 18:49:02 +0800

    Fix style

--- src/main/java/seedu/address/logic/commands/DeletePersonCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePersonCommand.java
@@ -41,9 +41,11 @@ public class DeletePersonCommand extends DeleteCommand {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
+
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
+
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.clearEventsWithPerson(personToDelete);
         model.clearPersonFromContacts(personToDelete);
