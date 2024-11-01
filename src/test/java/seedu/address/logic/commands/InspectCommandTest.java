c858144196ec93666df15a7ed72d87ff544e2f76
xGladiate <sharonsohxuanhui@hotmail.com>
2024-11-01 16:02:52 +0800

    Add negative testcases for inspect command

--- src/test/java/seedu/address/logic/commands/InspectCommandTest.java
+++ src/test/java/seedu/address/logic/commands/InspectCommandTest.java
@@ -2,11 +2,37 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for
+ * {@code InspectCommand}.
+ */
 public class InspectCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_outOfBoundIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        InspectCommand inspectCommand = new InspectCommand(outOfBoundIndex);
+        String expectedErrorMessage = String.format(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX,
+                outOfBoundIndex.getOneBased());
+
+        assertCommandFailure(inspectCommand, model, expectedErrorMessage);
+    }
+
     @Test
     public void equals() {
         InspectCommand inspectFirstCommand = new InspectCommand(INDEX_FIRST);
