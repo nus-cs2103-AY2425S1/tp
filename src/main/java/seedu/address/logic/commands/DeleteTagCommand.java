5050e742fd8eab0e99e81819c410762a570ca1dd
Chew Tsze Honn <zeonchew0324@gmail.com>
2024-11-01 21:58:23 +0800

    Fix Delete Tag

--- src/main/java/seedu/address/logic/commands/DeleteTagCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTagCommand.java
@@ -7,15 +7,12 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.tag.Tag;
-import seedu.address.ui.UserConfirmation;
 
 /**
- * Deletes specified tags from the tag list and removes them from any persons tagged with these tags.
- * Prompts the user for confirmation if the tags are used to tag existing persons.
+ * Deletes specified tags from the tag list, but only if they are not in use by any persons.
  */
 public class DeleteTagCommand extends Command {
     public static final String COMMAND_WORD = "deletetag";
@@ -26,123 +23,71 @@ public class DeleteTagCommand extends Command {
             + "Parameters: " + PREFIX_TAG + "TAG...\n"
             + "Example: " + COMMAND_WORD + " t/bride's side t/groom's side";
 
-    public static final String MESSAGE_SUCCESS = "Tag(s) deleted.";
+    public static final String MESSAGE_SUCCESS = "Tag(s) deleted successfully.";
+    public static final String MESSAGE_SUCCESS_FORCE = "Tag(s) force deleted successfully.";
+    public static final String MESSAGE_TAGS_IN_USE = "The following tags are in use and cannot be deleted:\n%s\n"
+            + "Use 'deletetag -force' to delete tags that are in use.";
     public static final String MESSAGE_NONEXISTENT = "Some tag(s) provided have not been added before.\n";
-    public static final String MESSAGE_CONFIRMATION = "The following tags are tagged on some guests:\n"
-            + "%s\n"
-            + "Deleting the tags will remove them from the guests.\n"
-            + "Are you sure you want to delete? Click 'OK' to confirm.";
-
-    public static final String MESSAGE_CANCELLED = "Deletion has been cancelled.";
 
     private final List<Tag> tags;
+    private final boolean isForceDelete;
 
     /**
      * Constructs a DeleteTagCommand to delete the specified tags.
      *
      * @param tags The tags to be deleted.
      */
-    public DeleteTagCommand(List<Tag> tags) {
+    public DeleteTagCommand(List<Tag> tags, boolean isForceDelete) {
         requireAllNonNull(tags);
         this.tags = tags;
+        this.isForceDelete = isForceDelete;
     }
 
-    /**
-     * Removes the specified tags from all persons who are tagged with these tags.
-     *
-     * @param model Model that contains the list of persons to remove tags from.
-     */
-    private void removeTagsFromPersons(Model model) {
-        for (Tag tag : tags) {
-            model.removeTagFromPersons(tag);
-        }
-    }
-
-    /**
-     * Creates and returns a CommandResult based on the success of the deletion.
-     *
-     * @param isSuccessful Boolean indicating if the deletion was successful.
-     * @return CommandResult indicating the outcome of the deletion attempt.
-     */
-    private CommandResult createCommandResult(boolean isSuccessful) {
-        if (!isSuccessful) {
-            return new CommandResult(MESSAGE_NONEXISTENT);
-        }
-        return new CommandResult(MESSAGE_SUCCESS);
-    }
-
-    /**
-     * Executes the DeleteTagCommand by deleting the specified tags from the tag list in the model.
-     * If the tags are currently being used to tag any persons, the user is prompted for confirmation.
-     *
-     * @param model Model containing the tags and persons to operate on.
-     * @return CommandResult indicating the result of the command.
-     * @throws CommandException If the user cancels the deletion confirmation.
-     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireAllNonNull(model);
 
-        // Get a set of tags currently being used to tag any Person.
-        Set<Tag> tagsInUse = model.getTagsInUse();
-
-        if (!tagsInUse.isEmpty()) {
-            getUserConfirmation(tagsInUse);
+        for (Tag tag : tags) {
+            if (!model.hasTag(tag)) {
+                return new CommandResult(MESSAGE_NONEXISTENT);
+            }
         }
 
-        boolean isSuccessful = model.deleteTags(tags);
-        removeTagsFromPersons(model);
+        // Proceed with force deletion logic
+        if (isForceDelete) {
+            for (Tag tag : tags) {
+                model.deleteTags(List.of(tag)); // Remove from model tags
+                model.removeTagFromPersons(tag); // Remove from all persons
+            }
+            return new CommandResult(MESSAGE_SUCCESS_FORCE);
+        } else {
+            // Check if any tags are in use
+            Set<Tag> tagsInUse = model.getTagsInUse();
+            Set<Tag> matchingTags = tagsInUse.stream().filter(tag -> tags.contains(tag)).collect(Collectors.toSet());
 
-        return createCommandResult(isSuccessful);
-    }
+            if (!matchingTags.isEmpty()) {
+                return new CommandResult(String.format(MESSAGE_TAGS_IN_USE, tagsInUse));
+            }
 
-    /**
-     * Prompts the user for confirmation before deleting tags that are currently in use by any persons.
-     *
-     * @param tagsInUse Set of tags that are in use by persons in the model.
-     * @throws CommandException If the user chooses to cancel the deletion.
-     */
-    private void getUserConfirmation(Set<Tag> tagsInUse) throws CommandException {
-        String tagsInUseString = tagsInUse.stream()
-                .map(Tag::toString)
-                .collect(Collectors.joining(", "));
 
-        String confirmationMessage = String.format(MESSAGE_CONFIRMATION, tagsInUseString);
-        boolean isConfirmedDeletion = UserConfirmation.getConfirmation(confirmationMessage);
-        if (!isConfirmedDeletion) {
-            throw new CommandException(MESSAGE_CANCELLED);
+            // Standard delete logic
+            for (Tag tag : tags) {
+                model.deleteTags(List.of(tag)); // Just delete from the tag list
+            }
+            return new CommandResult(MESSAGE_SUCCESS);
         }
     }
 
-    /**
-     * Compares this DeleteTagCommand to another object for equality.
-     *
-     * @param other The other object to compare with.
-     * @return True if the other object is a DeleteTagCommand with the same tags.
-     */
     @Override
     public boolean equals(Object other) {
         if (this == other) {
             return true;
         }
-
         if (!(other instanceof DeleteTagCommand)) {
             return false;
         }
-
         DeleteTagCommand otherCommand = (DeleteTagCommand) other;
-        return tags.equals(otherCommand.tags);
-    }
-
-    /**
-     * Returns a string representation of this DeleteTagCommand for debugging purposes.
-     *
-     * @return String representation of the command.
-     */
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .add("tags", tags)
-                .toString();
+        return tags.equals(otherCommand.tags)
+                && isForceDelete == otherCommand.isForceDelete;
     }
 }
--- src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
@@ -2,12 +2,14 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalTags.BRIDES_SIDE;
 import static seedu.address.testutil.TypicalTags.COLLEAGUES;
 import static seedu.address.testutil.TypicalTags.FRIENDS;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.TypicalTags;
 
@@ -34,12 +37,12 @@ public class DeleteTagCommandTest {
      */
     @Test
     public void execute_existingTag_success() {
+        // Create existing tag.
         Tag existingTag = BRIDES_SIDE;
 
-        List<Tag> existingTags = List.of(existingTag);
         model.addTag(existingTag);
 
-        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(existingTags);
+        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(List.of(existingTag), false);
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
 
@@ -58,7 +61,7 @@ public class DeleteTagCommandTest {
         List<Tag> existingTags = List.of(existingFriendsTag, existingColleaguesTag);
         model.addTags(existingTags);
 
-        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(existingTags);
+        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(existingTags, false);
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
 
@@ -71,17 +74,15 @@ public class DeleteTagCommandTest {
      * EP: Single non-existent tag.
      */
     @Test
-    public void execute_nonExistentTag_successWithWarning() {
+    public void executeNonExistentTagWithError() {
         Tag nonExistentTag = BRIDES_SIDE;
         List<Tag> nonExistentTags = List.of(nonExistentTag);
 
-        DeleteTagCommand newTagCommand = new DeleteTagCommand(nonExistentTags);
+        DeleteTagCommand newTagCommand = new DeleteTagCommand(nonExistentTags, false);
         String expectedMessage = DeleteTagCommand.MESSAGE_NONEXISTENT;
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
 
-        // No exception is thrown, but the user will be notified of
-        // non-existent tag(s).
         assertCommandSuccess(newTagCommand, model, expectedMessage, expectedModel);
     }
 
@@ -89,14 +90,14 @@ public class DeleteTagCommandTest {
      * EP: A mix of existing and non-existent tags.
      */
     @Test
-    public void execute_mixedExistingAndNonExistentTags_successWithWarning() {
+    public void execute_mixedExistingAndNonExistentTags_withError() {
         Tag existingTag = TypicalTags.BRIDES_SIDE;
         Tag newColleaguesTag = TypicalTags.COLLEAGUES;
         Tag newFriendsTag = TypicalTags.FRIENDS;
         List<Tag> mixedTags = List.of(newFriendsTag, existingTag, newColleaguesTag);
         model.addTag(existingTag);
 
-        DeleteTagCommand newTagCommand = new DeleteTagCommand(mixedTags);
+        DeleteTagCommand newTagCommand = new DeleteTagCommand(mixedTags, false);
         String expectedMessage = DeleteTagCommand.MESSAGE_NONEXISTENT;
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
@@ -107,15 +108,84 @@ public class DeleteTagCommandTest {
         assertCommandSuccess(newTagCommand, model, expectedMessage, expectedModel);
     }
 
+    /**
+     * EP: Single existing tag with force delete.
+     */
+    @Test
+    public void executeExistingTagForceDeleteSuccess() {
+        // Create existing tag.
+        Tag existingTag = BRIDES_SIDE;
+
+        model.addTag(existingTag);
+
+        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(List.of(existingTag), true);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+        String expectedMessage = DeleteTagCommand.MESSAGE_SUCCESS_FORCE;
+
+        assertCommandSuccess(deleteTagCommand, model, expectedMessage, expectedModel);
+    }
+
+    /**
+     * EP: Attempt to delete tag in use throw error.
+     */
+    @Test
+    public void executeTagInUseWithError() {
+        // Create existing tag.
+        Tag existingTag = BRIDES_SIDE;
+
+        model.addTag(existingTag);
+        model.addPerson(ALICE);
+        model.setPerson(ALICE, new Person(ALICE.getName(), ALICE.getPhone(), ALICE.getEmail(),
+                ALICE.getRsvpStatus(), Set.of(existingTag)));
+
+        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(List.of(existingTag), false);
+
+        String expectedMessage = String.format(DeleteTagCommand.MESSAGE_TAGS_IN_USE, model.getTagsInUse());
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        assertCommandSuccess(deleteTagCommand, model, expectedMessage, expectedModel);
+    }
+
+    /**
+     * EP: Attempt to delete tag in use with force delete.
+     */
+    @Test
+    public void executeTagInUseForceDeleteSuccess() {
+        // Arrange
+        // Set up the existing tag and associate it with ALICE
+        Tag existingTag = TypicalTags.BRIDES_SIDE;
+        Person aliceWithTag = new Person(ALICE.getName(), ALICE.getPhone(), ALICE.getEmail(),
+                ALICE.getRsvpStatus(), Set.of(existingTag));
+
+        model.addPerson(aliceWithTag);
+        model.addTag(existingTag);
+
+        // Create the force delete command for the existing tag
+        DeleteTagCommand deleteTagCommand = new DeleteTagCommand(List.of(existingTag), true);
+
+        // Set up the expected model after force delete
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setPerson(aliceWithTag, new Person(ALICE.getName(), ALICE.getPhone(), ALICE.getEmail(),
+                ALICE.getRsvpStatus(), Set.of())); // ALICE with tag removed
+
+        String expectedMessage = DeleteTagCommand.MESSAGE_SUCCESS_FORCE;
+
+        // Act & Assert
+        assertCommandSuccess(deleteTagCommand, model, expectedMessage, expectedModel);
+    }
+
+
     @Test
     public void equals() {
         List<Tag> firstTags = new ArrayList<>();
         firstTags.add(BRIDES_SIDE);
-        DeleteTagCommand firstDeleteTagCommand = new DeleteTagCommand(firstTags);
+        DeleteTagCommand firstDeleteTagCommand = new DeleteTagCommand(firstTags, false);
 
         List<Tag> secondTags = new ArrayList<>();
         secondTags.add(BRIDES_SIDE);
-        DeleteTagCommand secondDeleteTagCommand = new DeleteTagCommand(secondTags);
+        DeleteTagCommand secondDeleteTagCommand = new DeleteTagCommand(secondTags, false);
 
         assertTrue(firstDeleteTagCommand.equals(secondDeleteTagCommand));
 
