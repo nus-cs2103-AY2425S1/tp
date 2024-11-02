c265b6dd88b27ffec19d67864b9289d6196fe00f
Jaynon <jaynonfoong@hotmail.com>
2024-11-02 14:46:26 +0800

    Update Testcases
    Improved code coverage

--- src/main/java/seedu/address/model/OperatingHours.java
+++ src/main/java/seedu/address/model/OperatingHours.java
@@ -39,14 +39,11 @@ public class OperatingHours {
 
         String[] tmp = source.split(" to ", 2);
 
-        if (tmp.length != 2) {
-            throw new IllegalArgumentException(MESSAGE_CONSTRAINTS);
-        }
-
         try {
             this.openingHour = parseTime(tmp[0]);
             this.closingHour = parseTime(tmp[1]);
-        } catch (ParseException e) {
+        } catch (ParseException ignored) {
+            // should not occur since isValidOperatingHours ensures that source can be parsed.
             throw new TimeParseException();
         }
     }
@@ -107,12 +104,17 @@ public class OperatingHours {
      */
     public static boolean isValidOperatingHours(String test) {
         String[] tmp = test.split(" to ", 2);
+
+        if (tmp.length != 2) {
+            return false;
+        }
+
         try {
             parseTime(tmp[0]);
             parseTime(tmp[1]);
             return true;
 
-        } catch (Exception e) {
+        } catch (ParseException e) {
             return false;
         }
     }
