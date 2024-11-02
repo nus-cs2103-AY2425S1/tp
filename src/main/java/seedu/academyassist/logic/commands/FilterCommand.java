aa2ee5f2badeea319c4ba084cc23279020f16a57
Ethan Wang <ethanwangkangen@gmail.com>
2024-11-02 15:35:37 +0800

    Change filter command to be more user friendly and fix bugs

--- src/main/java/seedu/academyassist/logic/commands/FilterCommand.java
+++ src/main/java/seedu/academyassist/logic/commands/FilterCommand.java
@@ -3,6 +3,8 @@ package seedu.academyassist.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.academyassist.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.academyassist.logic.parser.CliSyntax.PREFIX_FILTER_PARAM;
+import static seedu.academyassist.logic.parser.CliSyntax.PREFIX_SUBJECT;
+import static seedu.academyassist.logic.parser.CliSyntax.PREFIX_YEARGROUP;
 
 import seedu.academyassist.logic.commands.exceptions.CommandException;
 import seedu.academyassist.model.Model;
@@ -13,19 +15,20 @@ import seedu.academyassist.model.person.Subject;
 import seedu.academyassist.model.person.YearGroup;
 
 /**
- * Filters contacts based on year group or class.
+ * Filters contacts based on year group or subject.
  */
 public class FilterCommand extends Command {
 
     public static final String COMMAND_WORD = "filter";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filter contacts based on year group or class. " + "\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filter contacts based on year group or subject. " + "\n"
             + "Parameters: "
-            + PREFIX_FILTER_PARAM + "[FILTER TYPE] " + "[FILTER VALUE]" + "\n"
+            + PREFIX_YEARGROUP + "YEARGROUP " + "or "
+            + PREFIX_SUBJECT + "SUBJECT " + "\n"
             + "Examples: " + "\n" + COMMAND_WORD + " "
-            + PREFIX_FILTER_PARAM + "yearGroup " + "yg/2" + "\n"
+            + "yg/2" + "\n"
             + COMMAND_WORD + " "
-            + PREFIX_FILTER_PARAM + "subject " + "s/Science" + "\n";
+            + "s/Science";
 
     public static final String MESSAGE_SUCCESS = "Filtered by: %1$s, %2$s";
 
@@ -38,6 +41,7 @@ public class FilterCommand extends Command {
      */
     public FilterCommand(FilterParam filterParam, Object filterValue) {
         // Ensure that the filterParam and filterValue are not null using assertions
+
         assert filterParam != null : "FilterParam should not be null";
         assert filterValue != null : "FilterValue should not be null";
 
