0f4e3751eda1c9e0f68c608e02a39259720594b7
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-11-03 00:36:30 +0800

    Add search via role functionality

--- src/main/java/seedu/address/logic/parser/SearchCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchCommandParser.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
@@ -14,6 +15,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.PhoneNumberMatchesPredicate;
+import seedu.address.model.person.Role;
+import seedu.address.model.person.RoleMatchesPredicate;
 import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 /**
@@ -21,6 +24,7 @@ import seedu.address.model.person.TagContainsKeywordsPredicate;
  */
 public class SearchCommandParser implements Parser<SearchCommand> {
     public static final String MESSAGE_EMPTY_SEARCH_PREFIX = "The prefix cannot be empty. Please input a prefix.";
+    public static final String MESSAGE_INVALID_SEARCH_ROLE_INPUT = "Invalid role";
 
     /**
      * Parses the given {@code String} of arguments in the context of the FindCommand
@@ -29,24 +33,26 @@ public class SearchCommandParser implements Parser<SearchCommand> {
      */
     public SearchCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG,
-                PREFIX_GROUP_NAME, PREFIX_PHONE);
+                PREFIX_GROUP_NAME, PREFIX_PHONE, PREFIX_ROLE);
 
         if (!argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_TAG, PREFIX_GROUP_NAME, PREFIX_PHONE);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_TAG, PREFIX_GROUP_NAME, PREFIX_PHONE, PREFIX_ROLE);
 
         String nameArgs = argMultimap.getValue(PREFIX_NAME).orElse("");
         String tagArgs = argMultimap.getValue(PREFIX_TAG).orElse("");
         String groupArgs = argMultimap.getValue(PREFIX_GROUP_NAME).orElse("");
         String phoneArgs = argMultimap.getValue(PREFIX_PHONE).orElse("");
+        String roleArgs = argMultimap.getValue(PREFIX_ROLE).orElse("");
 
         // Check for empty inputs after the prefixes
         checkForEmptyInput(argMultimap, PREFIX_NAME, nameArgs);
         checkForEmptyInput(argMultimap, PREFIX_TAG, tagArgs);
         checkForEmptyInput(argMultimap, PREFIX_GROUP_NAME, groupArgs);
         checkForEmptyInput(argMultimap, PREFIX_PHONE, phoneArgs);
+        checkForEmptyInput(argMultimap, PREFIX_ROLE, roleArgs);
 
         Predicate<Person> combinedPredicate = null;
 
@@ -66,6 +72,17 @@ public class SearchCommandParser implements Parser<SearchCommand> {
             PhoneNumberMatchesPredicate phonePredicate =
                     new PhoneNumberMatchesPredicate(phoneArgs);
             combinedPredicate = combinePredicate(combinedPredicate, phonePredicate);
+        }
+
+        if (!roleArgs.isEmpty()) {
+            try {
+                Role role = Role.fromString(roleArgs);
+                RoleMatchesPredicate rolePredicate =
+                        new RoleMatchesPredicate(role);
+                combinedPredicate = combinePredicate(combinedPredicate, rolePredicate);
+            } catch (IllegalArgumentException e) {
+                throw new ParseException(MESSAGE_INVALID_SEARCH_ROLE_INPUT);
+            }
         }
 
         if (combinedPredicate == null && groupArgs.isEmpty()) {
