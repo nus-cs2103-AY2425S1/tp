b69ed2dc50e4fc2c89c44b963a0bbb4d5cb328ef
Nicholas Koh Zi Lun <nicholaskoh2@gmail.com>
2024-11-03 00:41:53 +0800

    Fix checkstyle violations

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.id.counter.list.IdCounterList;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.EventDescription;
 import seedu.address.model.event.EventDuration;
 import seedu.address.model.event.EventName;
+import seedu.address.model.id.counter.list.IdCounterList;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
