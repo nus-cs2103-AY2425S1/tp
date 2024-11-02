ad5d3a72e163abf8a23d2c11521ab67b46dc29f1
valhrd <valentinhn872@gmail.com>
2024-11-02 18:42:57 +0800

    Update Name regex

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -29,6 +29,7 @@ public class NameTest {
         assertFalse(Name.isValidName(" ")); // spaces only
         assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
         assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
+        assertFalse(Name.isValidName("balakrishnan s\\o balakrishnan")); // names with back slash
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
@@ -36,6 +37,8 @@ public class NameTest {
         assertTrue(Name.isValidName("peter the 2nd")); // alphanumeric characters
         assertTrue(Name.isValidName("Capital Tan")); // with capital letters
         assertTrue(Name.isValidName("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Name.isValidName("Batman s/o Suparman")); // names with forward slash
+        assertTrue(Name.isValidName("Jeanne D'arc")); // names with apostrophes
     }
 
     @Test
