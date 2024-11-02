22da57d0cbbf5d4fe697aeafbd0ba7c8704244f7
Curtis <curtischang2510@gmail.com>
2024-11-02 21:57:55 +0800

    Refactor: Remove unused import

--- src/main/java/tuteez/model/person/lesson/Lesson.java
+++ src/main/java/tuteez/model/person/lesson/Lesson.java
@@ -7,7 +7,6 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
