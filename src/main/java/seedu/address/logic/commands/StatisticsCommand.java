603de800d39c94487307e2c48b16ca5b52e2b49f
ITZXITZX <isaacteozixian@hotmail.com>
2024-11-03 01:32:15 +0800

    Add StatisticsCommand

---
+++ src/main/java/seedu/address/logic/commands/StatisticsCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class StatisticsCommand extends Command {
+
+    public static final String COMMAND_WORD = "stats";
+
+    public static final String MESSAGE_SUCCESS = "Statistics: ";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
