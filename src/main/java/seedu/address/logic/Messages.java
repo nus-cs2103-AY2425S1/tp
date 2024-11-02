6a658fab05ea2b8467af56f1490e04885ab66802
lithops-zty <1220708797@qq.com>
2024-11-03 00:50:21 +0800

    Let AddressBookParser use Messages.getErrorMessageWithUsage as well

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,7 +14,9 @@ import seedu.address.model.tag.Tag;
 public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format!";
+    public static final String MESSAGE_CHECK_OUT_HELP =
+            "You may use the help command to see all commands available!";
     public static final String MESSAGE_EMPTY_FIND_KEYWORD = "Find keyword(s) cannot be empty!";
     public static final String MESSAGE_FIND_KEYWORD_CONTAINS_WHITESPACE =
             "Find keyword(s) cannot contain whitespace(s)!";
