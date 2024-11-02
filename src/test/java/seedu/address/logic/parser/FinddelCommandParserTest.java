65d88b89e56001270f317f4df548c44fb3f623e1
linedoestrolling <yiheng.yh.zhao@gmail.com>
2024-11-02 19:16:10 +0800

    Fix checkstyle erros in test classes

--- src/test/java/seedu/address/logic/parser/FinddelCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FinddelCommandParserTest.java
@@ -1,14 +1,15 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.FinddelCommand;
-import seedu.address.model.delivery.ItemNameContainsKeywordPredicate;
-
-import java.util.Arrays;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FinddelCommand;
+import seedu.address.model.delivery.ItemNameContainsKeywordPredicate;
 
 public class FinddelCommandParserTest {
     private FinddelCommandParser parser = new FinddelCommandParser();
