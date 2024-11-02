79063f6bb092d9411f36a420226ceae24d35395f
yooplo <jjwr2002@gmail.com>
2024-11-02 21:02:05 +0800

    Checkstyle fix

--- src/main/java/seedu/address/ui/TagCard.java
+++ src/main/java/seedu/address/ui/TagCard.java
@@ -1,7 +1,6 @@
 package seedu.address.ui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
