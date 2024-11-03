9ce6a4246c2586cd06ca8a57bf7b9c9ed149d32d
Chen Shaoheng <82189093+cshao02@users.noreply.github.com>
2024-11-03 12:10:20 +0800

    Revert back to original PR

--- src/main/java/seedu/address/model/event/EventManager.java
+++ src/main/java/seedu/address/model/event/EventManager.java
@@ -6,8 +6,8 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import java.util.List;
 
 import javafx.collections.ObservableList;
+
 import seedu.address.model.event.exceptions.EventNotFoundException;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonInEventPredicate;
 
 /**
