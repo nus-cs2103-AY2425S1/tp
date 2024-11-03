0519032d8b824bb16043173e6f07af0bd84ace2c
IanCheah <iancheah24@gmail.com>
2024-11-04 02:21:57 +0800

    Fix style

--- src/main/java/seedu/address/model/person/PriorityLowToHighComparator.java
+++ src/main/java/seedu/address/model/person/PriorityLowToHighComparator.java
@@ -9,7 +9,7 @@ public class PriorityLowToHighComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        int comparisonResult =  o2.getPriority().compareTo(o1.getPriority());
+        int comparisonResult = o2.getPriority().compareTo(o1.getPriority());
 
         if (comparisonResult != 0) {
             return comparisonResult;
