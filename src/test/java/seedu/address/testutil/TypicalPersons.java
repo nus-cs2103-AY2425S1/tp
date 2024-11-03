19e06e0b699a1524bbe4a9cd330686e783ddcd99
rubinnn <e1155263@u.nus.edu>
2024-11-03 17:37:10 +0800

    Update Json files
    Update Json files to have reminder fields

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -13,36 +13,73 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import seedu.address.model.ClientHub;
 import seedu.address.model.person.Person;
+import seedu.address.model.reminder.Reminder;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
  */
 public class TypicalPersons {
 
+    public static final Reminder ALICE_REMINDER = new ReminderBuilder()
+            .withPersonName("Alice Pauline")
+            .withDescription("Reminder 1")
+            .withDateTime(LocalDate.of(2024, 10, 10),
+                    LocalTime.of(11, 11)).build();
+
+    public static final Reminder BENSON_REMINDER = new ReminderBuilder()
+            .withPersonName("Benson Meier")
+            .withDescription("Reminder 2")
+            .withDateTime(LocalDate.of(2024, 11, 10),
+                    LocalTime.of(6, 11)).build();
+
+    public static final Reminder CARL_REMINDER = new ReminderBuilder()
+            .withPersonName("Carl Kurz")
+            .withDescription("Reminder 3")
+            .withDateTime(LocalDate.of(2024, 12, 10),
+                    LocalTime.of(7, 11)).build();
+
+    public static final Reminder ALICEY_REMINDER = new ReminderBuilder()
+            .withPersonName("Alice Potter")
+            .withDescription("Reminder 5")
+            .withDateTime(LocalDate.of(2024, 12, 10),
+                    LocalTime.of(7, 11)).build();
+
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
             .withClientTypes("Investment")
-            .withDescription("A type").build();
+            .withDescription("A type")
+            .withReminders(ALICE_REMINDER)
+            .build();
+
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com")
             .withPhone("98765432")
-            .withClientTypes("Investment", "Savings").build();
+            .withClientTypes("Investment", "Savings")
+            .withReminders(BENSON_REMINDER)
+            .build();
+
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
             .withEmail("heinz@example.com")
             .withAddress("wall street")
-            .withClientTypes("Investment", "Healthcare").build();
+            .withClientTypes("Investment", "Healthcare")
+            .withReminders(CARL_REMINDER)
+            .build();
+
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
             .withEmail("cornelia@example.com")
             .withAddress("10th street")
             .withClientTypes("B").build();
+
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
             .withEmail("werner@example.com").withAddress("michegan ave").build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
@@ -50,7 +87,8 @@ public class TypicalPersons {
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
             .withEmail("anna@example.com").withAddress("4th street").build();
     public static final Person ALICEY = new PersonBuilder().withName("Alice Potter").withPhone("9992442")
-            .withEmail("APotter@example.com").withAddress("china").build();
+            .withEmail("APotter@example.com").withAddress("china")
+            .withReminders(ALICEY_REMINDER).build();
 
 
     // Manually added
