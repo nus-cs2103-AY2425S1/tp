7f4f80f6e4519e2357e63763b738e8dd5c2f3de3
yooplo <jjwr2002@gmail.com>
2024-11-05 02:08:22 +0800

    Update the test cases for cattag

--- src/test/java/seedu/address/logic/parser/CategorizeTagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CategorizeTagCommandParserTest.java
@@ -4,6 +4,8 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.CategorizeTagCommand;
@@ -34,9 +36,31 @@ public class CategorizeTagCommandParserTest {
     }
 
     @Test
-    public void parse_validCategoryArg_success() {
+    public void parse_validSingleTagCategoryArg_success() {
         CategorizeTagCommand expectedCatTagCommand =
-                new CategorizeTagCommand(new Tag("test"), TagCategory.ACTIVITIES);
+                new CategorizeTagCommand(Arrays.asList(new Tag("test")), TagCategory.ACTIVITIES);
         assertParseSuccess(parser, "cattag t/test activity", expectedCatTagCommand);
+    }
+
+    @Test
+    public void parse_validMultipleTagsCategoryArg_success() {
+        CategorizeTagCommand expectedCatTagCommand =
+                new CategorizeTagCommand(Arrays.asList(new Tag("test1"), new Tag("test2")), TagCategory.ACADEMICS);
+        assertParseSuccess(parser, "cattag t/test1 t/test2 acads", expectedCatTagCommand);
+    }
+
+    @Test
+    public void parse_validTagsWithMixedSpacing_success() {
+        CategorizeTagCommand expectedCatTagCommand =
+                new CategorizeTagCommand(Arrays.asList(new Tag("test"), new Tag("test2")), TagCategory.GENERAL);
+        assertParseSuccess(parser, "cattag  t/test   t/test2   general", expectedCatTagCommand);
+    }
+
+    @Test
+    public void parse_categoryInDifferentFormats_throwsParseException() {
+        assertParseFailure(parser, "cattag t/test 1invalid",
+                String.format(CategorizeTagCommand.MESSAGE_INVALID_CATEGORY, "1invalid"));
+        assertParseFailure(parser, "cattag t/test# invalidcategory",
+                Tag.MESSAGE_CONSTRAINTS);
     }
 }
