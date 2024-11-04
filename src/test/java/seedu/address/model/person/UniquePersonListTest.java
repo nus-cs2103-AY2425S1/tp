233ec97443bbf96a2df2eeeb579b137ede0d26fc
Nicholas Koh Zi Lun <nicholaskoh2@gmail.com>
2024-11-05 02:16:42 +0800

    Fix checkstyle violations

--- src/test/java/seedu/address/model/person/UniquePersonListTest.java
+++ src/test/java/seedu/address/model/person/UniquePersonListTest.java
@@ -14,7 +14,6 @@ import static seedu.address.testutil.TypicalPersons.BOB;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
