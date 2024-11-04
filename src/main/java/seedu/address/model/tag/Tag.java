227c01e6c4460bd875198a1231b9cc2eb3c80b9f
EMMANUEL\Emmanuel <emmanueler97@gmail.com>
2024-11-04 22:15:15 +0800

    Update UG to match TAchy

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -12,7 +12,7 @@ public class Tag {
     public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
     public static final int MAXIMUM_NAME_LENGTH = 40;
     public static final String MESSAGE_NAME_TOO_LONG = "A tag name cannot exceed the maximum length of %d characters";
-    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String tagName;
 
