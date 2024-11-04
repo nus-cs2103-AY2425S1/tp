d6043336e710b6180fbaee2637ebfc5d67b2cca4
Jabez Tho <jabeztho@gmail.com>
2024-11-05 02:09:43 +0800

    Update test cases for new unassign format

--- src/main/java/seedu/eventtory/logic/parser/UnassignCommandParser.java
+++ src/main/java/seedu/eventtory/logic/parser/UnassignCommandParser.java
@@ -2,10 +2,6 @@ package seedu.eventtory.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.eventtory.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.eventtory.logic.parser.CliSyntax.PREFIX_EVENT;
-import static seedu.eventtory.logic.parser.CliSyntax.PREFIX_VENDOR;
-
-import java.util.stream.Stream;
 
 import seedu.eventtory.commons.core.index.Index;
 import seedu.eventtory.logic.commands.UnassignCommand;
@@ -18,6 +14,7 @@ public class UnassignCommandParser implements Parser<UnassignCommand> {
 
     @Override
     public UnassignCommand parse(String args) throws ParseException {
+        requireNonNull(args);
         try {
             Index index = ParserUtil.parseIndex(args);
             return new UnassignCommand(index);
