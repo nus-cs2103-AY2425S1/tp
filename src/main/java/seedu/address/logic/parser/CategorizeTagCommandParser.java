3c196a6df02a94a7e185bcacbf7c6d9124e622b3
yooplo <jjwr2002@gmail.com>
2024-11-05 01:24:08 +0800

    Update the cattag implementation This should allow multiple tags to be recategorized at the same time.

--- src/main/java/seedu/address/logic/parser/CategorizeTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CategorizeTagCommandParser.java
@@ -4,7 +4,8 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 import seedu.address.logic.commands.CategorizeTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -12,45 +13,83 @@ import seedu.address.model.tag.Tag;
 import seedu.address.model.tag.TagCategory;
 
 /**
- * Parse input arguments and returns a new CategorizeTagCommand object.
+ * Parses input arguments and returns a new CategorizeTagCommand object.
  */
 public class CategorizeTagCommandParser implements Parser<CategorizeTagCommand> {
 
+    private static final String CATEGORY_GENERAL = "general";
+    private static final String CATEGORY_ACADEMICS = "acads";
+    private static final String CATEGORY_ACTIVITIES = "activity";
+    private static final String CATEGORY_NETWORKING = "networking";
+    private static final String CATEGORY_MENTORSHIP = "mentor";
+
     /**
      * Parses the given {@code String} of arguments in the context of the CategorizeTagCommand
-     * and returns an CategorizeTagCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * and returns a CategorizeTagCommand object for execution.
+     *
+     * @param args The input arguments as a string.
+     * @return A CategorizeTagCommand object for execution.
+     * @throws ParseException if the user input does not conform to the expected format.
      */
     public CategorizeTagCommand parse(String args) throws ParseException {
         requireNonNull(args);
+
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TAG);
-        Optional<String> parsedArgs = argMultimap.getValue(PREFIX_TAG);
+        List<String> parsedArgs = argMultimap.getAllValues(PREFIX_TAG);
+
         if (parsedArgs.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CategorizeTagCommand.MESSAGE_USAGE));
         }
-        String[] split = parsedArgs.get().split("\\s+", 2);
-        if (split.length < 2) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CategorizeTagCommand.MESSAGE_USAGE));
+
+        String[] parsedArgsArray = getStringsArray(parsedArgs);
+
+        List<Tag> tags = new ArrayList<>();
+
+        for (int i = 0; i < parsedArgsArray.length - 1; i++) {
+            tags.add(ParserUtil.parseTag(parsedArgsArray[i]));
         }
-        Tag tag = ParserUtil.parseTag(split[0]);
-        TagCategory cat = parseCategory(split[1]);
-        return new CategorizeTagCommand(tag, cat);
+
+        TagCategory category = parseCategory(parsedArgsArray[parsedArgsArray.length - 1]);
+
+        return new CategorizeTagCommand(tags, category);
     }
 
-    private TagCategory parseCategory(String cat) throws ParseException {
-        switch (cat) {
-        case "general":
+    private String[] getStringsArray(List<String> parsedArgs) throws ParseException {
+        String[] argsArray = parsedArgs.toArray(String[]::new);
+
+        String[] lastTagAndCategory = argsArray[argsArray.length - 1].split("\\s+");
+        if (lastTagAndCategory.length < 2) { // ensure at least one tag is provided
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, CategorizeTagCommand.MESSAGE_USAGE));
+        }
+        String[] allTagsAndCategory = new String[argsArray.length + 1];
+
+        System.arraycopy(argsArray, 0, allTagsAndCategory, 0, argsArray.length - 1);
+        allTagsAndCategory[allTagsAndCategory.length - 2] = lastTagAndCategory[0];
+        allTagsAndCategory[allTagsAndCategory.length - 1] = lastTagAndCategory[1];
+        return allTagsAndCategory;
+    }
+
+    /**
+     * Parses the given category string and returns the corresponding TagCategory.
+     *
+     * @param category The category string to parse.
+     * @return The corresponding TagCategory.
+     * @throws ParseException if the category is invalid.
+     */
+    private TagCategory parseCategory(String category) throws ParseException {
+        switch (category) {
+        case CATEGORY_GENERAL:
             return TagCategory.GENERAL;
-        case "acads":
+        case CATEGORY_ACADEMICS:
             return TagCategory.ACADEMICS;
-        case "activity":
+        case CATEGORY_ACTIVITIES:
             return TagCategory.ACTIVITIES;
-        case "networking":
+        case CATEGORY_NETWORKING:
             return TagCategory.NETWORKING;
-        case "mentor":
+        case CATEGORY_MENTORSHIP:
             return TagCategory.MENTORSHIP;
         default:
-            throw new ParseException(String.format(CategorizeTagCommand.MESSAGE_INVALID_CATEGORY, cat));
+            throw new ParseException(String.format(CategorizeTagCommand.MESSAGE_INVALID_CATEGORY, category));
         }
     }
 }
