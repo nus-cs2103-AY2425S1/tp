4b6f45bbab5de8f9dbbcee945100a4a637baa164
Jeremy <e1155908@u.nus.edu>
2024-11-04 18:21:35 +0800

    Update addressbook to clienthub

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -23,7 +23,7 @@ public class AddCommand extends Command {
 
     public static final String SHORT_COMMAND_WORD = "a";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to ClientHub.\n"
             + "Parameters: "
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
