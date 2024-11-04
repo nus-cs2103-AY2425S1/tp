ce000bd21d137da2bbc687925d8f61ede4ad9c87
Nayana Mahesh <nayanamahesh7@gmail.com>
2024-11-04 12:30:18 +0800

    Fix check style and test case issues

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -2,7 +2,6 @@ package seedu.address.model.person;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.time.LocalDate;
 import java.util.Objects;
 
 import seedu.address.commons.util.ToStringBuilder;
