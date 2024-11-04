02aade42ad4b258a091a8d91394860294896a2e1
BunnyHoppp <e1122434@u.nus.edu>
2024-11-04 21:51:44 +0800

    Remove unused import statement
    Remove dead code

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
