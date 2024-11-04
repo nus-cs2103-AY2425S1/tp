abb8c1538f0dc76f2459c9d723110c6fdb7fe4c2
jia xin <jjiiaaxxiinn@gmail.com>
2024-11-04 17:04:00 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteTagCommandTest.java
@@ -25,7 +25,6 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
-import seedu.address.testutil.TypicalTags;
 
 /**
  * Contains tests for DeleteTagCommand.
