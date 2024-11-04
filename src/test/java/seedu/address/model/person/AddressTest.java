ab9ed1c5ff595770a3d83d73156939afd107a2b5
yan yee <143587837+yyanyee@users.noreply.github.com>
2024-11-04 23:29:59 +0800

    Allow for addresses without unit numbers

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -28,10 +28,13 @@ public class AddressTest {
         assertFalse(Address.isValidAddress("")); // empty string
         assertFalse(Address.isValidAddress(" ")); // spaces only
         assertFalse(Address.isValidAddress("-"));
-        assertFalse(Address.isValidAddress("Blk 456, Den Road"));
+        assertFalse(Address.isValidAddress(", Den Road, #01-355"));
+
 
         // valid addresses
         assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
+        assertTrue(Address.isValidAddress("#01-355 Blk 456, Den Road"));
+        assertTrue(Address.isValidAddress("91 Pemimpin Pl"));
         assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA #12-345; USA")); // long address
     }
 
