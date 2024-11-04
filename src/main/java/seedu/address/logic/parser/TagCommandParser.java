5374a488c641433ad36a8d28dbcd1fadeb272708
Charlene <charlenecyteoh@gmail.com>
2024-11-05 03:47:37 +0800

    Refine tag parser

--- src/main/java/seedu/address/logic/parser/TagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collection;
-import java.util.Optional;
 import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
@@ -34,10 +33,7 @@ public class TagCommandParser implements Parser<TagCommand> {
         // Parses the index after format verification
         Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
 
-        // Parses the tags and handle empty or invalid tags
-        Set<Tag> tagsToAdd = parseTagsForAdd(argMultimap.getAllValues(PREFIX_TAG))
-                .orElseThrow(() -> new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                        TagCommand.MESSAGE_USAGE)));
+        Set<Tag> tagsToAdd = parseTagsForAdd(argMultimap.getAllValues(PREFIX_TAG));
 
         return new TagCommand(index, tagsToAdd);
     }
@@ -47,12 +43,8 @@ public class TagCommandParser implements Parser<TagCommand> {
      * If {@code tags} is an empty string, it will be parsed into a
      * {@code Set<Tag>} containing zero tags.
      */
-    private Optional<Set<Tag>> parseTagsForAdd(Collection<String> tags) throws ParseException {
+    private Set<Tag> parseTagsForAdd(Collection<String> tags) throws ParseException {
         assert tags != null;
-
-        if (tags.isEmpty()) {
-            return Optional.empty();
-        }
 
         for (String tag : tags) {
             if (tag.trim().isEmpty()) {
@@ -60,6 +52,6 @@ public class TagCommandParser implements Parser<TagCommand> {
             }
         }
 
-        return Optional.of(ParserUtil.parseTags(tags));
+        return ParserUtil.parseTags(tags);
     }
 }
