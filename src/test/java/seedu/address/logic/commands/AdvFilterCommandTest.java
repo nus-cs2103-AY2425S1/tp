464149492a8da529a4205742d9890ce18689ed02
valhrd <valentinhn872@gmail.com>
2024-11-04 21:46:20 +0800

    Update JavaDoc

--- src/test/java/seedu/address/logic/commands/AdvFilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AdvFilterCommandTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.AMY_CLONE;
