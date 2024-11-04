3b4043d2543090744d52919e78cc793194e5aefe
NgZiXin <ngzixin02@gmail.com>
2024-11-04 18:02:09 +0800

    Edit UI to show invalid addedBuyer

--- src/test/java/seedu/address/model/meetup/AddedBuyerTest.java
+++ src/test/java/seedu/address/model/meetup/AddedBuyerTest.java
@@ -1,5 +1,7 @@
 package seedu.address.model.meetup;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -13,13 +15,27 @@ public class AddedBuyerTest {
 
     @Test
     public void constructor_invalidAddedBuyerName_throwsIllegalArgumentException() {
-        String invalidAddedBuyerName = "H!";
-        assertThrows(IllegalArgumentException.class, () -> new AddedBuyer(invalidAddedBuyerName));
+        String invalidName = "H!";
+        assertThrows(IllegalArgumentException.class, () -> new AddedBuyer(invalidName));
     }
 
     @Test
-    public void isValidAddedBuyerName() {
-        // null addedBuyer name
-        assertThrows(NullPointerException.class, () -> AddedBuyer.isValidBuyerName(null));
+    public void equals() {
+        AddedBuyer addedBuyer = new AddedBuyer("ValidAddedBuyerName");
+
+        // same values -> returns true
+        assertTrue(addedBuyer.equals(new AddedBuyer("ValidAddedBuyerName")));
+
+        // same object -> returns true
+        assertTrue(addedBuyer.equals(addedBuyer));
+
+        // null -> returns false
+        assertFalse(addedBuyer.equals(null));
+
+        // different types -> returns false
+        assertFalse(addedBuyer.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(addedBuyer.equals(new AddedBuyer("OtherValidAddedBuyerName")));
     }
 }
