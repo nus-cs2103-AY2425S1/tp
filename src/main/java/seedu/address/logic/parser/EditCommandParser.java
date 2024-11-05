a70c587ca8689f381e7b4b003b5b0cc28f911947
rubinnn <e1155263@u.nus.edu>
2024-11-05 14:33:54 +0800

    Update Code for style
    Update code to delete unused imports

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -10,7 +10,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 
