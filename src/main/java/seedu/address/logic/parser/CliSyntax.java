7608004108c6aef05ce2d0a5ee6f63d630d48158
estellelim <estlim@hotmail.com>
2024-11-06 02:41:39 +0800

    Remove Prefix Tag

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,7 +11,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_HOURS = new Prefix("\\h");
     public static final Prefix PREFIX_EMAIL = new Prefix("\\e");
     public static final Prefix PREFIX_ADDRESS = new Prefix("\\a");
-    public static final Prefix PREFIX_TAG = new Prefix("\\t");
     public static final Prefix PREFIX_SUBJECT = new Prefix("\\s");
     public static final Prefix PREFIX_FILEPATH = new Prefix("\\f");
 
