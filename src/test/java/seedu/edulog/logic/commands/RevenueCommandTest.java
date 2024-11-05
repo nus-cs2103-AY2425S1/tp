cc933f26081aa3ed22e6eb25f32e8e946df80343
kimxw <kimaya.wanjari@nuscomputing.com>
2024-11-05 15:38:33 +0800

    Fix tests post-OOPizing RevenueCommand

--- src/test/java/seedu/edulog/logic/commands/RevenueCommandTest.java
+++ src/test/java/seedu/edulog/logic/commands/RevenueCommandTest.java
@@ -16,43 +16,43 @@ public class RevenueCommandTest {
 
     @Test
     public void execute_noHasPaidOptionPaid_successful() {
-        RevenueCommand command = new RevenueCommand("paid");
+        RevenueCommand command = new PaidRevenueCommand();
         Model model = new ModelManager(getTypicalEduLogWithCopiedStudents(), new UserPrefs());
         model.unmarkAllStudents();
 
         // none of the student has paid
-        String expectedMessage = String.format(RevenueCommand.COMMAND_SUCCESS, "paid", 0);
+        String expectedMessage = String.format(RevenueCommand.MESSAGE_SUCCESS, "paid", 0);
         assertCommandSuccess(command, model, expectedMessage, model);
     }
 
     @Test
     public void execute_allHasPaidOptionPaid_successful() {
-        RevenueCommand command = new RevenueCommand("paid");
+        RevenueCommand command = new PaidRevenueCommand();
         Model model = new ModelManager(getTypicalEduLogWithCopiedStudents(), new UserPrefs());
         model.markAllStudents();
 
-        String expectedMessage = String.format(RevenueCommand.COMMAND_SUCCESS, "paid", 700);
+        String expectedMessage = String.format(RevenueCommand.MESSAGE_SUCCESS, "paid", 700);
         assertCommandSuccess(command, model, expectedMessage, model);
     }
 
     @Test
     public void execute_noHasPaidOptionUnpaid_successful() {
-        RevenueCommand command = new RevenueCommand("unpaid");
+        RevenueCommand command = new UnpaidRevenueCommand();
         Model model = new ModelManager(getTypicalEduLogWithCopiedStudents(), new UserPrefs());
         model.unmarkAllStudents();
 
         // none of the student has paid
-        String expectedMessage = String.format(RevenueCommand.COMMAND_SUCCESS, "unpaid", 700);
+        String expectedMessage = String.format(RevenueCommand.MESSAGE_SUCCESS, "unpaid", 700);
         assertCommandSuccess(command, model, expectedMessage, model);
     }
 
     @Test
     public void execute_allHasPaidOptionUnpaid_successful() {
-        RevenueCommand command = new RevenueCommand("unpaid");
+        RevenueCommand command = new UnpaidRevenueCommand();
         Model model = new ModelManager(getTypicalEduLogWithCopiedStudents(), new UserPrefs());
         model.markAllStudents();
 
-        String expectedMessage = String.format(RevenueCommand.COMMAND_SUCCESS, "unpaid", 0);
+        String expectedMessage = String.format(RevenueCommand.MESSAGE_SUCCESS, "unpaid", 0);
         assertCommandSuccess(command, model, expectedMessage, model);
     }
 }
--- src/test/java/seedu/edulog/logic/parser/RevenueCommandParserTest.java
+++ src/test/java/seedu/edulog/logic/parser/RevenueCommandParserTest.java
@@ -5,7 +5,9 @@ import static seedu.edulog.logic.parser.CommandParserTestUtil.assertParseSuccess
 
 import org.junit.jupiter.api.Test;
 
+import seedu.edulog.logic.commands.PaidRevenueCommand;
 import seedu.edulog.logic.commands.RevenueCommand;
+import seedu.edulog.logic.commands.UnpaidRevenueCommand;
 
 
 public class RevenueCommandParserTest {
@@ -13,8 +15,8 @@ public class RevenueCommandParserTest {
 
     @Test
     public void parse_validArgs_successful() {
-        assertParseSuccess(parser, "paid", new RevenueCommand("paid"));
-        assertParseSuccess(parser, "unpaid", new RevenueCommand("unpaid"));
+        assertParseSuccess(parser, "paid", new PaidRevenueCommand());
+        assertParseSuccess(parser, "unpaid", new UnpaidRevenueCommand());
     }
 
     @Test
