1383cf3801b35bfe876675843ae457f51220ccd0
Wei Lian <weilian2003@live.com>
2024-11-05 11:12:07 +0800

    add test cases for `deleteProfile` command

---
+++ src/test/java/seedu/address/logic/commands/DeleteProfileCommandTest.java
@@ -0,0 +1,104 @@
+
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.profile.Profile;
+
+public class DeleteProfileCommandTest {
+
+    @Test
+    public void execute_deleteExistingProfile_success() throws CommandException {
+        Profile profileToDelete = new Profile("deletable-profile");
+        Model model = new ModelManager();
+        model.addToProfiles(profileToDelete);
+
+        DeleteProfileCommand deleteCommand = new DeleteProfileCommand(profileToDelete);
+        CommandResult result = deleteCommand.execute(model);
+
+        assertEquals(
+                String.format(DeleteProfileCommand.MESSAGE_SUCCESS, "deletable-profile"),
+                result.getFeedbackToUser()
+        );
+        assertEquals(false, model.getProfiles().contains(profileToDelete));
+    }
+
+    @Test
+    public void execute_boundaryProfileNames_success() throws CommandException {
+        Profile minLengthProfile = new Profile("a");
+        Model model = new ModelManager();
+        model.addToProfiles(minLengthProfile);
+
+        DeleteProfileCommand minDeleteCommand = new DeleteProfileCommand(minLengthProfile);
+        CommandResult minResult = minDeleteCommand.execute(model);
+        assertEquals(String.format(DeleteProfileCommand.MESSAGE_SUCCESS, "a"), minResult.getFeedbackToUser());
+        assertEquals(false, model.getProfiles().contains(minLengthProfile));
+
+        Profile maxLengthProfile = new Profile("a".repeat(30));
+        model.addToProfiles(maxLengthProfile);
+
+        DeleteProfileCommand maxDeleteCommand = new DeleteProfileCommand(maxLengthProfile);
+        CommandResult maxResult = maxDeleteCommand.execute(model);
+        assertEquals(
+                String.format(DeleteProfileCommand.MESSAGE_SUCCESS, "a".repeat(30)),
+                maxResult.getFeedbackToUser()
+        );
+        assertEquals(false, model.getProfiles().contains(maxLengthProfile));
+    }
+
+    @Test
+    public void execute_deleteNonExistentProfile_errorMessage() {
+        Profile nonExistentProfile = new Profile("non-existent-profile");
+        Model model = new ModelManager();
+
+        DeleteProfileCommand deleteCommand = new DeleteProfileCommand(nonExistentProfile);
+
+        assertThrows(CommandException.class,
+                DeleteProfileCommand.MESSAGE_NO_SUCH_PROFILE, () -> deleteCommand.execute(model));
+    }
+
+    @Test
+    public void execute_deleteCurrentProfile_errorMessage() {
+        Profile currentProfile = new Profile("active-profile");
+        Model model = new ModelManager();
+        model.addToProfiles(currentProfile);
+        model.setAddressBookFilePath(currentProfile.toPath());
+
+        DeleteProfileCommand deleteCommand = new DeleteProfileCommand(currentProfile);
+
+        assertThrows(CommandException.class,
+                String.format(DeleteProfileCommand.MESSAGE_IN_USE), () -> deleteCommand.execute(model));
+    }
+
+    @Test
+    public void execute_invalidProfileName_exceptionThrown() {
+        Profile validProfile = new Profile("valid-profile");
+        String illegalProfileName = " space-in-name";
+        Model model = new ModelManager();
+        model.setAddressBookFilePath(Profile.profileStringToPath(illegalProfileName));
+
+        DeleteProfileCommand deleteCommand = new DeleteProfileCommand(validProfile);
+
+        assertThrows(CommandException.class,
+                DeleteProfileCommand.MESSAGE_ILLEGAL_MODIFICATION, () -> deleteCommand.execute(model));
+    }
+
+    @Test
+    public void execute_invalidProfileNameWithSubDir_exceptionThrown() {
+        Profile validProfile = new Profile("valid-profile");
+        String illegalProfilePath = "data/name";
+        Model model = new ModelManager();
+        model.setAddressBookFilePath(Profile.profileStringToPath(illegalProfilePath));
+
+        DeleteProfileCommand deleteCommand = new DeleteProfileCommand(validProfile);
+
+        assertThrows(CommandException.class,
+                DeleteProfileCommand.MESSAGE_ILLEGAL_MODIFICATION, () -> deleteCommand.execute(model));
+    }
+}
