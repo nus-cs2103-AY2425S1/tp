6fd66997cb34fdee1fda210c6ed1ec16c120f1ac
Ponharish <e1123017@u.nus.edu>
2024-11-05 13:47:24 +0800

    Update Message.java

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -37,6 +37,8 @@ public class Messages {
     public static String format(Person person) {
         final StringBuilder builder = new StringBuilder();
         builder.append(person.getName())
+                .append("; NRIC: ")
+                .append(person.getNric())
                 .append("; Phone: ")
                 .append(person.getPhone())
                 .append("; Email: ")
