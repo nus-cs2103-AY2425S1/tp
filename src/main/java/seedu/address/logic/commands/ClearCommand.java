4973e08d2d5244b0a420ea6377789300095e0942
Ponharish <e1123017@u.nus.edu>
2024-11-05 12:45:09 +0800

    Update Clear Confirmation prompt

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -12,8 +12,8 @@ public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
     public static final String COMMAND_CONFIRM_WORD = "confirm";
-    public static final String MESSAGE_CONFIRMATION_REQUIRED = "Please confirm the clear action"
-            + "by typing: clear confirm";
+    public static final String MESSAGE_CONFIRMATION_REQUIRED =
+            "Please confirm the clear action by typing: clear confirm";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
 
     @Override
