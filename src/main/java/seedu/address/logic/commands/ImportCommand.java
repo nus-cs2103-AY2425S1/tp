bf0691b657b8fa7842caa04af02bb81ed71586c8
myrilla12 <myrilla12@gmail.com>
2024-11-05 13:16:24 +0800

    Update import command parser
    There's a lot of repetition in note validation method. Since Note::isValidAppointment now takes in a string, let's increase modularity for note validation.

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -10,7 +10,7 @@ import seedu.address.model.Model;
  */
 public class ImportCommand extends Command {
     public static final String COMMAND_WORD = "import";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports a json file with patient data if"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Imports a json file with patient data if "
             + "json file exists in the same directory as MediContact.jar and the data "
             + "is in the correct format (refer to user guide linked in help window for more information).\n"
             + "Parameters: FILE_NAME.json\n"
