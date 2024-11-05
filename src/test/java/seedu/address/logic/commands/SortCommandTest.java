a43188dd10721752c1b72681af4b671319c8b259
valhrd <valentinhn872@gmail.com>
2024-11-05 09:27:00 +0800

    Add new test cases for SortCommand

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -21,7 +21,7 @@ import seedu.address.model.tag.Tag;
 public class SortCommandTest {
     private Model model;
 
-    private void addTagwValuesPersons(Model model) {
+    private void addTagsWithValuesPersons(Model model) {
         Set<Tag> tags1 = new HashSet<>(List.of(new Tag("friends", "1")));
         Set<Tag> tags2 = new HashSet<>(List.of(new Tag("friends", "3")));
         Set<Tag> tags3 = new HashSet<>(List.of(new Tag("friends", "2")));
@@ -39,9 +39,9 @@ public class SortCommandTest {
     @BeforeEach
     public void setUp() {
         model = new ModelManager();
-        addTagwValuesPersons(model);
+        addTagsWithValuesPersons(model);
     }
-    //Test normal ascending
+    // Test normal ascending
     @Test
     public void execute_validSortOrderAsc_returnsSortedCommandResult() {
         SortCommand sortCommand = new SortCommand("friends", "asc");
@@ -56,7 +56,7 @@ public class SortCommandTest {
         assertEquals("Bob", sortedList.get(2).getName().fullName);
         assertEquals("Eve", sortedList.get(3).getName().fullName);
     }
-    //Test normal descending
+    // Test normal descending
     @Test
     public void execute_validSortOrderDesc_returnsSortedCommandResult() {
         SortCommand sortCommand = new SortCommand("friends", "desc");
@@ -71,7 +71,7 @@ public class SortCommandTest {
         assertEquals("Charlie", sortedList.get(2).getName().fullName);
         assertEquals("Alice", sortedList.get(3).getName().fullName);
     }
-    //Tag doesnt exist
+    // Tag doesnt exist
     @Test
     public void execute_noMatchingContacts_returnsNoContactFoundMessage() {
         SortCommand sortCommand = new SortCommand("nonexistent", "asc");
@@ -79,5 +79,104 @@ public class SortCommandTest {
         assertEquals(SortCommand.MESSAGE_NO_CONTACT_FOUND, result.getFeedbackToUser());
     }
 
+    // Test ascending with Tags with string value
+    @Test
+    public void execute_ascWithStringValueTag_returnsSortedCommandResult() {
+        Set<Tag> stringValueTag1 = new HashSet<>(List.of(new Tag("friends", "always")));
+        Set<Tag> stringValueTag2 = new HashSet<>(List.of(new Tag("friends", "before")));
+        Set<Tag> stringValueTag3 = new HashSet<>(List.of(new Tag("friends", "Z")));
+        model.addPerson(new Person(new Name("Friendo"), new Phone("10000000"), new Email("f@example.com"),
+                new Address("100 Empty St"), stringValueTag1, "Finance Info 100", "@ffff"));
+        model.addPerson(new Person(new Name("Griendo"), new Phone("20000000"), new Email("g@example.com"),
+                new Address("200 Empty St"), stringValueTag2, "Finance Info 200", "@gggg"));
+        model.addPerson(new Person(new Name("Hriendo"), new Phone("30000000"), new Email("h@example.com"),
+                new Address("300 Empty St"), stringValueTag3, "Finance Info 300", "@hhhh"));
+
+        SortCommand sortCommand = new SortCommand("friends", "asc");
+        CommandResult result = sortCommand.execute(model);
+        String expectedMessage = "Displaying sorted results: Tag: friends";
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+
+        List<Person> sortedList = model.getFilteredPersonList();
+
+        assertEquals("Alice", sortedList.get(0).getName().fullName);
+        assertEquals("Charlie", sortedList.get(1).getName().fullName);
+        assertEquals("Bob", sortedList.get(2).getName().fullName);
+        assertEquals("Eve", sortedList.get(3).getName().fullName);
+        assertEquals("Hriendo", sortedList.get(4).getName().fullName);
+        assertEquals("Friendo", sortedList.get(5).getName().fullName);
+        assertEquals("Griendo", sortedList.get(6).getName().fullName);
+    }
+
+    // Test descending with Tags with string value
+    @Test
+    public void execute_descWithStringValueTag_returnsSortedCommandResult() {
+        Set<Tag> stringValueTag1 = new HashSet<>(List.of(new Tag("friends", "always")));
+        Set<Tag> stringValueTag2 = new HashSet<>(List.of(new Tag("friends", "before")));
+        Set<Tag> stringValueTag3 = new HashSet<>(List.of(new Tag("friends", "Z")));
+        model.addPerson(new Person(new Name("Friendo"), new Phone("10000000"), new Email("f@example.com"),
+                new Address("100 Empty St"), stringValueTag1, "Finance Info 100", "@ffff"));
+        model.addPerson(new Person(new Name("Griendo"), new Phone("20000000"), new Email("g@example.com"),
+                new Address("200 Empty St"), stringValueTag2, "Finance Info 200", "@gggg"));
+        model.addPerson(new Person(new Name("Hriendo"), new Phone("30000000"), new Email("h@example.com"),
+                new Address("300 Empty St"), stringValueTag3, "Finance Info 300", "@hhhh"));
+
+        SortCommand sortCommand = new SortCommand("friends", "desc");
+        CommandResult result = sortCommand.execute(model);
+        String expectedMessage = "Displaying sorted results: Tag: friends";
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+
+        List<Person> sortedList = model.getFilteredPersonList();
+
+        assertEquals("Griendo", sortedList.get(0).getName().fullName);
+        assertEquals("Friendo", sortedList.get(1).getName().fullName);
+        assertEquals("Hriendo", sortedList.get(2).getName().fullName);
+        assertEquals("Eve", sortedList.get(3).getName().fullName);
+        assertEquals("Bob", sortedList.get(4).getName().fullName);
+        assertEquals("Charlie", sortedList.get(5).getName().fullName);
+        assertEquals("Alice", sortedList.get(6).getName().fullName);
+    }
+
+    // Test ascending with Tag with no value
+    @Test
+    public void execute_ascWithNullValueTag_returnsSortedCommandResult() {
+        Set<Tag> nullValueTag = new HashSet<>(List.of(new Tag("friends")));
+        model.addPerson(new Person(new Name("Null"), new Phone("00000000"), new Email("nullify@example.com"),
+                new Address("000 Empty St"), nullValueTag, "Finance Info 0", "@nullify"));
+
+        SortCommand sortCommand = new SortCommand("friends", "asc");
+        CommandResult result = sortCommand.execute(model);
+        String expectedMessage = "Displaying sorted results: Tag: friends";
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+
+        List<Person> sortedList = model.getFilteredPersonList();
+
+        assertEquals("Alice", sortedList.get(0).getName().fullName);
+        assertEquals("Charlie", sortedList.get(1).getName().fullName);
+        assertEquals("Bob", sortedList.get(2).getName().fullName);
+        assertEquals("Eve", sortedList.get(3).getName().fullName);
+        assertEquals("Null", sortedList.get(4).getName().fullName);
+    }
+
+    // Test descending with Tag with no value
+    @Test
+    public void execute_descWithNullValueTag_returnsSortedCommandResult() {
+        Set<Tag> nullValueTag = new HashSet<>(List.of(new Tag("friends")));
+        model.addPerson(new Person(new Name("Null"), new Phone("00000000"), new Email("nullify@example.com"),
+                new Address("000 Empty St"), nullValueTag, "Finance Info 0", "@nullify"));
+
+        SortCommand sortCommand = new SortCommand("friends", "desc");
+        CommandResult result = sortCommand.execute(model);
+        String expectedMessage = "Displaying sorted results: Tag: friends";
+        assertEquals(expectedMessage, result.getFeedbackToUser());
+
+        List<Person> sortedList = model.getFilteredPersonList();
+
+        assertEquals("Eve", sortedList.get(0).getName().fullName);
+        assertEquals("Bob", sortedList.get(1).getName().fullName);
+        assertEquals("Charlie", sortedList.get(2).getName().fullName);
+        assertEquals("Alice", sortedList.get(3).getName().fullName);
+        assertEquals("Null", sortedList.get(4).getName().fullName);
+    }
 }
 
