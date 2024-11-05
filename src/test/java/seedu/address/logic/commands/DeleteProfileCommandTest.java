f0356195e3648d5ed1159a7263ff67c15b12bc67
Wei Lian <weilian2003@live.com>
2024-11-05 11:46:21 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/commands/DeleteProfileCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteProfileCommandTest.java
@@ -2,9 +2,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
