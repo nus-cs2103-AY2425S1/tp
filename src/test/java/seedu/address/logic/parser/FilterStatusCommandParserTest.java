7cef604e9dd924188e0469407c9f8d22dfcae058
dominic2412 <521hxt@gmail.com>
2024-11-05 19:24:31 +0800

    created more test cases to cover the parser

--- src/test/java/seedu/address/logic/parser/FilterStatusCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterStatusCommandParserTest.java
@@ -12,7 +12,7 @@ import seedu.address.logic.commands.FilterStatusCommand;
 import seedu.address.model.person.StatusContainsKeywordsPredicate;
 
 public class FilterStatusCommandParserTest {
-    private FilterStatusCommandParser parser = new FilterStatusCommandParser();
+    private final FilterStatusCommandParser parser = new FilterStatusCommandParser();
 
     @Test
     public void parse_emptyArg_throwsParseException() {
@@ -29,5 +29,31 @@ public class FilterStatusCommandParserTest {
 
         // multiple whitespaces between keywords
         assertParseSuccess(parser, " \n Applied \n \t Hired  \t", expectedFilterStatusCommand);
+    }
+
+    @Test
+    public void parse_invalidArg_throwsParseException() {
+        // single invalid status
+        assertParseFailure(parser, "Recruited", "Invalid status: Recruited. Valid statuses are: "
+            + String.join(", ", FilterStatusCommandParser.VALID_STATUSES));
+
+        // multiple invalid statuses
+        assertParseFailure(parser, "Applied Recruited", "Invalid status: Recruited. Valid statuses are: "
+            + String.join(", ", FilterStatusCommandParser.VALID_STATUSES));
+
+        // multiple invalid statuses
+        assertParseFailure(parser, "Fired Screened", "Invalid status: Fired. Valid statuses are: "
+            + String.join(", ", FilterStatusCommandParser.VALID_STATUSES));
+    }
+
+    @Test
+    public void parse_mixedValidAndInvalidArgs_throwsParseException() {
+        // valid and invalid statuses mixed
+        assertParseFailure(parser, "Applied Recruited Hired", "Invalid status: Recruited. Valid statuses are: "
+            + String.join(", ", FilterStatusCommandParser.VALID_STATUSES));
+
+        // another case with mixed valid and invalid statuses
+        assertParseFailure(parser, "Screening Promoted", "Invalid status: Promoted. Valid statuses are: "
+            + String.join(", ", FilterStatusCommandParser.VALID_STATUSES));
     }
 }
