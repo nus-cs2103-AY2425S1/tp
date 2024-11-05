40f59444b6c6b103f76f4e729199e69172221fd8
Jeremy <e1155908@u.nus.edu>
2024-11-05 13:14:27 +0800

    Add a function to toggle between past commands

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,8 +1,12 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -18,6 +22,10 @@ public class CommandBox extends UiPart<Region> {
 
     private final CommandExecutor commandExecutor;
 
+    // Attributes for command history
+    private int commandHistoryIndex = -1;
+    private List<String> commandHistory = new ArrayList<>();
+
     @FXML
     private TextField commandTextField;
 
@@ -27,8 +35,52 @@ public class CommandBox extends UiPart<Region> {
     public CommandBox(CommandExecutor commandExecutor) {
         super(FXML);
         this.commandExecutor = commandExecutor;
+
+        // Listens for key presses on the command box.
+        commandTextField.setOnKeyPressed(this::handleKeyPress);
         // calls #setStyleToDefault() whenever there is a change to the text of the command box.
         commandTextField.textProperty().addListener((unused1, unused2, unused3) -> setStyleToDefault());
+    }
+
+    private void handleKeyPress(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+        case UP:
+            navigateToPreviousCommand();
+            keyEvent.consume();
+            break;
+        case DOWN:
+            navigateToNextCommand();
+            keyEvent.consume();
+            break;
+        default:
+                // do nothing
+        }
+    }
+
+    private void navigateToPreviousCommand() {
+        if (commandHistoryIndex > 0) {
+            commandHistoryIndex--;
+            commandTextField.setText(commandHistory.get(commandHistoryIndex));
+            commandTextField.positionCaret(commandTextField.getText().length());
+        } else if (commandHistoryIndex == 0) {
+            commandTextField.setText(commandHistory.get(0));
+            commandTextField.positionCaret(commandTextField.getText().length());
+        } else {
+            commandTextField.clear();
+        }
+    }
+
+    private void navigateToNextCommand() {
+        if (commandHistoryIndex < commandHistory.size() - 1) {
+            commandHistoryIndex++;
+            commandTextField.setText(commandHistory.get(commandHistoryIndex));
+            commandTextField.positionCaret(commandTextField.getText().length());
+        } else if (commandHistoryIndex == commandHistory.size() - 1) {
+            commandHistoryIndex = commandHistory.size();
+            commandTextField.clear();
+        } else {
+            commandTextField.clear();
+        }
     }
 
     /**
@@ -40,12 +92,17 @@ public class CommandBox extends UiPart<Region> {
         if (commandText.equals("")) {
             return;
         }
-
         try {
             commandExecutor.execute(commandText);
             commandTextField.setText("");
+
         } catch (CommandException | ParseException e) {
             setStyleToIndicateCommandFailure();
+
+        } finally {
+            // Update command history
+            commandHistory.add(commandText);
+            commandHistoryIndex = commandHistory.size();
         }
     }
 
