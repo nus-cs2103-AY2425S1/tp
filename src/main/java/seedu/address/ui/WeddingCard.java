7f9228d9f828dd153a2a1766b40706379809a99b
SherniceSng <shernice.syx@gmail.com>
2024-11-05 21:30:09 +0800

    Fix issue #149

--- src/main/java/seedu/address/ui/WeddingCard.java
+++ src/main/java/seedu/address/ui/WeddingCard.java
@@ -40,7 +40,7 @@ public class WeddingCard extends UiPart<Region> {
     public WeddingCard(Wedding wedding, int displayedIndex) {
         super(FXML);
         this.wedding = wedding;
-        id.setText(displayedIndex + ".");
+        id.setText(displayedIndex + ". ");
         weddingName.setText(wedding.getName().fullName);
         date.setText(wedding.getDate() == null ? null : wedding.getDate().toString());
         venue.setText(wedding.getVenue() == null ? null : wedding.getVenue().toString());
