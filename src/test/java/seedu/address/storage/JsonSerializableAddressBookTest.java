799ad653ce4db3ab925a1a6d476887d0880a9d59
Lewis <elyreum7@gmail.com>
2024-11-05 23:29:20 +0800

    Reapply "Merge pull request #259 from SooYap/fix-group-test-case"
    This reverts commit 96946eac5616ea9fd0757b97a178c7616d13d79d.

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.AddressBook;
+import seedu.address.testutil.TypicalGroups;
 import seedu.address.testutil.TypicalPersons;
 
 public class JsonSerializableAddressBookTest {
@@ -63,13 +64,12 @@ public class JsonSerializableAddressBookTest {
     }
 
 
-    //    @Test
-    //    public void toModelType_typicalGroupsFile_success() throws Exception {
-    //        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_GROUPS_FILE,
-    //                JsonSerializableAddressBook.class).get();
-    //        AddressBook addressBookFromFile = dataFromFile.toModelType();
-    //        AddressBook typicalPersonsAddressBook = TypicalGroups.getTypicalAddressBook();
-    //        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
-    //    }
-
+    @Test
+    public void toModelType_typicalGroupsFile_success() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_GROUPS_FILE,
+                JsonSerializableAddressBook.class).get();
+        AddressBook addressBookFromFile = dataFromFile.toModelType();
+        AddressBook typicalPersonsAddressBook = TypicalGroups.getTypicalAddressBook();
+        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
+    }
 }
