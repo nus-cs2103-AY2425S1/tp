dc9fe74f91ef429ae9ade83d9773ddcb2d831b14
Slipperybara <111327931+Slipperybara@users.noreply.github.com>
2024-11-05 16:29:13 +0800

    Update the edit command
    edit command now throws prefix specific exception messages when tut/ and attend/ is used

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_PREFIX_NOT_ALLOWED;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ATTENDANCE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
@@ -9,6 +10,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PAYMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TUTORIAL;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,7 +39,14 @@ public class EditCommandParser implements Parser<EditCommand> {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
-                        PREFIX_PAYMENT, PREFIX_ATTENDANCE, PREFIX_TAG);
+                        PREFIX_PAYMENT, PREFIX_ATTENDANCE, PREFIX_TUTORIAL, PREFIX_TAG);
+
+        if (argMultimap.getValue(PREFIX_ATTENDANCE).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_PREFIX_NOT_ALLOWED, PREFIX_ATTENDANCE));
+        }
+        if (argMultimap.getValue(PREFIX_TUTORIAL).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_PREFIX_NOT_ALLOWED, PREFIX_TUTORIAL));
+        }
 
         Index index;
 
