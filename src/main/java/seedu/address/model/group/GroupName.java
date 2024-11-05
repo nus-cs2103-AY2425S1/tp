eb4ff6e58ebba71b9fd75151252a9963713e043f
Lewis <elyreum7@gmail.com>
2024-11-05 19:40:54 +0800

    Update GroupName parsing

--- src/main/java/seedu/address/model/group/GroupName.java
+++ src/main/java/seedu/address/model/group/GroupName.java
@@ -3,6 +3,9 @@ package seedu.address.model.group;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Represents a Group name in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
@@ -20,7 +23,8 @@ public class GroupName {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^(?i)CS2103T?-[A-Z]\\d+-\\d+$";
+    public static final String VALIDATION_REGEX = "^(?i)(?<coursepart>CS2103T?)-(?<tutorialpart>[A-Z]\\d+)-" +
+        "(?<grouppart>\\d+)$";
 
     private final String fullName;
 
@@ -32,7 +36,8 @@ public class GroupName {
     public GroupName(String name) {
         requireNonNull(name);
         checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
-        fullName = name.toUpperCase();
+        name = formatGroupName(name);
+        fullName = name.toUpperCase(); // leaving here for consistency through the app
     }
 
     /**
@@ -49,7 +54,33 @@ public class GroupName {
         return fullName;
     }
 
+    /**
+     * Returns reformatted group name string, based on regex.
+     */
+    private static String formatGroupName(String groupName) {
+        // this method is only called after validity of groupName is checked.
+        assert isValidName(groupName) : "Validity of group name should have been checked already.";
+        Pattern namePattern = Pattern.compile(VALIDATION_REGEX);
+        Matcher matcher = namePattern.matcher(groupName);
+        String coursePart = "";
+        String tutorialPart = "";
+        String groupPart = "";
+        if (matcher.matches()) {
+            coursePart = matcher.group("coursepart");
+            tutorialPart = matcher.group("tutorialpart");
+            groupPart = matcher.group("grouppart");
+        }
+        // removes leading 0s
+        groupPart = Integer.toString(Integer.parseInt(groupPart));
+        // removes leading 0 in number part
+        tutorialPart = tutorialPart.charAt(0)
+            + Integer.toString(Integer.parseInt(tutorialPart.substring(1)));
+        return coursePart + "-" + tutorialPart + "-" + groupPart;
+    }
+
+
     @Override
+
     public String toString() {
         return fullName;
     }
