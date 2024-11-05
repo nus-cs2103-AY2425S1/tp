2bbfe1a7e2f4939285a95d42be24786aed25be0d
ossaris56 <ossaris56@gmail.com>
2024-11-05 21:21:28 +0800

    Add DeliveredPredicate tests

---
+++ src/test/java/seedu/address/model/goodsreceipt/DeliveredPredicateTest.java
@@ -0,0 +1,52 @@
+package seedu.address.model.goodsreceipt;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.goods.Goods;
+import seedu.address.model.goods.GoodsCategories;
+import seedu.address.model.goods.GoodsName;
+import seedu.address.model.person.Name;
+
+public class DeliveredPredicateTest {
+    private static final String DATETIME_PROCUREMENT_VALID = "2024-10-10 12:00";
+    private static final String DATETIME_ARRIVAL_VALID_PENDING = "2024-12-12 12:00";
+    private static final String DATETIME_ARRIVAL_VALID_DELIVERED = "2024-12-12 12:00";
+    private final Goods testGoods = new Goods(new GoodsName("Gardenia Bread"), GoodsCategories.CONSUMABLES);
+
+    @Test
+    public void deliveredPredicateTest_delivered_predicateSuccess() {
+        GoodsReceipt testReceipt = new GoodsReceipt(testGoods, new Name("Alex Yeoh"),
+                new Date(DATETIME_PROCUREMENT_VALID), new Date(DATETIME_ARRIVAL_VALID_DELIVERED), true, 1, 1.0);
+
+        DeliveredPredicate testPredicate = new DeliveredPredicate(true);
+        boolean posResult = testPredicate.test(testReceipt);
+        assertTrue(posResult);
+    }
+
+    @Test
+    public void deliveredPredicateTest_pending_predicateSuccess() {
+        GoodsReceipt testReceipt = new GoodsReceipt(testGoods, new Name("Alex Yeoh"),
+                new Date(DATETIME_PROCUREMENT_VALID), new Date(DATETIME_ARRIVAL_VALID_PENDING), false, 1, 1.0);
+
+        DeliveredPredicate testPredicate = new DeliveredPredicate(false);
+        boolean posResult = testPredicate.test(testReceipt);
+        assertTrue(posResult);
+    }
+
+    @Test
+    public void equals() {
+        DeliveredPredicate predicate = new DeliveredPredicate(true);
+
+        // check that it is equal to itself
+        assertTrue(predicate.equals(predicate));
+
+        // check that it is equal when isDelivered is equal
+        assertTrue(predicate.equals(new DeliveredPredicate(true)));
+
+        // check that it is not equal with different isDelivered
+        assertFalse(predicate.equals(new DeliveredPredicate(false)));
+    }
+}
