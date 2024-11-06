79ae1e6e25986bcc70799ac3ebf65eea89f386df
Otto Tan <OttoTan>
2024-11-06 17:22:41 +0800

    Update EditCommand to handle behaviour when no changes are made

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -59,6 +59,7 @@ public class EditCommand extends Command {
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+    public static final String MESSAGE_NO_CHANGE_DETECTED = "No change detected, please try again.";
 
     private final Name name;
     private final EditPersonDescriptor editPersonDescriptor;
@@ -95,6 +96,10 @@ public class EditCommand extends Command {
 
         if (!personToEdit.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        if (personToEdit.equals(editedPerson)) {
+            throw new CommandException(MESSAGE_NO_CHANGE_DETECTED);
         }
 
         model.setPerson(personToEdit, editedPerson);
