8242c5fe2f313484ecb7026c4793a8f5c3c7cd84
Shaun <Hundin231@gmail.com>
2024-11-06 16:49:35 +0800

    Fix list command messages to user

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -2,8 +2,10 @@ package seedu.address.logic.commands;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.address.testutil.EmptyPersons.getEmptyAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,12 +30,19 @@ public class ListCommandTest {
 
     @Test
     public void execute_listIsNotFiltered_showsSameList() {
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListCommand(), model, String.format(ListCommand.MESSAGE_SUCCESS, 7), expectedModel);
     }
 
     @Test
     public void execute_listIsFiltered_showsEverything() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
-        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+        assertCommandSuccess(new ListCommand(), model, String.format(ListCommand.MESSAGE_SUCCESS, 7), expectedModel);
+    }
+
+    @Test
+    public void execute_listIsEmpty_showsEmptyMessage() {
+        model = new ModelManager(getEmptyAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_EMPTY_LIST, expectedModel);
     }
 }
