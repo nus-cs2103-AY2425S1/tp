0c561a30389fa00da708a97e304184715751d3c6
Zhanzhi Lou <zhanzhilou@gmail.com>
2024-11-06 20:44:00 +0800

    Update add command format

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -26,7 +26,7 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: "
-            + PREFIX_ROLE + "ROLE "
+            + "[" + PREFIX_ROLE + "ROLE] "
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
