261e1d099eaa8ae5111ac67014d5ee63f33fee7b
FionaQY <fionaandfauna@gmail.com>
2024-11-06 20:00:13 +0800

    Fix spacing

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -24,7 +24,6 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
         }
-
     }
 
 }
