42f1820d364f7a0ac3ac8ef15c272072d7df3c11
jankai <android.agent.k@gmail.com>
2024-11-06 13:02:07 +0800

    Change text response for CheckExcludedCommand

--- src/main/java/seedu/address/logic/commands/searchmode/ClearExcludedCommand.java
+++ src/main/java/seedu/address/logic/commands/searchmode/ClearExcludedCommand.java
@@ -2,13 +2,10 @@ package seedu.address.logic.commands.searchmode;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.function.Predicate;
-
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.event.EventManager;
-import seedu.address.model.person.Person;
 
 
 /**
@@ -16,12 +13,13 @@ import seedu.address.model.person.Person;
  */
 
 public class ClearExcludedCommand extends Command {
-    public static final String COMMAND_WORD = "clearexclude";
+    public static final String COMMAND_WORD = "clearexcluded";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears all excluded persons.\n"
             + "Example: " + COMMAND_WORD;
 
     public static final String MESSAGE_SUCCESS = "Cleared all excluded persons.";
+    public static final String MESSAGE_EMPTY = "No persons are currently excluded.";
 
     public ClearExcludedCommand() {
     }
@@ -30,6 +28,9 @@ public class ClearExcludedCommand extends Command {
     public CommandResult execute(Model model, EventManager eventManager) {
         requireNonNull(model);
         // Update the filtered person list without the exclusion predicate
+        if (model.getExcludedPersons().isEmpty()) {
+            return new CommandResult(MESSAGE_EMPTY);
+        }
         model.clearExcludedPersons();
         model.updateFilteredListWithExclusions(null);
         return new CommandResult(MESSAGE_SUCCESS);
