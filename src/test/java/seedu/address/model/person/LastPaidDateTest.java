ec203233d645dd326be4944a0e1a4dd287c829b5
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-11-07 00:38:20 +0800

    add test for LastPaidDate

--- src/test/java/seedu/address/model/person/LastPaidDateTest.java
+++ src/test/java/seedu/address/model/person/LastPaidDateTest.java
@@ -1,2 +1,41 @@
-package seedu.address.model.person;public class LastPaidDateTest {
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class LastPaidDateTest {
+    @Test
+    public void constructor_invalidDate_throwsIllegalArgumentException() {
+        String invalidDate = "invalid date";
+        assertThrows(IllegalArgumentException.class, () -> new LastPaidDate(invalidDate));
+    }
+
+    @Test
+    public void constructor_validDate_createsLastPaidDate() {
+        String validDate = "07 11 2024";
+        LastPaidDate lastPaidDate = new LastPaidDate(validDate);
+        assertEquals(validDate, lastPaidDate.toString());
+    }
+
+    @Test
+    public void equals_sameObject_returnsTrue() {
+        LastPaidDate lastPaidDate = new LastPaidDate("07 11 2024");
+        assertEquals(lastPaidDate, lastPaidDate);
+    }
+
+    @Test
+    public void equals_differentObjectSameValue_returnsTrue() {
+        LastPaidDate lastPaidDate1 = new LastPaidDate("07 11 2024");
+        LastPaidDate lastPaidDate2 = new LastPaidDate("07 11 2024");
+        assertEquals(lastPaidDate1, lastPaidDate2);
+    }
+
+    @Test
+    public void equals_differentObjectDifferentValue_returnsFalse() {
+        LastPaidDate lastPaidDate1 = new LastPaidDate("07 11 2024");
+        LastPaidDate lastPaidDate2 = new LastPaidDate("08 11 2024");
+        assertEquals(false, lastPaidDate1.equals(lastPaidDate2));
+    }
 }
