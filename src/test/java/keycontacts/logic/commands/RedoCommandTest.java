b8a2fa1c1bcfb0e9aed05b10bdef01fb2ef87244
Izn432 <isaac.ng432@gmail.com>
2024-11-06 21:20:59 +0800

    Fix style issues

--- src/test/java/keycontacts/logic/commands/RedoCommandTest.java
+++ src/test/java/keycontacts/logic/commands/RedoCommandTest.java
@@ -1,14 +1,15 @@
 package keycontacts.logic.commands;
 
+import static keycontacts.logic.commands.CommandTestUtil.assertCommandFailure;
+import static keycontacts.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static keycontacts.testutil.TypicalStudents.getTypicalStudentDirectory;
+
+import org.junit.jupiter.api.Test;
+
 import keycontacts.model.Model;
 import keycontacts.model.ModelManager;
 import keycontacts.model.StudentDirectory;
 import keycontacts.model.UserPrefs;
-import org.junit.jupiter.api.Test;
-
-import static keycontacts.logic.commands.CommandTestUtil.assertCommandFailure;
-import static keycontacts.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static keycontacts.testutil.TypicalStudents.getTypicalStudentDirectory;
 
 public class RedoCommandTest {
     private final Model model = new ModelManager(getTypicalStudentDirectory(), new UserPrefs());
--- src/test/java/keycontacts/logic/commands/UndoCommandTest.java
+++ src/test/java/keycontacts/logic/commands/UndoCommandTest.java
@@ -4,11 +4,11 @@ import static keycontacts.logic.commands.CommandTestUtil.assertCommandFailure;
 import static keycontacts.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static keycontacts.testutil.TypicalStudents.getTypicalStudentDirectory;
 
+import org.junit.jupiter.api.Test;
+
 import keycontacts.model.Model;
 import keycontacts.model.ModelManager;
 import keycontacts.model.UserPrefs;
-import org.junit.jupiter.api.Test;
-
 
 public class UndoCommandTest {
 
