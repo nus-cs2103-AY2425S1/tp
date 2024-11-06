cc8ee9e1e2917d1de9ace54e3767a30404100d1d
gnoossk <143413957+gnoossk@users.noreply.github.com>
2024-11-06 21:28:22 +0800

    remove unnecessary test

--- src/test/java/seedu/address/logic/commands/CloseWindowCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CloseWindowCommandTest.java
@@ -1,15 +1,11 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javafx.application.Platform;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 class CloseWindowCommandTest {
@@ -23,36 +19,6 @@ class CloseWindowCommandTest {
         closeWindowCommand = new CloseWindowCommand();
     }
 
-    @BeforeAll
-    public static void initJavaFX() {
-        if (!Platform.isFxApplicationThread()) {
-            try {
-                Platform.startup(() -> {});
-                Platform.setImplicitExit(false);
-            } catch (IllegalStateException e) {
-                // JavaFX is already initialized, no need to do anything here
-            }
-        }
-    }
-
-    @AfterAll
-    public static void closeJavaFX() {
-        if (!Platform.isFxApplicationThread()) {
-            Platform.exit();
-        }
-    }
-
-    @Test
-    public void execute_closeWindowWhenNoWindow_failure() {
-        // Ensure no AttendanceWindow is currently open
-        GetAttendanceByTgCommand.closeCurrentWindow();
-
-        // Execute CloseWindowCommand
-        CommandResult result = closeWindowCommand.execute(model);
-
-        // Verify that the correct message is returned when no window is open
-        assertEquals(new CommandResult(CloseWindowCommand.NO_WINDOW), result);
-    }
 
     @Test
     public void equals() {
