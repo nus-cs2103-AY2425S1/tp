ff363158c7ab4c196048b65ab30bc143370ea996
Dhruv <156744940+shardhrv@users.noreply.github.com>
2024-11-06 21:55:26 +0800

    Changed Telegram message as per #150

--- src/main/java/seedu/address/model/person/TelegramUsername.java
+++ src/main/java/seedu/address/model/person/TelegramUsername.java
@@ -8,8 +8,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class TelegramUsername {
     public static final String MESSAGE_CONSTRAINTS =
-            "Telegram Usernames should only contain alphanumeric characters and underscores, between 5 to 32 characters"
-                    + " and it should not be blank";
+            "Telegram Usernames should only contain alphanumeric characters and underscores, "
+                    + "between 5 to 32 characters";
 
     /*
      * The first character of the address must be an alphabet.
