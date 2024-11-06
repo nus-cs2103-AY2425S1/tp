fe13c2db57788a9d8eadd8a8feaf60c165575670
Daniel Lai <e0360577@u.nus.edu>
2024-11-06 12:33:04 +0800

    Finish incomplete refactoring

--- src/main/java/hallpointer/address/logic/commands/ClearCommand.java
+++ src/main/java/hallpointer/address/logic/commands/ClearCommand.java
@@ -6,13 +6,13 @@ import hallpointer.address.model.HallPointer;
 import hallpointer.address.model.Model;
 
 /**
- * Clears the address book.
+ * Clears the CCA system.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
 
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "CCA system has been cleared!";
 
     @Override
     public CommandResult execute(Model model) {
--- src/main/java/hallpointer/address/model/tag/Tag.java
+++ src/main/java/hallpointer/address/model/tag/Tag.java
@@ -4,7 +4,7 @@ import static hallpointer.address.commons.util.AppUtil.checkArgument;
 import static java.util.Objects.requireNonNull;
 
 /**
- * Represents a Tag in the address book.
+ * Represents a Tag in the hall pointer.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
  */
 public class Tag {
