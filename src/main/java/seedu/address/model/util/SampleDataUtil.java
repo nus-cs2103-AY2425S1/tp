7adca10b2371dbe2aa93985f568582f968112d37
yyueyii <e1271791@u.nus.edu>
2024-11-06 14:03:10 +0800

    Change default sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.AttendanceCount;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.person.Role;
+import seedu.address.model.person.Student;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -20,30 +22,34 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Role("Student"), new Phone("87438807"),
+            new Student(new Name("Alex Yeoh"), new Role("Student"), new Phone("87438807"),
                     new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Role("Student"), new Phone("99272758"),
+                getTagSet("friends"),
+                new AttendanceCount("1")),
+            new Student(new Name("Bernice Yu"), new Role("Student"), new Phone("99272758"),
                     new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Role("Student"), new Phone("93210283"),
+                getTagSet("colleagues", "friends"),
+                    new AttendanceCount("1")),
+            new Student(new Name("Charlotte Oliveiro"), new Role("Student"), new Phone("93210283"),
                     new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Role("Student"), new Phone("91031282"),
+                getTagSet("neighbours"),
+                    new AttendanceCount("2")),
+            new Person(new Name("David Li"), new Role("Parent"), new Phone("91031282"),
                     new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                 getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Role("Student"), new Phone("92492021"),
+            new Person(new Name("Irfan Ibrahim"), new Role("Parent"), new Phone("92492021"),
                     new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Role("Student"), new Phone("92624417"),
+            new Student(new Name("Roy Balakrishnan"), new Role("Student"), new Phone("92624417"),
                     new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+                getTagSet("colleagues"),
+                    new AttendanceCount("3"))
         };
     }
 
