7babbe076bfc4506f60a061f64e536c173bb97ae
Wong Wei Han <e0958752@nus.edu.sg>
2024-11-06 16:10:03 +0800

    Fix JsonAddressBookStorage

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -16,13 +16,13 @@
 //public class JsonSerializableAddressBookTest {
 //
 //    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-//    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalRestaurantsAddressBook.json");
-//    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidRestaurantAddressBook.json");
-//    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicateRestaurantAddressBook.json");
+//    private static final Path TYPICAL_RESTAURANTS_FILE = TEST_DATA_FOLDER.resolve("typicalRestaurantsAddressBook.json");
+//    private static final Path INVALID_RESTAURANT_FILE = TEST_DATA_FOLDER.resolve("invalidRestaurantAddressBook.json");
+//    private static final Path DUPLICATE_RESTAURANT_FILE = TEST_DATA_FOLDER.resolve("duplicateRestaurantAddressBook.json");
 //
 //    @Test
 //    public void toModelType_typicalRestaurantsFile_success() throws Exception {
-//        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
+//        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_RESTAURANTS_FILE,
 //                JsonSerializableAddressBook.class).get();
 //        AddressBook addressBookFromFile = dataFromFile.toModelType();
 //        AddressBook typicalRestaurantsAddressBook = TypicalRestaurants.getTypicalAddressBook();
@@ -31,16 +31,16 @@
 //
 //    @Test
 //    public void toModelType_invalidRestaurantFile_throwsIllegalValueException() throws Exception {
-//        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
+//        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_RESTAURANT_FILE,
 //                JsonSerializableAddressBook.class).get();
 //        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
 //    }
 //
 //    @Test
 //    public void toModelType_duplicateRestaurants_throwsIllegalValueException() throws Exception {
-//        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
+//        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_RESTAURANT_FILE,
 //                JsonSerializableAddressBook.class).get();
-//        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON,
+//        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_RESTAURANT,
 //                dataFromFile::toModelType);
 //    }
 //
