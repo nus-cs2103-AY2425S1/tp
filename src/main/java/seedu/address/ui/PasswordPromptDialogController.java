1d649b918b4e943095a9586091f39beb6f2b5f5a
anselmlong <anselmpius@gmail.com>
2024-11-06 23:30:40 +0800

    Enhance UI for password prompt dialog

---
+++ src/main/java/seedu/address/ui/PasswordPromptDialogController.java
@@ -0,0 +1,86 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import seedu.address.security.PasswordManager;
+
+/**
+ * Controller class for the PasswordPromptDialog.
+ */
+public class PasswordPromptDialogController {
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label passwordLabel;
+    private Stage dialog;
+    private String existingPassword;
+
+    public void setDialog(Stage dialog) {
+        this.dialog = dialog;
+    }
+
+    public void setExistingPassword(String existingPassword) {
+        this.existingPassword = existingPassword;
+    }
+
+    public void setPasswordLabel() {
+        if (existingPassword == null) {
+            passwordLabel.setText("Please set a new password:");
+        } else {
+            passwordLabel.setText("Please enter your password:");
+        }
+    }
+
+    /**
+     * Allows the user to press "Enter" to confirm the password.
+     * If the password is correct, the dialog will close.
+     * If the password is incorrect, an alert will be shown.
+     */
+    @FXML
+    private void initialize() {
+        // Set the action for pressing "Enter" while focused on the password field
+        passwordField.setOnAction(event -> handleConfirm());
+    }
+
+    /**
+     * Handles the confirm button press.
+     */
+    @FXML
+    private void handleConfirm() {
+        String inputPassword = passwordField.getText();
+        if (existingPassword == null) {
+            PasswordManager.savePassword(inputPassword);
+            dialog.close();
+        } else if (PasswordManager.isPasswordCorrect(inputPassword)) {
+            dialog.close();
+        } else {
+            showAlert("Incorrect Password!", "Please try again.");
+        }
+    }
+
+    @FXML
+    private void handleCancel() {
+        dialog.close();
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+
+        // Apply font styling to the alert's dialog pane
+        alert.getDialogPane().setStyle(
+                "-fx-font-family: 'Consolas'; -fx-font-size: 13pt; -fx-font-weight: bold;"
+        );
+        alert.showAndWait();
+    }
+
+    public PasswordField getPasswordField() {
+        return passwordField;
+    }
+}
