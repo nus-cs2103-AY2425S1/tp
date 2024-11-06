81e0e43a7d1718a57ec621a50510ab4af4db4fea
yi hao <choiyihao123@gmail.com>
2024-11-06 14:38:33 +0800

    Allow add command to take in nf/ prefix

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -11,7 +11,6 @@ import seedu.address.model.person.Person;
  * Container for user visible messages.
  */
 public class Messages {
-    
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
