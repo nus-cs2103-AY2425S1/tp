0d37212f47b87ca24705086de550ed31283397f4
wenjebs <wenjieb2@gmail.com>
2024-11-06 17:50:13 +0800

    Add test coverage to group command

--- src/test/java/seedu/address/logic/commands/GroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/GroupCommandTest.java
@@ -11,6 +11,7 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.group.Group;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.Assert;
 
 public class GroupCommandTest {
     private Model model;
@@ -34,8 +36,8 @@ public class GroupCommandTest {
 
     @Test
     public void execute_validGroup_success() {
-        List<String> studentsNames =
-                List.of(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()).getName().fullName);
+        List<String> studentsNames = List
+                .of(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()).getName().fullName);
         GroupCommand groupCommand = new GroupCommand("StudyGroup1", studentsNames);
 
         String expectedMessage = String.format(GroupCommand.MESSAGE_SUCCESS, "StudyGroup1", studentsNames.size());
@@ -48,8 +50,8 @@ public class GroupCommandTest {
 
     @Test
     public void execute_duplicateGroup_failure() {
-        List<String> studentsNames =
-                List.of(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()).getName().fullName);
+        List<String> studentsNames = List
+                .of(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()).getName().fullName);
         GroupCommand groupCommand = new GroupCommand("StudyGroup1", studentsNames);
         List<Person> students = List.of(model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased()));
         model.addGroup(new Group("StudyGroup1", students));
@@ -67,6 +69,79 @@ public class GroupCommandTest {
         String expectedMessage = GroupCommand.MESSAGE_NO_STUDENTS_FOUND;
 
         assertCommandFailure(groupCommand, model, expectedMessage);
+    }
+
+    @Test
+    public void execute_duplicateStudentNamesInInput_throwsCommandException() {
+        // Prepare duplicate student names
+        String duplicateStudentName = model.getFilteredPersonList().get(0).getName().fullName;
+        List<String> studentNames = List.of(duplicateStudentName, duplicateStudentName);
+        String groupName = "StudyGroup2";
+
+        GroupCommand groupCommand = new GroupCommand(groupName, studentNames);
+
+        String expectedMessage = String.format(GroupCommand.DUPLICATE_STUDENT_FOUND, duplicateStudentName);
+
+        assertCommandFailure(groupCommand, model, expectedMessage);
+    }
+
+    @Test
+    public void execute_emptyStudentName_throwsCommandException() {
+        // Prepare student names with an empty string
+        List<String> studentNames = List.of("");
+        String groupName = "StudyGroup4";
+
+        GroupCommand groupCommand = new GroupCommand(groupName, studentNames);
+
+        assertCommandFailure(groupCommand, model, GroupCommand.EMPTY_STUDENT);
+    }
+
+    @Test
+    public void execute_noMatchingStudentsFound_throwsCommandException() {
+        // Prepare student names that do not match any existing person
+        List<String> studentNames = List.of("NonExistentStudent");
+        String groupName = "StudyGroup5";
+
+        GroupCommand groupCommand = new GroupCommand(groupName, studentNames);
+
+        assertCommandFailure(groupCommand, model, String.format(GroupCommand.STUDENTS_NOT_FOUND,
+                "NonExistentStudent"));
+    }
+
+    @Test
+    public void execute_partialMatchingStudentsFound_throwsCommandException() {
+        // Prepare a mix of valid and invalid student names
+        String validStudentName = model.getFilteredPersonList().get(0).getName().fullName;
+        String invalidStudentName = "NonExistentStudent";
+        List<String> studentNames = List.of(validStudentName, invalidStudentName);
+        String groupName = "StudyGroup6";
+
+        GroupCommand groupCommand = new GroupCommand(groupName, studentNames);
+
+        String expectedMessage = String.format(GroupCommand.STUDENTS_NOT_FOUND, invalidStudentName);
+
+        assertCommandFailure(groupCommand, model, expectedMessage);
+    }
+
+    @Test
+    public void constructor_nullGroupName_throwsNullPointerException() {
+        List<String> studentNames = new ArrayList<>();
+        Assert.assertThrows(NullPointerException.class, () -> new GroupCommand(null, studentNames));
+    }
+
+    @Test
+    public void constructor_nullStudentsList_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new GroupCommand("GroupName", null));
+    }
+
+    @Test
+    public void execute_emptyStudentsList_throwsCommandException() {
+        List<String> studentNames = new ArrayList<>();
+        String groupName = "StudyGroup7";
+
+        GroupCommand groupCommand = new GroupCommand(groupName, studentNames);
+
+        assertCommandFailure(groupCommand, model, GroupCommand.MESSAGE_NO_STUDENTS_FOUND);
     }
 
     @Test
