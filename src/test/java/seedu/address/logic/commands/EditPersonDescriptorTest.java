3b0c898a9cc886f25c5da886dbfd79b560a03305
Albert Ariel Putra <142318694+hpcman@users.noreply.github.com>
2024-11-07 06:34:46 +0700

    Change default data tag to more relevant words

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -9,7 +9,7 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_COURSE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_SENIOR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_STRUGGLING;
 
 import org.junit.jupiter.api.Test;
 
@@ -53,7 +53,7 @@ public class EditPersonDescriptorTest {
         assertFalse(DESC_AMY.equals(editedAmy));
 
         // different tags -> returns false
-        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_SENIOR).build();
+        editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_STRUGGLING).build();
         assertFalse(DESC_AMY.equals(editedAmy));
     }
 
