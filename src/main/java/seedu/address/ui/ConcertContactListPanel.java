1ec6efabbba2a3a36a33b499a4d434515ddb7c61
hj235 <hongjing235@gmail.com>
2024-11-07 01:53:14 +0800

    Changed truncation to wrap lines

--- src/main/java/seedu/address/ui/ConcertContactListPanel.java
+++ src/main/java/seedu/address/ui/ConcertContactListPanel.java
@@ -26,7 +26,25 @@ public class ConcertContactListPanel extends UiPart<Region> {
     public ConcertContactListPanel(ObservableList<ConcertContact> concertContactList) {
         super(FXML);
         concertContactListView.setItems(concertContactList);
-        concertContactListView.setCellFactory(listView -> new ConcertContactListViewCell());
+        concertContactListView.setCellFactory(listView -> new ConcertContactListViewCell(false));
+    }
+
+    /**
+     * Sets the list to show full details of its {@code Concert} cards.
+     */
+    public void showFullConcert() {
+        concertContactListView.setCellFactory(
+                listView -> new ConcertContactListPanel.ConcertContactListViewCell(true));
+        concertContactListView.refresh();
+    }
+
+    /**
+     * Sets the list to hide full details of its {@code Concert} cards.
+     */
+    public void hideFullConcert() {
+        concertContactListView.setCellFactory(
+                listView -> new ConcertContactListPanel.ConcertContactListViewCell(false));
+        concertContactListView.refresh();
     }
 
     /**
@@ -34,6 +52,18 @@ public class ConcertContactListPanel extends UiPart<Region> {
      * {@code ConcertContactCard}.
      */
     class ConcertContactListViewCell extends ListCell<ConcertContact> {
+        private final boolean showFullDetails;
+
+        /**
+         * Creates a {@code ConcertContactListViewCell} with {@param showFullDetails} to indicate whether the details
+         * of the {@code ConcertContactCard} should be truncated or wrapped to a new line.
+         *
+         * @param showFullDetails
+         */
+        protected ConcertContactListViewCell(boolean showFullDetails) {
+            this.showFullDetails = showFullDetails;
+        }
+
         @Override
         protected void updateItem(ConcertContact concertContact, boolean empty) {
             super.updateItem(concertContact, empty);
@@ -42,7 +72,8 @@ public class ConcertContactListPanel extends UiPart<Region> {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new ConcertContactCard(concertContact, getIndex() + 1).getRoot());
+                setGraphic(new ConcertContactCard(concertContact, getIndex() + 1, showFullDetails)
+                        .getRoot());
             }
         }
     }
--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -25,6 +25,7 @@ public class PersonCard extends UiPart<Region> {
      */
 
     public final Person person;
+    private boolean showFullDetails;
 
     @FXML
     private HBox cardPane;
@@ -46,7 +47,7 @@ public class PersonCard extends UiPart<Region> {
     /**
      * Creates a {@code PersonCard} with the given {@code Person} and index to display.
      */
-    public PersonCard(Person person, int displayedIndex) {
+    public PersonCard(Person person, int displayedIndex, boolean showFullDetails) {
         super(FXML);
         this.person = person;
         id.setText(displayedIndex + ". ");
@@ -58,5 +59,17 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        this.showFullDetails = showFullDetails;
+
+        setLabelsToWrapText();
+    }
+
+    private void setLabelsToWrapText() {
+        id.setWrapText(showFullDetails);
+        name.setWrapText(showFullDetails);
+        phone.setWrapText(showFullDetails);
+        address.setWrapText(showFullDetails);
+        email.setWrapText(showFullDetails);
+        role.setWrapText(showFullDetails);
     }
 }
