2a21c2b01c518e7ed1a66215be3047bb6c87d63d
QuekBingHeng <quekbingheng.17@gmail.com>
2024-11-06 19:13:52 +0800

    Add checkstyle change

--- src/test/java/seedu/address/logic/commands/EditScheduleCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditScheduleCommandTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MEETING;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_MEETING;
-import static seedu.address.testutil.TypicalMeetings.getAlmostDuplicateMeetings;
 import static seedu.address.testutil.TypicalMeetings.getTypicalMeetings;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
