ebe01c4c1e35a1dbcc9a6aec09cd44f7ede7076a
beginner-web-developer <e1156242@u.nus.edu>
2024-11-06 08:48:30 +0800

    Increase code quality

--- src/main/java/seedu/address/logic/commands/ListTransactionCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTransactionCommand.java
@@ -3,7 +3,9 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -24,6 +26,7 @@ public class ListTransactionCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Listed %1$d transaction(s) for %2$s";
 
     private final Index index;
+    private final Logger logger = LogsCenter.getLogger(ListTransactionCommand.class);
 
     /**
      * @param index the index of the person to view transactions of.
@@ -38,15 +41,18 @@ public class ListTransactionCommand extends Command {
         requireNonNull(model);
 
         if (model.getIsViewTransactions()) {
+            logger.fine("CommandException caused by attempt to use listt command in transaction view.");
             throw new CommandException(String.format(Messages.MESSAGE_MUST_BE_PERSON_LIST, COMMAND_WORD));
         }
 
         List<Person> lastShownList = model.getFilteredPersonList();
         if (index.getZeroBased() >= lastShownList.size()) {
+            logger.fine("CommandException caused by invalid person index provided.");
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
         Person selected = lastShownList.get(index.getZeroBased());
+        assert selected != null : "Person should not be null";
         model.updateFilteredPersonList(new IsSelectedPredicate(model, index));
         model.updateTransactionList(selected.getTransactions());
         return new CommandResult(String.format(MESSAGE_SUCCESS,
