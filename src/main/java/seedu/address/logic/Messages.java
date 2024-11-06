207a9a16a5df83d9ed51b435ea9c211e627cb55c
Karthikraj Sivakumar <techraj0207@gmail.com>
2024-11-06 23:52:37 +0800

    Fix Typo
    error message said 'by' instead of 'be'

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -12,7 +12,7 @@ import seedu.address.model.person.Person;
  */
 public class Messages {
 
-    public static final String MESSAGE_EMPTY_FIELD = "field should not by empty";
+    public static final String MESSAGE_EMPTY_FIELD = "field should not be empty";
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_PERSON_NRIC_NOT_FOUND = "The patient with the specified NRIC does not exist";
