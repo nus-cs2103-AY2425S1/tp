e47e4e706c05627904d522948e15b6f26988c2b4
leroytan <mingxiang130@gmail.com>
2024-11-07 03:12:44 +0800

    Update command message
    Include both long and short form in the message and example

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -25,7 +25,9 @@ public class AddCommand extends Command {
     public static final String SHORT_COMMAND_WORD = ":a";
     public static final String LONG_COMMAND_WORD = ":add";
 
-    public static final String MESSAGE_USAGE = SHORT_COMMAND_WORD + " or " + LONG_COMMAND_WORD
+    public static final String MESSAGE_USAGE =
+            "\"" + SHORT_COMMAND_WORD + "\"" + " OR "
+            + "\"" + LONG_COMMAND_WORD + "\""
             + ": Adds a person to the address book. "
             + "\nCompulsory parameters: "
             + PREFIX_NAME + "NAME "
@@ -51,10 +53,10 @@ public class AddCommand extends Command {
     public static final String COMMAND_SUMMARY_ACTION = "Add";
     public static final String COMMAND_SUMMARY_FORMAT =
             ":add -n NAME -p PHONE_NUMBER -e EMAIL -l LOCATION -t TAG -r REMARK…\n"
-            + ":a -n NAME -p PHONE_NUMBER -e EMAIL -l LOCATION -t TAG -r REMARK…";
+                    + ":a -n NAME -p PHONE_NUMBER -e EMAIL -l LOCATION -t TAG -r REMARK…";
     public static final String COMMAND_SUMMARY_EXAMPLES =
             ":add -n James Ho -p 98765432 -e jamesho@example.com -l 123, "
-                   + "Clementi Rd, 1234665 -t friend -r My favourite colleague";
+                    + "Clementi Rd, 1234665 -t friend -r My favourite colleague";
 
     public static final List<String> INVALID_VARIANTS = Arrays.asList("add", "a");
     private final Person toAdd;
--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -13,8 +13,12 @@ public class HelpCommand extends Command {
     public static final String SHORT_COMMAND_WORD = ":h";
     public static final String LONG_COMMAND_WORD = ":help";
 
-    public static final String MESSAGE_USAGE = LONG_COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + LONG_COMMAND_WORD;
+    public static final String MESSAGE_USAGE =
+            "\"" + SHORT_COMMAND_WORD + "\"" + " OR "
+            + "\"" + LONG_COMMAND_WORD + "\""
+            +  ": Shows program usage instructions.\n"
+            + "Example 1: " + LONG_COMMAND_WORD + "\n"
+            + "Example 2: " + SHORT_COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE = "Opened command cheatsheet.";
     public static final String COMMAND_SUMMARY_ACTION = "Help";
