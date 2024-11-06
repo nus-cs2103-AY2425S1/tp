420d39617fcca8b34b9dce3af1d17a36955dade7
thenosewizard <abicharan.b@gmail.com>
2024-11-06 20:06:20 +0800

    Add test

--- src/test/java/seedu/address/model/ReceiptLogTest.java
+++ src/test/java/seedu/address/model/ReceiptLogTest.java
@@ -1,4 +1,17 @@
 package seedu.address.model;
 
+import org.junit.jupiter.api.Test;
+
 public class ReceiptLogTest {
+
+    private ReceiptLog receiptLog = new ReceiptLog();
+    @Test
+    public void equals_sameContent_success() {
+        //TODO
+    }
+
+    @Test
+    public void hashcode_sameContent_success() {
+        //TODO
+    }
 }
