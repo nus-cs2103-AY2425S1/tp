bb303ab9e3aeef134725388192721d3f2c6751ef
annabellekk <yuyien.khoo@gmail.com>
2024-11-06 11:15:06 +0800

    Update help window messages and format

--- src/main/java/seedu/academyassist/model/AcademyAssist.java
+++ src/main/java/seedu/academyassist/model/AcademyAssist.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import javafx.collections.ObservableList;
 import seedu.academyassist.commons.util.ToStringBuilder;
+import seedu.academyassist.logic.parser.SortCommandParser;
 import seedu.academyassist.model.person.Ic;
 import seedu.academyassist.model.person.Person;
 import seedu.academyassist.model.person.StudentId;
@@ -178,7 +179,6 @@ public class AcademyAssist implements ReadOnlyAcademyAssist {
             person = person.addSubject(s);
         }
         persons.add(person);
-
     }
 
     //// student count operations
