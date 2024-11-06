b109f51a5161de9ed85a6aff13093335cc66f0c7
DiegoTheExplorar <133507153+DiegoTheExplorar@users.noreply.github.com>
2024-11-07 02:13:42 +0800

    Incorrect merge conflict resolution
    - I am sorry
    - I made the wrong merged wrongly

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -8,11 +8,11 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
  */
 public class Tag {
+
     public static final String MESSAGE_TAG_NAMES_SHOULD_BE_ALPHANUMERIC = "Tag names should be alphanumeric";
     public static final String MESSAGE_TAG_NAMES_CANNOT_BE_EMPTY = "Tag names cannot be empty.";
     public static final String MESSAGE_TAG_NAME_OR_VALUE_MISSING = "Tag name or value is missing";
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}]+";
-    public static final String MESSAGE_CONSTRAINTS = "Tags names should be alphanumeric";
+    public static final String VALIDATION_REGEX = "(\\p{Alpha}+|\\d+(\\.\\d+)?)";
 
     public final String tagName;
     public final String tagValue;
