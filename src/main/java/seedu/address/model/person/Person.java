d45f8db4c390341c87c6eb3e0ccbe69b3a2c65f9
Plishh <101051455+Plishh@users.noreply.github.com>
2024-11-06 14:32:39 +0800

    Add JsonSerializableAddressBookTest

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -115,7 +115,7 @@ public abstract class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && otherPerson.getName().equals(getName()) && otherPerson.getPhone().equals(getPhone()) && otherPerson.isTutee() == this.isTutee();
     }
 
     /**
