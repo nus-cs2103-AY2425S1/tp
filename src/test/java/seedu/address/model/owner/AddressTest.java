36d4b7149d6195cb6cb12c2249dec8118ec29dc2
Jonathan Loh <jonathanloh02@gmail.com>
2024-11-06 21:08:06 +0800

    FIx tests and validation

--- src/test/java/seedu/address/model/owner/AddressTest.java
+++ src/test/java/seedu/address/model/owner/AddressTest.java
@@ -27,10 +27,12 @@ public class AddressTest {
         // invalid addresses
         assertFalse(Address.isValidAddress("")); // empty string
         assertFalse(Address.isValidAddress(" ")); // spaces only
+        assertFalse(Address.isValidAddress("-")); // one character
+        assertFalse(Address.isValidAddress("-a")); // two character
 
         // valid addresses
         assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
-        assertTrue(Address.isValidAddress("-")); // one character
+        assertTrue(Address.isValidAddress("abc")); // three character
         assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
     }
 
