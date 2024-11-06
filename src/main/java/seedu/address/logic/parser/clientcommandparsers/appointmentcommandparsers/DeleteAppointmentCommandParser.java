76aee7dd7fac98f1f615ac92863d47e0ade6e03f
lrongyi <iygnormal@gmail.com>
2024-11-06 10:21:33 +0800

    Refactor Code
    Refactor commands and their parsers into packages:
    - Clients (and Appointments)
    - Listings
    
    Rename classes for standardisation and to differentiate
    between Client and Listing commands/parsers

---
+++ src/main/java/seedu/address/logic/parser/clientcommandparsers/appointmentcommandparsers/DeleteAppointmentCommandParser.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser.clientcommandparsers.appointmentcommandparsers;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.logic.commands.clientcommands.appointmentcommands.DeleteAppointmentCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Name;
+
+/**
+ * Parses input arguments and creates a new {@code DeleteAppointmentCommand} object.
+ */
+public class DeleteAppointmentCommandParser implements Parser<DeleteAppointmentCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments and returns a {@code DeleteAppointmentCommand} object for execution.
+     *
+     * @param args User input containing the client's name prefixed with "n/".
+     * @return A {@code DeleteAppointmentCommand} object.
+     * @throws ParseException If the user input does not conform to the expected format or the name is missing.
+     */
+    public DeleteAppointmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(args);
+
+        try {
+            // Parse the name and return the DeleteAppointmentCommand
+            Name name = ParserUtil.parseName(argumentMultimap.getPreamble());
+            return new DeleteAppointmentCommand(name);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    DeleteAppointmentCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
---
+++ src/test/java/seedu/address/logic/parser/clientcommandparsers/MoreInfoCommandParserTest.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.parser.clientcommandparsers;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.clientcommands.MoreInfoCommand;
+import seedu.address.logic.parser.clientcommandparsers.MoreInfoCommandParser;
+
+public class MoreInfoCommandParserTest {
+    private MoreInfoCommandParser parser = new MoreInfoCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsMoreInfoCommand() {
+        assertParseSuccess(parser, "Alice Pauline", new MoreInfoCommand(ALICE.getName()));
+    }
+
+    @Test
+    public void parse_missingField_throwsParseException() {
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                MoreInfoCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "$$", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                MoreInfoCommand.MESSAGE_USAGE));
+    }
+}
