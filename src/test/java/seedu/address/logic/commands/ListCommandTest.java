a4f11ebcf13cb9f8c01e76558c44efd9b0de4ada
Shaun <Hundin231@gmail.com>
2024-11-06 16:54:54 +0800

    Fix checkstyle issue with imports

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -6,7 +6,6 @@ import static seedu.address.testutil.EmptyPersons.getEmptyAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
