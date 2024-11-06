2a87f0dcc73bd54323f3ee8567465605998d1d95
Jonathan Loh <jonathanloh02@gmail.com>
2024-11-06 15:38:06 +0800

    Disallow editing of owner NRIC

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -1,10 +1,21 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AGE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_BREED;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SEX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SPECIES;
+
+import java.util.Arrays;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.Prefix;
 import seedu.address.model.Model;
 
 /**
@@ -18,7 +29,13 @@ public abstract class EditCommand<T> extends Command {
         + "Existing values will be overwritten by the input values.\n"
         + "To edit owners: edit o[INDEX] (must be a positive integer) KEYWORD [MORE_KEYWORDS]...\n"
         + "To edit pets: edit p[INDEX] (must be a positive integer) KEYWORD [MORE_KEYWORDS]...\n"
-        + "Example: edit o1 n/bobby, edit p1 n/tom";
+        + "Example: edit o1 n/bobby, edit p1 n/tom\n"
+        + "Allowed field tags for owners: "
+        + String.join(", ", Arrays.stream(new Prefix[]{PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS})
+          .map(Prefix::toString).toArray(String[]::new)) + "\n"
+        + "Allowed field tags for pets: "
+        + String.join(", ", Arrays.stream(new Prefix[]{PREFIX_NAME, PREFIX_SPECIES, PREFIX_BREED, PREFIX_AGE, PREFIX_SEX})
+          .map(Prefix::toString).toArray(String[]::new)) + "\n";
 
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
 
--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -67,8 +67,7 @@ public class EditCommandParser implements Parser<EditCommand<?>> {
         requireNonNull(args);
 
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_IC_NUMBER, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                PREFIX_ADDRESS);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
 
         Index index;
 
@@ -78,15 +77,10 @@ public class EditCommandParser implements Parser<EditCommand<?>> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
 
-        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_IC_NUMBER, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-            PREFIX_ADDRESS);
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
 
         EditOwnerDescriptor editOwnerDescriptor = new EditOwnerDescriptor();
 
-        if (argMultimap.getValue(PREFIX_IC_NUMBER).isPresent()) {
-            editOwnerDescriptor.setIdentificationNumber(
-                ParserUtil.parseOwnerIcNumber(argMultimap.getValue(PREFIX_IC_NUMBER).get()));
-        }
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
             editOwnerDescriptor.setName(ParserUtil.parseOwnerName(argMultimap.getValue(PREFIX_NAME).get()));
         }
