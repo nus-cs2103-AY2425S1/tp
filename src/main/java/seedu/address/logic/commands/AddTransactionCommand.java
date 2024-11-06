1db2851bc0e47856fb36795174a45196d3f66d50
kngys <kristiangys@gmail.com>
2024-11-06 11:03:04 +0800

    Update logging

--- src/main/java/seedu/address/logic/commands/AddTransactionCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTransactionCommand.java
@@ -68,7 +68,7 @@ public class AddTransactionCommand extends Command {
 
         List<Person> lastShownList = model.getFilteredPersonList();
         if (index.getZeroBased() >= lastShownList.size()) {
-            logger.fine("CommandException caused by invalid index.");
+            logger.fine("CommandException caused by invalid index provided.");
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
