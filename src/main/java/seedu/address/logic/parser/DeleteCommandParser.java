383d876fa6166a733d0148c030c5724e594733da
Jonathan Loh <jonathanloh02@gmail.com>
2024-11-06 16:29:41 +0800

    Fix delete command error message

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -23,8 +23,10 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             Pair p = ParserUtil.parseIndexAndType(args);
             if (p.second().equals("p")) {
                 return new DeletePetCommand(p.first());
-            } else {
+            } else if (p.second().equals("o")) {
                 return new DeleteOwnerCommand(p.first());
+            } else {
+                throw new ParseException("Invalid type to delete");
             }
         } catch (ParseException pe) {
             throw new ParseException(
