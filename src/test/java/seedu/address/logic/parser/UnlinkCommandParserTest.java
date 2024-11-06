88312ea8765989f0e0dc878411494b9c9f72253f
Elliot Tang <elliot.tang.379@gmail.com>
2024-11-06 19:03:03 +0800

    Add test for Parser

--- src/test/java/seedu/address/logic/parser/UnlinkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/UnlinkCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_NON_EMPTY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CHILD;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -36,6 +37,13 @@ public class UnlinkCommandParserTest {
     public void parse_invalidStudentNameField_failure() {
         assertParseFailure(parser, INVALID_CHILD_NAME_DESC, Name.MESSAGE_CONSTRAINTS);
         assertParseFailure(parser, " " + PREFIX_CHILD, Name.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_invalidCommandFormat_failure() {
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY, UnlinkCommand.MESSAGE_USAGE);
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + VALID_CHILD_NAME_DESC_JOHN,
+                UnlinkCommand.MESSAGE_USAGE);
     }
 
     @Test
