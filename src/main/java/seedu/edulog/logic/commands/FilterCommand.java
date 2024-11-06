204d4a82df1dd0dbd90a6e4cf99204a03841673c
Timenikhil <48746822+Timenikhil@users.noreply.github.com>
2024-11-06 20:24:39 +0800

    Add a new filter command
    Filter students based on payment status.

---
+++ src/main/java/seedu/edulog/logic/commands/FilterCommand.java
@@ -0,0 +1,58 @@
+package seedu.edulog.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import seedu.edulog.commons.util.ToStringBuilder;
+import seedu.edulog.logic.Messages;
+import seedu.edulog.model.Model;
+import seedu.edulog.model.student.NameContainsKeywordsPredicate;
+import seedu.edulog.model.student.StudentHasPaidPredicate;
+
+/**
+ * Finds and lists all students in edulog book whose name contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all students whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+    private final StudentHasPaidPredicate predicate;
+
+    public FilterCommand(NameContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredStudentList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW, model.getFilteredStudentList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        FilterCommand otherFindCommand = (FilterCommand) other;
+        return predicate.equals(otherFindCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/edulog/model/student/StudentHasPaidPredicate.java
@@ -0,0 +1,41 @@
+package seedu.edulog.model.student;
+
+import java.util.function.Predicate;
+
+import seedu.edulog.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Student}'s {@code hasPaid} matches the payment status specified.
+ */
+public class StudentHasPaidPredicate implements Predicate<Student> {
+    private final boolean hasPaid;
+
+    public StudentHasPaidPredicate(boolean hasPaid) {
+        this.hasPaid = hasPaid;
+    }
+
+    @Override
+    public boolean test(Student student) {
+        return student.getHasPaid() == hasPaid;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof StudentHasPaidPredicate)) {
+            return false;
+        }
+
+        StudentHasPaidPredicate otherStudentHasPaidPredicate = (StudentHasPaidPredicate) other;
+        return hasPaid == otherStudentHasPaidPredicate.hasPaid;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("hasPaid", hasPaid).toString();
+    }
+}
