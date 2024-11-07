a0be1becd41b54cbc2a358fd37a38b3c9596bf23
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-11-07 21:36:41 +0800

    Use getCause() instead of string matching

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -158,7 +158,7 @@ public class ParserUtil {
         try {
             return LocalDateTime.parse(trimmedDate, formatter);
         } catch (DateTimeParseException e) {
-            if (e.getMessage().contains("Invalid")) {
+            if (e.getCause() != null) {
                 throw new ParseException("Invalid date");
             } else {
                 throw new ParseException("Date must be in the format: " + AttendanceList.DATE_TIME_FORMAT);
