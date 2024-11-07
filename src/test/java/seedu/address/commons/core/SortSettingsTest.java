c8acd8e2e836628bda9242f57728c078139011aa
AbdulrahmanAlRammah <142239643+AbdulrahmanAlRammah@users.noreply.github.com>
2024-11-07 19:08:14 +0800

    Fix Checkstyle test

--- src/test/java/seedu/address/commons/core/SortSettingsTest.java
+++ src/test/java/seedu/address/commons/core/SortSettingsTest.java
@@ -3,7 +3,6 @@ package seedu.address.commons.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.commons.util.ToStringBuilder;
 
 public class SortSettingsTest {
     @Test
