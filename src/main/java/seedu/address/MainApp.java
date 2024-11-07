0bf7f372dd2bb673b629837626f2bf2a1dadb659
LeeZeHao <zehaolee2004@gmail.com>
2024-11-07 12:02:27 +0800

    Change save file name to hrconnect.json

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -51,7 +51,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing AddressBook ]===========================");
+        logger.info("=============================[ Initializing HRConnect ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
