f0ed6c232bfe5ae7aaee1bd24e44ff05a494d52f
tohjh <h1710141@nushigh.edu.sg>
2024-11-07 12:58:31 +0800

    Isolate test cases

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -7,6 +7,10 @@ import static seedu.address.logic.commands.ImportCommand.MESSAGE_READING_ERROR;
 import static seedu.address.model.person.Name.MESSAGE_CONSTRAINTS;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -149,26 +153,41 @@ public class ImportCommandTest {
      * indicating that duplicate persons are present.
      */
     @Test
-    public void duplicatePersonExecution_fail() {
-        String projectDir = System.getProperty("user.dir");
-        String filePath = projectDir + "/src/test/data/duplicatePerson.csv";
-        ImportCommand command = new ImportCommand(filePath);
-        String expectedMsg = MESSAGE_READING_ERROR + "Operation would result in duplicate persons"
-            + "\nPlease ensure that there are no duplicate person in the CSV file";
-        assertCommandFailure(command, model, expectedMsg);
+    public void duplicatePersonExecution_fail() throws IOException {
+        // Create a temporary CSV file with invalid name data
+        Path tempFile = Files.createTempFile("duplicatePerson", ".csv");
+        String data ="\"Name\",\"Email\",\"Telegram\",\"Tags\",\"Github\",\"Assignments\",\"WeeksPresent\"\n" +
+            "\"jh\",\"he@gmial.com\",\"@hj\",\"\",\"kkk\",\"Ex02 | 8.0,Ex01 | 1.0\",\"1,2\"\n" +
+            "\"jh\",\"he@gmial.com\",\"@hj\",\"\",\"kkk\",\"Ex02 | 8.0,Ex01 | 1.0\",\"1,2\"";
+        Files.writeString(tempFile, data);  // Write some invalid content
+        try {
+            ImportCommand command = new ImportCommand(tempFile.toString());
+            String expectedMsg = MESSAGE_READING_ERROR + "Operation would result in duplicate persons"
+                + "\nPlease ensure that there are no duplicate person in the CSV file";
+            assertCommandFailure(command, model, expectedMsg);
+        } finally {
+            Files.deleteIfExists(tempFile);
+        }
     }
 
     /**
-     * Tests the execution of an import command with a CSV file containing invalid person name,
-     * expecting failure. Verifies that the command fails with the expected error message
-     * indicating that duplicate persons are present.
+     * Tests that the ImportCommand fails when given a CSV file with an invalid person name.
+     *
+     * @throws IOException if an error occurs while handling the temporary file.
      */
     @Test
-    public void invalidPersonNameExecution_fail() {
-        String projectDir = System.getProperty("user.dir");
-        String filePath = projectDir + "/src/test/data/invalidPersonName.csv";
-        ImportCommand command = new ImportCommand(filePath);
-        String expectedMsg = MESSAGE_READING_ERROR + MESSAGE_CONSTRAINTS;
-        assertCommandFailure(command, model, expectedMsg);
+    public void invalidPersonNameExecution_fail() throws IOException {
+        // Create a temporary CSV file with invalid name data
+        Path tempFile = Files.createTempFile("invalidPersonName", ".csv");
+        String data = "\"Name\",\"Email\",\"Telegram\",\"Tags\",\"Github\",\"Assignments\",\"WeeksPresent\"\n" +
+            "\"inv@l1d\",\"hellokitty@gmail.com\",\"@hj2909\",\"\",\"lfgcode\",\"Ex02 | 8.0,Ex01 | 1.0\",\"1,2\"";
+        Files.writeString(tempFile, data);  // Write content into file
+        try {
+            ImportCommand command = new ImportCommand(tempFile.toString());
+            String expectedMsg = MESSAGE_READING_ERROR + MESSAGE_CONSTRAINTS;
+            assertCommandFailure(command, model, expectedMsg);
+        } finally {
+            Files.deleteIfExists(tempFile);
+        }
     }
 }
