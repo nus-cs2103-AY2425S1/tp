37aae930d03c570680e3be3c2dee556d548ad99a
tohhengxing <102740235+tohhengxing@users.noreply.github.com>
2024-11-07 10:45:10 +0800

    fix: Linting and style

--- src/test/java/seedu/address/logic/commands/ListArchivedCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListArchivedCommandTest.java
@@ -1,15 +1,14 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
