53f318d33f7dd80d0be293d18e7055bb55d0e229
saajidshaik02 <saajidshaik02@gmail.com>
2024-11-07 19:32:20 +0800

    Revert "Modify Undo and Redo output"
    This reverts commit ba543bcf457b7ac898247d3ffcf0551d526e776d.

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -15,7 +15,7 @@ public class UndoCommand extends Command {
 
     public static final String COMMAND_WORD = "undo";
     public static final String MESSAGE_SUCCESS = "Address book has undone previous command!";
-    public static final String MESSAGE_FAILURE = "Address book failed to be undone. Please try again later.";
+    public static final String MESSAGE_FAILURE = "There is no current command to be undone!";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
