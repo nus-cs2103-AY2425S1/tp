f7a5d3727b477a92063ce6bd403ffd36be0a763b
CYX22222003 <e1155533@u.nus.edu>
2024-11-07 20:37:48 +0800

    Correct formatting errors

--- src/main/java/seedu/address/model/CampusConnect.java
+++ src/main/java/seedu/address/model/CampusConnect.java
@@ -126,7 +126,8 @@ public class CampusConnect implements ReadOnlyCampusConnect {
     /**
      * Replaces the given person {@code target} in the list with {@code editedPerson}.
      * {@code target} must exist in the campus connect.
-     * The person identity of {@code editedPerson} must not be the same as another existing person in the campus connect.
+     * The person identity of {@code editedPerson} must not be the same as
+     * another existing person in the campus connect.
      */
     public void setPerson(Person target, Person editedPerson) {
         requireNonNull(editedPerson);
