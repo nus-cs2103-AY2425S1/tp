afcf547e267ef006270c4ae113343ca1361ebb3a
Oceankoh <36119714+Oceankoh@users.noreply.github.com>
2024-11-07 19:00:10 +0800

    Differentiate unique entries by email instead of name

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -132,7 +132,7 @@ public class Person {
         }
 
         return otherPerson != null
-                && otherPerson.getName().equals(getName());
+                && otherPerson.getEmail().equals(getEmail());
     }
 
 
