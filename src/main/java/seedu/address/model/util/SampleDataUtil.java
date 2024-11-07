f891b5f0af6a8b84dacf5e38022a6b822c9961d0
jonushzw <jonushzw@gmail.com>
2024-11-07 11:12:17 +0800

    update test cases for sampledatautil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -28,22 +28,22 @@ public class SampleDataUtil {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Address("Blk 30 Geylang Street 29, #06-40"), new Job("Caterer"),
-                    getTagSet("friends")),
+                    getTagSet("Mary & John")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new Job("Photographer"),
-                    getTagSet("colleagues", "friends")),
+                    getTagSet("Mary & John", "Anthony & Jane")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"), new Job("Usher"),
-                     getTagSet("neighbours")),
+                     getTagSet("Ahmad Bin Ali & Siti Bte Rahim")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                     new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new Job("Security"),
-                    getTagSet("family")),
+                    getTagSet("Devesh s/o Ramesh & Priya d/o Ramesh")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                     new Address("Blk 47 Tampines Street 20, #17-35"), new Job("Videographer"),
-                    getTagSet("classmates")),
+                    getTagSet("Carol & John")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                     new Address("Blk 45 Aljunied Street 85, #11-31"), new Job("Audio"),
-                    getTagSet("colleagues"))
+                    getTagSet("Fiona & John"))
         };
     }
 
