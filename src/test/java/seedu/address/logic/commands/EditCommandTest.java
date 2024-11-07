c28fca89f7f7a47dece56e7ceb1c33e027b2eed7
sandyk0105 <sandykristianwaluyo3@gmail.com>
2024-11-07 22:58:10 +0800

    Revert "Fix DarkTheme.css"
    This reverts commit 6b94566c580b22274c8613b594d847b783763c1f.

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
@@ -15,24 +14,17 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.GuiSettings;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
-import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
 import seedu.address.testutil.PersonBuilder;
-
-import java.nio.file.Path;
-import java.util.function.Predicate;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for EditCommand.
@@ -43,7 +35,6 @@ public class EditCommandTest {
 
     @Test
     public void execute_allFieldsSpecifiedUnfilteredList_success() {
-
         Person editedPerson = new PersonBuilder().build();
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(editedPerson).build();
         EditCommand editCommand = new EditCommand(ID_FIRST_PERSON, descriptor);
@@ -136,136 +127,5 @@ public class EditCommandTest {
 
         // different descriptor -> returns false
         assertFalse(standardCommand.equals(new EditCommand(ID_FIRST_PERSON, DESC_BOB)));
-    }
-
-
-    private class ModelStub implements Model {
-        @Override
-        public void setUserPrefs(ReadOnlyUserPrefs userPrefs) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyUserPrefs getUserPrefs() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public GuiSettings getGuiSettings() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setGuiSettings(GuiSettings guiSettings) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public Path getAddressBookFilePath() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setAddressBookFilePath(Path addressBookFilePath) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void addPerson(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-        @Override
-        public String getPersonRole(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setAddressBook(ReadOnlyAddressBook newData) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ReadOnlyAddressBook getAddressBook() {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public boolean hasPerson(Person person) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void deletePerson(Person target) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public void setPerson(Person target, Person editedPerson) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredPersonList() {
-            return null;
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredPersonById(int id) {
-            return null;
-        }
-
-        @Override
-        public Person getFilteredPersonById(ObservableList<Person> allPersons, int id) {
-            return null; // TODO?
-        }
-
-        @Override
-        public Person getFilteredPatientById(ObservableList<Person> allPersons, int id) {
-            return null;
-        }
-
-        @Override
-        public Person getFilteredDoctorById(ObservableList<Person> allPersons, int id) {
-            return null;
-        }
-
-        @Override
-        public void updateFilteredPersonList(Predicate<Person> predicate) {
-        }
-    }
-    /**
-     * A Model stub that contains a single person.
-     */
-    private class ModelStubWithAppointment extends CheckAppointmentCommandTest.ModelStub {
-        private final Person patient;
-        private final Person doctor;
-
-        ModelStubWithAppointment(Person patient, Person doctor) {
-            requireNonNull(patient);
-            requireNonNull(doctor);
-            this.patient = patient;
-            this.doctor = doctor;
-        }
-
-        @Override
-        public boolean hasPerson(Person person) {
-            requireNonNull(person);
-            return person.isSamePerson(patient) || person.isSamePerson(doctor);
-        }
-
-        @Override
-        public ObservableList<Person> getFilteredPersonList() {
-            return javafx.collections.FXCollections.observableArrayList(patient, doctor);
-        }
-
-        @Override
-        public Person getFilteredPatientById(ObservableList<Person> allPersons, int id) {
-            return patient.getId() == (id) ? patient : null;
-        }
-
-        @Override
-        public Person getFilteredDoctorById(ObservableList<Person> allPersons, int id) {
-            return doctor.getId() == (id) ? doctor : null;
-        }
     }
 }
