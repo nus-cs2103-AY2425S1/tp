a87d19021f9e48f701e846483050d613b847bc49
blanklogic <koh.junxian.jaymeson@icloud.com>
2024-11-07 15:21:16 +0800

    Add Javadoc comments to NameContainsKeywordsPredicate.java

--- src/main/java/seedu/internbuddy/model/company/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/internbuddy/model/company/NameContainsKeywordsPredicate.java
@@ -14,6 +14,12 @@ import seedu.internbuddy.commons.util.ToStringBuilder;
 public class NameContainsKeywordsPredicate implements Predicate<Company> {
     private final List<String> keywords;
 
+    /**
+     * Constructs a {@code NameContainsKeywordsPredicate} with the specified keywords.
+     * Keywords are converted to lowercase to enable case-insensitive matching.
+     *
+     * @param keywords A list of keywords to search for within a company's fields.
+     */
     public NameContainsKeywordsPredicate(List<String> keywords) {
         // Store keywords in lowercase to simplify case-insensitive matching
         this.keywords = keywords.stream()
@@ -21,6 +27,15 @@ public class NameContainsKeywordsPredicate implements Predicate<Company> {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Evaluates this predicate on the given {@code Company}.
+     * Checks if any of the specified keywords are partially present (case-insensitively)
+     * in the company's name, tags, application name, or application description.
+     *
+     * @param company The company to evaluate the keywords against.
+     * @return {@code true} if any keyword is found as a substring in the company's name, tags,
+     *         application name, or application description; {@code false} otherwise.
+     */
     @Override
     public boolean test(Company company) {
         // Convert the company fields to lowercase strings for case-insensitive matching
@@ -39,6 +54,13 @@ public class NameContainsKeywordsPredicate implements Predicate<Company> {
                         || lowerCaseAppDescription.contains(keyword));
     }
 
+    /**
+     * Indicates whether another object is equal to this {@code NameContainsKeywordsPredicate}.
+     * Two {@code NameContainsKeywordsPredicate} instances are equal if they have the same keywords.
+     *
+     * @param other The object to compare with this predicate.
+     * @return {@code true} if the specified object is equal to this predicate; {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -53,6 +75,12 @@ public class NameContainsKeywordsPredicate implements Predicate<Company> {
         return keywords.equals(otherPredicate.keywords);
     }
 
+    /**
+     * Returns a string representation of the {@code NameContainsKeywordsPredicate}.
+     * The string includes the list of keywords that this predicate checks for.
+     *
+     * @return A string representation of this predicate.
+     */
     @Override
     public String toString() {
         return new ToStringBuilder(this).add("keywords", keywords).toString();
