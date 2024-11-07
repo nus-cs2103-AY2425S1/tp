b4ccc4725651aa03569b8181f5b1005b84cb9dd5
Professional-Procrastinat0r <joshualoh@u.nus.edu>
2024-11-07 18:18:56 +0800

    Update list command messages

--- src/main/java/seedu/address/logic/commands/listcommands/ListAssignmentsCommand.java
+++ src/main/java/seedu/address/logic/commands/listcommands/ListAssignmentsCommand.java
@@ -15,12 +15,13 @@ public class ListAssignmentsCommand extends ListCommand {
     // Command word using the 'list' prefix + 'assignments'
     public static final String COMMAND_WORD = COMMAND_PREFIX + "assignments";
 
-    public static final String MESSAGE_SUCCESS = "Listed all assignments";
+    public static final String MESSAGE_SUCCESS = "%1$s assignments listed!";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredAssignmentList(PREDICATE_SHOW_ALL_ASSIGNMENTS);
-        return new CommandResult(MESSAGE_SUCCESS, DisplayType.ASSIGNMENT_LIST);
+        int numAssignmentsShown = model.getFilteredAssignmentList().size();
+        return new CommandResult(String.format(MESSAGE_SUCCESS, numAssignmentsShown), DisplayType.ASSIGNMENT_LIST);
     }
 }
