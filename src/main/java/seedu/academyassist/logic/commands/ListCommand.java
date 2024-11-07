8bf92faa544be30156d531637d35139f40c5752d
JoanneHing <yenxinghing@gmail.com>
2024-11-07 12:57:16 +0800

    Add list command to sort by studentId

--- src/main/java/seedu/academyassist/logic/commands/ListCommand.java
+++ src/main/java/seedu/academyassist/logic/commands/ListCommand.java
@@ -11,13 +11,15 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all students details";
+    public static final String MESSAGE_SUCCESS = "Listed all students details\n"
+            + "Students are sorted by student ID";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+        model.sortAcademyAssistById();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
