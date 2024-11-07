f15968e03f8271d8de0656af69086bdbebf7069c
Alek Kwek <40349610+AlekKwek18@users.noreply.github.com>
2024-11-07 17:05:25 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_INDEX;
-
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.DeleteCommand;
