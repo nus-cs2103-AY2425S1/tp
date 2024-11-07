d9991a2a416f0e37d8913956d25b0b152d830f15
psh12320 <pshricharan2020@gmail.com>
2024-11-07 21:49:43 +0800

    Fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/EditMeetingDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditMeetingDescriptorTest.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_MEETING_ONE;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_MEETING_TWO;
 
