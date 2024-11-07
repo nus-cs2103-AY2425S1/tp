06ddb9843b8e6e324f401ec0d1346d665c2aabeb
hj235 <hongjing235@gmail.com>
2024-11-07 10:59:54 +0800

    Change focus for F2 key accelerator

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -91,7 +91,13 @@ public class MainWindow extends UiPart<Stage> {
 
     private void setAccelerators() {
         setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
-        setAccelerator(toggleConcertContactViewItem, KeyCombination.valueOf("F2"));
+        getRoot().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (KeyCombination.valueOf("F2").match(event)) {
+                toggleConcertContactViewItem.getOnAction().handle(new ActionEvent());
+                event.consume();
+            }
+        });
+//        setAccelerator(toggleConcertContactViewItem, KeyCombination.valueOf("F2"));
     }
 
     /**
