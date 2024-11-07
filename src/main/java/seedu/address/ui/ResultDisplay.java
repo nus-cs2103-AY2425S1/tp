f2cf287b79f9c9277773d43f6e8e3051a500ef8d
anselmlong <anselmpius@gmail.com>
2024-11-07 23:47:34 +0800

    Make result display scrollable

--- src/main/java/seedu/address/ui/ResultDisplay.java
+++ src/main/java/seedu/address/ui/ResultDisplay.java
@@ -3,6 +3,7 @@ package seedu.address.ui;
 import static java.util.Objects.requireNonNull;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -17,8 +18,18 @@ public class ResultDisplay extends UiPart<Region> {
     @FXML
     private TextFlow resultDisplay;
 
+    @FXML
+    private ScrollPane scrollPane = new ScrollPane();
+
+    /**
+     * Creates a ResultDisplay, along with a ScrollPane to scroll through the contents.
+     */
     public ResultDisplay() {
         super(FXML);
+        scrollPane.setContent(resultDisplay);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
     }
 
     /**
