5f3b5720b011bc4f6dac255f490ec1e938887b4e
yyueyii <e1271791@u.nus.edu>
2024-11-07 21:08:45 +0800

    Add test cases for reset attendance

--- src/test/java/seedu/address/logic/commands/ResetAttendanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ResetAttendanceCommandTest.java
@@ -4,18 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.AMY;
 import static seedu.address.testutil.TypicalPersons.getParentOnlyAddressBook;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.AttendanceCount;
+import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Student;
+import seedu.address.testutil.StudentBuilder;
 
 public class ResetAttendanceCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
@@ -24,7 +28,7 @@ public class ResetAttendanceCommandTest {
 
 
     @Test
-    public void execute_resetAttendance_success() {
+    public void execute_unfilteredListWithStudents_success() {
         Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
         for (Person p : expectedModel.getFilteredPersonList()) {
@@ -41,10 +45,38 @@ public class ResetAttendanceCommandTest {
         assertCommandSuccess(resetAttendanceCommandCommand, model, expectedMessage, expectedModel);
     }
 
+
     @Test
     public void execute_unfilteredListWithoutStudents_throwsCommandException() {
         BatchMarkCommand batchMarkCommand = new BatchMarkCommand();
-        assertCommandFailure(batchMarkCommand, parentsOnlyModel, BatchMarkCommand.MESSAGE_BATCH_MARK_NO_STUDENT_LIST);
+        assertCommandFailure(batchMarkCommand, parentsOnlyModel, ResetAttendanceCommand.MESSAGE_BATCH_MARK_NO_STUDENT_LIST);
+    }
+
+    @Test
+    public void execute_filteredListWithStudents_success() {
+        String expectedMessage = String.format(ResetAttendanceCommand.MESSAGE_RESET_ATTENDANCE_SUCCESS,
+                "Alice Pauline");
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate("Alice");
+        model.updateFilteredPersonList(predicate);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        Student newAlice = new StudentBuilder().withName("Alice Pauline")
+                .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
+                .withPhone("94351253").withRole("student")
+                .withTags("friends").withAttendanceCount("0").build();
+        expectedModel.setPerson(ALICE, newAlice);
+        ResetAttendanceCommand resetAttendanceCommandCommand = new ResetAttendanceCommand();
+        assertCommandSuccess(resetAttendanceCommandCommand, model, expectedMessage, expectedModel);
+
+    }
+
+    @Test
+    public void execute_filteredListWithoutStudents_throwsCommandException() {
+        Model model = new ModelManager(getParentOnlyAddressBook(), new UserPrefs());
+        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate("Carl");
+        model.updateFilteredPersonList(predicate);
+        ResetAttendanceCommand resetAttendanceCommand = new ResetAttendanceCommand();
+        assertCommandFailure(resetAttendanceCommand, model, ResetAttendanceCommand.MESSAGE_BATCH_MARK_NO_STUDENT_LIST);
     }
 
     @Test
