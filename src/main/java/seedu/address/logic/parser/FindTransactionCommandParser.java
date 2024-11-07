8f04b528605c39293ea4b18e52ed2e3c9e82bf28
reginateh <e1312290@u.nus.edu>
2024-11-07 16:24:58 +0800

    Change findt to be used in transaction view

--- src/main/java/seedu/address/logic/parser/FindTransactionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindTransactionCommandParser.java
@@ -4,7 +4,6 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.FindTransactionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.TransactionContainsKeywordsPredicate;
@@ -20,35 +19,12 @@ public class FindTransactionCommandParser implements Parser<FindTransactionComma
      * @throws ParseException if the user input does not conform the expected format.
      */
     public FindTransactionCommand parse(String args) throws ParseException {
-        // first item is index, the rest are keywords
-        String[] transactionKeywords = getStrings(args);
-        Index index;
-        try {
-            index = ParserUtil.parseIndex(transactionKeywords[0]);
-            // remove the first item (index)
-            transactionKeywords = Arrays.copyOfRange(transactionKeywords, 1, transactionKeywords.length);
-        } catch (ParseException e) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTransactionCommand.MESSAGE_USAGE));
-        }
-        return new FindTransactionCommand(index,
-                new TransactionContainsKeywordsPredicate(Arrays.asList(transactionKeywords)));
-    }
-
-    private String[] getStrings(String args) throws ParseException {
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTransactionCommand.MESSAGE_USAGE));
         }
-
-        // first item is index, the rest are keywords
         String[] transactionKeywords = trimmedArgs.split("\\s+");
-        // check if there is at least 2 items (index and at least 1 keyword)
-        if (transactionKeywords.length < 2) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindTransactionCommand.MESSAGE_USAGE));
-        }
-        return transactionKeywords;
+        return new FindTransactionCommand(new TransactionContainsKeywordsPredicate(Arrays.asList(transactionKeywords)));
     }
 }
