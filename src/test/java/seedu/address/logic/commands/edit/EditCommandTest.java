2f6b67e351a69bf9de8bc7a57159148e2e5e125c
Elinengu <Eline.Ngu2003@gmail.com>
2024-11-07 15:31:04 +0800

    Make the error more meaningful

--- src/test/java/seedu/address/logic/commands/edit/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/edit/EditCommandTest.java
@@ -151,7 +151,7 @@ public class EditCommandTest {
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(firstPerson).build();
         EditCommand editCommand = new EditCommand(INDEX_SECOND_PERSON, descriptor);
 
-        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON);
+        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PHONE_AND_EMAIL);
     }
 
     @Test
@@ -183,7 +183,7 @@ public class EditCommandTest {
         EditCommand editCommand = new EditCommand(INDEX_FIRST_PERSON,
                 new EditPersonDescriptorBuilder(personInList).build());
 
-        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PERSON);
+        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_PHONE_AND_EMAIL);
     }
 
     @Test
