9b40445e1c5e1a95c437159ab2a7b85cd6d5abd5
Nayana Mahesh <nayanamahesh7@gmail.com>
2024-11-07 21:13:23 +0800

    Fix check style issues

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -10,7 +10,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
 import java.util.Set;
 
