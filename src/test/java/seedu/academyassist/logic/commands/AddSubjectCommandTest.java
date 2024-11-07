22d7c033ab632a726603dd007bff25f58bb6b1ea
JoanneHing <yenxinghing@gmail.com>
2024-11-07 19:25:15 +0800

    Update test cases

--- src/test/java/seedu/academyassist/logic/commands/AddSubjectCommandTest.java
+++ src/test/java/seedu/academyassist/logic/commands/AddSubjectCommandTest.java
@@ -75,7 +75,8 @@ public class AddSubjectCommandTest {
         AddSubjectCommand addSubjectCommand = new AddSubjectCommand(new StudentId("S00008"), subjects);
         CommandResult commandResult = addSubjectCommand.execute(modelStub);
         assertEquals(String.format(AddSubjectCommand.MESSAGE_SUCCESS, "S00008",
-                        "Amy Bee", subjects.toString()), commandResult.getFeedbackToUser());
+                        "Amy Bee",
+                subjects.toString().replace("[","").replace("]","")), commandResult.getFeedbackToUser());
 
     }
 
@@ -90,7 +91,8 @@ public class AddSubjectCommandTest {
         AddSubjectCommand addSubjectCommand = new AddSubjectCommand(new StudentId("S00008"), subjects);
         CommandResult commandResult = addSubjectCommand.execute(modelStub);
         assertEquals(String.format(AddSubjectCommand.MESSAGE_SUCCESS, "S00008",
-                "Amy Bee", subjects.toString()), commandResult.getFeedbackToUser());
+                "Amy Bee",
+        subjects.toString().replace("[","").replace("]","")), commandResult.getFeedbackToUser());
 
     }
 
