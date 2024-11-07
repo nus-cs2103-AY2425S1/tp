a515834a3617617e231bbd010981fade26ff2fd9
Colin Chiang <colinchiang.cc@gmail.com>
2024-11-07 21:55:36 +0800

    Modify result display size

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -79,18 +79,6 @@ public class MainWindow extends UiPart<Stage> {
         setAccelerators();
 
         helpWindow = new HelpWindow();
-
-        primaryStage.getScene().heightProperty().addListener((observable, oldHeight, newHeight) -> {
-            double heightDifference = newHeight.doubleValue() - oldHeight.doubleValue();
-
-            // Check if the height difference is at least 3 cm (approximately 113.4 pixels)
-            if (Math.abs(heightDifference) >= 113.4) {
-                double resultDisplayIncrement = 28; // Increase by 1 cm (approx. 37.8 pixels)
-                double newResultHeight = resultDisplayPlaceholder.getHeight() + resultDisplayIncrement;
-
-                resultDisplay.setPrefHeight(Math.max(newResultHeight, 100));
-            }
-        });
     }
 
     public Stage getPrimaryStage() {
@@ -139,6 +127,7 @@ public class MainWindow extends UiPart<Stage> {
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
+        resultDisplay.setPrefHeight(200);
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
         StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
