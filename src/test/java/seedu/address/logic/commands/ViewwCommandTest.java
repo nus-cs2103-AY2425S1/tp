6bdb832929fe307eda23427a0b195472f38611e2
Sim GUan Yi <e1130009@u.nus.edu>
2024-11-07 11:34:33 +0800

    fix the tests

--- src/test/java/seedu/address/logic/commands/ViewwCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewwCommandTest.java
@@ -11,10 +11,10 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_WEDDING;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBookFilterWithWeddings;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,23 +24,82 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonMatchesWeddingPredicate;
 import seedu.address.model.wedding.NameMatchesWeddingPredicate;
 import seedu.address.model.wedding.Wedding;
+import seedu.address.testutil.PersonBuilder;
 import seedu.address.testutil.WeddingBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ViewwCommand.
  */
 public class ViewwCommandTest {
-    private static Model model;
-    @BeforeAll
-    public static void setUp() {
-        model = new ModelManager(getTypicalAddressBookFilterWithWeddings(), new UserPrefs());
+    private Model model;
+
+    public static AddressBook getTypicalAddressBookForVieww() {
+        Wedding aliceWedding = new WeddingBuilder()
+                .withName("Alice Adam Wedding")
+                .withVenue("Marina Bay Sands")
+                .withDate("2024-12-12")
+                .build();
+
+        Wedding georgeWedding = new WeddingBuilder()
+                .withName("George Jane Wedding")
+                .withVenue("Sentosa")
+                .withDate("2025-01-01")
+                .build();
+
+        Person alice = new PersonBuilder()
+                .withName("Alice Pauline")
+                .withAddress("123, Jurong West Ave 6, #08-111")
+                .withEmail("alice@example.com")
+                .withPhone("94351253")
+                .withRole("florist")
+                .withOwnWedding(aliceWedding)
+                .build();
+
+        Person benson = new PersonBuilder()
+                .withName("Benson Meier")
+                .withAddress("311, Clementi Ave 2, #02-25")
+                .withEmail("johnd@example.com")
+                .withPhone("98756432")
+                .withRole("caterer")
+                .addWeddingJob(georgeWedding)
+                .build();
+
+        Person carl = new PersonBuilder()
+                .withName("Carl Kurz")
+                .withPhone("95352563")
+                .withEmail("heinz@example.com")
+                .withAddress("wall street")
+                .withRole("florist")
+                .addWeddingJob(georgeWedding)
+                .build();
+
+        Person george = new PersonBuilder()
+                .withName("George Best")
+                .withPhone("94824422")
+                .withEmail("anna@example.com")
+                .withAddress("4th street")
+                .withOwnWedding(georgeWedding)
+                .build();
+
+        ArrayList<Person> persons = new ArrayList<>(Arrays.asList(alice, benson, carl, george));
+        ArrayList<Wedding> weddings = new ArrayList<>(Arrays.asList(aliceWedding, georgeWedding));
+        AddressBook addressBook = new AddressBook();
+        for (Person person : persons) {
+            addressBook.addPerson(person);
+        }
+        for (Wedding wedding : weddings) {
+            addressBook.addWedding(wedding);
+        }
+        return addressBook;
     }
 
     @BeforeEach
     public void setUpEach() {
+        model = new ModelManager(getTypicalAddressBookForVieww(), new UserPrefs());
         model.updateFilteredWeddingList(PREDICATE_SHOW_ALL_WEDDINGS);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
     }
@@ -48,13 +107,15 @@ public class ViewwCommandTest {
     @Test
     public void execute_validIndex_success() {
         Wedding weddingToView = model.getFilteredWeddingList().get(INDEX_FIRST_WEDDING.getZeroBased());
+
         ViewwCommand viewwCommand = new ViewwCommand(INDEX_FIRST_WEDDING, null);
         String expectedMessage = String.format(ViewwCommand.MESSAGE_VIEW_WEDDING_SUCCESS,
                 Messages.format(weddingToView));
 
-        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        ModelManager expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
         expectedModel.updateFilteredPersonList(new PersonMatchesWeddingPredicate(weddingToView));
         expectedModel.updateFilteredWeddingList(p -> p.equals(weddingToView));
+
         assertCommandSuccess(viewwCommand, model, expectedMessage, expectedModel);
     }
 
@@ -98,14 +159,16 @@ public class ViewwCommandTest {
     @Test
     public void execute_multipleMatchesKeyword_returnsHandlingMessage() {
         // Set up scenario where multiple weddings match the keyword
+        Model testModel = new ModelManager(getTypicalAddressBookFilterWithWeddings(), new UserPrefs());
+        testModel.addWedding(new WeddingBuilder().withName("common wedding").build());
+        testModel.addWedding(new WeddingBuilder().withName("john common wedding").build());
+
         List<String> commonKeyword = Arrays.asList("common");
         NameMatchesWeddingPredicate predicate = new NameMatchesWeddingPredicate(commonKeyword);
         ViewwCommand viewwCommand = new ViewwCommand(null, predicate);
-        Model model1 = new ModelManager(getTypicalAddressBookFilterWithWeddings(), new UserPrefs());
-        model1.addWedding(new WeddingBuilder().withName("common wedding").build());
-        model1.addWedding(new WeddingBuilder().withName("john common wedding").build());
 
-        assertCommandSuccess(viewwCommand, model1, ViewwCommand.MESSAGE_DUPLICATE_HANDLING, model);
+        ModelManager expectedModel = new ModelManager(testModel.getAddressBook(), new UserPrefs());
+        assertCommandSuccess(viewwCommand, testModel, ViewwCommand.MESSAGE_DUPLICATE_HANDLING, expectedModel);
     }
 
     @Test
