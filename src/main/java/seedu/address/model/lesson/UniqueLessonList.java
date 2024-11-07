e70442d2fd6d36a8acf827c6b95100dc407b88d3
estellelim <156592785+estellelim@users.noreply.github.com>
2024-11-07 17:10:35 +0800

    Fix bugs

--- src/main/java/seedu/address/model/lesson/UniqueLessonList.java
+++ src/main/java/seedu/address/model/lesson/UniqueLessonList.java
@@ -14,6 +14,7 @@ import javafx.collections.ObservableList;
 import seedu.address.model.lesson.exceptions.DuplicateLessonException;
 import seedu.address.model.lesson.exceptions.LessonNotFoundException;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Subject;
 import seedu.address.model.person.Tutor;
 
 /**
