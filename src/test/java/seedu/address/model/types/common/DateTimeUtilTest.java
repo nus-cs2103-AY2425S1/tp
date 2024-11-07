5459a6ea3ed22d5919a53a51ca4a4cf8cce3fd73
Jayden-Kim-NUS <e1121727@u.nus.edu>
2024-11-07 22:08:14 +0800

    Add test for datetimeutil and clearcommand

--- src/test/java/seedu/address/model/types/common/DateTimeUtilTest.java
+++ src/test/java/seedu/address/model/types/common/DateTimeUtilTest.java
@@ -1,11 +1,19 @@
 package seedu.address.model.types.common;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+
 /**
- * Note: testing of timeline is not possible without adding dependencies
+ * Note: Realtime testing of timeline is not possible without adding dependencies
  * Also, the DateTimeUtil class mainly acts as a facade for Timeline and LocalDateTime,
  * both of which are part of Java and not editable
  */
@@ -15,5 +23,56 @@ public class DateTimeUtilTest {
     public void getCurrentDateTimeString() {
         String output = DateTimeUtil.getCurrentDateTimeString();
         assertTrue(output.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}$"));
+    }
+
+    @Test
+    public void createTimeLine_oneArgument() {
+        AtomicBoolean actionExecuted = new AtomicBoolean(false);
+        Runnable action = () -> actionExecuted.set(true);
+
+        Timeline timeline = DateTimeUtil.createTimeline(action);
+        assertEquals(timeline.getCycleDuration(), Duration.seconds(1));
+        assertEquals(timeline.getDelay(), Duration.seconds(0));
+        assertEquals(timeline.getCycleCount(), Timeline.INDEFINITE);
+
+        KeyFrame keyFrame = timeline.getKeyFrames().get(0);
+        keyFrame.getOnFinished().handle(new ActionEvent());
+        assertTrue(actionExecuted.get());
+
+        timeline.stop();
+    }
+
+    @Test
+    public void createTimeLine_noDelay() {
+        AtomicBoolean actionExecuted = new AtomicBoolean(false);
+        Runnable action = () -> actionExecuted.set(true);
+
+        Timeline timeline = DateTimeUtil.createTimeline(action, Duration.seconds(1), Integer.toUnsignedLong(0));
+        assertEquals(timeline.getCycleDuration(), Duration.seconds(1));
+        assertEquals(timeline.getDelay(), Duration.seconds(0));
+        assertEquals(timeline.getCycleCount(), Timeline.INDEFINITE);
+
+        KeyFrame keyFrame = timeline.getKeyFrames().get(0);
+        keyFrame.getOnFinished().handle(new ActionEvent());
+        assertTrue(actionExecuted.get());
+
+        timeline.stop();
+    }
+
+    @Test
+    public void createTimeLine_withDelay() {
+        AtomicBoolean actionExecuted = new AtomicBoolean(false);
+        Runnable action = () -> actionExecuted.set(true);
+
+        Timeline timeline = DateTimeUtil.createTimeline(action, Duration.seconds(1), Integer.toUnsignedLong(1000));
+        assertEquals(timeline.getCycleDuration(), Duration.seconds(1));
+        assertEquals(timeline.getDelay(), Duration.seconds(1));
+        assertEquals(timeline.getCycleCount(), Timeline.INDEFINITE);
+
+        KeyFrame keyFrame = timeline.getKeyFrames().get(0);
+        keyFrame.getOnFinished().handle(new ActionEvent());
+        assertTrue(actionExecuted.get());
+
+        timeline.stop();
     }
 }
