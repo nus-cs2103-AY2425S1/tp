7a90353c06e6c62d1246933cda56e39a3f63b5a2
chongtzezhao <47619363+chongtzezhao@users.noreply.github.com>
2024-11-07 17:12:27 +0800

    Logic: Remove public address parser in ParserUtil and respective tests

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -17,7 +17,6 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AbstractEditCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.addresses.PublicAddressesComposition;
 import seedu.address.model.tag.Tag;
 
 /**
