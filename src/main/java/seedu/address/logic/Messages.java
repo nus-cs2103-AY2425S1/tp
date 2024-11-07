e53cf8c8ec387b4d3f9937ad1079a8bb5f408c9d
fusin <fusin02@gmail.com>
2024-11-07 13:40:06 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -79,7 +79,7 @@ public class Messages {
                 .append("; Celebrity: ")
                 .append(event.getCelebrityName())
                 .append("; Contacts: ")
-                .append(event.getContacts().isEmpty() ? "[To be added]" : event.getContactsString() );
+                .append(event.getContacts().isEmpty() ? "[To be added]" : event.getContactsString());
         return builder.toString();
     }
 
--- src/main/java/seedu/address/logic/commands/exceptions/PendingCommandException.java
+++ src/main/java/seedu/address/logic/commands/exceptions/PendingCommandException.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.commands.exceptions;
 
+/**
+ * Represents an error which occurs during the execution of a ConfirmCommand.
+ */
 public class PendingCommandException extends CommandException {
     public PendingCommandException(String message) {
         super(message);
