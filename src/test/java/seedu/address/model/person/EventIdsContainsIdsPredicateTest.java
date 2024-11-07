6c624015ab3c21d825ce71fc1757923b216ace79
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-07 19:46:01 +0800

    Remove import statement

--- src/test/java/seedu/address/model/person/EventIdsContainsIdsPredicateTest.java
+++ src/test/java/seedu/address/model/person/EventIdsContainsIdsPredicateTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.testutil.EventBuilder;
 import seedu.address.testutil.PersonBuilder;
 
 public class EventIdsContainsIdsPredicateTest {
