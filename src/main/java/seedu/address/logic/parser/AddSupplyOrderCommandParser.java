ddfdd9fcfc5d5ee8716be681cdd7d2ed42503304
Samuel Then <samuelthen@Samuels-MacBook-Pro.local>
2024-11-07 12:45:31 +0800

    Fix remaining style errors

--- src/main/java/seedu/address/logic/parser/AddSupplyOrderCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddSupplyOrderCommandParser.java
@@ -6,6 +6,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ORDER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
