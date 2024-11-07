f52856c64d16a8060cea30dced3deee0f95c4f4e
Dong Qianbo <pencilate@outlook.com>
2024-11-07 23:19:43 +0800

    Update name of app in log

--- src/main/java/spleetwaise/commons/MainApp.java
+++ src/main/java/spleetwaise/commons/MainApp.java
@@ -200,7 +200,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting AddressBook " + MainApp.VERSION);
+        logger.info("Starting SpleetWaise " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
