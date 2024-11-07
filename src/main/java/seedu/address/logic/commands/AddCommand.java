155591011d41e91e99736bf49c0c1fcb4c807b58
Yong Quan <57471791+yongqqqq@users.noreply.github.com>
2024-11-07 21:25:38 +0800

    Update ui and error messages

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -25,7 +25,7 @@ public class AddCommand extends Command {
             + PREFIX_NAME + "NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_GENDER + "GENDER "
-            + "[" + PREFIX_MODULE + "MODULE]..."
+            + PREFIX_MODULE + "MODULE... "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
