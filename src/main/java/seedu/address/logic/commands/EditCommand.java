cff1ee53b52ef446dd36a5e4e928e0bd9b9dd9b7
Adrian Leonardo Liang <adrianleonardoliang@gmail.com>
2024-11-08 01:57:24 +0800

    Update UG to latest release details

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -189,7 +189,8 @@ public class EditCommand extends Command {
          * Returns true if at least one field is edited.
          */
         public boolean isAnyFieldEdited() {
-            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags, classes, subjects, gender);
+            return CollectionUtil.isAnyNonNull(name, phone, email, address, tags, classes, subjects, gender, nextOfKin,
+                    emergencyContact);
         }
 
         public void setNextOfKin(Name nextOfKin) {
