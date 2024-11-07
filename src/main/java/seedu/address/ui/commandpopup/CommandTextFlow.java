830c993fba3e57627ddeea77851e0a588f5b0779
flyingsalsa <107453007+flyingsalsa@users.noreply.github.com>
2024-11-07 23:49:14 +0800

    Update UI

--- src/main/java/seedu/address/ui/commandpopup/CommandTextFlow.java
+++ src/main/java/seedu/address/ui/commandpopup/CommandTextFlow.java
@@ -85,8 +85,8 @@ public class CommandTextFlow extends TextFlow {
         case "favgame" -> "favgame INDEX g/GAME";
         case "unfavgame" -> "unfavgame INDEX g/GAME";
         case "find" -> "find KEYWORD [MORE_KEYWORDS] - Finds persons whose names contain any of the keywords";
-        case "findtime" -> "find TIME-TIME [TIME-TIME]";
-        case "findgame" -> "findgame g/GAME";
+        case "findtime" -> "find TIME-TIME [TIME-TIME] - Finds persons whose preferred time contains any TIMES";
+        case "findgame" -> "findgame KEYWORD [MORE_KEYWORDS] - Finds persons whose games contain any of the keywords";
         case "delete" -> "delete INDEX - Deletes the specified person (e.g., delete 3)";
         case "clear" -> "clear - Clears all entries from the gamer address book";
         case "undo" -> "undo - Undoes the previous command (aside from save and load)";
