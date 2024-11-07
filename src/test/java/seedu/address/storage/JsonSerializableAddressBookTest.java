259d6d2881d29ad34e09a2b41a21cc53e7e9bf71
yixianggg <e1186210@u.nus.edu>
2024-11-07 21:43:39 +0800

    Fix syntax in JsonSerializableAddressBookTest

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -17,7 +17,8 @@ import seedu.address.testutil.TypicalPersons;
 
 public class JsonSerializableAddressBookTest {
 
-    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data",
+            "JsonSerializableAddressBookTest");
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
@@ -83,8 +84,10 @@ public class JsonSerializableAddressBookTest {
         AddressBook addressBookFromFile = dataFromFile.toModelType();
 
         AddressBook expectedAddressBook = new AddressBook();
-        expectedAddressBook.addTask(new Task(new PersonBuilder(ALICE).build(), "Buy medication", true));
-        expectedAddressBook.addTask(new Task(new PersonBuilder(ALICE).build(), "Visit doctor", false));
+        expectedAddressBook.addTask(new Task(new PersonBuilder(ALICE).build(), "Buy medication",
+                true));
+        expectedAddressBook.addTask(new Task(new PersonBuilder(ALICE).build(), "Visit doctor",
+                false));
         assertEquals(expectedAddressBook.getTaskList(), addressBookFromFile.getTaskList());
     }
 }
