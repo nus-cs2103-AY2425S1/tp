b30f1d3f04347c1693b486281f09d95393ad0473
Chen Lei Yu <128605764+ChenLeiyu@users.noreply.github.com>
2024-11-07 16:42:31 +0800

    Fix "edit" command showing whole list

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -95,7 +95,6 @@ public class EditCommand extends Command {
         }
 
         model.setPerson(personToEdit, editedPerson);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson)));
     }
 
@@ -104,7 +103,6 @@ public class EditCommand extends Command {
         requireNonNull(model);
 
         model.setPerson(editedPerson, personToEdit);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
     }
 
     @Override
