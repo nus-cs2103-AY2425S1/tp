0d9baa51fc478ea1364f87e9a4263cc863dd7e69
LinkesV <linkesvarun@gmail.com>
2024-11-08 01:43:52 +0800

    Add character limit

--- src/test/java/seedu/address/model/tag/TagTest.java
+++ src/test/java/seedu/address/model/tag/TagTest.java
@@ -1,8 +1,11 @@
 package seedu.address.model.tag;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.delivery.Quantity;
 
 public class TagTest {
 
@@ -21,6 +24,20 @@ public class TagTest {
     public void isValidTagName() {
         // null tag name
         assertThrows(NullPointerException.class, () -> Tag.isValidTagName(null));
+        
+        // blank tag
+        assertFalse(Tag.isValidTagName("")); // empty string
+        assertFalse(Tag.isValidTagName(" ")); // spaces only
+
+        // invalid tag name
+        assertFalse(Tag.isValidTagName("linke$")); // not alphanumeric
+        assertFalse(Tag.isValidTagName("a".repeat(51))); // 51 characters
+
+        // valid quantities
+        assertTrue(Tag.isValidTagName("hello"));
+        assertTrue(Tag.isValidTagName("a".repeat(49))); // 49 characters
+        assertTrue(Tag.isValidTagName("a".repeat(50))); // 50 characters
+
     }
 
 }
