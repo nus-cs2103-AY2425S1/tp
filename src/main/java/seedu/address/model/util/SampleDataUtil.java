be8d2099807b43ce5cf64205584b99cc9e91c964
gabriellegtw <gabrielle.gianna.tan@gmail.com>
2024-11-08 00:07:50 +0800

    Update Sample Data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -24,22 +24,22 @@ public class SampleDataUtil {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new StudentId("A8743880E"), Email.makeEmail("e1234567@u.nus.edu"),
                 Major.makeMajor("Business Analytics"),
-                getGroupList("group 1"), Year.makeYear("1"), EMPTY_COMMENT),
+                getGroupList("Team Terrific Trio"), Year.makeYear("1"), EMPTY_COMMENT),
             new Person(new Name("Bernice Yu"), new StudentId("A9272757L"), Email.makeEmail("e9999999@u.nus.edu"),
-                Major.makeMajor("Computer Science"), getGroupList("group 1"),
+                Major.makeMajor("Computer Science"), getGroupList("Team Terrific Trio"),
                     Year.makeYear("1"), EMPTY_COMMENT),
             new Person(new Name("Charlotte Oliveiro"), new StudentId("A9321028P"),
                     Email.makeEmail("e3456819@u.nus.edu"), Major.makeMajor("Political Science"),
-                getGroupList("group 2"), Year.makeYear("1"), EMPTY_COMMENT),
+                getGroupList("Team Terrific Trio"), Year.makeYear("1"), EMPTY_COMMENT),
             new Person(new Name("David Li"), new StudentId("A9103128E"), Email.makeEmail("e0000001@u.nus.edu"),
                 Major.makeMajor("Business Administration"),
-                getGroupList("group 2"), Year.makeYear("1"), EMPTY_COMMENT),
+                getGroupList("Group Awesome"), Year.makeYear("1"), EMPTY_COMMENT),
             new Person(new Name("Irfan Ibrahim"), new StudentId("A2492021T"), Email.makeEmail("e3456718@u.nus.edu"),
                 Major.makeMajor("Chemistry"),
-                getGroupList("group 3"), Year.makeYear("1"), EMPTY_COMMENT),
+                getGroupList("Group Awesome"), Year.makeYear("1"), EMPTY_COMMENT),
             new Person(new Name("Roy Balakrishnan"), new StudentId("A9262441K"), Email.makeEmail("e5739264@u.nus.edu"),
                 Major.makeMajor("Mechanical Engineering"),
-                getGroupList("group 3"), Year.makeYear("1"), EMPTY_COMMENT)
+                getGroupList("Group Awesome"), Year.makeYear("1"), EMPTY_COMMENT)
         };
     }
 
