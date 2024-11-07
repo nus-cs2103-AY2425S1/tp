23cbeda711aa196a2f94551c7465c8a92bcdbaa2
Jonathan Loh <jonathanloh02@gmail.com>
2024-11-07 20:09:31 +0800

    Allow at character in names

--- src/test/java/seedu/address/storage/JsonAdaptedOwnerTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedOwnerTest.java
@@ -15,7 +15,7 @@ import seedu.address.model.owner.Name;
 import seedu.address.model.owner.Phone;
 
 public class JsonAdaptedOwnerTest {
-    private static final String INVALID_NAME = "R@chel";
+    private static final String INVALID_NAME = "R[]chel";
     private static final String INVALID_PHONE = "+651234";
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
