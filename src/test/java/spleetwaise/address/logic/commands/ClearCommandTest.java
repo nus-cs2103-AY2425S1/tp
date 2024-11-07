8c284a40e6cc8f25fc0f58ea03ac7c5f9a90b5aa
GavinSin <51268353+GavinSin@users.noreply.github.com>
2024-11-07 17:47:47 +0800

    Add test case that test for non empty txnlist

--- src/test/java/spleetwaise/address/logic/commands/ClearCommandTest.java
+++ src/test/java/spleetwaise/address/logic/commands/ClearCommandTest.java
@@ -7,6 +7,10 @@ import spleetwaise.address.model.AddressBookModel;
 import spleetwaise.address.model.AddressBookModelManager;
 import spleetwaise.address.testutil.TypicalPersons;
 import spleetwaise.commons.model.CommonModelManager;
+import spleetwaise.transaction.model.TransactionBook;
+import spleetwaise.transaction.model.TransactionBookModel;
+import spleetwaise.transaction.model.TransactionBookModelManager;
+import spleetwaise.transaction.testutil.TypicalTransactions;
 
 public class ClearCommandTest {
 
@@ -15,8 +19,9 @@ public class ClearCommandTest {
     public void execute_emptyAddressBook_success() {
         AddressBookModel model = new AddressBookModelManager();
         AddressBookModel expectedModel = new AddressBookModelManager();
+        TransactionBookModel tbModel = new TransactionBookModelManager();
 
-        CommonModelManager.initialise(model, null);
+        CommonModelManager.initialise(model, tbModel);
 
         CommandTestUtil.assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
@@ -27,9 +32,23 @@ public class ClearCommandTest {
         AddressBookModel expectedModel = new AddressBookModelManager(
                 TypicalPersons.getTypicalAddressBook());
         expectedModel.setAddressBook(new AddressBook());
+        TransactionBookModel tbModel = new TransactionBookModelManager();
 
-        CommonModelManager.initialise(model, null);
+        CommonModelManager.initialise(model, tbModel);
         CommandTestUtil.assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_nonEmptyTransactionBook_success() {
+        TransactionBookModel model = new TransactionBookModelManager(TypicalTransactions.getTypicalTransactionBook());
+        TransactionBookModel expectedModel = new TransactionBookModelManager(
+                TypicalTransactions.getTypicalTransactionBook());
+        expectedModel.setTransactionBook(new TransactionBook());
+
+        spleetwaise.transaction.logic.commands.CommandTestUtil.assertCommandSuccess(
+                new spleetwaise.transaction.logic.commands.ClearCommand(), model,
+                spleetwaise.transaction.logic.commands.ClearCommand.MESSAGE_SUCCESS, expectedModel
+        );
     }
 
 }
