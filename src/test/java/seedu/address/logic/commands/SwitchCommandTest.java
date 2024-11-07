8d694c1b44d9586b498d86c76dbc52e9033cad44
Wei Lian <weilian2003@live.com>
2024-11-07 19:22:15 +0800

    update switch test cases

--- src/test/java/seedu/address/logic/commands/SwitchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SwitchCommandTest.java
@@ -5,19 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.profile.Profile;
+
 public class SwitchCommandTest {
+    private Model model;
+    private Profile currentProfile;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager();
+        currentProfile = new Profile("current-profile");
+        model.setAddressBookFilePath(currentProfile.toPath());
+    }
+
     @Test
     public void execute_switchToValidDifferentProfile_success() throws CommandException {
         Profile validProfile = new Profile("new-profile");
-        Model model = new ModelManager();
-        model.addToProfiles(new Profile("current-profile"));
-        model.setAddressBookFilePath(Profile.profileStringToPath("current-profile"));
+        model.addToProfiles(validProfile);
 
         SwitchCommand switchCommand = new SwitchCommand(validProfile);
         CommandResult result = switchCommand.execute(model);
@@ -26,43 +36,30 @@ public class SwitchCommandTest {
     }
 
     @Test
-    public void execute_boundaryProfileNames_success() throws CommandException {
+    public void execute_switchToMinLengthProfile_success() throws CommandException {
         Profile minLengthProfile = new Profile("a");
-        Model model = new ModelManager();
-        model.addToProfiles(new Profile("current-profile"));
         model.addToProfiles(minLengthProfile);
-        model.setAddressBookFilePath(Profile.profileStringToPath("current-profile"));
 
         SwitchCommand minSwitchCommand = new SwitchCommand(minLengthProfile);
         CommandResult minResult = minSwitchCommand.execute(model);
-        assertEquals(String.format(SwitchCommand.MESSAGE_SUCCESS, "a"), minResult.getFeedbackToUser());
 
+        assertEquals(String.format(SwitchCommand.MESSAGE_SUCCESS, "a"), minResult.getFeedbackToUser());
+    }
+
+    @Test
+    public void execute_switchToMaxLengthProfile_success() throws CommandException {
         Profile maxLengthProfile = new Profile("a".repeat(30));
         model.addToProfiles(maxLengthProfile);
 
         SwitchCommand maxSwitchCommand = new SwitchCommand(maxLengthProfile);
         CommandResult maxResult = maxSwitchCommand.execute(model);
-        assertEquals(String.format(SwitchCommand.MESSAGE_SUCCESS, "a".repeat(30)),
-                maxResult.getFeedbackToUser());
+
+        assertEquals(String.format(SwitchCommand.MESSAGE_SUCCESS, "a".repeat(30)), maxResult.getFeedbackToUser());
     }
 
     @Test
-    public void execute_emptyProfileListAvailableProfiles() {
-        Model model = new ModelManager();
-        model.addToProfiles(new Profile("profile1"));
-        model.addToProfiles(new Profile("profile2"));
-
-        SwitchCommand switchCommand = new SwitchCommand(Profile.getEmptyProfile());
-
-        assertThrows(CommandException.class,
-                String.format(SwitchCommand.MESSAGE_SHOW_PROFILES, "profile1, profile2", "profile1"), () ->
-                        switchCommand.execute(model)
-        );
-    }
-
-    @Test
-    public void execute_noProfilesAvailable_errorMessage() {
-        Model model = new ModelManager();
+    public void execute_noProfilesAvailable_throwsCommandException() {
+        model = new ModelManager(); // Reset model to ensure no profiles
         SwitchCommand switchCommand = new SwitchCommand(Profile.getEmptyProfile());
 
         assertThrows(CommandException.class,
@@ -72,27 +69,32 @@ public class SwitchCommandTest {
     }
 
     @Test
-    public void execute_switchToCurrentProfile_error() {
-        Profile currentProfile = new Profile("active-profile");
-        Model model = new ModelManager();
-        model.addToProfiles(currentProfile);
-        model.setAddressBookFilePath(currentProfile.toPath());
-
-        SwitchCommand switchCommand = new SwitchCommand(currentProfile);
+    public void execute_profilesAvailable_showsAvailableProfiles() {
+        model.addToProfiles(new Profile("profile1"));
+        model.addToProfiles(new Profile("profile2"));
+        SwitchCommand switchCommand = new SwitchCommand(Profile.getEmptyProfile());
 
         assertThrows(CommandException.class,
-                String.format(SwitchCommand.MESSAGE_NO_SWITCH, "active-profile"), () -> switchCommand.execute(model)
+                String.format(SwitchCommand.MESSAGE_SHOW_PROFILES, "profile1, profile2", "profile1"), () ->
+                        switchCommand.execute(model)
         );
     }
 
     @Test
-    public void execute_invalidProfileName_exceptionThrown() {
-        Profile validProfile = new Profile("valid-profile");
+    public void execute_switchToCurrentProfile_throwsError() {
+        SwitchCommand switchCommand = new SwitchCommand(currentProfile);
+
+        assertThrows(CommandException.class,
+                String.format(SwitchCommand.MESSAGE_NO_SWITCH, "current-profile"), () -> switchCommand.execute(model)
+        );
+    }
+
+    @Test
+    public void execute_invalidProfileName_throwsException() {
         String illegalModifiedProfileName = " space-in-name";
-        Model model = new ModelManager();
         model.setAddressBookFilePath(Profile.profileStringToPath(illegalModifiedProfileName));
 
-        SwitchCommand switchCommand = new SwitchCommand(validProfile);
+        SwitchCommand switchCommand = new SwitchCommand(new Profile("valid-profile"));
 
         assertThrows(CommandException.class,
                 SwitchCommand.MESSAGE_ILLEGAL_MODIFICATION, () -> switchCommand.execute(model)
@@ -100,13 +102,11 @@ public class SwitchCommandTest {
     }
 
     @Test
-    public void execute_invalidProfileNameWithSubDir_exceptionThrown() {
-        Profile validProfile = new Profile("valid-profile");
-        String illegalModifiedProfileName = "data/name";
-        Model model = new ModelManager();
-        model.setAddressBookFilePath(Profile.profileStringToPath(illegalModifiedProfileName));
+    public void execute_invalidProfilePathWithSubDir_throwsException() {
+        String illegalModifiedProfilePath = "data/name";
+        model.setAddressBookFilePath(Profile.profileStringToPath(illegalModifiedProfilePath));
 
-        SwitchCommand switchCommand = new SwitchCommand(validProfile);
+        SwitchCommand switchCommand = new SwitchCommand(new Profile("valid-profile"));
 
         assertThrows(CommandException.class,
                 SwitchCommand.MESSAGE_ILLEGAL_MODIFICATION, () -> switchCommand.execute(model)
@@ -122,10 +122,8 @@ public class SwitchCommandTest {
         SwitchCommand switchCommand3 = new SwitchCommand(profile2);
 
         assertEquals(switchCommand1, switchCommand2);
-        assertEquals(switchCommand1, switchCommand1);
         assertNotEquals(switchCommand1, switchCommand3);
         assertNotEquals("not a SwitchCommand", switchCommand1);
         assertNotEquals(null, switchCommand1);
     }
-
 }
