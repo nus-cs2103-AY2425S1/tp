cd2eeb1bb995cc0dbfe6607d16106744dbbddf94
Jaynon <jaynonfoong@hotmail.com>
2024-11-09 14:40:24 +0800

    Update address description
    Fixed dg issues

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -9,7 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values except a whitespace followed by a "
+            + "character and '/', and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
