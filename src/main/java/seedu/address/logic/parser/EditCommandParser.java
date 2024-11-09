08d78751cb4a98e53ff58fe1a6b9c7f1c7772dd3
collintanyq@gmail.com <collintanyq@gmail.com>
2024-11-09 11:18:50 +0800

    Remove unused code

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -8,7 +8,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
