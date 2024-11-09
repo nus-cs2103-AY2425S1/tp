f7a2f658b24c97d696662c3c9bddea90db7d7fbc
collintanyq@gmail.com <collintanyq@gmail.com>
2024-11-09 16:07:03 +0800

    Revert changes made to the error message

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -30,9 +30,8 @@ public class FilterCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filter all persons whose details contain all "
             + "the specified keywords (case-insensitive) as condition and display them as  a list with index numbers.\n"
-            + "At least one optional field must be given.\n"
-            + "Parameters: [n/NAME] [p/PHONE] [g/GENDER] [t/TAG]... [m/MODULE]...\n"
-            + "Example: " + COMMAND_WORD + " n/Alex m/Chemistry";
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " n/joe m/MA1521";
 
     private final FilterPersonDescriptor filterPersonDescriptor;
     private final FilterPredicate predicate;
