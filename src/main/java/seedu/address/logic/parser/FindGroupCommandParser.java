d42dad5bf22010593d2be353257cd318f1d868df
Shaun <Hundin231@gmail.com>
2024-11-09 20:33:07 +0800

    Change findGroup to search for a single keyword

--- src/main/java/seedu/address/logic/parser/FindGroupCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindGroupCommandParser.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.Arrays;
+import java.util.List;
 
 import seedu.address.logic.commands.FindGroupCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -18,16 +19,16 @@ public class FindGroupCommandParser implements Parser<FindGroupCommand> {
      * and returns a FindGroupCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public FindGroupCommand parse(String args) throws ParseException {
+    public FindGroupCommand parse(String arg) throws ParseException {
 
-        String trimmedArgs = args.trim();
+        String trimmedArgs = arg.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindGroupCommand.MESSAGE_USAGE));
         }
 
-        String[] groupNameKeywords = trimmedArgs.split("\\s+");
-        return new FindGroupCommand(new GroupContainsKeywordsPredicate(Arrays.asList(groupNameKeywords)));
+        List<String> keyword = Arrays.asList(trimmedArgs);
+        return new FindGroupCommand(new GroupContainsKeywordsPredicate(keyword));
 
     }
 
