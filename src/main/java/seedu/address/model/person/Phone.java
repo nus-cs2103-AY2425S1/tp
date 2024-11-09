28bcc563ddb770861d9f1e7a7b0be4426b825aa0
appleshill <eliot@u.nus.edu>
2024-11-09 23:08:49 +0800

    Adjust phone message

--- src/main/java/seedu/address/model/person/Phone.java
+++ src/main/java/seedu/address/model/person/Phone.java
@@ -11,7 +11,7 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
+            "Phone numbers may begin with a '+', followed by numbers only, and it should be at least 3 digits long";
     // Updated regex to allow for numbers to start with +
     public static final String VALIDATION_REGEX = "\\+?\\d{3,}";
     public final String value;
