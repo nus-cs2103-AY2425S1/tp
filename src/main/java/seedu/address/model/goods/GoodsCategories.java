f149c6214f01a95a29c712c07fbdefd75ce3a659
Yan Hwee <yanhweeyee@gmail.com>
2024-11-10 18:17:55 +0800

    Refactor goods categories class

--- src/main/java/seedu/address/model/goods/GoodsCategories.java
+++ src/main/java/seedu/address/model/goods/GoodsCategories.java
@@ -1,12 +1,11 @@
 package seedu.address.model.goods;
 
 /**
- * Enum to represent Categories of Goods
+ * An enum to represent the categories of goods
  */
 public enum GoodsCategories {
-    CONSUMABLES,
-    LIFESTYLE,
-    SPECIALTY;
+
+    CONSUMABLES, LIFESTYLE, SPECIALTY;
 
     public static final String MESSAGE_UNKNOWN_CATEGORY =
             "Category should strictly be one of [CONSUMABLES, LIFESTYLE, SPECIALTY]";
