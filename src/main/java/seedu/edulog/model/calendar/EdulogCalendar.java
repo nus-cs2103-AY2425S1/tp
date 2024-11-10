1ed0cf2872eb5e4237c564d2d5df9c47d3aef390
kimxw <kimaya.wanjari@nuscomputing.com>
2024-11-10 23:50:23 +0800

    Fix checkstyle for main

--- src/main/java/seedu/edulog/model/calendar/EdulogCalendar.java
+++ src/main/java/seedu/edulog/model/calendar/EdulogCalendar.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.edulog.model.calendar.exceptions.DuplicateLessonException;
-import seedu.edulog.model.student.Student;
 
 /**
  * Calendar class
