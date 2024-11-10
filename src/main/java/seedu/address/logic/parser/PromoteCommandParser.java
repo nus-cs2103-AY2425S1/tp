f363a30d08087d1af67a2e6ef9ba500c30169242
LimErnest <e1122433@u.nus.edu>
2024-11-10 19:50:16 +0800

    Improve content of DG

--- src/main/java/seedu/address/logic/parser/PromoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/PromoteCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DemoteCommand;
 import seedu.address.logic.commands.PromoteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.ContractEndDate;
