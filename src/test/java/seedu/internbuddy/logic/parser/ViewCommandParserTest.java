817ff4dffe8f8e4cccce2a77ddc8f1767a1cc561
uniqly <simontong09@gmail.com>
2024-11-10 15:02:34 +0800

    Improve parser package test cases

--- src/test/java/seedu/internbuddy/logic/parser/ViewCommandParserTest.java
+++ src/test/java/seedu/internbuddy/logic/parser/ViewCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.internbuddy.logic.parser;
 
 import static seedu.internbuddy.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.internbuddy.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.internbuddy.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.internbuddy.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.internbuddy.testutil.TypicalIndexes.INDEX_FIRST_COMPANY;
@@ -15,11 +16,27 @@ public class ViewCommandParserTest {
 
     @Test
     public void parse_validArgs_returnsViewCommand() {
+        // no whitespace
         assertParseSuccess(parser, "1", new ViewCommand(INDEX_FIRST_COMPANY));
+
+        // whitespace before
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE + "1", new ViewCommand(INDEX_FIRST_COMPANY));
+
+        // whitespace after
+        assertParseSuccess(parser, "1" + PREAMBLE_WHITESPACE, new ViewCommand(INDEX_FIRST_COMPANY));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
+        // non positive integer
+        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
+
+        // not an integer
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "1 1", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
+
+        // empty string
+        assertParseFailure(parser, "", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "    ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewCommand.MESSAGE_USAGE));
     }
 }
