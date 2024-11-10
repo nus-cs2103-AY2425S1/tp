b6a1ea46b059aa3a39787e053a4c6553dde76357
WindiStone <e0958609@u.nus.edu>
2024-11-10 11:51:43 +0800

    Main Window

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -73,8 +73,9 @@ public class MainWindow extends UiPart<Stage> {
     }
 
     private void setAccelerators() {
-        setAccelerator(helpMenuItem, KeyCombination.valueOf("F11")); // changed
+        setAccelerator(helpMenuItem, KeyCombination.valueOf("F1")); // changed
     }
+
 
     /**
      * Sets the accelerator of a MenuItem.
