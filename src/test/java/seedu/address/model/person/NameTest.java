82406af7deb5e3f9431446f7edb29e774ac5bf8a
chuajunyu <chuajunyu1@gmail.com>
2024-11-11 00:27:01 +0800

    Fix checkstyle

--- src/test/java/seedu/address/model/person/NameTest.java
+++ src/test/java/seedu/address/model/person/NameTest.java
@@ -30,7 +30,7 @@ public class NameTest {
         assertFalse(Name.isValidName("^")); // only non-alphanumeric characters
         assertFalse(Name.isValidName("peter*")); // contains non-alphanumeric characters
         assertFalse(Name.isValidName("Lord Chapter Master Grandmaster Calgar Anduro Jackson Ultramarine"
-        + " David Roger Jackson Ray Jr 2nd dean"));  // 121 characters > 100 allowed
+                + " David Roger Jackson Ray Jr 2nd dean")); // 121 characters > 100 allowed
 
         // valid name
         assertTrue(Name.isValidName("peter jack")); // alphabets only
