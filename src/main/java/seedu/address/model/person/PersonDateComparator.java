9e14b60b8c6106515336c0407b41b0dbcddca7ef
neohengkai <neohengkai2002@gmail.com>
2024-11-10 21:21:04 +0800

    Make date comparators safer

--- src/main/java/seedu/address/model/person/PersonDateComparator.java
+++ src/main/java/seedu/address/model/person/PersonDateComparator.java
@@ -10,13 +10,18 @@ public class PersonDateComparator implements Comparator<Person> {
      * Compares two persons based on their names.
      */
     public int compare(Person p1, Person p2) {
+        // Place all potential hires at the bottom as 
+        // they should not have a contract end date
         boolean isEmployeeP1 = p1.isEmployee();
         boolean isEmployeeP2 = p2.isEmployee();
         if (!isEmployeeP1 && isEmployeeP2) {
             return 1;
         } else if (isEmployeeP1 && !isEmployeeP2) {
             return -1;
+        } else if (!isEmployeeP1 && !isEmployeeP2) {
+            return 0;
+        } else {
+            return p1.getContractEndDate().compareTo(p2.getContractEndDate());
         }
-        return p1.getContractEndDate().compareTo(p2.getContractEndDate());
     }
 }
