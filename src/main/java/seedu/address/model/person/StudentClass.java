908c1a8dbf532f64ad091b974b5b57752a12158a
hyxnnii <E1156838@u.nus.edu>
2024-11-10 21:09:11 +0800

    Increase code coverage
    PR does not pass code coverage.
    
    More tests are added.

--- src/main/java/seedu/address/model/person/StudentClass.java
+++ src/main/java/seedu/address/model/person/StudentClass.java
@@ -9,8 +9,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class StudentClass {
 
-    public static final String MESSAGE_CONSTRAINTS =
-            "Student's class names cannot be empty and should be alphanumeric";
+    public static final String MESSAGE_CONSTRAINTS = "Student's class names should be alphanumeric"; // alphanumeric
+    // and special characters
     public static final String VALIDATION_REGEX = "\\p{Alnum}+";
 
     public final String value;
