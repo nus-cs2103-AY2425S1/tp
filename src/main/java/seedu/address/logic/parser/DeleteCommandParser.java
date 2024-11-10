bbf06950284ab82f8744ac13c53582042b0cb4a9
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-11-10 13:28:37 +0800

    Add error message for when user does not input index for delete command

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -17,6 +17,9 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        if (args.trim().isEmpty()) {
+            throw new ParseException("Please enter a valid index.");
+        }
         try {
             Index index = ParserUtil.parseIndex(args);
             return new DeleteCommand(index);
