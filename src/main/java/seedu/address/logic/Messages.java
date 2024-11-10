e9510df1781544120d45f3a9d20b42629179a532
JagdeepSinghNUS <E1186181@u.nus.edu>
2024-11-10 10:51:14 +0800

    Fix styling for Message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,8 +14,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_TAG_PRICE_SEARCH = "Tags are alphanumeric, if you are searching " +
-            "for price, use the price command instead";
+    public static final String MESSAGE_INVALID_TAG_PRICE_SEARCH = "Tags are alphanumeric, if you are searching "
+            + "for price, use the price command instead";
     public static final String MESSAGE_INVALID_RESTAURANT_DISPLAYED_INDEX = "The restaurant index provided is invalid";
     public static final String MESSAGE_RESTAURANTS_LISTED_OVERVIEW = "%1$d restaurants listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
