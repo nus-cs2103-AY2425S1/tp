3ff4b69c317c99db84c5b2019bde5fa7754880d6
Vatinius <102.23.dylan.chan@gmail.com>
2024-11-10 15:53:43 +0800

    Edit acknowledgements to pass checkstyle

--- src/main/java/seedu/address/model/person/ContainsSpecificKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/ContainsSpecificKeywordsPredicate.java
@@ -6,7 +6,8 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
-// Solution below adapted from https://github.com/se-edu/addressbook-level3/blob/master/src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java.
+// Solution below adapted from https://github.com/se-edu/addressbook-level3/blob/master/src/main/java/seedu/address/
+// model/person/NameContainsKeywordsPredicate.java.
 /**
  * Tests that a {@code Person}'s {@code Name or Phone number or Address or Email or Tag}
  * matches all of the keywords given.
