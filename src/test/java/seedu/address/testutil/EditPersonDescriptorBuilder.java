c38cde36df5337554f3c52341cf5bd147a75952c
George <156641986+george-yeo@users.noreply.github.com>
2024-11-10 20:51:52 +0800

    Fix style

--- src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
+++ src/test/java/seedu/address/testutil/EditPersonDescriptorBuilder.java
@@ -10,7 +10,6 @@ import seedu.address.model.person.Age;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Income;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Notes;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
