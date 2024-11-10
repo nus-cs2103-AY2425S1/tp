f097ba3f78cef1d9416c3956291a265b2c3fbb6f
Jeremy <jeremytoo989@gmail.com>
2024-11-10 17:17:22 +0800

    Simplify error message

--- src/main/java/seedu/address/logic/commands/ImportCommand.java
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -43,7 +43,7 @@ public class ImportCommand extends Command {
         try {
             jsonFiles = converter.convertAllCsvFiles();
         } catch (ConverterException ce) {
-            throw new CommandException(ce);
+            throw new CommandException(ce.getMessage());
         }
 
         JsonImporter importer = new JsonImporter(jsonFiles);
