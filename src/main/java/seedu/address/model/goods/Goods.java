af1cd59e40ac3297fab752a59163977b725a05d7
Yan Hwee <yanhweeyee@gmail.com>
2024-11-10 20:37:00 +0800

    Revert goods back to class

--- src/main/java/seedu/address/model/goods/Goods.java
+++ src/main/java/seedu/address/model/goods/Goods.java
@@ -2,12 +2,17 @@ package seedu.address.model.goods;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.Objects;
+
 /**
  * An immutable class that represents goods.
  */
-public record Goods(GoodsName goodsName, GoodsCategories category) {
+public final class Goods {
 
     public static final String MESSAGE_CONSTRAINTS = "Product quantity cannot be negative or 0!";
+    private final GoodsName goodsName;
+    private final GoodsCategories category;
+
 
     /**
      * Constructs a {@Code Goods}.
@@ -16,8 +21,10 @@ public record Goods(GoodsName goodsName, GoodsCategories category) {
      * @param goodsName A valid goods name.
      * @param category  A category for the goods.
      */
-    public Goods {
+    public Goods(GoodsName goodsName, GoodsCategories category) {
         requireAllNonNull(goodsName, category);
+        this.goodsName = goodsName;
+        this.category = category;
     }
 
     /**
@@ -50,4 +57,18 @@ public record Goods(GoodsName goodsName, GoodsCategories category) {
         return otherGoods.goodsName.equals(this.goodsName)
                 && otherGoods.category.equals(this.category);
     }
+
+    public GoodsName goodsName() {
+        return goodsName;
+    }
+
+    public GoodsCategories category() {
+        return category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(goodsName, category);
+    }
+
 }
