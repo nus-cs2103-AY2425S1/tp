e6a7535c8e00a8e61a42ee37bba3023a9a1502b8
brianchoon <brianchoonweihao@gmail.com>
2024-11-11 03:52:50 +0800

    Fix minor English problem

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -32,7 +32,7 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
         assertFalse(Phone.isValidPhone("9312 1534")); // spaces within digits
         assertFalse(Phone.isValidPhone("124293842033123456")); // more than 17 numbers
-        assertFalse(Phone.isValidPhone("124293842 033123456")); // more than 17 numbers, split
+        assertFalse(Phone.isValidPhone("124293842 033123456")); // more than 17 numbers, split up
 
         // valid phone numbers
         assertTrue(Phone.isValidPhone("911")); // exactly 3 numbers
