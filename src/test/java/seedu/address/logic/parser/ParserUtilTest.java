b56968ab57f2481b469fa389db62ff7c93634035
t-leongchuan <156800445+t-leongchuan@users.noreply.github.com>
2024-11-11 22:08:56 +0800

    Improve code coverage for Parser Util for AppointmentDate

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -8,6 +8,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.log.AppointmentDate;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
@@ -20,6 +21,8 @@ public class ParserUtilTest {
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_STATUS = "CAUTION";
+    private static final String INVALID_DATE_FORMAT = "25 November 2024";
+    private static final String INVALID_DATE = "32 November 2024";
 
     private static final String VALID_NAME = "Rachel Walker";
     private static final String VALID_PHONE = "89013456";
@@ -27,6 +30,7 @@ public class ParserUtilTest {
     private static final String VALID_EMAIL = "rachel@example.com";
     private static final String VALID_STATUS = "HIGH";
     private static final String WHITESPACE = " \t\r\n";
+    private static final String VALID_DATE = "25 Nov 2024";
 
     @Test
     public void parseIndex_invalidInput_throwsParseException() {
@@ -161,5 +165,33 @@ public class ParserUtilTest {
         String statusWithWhitespace = WHITESPACE + VALID_STATUS + WHITESPACE;
         Status expectedStatus = new Status(VALID_STATUS);
         assertEquals(expectedStatus, ParserUtil.parseStatus(statusWithWhitespace));
+    }
+
+    @Test
+    public void parseAppointmentDate_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> ParserUtil.parseAppointmentDate((String) null));
+    }
+
+    @Test
+    public void parseAppointmentDate_invalidValueFormat_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseStatus(INVALID_DATE_FORMAT));
+    }
+
+    @Test
+    public void parseAppointmentDate_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseStatus(INVALID_DATE));
+    }
+
+    @Test
+    public void parseAppointmentDate_validValueWithWhitespace_returnsAppointmentDate() throws Exception {
+        String dateWithWhitespace = WHITESPACE + VALID_DATE + WHITESPACE;
+        AppointmentDate expectedAppointmentDate = new AppointmentDate(VALID_DATE);
+        assertEquals(expectedAppointmentDate, ParserUtil.parseAppointmentDate(dateWithWhitespace));
+    }
+
+    @Test
+    public void parseAppointmentDate_validValue_returnsAppointmentDate() throws Exception {
+        AppointmentDate expectedAppointmentDate = new AppointmentDate(VALID_DATE);
+        assertEquals(expectedAppointmentDate, ParserUtil.parseAppointmentDate(VALID_DATE));
     }
 }
