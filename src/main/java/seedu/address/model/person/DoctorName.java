e74b81cc131781499be41a9855620944ffc412a5
aldentantan <aldentanfk@gmail.com>
2024-11-11 09:14:27 +0800

    Fix Doctor Name Regex

--- src/main/java/seedu/address/model/person/DoctorName.java
+++ src/main/java/seedu/address/model/person/DoctorName.java
@@ -13,7 +13,7 @@ public class DoctorName extends Name {
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}-.()@/'][\\p{Alnum}-.()@/' ]*";
 
     public final String doctorName;
     /**
