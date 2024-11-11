6ff95e2b35d7066ba86d36352c88eb322322044d
devivy <divyalath.nus@gmail.com>
2024-11-11 21:46:56 +0800

    Correct Checkstyles 3

--- src/test/java/tahub/contacts/logic/commands/course/CoursePersonAddCommandTest.java
+++ src/test/java/tahub/contacts/logic/commands/course/CoursePersonAddCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static tahub.contacts.commons.util.CollectionUtil.requireAllNonNull;
 import static tahub.contacts.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
