a36467e3a40ee70d6edc77e68948334cfe4fd704
choonzies <pohjingchoon@gmail.com>
2024-11-12 06:21:06 +0800

    Add testing for Delete Appointment Command parser

--- src/test/java/seedu/address/logic/parser/DeleteAppointmentParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteAppointmentParserTest.java
@@ -7,6 +7,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteAppointmentCommand;
 
 /**
@@ -20,14 +21,57 @@ public class DeleteAppointmentParserTest {
 
     private DeleteAppointmentCommandParser parser = new DeleteAppointmentCommandParser();
 
+    // EP [1..INT_MAX]
     @Test
-    public void parse_validArgs_returnsDeleteCommand() {
+    public void parse_validArgsSpecificIndex_returnsDeleteAppointmentCommand() {
+        assertParseSuccess(parser, "627642", new DeleteAppointmentCommand(Index.fromOneBased(627642)));
+    }
+
+    // EP [1..INT_MAX], BV test (1)
+    @Test
+    public void parse_validArgsFirstIndex_returnsDeleteAppointmentCommand() {
         assertParseSuccess(parser, "1", new DeleteAppointmentCommand(INDEX_FIRST_PERSON));
     }
 
+    // EP [1..INT_MAX], BV test (INT_MAX)
     @Test
-    public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteAppointmentCommand.MESSAGE_USAGE));
+    public void parse_validArgsIntMax_returnsDeleteAppointmentCommand() {
+        assertParseSuccess(parser, "2147483647", new DeleteAppointmentCommand(Index.fromOneBased(2147483647)));
+    }
+
+    // EP [-MAX_INT..0]
+    @Test
+    public void parse_invalidArgsSpecificIndex_throwsParseException() {
+        assertParseFailure(parser, "-74214", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+
+    // EP [-MAX_INT..0], BV test (-INT_MAX)
+    @Test
+    public void parse_invalidArgsNegativeIntMax_throwsParseException() {
+        assertParseFailure(parser, "-2147483647", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+
+    // EP [-MAX_INT..0], BV test (0)
+    @Test
+    public void parse_invalidArgsZero_throwsParseException() {
+        assertParseFailure(parser, "0", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+
+    // EP [<MAX_INT]
+    @Test
+    public void parse_invalidArgsLessThanMaxInt_throwsParseException() {
+        assertParseFailure(parser, "-2147483648", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+
+    // EP [>MAX_INT]
+    @Test
+    public void parse_invalidArgsZeroMoreThanMaxInt_throwsParseException() {
+        assertParseFailure(parser, "2147483648", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
+    }
+
+    // EP non-integers parsed
+    @Test
+    public void parse_invalidArgsNonInteger_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAppointmentCommand.MESSAGE_USAGE));
     }
 }
