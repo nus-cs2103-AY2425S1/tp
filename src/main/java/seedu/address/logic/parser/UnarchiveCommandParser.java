8ee8bc33d3e202addcff6181452bef56477aed06
xGladiate <sharonsohxuanhui@hotmail.com>
2024-11-12 00:42:47 +0800

    Resolve bug: Help message for archive and unarchive command

--- src/main/java/seedu/address/logic/parser/UnarchiveCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UnarchiveCommandParser.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import java.util.List;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.ArchiveCommand;
 import seedu.address.logic.commands.UnarchiveCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -23,8 +24,13 @@ public class UnarchiveCommandParser implements Parser<UnarchiveCommand> {
             List<Index> indexes = ParserUtil.parseIndex(args);
             return new UnarchiveCommand(indexes);
         } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnarchiveCommand.MESSAGE_USAGE), pe);
+            if (AddressBookParser.getInspect()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnarchiveCommand.MESSAGE_USAGE_DELIVERY), pe);
+            } else {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnarchiveCommand.MESSAGE_USAGE_PERSON), pe);
+            }
         }
     }
 
