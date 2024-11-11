ae31c32ccfeb5876489dd98abaa0f27d54b57903
hansneddyanto <hans.neddy@gmail.com>
2024-11-12 00:21:30 +0800

    Add tests and disallow duplicate predicate in massreject

--- src/main/java/seedu/address/logic/parser/MassRejectCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MassRejectCommandParser.java
@@ -1,7 +1,11 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_JOBCODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.MassRejectCommand;
@@ -28,6 +32,9 @@ public class MassRejectCommandParser implements Parser<MassRejectCommand> {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, MassRejectCommand.MESSAGE_USAGE));
         }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_JOBCODE,
+                PREFIX_TAG, PREFIX_REMARK);
 
         if (argMultimap.getValue(PREFIX_JOBCODE).isPresent() && argMultimap.getValue(PREFIX_TAG).isPresent()) {
             // Job Code and Tag mass reject
