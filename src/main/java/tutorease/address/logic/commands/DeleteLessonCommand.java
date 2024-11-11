d4eaa9e7ff274692d812229f5b648ba185635ae6
Ma-Xizi <124993978+Ma-Xizi@users.noreply.github.com>
2024-11-11 14:05:10 +0800

    Add more loggings

--- src/main/java/tutorease/address/logic/commands/DeleteLessonCommand.java
+++ src/main/java/tutorease/address/logic/commands/DeleteLessonCommand.java
@@ -1,7 +1,6 @@
 package tutorease.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static tutorease.address.logic.Messages.format;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
