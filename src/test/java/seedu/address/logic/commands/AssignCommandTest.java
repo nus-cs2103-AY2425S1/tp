63e629a88e2d857e4013510d976ad8e5c2239c70
Kan_Jun_Hong <junhongkannot@gmail.com>
2024-11-12 01:02:24 +0800

    Set up AssignCommandTest

--- src/test/java/seedu/address/logic/commands/AssignCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AssignCommandTest.java
@@ -1,4 +1,22 @@
 package seedu.address.logic.commands;
 
+import seedu.address.model.AddressBook;
+import seedu.address.testutil.TypicalEvents;
+import seedu.address.testutil.TypicalVolunteers;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class AssignCommandTest {
+    AddressBook addressBook = new AddressBook();
+
+
+    @BeforeEach
+    public void resetAddressBook() {
+        addressBook = new AddressBook();
+        addressBook.addEvent(TypicalEvents.EVENT_A);
+        addressBook.addVolunteer(TypicalVolunteers.ALICE);
+    }
+
+    
+
 }
