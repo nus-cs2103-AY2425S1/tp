03f5d4ef36f7a38c3f3ee5fc8b08e7fbeb67d36d
Nicholas Koh Zi Lun <nicholaskoh2@gmail.com>
2024-11-11 14:03:01 +0800

    Update UG to describe parameters' constraints

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -14,8 +14,7 @@ public class Name {
                     + "and it should start with an alphabet";
 
     /*
-     * The first character of the address must not be a whitespace,
-     * otherwise " " (a blank string) becomes a valid input.
+     * The first character of the name must be an alphabet.
      */
     public static final String VALIDATION_REGEX = "^[A-Za-z][\\p{Alnum} ]*$";
 
