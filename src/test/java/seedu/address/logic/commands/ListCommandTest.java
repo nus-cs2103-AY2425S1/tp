bb3696e6e75f6d0dd8db414806a260ada2bfb1d6
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2024-11-11 15:19:52 +0800

    Update tests to also test missing fields

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -12,22 +12,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
+import seedu.address.testutil.PersonBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
  */
 public class ListCommandTest {
 
+    private static final Person GHOST = new PersonBuilder().withName("Ghost").withPhone("12345678").build();
+
     private Model model;
     private Model expectedModel;
 
     @BeforeEach
     public void setUp() {
-        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        AddressBook ab = getTypicalAddressBook();
+        // Add a person with all optional fields missing
+        ab.addPerson(GHOST);
+        model = new ModelManager(ab, new UserPrefs());
         expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
     }
 
