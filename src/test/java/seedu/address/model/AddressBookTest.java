908f5cb63cea86eabb1d57c3d08d8edf727774d9
Chua Tse Hui <htcylsg@gmail.com>
2024-11-29 21:45:23 +0800

    Shift author tag in AddressBookTest.java

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -1,3 +1,4 @@
+//@@author
 package seedu.address.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,7 +23,6 @@ import seedu.address.model.contact.Contact;
 import seedu.address.model.contact.exceptions.DuplicateContactException;
 import seedu.address.testutil.ContactBuilder;
 
-//@@author
 public class AddressBookTest {
 
     private final AddressBook addressBook = new AddressBook();
