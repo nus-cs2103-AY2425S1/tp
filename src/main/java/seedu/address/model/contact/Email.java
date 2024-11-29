c583c0a5043fa6530af81b3c6990e87af31e2816
Chua Tse Hui <htcylsg@gmail.com>
2024-11-29 13:59:44 +0800

    Add no author in Email.java and ContactException2

--- src/main/java/seedu/address/model/contact/Email.java
+++ src/main/java/seedu/address/model/contact/Email.java
@@ -1,3 +1,4 @@
+//@@author
 package seedu.address.model.contact;
 
 import static java.util.Objects.requireNonNull;
--- src/main/java/seedu/address/model/contact/exceptions/DuplicateContactException.java
+++ src/main/java/seedu/address/model/contact/exceptions/DuplicateContactException.java
@@ -1,3 +1,4 @@
+//@@author
 package seedu.address.model.contact.exceptions;
 
 /**
